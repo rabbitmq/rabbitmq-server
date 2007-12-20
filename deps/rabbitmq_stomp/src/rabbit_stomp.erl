9bb56d7abdbec03a6b935ea17432eef38403fc84
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2007-12-20 22:57:58 +0000

    [project @ Comments and license information at the top of source files.]

--- deps/rabbitmq_stomp/src/rabbit_stomp.erl
+++ deps/rabbitmq_stomp/src/rabbit_stomp.erl
@@ -1,3 +1,33 @@
+%%   The contents of this file are subject to the Mozilla Public License
+%%   Version 1.1 (the "License"); you may not use this file except in
+%%   compliance with the License. You may obtain a copy of the License at
+%%   http://www.mozilla.org/MPL/
+%%
+%%   Software distributed under the License is distributed on an "AS IS"
+%%   basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%%   License for the specific language governing rights and limitations
+%%   under the License.
+%%
+%%   The Original Code is RabbitMQ.
+%%
+%%   The Initial Developers of the Original Code are LShift Ltd.,
+%%   Cohesive Financial Technologies LLC., and Rabbit Technologies Ltd.
+%%
+%%   Portions created by LShift Ltd., Cohesive Financial
+%%   Technologies LLC., and Rabbit Technologies Ltd. are Copyright (C) 
+%%   2007 LShift Ltd., Cohesive Financial Technologies LLC., and Rabbit 
+%%   Technologies Ltd.; 
+%%
+%%   All Rights Reserved.
+%%
+%%   Contributor(s): ______________________________________.
+%%
+
+%% rabbit_stomp implements STOMP messaging semantics, as per protocol
+%% "version 1.0", at http://stomp.codehaus.org/Protocol
+%%
+%% Implemented: everything but transactions.
+
 -module(rabbit_stomp).
 
 -export([kickstart/0,
