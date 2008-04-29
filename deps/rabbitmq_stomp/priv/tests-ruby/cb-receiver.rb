ac84c959beeb0c466f480e63c3a57c9f5fd695ce
Tony Garnock-Jones <tonyg@lshift.net>
2008-04-29 18:21:36 +0100

    Simplify and remove delays in the cb-receiver program.

--- deps/rabbitmq_stomp/priv/tests-ruby/cb-receiver.rb
+++ deps/rabbitmq_stomp/priv/tests-ruby/cb-receiver.rb
@@ -5,9 +5,5 @@ conn = Stomp::Connection.open('guest', 'guest', 'localhost')
 conn.subscribe('carl')
 while true
   mesg = conn.receive
-
-  puts mesg if mesg
-
   puts mesg.body if mesg
-  sleep 0.0001
 end
