be90e8b4e7d163466e9c1dba18190451a5999ad4
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2008-06-01 20:54:15 +0100

    Make the example actually *use* a durable declaration :-)

--- deps/rabbitmq_stomp/examples/ruby/persistent-receiver.rb
+++ deps/rabbitmq_stomp/examples/ruby/persistent-receiver.rb
@@ -2,8 +2,7 @@ require 'rubygems'
 require 'stomp'
 
 conn = Stomp::Connection.open('guest', 'guest', 'localhost')
-#conn.subscribe('/queue/durable', :'auto-delete' => false, :durable => true)
-conn.subscribe('/queue/durable')
+conn.subscribe('/queue/durable', :'auto-delete' => false, :durable => true)
 
 puts "Waiting for messages..."
 
