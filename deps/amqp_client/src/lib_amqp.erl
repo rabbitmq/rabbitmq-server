6f40756e7f97d80e43fe08a10b4846ec1c8afd21
Ben Hood <0x6e6562@gmail.com>
2008-09-10 13:20:17 +0100

    Removed tickets and realms

--- deps/amqp_client/src/lib_amqp.erl
+++ deps/amqp_client/src/lib_amqp.erl
@@ -13,28 +13,18 @@ start_connection(Host) ->
     amqp_connection:start("guest", "guest", Host).    
 
 start_channel(Connection) ->
-    Realm = <<"/data">>,
-    Channel = amqp_connection:open_channel(Connection),
-    Access = #'access.request'{realm = Realm,
-                               exclusive = false,
-                               passive = true,
-                               active = true,
-                               write = true,
-                               read = true},
-    #'access.request_ok'{ticket = Ticket} = amqp_channel:call(Channel, Access),
-    {Channel,Ticket}.
+    amqp_connection:open_channel(Connection).
     
-declare_exchange(Channel,Ticket,X) ->
-    ExchangeDeclare = #'exchange.declare'{ticket = Ticket, exchange = X,
+declare_exchange(Channel,X) ->
+    ExchangeDeclare = #'exchange.declare'{exchange = X,
                                           type = <<"direct">>,
                                           passive = false, durable = false, 
                                           auto_delete = false, internal = false,
                                           nowait = false, arguments = []},
     amqp_channel:call(Channel, ExchangeDeclare).
     
-publish(Channel,Ticket,X,RoutingKey,Payload) ->
-    BasicPublish = #'basic.publish'{ticket = Ticket,
-                                    exchange = X,
+publish(Channel,X,RoutingKey,Payload) ->
+    BasicPublish = #'basic.publish'{exchange = X,
                                     routing_key = RoutingKey,
                                     mandatory = false,immediate = false},
     Content = #content{class_id = 60,
@@ -52,34 +42,34 @@ teardown(Connection,Channel) ->
     amqp_connection:close(Connection, ConnectionClose).
     
     
-subscribe(Channel,Ticket,Q,Consumer) ->
-    BasicConsume = #'basic.consume'{ticket = Ticket, queue = Q,
+subscribe(Channel,Q,Consumer) ->
+    BasicConsume = #'basic.consume'{queue = Q,
                                     no_local = false, no_ack = true,
                                     exclusive = false, nowait = false},
     #'basic.consume_ok'{consumer_tag = ConsumerTag} = amqp_channel:call(Channel,BasicConsume, Consumer),
     ConsumerTag.
 
-unsubscribe(Channel,Ticket,Tag) ->
+unsubscribe(Channel,Tag) ->
     BasicCancel = #'basic.cancel'{consumer_tag = Tag, nowait = false},
     #'basic.cancel_ok'{consumer_tag = ConsumerTag} = amqp_channel:call(Channel,BasicCancel),
     ok.
 
-declare_queue(Channel,Ticket,Q) ->
-    QueueDeclare = #'queue.declare'{ticket = Ticket, queue = Q,
+declare_queue(Channel,Q) ->
+    QueueDeclare = #'queue.declare'{queue = Q,
                                     passive = false, durable = false,
                                     exclusive = false, auto_delete = false,
                                     nowait = false, arguments = []},
     #'queue.declare_ok'{} = amqp_channel:call(Channel, QueueDeclare).
 
-delete_queue(Channel,Ticket,Q) ->
-    QueueDelete = #'queue.delete'{ticket = Ticket, queue = Q,
+delete_queue(Channel,Q) ->
+    QueueDelete = #'queue.delete'{queue = Q,
                                   if_unused = false,
                                   if_empty = false,
                                   nowait = false},
     #'queue.delete_ok'{} = amqp_channel:call(Channel, QueueDelete).
 
-bind_queue(Channel,Ticket,X,Q,Binding) ->
-    QueueBind = #'queue.bind'{ticket = Ticket, queue = Q, exchange = X,
+bind_queue(Channel,X,Q,Binding) ->
+    QueueBind = #'queue.bind'{queue = Q, exchange = X,
                               routing_key = Binding, nowait = false, arguments = []},
     #'queue.bind_ok'{} = amqp_channel:call(Channel, QueueBind).       
 
