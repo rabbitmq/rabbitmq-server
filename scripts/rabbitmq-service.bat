d7386e8fc36b35d4d669c77d35e794c17ce643da
Tony Garnock-Jones <tonyg@lshift.net>
2008-12-08 20:20:44 +0000

    Add RABBITMQ_ prefix, and source /etc/default/rabbitmq. Update erlang code for the new environment variable names.

--- scripts/rabbitmq-service.bat
+++ scripts/rabbitmq-service.bat
@@ -24,24 +24,24 @@ REM
 REM   Contributor(s): ______________________________________.
 REM
 
-if "%SERVICENAME%"=="" (
-    set SERVICENAME=RabbitMQ
+if "%RABBITMQ_SERVICENAME%"=="" (
+    set RABBITMQ_SERVICENAME=RabbitMQ
 )
 
 if "%RABBITMQ_BASE%"=="" (
-    set RABBITMQ_BASE=%APPDATA%\%SERVICENAME%
+    set RABBITMQ_BASE=%APPDATA%\%RABBITMQ_SERVICENAME%
 )
 
-if "%NODENAME%"=="" (
-    set NODENAME=rabbit
+if "%RABBITMQ_NODENAME%"=="" (
+    set RABBITMQ_NODENAME=rabbit
 )
 
-if "%NODE_IP_ADDRESS%"=="" (
-    set NODE_IP_ADDRESS=0.0.0.0
+if "%RABBITMQ_NODE_IP_ADDRESS%"=="" (
+    set RABBITMQ_NODE_IP_ADDRESS=0.0.0.0
 )
 
-if "%NODE_PORT%"=="" (
-    set NODE_PORT=5672
+if "%RABBITMQ_NODE_PORT%"=="" (
+    set RABBITMQ_NODE_PORT=5672
 )
 
 if "%ERLANG_SERVICE_MANAGER_PATH%"=="" (
@@ -50,9 +50,9 @@ if "%ERLANG_SERVICE_MANAGER_PATH%"=="" (
 
 set CONSOLE_FLAG=
 set CONSOLE_LOG_VALID=
-for %%i in (new reuse) do if "%%i" == "%CONSOLE_LOG%" set CONSOLE_LOG_VALID=TRUE
+for %%i in (new reuse) do if "%%i" == "%RABBITMQ_CONSOLE_LOG%" set CONSOLE_LOG_VALID=TRUE
 if "%CONSOLE_LOG_VALID%" == "TRUE" (
-    set CONSOLE_FLAG=-debugtype %CONSOLE_LOG%
+    set CONSOLE_FLAG=-debugtype %RABBITMQ_CONSOLE_LOG%
 )
 
 rem *** End of configuration ***
@@ -80,11 +80,11 @@ rem Log management (rotation, filtering based on size...) is left as an exercise
 
 set BACKUP_EXTENSION=.1
 
-set LOGS="%RABBITMQ_BASE%\log\%NODENAME%.log"
-set SASL_LOGS="%RABBITMQ_BASE%\log\%NODENAME%-sasl.log"
+set LOGS="%RABBITMQ_BASE%\log\%RABBITMQ_NODENAME%.log"
+set SASL_LOGS="%RABBITMQ_BASE%\log\%RABBITMQ_NODENAME%-sasl.log"
 
-set LOGS_BACKUP="%RABBITMQ_BASE%\log\%NODENAME%.log%BACKUP_EXTENSION%"
-set SASL_LOGS_BACKUP="%RABBITMQ_BASE%\log\%NODENAME%-sasl.log%BACKUP_EXTENSION%"
+set LOGS_BACKUP="%RABBITMQ_BASE%\log\%RABBITMQ_NODENAME%.log%BACKUP_EXTENSION%"
+set SASL_LOGS_BACKUP="%RABBITMQ_BASE%\log\%RABBITMQ_NODENAME%-sasl.log%BACKUP_EXTENSION%"
 
 if exist %LOGS% (
 	type %LOGS% >> %LOGS_BACKUP%
@@ -96,13 +96,15 @@ if exist %SASL_LOGS% (
 rem End of log management
 
 
-set CLUSTER_CONFIG_FILE=%RABBITMQ_BASE%\rabbitmq_cluster.config
+if "%RABBITMQ_CLUSTER_CONFIG_FILE%"=="" (
+    set RABBITMQ_CLUSTER_CONFIG_FILE=%RABBITMQ_BASE%\rabbitmq_cluster.config
+)
 set CLUSTER_CONFIG=
-if not exist "%CLUSTER_CONFIG_FILE%" GOTO L1
-set CLUSTER_CONFIG=-rabbit cluster_config \""%CLUSTER_CONFIG_FILE:\=/%"\"
+if not exist "%RABBITMQ_CLUSTER_CONFIG_FILE%" GOTO L1
+set CLUSTER_CONFIG=-rabbit cluster_config \""%RABBITMQ_CLUSTER_CONFIG_FILE:\=/%"\"
 :L1
 
-set MNESIA_DIR=%MNESIA_BASE%/%NODENAME%-mnesia
+set MNESIA_DIR=%MNESIA_BASE%/%RABBITMQ_NODENAME%-mnesia
 
 
 if "%1" == "install" goto INSTALL_SERVICE
@@ -114,16 +116,16 @@ echo Service control usage
 echo *********************
 echo.
 echo %~n0 help    - Display this help
-echo %~n0 install - Install the %SERVICENAME% service
-echo %~n0 remove  - Remove the %SERVICENAME% service
+echo %~n0 install - Install the %RABBITMQ_SERVICENAME% service
+echo %~n0 remove  - Remove the %RABBITMQ_SERVICENAME% service
 echo.
 echo The following actions can also be accomplished by using 
 echo Windows Services Management Console (services.msc):
 echo.
-echo %~n0 start   - Start the %SERVICENAME% service
-echo %~n0 stop    - Stop the %SERVICENAME% service
-echo %~n0 disable - Disable the %SERVICENAME% service
-echo %~n0 enable  - Enable the %SERVICENAME% service
+echo %~n0 start   - Start the %RABBITMQ_SERVICENAME% service
+echo %~n0 stop    - Stop the %RABBITMQ_SERVICENAME% service
+echo %~n0 disable - Disable the %RABBITMQ_SERVICENAME% service
+echo %~n0 enable  - Enable the %RABBITMQ_SERVICENAME% service
 echo.
 exit /B
 
@@ -134,11 +136,11 @@ if not exist "%RABBITMQ_BASE%" (
     echo Creating base directory %RABBITMQ_BASE% & md "%RABBITMQ_BASE%" 
 )
 
-"%ERLANG_SERVICE_MANAGER_PATH%\erlsrv" list %SERVICENAME% 2>NUL 1>NUL
+"%ERLANG_SERVICE_MANAGER_PATH%\erlsrv" list %RABBITMQ_SERVICENAME% 2>NUL 1>NUL
 if errorlevel 1 (
-    "%ERLANG_SERVICE_MANAGER_PATH%\erlsrv" add %SERVICENAME%
+    "%ERLANG_SERVICE_MANAGER_PATH%\erlsrv" add %RABBITMQ_SERVICENAME%
 ) else (
-    echo %SERVICENAME% service is already present - only updating service parameters
+    echo %RABBITMQ_SERVICENAME% service is already present - only updating service parameters
 )
 
 set RABBIT_EBIN=%~dp0..\ebin
@@ -151,10 +153,10 @@ set ERLANG_SERVICE_ARGUMENTS= ^
 +A30 ^
 -kernel inet_default_listen_options "[{nodelay,true},{sndbuf,16384},{recbuf,4096}]" ^
 -kernel inet_default_connect_options "[{nodelay,true}]" ^
--rabbit tcp_listeners "[{\"%NODE_IP_ADDRESS%\",%NODE_PORT%}]" ^
--kernel error_logger {file,\""%LOG_BASE%/%NODENAME%.log"\"} ^
+-rabbit tcp_listeners "[{\"%RABBITMQ_NODE_IP_ADDRESS%\",%RABBITMQ_NODE_PORT%}]" ^
+-kernel error_logger {file,\""%LOG_BASE%/%RABBITMQ_NODENAME%.log"\"} ^
 -sasl errlog_type error ^
--sasl sasl_error_logger {file,\""%LOG_BASE%/%NODENAME%-sasl.log"\"} ^
+-sasl sasl_error_logger {file,\""%LOG_BASE%/%RABBITMQ_NODENAME%-sasl.log"\"} ^
 -os_mon start_cpu_sup true ^
 -os_mon start_disksup false ^
 -os_mon start_memsup true ^
@@ -169,12 +171,12 @@ set ERLANG_SERVICE_ARGUMENTS= ^
 set ERLANG_SERVICE_ARGUMENTS=%ERLANG_SERVICE_ARGUMENTS:\=\\%
 set ERLANG_SERVICE_ARGUMENTS=%ERLANG_SERVICE_ARGUMENTS:"=\"%
 
-"%ERLANG_SERVICE_MANAGER_PATH%\erlsrv" set %SERVICENAME% ^
+"%ERLANG_SERVICE_MANAGER_PATH%\erlsrv" set %RABBITMQ_SERVICENAME% ^
 -machine "%ERLANG_SERVICE_MANAGER_PATH%\erl.exe" ^
 -env ERL_CRASH_DUMP="%RABBITMQ_BASE_UNIX%/log" ^
 -workdir "%RABBITMQ_BASE%" ^
 -stopaction "rabbit:stop_and_halt()." ^
--sname %NODENAME% ^
+-sname %RABBITMQ_NODENAME% ^
 %CONSOLE_FLAG% ^
 -args "%ERLANG_SERVICE_ARGUMENTS%" > NUL
 goto END
@@ -182,7 +184,7 @@ goto END
 
 :MODIFY_SERVICE
 
-"%ERLANG_SERVICE_MANAGER_PATH%\erlsrv" %1 %SERVICENAME%
+"%ERLANG_SERVICE_MANAGER_PATH%\erlsrv" %1 %RABBITMQ_SERVICENAME%
 goto END
 
 
--- src/rabbit_control.erl
+++ src/rabbit_control.erl
@@ -116,11 +116,12 @@ Available commands:
 Quiet output mode is selected with the \"-q\" flag. Informational messages
 are suppressed when quiet mode is in effect.
 
-<node> should be the name of the master node of the RabbitMQ cluster. It
-defaults to the node named \"rabbit\" on the local host. On a host named
-\"server.example.com\", the master node will usually be rabbit@server (unless
-NODENAME has been set to some non-default value at broker startup time). The
-output of hostname -s is usually the correct suffix to use after the \"@\" sign.
+<node> should be the name of the master node of the RabbitMQ
+cluster. It defaults to the node named \"rabbit\" on the local
+host. On a host named \"server.example.com\", the master node will
+usually be rabbit@server (unless RABBITMQ_NODENAME has been set to
+some non-default value at broker startup time). The output of hostname
+-s is usually the correct suffix to use after the \"@\" sign.
 
 "),
     halt(1).
