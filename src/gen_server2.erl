16e27751f94f5ffa847573457db546853627dec8
Matthias Radestock <matthias@lshift.net>
2009-07-24 13:49:02 +0100

    remove R13B-1ism

--- src/gen_server2.erl
+++ src/gen_server2.erl
@@ -152,10 +152,6 @@
 %%%  API
 %%%=========================================================================
 
--ifdef(use_specs).
--spec behaviour_info(atom()) -> 'undefined' | [{atom(), arity()}].
--endif.
-
 behaviour_info(callbacks) ->
     [{init,1},{handle_call,3},{handle_cast,2},{handle_info,2},
      {terminate,2},{code_change,3}];
