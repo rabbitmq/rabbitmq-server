dc0bfa67da4c0e7527fa691dcd0115ae5f2e55ed
Tony Garnock-Jones <tonyg@lshift.net>
2009-08-26 14:28:07 +0100

    Update to renamed interface.

--- src/rabbit_queue_backing_store.erl
+++ src/rabbit_queue_backing_store.erl
@@ -14,7 +14,7 @@ behaviour_info(callbacks) ->
      {publish, 2},
      {requeue, 2},
      {tx_commit, 3},
-     {tx_cancel, 2},
+     {tx_rollback, 2},
      {storage_mode, 1},
      {len, 1},
      {estimate_queue_memory_and_reset_counters, 1},
