c7d1554a90fed21739ecf67b46787b5f12f2e068
Ben Hood <0x6e6562@gmail.com>
2009-08-27 16:05:38 +0100

    Somehow some io:formats got merged in from the SSL branch I think

--- deps/amqp_client/include/amqp_client.hrl
+++ deps/amqp_client/include/amqp_client.hrl
@@ -80,3 +80,4 @@
 -define(LOG_DEBUG(Format), error_logger:info_msg(Format)).
 -define(LOG_INFO(Format, Args), error_logger:info_msg(Format, Args)).
 -define(LOG_WARN(Format, Args), error_logger:warning_msg(Format, Args)).
+
