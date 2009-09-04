1ec76ab881dda86c30314c100c95a44d0e7b0f46
Marek Majkowski <majek@lshift.net>
2009-09-04 17:45:48 +0100

    rabbitmqctl now accepts -nodename

--- src/rabbit_control.erl
+++ src/rabbit_control.erl
@@ -52,10 +52,12 @@
 %%----------------------------------------------------------------------------
 
 start() ->
+    {ok, [[Nodenamestr|_]|_]} = init:get_argument(nodename),
+    Nodename = list_to_atom(Nodenamestr),
     FullCommand = init:get_plain_arguments(),
     #params{quiet = Quiet, node = Node, command = Command, args = Args} = 
         parse_args(FullCommand, #params{quiet = false,
-                                        node = rabbit_misc:localnode(rabbit)}),
+                                        node = rabbit_misc:localnode(Nodename)}),
     Inform = case Quiet of
                  true  -> fun(_Format, _Args1) -> ok end;
                  false -> fun(Format, Args1) ->
