bbd784b6bb98f7a99481a3398d8bc8ca28b8dbcc
Matthew Sackman <matthew@lshift.net>
2010-01-26 17:04:02 +0000

    Added license boilerplates

--- deps/rabbitmq_shovel/src/rabbit_shovel_worker.erl
+++ deps/rabbitmq_shovel/src/rabbit_shovel_worker.erl
@@ -1,3 +1,24 @@
+%%   The contents of this file are subject to the Mozilla Public License
+%%   Version 1.1 (the "License"); you may not use this file except in
+%%   compliance with the License. You may obtain a copy of the License at
+%%   http://www.mozilla.org/MPL/
+%%
+%%   Software distributed under the License is distributed on an "AS IS"
+%%   basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%%   License for the specific language governing rights and limitations
+%%   under the License.
+%%
+%%   The Original Code is RabbitMQ-shovel.
+%%
+%%   The Initial Developers of the Original Code are LShift Ltd,
+%%   Cohesive Financial Technologies LLC, and Rabbit Technologies Ltd.
+%%
+%%   Copyright (C) 2010 LShift Ltd.
+%%
+%%   All Rights Reserved.
+%%
+%%   Contributor(s): ______________________________________.
+%%
 -module(rabbit_shovel_worker).
 -behaviour(gen_server).
 
