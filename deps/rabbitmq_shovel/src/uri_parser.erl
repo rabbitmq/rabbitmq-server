18498aa6acef853d2698dd7a8c1c6f94305ad11c
Matthew Sackman <matthew@lshift.net>
2010-01-27 12:27:27 +0000

    Modified uri parser to LShift performance specifications

--- deps/rabbitmq_shovel/src/uri_parser.erl
+++ deps/rabbitmq_shovel/src/uri_parser.erl
@@ -1,116 +1,125 @@
-%%
-%% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 2006-2009. All Rights Reserved.
-%% 
-%% The contents of this file are subject to the Erlang Public License,
+%% This file is a copy of http_uri.erl from the R13B-1 Erlang/OTP
+%% distribution with several modifications.
+
+%% All modifications are (C) 2009 LShift Ltd.
+
+%% ``The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
-%% retrieved online at http://www.erlang.org/.
+%% retrieved via the world wide web at http://www.erlang.org/.
 %% 
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
 %% 
-%% %CopyrightEnd%
-%%
-%%
+%% The Initial Developer of the Original Code is Ericsson Utvecklings AB.
+%% Portions created by Ericsson are Copyright 1999, Ericsson Utvecklings
+%% AB. All Rights Reserved.''
 
--module(http_uri).
+%% See http://tools.ietf.org/html/rfc3986
 
--export([parse/1]).
+-module(uri_parser).
+
+-export([parse/2]).
 
 %%%=========================================================================
 %%%  API
 %%%=========================================================================
-parse(AbsURI) ->
+
+%% Returns a key list of elements extracted from the URI. Note that
+%% only 'scheme' is guaranteed to exist. Key-Value pairs from the
+%% Defaults list will be used absence of a non-empty value extracted
+%% from the URI. The values extracted are strings, except for 'port'
+%% which is an integer, 'userinfo' which is a list of strings (split
+%% on $:), and 'query' which is a list of strings where no $= char
+%% found, or a {key,value} pair where a $= char is found (initial
+%% split on $& and subsequent optional split on $=). Possible keys
+%% are: 'scheme', 'userinfo', 'host', 'port', 'path', 'query',
+%% 'fragment'.
+
+parse(AbsURI, Defaults) ->
     case parse_scheme(AbsURI) of
 	{error, Reason} ->
 	    {error, Reason};
 	{Scheme, Rest} ->
-	    case (catch parse_uri_rest(Scheme, Rest)) of
-		{UserInfo, Host, Port, Path, Query} ->
-		    {Scheme, UserInfo, Host, Port, Path, Query};
-		_  ->
-		    {error, {malformed_url, AbsURI}}    
-	    end
+            case (catch parse_uri_rest(Rest)) of
+                [_|_] = List ->
+                    merge_keylists([{scheme, Scheme} | List], Defaults);
+                _ ->
+                    {error, {malformed_uri, AbsURI}}
+            end
     end.
 
 %%%========================================================================
 %%% Internal functions
 %%%========================================================================
 parse_scheme(AbsURI) ->
-    case split_uri(AbsURI, ":", {error, no_scheme}, 1, 1) of
-	{error, no_scheme} ->
-	    {error, no_scheme};
-	{StrScheme, Rest} ->
-	    case list_to_atom(http_util:to_lower(StrScheme)) of
-		Scheme when Scheme == http; Scheme == https ->
-		    {Scheme, Rest};
-		Scheme ->
-		    {error, {not_supported_scheme, Scheme}}
-	    end
+    split_uri(AbsURI, ":", {error, no_scheme}, 1, 1).
+
+parse_uri_rest("//" ++ URIPart) ->
+    %% we have an authority
+    {Authority, PathQueryFrag} =
+	split_uri(URIPart, "/|\\?|#", {URIPart, ""}, 1, 0),
+    AuthorityParts = parse_authority(Authority),
+    parse_uri_rest(PathQueryFrag) ++ AuthorityParts;
+parse_uri_rest(PathQueryFrag) ->
+    %% no authority, just a path and maybe query
+    {PathQuery, Frag} =
+        split_uri(PathQueryFrag, "#", {PathQueryFrag, ""}, 1, 1),
+    {Path, QueryString} = split_uri(PathQuery, "\\?", {PathQuery, ""}, 1, 1),
+    QueryPropList = split_query(QueryString),
+    [{path, Path}, {'query', QueryPropList}, {fragment, Frag}].
+
+parse_authority(Authority) ->
+    {UserInfo, HostPort} = split_uri(Authority, "@", {"", Authority}, 1, 1),
+    UserInfoSplit = case inets_regexp:split(UserInfo, ":") of
+                        {ok, [""]} -> [];
+                        {ok, UIS } -> UIS
+                    end,
+    [{userinfo, UserInfoSplit} | parse_host_port(HostPort)].
+
+parse_host_port("[" ++ HostPort) -> %ipv6
+    {Host, ColonPort} = split_uri(HostPort, "\\]", {HostPort, ""}, 1, 1),
+    [{host, Host} | case split_uri(ColonPort, ":", not_found, 0, 1) of
+                        not_found -> [];
+                        {_, Port} -> [{port, list_to_integer(Port)}]
+                    end];
+
+parse_host_port(HostPort) ->
+    {Host, Port} = split_uri(HostPort, ":", {HostPort, not_found}, 1, 1),
+    [{host, Host} | case Port of
+                        not_found -> [];
+                        _         -> [{port, list_to_integer(Port)}]
+                    end].
+
+split_query(Query) ->
+    case inets_regexp:split(Query, "&") of
+        {ok, [""]} ->
+            [];
+        {ok, QParams} ->
+            lists:map(fun(Param) -> split_uri(Param, "=", Param, 1, 1) end,
+                      QParams)
     end.
 
-parse_uri_rest(Scheme, "//" ++ URIPart) ->
-
-    {Authority, PathQuery} = 
-	case split_uri(URIPart, "/", URIPart, 1, 0) of
-	    Split = {_, _} ->
-		Split;
-	    URIPart ->
-		case split_uri(URIPart, "\\?", URIPart, 1, 0) of
-		    Split = {_, _} ->
-			Split;
-		    URIPart ->
-			{URIPart,""}
-		end
-	end,
-    
-    {UserInfo, HostPort} = split_uri(Authority, "@", {"", Authority}, 1, 1),
-    {Host, Port} = parse_host_port(Scheme, HostPort),
-    {Path, Query} = parse_path_query(PathQuery),
-    {UserInfo, Host, Port, Path, Query}.
-
-
-parse_path_query(PathQuery) ->
-    {Path, Query} =  split_uri(PathQuery, "\\?", {PathQuery, ""}, 1, 0),
-    {path(Path), Query}.
-    
-
-parse_host_port(Scheme,"[" ++ HostPort) -> %ipv6
-    DefaultPort = default_port(Scheme),
-    {Host, ColonPort} = split_uri(HostPort, "\\]", {HostPort, ""}, 1, 1),
-    {_, Port} = split_uri(ColonPort, ":", {"", DefaultPort}, 0, 1),
-    {Host, int_port(Port)};
-
-parse_host_port(Scheme, HostPort) ->
-    DefaultPort = default_port(Scheme),
-    {Host, Port} = split_uri(HostPort, ":", {HostPort, DefaultPort}, 1, 1),
-    {Host, int_port(Port)}.
-    
 split_uri(UriPart, SplitChar, NoMatchResult, SkipLeft, SkipRight) ->
     case inets_regexp:first_match(UriPart, SplitChar) of
 	{match, Match, _} ->
 	    {string:substr(UriPart, 1, Match - SkipLeft),
-	     string:substr(UriPart, Match + SkipRight, length(UriPart))}; 
+	     string:substr(UriPart, Match + SkipRight, length(UriPart))};
 	nomatch ->
 	    NoMatchResult
     end.
 
-default_port(http) ->
-    80;
-default_port(https) ->
-    443.
+merge_keylists(A, B) ->
+    lists:ukeysort(1, lists:foldl(
+                        fun ({Key, ""}, Acc) ->
+                                case lists:keysearch(Key, 1, B) of
+                                    {value, Pair} -> [Pair | Acc];
+                                    false         -> [{Key, ""} | Acc]
+                                end;
+                            (Pair, Acc) ->
+                                [Pair | Acc]
+                        end, [], A) ++ B).
 
-int_port(Port) when is_integer(Port) ->
-    Port;
-int_port(Port) when is_list(Port) ->
-    list_to_integer(Port).
-
-path("") ->
-    "/";
-path(Path) ->
-    Path.
