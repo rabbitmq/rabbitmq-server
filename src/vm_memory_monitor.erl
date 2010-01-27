9181d3406c85be7dd8c79448f8d9fb701874a60c
Matthias Radestock <matthias@lshift.net>
2010-01-27 16:34:39 +0000

    infinite timeout in all vm_memory_monitor gen_server:calls since we have come to the conclusion that this is generally preferable to things failing after some some semi-arbitrary interval.

--- src/vm_memory_monitor.erl
+++ src/vm_memory_monitor.erl
@@ -96,16 +96,17 @@ get_total_memory() ->
     get_total_memory(os:type()).
 
 get_check_interval() ->
-    gen_server:call(?MODULE, get_check_interval).
+    gen_server:call(?MODULE, get_check_interval, infinity).
 
 set_check_interval(Fraction) ->
-    gen_server:call(?MODULE, {set_check_interval, Fraction}).
+    gen_server:call(?MODULE, {set_check_interval, Fraction}, infinity).
 
 get_vm_memory_high_watermark() ->
-    gen_server:call(?MODULE, get_vm_memory_high_watermark).
+    gen_server:call(?MODULE, get_vm_memory_high_watermark, infinity).
 
 set_vm_memory_high_watermark(Fraction) ->
-    gen_server:call(?MODULE, {set_vm_memory_high_watermark, Fraction}).
+    gen_server:call(?MODULE, {set_vm_memory_high_watermark, Fraction},
+                    infinity).
 
 %%----------------------------------------------------------------------------
 %% gen_server callbacks
