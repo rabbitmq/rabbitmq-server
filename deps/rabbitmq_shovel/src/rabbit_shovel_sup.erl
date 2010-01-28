9336e8b4f7fa1fd4def15d39cc6231e10588ad77
Matthew Sackman <matthew@lshift.net>
2010-01-28 14:41:56 +0000

    Only parsing and building the resource declarations to go

--- deps/rabbitmq_shovel/src/rabbit_shovel_sup.erl
+++ deps/rabbitmq_shovel/src/rabbit_shovel_sup.erl
@@ -88,8 +88,8 @@ parse_shovel_config(ShovelName, ShovelConfig) ->
                           end, {2, Dict}, Fields),
                     try
                         {ok, run_reader_state_monad(
-                               [{fun parse_uris/3, sources},
-                                {fun parse_uris/3, destinations},
+                               [{fun parse_endpoint/3, sources},
+                                {fun parse_endpoint/3, destinations},
                                 {fun parse_non_negative_integer/3, qos},
                                 {fun parse_boolean/3, auto_ack},
                                 {fun parse_non_negative_integer/3, tx_size},
@@ -121,7 +121,7 @@ run_reader_state_monad(FunList, State, Reader) ->
     State1.
 
 reader_state_monad({Fun, Key}, {ok, State, Reader}) ->
-    {ok, State1} = Fun(dict:fetch(Key, Reader), Key, State),
+    {ok, State1} = Fun(dict:find(Key, Reader), Key, State),
     {ok, State1, Reader}.
 
 %% --=: Plain state monad implementation start :=--
@@ -138,26 +138,57 @@ return(State) -> {ok, State}.
 fail(Reason) -> throw({error, Reason}).
 %% --=: end :=--
 
-parse_uris({{Uris = [Uri|_], QueueExchange, ResourceDecls}, Pos}, _FieldName,
-           Shovel) when is_list(Uri) ->
-    Len = length(Uris),
-    {[], ParsedUris} =
-        run_state_monad(lists:duplicate(Len, fun parse_uri/1), {Uris, []}),
-    Endpoint = #endpoint { amqp_params = ParsedUris,
-                           queue_or_exchange = QueueExchange,
-                           resource_declarations = ResourceDecls },
-    return(setelement(Pos, Shovel, Endpoint));
-parse_uris({{Uri, QueueExchange, ResourceDecls}, Pos}, FieldName, Shovel)
-  when is_list(Uri) ->
-    parse_uris({{[Uri], QueueExchange, ResourceDecls}, Pos}, FieldName, Shovel);
-parse_uris({{Uri, QueueExchange}, Pos}, FieldName, Shovel) when is_list(Uri) ->
-    parse_uris({{Uri, QueueExchange, undefined}, Pos}, FieldName, Shovel);
-parse_uris({NotUri, _Pos}, FieldName, _Shovel) ->
-    fail({require_something_closer_to_a_uri_and_queue_or_exchange,
-          FieldName, NotUri}).
+parse_endpoint({ok, {Endpoint, Pos}}, FieldName, Shovel) ->
+    Brokers = case lists:keysearch(brokers, 1, Endpoint) of
+                  {value, {brokers, [B|_] = Bs}} when is_list(B) ->
+                      Bs;
+                  {value, {brokers, B}} ->
+                      fail({require_a_list_of_brokers, B, FieldName});
+                  false ->
+                      case lists:keysearch(broker, 1, Endpoint) of
+                          {value, {broker, B}} when is_list(B) ->
+                              [B];
+                          {value, {broker, B}} ->
+                              fail({broker_should_be_string_uri, B, FieldName});
+                          false ->
+                              fail({require_broker_or_brokers, FieldName})
+                      end
+              end,
+    {[], Brokers1} = run_state_monad(
+                       lists:duplicate(length(Brokers), fun parse_uri/1),
+                       {Brokers, []}),
+    QueueExchangeField = case FieldName of
+                             sources      -> queue;
+                             destinations -> exchange
+                         end,
+    QueueExchange =
+        case lists:keysearch(QueueExchangeField, 1, Endpoint) of
+            {value, {_Field, Value}} when is_list(Value) ->
+                list_to_binary(Value);
+            {value, {queue, private}} ->
+                private;
+            {value, {_Field, Value}} ->
+                fail({need_list_for_field, QueueExchangeField, Value, FieldName});
+            false ->
+                fail({field_required, QueueExchangeField, FieldName})
+        end,
+    ResourceDecls =
+        case lists:keysearch(resource_declarations, 1, Endpoint) of
+            false ->
+                [];
+            {value, {resource_declarations, Decls}} when is_list(Decls) ->
+                Decls;
+            {value, {resource_declarations, Decls}} ->
+                fail({resource_declarations_should_be_a_list, Decls, FieldName})
+        end,
+    Result = #endpoint { amqp_params = Brokers1,
+                         queue_or_exchange = QueueExchange,
+                         resource_declarations = ResourceDecls },
+    return(setelement(Pos, Shovel, Result)).
 
 parse_uri({[Uri | Uris], Acc}) ->
-    case uri_parser:parse(Uri, [{host, undefined}, {path, "/"}]) of
+    case uri_parser:parse(Uri, [{host, undefined}, {path, "/"},
+                                {port, undefined}, {'query', []}]) of
         {error, Reason} ->
             fail({unable_to_parse_uri, Uri, Reason});
         Parsed ->
@@ -166,20 +197,22 @@ parse_uri({[Uri | Uris], Acc}) ->
                   [fun (_) ->
                            case lists:keysearch(scheme, 1, Parsed) of
                                {value, {scheme, "amqp"}} ->
-                                   build_endpoint(Parsed);
+                                   build_plain_broker(Parsed);
                                {value, {scheme, "amqps"}} ->
-                                   build_ssl_endpoint(Parsed);
+                                   build_ssl_broker(Parsed);
                                {value, {scheme, Scheme}} ->
                                    fail({unexpected_uri_scheme, Scheme, Uri})
                            end
-                  end], undefined),
+                   end], undefined),
             return({Uris, [Endpoint | Acc]})
     end.
 
-build_endpoint(ParsedUri) ->
+build_broker(ParsedUri) ->
     {value, {host, Host}} = lists:keysearch(host, 1, ParsedUri),
+    {value, {port, Port}} = lists:keysearch(port, 1, ParsedUri),
     {value, {path, Path}} = lists:keysearch(path, 1, ParsedUri),
-    Params = #amqp_params { host = Host, virtual_host = list_to_binary(Path) },
+    Params = #amqp_params { host = Host, port = Port,
+                            virtual_host = list_to_binary(Path) },
     Params1 =
         case lists:keysearch(userinfo, 1, ParsedUri) of
             {value, {userinfo, [Username, Password | _ ]}} ->
@@ -190,25 +223,66 @@ build_endpoint(ParsedUri) ->
         end,
     return(Params1).
 
-build_ssl_endpoint(ParsedUri) ->
-    #amqp_params{}.
+build_plain_broker(ParsedUri) ->
+    Params = run_state_monad([fun build_broker/1], ParsedUri),
+    return(case Params #amqp_params.port of
+               undefined -> Params #amqp_params { port = ?PROTOCOL_PORT };
+               _         -> Params
+           end).
 
-parse_non_negative_integer({N, Pos}, _FieldName, Shovel)
+build_ssl_broker(ParsedUri) ->
+    Params = run_state_monad([fun build_broker/1], ParsedUri),
+    {value, {'query', Query}} = lists:keysearch('query', 1, ParsedUri),
+    SSLOptions = run_reader_state_monad(
+                   [{fun find_path_parameter/3,    "cacertfile"},
+                    {fun find_path_parameter/3,    "certfile"},
+                    {fun find_path_parameter/3,    "keyfile"},
+                    {fun find_boolean_parameter/3, "verify"},
+                    {fun find_boolean_parameter/3, "fail_if_no_peer_cert"}],
+                   [], dict:from_list(Query)),
+    Params1 = Params #amqp_params { ssl_options = SSLOptions },
+    return(case Params1 #amqp_params.port of
+               undefined -> Params1 #amqp_params { port = ?PROTOCOL_PORT - 1 };
+               _         -> Params1
+           end).
+
+find_path_parameter({ok, Value}, FieldName, Acc) ->
+    return([{list_to_atom(FieldName), Value} | Acc]);
+find_path_parameter(error, FieldName, _Acc) ->
+    fail({require_field, list_to_atom(FieldName)}).
+
+find_boolean_parameter({ok, Value}, FieldName, Acc) ->
+    Bool = list_to_atom(Value),
+    Field = list_to_atom(FieldName),
+    case is_boolean(Bool) of
+        true  -> return([{Field, Bool} | Acc]);
+        false -> fail({require_boolean_for_field, Field, Bool})
+    end;
+find_boolean_parameter(error, FieldName, _Acc) ->
+    fail({require_boolean_field, list_to_atom(FieldName)}).
+
+parse_non_negative_integer({ok, {N, Pos}}, _FieldName, Shovel)
   when is_integer(N) andalso N >= 0 ->
     return(setelement(Pos, Shovel, N));
-parse_non_negative_integer({N, _Pos}, FieldName, _Shovel) ->
-    fail({require_non_negative_integer_in_field, FieldName, N}).
+parse_non_negative_integer({ok, {N, _Pos}}, FieldName, _Shovel) ->
+    fail({require_non_negative_integer_in_field, FieldName, N});
+parse_non_negative_integer(error, FieldName, _Shovel) ->
+    fail({require_field, FieldName}).
 
-parse_boolean({Bool, Pos}, _FieldName, Shovel)
+parse_boolean({ok, {Bool, Pos}}, _FieldName, Shovel)
   when is_boolean(Bool) ->
     return(setelement(Pos, Shovel, Bool));
-parse_boolean({NotABool, _Pos}, FieldName, _Shovel) ->
-    fail({require_boolean_in_field, FieldName, NotABool}).
+parse_boolean({ok, {NotABool}, _Pos}, FieldName, _Shovel) ->
+    fail({require_boolean_in_field, FieldName, NotABool});
+parse_boolean(error, FieldName, _Shovel) ->
+    fail({require_field, FieldName}).
 
-parse_delivery_mode({N, Pos}, _FieldName, Shovel)
+parse_delivery_mode({ok, {N, Pos}}, _FieldName, Shovel)
   when N =:= 0 orelse N =:= 2 ->
     return(setelement(Pos, Shovel, N));
-parse_delivery_mode({keep, Pos}, _FieldName, Shovel) ->
+parse_delivery_mode({ok, {keep, Pos}}, _FieldName, Shovel) ->
     return(setelement(Pos, Shovel, keep));
-parse_delivery_mode({N, _Pos}, FieldName, _Shovel) ->
-    fail({require_valid_delivery_mode_in_field, FieldName, N}).
+parse_delivery_mode({ok, {N, _Pos}}, FieldName, _Shovel) ->
+    fail({require_valid_delivery_mode_in_field, FieldName, N});
+parse_delivery_mode(error, FieldName, _Shovel) ->
+    fail({require_field, FieldName}).
