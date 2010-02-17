75fa8b11634f6ddcb43df825cad68b2225680e3f
Matthew Sackman <matthew@lshift.net>
2010-02-17 16:05:40 +0000

    Backed out changeset 4922b15fa148 as it's not needed.

--- codegen.py
+++ codegen.py
@@ -291,9 +291,6 @@ def genErl(spec):
         print 'amqp_exception(?%s) -> %s;' % \
             (n, n.lower())
 
-    def genClassPropsFieldnames(cls):
-        print "class_properties_fieldnames('P_%s') -> %s;" % (erlangize(cls.name), fieldNameList(cls.fields))
-
     methods = spec.allMethods()
 
     printFileHeader()
@@ -313,7 +310,6 @@ def genErl(spec):
 -export([encode_properties/1]).
 -export([lookup_amqp_exception/1]).
 -export([amqp_exception/1]).
--export([class_properties_fieldnames/1]).
 
 bitvalue(true) -> 1;
 bitvalue(false) -> 0;
@@ -357,9 +353,6 @@ bitvalue(undefined) -> 0.
 
     for(c,v,cls) in spec.constants: genAmqpException(c,v,cls)
     print "amqp_exception(_Code) -> undefined."
-
-    for c in spec.allClasses(): genClassPropsFieldnames(c)
-    print "class_properties_fieldnames(PropName) -> exit({unknown_class_properties_name, PropName})."
 
 def genHrl(spec):
     def erlType(domain):
