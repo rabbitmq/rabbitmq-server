d91cc93d8c0332f699bec19b1da4d844eb8c0641
Matthias Radestock <matthias@rabbitmq.com>
2010-07-01 11:32:56 +0100

    cosmetic

--- src/rabbit_channel.erl
+++ src/rabbit_channel.erl
@@ -747,7 +747,7 @@ handle_method(#'queue.declare'{queue       = QueueNameBin,
                     %% the connection shuts down.
                     ok = case Owner of
                              none -> ok;
-                             _ -> rabbit_reader_queue_collector:register_exclusive_queue(CollectorPid, Q)
+                             _    -> rabbit_reader_queue_collector:register_exclusive_queue(CollectorPid, Q)
                          end,
                     return_queue_declare_ok(QueueName, NoWait, 0, 0, State);
                 {existing, _Q} ->
