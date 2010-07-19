e1ab8022667cfdbfacab2ecbc71f9c73f1abe3e2
Matthias Radestock <matthias@rabbitmq.com>
2010-07-19 08:06:13 +0100

    tweak to assist testing ...outside the context of the rabbit app cherry-picked from bug 21673.

--- src/rabbit_amqqueue.erl
+++ src/rabbit_amqqueue.erl
@@ -154,7 +154,7 @@
 
 start() ->
     DurableQueues = find_durable_queues(),
-    {ok, BQ} = application:get_env(backing_queue_module),
+    {ok, BQ} = application:get_env(rabbit, backing_queue_module),
     ok = BQ:start([QName || #amqqueue{name = QName} <- DurableQueues]),
     {ok,_} = supervisor:start_child(
                rabbit_sup,
