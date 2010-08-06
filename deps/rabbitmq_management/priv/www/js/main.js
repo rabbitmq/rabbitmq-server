819e7c79e5f29069e59443bf73faf9c55e1a5810
Simon MacMullen <simon@rabbitmq.com>
2010-08-06 17:52:26 +0100

    Merge two requests to get a list of all (active) channels.

--- deps/rabbitmq_management/priv/www/js/main.js
+++ deps/rabbitmq_management/priv/www/js/main.js
@@ -16,8 +16,8 @@ function apply_url(url) {
 function update() {
     var [reqs, template] = dispatch(current_page);
 
-    with_req('/json' + reqs[0], function(text) {
-            var json = jQuery.parseJSON(text);
+    with_reqs(reqs, [], function(jsons) {
+            var json = merge(jsons, template);
             var html = format(template, json);
             replace_content('main', html);
             update_status('ok', json['datetime']);
@@ -25,13 +25,52 @@ function update() {
                 apply_url(this.href);
             });
             $('a[href="#' + current_page + '"]').addClass('selected');
-    });
+        });
+}
+
+function with_reqs(reqs, acc, fun) {
+    if (reqs.length > 0) {
+        // alert(reqs[0]);
+        with_req('/json' + reqs[0], function(text) {
+                acc.push(jQuery.parseJSON(text));
+                reqs.shift();
+                with_reqs(reqs, acc, fun);
+            });
+    }
+    else {
+        fun(acc);
+    }
+}
+
+function merge(jsons, template) {
+    if (template == 'channels') {
+        var stats0 = jsons[0].stats;
+        var stats1 = jsons[1].stats;
+        for (var i = 0; i < stats0.length; i++) {
+            for (var j = 0; j < stats1.length; j++) {
+                if (stats0[i].channel == stats1[j].channel) {
+                    for (var key in stats1[j].msg_stats) {
+                        stats0[i].msg_stats[key] = stats1[j].msg_stats[key];
+                    }
+                    stats1[j].used = true;
+                }
+            }
+        }
+        for (var j = 0; j < stats1.length; j++) {
+            if (stats1[j].used == undefined) {
+                stats0.push(stats1[j]);
+            }
+        }
+    }
+
+    return jsons[0];
 }
 
 function dispatch(page) {
-    if (page == 'consuming-channels') {
-        return [['/stats/channel_queue_stats/?group_by=channel'],
-                'consuming-channels'];
+    if (page == 'channel') {
+        return [['/stats/channel_queue_stats/?group_by=channel',
+                 '/stats/channel_exchange_stats/?group_by=channel'],
+                'channels'];
     }
     else if (page == 'connection') {
         return [['/connection/'], 'connections'];
