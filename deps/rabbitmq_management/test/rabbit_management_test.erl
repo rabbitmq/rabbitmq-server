61d8735679fd6bc97ff60eb6020e151a3e606af1
Simon MacMullen <simon@rabbitmq.com>
2010-08-11 15:51:30 +0100

    More tests.

--- deps/rabbitmq_management/test/rabbit_management_test.erl
+++ deps/rabbitmq_management/test/rabbit_management_test.erl
@@ -25,7 +25,7 @@
 
 -compile([export_all]).
 
--define(TESTS, [test_queues]).
+-define(TESTS, [test_queues, test_fine_types]).
 
 test() ->
     SecondHalves = [first_half(Test) || Test <- ?TESTS],
@@ -50,11 +50,10 @@ test_queues(Chan) ->
     Queue1 = declare_queue(Chan),
     Queue2 = declare_queue(Chan),
     publish(Chan, Queue1, 4),
-    basic_get(Chan, Queue1, true),
-    basic_get(Chan, Queue1, false),
+    basic_get(Chan, Queue1, true, false),
+    basic_get(Chan, Queue1, false, false),
 
     fun() ->
-            %%Res = rabbit_management_stats:get_msg_stats(channel_queue_stats, "channel", ignored, ignored),
             Queues = rabbit_management_stats:get_queues(),
             Queue1Info = find_by_name(Queue1, Queues),
             Queue2Info = find_by_name(Queue2, Queues),
@@ -68,10 +67,47 @@ test_queues(Chan) ->
             0 = pget(messages_unacknowledged, Queue2Info)
     end.
 
+test_fine_types(Chan) ->
+    Queue = declare_queue(Chan),
+    publish(Chan, Queue, 10),
+    basic_get(Chan, Queue, true, false),
+    basic_get(Chan, Queue, false, true),
+    consume(Chan, Queue, 1, true, false),
+    consume(Chan, Queue, 1, false, true),
+
+    fun() ->
+            QStats = rabbit_management_stats:get_msg_stats(channel_queue_stats,
+                                                           undefined,
+                                                           ignored,
+                                                           ignored),
+            {_, MsgQStats} = find_by_sub_name(queue_details, Queue, QStats),
+            1 = pget(get, MsgQStats),
+            1 = pget(get_no_ack, MsgQStats),
+            2 = pget(ack, MsgQStats),
+            1 = pget(deliver, MsgQStats),
+            7 = pget(deliver_no_ack, MsgQStats), % Since 2nd consume ate
+                                                 % everything
+
+            QXStats = rabbit_management_stats:get_msg_stats(
+                        channel_queue_exchange_stats,
+                        undefined,
+                        ignored,
+                        ignored),
+            {_, MsgQXStats} =
+                find_by_sub_name(queue_details, Queue, QXStats),
+            10 = pget(publish, MsgQXStats)
+    end.
+
 %%---------------------------------------------------------------------------
 
 find_by_name(Name, Items) ->
     [Thing] = lists:filter(fun(Item) -> pget(name, Item) == Name end, Items),
+    Thing.
+
+find_by_sub_name(Key, Name, Items) ->
+    [Thing] = lists:filter(fun({Ids, _Stats}) ->
+                                   pget(name, pget(Key, Ids)) == Name
+                           end, Items),
     Thing.
 
 declare_queue(Chan) ->
@@ -87,12 +123,36 @@ publish(Chan, Queue) ->
 publish(Chan, Queue, Count) ->
     [publish(Chan, Queue) || _ <- lists:seq(1, Count)].
 
-basic_get(Chan, Queue, Ack) ->
+basic_get(Chan, Queue, ExplicitAck, AutoAck) ->
     {#'basic.get_ok'{delivery_tag = Tag}, _} =
-        amqp_channel:call(Chan, #'basic.get' { queue = Queue, no_ack = false }),
-    case Ack of
+        amqp_channel:call(Chan, #'basic.get' { queue = Queue,
+                                               no_ack = AutoAck }),
+    case ExplicitAck of
         true  -> amqp_channel:call(Chan, #'basic.ack' { delivery_tag = Tag });
         false -> ok
+    end.
+
+%% NB: Using AutoAck will actually consume everything.
+consume(Chan, Queue, Count, ExplicitAck, AutoAck) ->
+    amqp_channel:call(Chan, #'basic.qos' { prefetch_count = 1 }),
+    amqp_channel:subscribe(Chan, #'basic.consume' { queue = Queue,
+                                                    no_ack = AutoAck },
+                           self()),
+    receive
+        #'basic.consume_ok'{consumer_tag = CTag} -> ok
+    end,
+    DTags = [receive
+                 {#'basic.deliver'{delivery_tag = DTag}, _Msg} -> DTag
+             end || _ <- lists:seq(1, Count)],
+    amqp_channel:call(Chan, #'basic.cancel' { consumer_tag = CTag }),
+    receive
+        #'basic.cancel_ok'{consumer_tag = CTag} -> ok
+    end,
+    case ExplicitAck of
+         true  -> [amqp_channel:call(
+                     Chan,
+                     #'basic.ack' { delivery_tag = DTag }) || DTag <- DTags];
+         false -> ok
     end.
 
 pget(K, L) ->
