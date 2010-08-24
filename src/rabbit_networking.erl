7585c95b3afb1dcb10f1641d415a93e99886294d
Emile Joubert <emile@rabbitmq.com>
2010-08-24 14:56:27 +0100

    Take peer verification config into account in verify function
    This works around a limitation in the ssl module, which does not honour verify_none

--- src/rabbit_networking.erl
+++ src/rabbit_networking.erl
@@ -108,9 +108,15 @@ boot_ssl() ->
         {ok, SslListeners} ->
             ok = rabbit_misc:start_applications([crypto, public_key, ssl]),
             {ok, SslOptsConfig} = application:get_env(ssl_options),
-            SslOpts = [{verify_fun, fun([])    -> true;
-                                       ([_|_]) -> false
-                                    end} | SslOptsConfig],
+            VerifyFun =
+              fun([])    -> true;
+                 ([_|_]) ->
+                case proplists:get_value(verify, SslOptsConfig, verify_none) of
+                  verify_none -> true;
+                  verify_peer -> false
+                end
+              end,
+            SslOpts = [{verify_fun, VerifyFun} | SslOptsConfig],
             [start_ssl_listener(Host, Port, SslOpts) || {Host, Port} <- SslListeners],
             ok
     end.
