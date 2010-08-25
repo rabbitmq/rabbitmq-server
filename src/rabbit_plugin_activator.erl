35ab5479ddd4604d6a87d192a493379607c770cd
Matthias Radestock <matthias@rabbitmq.com>
2010-08-25 17:03:31 +0100

    oops

--- src/rabbit_plugin_activator.erl
+++ src/rabbit_plugin_activator.erl
@@ -132,8 +132,8 @@ start() ->
     end,
     io:format("~n~w plugins activated:~n", [length(PluginApps)]),
     [io:format("* ~s-~s~n", [App, Vsn]) ||
-        App <- PluginApps,
-        {App, Vsn} <- [proplists:lookup(App, AppVersions)]],
+        App      <- PluginApps,
+        {_, Vsn} <- [proplists:lookup(App, AppVersions)]],
     io:nl(),
     halt(),
     ok.
