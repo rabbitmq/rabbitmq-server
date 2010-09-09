ce186893ec7f4f7816b4620429c538e4c4bb23ea
Matthew Sackman <matthew@rabbitmq.com>
2010-09-09 15:05:14 +0100

    With the erlang client correctly throwing on connection error again, avoid fugly is_pid failure for control flow

--- deps/rabbitmq_management/src/rabbit_mgmt_util.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_util.erl
@@ -159,22 +159,21 @@ amqp_request(VHost, ReqData, Context, Method) ->
                               password = Context#context.password,
                               virtual_host = VHost},
         Conn = amqp_connection:start_direct(Params),
-        true = is_pid(Conn),
         Ch = amqp_connection:open_channel(Conn),
         amqp_channel:call(Ch, Method),
         amqp_channel:close(Ch),
         amqp_connection:close(Conn),
         {true, ReqData, Context}
     %% See bug 23187
-    catch error:{badmatch,false} ->
+    catch
+        throw:{error, {auth_failure_likely,
+                       {#amqp_error{name = access_refused}, _}}} ->
             not_authorised(not_authorised, ReqData, Context);
-          error:{badmatch,{error, #amqp_error{name = access_refused}}} ->
-            not_authorised(not_authorised, ReqData, Context);
-          error:{badmatch,{error, #amqp_error{name = {error, Error}}}} ->
+        error:{badmatch,{error, #amqp_error{name = {error, Error}}}} ->
             bad_request(Error, ReqData, Context);
-          exit:{{server_initiated_close, ?NOT_FOUND, Reason}, _} ->
+        exit:{{server_initiated_close, ?NOT_FOUND, Reason}, _} ->
             not_found(list_to_binary(Reason), ReqData, Context);
-          exit:{{server_initiated_close, _Code, Reason}, _} ->
+        exit:{{server_initiated_close, _Code, Reason}, _} ->
             bad_request(list_to_binary(Reason), ReqData, Context)
     end.
 
