d4990bc5e47385e746d4291ee0671e5cb516734f
Simon MacMullen <simon@rabbitmq.com>
2010-09-09 13:25:48 +0100

    Further changes due to Erlang client

--- deps/rabbitmq_management/src/rabbit_mgmt_util.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_util.erl
@@ -159,13 +159,16 @@ amqp_request(VHost, ReqData, Context, Method) ->
                               password = Context#context.password,
                               virtual_host = VHost},
         Conn = amqp_connection:start_direct(Params),
+        true = is_pid(Conn),
         Ch = amqp_connection:open_channel(Conn),
         amqp_channel:call(Ch, Method),
         amqp_channel:close(Ch),
         amqp_connection:close(Conn),
         {true, ReqData, Context}
     %% See bug 23187
-    catch error:{badmatch,{error, #amqp_error{name = access_refused}}} ->
+    catch error:{badmatch,false} ->
+            not_authorised(not_authorised, ReqData, Context);
+          error:{badmatch,{error, #amqp_error{name = access_refused}}} ->
             not_authorised(not_authorised, ReqData, Context);
           error:{badmatch,{error, #amqp_error{name = {error, Error}}}} ->
             bad_request(Error, ReqData, Context);
