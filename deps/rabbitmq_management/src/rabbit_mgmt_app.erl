f840450118a43a8090b67e9bee2fa3cbfb13ea60
Simon MacMullen <simon@rabbitmq.com>
2010-09-14 11:40:40 +0100

    Use a dummy supervisor rather than rabbit_mgmt_dummy as recommended by Ulf.

--- deps/rabbitmq_management/src/rabbit_mgmt_app.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_app.erl
@@ -21,8 +21,21 @@
 -module(rabbit_mgmt_app).
 
 -behaviour(application).
-
 -export([start/2, stop/1]).
+
+%% Dummy supervisor - see Ulf Wiger's comment at
+%% http://erlang.2086793.n4.nabble.com/initializing-library-applications-without-processes-td2094473.html
+
+%% All of our actual server processes are supervised by rabbit_mgmt_sup, which
+%% is started by a rabbit_boot_step (since it needs to start up before queue
+%% recovery or the network being up, so it can't be part of our application).
+%%
+%% However, we still need an application behaviour since we need to depend on
+%% the rabbit_mochiweb application and call into it once it's running. Since
+%% the application behaviour needs a tree of processes to supervise, this is
+%% it...
+-behaviour(supervisor).
+-export([init/1]).
 
 -define(PREFIX, "api").
 -define(UI_PREFIX, "mgmt").
@@ -50,7 +63,7 @@ start(_Type, _StartArgs) ->
         true -> setup_wm_trace_app();
         _    -> ok
     end,
-    rabbit_mgmt_dummy:start_link().
+    supervisor:start_link({local,?MODULE},?MODULE,[]).
 
 stop(_State) ->
     ok.
@@ -118,3 +131,8 @@ get_port() ->
         {ok, P} ->
             P
     end.
+
+%%----------------------------------------------------------------------------
+
+init([]) ->
+    {ok, {{one_for_one,3,10},[]}}.
