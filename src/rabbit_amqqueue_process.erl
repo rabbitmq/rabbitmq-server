155f4b5a8d892c364f93b18e3d10fced77807fe6
Rob Harrop <rharrop@vmware.com>
2010-09-27 14:46:48 +0100

    minor stylistic improvement

--- src/rabbit_amqqueue_process.erl
+++ src/rabbit_amqqueue_process.erl
@@ -150,7 +150,8 @@ code_change(_OldVsn, State, _Extra) ->
 %%----------------------------------------------------------------------------
 
 init_queue_state(State) ->
-    init_expires(init_ttl(State)).
+    lists:foldr(fun(F, S) -> F(S) end, State, 
+                [fun init_expires/1, fun init_ttl/1]).
 
 init_expires(State = #q{q = #amqqueue{arguments = Arguments}}) ->
     case rabbit_misc:table_lookup(Arguments, <<"x-expires">>) of
