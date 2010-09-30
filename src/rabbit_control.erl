4c1f1bfa18f35ffa8b19e566ece92a0aebdc2782
Alexandru Scvortov <alexandru@rabbitmq.com>
2010-09-30 16:35:44 +0100

    remove client scope and scopes

--- src/rabbit_control.erl
+++ src/rabbit_control.erl
@@ -39,7 +39,6 @@
 -define(QUIET_OPT, "-q").
 -define(NODE_OPT, "-n").
 -define(VHOST_OPT, "-p").
--define(SCOPE_OPT, "-s").
 
 %%----------------------------------------------------------------------------
 
@@ -67,7 +66,7 @@ start() ->
     {[Command0 | Args], Opts} =
         rabbit_misc:get_options(
           [{flag, ?QUIET_OPT}, {option, ?NODE_OPT, NodeStr},
-           {option, ?VHOST_OPT, "/"}, {option, ?SCOPE_OPT, "client"}],
+           {option, ?VHOST_OPT, "/"}],
           FullCommand),
     Opts1 = lists:map(fun({K, V}) ->
                               case K of
@@ -288,10 +287,9 @@ action(list_consumers, Node, _Args, Opts, Inform) ->
 
 action(set_permissions, Node, [Username, CPerm, WPerm, RPerm], Opts, Inform) ->
     VHost = proplists:get_value(?VHOST_OPT, Opts),
-    Scope = proplists:get_value(?SCOPE_OPT, Opts),
     Inform("Setting permissions for user ~p in vhost ~p", [Username, VHost]),
     call(Node, {rabbit_access_control, set_permissions,
-                [Scope, Username, VHost, CPerm, WPerm, RPerm]});
+                [Username, VHost, CPerm, WPerm, RPerm]});
 
 action(clear_permissions, Node, [Username], Opts, Inform) ->
     VHost = proplists:get_value(?VHOST_OPT, Opts),
