87464af6c5273499174bfd83dae0699f1cb4baa9
Rob Harrop <rob@rabbitmq.com>
2010-10-07 10:27:34 +0100

    missing test file

---
+++ deps/rabbitmq_stomp/test/errors.py
@@ -0,0 +1,49 @@
+import unittest
+import stomp
+import base
+import time
+
+class TestErrors(base.BaseTest):
+
+    def test_invalid_queue_destination(self):
+        self.__test_invalid_destination("queue", "/bah/baz")
+
+    def test_invalid_empty_queue_destination(self):
+        self.__test_invalid_destination("queue", "")
+
+    def test_invalid_topic_destination(self):
+        self.__test_invalid_destination("topic", "/bah/baz")
+
+    def test_invalid_empty_topic_destination(self):
+        self.__test_invalid_destination("topic", "")
+
+    def test_invalid_exchange_destination(self):
+        self.__test_invalid_destination("exchange", "/bah/baz/boo")
+
+    def test_invalid_empty_exchange_destination(self):
+        self.__test_invalid_destination("exchange", "")
+
+    def test_unknown_destination(self):
+        self.listener.reset()
+        self.conn.send(destination="/something/interesting")
+
+        self.assertTrue(self.listener.await())
+        self.assertEquals(1, len(self.listener.errors))
+
+        err = self.listener.errors[0]
+        self.assertEquals("Unknown destination", err['headers']['message'])
+
+    def __test_invalid_destination(self, dtype, content):
+        self.listener.reset()
+        self.conn.send(destination="/" + dtype + content)
+
+        self.assertTrue(self.listener.await())
+        self.assertEquals(1, len(self.listener.errors))
+
+        err = self.listener.errors[0]
+        self.assertEquals("Invalid destination", err['headers']['message'])
+        self.assertEquals("'" + content + "' is not a valid " +
+                              dtype + " destination\n",
+                          err['message'])
+
+
