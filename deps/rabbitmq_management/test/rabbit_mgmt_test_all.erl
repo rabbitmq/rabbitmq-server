e04f4f8ce31918f356d4e8e65c4a18b421aa084b
Simon MacMullen <simon@rabbitmq.com>
2010-10-08 16:53:45 +0100

    If a test suite fails, stop.

---
+++ deps/rabbitmq_management/test/rabbit_mgmt_test_all.erl
@@ -0,0 +1,28 @@
+%%   The contents of this file are subject to the Mozilla Public License
+%%   Version 1.1 (the "License"); you may not use this file except in
+%%   compliance with the License. You may obtain a copy of the License at
+%%   http://www.mozilla.org/MPL/
+%%
+%%   Software distributed under the License is distributed on an "AS IS"
+%%   basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%%   License for the specific language governing rights and limitations
+%%   under the License.
+%%
+%%   The Original Code is RabbitMQ Management Console.
+%%
+%%   The Initial Developers of the Original Code are Rabbit Technologies Ltd.
+%%
+%%   Copyright (C) 2010 Rabbit Technologies Ltd.
+%%
+%%   All Rights Reserved.
+%%
+%%   Contributor(s): ______________________________________.
+%%
+-module(rabbit_mgmt_test_all).
+
+-export([all_tests/0]).
+
+all_tests() ->
+    ok = eunit:test(rabbit_mgmt_test_unit,[verbose]),
+    ok = eunit:test(rabbit_mgmt_test_http,[verbose]),
+    ok = rabbit_mgmt_test_db:test().
