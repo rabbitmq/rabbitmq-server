ba7093ab5bc0582ec1266da044ead68a9456cc99
Simon MacMullen <simon@rabbitmq.com>
2010-11-01 15:49:21 +0000

    Fix copy right info in Erlang files. Add copyright info to Python file. Add LICENSE files similar to those in rabbitmq-server.

--- deps/rabbitmq_management/src/rabbit_mgmt_format.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_format.erl
@@ -1,4 +1,3 @@
-
 %%   The contents of this file are subject to the Mozilla Public License
 %%   Version 1.1 (the "License"); you may not use this file except in
 %%   compliance with the License. You may obtain a copy of the License at
@@ -9,16 +8,11 @@
 %%   License for the specific language governing rights and limitations
 %%   under the License.
 %%
-%%   The Original Code is RabbitMQ Management Console.
+%%   The Original Code is RabbitMQ Management Plugin.
 %%
-%%   The Initial Developers of the Original Code are Rabbit Technologies Ltd.
-%%
-%%   Copyright (C) 2010 Rabbit Technologies Ltd.
-%%
-%%   All Rights Reserved.
-%%
-%%   Contributor(s): ______________________________________.
-%%
+%%   The Initial Developer of the Original Code is VMware, Inc.
+%%   Copyright (c) 2007-2010 VMware, Inc.  All rights reserved.
+
 -module(rabbit_mgmt_format).
 
 -export([format/2, print/2, pid/1, ip/1, table/1, tuple/1, timestamp/1]).
--- deps/rabbitmq_management/src/rabbit_mgmt_wm_exchange.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_exchange.erl
@@ -8,16 +8,10 @@
 %%   License for the specific language governing rights and limitations
 %%   under the License.
 %%
-%%   The Original Code is RabbitMQ Management Console.
+%%   The Original Code is RabbitMQ Management Plugin.
 %%
-%%   The Initial Developers of the Original Code are Rabbit Technologies Ltd.
-%%
-%%   Copyright (C) 2010 Rabbit Technologies Ltd.
-%%
-%%   All Rights Reserved.
-%%
-%%   Contributor(s): ______________________________________.
-%%
+%%   The Initial Developer of the Original Code is VMware, Inc.
+%%   Copyright (c) 2007-2010 VMware, Inc.  All rights reserved.
 -module(rabbit_mgmt_wm_exchange).
 
 -export([init/1, resource_exists/2, to_json/2,
