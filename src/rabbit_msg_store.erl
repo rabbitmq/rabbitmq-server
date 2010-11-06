9cc831ff72187513eafd184739e7e5b61cc2d39f
Matthias Radestock <matthias@rabbitmq.com>
2010-11-06 17:30:23 +0000

    correct some more specs

--- src/rabbit_msg_store.erl
+++ src/rabbit_msg_store.erl
@@ -164,8 +164,8 @@
 -spec(set_maximum_since_use/2 :: (server(), non_neg_integer()) -> 'ok').
 -spec(has_readers/2 :: (non_neg_integer(), gc_state()) -> boolean()).
 -spec(combine_files/3 :: (non_neg_integer(), non_neg_integer(), gc_state()) ->
-                              non_neg_integer()).
--spec(delete_file/2 :: (non_neg_integer(), gc_state()) -> non_neg_integer()).
+                              'ok').
+-spec(delete_file/2 :: (non_neg_integer(), gc_state()) -> 'ok').
 
 -endif.
 
