9c2b7444e3b7e76a7227a8043fe9866e9202b095
Simon MacMullen <simon@rabbitmq.com>
2010-11-08 11:19:32 +0000

    Remove the db monitor.

--- deps/rabbitmq_management/src/rabbit_mgmt_app.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_app.erl
@@ -19,6 +19,20 @@
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
+%% Dummy supervisor - see Ulf Wiger's comment at
+%% http://erlang.2086793.n4.nabble.com/initializing-library-applications-without-processes-td2094473.html
+
+%% All of our actual server processes are supervised by rabbit_mgmt_sup, which
+%% is started by a rabbit_boot_step (since it needs to start up before queue
+%% recovery or the network being up, so it can't be part of our application).
+%%
+%% However, we still need an application behaviour since we need to depend on
+%% the rabbit_mochiweb application and call into it once it's running. Since
+%% the application behaviour needs a tree of processes to supervise, this is
+%% it...
+-behaviour(supervisor).
+-export([init/1]).
+
 -define(PREFIX, "api").
 -define(UI_PREFIX, "mgmt").
 -define(CLI_PREFIX, "cli").
@@ -41,7 +55,7 @@ start(_Type, _StartArgs) ->
         true -> setup_wm_trace_app();
         _    -> ok
     end,
-    rabbit_mgmt_sup:start_link().
+    supervisor:start_link({local,?MODULE},?MODULE,[]).
 
 stop(_State) ->
     ok.
@@ -103,3 +117,8 @@ get_port() ->
         {ok, P} ->
             P
     end.
+
+%%----------------------------------------------------------------------------
+
+init([]) ->
+    {ok, {{one_for_one,3,10},[]}}.
