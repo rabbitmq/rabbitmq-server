67cce9fb792a2a802ef4a4ce598619805cfae38c
Simon MacMullen <simon@rabbitmq.com>
2010-11-18 13:25:36 +0000

    Take a backup before upgrading.

--- src/rabbit_upgrade.erl
+++ src/rabbit_upgrade.erl
@@ -120,14 +120,33 @@ heads(G) ->
 %% -------------------------------------------------------------------
 
 apply_upgrades(Upgrades) ->
-    LockFile = lock_filename(),
+    LockFile = lock_filename(dir()),
     case file:open(LockFile, [write, exclusive]) of
         {ok, Lock} ->
             ok = file:close(Lock),
+            BackupDir = dir() ++ "-upgrade-backup",
             info("Upgrades: ~w to apply~n", [length(Upgrades)]),
+            case rabbit_misc:recursive_copy(dir(), BackupDir) of
+                ok ->
+                    %% We need to make the backup after creating the lock file
+                    %% so that it protects us from trying to overwrite the
+                    %% backup. Unfortunately this means the lock file exists in
+                    %% the backup too, which is not intuitive. Remove it.
+                    ok = file:delete(lock_filename(BackupDir)),
+                    ok;
+                {error, E} ->
+                    %% If we can't backup, the upgrade hasn't started hence we
+                    %% don't need the lockfile since the real mnesia dir is the
+                    %% good one.
+                    ok = file:delete(LockFile),
+                    exit({could_not_back_up_mnesia_dir, E})
+            end,
+            info("Upgrades: Mnesia dir backed up to ~p~n", [BackupDir]),
             [apply_upgrade(Upgrade) || Upgrade <- Upgrades],
-            info("Upgrades: All applied~n", []),
+            info("Upgrades: All upgrades applied successfully~n", []),
             ok = write_version(),
+            ok = rabbit_misc:recursive_delete([BackupDir]),
+            info("Upgrades: Mnesia backup removed~n", []),
             ok = file:delete(LockFile);
         {error, eexist} ->
             exit(previous_upgrade_failed);
@@ -145,7 +164,7 @@ dir() -> rabbit_mnesia:dir().
 
 schema_filename() -> filename:join(dir(), ?VERSION_FILENAME).
 
-lock_filename()   -> filename:join(dir(), ?LOCK_FILENAME).
+lock_filename(Dir) -> filename:join(Dir, ?LOCK_FILENAME).
 
 %% NB: we cannot use rabbit_log here since it may not have been
 %% started yet
