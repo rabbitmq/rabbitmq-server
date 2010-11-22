20eb189dbee40d767ab8801c62fcd777745dd0bd
Simon MacMullen <simon@rabbitmq.com>
2010-11-22 19:14:11 +0000

    Make setup.sh really reset everything, start to set up some objects we could attach ACLs to.

--- deps/rabbitmq_auth_backend_ldap/example/setup.sh
+++ deps/rabbitmq_auth_backend_ldap/example/setup.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # Based on instructions found at
 # http://ubuntuforums.org/showthread.php?p=8161118#post8161118
 # - yes that does sseem to be the most authoritative place.
 
-ldapadd -Y EXTERNAL -H ldapi:/// -f example.ldif
+sudo apt-get --yes purge slapd
+sudo rm -rf /var/lib/ldap
+sudo apt-get --yes install slapd
+sleep 1
+
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f example.ldif
 ldapadd -x -D cn=admin,dc=example,dc=com -w admin -f simon.ldif
+ldapadd -x -D cn=admin,dc=example,dc=com -w admin -f rabbit.ldif
