ca2609606731d2d9f4ce05a2b95f7dbaf7a04474
Simon MacMullen <simon@rabbitmq.com>
2010-11-23 11:57:30 +0000

    Get ACLs working, add a less privileged user.

--- deps/rabbitmq_auth_backend_ldap/example/setup.sh
+++ deps/rabbitmq_auth_backend_ldap/example/setup.sh
@@ -2,13 +2,13 @@
 
 # Based on instructions found at
 # http://ubuntuforums.org/showthread.php?p=8161118#post8161118
-# - yes that does sseem to be the most authoritative place.
+# - yes that does seem to be the most authoritative place.
 
 sudo apt-get --yes purge slapd
 sudo rm -rf /var/lib/ldap
 sudo apt-get --yes install slapd
 sleep 1
 
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f example.ldif
-ldapadd -x -D cn=admin,dc=example,dc=com -w admin -f simon.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f global.ldif
+ldapadd -x -D cn=admin,dc=example,dc=com -w admin -f people.ldif
 ldapadd -x -D cn=admin,dc=example,dc=com -w admin -f rabbit.ldif
