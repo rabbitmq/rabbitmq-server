a2b3712bd473034ce7b1a05fc1cd3e12593c739f
John DeTreville <jdetreville@vmware.com>
2010-12-13 14:24:41 -0800

    Updated rabbit_amqqueue_process.

--- src/rabbit_amqqueue_process.erl
+++ src/rabbit_amqqueue_process.erl
@@ -276,10 +276,9 @@ ensure_expiry_timer(State = #q{expires = Expires}) ->
     end.
 
 ensure_stats_timer(State = #q{stats_timer = StatsTimer,
-                              q = Q}) ->
-    State#q{stats_timer = rabbit_event:old_ensure_stats_timer(
-                            StatsTimer,
-                            fun() -> rabbit_amqqueue:emit_stats(Q) end)}.
+                              q = #amqqueue{pid = QPid}}) ->
+    State#q{stats_timer = rabbit_event:ensure_stats_timer(
+                            StatsTimer, QPid, emit_stats)}.
 
 assert_invariant(#q{active_consumers = AC,
                     backing_queue = BQ, backing_queue_state = BQS}) ->
