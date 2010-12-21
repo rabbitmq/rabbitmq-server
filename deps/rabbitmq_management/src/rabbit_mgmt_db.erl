2fc9f4c77133f7f9b09eff4dee7ac41d831de605
Simon MacMullen <simon@rabbitmq.com>
2010-12-21 11:35:01 +0000

    Format exclusive consumer pids so we don't blow up.

--- deps/rabbitmq_management/src/rabbit_mgmt_db.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_db.erl
@@ -290,7 +290,8 @@ handle_event(#event{type = queue_stats, props = Stats, timestamp = Timestamp},
              State) ->
     handle_stats(queue_stats, Stats, Timestamp,
                  [{fun rabbit_mgmt_format:properties/1,[backing_queue_status]},
-                  {fun rabbit_mgmt_format:timestamp/1, [idle_since]}],
+                  {fun rabbit_mgmt_format:timestamp/1, [idle_since]},
+                  {fun rabbit_mgmt_format:pid/1, [exclusive_consumer_pid]}],
                  [], State);
 
 handle_event(Event = #event{type = queue_deleted}, State) ->
