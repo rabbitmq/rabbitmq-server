43ebb9465bca66ae20b54c0677dbd0574968c89b
Simon MacMullen <simon@rabbitmq.com>
2010-12-21 15:25:31 +0000

    Don't attempt to verify the pid represents a connection.

--- src/rabbit_networking.erl
+++ src/rabbit_networking.erl
@@ -258,10 +258,7 @@ connection_info_all() -> cmap(fun (Q) -> connection_info(Q) end).
 connection_info_all(Items) -> cmap(fun (Q) -> connection_info(Q, Items) end).
 
 close_connection(Pid, Explanation) ->
-    case lists:member(Pid, connections()) of
-        true  -> rabbit_reader:shutdown(Pid, Explanation);
-        false -> throw({error, {not_a_connection_pid, Pid}})
-    end.
+    rabbit_reader:shutdown(Pid, Explanation).
 
 %%--------------------------------------------------------------------
 
