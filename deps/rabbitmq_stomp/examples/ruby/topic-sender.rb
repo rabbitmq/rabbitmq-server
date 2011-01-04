ebf1f7df795b635c88119343ff617768cf683bea
Rob Harrop <rob@rabbitmq.com>
2011-01-04 10:19:54 +0000

    Reworked Ruby examples to allow more flexibility in the topic examples. Still need to document this

--- deps/rabbitmq_stomp/examples/ruby/topic-sender.rb
+++ deps/rabbitmq_stomp/examples/ruby/topic-sender.rb
@@ -2,6 +2,6 @@ require 'rubygems'
 require 'stomp'
 
 client = Stomp::Client.new("guest", "guest", "localhost", 61613)
-client.publish '/topic/x', 'first message'
-client.publish '/topic/x', 'second message'
+client.publish '/topic/x.y', 'first message'
+client.publish '/topic/x.z', 'second message'
 client.publish '/topic/x', 'third message'
