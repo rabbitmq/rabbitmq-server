69c80d15c78518e0ebc9e95d74d781ba597bfa4a
Matthew Sackman <matthew@rabbitmq.com>
2011-01-19 17:14:59 +0000

    Update license boiler plate copyright dodats

--- deps/rabbitmq_shovel/src/rabbit_shovel_worker.erl
+++ deps/rabbitmq_shovel/src/rabbit_shovel_worker.erl
@@ -1,23 +1,19 @@
-%%   The contents of this file are subject to the Mozilla Public License
-%%   Version 1.1 (the "License"); you may not use this file except in
-%%   compliance with the License. You may obtain a copy of the License at
-%%   http://www.mozilla.org/MPL/
+%%  The contents of this file are subject to the Mozilla Public License
+%%  Version 1.1 (the "License"); you may not use this file except in
+%%  compliance with the License. You may obtain a copy of the License
+%%  at http://www.mozilla.org/MPL/
 %%
-%%   Software distributed under the License is distributed on an "AS IS"
-%%   basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-%%   License for the specific language governing rights and limitations
-%%   under the License.
+%%  Software distributed under the License is distributed on an "AS IS"
+%%  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%%  the License for the specific language governing rights and
+%%  limitations under the License.
 %%
-%%   The Original Code is RabbitMQ-shovel.
+%%  The Original Code is RabbitMQ.
 %%
-%%   The Initial Developers of the Original Code are LShift Ltd.
+%%  The Initial Developer of the Original Code is VMware, Inc.
+%%  Copyright (c) 2007-2011 VMware, Inc.  All rights reserved.
 %%
-%%   Copyright (C) 2010 LShift Ltd.
-%%
-%%   All Rights Reserved.
-%%
-%%   Contributor(s): ______________________________________.
-%%
+
 -module(rabbit_shovel_worker).
 -behaviour(gen_server2).
 
