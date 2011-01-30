9aecd8c97e557de1259e1194ff5621ae0a91899d
David Wragg <david@rabbitmq.com>
2011-01-30 12:59:50 +0000

    Check the erlang version before running tests
    Because this keeps biting me

--- deps/rabbitmq_management/package.mk
+++ deps/rabbitmq_management/package.mk
@@ -6,3 +6,11 @@ EXTRA_TARGETS:=$(shell find $(EXTRA_PACKAGE_DIRS) -type f) $(PACKAGE_DIR)/priv/w
 
 $(PACKAGE_DIR)/priv/www-cli/rabbitmqadmin: $(PACKAGE_DIR)/bin/rabbitmqadmin
 	cp $< $@
+
+# Check the erlang version before running tests (httpc issue)
+$(PACKAGE_DIR)+pre-test-checks::
+	if [ $$(erl -noshell -eval 'io:format(lists:map(fun erlang:list_to_integer/1, string:tokens(erlang:system_info(version), ".")) >= [5,8]),halt().') != true ] ; then \
+	  echo "Need Erlang/OTP R14A or higher to run tests" ; \
+	  exit 1 ; \
+	fi
+
