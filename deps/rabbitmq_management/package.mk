c22f87fbc84f0cf04b87dea990ea16c543a72906
David Wragg <david@rabbitmq.com>
2011-02-01 16:01:52 +0000

    Distinguish in-broker and standalone tests

--- deps/rabbitmq_management/package.mk
+++ deps/rabbitmq_management/package.mk
@@ -1,6 +1,6 @@
 RELEASABLE:=true
 DEPS:=rabbitmq-mochiweb webmachine-wrapper rabbitmq-server rabbitmq-erlang-client rabbitmq-management-agent
-TEST_COMMANDS:=rabbit_mgmt_test_all:all_tests()
+IN_BROKER_TEST_COMMANDS:=rabbit_mgmt_test_all:all_tests()
 EXTRA_PACKAGE_DIRS:=$(PACKAGE_DIR)/priv
 EXTRA_TARGETS:=$(shell find $(EXTRA_PACKAGE_DIRS) -type f) $(PACKAGE_DIR)/priv/www-cli/rabbitmqadmin
 
@@ -10,7 +10,7 @@ $(PACKAGE_DIR)/priv/www-cli/rabbitmqadmin: $(PACKAGE_DIR)/bin/rabbitmqadmin
 	cp $$< $$@
 
 # The tests require erlang/OTP R14 (httpc issue)
-$(PACKAGE_DIR)+pre-test-checks:: assert-erlang-r14
+$(PACKAGE_DIR)+pre-test:: assert-erlang-r14
 
 $(PACKAGE_DIR)+clean::
 	rm -f $(PACKAGE_DIR)/priv/www-cli/rabbitmqadmin
