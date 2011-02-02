b85ebac6fba7a45a98f9c18364cf3e8415b4bc9b
David Wragg <david@rabbitmq.com>
2011-02-02 16:10:30 +0000

    Make the test actually test something

--- deps/rabbitmq_web_dispatch/test/src/rabbit_mochiweb_test.erl
+++ deps/rabbitmq_web_dispatch/test/src/rabbit_mochiweb_test.erl
@@ -3,8 +3,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 query_static_resource_test() ->
-  %% TODO this is a fairly rubbish test, but not as bad as it was
-  {ok, {_Status, _Headers, Body}} =
+    %% TODO this is a fairly rubbish test, but not as bad as it was
+    rabbit_mochiweb:register_static_context("rabbit_mochiweb_test", ?MODULE, "priv/www", "Test"),
+    {ok, {_Status, _Headers, Body}} =
         http:request("http://localhost:55672/rabbit_mochiweb_test/index.html"),
-  0 = string:str(Body, "RabbitMQ HTTP Server Test Page"),
-  ok.
+    ?assert(string:str(Body, "RabbitMQ HTTP Server Test Page") /= 0).
