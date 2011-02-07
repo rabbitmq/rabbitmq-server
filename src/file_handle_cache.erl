6ab77cedd14dd8081ac8d8747831a5a9b5ab95e2
John DeTreville <jdetreville@vmware.com>
2011-02-07 15:20:57 -0800

    Updated rabbit_event and rabbit_reader. Removed accidental tabs from file_handle_cache.

--- src/file_handle_cache.erl
+++ src/file_handle_cache.erl
@@ -1079,7 +1079,7 @@ reduce(State = #fhc_state { open_pending   = OpenPending,
                             timer_ref      = TRef }) ->
     Now = now(),
     {CStates, Sum, ClientCount} =
-        dict:fold(fun (Pid, Eldest, {CStatesAcc, SumAcc, CountAcc} = A<ccs) ->
+        dict:fold(fun (Pid, Eldest, {CStatesAcc, SumAcc, CountAcc} = Accs) ->
                           [#cstate { pending_closes = PendingCloses,
                                      opened         = Opened,
                                      blocked        = Blocked } = CState] =
