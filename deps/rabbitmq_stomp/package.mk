9582e82f8e1b1fd085b7be1c002e502bf5683622
David Wragg <david@rabbitmq.com>
2011-02-18 11:06:29 +0000

    package_targets -> package_rules

--- deps/rabbitmq_stomp/package.mk
+++ deps/rabbitmq_stomp/package.mk
@@ -3,7 +3,7 @@ DEPS:=rabbitmq-server rabbitmq-erlang-client
 STANDALONE_TEST_COMMANDS:=eunit:test([rabbit_stomp_test_util,rabbit_stomp_test_frame],[verbose])
 IN_BROKER_TEST_SCRIPTS:=$(PACKAGE_DIR)/test/src/test.py
 
-define package_targets
+define package_rules
 
 $(PACKAGE_DIR)+pre-test::
 	make -C $(PACKAGE_DIR)/deps/stomppy
