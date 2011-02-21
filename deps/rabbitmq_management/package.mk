bfec1fc23bfcc767b4e9733b21dbe75e4b66988a
David Wragg <david@rabbitmq.com>
2011-02-21 14:34:41 +0000

    s/IN_BROKER/WITH_BROKER/

--- deps/rabbitmq_management/package.mk
+++ deps/rabbitmq_management/package.mk
@@ -1,6 +1,6 @@
 RELEASABLE:=true
 DEPS:=rabbitmq-mochiweb webmachine-wrapper rabbitmq-server rabbitmq-erlang-client rabbitmq-management-agent
-IN_BROKER_TEST_COMMANDS:=rabbit_mgmt_test_all:all_tests()
+WITH_BROKER_TEST_COMMANDS:=rabbit_mgmt_test_all:all_tests()
 EXTRA_PACKAGE_DIRS:=$(PACKAGE_DIR)/priv
 EXTRA_TARGETS:=$(shell find $(EXTRA_PACKAGE_DIRS) -type f) $(PACKAGE_DIR)/priv/www-cli/rabbitmqadmin
 
