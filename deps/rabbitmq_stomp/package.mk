baa80391937019ffeef1267fbd858d9118849967
David Wragg <david@rabbitmq.com>
2011-02-21 14:39:56 +0000

    s/IN_BROKER/WITH_BROKER/

--- deps/rabbitmq_stomp/package.mk
+++ deps/rabbitmq_stomp/package.mk
@@ -1,7 +1,7 @@
 RELEASABLE:=true
 DEPS:=rabbitmq-server rabbitmq-erlang-client
 STANDALONE_TEST_COMMANDS:=eunit:test([rabbit_stomp_test_util,rabbit_stomp_test_frame],[verbose])
-IN_BROKER_TEST_SCRIPTS:=$(PACKAGE_DIR)/test/src/test.py
+WITH_BROKER_TEST_SCRIPTS:=$(PACKAGE_DIR)/test/src/test.py
 
 define package_rules
 
