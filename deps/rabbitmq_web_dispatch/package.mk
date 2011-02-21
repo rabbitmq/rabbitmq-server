00c6bea59d2214e13545256c0ea65066be1671b8
David Wragg <david@rabbitmq.com>
2011-02-21 14:35:13 +0000

    s/IN_BROKER/WITH_BROKER/

--- deps/rabbitmq_web_dispatch/package.mk
+++ deps/rabbitmq_web_dispatch/package.mk
@@ -1,3 +1,3 @@
 DEPS:=mochiweb-wrapper
-IN_BROKER_TEST_COMMANDS:=rabbit_mochiweb_test:test()
+WITH_BROKER_TEST_COMMANDS:=rabbit_mochiweb_test:test()
 STANDALONE_TEST_COMMANDS:=rabbit_mochiweb_test_unit:test()
