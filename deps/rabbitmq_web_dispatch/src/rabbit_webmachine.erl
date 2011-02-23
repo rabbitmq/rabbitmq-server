26291b561a73d2cf517a6675572af4009f63c28b
Michael Bridgen <mikeb@rabbitmq.com>
2011-02-23 14:51:44 +0000

    Change setup to set the logger env variable, so lookups internal to webmachine will at least work. provide convenience defaults.

--- deps/rabbitmq_web_dispatch/src/rabbit_webmachine.erl
+++ deps/rabbitmq_web_dispatch/src/rabbit_webmachine.erl
@@ -19,17 +19,29 @@
 %% We hardwire the "error handler" and use a "logging module" if
 %% supplied.
 
--export([makeloop/1, makeloop/2, setup/0]).
+-export([makeloop/1, setup/1, setup/2]).
 
-setup() ->
+-define(WM, webmachine).
+-define(LOGGER_KEY, webmachine_logger_module).
+-define(HANDLER_KEY, error_handler).
+
+setup(defaults) ->
+    setup(none);
+setup(LogModule) ->
+    setup(LogModule, webmachine_error_handler).
+
+setup(LogModule, ErrorHandler) ->
     %% Many internal procedures in webmachine check the application
-    %% env for the error_handler.
-    application:set_env(webmachine, error_handler, webmachine_error_handler).
+    %% env for the error_handler.  The best we can do in the presence
+    %% of more than one application using webmachine is "last one
+    %% to use setup, wins".
+    application:set_env(?WM, ?HANDLER_KEY, ErrorHandler),
+    case LogModule of
+        none -> application:unset_env(?WM, ?LOGGER_KEY);
+        Mod  -> application:set_env(?WM, ?LOGGER_KEY, Mod)
+    end.
 
 makeloop(Dispatch) ->
-    makeloop(Dispatch, none).
-
-makeloop(Dispatch, LogModule) ->
     fun (MochiReq) ->
             Req = webmachine:new_request(mochiweb, MochiReq),
             {Path, _} = Req:path(),
@@ -42,30 +54,34 @@ makeloop(Dispatch, LogModule) ->
                           404, Req, {none, none, []}),
                     Req1 = {webmachine_request, ReqState1},
                     {ok, ReqState2} = Req1:append_to_response_body(ErrorHTML),
-                    Req2 = {webmachine_request,ReqState2},
+                    Req2 = {webmachine_request, ReqState2},
                     {ok, ReqState3} = Req2:send_response(404),
-                    maybe_log_access(LogModule, ReqState3);
+                    maybe_log_access(ReqState3);
                 {Mod, ModOpts, HostTokens, Port, PathTokens, Bindings,
                  AppRoot, StringPath} ->
                     BootstrapResource = webmachine_resource:new(x,x,x,x),
                     {ok, Resource} = BootstrapResource:wrap(Mod, ModOpts),
-                    {ok,RS1} = Req:load_dispatch_data(Bindings,HostTokens,Port,
-                                                      PathTokens,AppRoot,StringPath),
-                    XReq1 = {webmachine_request,RS1},
-                    {ok,RS2} = XReq1:set_metadata('resource_module', Mod),
+                    {ok, RS1} = Req:load_dispatch_data(Bindings, HostTokens, Port,
+                                                      PathTokens,
+                                                      AppRoot, StringPath),
+                    XReq1 = {webmachine_request, RS1},
+                    {ok, RS2} = XReq1:set_metadata('resource_module', Mod),
                     try
                         webmachine_decision_core:handle_request(Resource, RS2)
                     catch
                         error:_ ->
-                            FailReq = {webmachine_request,RS2},
-                            {ok,RS3} = FailReq:send_response(500),
-                            maybe_log_access(LogModule, RS3)
+                            FailReq = {webmachine_request, RS2},
+                            {ok, RS3} = FailReq:send_response(500),
+                            maybe_log_access(RS3)
                     end
             end
     end.
 
-maybe_log_access(none, _) -> ok;
-maybe_log_access(Mod, ReqState) ->
-    Req = {webmachine_request,ReqState},
-    {LogData,_ReqState1} = Req:log_data(),
+maybe_log_access(ReqState) ->
+    maybe_log_access(application:get_env(?WM, ?LOGGER_KEY), ReqState).
+
+maybe_log_access(undefined, _) -> ok;
+maybe_log_access({ok, Mod}, ReqState) ->
+    Req = {webmachine_request, ReqState},
+    {LogData, _ReqState1} = Req:log_data(),
     Mod:log_access(LogData).
