254e3d88dc4e2c6b9ef3e441529565644ff1295e
Rob Harrop <rob@rabbitmq.com>
2011-03-11 11:29:54 +0000

    Take write lock when counting records, reduces the constant factor during delete

--- src/rabbit_exchange_type_topic.erl
+++ src/rabbit_exchange_type_topic.erl
@@ -247,7 +247,7 @@ trie_binding_count(X, Node) ->
               _            = '_'}).
 
 count(Table, Match) ->
-    length(mnesia:match_object(Table, Match, read)).
+    length(mnesia:match_object(Table, Match, write)).
 
 trie_remove_all_edges(X) ->
     remove_all(rabbit_topic_trie_edge,
