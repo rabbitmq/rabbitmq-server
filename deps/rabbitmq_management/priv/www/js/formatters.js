dbe042f77dd8f087964d06fa64d3f7352f7d76d2
Simon MacMullen <simon@rabbitmq.com>
2011-03-16 12:13:31 +0000

    Use a disabled textarea to enforce wrapping.

--- deps/rabbitmq_management/priv/www/js/formatters.js
+++ deps/rabbitmq_management/priv/www/js/formatters.js
@@ -226,29 +226,7 @@ function fmt_idle_long(obj) {
 }
 
 function fmt_escape_html(txt) {
-    return txt.replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/\n/g, '<br/>');
-}
-
-function fmt_maybe_wrap(txt, encoding) {
-    if (encoding == 'string') return fmt_escape_html(txt);
-
-    var WRAP = 120;
-    var res = '';
-    while (txt != '') {
-        var i = txt.indexOf('\n');
-        if (i == -1 || i > WRAP) {
-            i = Math.min(WRAP, txt.length);
-            res += txt.substring(0, i) + '\n';
-            txt = txt.substring(i);
-        }
-        else {
-            res += txt.substring(0, i + 1);
-            txt = txt.substring(i + 1);
-        }
-    }
-    return fmt_escape_html(res);
+    return txt.replace(/</g, '&lt;').replace(/>/g, '&gt;');
 }
 
 function alt_rows(i) {
