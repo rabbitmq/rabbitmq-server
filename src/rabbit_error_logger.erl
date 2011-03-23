52918e9da3964cf602499f75d420355e5451dd5c
Matthias Radestock <matthias@rabbitmq.com>
2011-03-23 16:26:33 +0000

    cosmetic

--- src/rabbit_error_logger.erl
+++ src/rabbit_error_logger.erl
@@ -67,13 +67,12 @@ publish(_Other, _Format, _Data, _State) ->
     ok.
 
 publish1(RoutingKey, Format, Data, LogExch) ->
+    %% 0-9-1 says the timestamp is a "64 bit POSIX timestamp". That's
+    %% second resolution, not millisecond.
+    Timestamp = rabbit_misc:now_ms() div 1000,
     {ok, _RoutingRes, _DeliveredQPids} =
         rabbit_basic:publish(LogExch, RoutingKey, false, false, none,
                              #'P_basic'{content_type = <<"text/plain">>,
-                                        %% NB: 0-9-1 says it's a "64 bit POSIX
-                                        %% timestamp". That's second
-                                        %% resolution, not millisecond.
-                                        timestamp    =
-                                            rabbit_misc:now_ms() div 1000},
+                                        timestamp    = Timestamp},
                              list_to_binary(io_lib:format(Format, Data))),
     ok.
