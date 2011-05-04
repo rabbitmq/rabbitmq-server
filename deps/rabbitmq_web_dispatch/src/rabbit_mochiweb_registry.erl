b55399be41da2b0e1b61300d217ce2328b35259a
Simon MacMullen <simon@rabbitmq.com>
2011-05-04 14:43:03 +0100

    Oops

--- deps/rabbitmq_web_dispatch/src/rabbit_mochiweb_registry.erl
+++ deps/rabbitmq_web_dispatch/src/rabbit_mochiweb_registry.erl
@@ -7,7 +7,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
 
--define(APP, rabbit_mochiweb).
+-define(APP, rabbitmq_mochiweb).
 
 %% This gen_server is merely to serialise modifications to the dispatch
 %% table for instances.
