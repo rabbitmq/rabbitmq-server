d03d08e75937bec437d664203c7a0ff845a3f46a
Matthew Sackman <matthew@rabbitmq.com>
2011-06-02 15:32:33 +0100

    Reduce diff from bug23554: Scaffolding for programmatically deciding backing queue

--- src/rabbit_amqqueue_process.erl
+++ src/rabbit_amqqueue_process.erl
@@ -97,12 +97,11 @@ info_keys() -> ?INFO_KEYS.
 init(Q) ->
     ?LOGDEBUG("Queue starting - ~p~n", [Q]),
     process_flag(trap_exit, true),
-    {ok, BQ} = application:get_env(backing_queue_module),
 
     {ok, #q{q                   = Q#amqqueue{pid = self()},
             exclusive_consumer  = none,
             has_had_consumers   = false,
-            backing_queue       = BQ,
+            backing_queue       = backing_queue_module(Q),
             backing_queue_state = undefined,
             active_consumers    = queue:new(),
             blocked_consumers   = queue:new(),
@@ -225,6 +224,10 @@ next_state(State = #q{backing_queue = BQ, backing_queue_state = BQS}) ->
         idle  -> {stop_sync_timer(State1),   0        };
         timed -> {ensure_sync_timer(State1), 0        }
     end.
+
+backing_queue_module(#amqqueue{}) ->
+    {ok, BQM} = application:get_env(backing_queue_module),
+    BQM.
 
 ensure_sync_timer(State = #q{sync_timer_ref = undefined}) ->
     {ok, TRef} = timer:apply_after(
