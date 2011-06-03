6d07bfa27c828fa56b58e62ac08a4312da32e4d6
Rob Harrop <rob@rabbitmq.com>
2011-06-03 14:47:36 +0100

    SSL tests running again

--- deps/rabbitmq_stomp/package.mk
+++ deps/rabbitmq_stomp/package.mk
@@ -3,10 +3,38 @@ DEPS:=rabbitmq-server rabbitmq-erlang-client
 STANDALONE_TEST_COMMANDS:=eunit:test([rabbit_stomp_test_util,rabbit_stomp_test_frame],[verbose])
 WITH_BROKER_TEST_SCRIPTS:=$(PACKAGE_DIR)/test/src/test.py
 
+RABBITMQ_TEST_PATH=../../rabbitmq-test
+CERTS_DIR:=$(abspath test/certs)
+CAN_RUN_SSL:=$(shell if [ -d $(RABBITMQ_TEST_PATH) ]; then echo "true"; else echo "false"; fi)
+
+TEST_CONFIG_PATH=$(TEST_EBIN_DIR)/test.config
+WITH_BROKER_TEST_CONFIG:=$(TEST_EBIN_DIR)/test
+
+ifeq ($(CAN_RUN_SSL),true)
+
+WITH_BROKER_TEST_SCRIPTS += $(PACKAGE_DIR)/test/src/test_ssl.py
+
+$(TEST_CONFIG_PATH): $(CERTS_DIR)
+	sed -e "s|%%CERTS_DIR%%|$(CERTS_DIR)|g" < test/src/ssl.config > $@
+	echo $(WITH_BROKER_TEST_CONFIG)
+
+$(CERTS_DIR): $(SSL_CONFIG_PATH)
+	mkdir -p $(CERTS_DIR)
+	make -C $(RABBITMQ_TEST_PATH)/certs all PASSWORD=test DIR=$(CERTS_DIR)
+
+$(SSL_CONFIG_PATH)
+else
+$(TEST_CONFIG_PATH):
+	echo "[]." >> $@
+endif
+
 define package_rules
 
-$(PACKAGE_DIR)+pre-test::
+$(PACKAGE_DIR)+pre-test:: $(TEST_CONFIG_PATH)
 	make -C $(PACKAGE_DIR)/deps/stomppy
+
+$(PACKAGE_DIR)+clean::
+	rm -rf $(CERTS_DIR)
 
 $(PACKAGE_DIR)+clean-with-deps::
 	make -C $(PACKAGE_DIR)/deps/stomppy distclean
--- deps/rabbitmq_stomp/test/src/ssl_lifecycle.py
+++ deps/rabbitmq_stomp/test/src/ssl_lifecycle.py
@@ -7,9 +7,9 @@ import base
 class TestSslClient(unittest.TestCase):
 
     def __ssl_connect(self):
-        ssl_key_file = os.path.abspath("certs/client/key.pem")
-        ssl_cert_file = os.path.abspath("certs/client/cert.pem")
-        ssl_ca_certs = os.path.abspath("certs/testca/cacert.pem")
+        ssl_key_file = os.path.abspath("test/certs/client/key.pem")
+        ssl_cert_file = os.path.abspath("test/certs/client/cert.pem")
+        ssl_ca_certs = os.path.abspath("test/certs/testca/cacert.pem")
 
         conn = stomp.Connection(user="guest", passcode="guest",
                                 host_and_ports = [ ('localhost', 61614) ],
