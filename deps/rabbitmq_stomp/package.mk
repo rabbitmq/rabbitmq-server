23dacfdc1907f527c0b1c087c906292ebdd5596b
Steve Powell <steve@rabbitmq.com>
2011-07-18 18:30:42 +0100

    Added ABS_PACKAGE_DIR as simple variable and use it to resolve *ssl.config files.

--- deps/rabbitmq_stomp/package.mk
+++ deps/rabbitmq_stomp/package.mk
@@ -4,7 +4,9 @@ STANDALONE_TEST_COMMANDS:=eunit:test([rabbit_stomp_test_util,rabbit_stomp_test_f
 WITH_BROKER_TEST_SCRIPTS:=$(PACKAGE_DIR)/test/src/test.py $(PACKAGE_DIR)/test/src/test_connect_options.py
 
 RABBITMQ_TEST_PATH=$(PACKAGE_DIR)/../../rabbitmq-test
-CERTS_DIR:=$(abspath test/certs)
+ABS_PACKAGE_DIR:=$(abspath $(PACKAGE_DIR))
+
+CERTS_DIR:=$(ABS_PACKAGE_DIR)/test/certs
 CAN_RUN_SSL:=$(shell if [ -d $(RABBITMQ_TEST_PATH) ]; then echo "true"; else echo "false"; fi)
 
 TEST_CONFIG_PATH=$(TEST_EBIN_DIR)/test.config
@@ -14,17 +16,18 @@ ifeq ($(CAN_RUN_SSL),true)
 
 WITH_BROKER_TEST_SCRIPTS += $(PACKAGE_DIR)/test/src/test_ssl.py
 
-$(TEST_CONFIG_PATH): $(CERTS_DIR) test/src/ssl.config
-	sed -e "s|%%CERTS_DIR%%|$(CERTS_DIR)|g" < test/src/ssl.config > $@
-	echo $(WITH_BROKER_TEST_CONFIG)
+$(TEST_CONFIG_PATH): $(CERTS_DIR) $(ABS_PACKAGE_DIR)/test/src/ssl.config
+	sed -e "s|%%CERTS_DIR%%|$(CERTS_DIR)|g" < $(ABS_PACKAGE_DIR)/test/src/ssl.config > $@
+	@echo "Running SSL tests" $(WITH_BROKER_TEST_CONFIG)
 
 $(CERTS_DIR):
 	mkdir -p $(CERTS_DIR)
 	make -C $(RABBITMQ_TEST_PATH)/certs all PASSWORD=test DIR=$(CERTS_DIR)
 
 else
-$(TEST_CONFIG_PATH): test/src/non_ssl.config
-	cp test/src/non_ssl.config $@
+$(TEST_CONFIG_PATH): $(ABS_PACKAGE_DIR)/test/src/non_ssl.config
+	cp $(ABS_PACKAGE_DIR)/test/src/non_ssl.config $@
+	@echo "NOT running SSL tests" $(WITH_BROKER_TEST_CONFIG)
 
 endif
 
