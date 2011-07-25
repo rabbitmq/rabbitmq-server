8ff380587f981e41f0da6aff58fb1cb5ae9eb7ca
Simon MacMullen <simon@rabbitmq.com>
2011-07-25 14:01:00 +0100

    This much more closely resembles something that works.

--- deps/rabbitmq_amqp1_0/package.mk
+++ deps/rabbitmq_amqp1_0/package.mk
@@ -3,18 +3,23 @@ APP_NAME=rabbitmq_amqp1_0
 DEPS:=rabbitmq-server rabbitmq-erlang-client
 WITH_BROKER_TEST_COMMANDS:=eunit:test(rabbit_amqp1_0_test,[verbose])
 
-FRAMING_ERL=src/rabbit_amqp1_0_framing0.erl
-FRAMING_HRL=include/rabbit_amqp1_0_framing.hrl
+FRAMING_HRL=$(PACKAGE_DIR)/include/rabbit_amqp1_0_framing.hrl
+FRAMING_ERL=$(PACKAGE_DIR)/src/rabbit_amqp1_0_framing0.erl
 CODEGEN=./codegen.py
 CODEGEN_SPECS=spec/messaging.xml spec/transport.xml
 
+INCLUDE_HRLS+=$(FRAMING_HRL)
+SOURCE_ERLS+=$(FRAMING_ERL)
+
+define package_rules
+
 $(FRAMING_ERL): $(CODEGEN) $(CODEGEN_SPECS)
-	$(CODEGEN) erl $(CODEGEN_SPECS) > $@
+	$(CODEGEN) erl $(CODEGEN_SPECS) > $$@
 
 $(FRAMING_HRL): $(CODEGEN) $(CODEGEN_SPECS)
-	$(CODEGEN) hrl $(CODEGEN_SPECS) > $@
-
-$(EBIN_BEAMS): $(FRAMING_HRL)
+	$(CODEGEN) hrl $(CODEGEN_SPECS) > $$@
 
 $(PACKAGE_DIR)+clean::
 	rm -f $(FRAMING_HRL) $(FRAMING_ERL)
+
+endef
