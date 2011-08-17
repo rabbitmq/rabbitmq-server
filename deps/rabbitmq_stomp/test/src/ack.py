ed027f0fb2ab063b8e0ec4c02c11f8541914ca5e
Steve Powell <steve@rabbitmq.com>
2011-08-17 14:20:57 +0100

    Improve ack tests

--- deps/rabbitmq_stomp/test/src/ack.py
+++ deps/rabbitmq_stomp/test/src/ack.py
@@ -57,25 +57,32 @@ class TestAck(base.BaseTest):
                             headers={'prefetch-count': '10'})
         self.conn.send("test1", destination=d)
         self.conn.send("test2", destination=d)
-        self.assertTrue(self.listener.await(4), "initial message not received")
+        self.assertTrue(self.listener.await(4), "Both initial messages not received")
         self.assertEquals(2, len(self.listener.messages))
 
-        # disconnect with no ack
+        # disconnect without acks
         self.conn.disconnect()
 
         # now reconnect
         conn2 = self.create_connection()
         try:
             listener2 = base.WaitableListener()
-            listener2.reset(2) ## expecting 2 messages
+            listener2.reset(2) ## expect 2 messages
             conn2.set_listener('', listener2)
             conn2.subscribe(destination=d, ack='client-individual',
                             headers={'prefetch-count': '10'})
-            self.assertTrue(listener2.await(), "message not received again")
-            self.assertEquals(2, len(listener2.messages))
+            self.assertTrue(listener2.await(2.5), "Did not receive 2 messages")
+            self.assertEquals(2, len(listener2.messages), "Not exactly 2 messages received")
 
-            # now ack only the last message - expecting individual behaviour
-            mid = listener2.messages[1]['headers']['message-id']
+            # now ack only the 'test2' message - expecting individual behaviour
+            nummsgs = len(listener2.messages)
+            mid = None
+            for ind in range(nummsgs):
+                if listener2.messages[ind]['message']=="test2":
+                    mid = listener2.messages[ind]['headers']['message-id']
+                    self.assertEquals(1, ind, 'Expecting test2 to be second message')
+                    break
+            self.assertTrue(mid, "Did not find test2 message id.")
             conn2.ack({'message-id':mid})
         finally:
             conn2.stop()
@@ -84,13 +91,13 @@ class TestAck(base.BaseTest):
         conn3 = self.create_connection()
         try:
             listener3 = base.WaitableListener()
-            listener3.reset(1) ## expecting a single message
+            listener3.reset(2) ## expecting a single message, but wait for two
             conn3.set_listener('', listener3)
             conn3.subscribe(destination=d)
-            self.assertTrue(listener3.await(20),
-                             "Expected to see a message. ACK not working?")
-            self.assertEquals(1, len(listener3.messages))
-            self.assertEquals("test1", listener3.messages[0]['message'])
+            self.assertFalse(listener3.await(2.5),
+                             "Expected to see only one message. ACK not working?")
+            self.assertEquals(1, len(listener3.messages), "Expecting exactly one message")
+            self.assertEquals("test1", listener3.messages[0]['message'], "Unexpected message remains")
         finally:
             conn3.stop()
 
@@ -142,14 +149,14 @@ class TestAck(base.BaseTest):
         d = "/topic/prefetch-test"
 
         # subscribe and send message
-        self.listener.reset(6) ## expecting 6 messages
+        self.listener.reset(6) ## expect 6 messages
         self.conn.subscribe(destination=d, ack='client',
                             headers={'prefetch-count': '5'})
 
         for x in range(10):
             self.conn.send("test" + str(x), destination=d)
 
-        self.assertFalse(self.listener.await(5),
+        self.assertFalse(self.listener.await(3),
                          "Should not have been able to see 6 messages")
         self.assertEquals(5, len(self.listener.messages))
 
@@ -165,7 +172,7 @@ class TestAck(base.BaseTest):
         self.listener.reset()
 
         self.conn.send_frame("NACK", {"message-id" : message_id})
-        self.assertTrue(self.listener.await(), "Not received message again")
+        self.assertTrue(self.listener.await(), "Not received message after NACK")
         message_id = self.listener.messages[0]['headers']['message-id']
         self.conn.ack({'message-id' : message_id})
 
