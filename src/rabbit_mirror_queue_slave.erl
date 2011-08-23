193a44a0eb518ef41fb8541ed254326283bd7136
Matthew Sackman <matthew@rabbitmq.com>
2011-08-23 13:48:08 +0100

    Format status on slaves too. Note in sender queue we take a different approach from in VQ - in VQ we have the msg_status wrapper, which contains the msg_id. In slave and sender_queues, we don't, so we mask out the content in the basic_message rather than the whole of the basic_message

--- src/rabbit_mirror_queue_slave.erl
+++ src/rabbit_mirror_queue_slave.erl
@@ -37,7 +37,8 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3, handle_pre_hibernate/1, prioritise_call/3,
-         prioritise_cast/2, prioritise_info/2]).
+         prioritise_cast/2, prioritise_info/2, format_message_queue/2,
+         format_status/2]).
 
 -export([joined/2, members_changed/3, handle_msg/3]).
 
@@ -303,6 +304,26 @@ prioritise_info(Msg, _State) ->
         sync_timeout                         -> 6;
         _                                    -> 0
     end.
+
+format_message_queue(Opt, MQ) -> rabbit_misc:format_message_queue(Opt, MQ).
+
+format_status(Opt, [PDict, State = #state { backing_queue       = BQ,
+                                            backing_queue_state = BQS,
+                                            sender_queues       = SQ,
+                                            msg_id_ack          = MA,
+                                            msg_id_status       = MS,
+                                            known_senders       = KS }]) ->
+    State1 = setelement(1, State, state_formatted),
+    State2 = lists:foldl(
+               fun({Pos, Value}, StateN) -> setelement(Pos, StateN, Value) end,
+               State1, [{#state.backing_queue_state,
+                         BQ:format_status(Opt, [PDict, BQS])},
+                        {#state.sender_queues, format_sender_queues(SQ)} |
+                        [{Pos, dict:to_list(Dict)} ||
+                           {Pos, Dict} <- [{#state.msg_id_ack,    MA},
+                                           {#state.msg_id_status, MS},
+                                           {#state.known_senders, KS}]]]),
+    [{data, [{"State", State2}]}].
 
 %% ---------------------------------------------------------------------------
 %% GM
@@ -905,3 +926,13 @@ set_synchronised(true, State) ->
     State;
 set_synchronised(false, State = #state { synchronised = false }) ->
     State.
+
+format_sender_queues(SQ) ->
+    [{ChPid, {format_sender_queue(MQ), sets:to_list(PendingCh)}}
+     || {ChPid, {MQ, PendingCh}} <- dict:to_list(SQ)].
+
+format_sender_queue(MQ) ->
+    [{Delivery #delivery { message = Msg #basic_message { content = '_' } },
+      EnqueueOnPromotion}
+     || {Delivery = #delivery { message = Msg }, EnqueueOnPromotion}
+            <- queue:to_list(MQ)].
