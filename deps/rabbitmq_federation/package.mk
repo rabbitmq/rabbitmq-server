5709670ba3154a2f5ac20fd27f31fc76016e5837
Matthias Radestock <matthias@rabbitmq.com>
2011-09-01 15:20:14 +0100

    mark federation as releasable

--- deps/rabbitmq_federation/package.mk
+++ deps/rabbitmq_federation/package.mk
@@ -1,3 +1,4 @@
+RELEASABLE:=true
 DEPS:=rabbitmq-erlang-client
 WITH_BROKER_TEST_COMMANDS:=eunit:test(rabbit_federation_unit_test,[verbose]) eunit:test(rabbit_federation_test,[verbose])
 WITH_BROKER_TEST_CONFIG:=$(PACKAGE_DIR)/etc/rabbit-test
