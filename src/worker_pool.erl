469f08821744d07be23cad7e7b22a5c4d040e953
Alexandru Scvortov <alexandru@rabbitmq.com>
2011-09-09 10:22:05 +0100

    refactor + handle MFAs

--- src/worker_pool.erl
+++ src/worker_pool.erl
@@ -61,14 +61,10 @@ start_link() ->
 submit(Fun) ->
     %% If the worker_pool is not running, just run the Fun in the
     %% current process.
-    case whereis(?SERVER) of
-        undefined -> Fun();
-        _         -> case get(worker_pool_worker) of
-                         true -> worker_pool_worker:run(Fun);
-                         _    -> Pid = gen_server2:call(?SERVER, next_free,
-                                                        infinity),
-                                 worker_pool_worker:submit(Pid, Fun)
-                     end
+    case get(worker_pool_worker) =:= true orelse whereis(?SERVER) =:= undefined of
+        true  -> worker_pool_worker:run(Fun);
+        false -> Pid = gen_server2:call(?SERVER, next_free, infinity),
+                 worker_pool_worker:submit(Pid, Fun)
     end.
 
 submit_async(Fun) ->
