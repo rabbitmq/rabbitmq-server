efa39432992e017fa837c32558aeab2f3d63d385
Matthew Sackman <matthew@rabbitmq.com>
2011-10-19 13:34:02 +0100

    There should be no space there.

--- deps/amqp_client/src/amqp_gen_connection.erl
+++ deps/amqp_client/src/amqp_gen_connection.erl
@@ -242,7 +242,7 @@ handle_command({open_channel, ProposedNumber, Consumer}, _From,
     {reply, amqp_channels_manager:open_channel(ChMgr, ProposedNumber, Consumer,
                                                Mod:open_channel_args(MState)),
      State};
- handle_command({close, #'connection.close'{} = Close}, From, State) ->
+handle_command({close, #'connection.close'{} = Close}, From, State) ->
      app_initiated_close(Close, From, State).
 
 %%---------------------------------------------------------------------------
