1a126a457c74b1e2fc45d13af9b993dbb31936f3
Matthias Radestock <matthias@rabbitmq.com>
2011-10-21 19:07:58 +0100

    eliminate unused var warning

--- src/rabbit_amqqueue_process.erl
+++ src/rabbit_amqqueue_process.erl
@@ -551,12 +551,11 @@ deliver_or_enqueue(Delivery = #delivery{message = Message,
                  ensure_ttl_timer(State2#q{backing_queue_state = BQS1})
     end.
 
-requeue_and_run(AckTags, State = #q{backing_queue = BQ, ttl=TTL}) ->
-    run_backing_queue(
-      BQ, fun (M, BQS) ->
-                  {_MsgIds, BQS1} = M:requeue(AckTags, BQS),
-                  BQS1
-          end, State).
+requeue_and_run(AckTags, State = #q{backing_queue = BQ}) ->
+    run_backing_queue(BQ, fun (M, BQS) ->
+                                  {_MsgIds, BQS1} = M:requeue(AckTags, BQS),
+                                  BQS1
+                          end, State).
 
 fetch(AckRequired, State = #q{backing_queue_state = BQS,
                               backing_queue       = BQ}) ->
