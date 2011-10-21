d532140b40aa0a61a6c045c8f428942719452b09
Matthias Radestock <matthias@rabbitmq.com>
2011-10-21 18:30:24 +0100

    expand scope of "no alternate exchange" optimisation to cover the "routed somewhere" case too

--- src/rabbit_exchange.erl
+++ src/rabbit_exchange.erl
@@ -257,9 +257,8 @@ route1(Delivery, {WorkList, SeenXs, QNames}) ->
                                DstNames))
     end.
 
-%% Optimisation
-process_alternate(#exchange{arguments = []}, []) -> [];
-
+process_alternate(#exchange{arguments = []}, Results) -> %% optimisation
+     Results;
 process_alternate(#exchange{name = XName, arguments = Args}, []) ->
     case rabbit_misc:r_arg(XName, exchange, Args, <<"alternate-exchange">>) of
         undefined -> [];
