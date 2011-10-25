e47f91aa09663cd5969c824cca78fe03c5001589
Alexandru Scvortov <alexandru@rabbitmq.com>
2011-10-25 15:45:13 +0100

    rebase on default

--- src/rabbit_control.erl
+++ src/rabbit_control.erl
@@ -161,6 +161,10 @@ usage() ->
 
 %%----------------------------------------------------------------------------
 
+action(stop, Node, [PidFile], _Opts, Inform) ->
+    action(stop, Node, [], _Opts, Inform),
+    wait_for_process_death(PidFile);
+
 action(stop, Node, [], _Opts, Inform) ->
     Inform("Stopping and halting node ~p", [Node]),
     call(Node, {rabbit, stop_and_halt, []});
@@ -374,6 +378,20 @@ wait_for_application(Node, Pid) ->
                               wait_for_application(Node, Pid)
                  end;
         false -> {error, process_not_running}
+    end.
+
+wait_for_process_death(PidFile) ->
+    Pid = case file:read_file(PidFile) of
+              {ok, Bin}      -> string:strip(binary_to_list(Bin), right, $\n);
+              {error, _} = E -> exit({error, {could_not_read_pid, E}})
+          end,
+    wait_for_process_death1(Pid).
+
+wait_for_process_death1(Pid) ->
+    case process_up(Pid) of
+        true  -> timer:sleep(1000),
+                 wait_for_process_death1(Pid);
+        false -> ok
     end.
 
 wait_and_read_pid_file(PidFile) ->
