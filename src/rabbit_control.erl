76116cdb4d6fd916411e1324e9f03601d3172ece
Emile Joubert <emile@rabbitmq.com>
2011-10-31 16:34:24 +0000

    Whitespace

--- src/rabbit_control.erl
+++ src/rabbit_control.erl
@@ -1,4 +1,3 @@
-
 %% The contents of this file are subject to the Mozilla Public License
 %% Version 1.1 (the "License"); you may not use this file except in
 %% compliance with the License. You may obtain a copy of the License
