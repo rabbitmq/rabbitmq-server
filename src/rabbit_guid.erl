c83aafd1c3d34e35e8c21cf809b15e42f54c985b
Matthias Radestock <matthias@rabbitmq.com>
2011-11-15 16:31:54 +0000

    don't explain why we don't do something that doesn't work

--- src/rabbit_guid.erl
+++ src/rabbit_guid.erl
@@ -77,12 +77,9 @@ guid() ->
     %% slow since it takes a global lock and makes a system call.
     %%
     %% A persisted serial number, the node, and a unique reference
-    %% (per node-incarnation) uniquely identifies a process in space
+    %% (per node incarnation) uniquely identifies a process in space
     %% and time. We combine that with a process-local counter to give
     %% us a GUID.
-    %%
-    %% We used to use self/0 here instead of the node and unique
-    %% reference. But PIDs wrap.
     G = case get(guid) of
             undefined -> Serial = gen_server:call(?SERVER, serial, infinity),
                          {{Serial, node(), make_ref()}, 0};
