3511aafb28dff91d553e454ea6fa6e4e8c22b3af
Simon MacMullen <simon@rabbitmq.com>
2012-01-05 13:26:44 +0000

    Use netcat rather than nmap.

--- deps/rabbitmq_auth_backend_ldap/package.mk
+++ deps/rabbitmq_auth_backend_ldap/package.mk
@@ -1,7 +1,7 @@
 RELEASABLE:=true
 DEPS:=rabbitmq-server rabbitmq-erlang-client eldap-wrapper
 
-ifeq ($(shell nmap -p 389 localhost | grep '389/tcp open' > /dev/null && echo true),true)
+ifeq ($(shell nc -z localhost 389 && echo true),true)
 WITH_BROKER_TEST_COMMANDS:=eunit:test(rabbit_auth_backend_ldap_test,[verbose])
 WITH_BROKER_TEST_CONFIG:=$(PACKAGE_DIR)/etc/rabbit-test
 endif
