03dbb4da7ea356324d2fbaa8f66193cfd0de37b8
Matthias Radestock <matthias@rabbitmq.com>
2012-01-10 12:38:59 +0000

    oops

--- deps/rabbitmq_management_agent/src/rabbit_mgmt_external_stats.erl
+++ deps/rabbitmq_management_agent/src/rabbit_mgmt_external_stats.erl
@@ -147,9 +147,9 @@ infos(Items, State) -> [{Item, i(Item, State)} || Item <- Items].
 i(fd_used,  #state{fd_used  = FdUsed})  -> FdUsed;
 i(fd_total, #state{fd_total = FdTotal}) -> FdTotal;
 i(sockets_used,   _State) ->
-    proplists:get_value(obtain_count, file_handle_cache:info([socket_count]));
+    proplists:get_value(socket_count, file_handle_cache:info([socket_count]));
 i(sockets_total,  _State) ->
-    proplists:get_value(obtain_limit, file_handle_cache:info([socket_limit]));
+    proplists:get_value(socket_limit, file_handle_cache:info([socket_limit]));
 i(os_pid,         _State) -> list_to_binary(os:getpid());
 i(mem_ets,        _State) -> erlang:memory(ets);
 i(mem_binary,     _State) -> erlang:memory(binary);
