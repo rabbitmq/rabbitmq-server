922fed5e800748f3e126747fc6148f4310b094ba
Simon MacMullen <simon@rabbitmq.com>
2012-01-19 16:08:02 +0000

    Remove underscores from the boot screen. How horrible!

--- src/rabbit.erl
+++ src/rabbit.erl
@@ -46,7 +46,8 @@
                     {enables,     external_infrastructure}]}).
 
 -rabbit_boot_step({database_sync,
-                   [{mfa,         {rabbit_sup, start_child, [mnesia_sync]}},
+                   [{description, "database sync"},
+                    {mfa,         {rabbit_sup, start_child, [mnesia_sync]}},
                     {requires,    database},
                     {enables,     external_infrastructure}]}).
 
@@ -163,8 +164,9 @@
                     {enables,     networking}]}).
 
 -rabbit_boot_step({direct_client,
-                   [{mfa,        {rabbit_direct, boot, []}},
-                    {requires,   log_relay}]}).
+                   [{description, "direct client"},
+                    {mfa,         {rabbit_direct, boot, []}},
+                    {requires,    log_relay}]}).
 
 -rabbit_boot_step({networking,
                    [{mfa,         {rabbit_networking, boot, []}},
