ff722cfaee7b255fc83c1d775dcc0084f2d2d1f9
Francesco Mazzoli <francesco@rabbitmq.com>
2012-01-31 13:55:53 +0000

    Update comment for gen/0.

--- src/rabbit_guid.erl
+++ src/rabbit_guid.erl
@@ -90,8 +90,9 @@ blocks_to_binary({B1, B2, B3, B4}) ->
 %% generate a GUID. This function should be used when performance is a
 %% priority and predictability is not an issue. Otherwise, use gen_secure/0.
 gen() ->
-    %% We hash a fresh GUID and then XOR it with a process-local counter
-    %% to get a new GUID each time.
+    %% We hash a fresh GUID with md5, split it in 5 blocks, and each time we
+    %% need a new guid we hash the first block and the counter and xor the
+    %% result with the remaining blocks.
     {BS, I} =
         case get(guid) of
             undefined ->
