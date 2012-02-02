924ccc903d5d81c16b42902bcfb9bb97da9723ea
Simon MacMullen <simon@rabbitmq.com>
2012-02-02 14:36:16 +0000

    Move rabbit_misc:nodeparts/1 and rabbit_misc:makenode/1 into rabbit_nodes

--- deps/amqp_client/test/test_util.erl
+++ deps/amqp_client/test/test_util.erl
@@ -939,7 +939,7 @@ new_connection(AllowedConnectionTypes, Params) ->
                                   {verify, verify_peer},
                                   {fail_if_no_peer_cert, true}]}] ++ Params);
             {_, "direct"} ->
-                make_direct_params([{node, rabbit_misc:makenode(rabbit)}] ++
+                make_direct_params([{node, rabbit_nodes:make(rabbit)}] ++
                                        Params)
         end,
     amqp_connection:start(Params1).
