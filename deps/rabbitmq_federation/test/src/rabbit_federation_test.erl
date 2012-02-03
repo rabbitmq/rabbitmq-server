dd68ca5fd730f1cb9ff4fba957ecf99532debc62
Emile Joubert <emile@rabbitmq.com>
2012-02-03 14:58:09 +0000

    Increase timeout to allow tests to pass

--- deps/rabbitmq_federation/test/src/rabbit_federation_test.erl
+++ deps/rabbitmq_federation/test/src/rabbit_federation_test.erl
@@ -364,7 +364,7 @@ delete_queue(Ch, Q) ->
 
 publish(Ch, X, Key, Payload) ->
     %% The trouble is that we transmit bindings upstream asynchronously...
-    timer:sleep(200),
+    timer:sleep(1000),
     amqp_channel:call(Ch, #'basic.publish'{exchange    = X,
                                            routing_key = Key},
                       #amqp_msg{payload = Payload}).
