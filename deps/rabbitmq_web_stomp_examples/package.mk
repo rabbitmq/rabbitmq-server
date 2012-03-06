322fce11efdc6e70ca640e9f0389f8f050f87b88
Marek Majkowski <marek@rabbitmq.com>
2012-03-06 16:20:52 +0000

    We do depend on rabbitmq-mochiweb

--- deps/rabbitmq_web_stomp_examples/package.mk
+++ deps/rabbitmq_web_stomp_examples/package.mk
@@ -1,5 +1,5 @@
 RELEASABLE:=true
-DEPS:=rabbitmq-server rabbitmq-web-stomp
+DEPS:=rabbitmq-server rabbitmq-web-stomp rabbitmq-mochiweb
 
 define construct_app_commands
 	cp -r $(PACKAGE_DIR)/priv $(APP_DIR)
