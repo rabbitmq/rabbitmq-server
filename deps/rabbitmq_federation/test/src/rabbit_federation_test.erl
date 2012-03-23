2d1573d65278042d8c574db1cdbb50d7b1668125
Simon MacMullen <simon@rabbitmq.com>
2012-03-23 13:05:06 +0000

    Fix most tests.

--- deps/rabbitmq_federation/test/src/rabbit_federation_test.erl
+++ deps/rabbitmq_federation/test/src/rabbit_federation_test.erl
@@ -447,7 +447,8 @@ links(#'exchange.declare'{exchange  = Name,
     {longstr, Set} = rabbit_misc:table_lookup(Args,  <<"upstream-set">>),
     {ok, Upstreams} = rabbit_federation_upstream:from_set(
                         Set, rabbit_misc:r(<<"/">>, exchange, Name)),
-    [{Name, U#upstream.connection_name, U#upstream.exchange} || U <- Upstreams];
+    [{Name, list_to_binary(U#upstream.connection_name), U#upstream.exchange} ||
+        U <- Upstreams];
 
 links(#'exchange.declare'{}) ->
     [].
