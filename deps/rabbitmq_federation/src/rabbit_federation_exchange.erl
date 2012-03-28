df0f677389d261e68bd4c4a304e5706c26e495bc
Simon MacMullen <simon@rabbitmq.com>
2012-03-28 17:45:03 +0100

    (Roughly) make federation into an exchange decorator. This actually works! Currently it has hard coded policy:
    * Federate exchanges whose names begin with "fed."
    * Always use the "all" upstream set.

--- deps/rabbitmq_federation/src/rabbit_federation_exchange.erl
+++ deps/rabbitmq_federation/src/rabbit_federation_exchange.erl
@@ -14,107 +14,85 @@
 %% Copyright (c) 2007-2012 VMware, Inc.  All rights reserved.
 %%
 
+%% TODO rename this
 -module(rabbit_federation_exchange).
 
 -rabbit_boot_step({?MODULE,
-                   [{description, "federation exchange type"},
+                   [{description, "federation exchange decorator"},
                     {mfa, {rabbit_registry, register,
-                           [exchange, <<"x-federation">>, ?MODULE]}},
+                           [exchange_decorator, <<"federation">>, ?MODULE]}},
                     {requires, rabbit_registry},
                     {enables, recovery}]}).
 
 -include_lib("rabbit_common/include/rabbit_exchange_type_spec.hrl").
 -include_lib("amqp_client/include/amqp_client.hrl").
 
--behaviour(rabbit_exchange_type).
+-behaviour(rabbit_exchange_decorator).
 
 -export([description/0, serialise_events/0, route/2]).
--export([validate/1, create/2, delete/3,
-         add_binding/3, remove_bindings/3, assert_args_equivalence/2]).
+-export([create/2, delete/3, add_binding/3, remove_bindings/3]).
 
 %%----------------------------------------------------------------------------
 
 description() ->
-    [{name, <<"x-federation">>},
-     {description, <<"Federation exchange">>}].
+    [{name, <<"federation">>},
+     {description, <<"Federation exchange decorator">>}].
 
 serialise_events() -> true.
 
-route(X, Delivery) -> with_module(X, fun (M) -> M:route(X, Delivery) end).
+route(_X, _Delivery) -> ok.
 
-validate(#exchange{name      = XName,
-                   arguments = Args} = X) ->
-    rabbit_federation_util:validate_arg(<<"upstream-set">>, longstr, Args),
-    rabbit_federation_util:validate_arg(<<"type">>,         longstr, Args),
-    {longstr, SetName} = rabbit_misc:table_lookup(Args, <<"upstream-set">>),
-    case rabbit_federation_upstream:from_set(SetName, XName) of
-        {error, E} -> fail_error(SetName, E);
-        {ok, _}    -> ok
-    end,
-    {longstr, TypeBin} = rabbit_misc:table_lookup(Args, <<"type">>),
-    case rabbit_exchange:check_type(TypeBin) of
-        'x-federation' -> rabbit_federation_util:fail(
-                            "Type argument must not be x-federation.", []);
-        _              -> ok
-    end,
-    with_module(X, fun (M) -> M:validate(X) end).
+create(transaction, _X) ->
+    ok;
+create(none, #exchange{name = XName}) ->
+    case federate(XName) of
+        true ->
+            Set = <<"all">>,
+            {ok, Upstreams} = rabbit_federation_upstream:from_set(Set, XName),
+            ok = rabbit_federation_db:prune_scratch(XName, Upstreams),
+            {ok, _} = rabbit_federation_link_sup_sup:start_child(XName, {Set, XName}),
+            ok;
+        false ->
+            ok
+    end.
 
-create(transaction, X) ->
-    with_module(X, fun (M) -> M:create(transaction, X) end);
-create(none, X = #exchange{name      = XName,
-                           arguments = Args}) ->
-    {longstr, Set} = rabbit_misc:table_lookup(Args, <<"upstream-set">>),
-    {ok, Upstreams} = rabbit_federation_upstream:from_set(Set, XName),
-    ok = rabbit_federation_db:prune_scratch(XName, Upstreams),
-    {ok, _} = rabbit_federation_link_sup_sup:start_child(XName, {Set, XName}),
-    with_module(X, fun (M) -> M:create(none, X) end).
+delete(transaction, _X, _Bs) ->
+    ok;
+delete(none, #exchange{name = XName}, _Bs) ->
+    case federate(XName) of
+        true ->
+            rabbit_federation_link:stop(XName),
+            ok = rabbit_federation_link_sup_sup:stop_child(XName),
+            rabbit_federation_status:remove(XName),
+            ok;
+        false ->
+            ok
+    end.
 
-delete(transaction, X, Bs) ->
-    with_module(X, fun (M) -> M:delete(transaction, X, Bs) end);
-delete(none, X = #exchange{name = XName}, Bs) ->
-    rabbit_federation_link:stop(XName),
-    ok = rabbit_federation_link_sup_sup:stop_child(XName),
-    rabbit_federation_status:remove(XName),
-    with_module(X, fun (M) -> M:delete(none, X, Bs) end).
+add_binding(transaction, _X, _B) ->
+    ok;
+add_binding(Serial, #exchange{name = XName}, B) ->
+    case federate(XName) of
+        true ->
+            rabbit_federation_link:add_binding(Serial, XName, B),
+            ok;
+        false ->
+            ok
+    end.
 
-add_binding(transaction, X, B) ->
-    with_module(X, fun (M) -> M:add_binding(transaction, X, B) end);
-add_binding(Serial, X = #exchange{name = XName}, B) ->
-    rabbit_federation_link:add_binding(Serial, XName, B),
-    with_module(X, fun (M) -> M:add_binding(serial(Serial, X), X, B) end).
-
-remove_bindings(transaction, X, Bs) ->
-    with_module(X, fun (M) -> M:remove_bindings(transaction, X, Bs) end);
-remove_bindings(Serial, X = #exchange{name = XName}, Bs) ->
-    rabbit_federation_link:remove_bindings(Serial, XName, Bs),
-    with_module(X, fun (M) -> M:remove_bindings(serial(Serial, X), X, Bs) end).
-
-assert_args_equivalence(X = #exchange{name = XName, arguments = Args},
-                        NewArgs) ->
-    rabbit_misc:assert_args_equivalence(Args, NewArgs, XName,
-                                        [<<"upstream">>, <<"type">>]),
-    with_module(X, fun (M) -> M:assert_args_equivalence(X, Args) end).
+remove_bindings(transaction, _X, _Bs) ->
+    ok;
+remove_bindings(Serial, #exchange{name = XName}, Bs) ->
+    case federate(XName) of
+        true ->
+            rabbit_federation_link:remove_bindings(Serial, XName, Bs),
+            ok;
+        false ->
+            ok
+    end.
 
 %%----------------------------------------------------------------------------
 
-serial(Serial, X) ->
-    case with_module(X, fun (M) -> M:serialise_events() end) of
-        true  -> Serial;
-        false -> none
-    end.
-
-with_module(#exchange{arguments = Args}, Fun) ->
-    %% TODO should this be cached? It's on the publish path.
-    {longstr, Type} = rabbit_misc:table_lookup(Args, <<"type">>),
-    {ok, Module} = rabbit_registry:lookup_module(
-                     exchange, list_to_existing_atom(binary_to_list(Type))),
-    Fun(Module).
-
-fail_error(SetName, Reason) ->
-    {Fmt, Args} = error_text(Reason),
-    rabbit_federation_util:fail("upstream-set ~s: " ++ Fmt, [SetName | Args]).
-
-error_text(set_not_found)         -> {"set not found", []};
-error_text(no_connection_name)    -> {"no connection name", []};
-error_text({no_connection, Name}) -> {"connection ~s not found", [Name]};
-error_text({no_host,       Name}) -> {"no host in connection ~s", [Name]}.
+%% TODO this is a bit noddy. OK, a lot noddy.
+federate(#resource{name = <<"fed.", _/binary>>}) -> true;
+federate(_)                                      -> false.
