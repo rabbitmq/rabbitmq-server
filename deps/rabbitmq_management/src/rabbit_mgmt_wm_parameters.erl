3e1ab02515ed42ddc3a7ec56319333f297a9770a
Simon MacMullen <simon@rabbitmq.com>
2012-03-28 15:08:26 +0100

    Make parameters part of the definitions dump.

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_parameters.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_parameters.erl
@@ -17,7 +17,7 @@
 -module(rabbit_mgmt_wm_parameters).
 
 -export([init/1, to_json/2, content_types_provided/2, is_authorized/2,
-         resource_exists/2]).
+         resource_exists/2, basic/1]).
 
 -include("rabbit_mgmt.hrl").
 -include_lib("webmachine/include/webmachine.hrl").
@@ -31,20 +31,20 @@ content_types_provided(ReqData, Context) ->
    {[{"application/json", to_json}], ReqData, Context}.
 
 resource_exists(ReqData, Context) ->
-    {case parameters(ReqData) of
+    {case basic(ReqData) of
          not_found -> false;
          _         -> true
      end, ReqData, Context}.
 
 to_json(ReqData, Context) ->
-    rabbit_mgmt_util:reply_list(parameters(ReqData), ReqData, Context).
+    rabbit_mgmt_util:reply_list(basic(ReqData), ReqData, Context).
 
 is_authorized(ReqData, Context) ->
     rabbit_mgmt_util:is_authorized_admin(ReqData, Context).
 
 %%--------------------------------------------------------------------
 
-parameters(ReqData) ->
+basic(ReqData) ->
     [rabbit_mgmt_format:parameter(P) ||
         P <- case rabbit_mgmt_util:id(application, ReqData) of
                  none -> rabbit_runtime_parameters:list();
