ff46aca6b2e8d12e7995194c7054b7e84ce82d6e
Francesco Mazzoli <francesco@rabbitmq.com>
2012-04-12 11:33:27 +0100

    Added dummy .hrl files so that we don't break files including them.

---
+++ include/rabbit_auth_mechanism_spec.hrl
@@ -0,0 +1,19 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License at
+%% http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%% License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Original Code is RabbitMQ.
+%%
+%% The Initial Developer of the Original Code is VMware, Inc.
+%% Copyright (c) 2007-2012 VMware, Inc.  All rights reserved.
+%%
+
+%% This file exists for compatibility reasons only, since dialyser now
+%% supports behaviours -callback signatures. -includes to this file
+%% should be removed.
