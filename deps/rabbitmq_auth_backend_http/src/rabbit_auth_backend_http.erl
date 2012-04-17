8eddc6861ce79ab2be440f8ad95c0d59f2a919d9
Simon MacMullen <simon@rabbitmq.com>
2012-04-17 11:35:26 +0100

    That hrl has gone away.

--- deps/rabbitmq_auth_backend_http/src/rabbit_auth_backend_http.erl
+++ deps/rabbitmq_auth_backend_http/src/rabbit_auth_backend_http.erl
@@ -18,7 +18,6 @@
 
 -include_lib("rabbit_common/include/rabbit.hrl").
 -behaviour(rabbit_auth_backend).
--include_lib("rabbit_common/include/rabbit_auth_backend_spec.hrl").
 
 -export([description/0, q/2]).
 -export([check_user_login/2, check_vhost_access/2, check_resource_access/3]).
