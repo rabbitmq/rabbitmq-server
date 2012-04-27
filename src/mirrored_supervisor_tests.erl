1376aa6062d68e3a6790bd4d81aad5e8e6b4a35f
Matthias Radestock <matthias@rabbitmq.com>
2012-04-27 13:17:05 +0100

    another attempt at pacifying the CI gods

--- src/mirrored_supervisor_tests.erl
+++ src/mirrored_supervisor_tests.erl
@@ -268,7 +268,7 @@ inc_group() ->
 get_group(Group) ->
     {Group, get(counter)}.
 
-call(Id, Msg) -> call(Id, Msg, 10000, 100).
+call(Id, Msg) -> call(Id, Msg, 60000, 100).
 
 call(Id, Msg, 0, _Decr) ->
     exit({timeout_waiting_for_server, {Id, Msg}, erlang:get_stacktrace()});
