6bc41e62177cdfbef01cab0dc1ec6f5d9c0640f1
Simon MacMullen <simon@rabbitmq.com>
2012-05-18 15:36:04 +0100

    Use put_parameter.

--- deps/rabbitmq_federation_management/priv/www/js/federation.js
+++ deps/rabbitmq_federation_management/priv/www/js/federation.js
@@ -19,22 +19,7 @@ dispatcher_add(function(sammy) {
     sammy.put('#/fed-parameters', function() {
             var num_keys = ['expires', 'message_ttl', 'max_hops',
                             'prefetch_count', 'reconnect_delay'];
-            for (var i in this.params) {
-                if (i === 'length' || !this.params.hasOwnProperty(i)) continue;
-                if (this.params[i] == '') {
-                    delete this.params[i];
-                }
-                else if (num_keys.indexOf(i) != -1) {
-                    this.params[i] = parseInt(this.params[i]);
-                }
-            }
-            this.params = {"component": this.params.component,
-                           "key":       this.params.key,
-                           "value":     this.params};
-            delete this.params.value.component;
-            delete this.params.value.key;
-            if (sync_put(this, '/parameters/:component/:key'))
-                update();
+            put_parameter(this, num_keys);
             return false;
         });
     sammy.del('#/fed-parameters', function() {
