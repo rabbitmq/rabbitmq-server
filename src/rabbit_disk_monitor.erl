8822e5bd2e4121c78b677f7b5b62805be126facf
Emile Joubert <emile@rabbitmq.com>
2012-05-25 14:47:34 +0100

    Parse disk free space in a language-independent manner

--- src/rabbit_disk_monitor.erl
+++ src/rabbit_disk_monitor.erl
@@ -178,8 +178,10 @@ parse_free_unix(CommandResult) ->
 
 parse_free_win32(CommandResult) ->
     LastLine = lists:last(string:tokens(CommandResult, "\r\n")),
-    [_, _Dir, Free, "bytes", "free"] = string:tokens(LastLine, " "),
-    list_to_integer(Free).
+    {ok, Digits} = re:compile("([0-9]+)"),
+    {match,[Free]} =
+      re:run(lists:reverse(LastLine), Digits, [{capture, all_but_first, list}]),
+    list_to_integer(lists:reverse(Free)).
 
 interpret_limit({mem_relative, R}) ->
     round(R * vm_memory_monitor:get_total_memory());
