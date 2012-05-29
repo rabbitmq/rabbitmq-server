2301d4fd174c83dd6fca16307382e749ca2f42a5
Simon MacMullen <simon@rabbitmq.com>
2012-05-29 14:31:00 +0100

    Unbreak the tests. Ahem.

--- deps/rabbitmq_stomp/test/src/rabbit_stomp_test_frame.erl
+++ deps/rabbitmq_stomp/test/src/rabbit_stomp_test_frame.erl
@@ -65,13 +65,13 @@ parse_command_only_test() ->
 parse_resume_mid_command_test() ->
     First = "COMM",
     Second = "AND\n\n\0",
-    {more, Resume, 0} = parse(First),
+    {more, Resume} = parse(First),
     {ok, #stomp_frame{command = "COMMAND"}, _Rest} = parse(Second, Resume).
 
 parse_resume_mid_header_key_test() ->
     First = "COMMAND\nheade",
     Second = "r1:value1\n\n\0",
-    {more, Resume, 0} = parse(First),
+    {more, Resume} = parse(First),
     {ok, Frame = #stomp_frame{command = "COMMAND"}, _Rest} =
         parse(Second, Resume),
     ?assertEqual({ok, "value1"},
@@ -80,7 +80,7 @@ parse_resume_mid_header_key_test() ->
 parse_resume_mid_header_val_test() ->
     First = "COMMAND\nheader1:val",
     Second = "ue1\n\n\0",
-    {more, Resume, 0} = parse(First),
+    {more, Resume} = parse(First),
     {ok, Frame = #stomp_frame{command = "COMMAND"}, _Rest} =
         parse(Second, Resume),
     ?assertEqual({ok, "value1"},
@@ -89,7 +89,7 @@ parse_resume_mid_header_val_test() ->
 parse_resume_mid_body_test() ->
     First = "COMMAND\n\nABC",
     Second = "DEF\0",
-    {more, Resume, 0} = parse(First),
+    {more, Resume} = parse(First),
     {ok, #stomp_frame{command = "COMMAND", body_iolist = Body}, _Rest} =
          parse(Second, Resume),
     ?assertEqual([<<"ABC">>, <<"DEF">>], Body).
