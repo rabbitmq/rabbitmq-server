28137e911f74b17a1edc6d8cfab0ea0da05272c0
Simon MacMullen <simon@rabbitmq.com>
2012-06-12 12:46:50 +0100

    Help.

--- deps/rabbitmq_federation_management/priv/www/js/federation.js
+++ deps/rabbitmq_federation_management/priv/www/js/federation.js
@@ -49,6 +49,30 @@ $("#tabs").append('<li class="administrator-only"><a href="#/federation">Federat
 VHOST_QUERIES["/federation-links"] = "";
 SORT_QUERIES["/federation-links"] = "";
 
+HELP['federation-explicit-identity'] =
+    'Each broker in a federated set of brokers needs a name. If you leave this blank RabbitMQ will generate one. If you set this, ensure that each broker has a unique name. Note that you <b>must</b> set this when using federation combined with clustering.';
+
+HELP['federation-local-username'] =
+    'The name of a local user which can be used to publish messages received over federated links.';
+
+HELP['federation-expires'] =
+    'Time in milliseconds that the upstream should remember about this node for. After this time all upstream state will be removed. Leave this blank to mean "forever".';
+
+HELP['federation-ttl'] =
+    'Time in milliseconds that undelivered messages should be held upstream when there is a network outage or backlog. Leave this blank to mean "forever".';
+
+HELP['federation-max-hops'] =
+    'Maximum number of federation links that messages can traverse before being dropped. Defaults to 1 if not set.';
+
+HELP['federation-prefetch'] =
+    'Maximum number of unacknowledged messages that may be in flight over a federation link at one time. Defaults to 1000 if not set.';
+
+HELP['federation-reconnect'] =
+    'Time in seconds to wait after a network link goes down before attempting reconnection. Defaults to 1 if not set.';
+
+HELP['federation-ha-policy'] =
+    'Federation declares a queue at the upstream node to buffer messages waiting to be sent. Use this to set the x-ha-policy argument for this queue.';
+
 function link_fed_conn(name) {
     return _link_to(fmt_escape_html(name), '#/federation-connection/' + esc(name))
 }
