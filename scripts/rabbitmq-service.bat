89d6705c3d6ce36e43a886c8e39fc073620f52d8
Simon MacMullen <simon@rabbitmq.com>
2012-06-22 18:18:05 +0100

    Backport 7ccc2df0536d (Another fix to bug25005)

--- scripts/rabbitmq-service.bat
+++ scripts/rabbitmq-service.bat
@@ -154,7 +154,10 @@ if "!RABBITMQ_ENABLED_PLUGINS_FILE!"=="" (
     set RABBITMQ_ENABLED_PLUGINS_FILE=!RABBITMQ_BASE!\enabled_plugins
 )
 
-set RABBITMQ_PLUGINS_DIR=!TDP0!..\plugins
+if "!RABBITMQ_PLUGINS_DIR!"=="" (
+    set RABBITMQ_PLUGINS_DIR=!TDP0!..\plugins
+)
+
 set RABBITMQ_EBIN_ROOT=!TDP0!..\ebin
 
 "!ERLANG_HOME!\bin\erl.exe" ^
