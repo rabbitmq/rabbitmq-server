7eae592338a762fde0feef79c97bf80af3a16119
Matthias Radestock <matthias@rabbitmq.com>
2012-07-12 16:55:38 +0100

    grammar

--- src/file_handle_cache.erl
+++ src/file_handle_cache.erl
@@ -374,7 +374,7 @@ sync(Ref) ->
       end).
 
 needs_sync(Ref) ->
-    %% This needs *not* to use with_handles/2; see bug 25052
+    %% This must *not* use with_handles/2; see bug 25052
     case get({Ref, fhc_handle}) of
         #handle { is_dirty = false, write_buffer = [] } -> false;
         _                                               -> true
