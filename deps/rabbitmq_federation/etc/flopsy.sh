e51d20fb8e1a446ece1ca4dbfc24bae25e1a6106
Emile Joubert <emile@rabbitmq.com>
2012-09-06 16:47:36 +0100

    Update federation tests for regex policies

--- deps/rabbitmq_federation/etc/flopsy.sh
+++ deps/rabbitmq_federation/etc/flopsy.sh
@@ -3,4 +3,4 @@ CTL=$1
 
 $CTL set_parameter federation-upstream mopsy '[{<<"uri">>,<<"amqp://localhost:5675">>}]'
 $CTL set_parameter federation-upstream-set ring '[[{<<"upstream">>,<<"mopsy">>},{<<"max-hops">>,2}]]'
-$CTL set_parameter policy ring '[{<<"prefix">>, <<"ring">>}, {<<"policy">>, [{<<"federation-upstream-set">>, <<"ring">>}]}].'
+$CTL set_parameter policy ring '[{<<"pattern">>, <<"^ring$">>}, {<<"priority">>, 1}, {<<"policy">>, [{<<"federation-upstream-set">>, <<"ring">>}]}].'
--- deps/rabbitmq_federation/etc/hare-two-upstreams.sh
+++ deps/rabbitmq_federation/etc/hare-two-upstreams.sh
@@ -3,5 +3,5 @@ CTL=$1
 
 $CTL set_parameter federation-upstream hare '[{<<"uri">>,<<"amqp://localhost:5673">>}]'
 $CTL set_parameter federation-upstream-set upstream '[[{<<"upstream">>,<<"hare">>},{<<"exchange">>,<<"upstream">>}],[{<<"upstream">>,<<"hare">>},{<<"exchange">>,<<"upstream2">>}]]'
-$CTL set_parameter policy fed '[{<<"prefix">>, <<"fed.">>}, {<<"policy">>, [{<<"federation-upstream-set">>, <<"upstream">>}]}]'
+$CTL set_parameter policy fed '[{<<"pattern">>, <<"^fed.">>}, {<<"priority">>, 1}, {<<"policy">>, [{<<"federation-upstream-set">>, <<"upstream">>}]}]'
 
