83075b20d02c2dac742a16755b78a7c3698c89f9
Simon MacMullen <simon@rabbitmq.com>
2012-09-07 16:50:28 +0100

    Unbreak the add policy button.

--- deps/rabbitmq_management/priv/www/js/dispatcher.js
+++ deps/rabbitmq_management/priv/www/js/dispatcher.js
@@ -161,7 +161,7 @@ dispatcher_add(function(sammy) {
                 'policy', '#/policies');
         });
     sammy.put('#/policies', function() {
-            put_parameter(this, ['prefix'], []);
+            put_parameter(this, ['prefix'], [], []);
             return false;
         });
     sammy.del('#/policies', function() {
