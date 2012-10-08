a343eb68af4b46c10c3f6055ca940ae9a3de4d3d
Simon MacMullen <simon@rabbitmq.com>
2012-10-08 12:47:03 +0100

    R12B-3 compatibility.

--- src/rabbit_mirror_queue_misc.erl
+++ src/rabbit_mirror_queue_misc.erl
@@ -280,7 +280,8 @@ suggested_queue_nodes(_, _, {MNode, _}, _) ->
     {MNode, []}.
 
 shuffle(L) ->
-    random:seed(now()),
+    {A1,A2,A3} = now(),
+    random:seed(A1, A2, A3),
     {_, L1} = lists:unzip(lists:keysort(1, [{random:uniform(), N} || N <- L])),
     L1.
 
