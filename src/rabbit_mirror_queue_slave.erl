c0fb4152fe9b9f7451be268f0816ed5ace098c2d
Simon MacMullen <simon@rabbitmq.com>
2012-10-09 16:46:52 +0100

    Unused var.

--- src/rabbit_mirror_queue_slave.erl
+++ src/rabbit_mirror_queue_slave.erl
@@ -173,7 +173,6 @@ handle_call({deliver, Delivery, true}, From, State) ->
 
 handle_call({gm_deaths, Deaths}, From,
             State = #state { q          = #amqqueue { name = QueueName },
-                             gm         = GM,
                              master_pid = MPid }) ->
     %% The GM has told us about deaths, which means we're not going to
     %% receive any more messages from GM
