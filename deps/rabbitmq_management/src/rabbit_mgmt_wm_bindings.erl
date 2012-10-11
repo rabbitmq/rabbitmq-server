da81e56491f775005556daad6c18e7927268d700
Simon MacMullen <simon@rabbitmq.com>
2012-10-11 13:47:14 +0100

    Sort by destination type before destination.
    Actually, also fix the sort order since that predates e2e, amazingly.

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_bindings.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_bindings.erl
@@ -55,7 +55,8 @@ to_json(ReqData, {Mode, Context}) ->
     Bs = [rabbit_mgmt_format:binding(B) || B <- list_bindings(Mode, ReqData)],
     rabbit_mgmt_util:reply_list(
       rabbit_mgmt_util:filter_vhost(Bs, ReqData, Context),
-      ["vhost", "exchange", "queue", "routing_key", "properties_key"],
+      ["vhost", "source", "type", "destination",
+       "routing_key", "properties_key"],
       ReqData, {Mode, Context}).
 
 create_path(ReqData, Context) ->
