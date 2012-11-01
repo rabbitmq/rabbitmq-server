614500b96d0d7dad48e9642950a6acba399a129a
Simon MacMullen <simon@rabbitmq.com>
2012-11-01 14:24:21 +0000

    Fix formatting of hostname-or-IP-address

--- deps/rabbitmq_management/src/rabbit_mgmt_db.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_db.erl
@@ -356,7 +356,7 @@ handle_event(Event = #event{type = queue_deleted}, State) ->
 handle_event(#event{type = connection_created, props = Stats}, State) ->
     handle_created(
       connection_stats, Stats,
-      [{fun rabbit_mgmt_format:addr/1,         [address, peer_address]},
+      [{fun rabbit_mgmt_format:addr/1,         [host, peer_host]},
        {fun rabbit_mgmt_format:port/1,         [port, peer_port]},
        {fun rabbit_mgmt_format:protocol/1,     [protocol]},
        {fun rabbit_mgmt_format:amqp_table/1,   [client_properties]}], State);
