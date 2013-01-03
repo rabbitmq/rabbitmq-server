ea2df79dbe2d66aa73ee39f576f4570d62e33ddc
Matthias Radestock <matthias@rabbitmq.com>
2013-01-03 21:54:35 +0000

    fix bug found by dialyzer

--- src/rabbit_amqqueue_process.erl
+++ src/rabbit_amqqueue_process.erl
@@ -1151,8 +1151,8 @@ handle_call(sync_mirrors, _From,
     S = fun(BQSN) -> State#q{backing_queue_state = BQSN} end,
     case BQ:depth(BQS) - BQ:len(BQS) of
         0 -> case rabbit_mirror_queue_master:sync_mirrors(BQS) of
-                 {shutdown, Reason, BQS1} -> {stop, Reason, S(BQS1)};
-                 {Result, BQS1}           -> reply(Result, S(BQS1))
+                 {ok, BQS1}           -> reply(ok, S(BQS1));
+                 {stop, Reason, BQS1} -> {stop, Reason, S(BQS1)}
              end;
         _ -> reply({error, pending_acks}, State)
     end;
