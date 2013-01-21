3e1e3ebd07d9c0ca5bddfb102f9746196f753415
Steve Powell <steve@rabbitmq.com>
2013-01-21 16:18:12 +0000

    Ignore built artefacts.

--- deps/rabbitmq_jms_topic_exchange/build/deps.mk
+++
@@ -1,2 +0,0 @@
-$(EBIN_DIR)/rabbit_jms_topic_exchange.beam: /Users/spowell/dev/rmq/rabbitmq-umbrella/rabbitmq-public-umbrella/rabbitmq-jms-topic-exchange/src/rabbit_jms_topic_exchange.erl
-$(DEPS_FILE): /Users/spowell/dev/rmq/rabbitmq-umbrella/rabbitmq-public-umbrella/generate_deps
