f225804df51556d509295ec350f8e8713d999332
Emile Joubert <emile@rabbitmq.com>
2013-04-04 17:08:30 +0100

    Transplant reverse of 8c13f168cf00

--- deps/rabbitmq_mqtt/src/rabbit_mqtt_reader.erl
+++ deps/rabbitmq_mqtt/src/rabbit_mqtt_reader.erl
@@ -182,7 +182,7 @@ network_error(Reason,
 stop(Reason, State = #state{ proc_state = PState }) ->
     % todo: maybe clean session
     ok = rabbit_mqtt_collector:unregister(
-           rabbit_mqtt_processor:info(client_info, PState)),
+           rabbit_mqtt_processor:info(client_id, PState)),
     {stop, Reason, close_connection(State)}.
 
 close_connection(State = #state{ proc_state = ProcState} ) ->
