ed3ee2a2ff0375280f58f32164fb7516ab10eed7
Emile Joubert <emile@rabbitmq.com>
2013-04-05 12:42:32 +0100

    client_sup API change

--- deps/rabbitmq_mqtt/src/rabbit_mqtt_sup.erl
+++ deps/rabbitmq_mqtt/src/rabbit_mqtt_sup.erl
@@ -41,9 +41,9 @@ init([{Listeners, SslListeners}]) ->
               end,
     {ok, {{one_for_all, 10, 10},
           [{rabbit_mqtt_client_sup,
-            {rabbit_client_sup, start_link,
+            {rabbit_client_sup, start_link_worker,
              [{local, rabbit_mqtt_client_sup},
-              {rabbit_mqtt_reader, start_link, []}, worker]},
+              {rabbit_mqtt_reader, start_link, []}]},
             transient, infinity, supervisor, [rabbit_client_sup]} |
            listener_specs(fun tcp_listener_spec/1,
                           [SocketOpts], Listeners) ++
