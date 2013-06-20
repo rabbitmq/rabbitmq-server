9fcbb9b80dffb6ea979e16563e1a8ea56232ab45
Simon MacMullen <simon@rabbitmq.com>
2013-06-20 14:48:31 +0100

    Do not "recover" queues if there is already a rabbit_queue record for them. If we are restarting quickly it could be the case that we have a slave alive that has not failed over yet. Don't usurp it.

--- src/rabbit_amqqueue.erl
+++ src/rabbit_amqqueue.erl
@@ -219,8 +219,12 @@ find_durable_queues() ->
     %% TODO: use dirty ops instead
     rabbit_misc:execute_mnesia_transaction(
       fun () ->
-              qlc:e(qlc:q([Q || Q = #amqqueue{pid = Pid}
+              qlc:e(qlc:q([Q || Q = #amqqueue{name = Name,
+                                              pid  = Pid}
                                     <- mnesia:table(rabbit_durable_queue),
+                                #amqqueue{name = Name2}
+                                    <- mnesia:table(rabbit_queue),
+                                Name =:= Name2,
                                 node(Pid) == Node]))
       end).
 
