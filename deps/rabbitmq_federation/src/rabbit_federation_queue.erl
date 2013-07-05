0b96fc27e91e6285ac841cb15d54131755317170
Simon MacMullen <simon@rabbitmq.com>
2013-07-05 14:48:08 +0100

    Start of queue decorators.

--- deps/rabbitmq_federation/src/rabbit_federation_queue.erl
+++ deps/rabbitmq_federation/src/rabbit_federation_queue.erl
@@ -16,25 +16,23 @@
 
 -module(rabbit_federation_queue).
 
+-rabbit_boot_step({?MODULE,
+                   [{description, "federation queue decorator"},
+                    {mfa, {rabbit_registry, register,
+                           [queue_decorator, <<"federation">>, ?MODULE]}},
+                    {requires, rabbit_registry},
+                    {enables, recovery}]}).
+
 -include_lib("amqp_client/include/amqp_client.hrl").
 -include("rabbit_federation.hrl").
 
--export([maybe_start/1, maybe_stop/1, policy_changed/2]).
+-behaviour(rabbit_queue_decorator).
+
+-export([maybe_start/1, maybe_stop/1, policy_changed/2, active_for/1]).
 -export([policy_changed_local/2]).
 -export([run/1, pause/1, basic_get/1]).
 
-maybe_start(Q) ->
-    case federate(Q) of
-        true  -> rabbit_federation_queue_link_sup_sup:start_child(Q);
-        false -> ok
-    end.
-
-maybe_stop(Q = #amqqueue{name = QName}) ->
-    case federate(Q) of
-        true  -> rabbit_federation_queue_link_sup_sup:stop_child(Q),
-                 rabbit_federation_status:remove_exchange_or_queue(QName);
-        false -> ok
-    end.
+%%----------------------------------------------------------------------------
 
 policy_changed(Q1 = #amqqueue{name = QName}, Q2) ->
     case rabbit_amqqueue:lookup(QName) of
@@ -49,12 +47,27 @@ policy_changed_local(Q1, Q2) ->
     maybe_stop(Q1),
     maybe_start(Q2).
 
-run(#amqqueue{name = QName})       -> rabbit_federation_queue_link:run(QName).
-pause(#amqqueue{name = QName})     -> rabbit_federation_queue_link:pause(QName).
-basic_get(#amqqueue{name = QName}) -> rabbit_federation_queue_link:basic_get(QName).
-
-federate(Q) ->
+active_for(Q) ->
     case rabbit_federation_upstream:set_for(Q) of
         {ok, _}    -> true;
         {error, _} -> false
     end.
+
+%%----------------------------------------------------------------------------
+
+maybe_start(Q) ->
+    case active_for(Q) of
+        true  -> rabbit_federation_queue_link_sup_sup:start_child(Q);
+        false -> ok
+    end.
+
+maybe_stop(Q = #amqqueue{name = QName}) ->
+    case active_for(Q) of
+        true  -> rabbit_federation_queue_link_sup_sup:stop_child(Q),
+                 rabbit_federation_status:remove_exchange_or_queue(QName);
+        false -> ok
+    end.
+
+run(#amqqueue{name = QName})       -> rabbit_federation_queue_link:run(QName).
+pause(#amqqueue{name = QName})     -> rabbit_federation_queue_link:pause(QName).
+basic_get(#amqqueue{name = QName}) -> rabbit_federation_queue_link:basic_get(QName).
