d1728e6f904b416a577ff10127a96694011f00cb
Simon MacMullen <simon@rabbitmq.com>
2013-07-31 14:56:02 +0100

    Minimise distance from stable

--- deps/rabbitmq_stomp/src/rabbit_stomp_processor.erl
+++ deps/rabbitmq_stomp/src/rabbit_stomp_processor.erl
@@ -349,7 +349,6 @@ ack_action(Command, Frame,
 %%----------------------------------------------------------------------------
 %% Internal helpers for processing frames callbacks
 %%----------------------------------------------------------------------------
-
 server_cancel_consumer(ConsumerTag, State = #state{subscriptions = Subs}) ->
     case dict:find(ConsumerTag, Subs) of
         error ->
