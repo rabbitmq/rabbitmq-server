e0f5282d1b9d4bc561b3ef7a024bb1b4992ef26f
Alvaro Videla <alvaro@rabbitmq.com>
2014-01-29 16:41:04 +0100

    cleans up rabbit_sharding_amqp_util module

--- deps/rabbitmq_sharding/src/rabbit_sharding_amqp_util.erl
+++ deps/rabbitmq_sharding/src/rabbit_sharding_amqp_util.erl
@@ -2,58 +2,9 @@
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
--export([disposable_channel_call/2, disposable_channel_call/3,
-         disposable_connection_call/3, disposable_connection_calls/3,
-         ensure_connection_closed/1]).
+-export([disposable_connection_calls/3]).
 
 -define(MAX_CONNECTION_CLOSE_TIMEOUT, 10000).
-
-%%----------------------------------------------------------------------------
-
-open(X) ->
-    case amqp_connection:start(local_params(X)) of
-        {ok, Conn} -> case amqp_connection:open_channel(Conn) of
-                          {ok, Ch} -> {ok, Conn, Ch};
-                          E        -> catch amqp_connection:close(Conn),
-                                      E
-                      end;
-        E -> E
-    end.
-
-ensure_channel_closed(Ch) -> catch amqp_channel:close(Ch).
-
-ensure_connection_closed(Conn) ->
-    catch amqp_connection:close(Conn, ?MAX_CONNECTION_CLOSE_TIMEOUT).
-
-%%----------------------------------------------------------------------------
-
-disposable_channel_call(Conn, Method) ->
-    disposable_channel_call(Conn, Method, fun(_, _) -> ok end).
-
-disposable_channel_call(Conn, Method, ErrFun) ->
-    {ok, Ch} = amqp_connection:open_channel(Conn),
-    try
-        amqp_channel:call(Ch, Method)
-    catch exit:{{shutdown, {server_initiated_close, Code, Text}}, _} ->
-            ErrFun(Code, Text)
-    after
-        ensure_channel_closed(Ch)
-    end.
-
-disposable_connection_call(X, Method, ErrFun) ->
-    case open(X) of
-        {ok, Conn, Ch} ->
-            try
-                amqp_channel:call(Ch, Method)
-            catch exit:{{shutdown, {connection_closing,
-                                    {server_initiated_close, Code, Txt}}}, _} ->
-                    ErrFun(Code, Txt)
-            after
-                ensure_connection_closed(Conn)
-            end;
-        E ->
-            E
-    end.
 
 disposable_connection_calls(X, Methods, ErrFun) ->
     case open(X) of
@@ -70,7 +21,20 @@ disposable_connection_calls(X, Methods, ErrFun) ->
             E
     end.
 
+ensure_connection_closed(Conn) ->
+    catch amqp_connection:close(Conn, ?MAX_CONNECTION_CLOSE_TIMEOUT).
+
 %%----------------------------------------------------------------------------
+
+open(X) ->
+    case amqp_connection:start(local_params(X)) of
+        {ok, Conn} -> case amqp_connection:open_channel(Conn) of
+                          {ok, Ch} -> {ok, Conn, Ch};
+                          E        -> catch amqp_connection:close(Conn),
+                                      E
+                      end;
+        E -> E
+    end.
 
 local_params(#exchange{name = #resource{virtual_host = VHost}} = X) ->
     Username = rabbit_sharding_util:username(X),
