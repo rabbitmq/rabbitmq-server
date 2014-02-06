5a69daa6f29858bf046af426472a287ef570d67a
Matthias Radestock <matthias@rabbitmq.com>
2014-02-06 20:04:41 +0000

    don't use amqqueue:with/3 in mq_misc:{add,drop}_mirror since we don't actually interact with the master QPid here.
    Also contain errors from the mq_slave:go invocation.

--- src/rabbit_mirror_queue_misc.erl
+++ src/rabbit_mirror_queue_misc.erl
@@ -148,53 +148,54 @@ drop_mirrors(QName, Nodes) ->
     ok.
 
 drop_mirror(QName, MirrorNode) ->
-    rabbit_amqqueue:with(
-      QName,
-      fun (#amqqueue { name = Name, pid = QPid, slave_pids = SPids }) ->
-              case [Pid || Pid <- [QPid | SPids], node(Pid) =:= MirrorNode] of
-                  [] ->
-                      {error, {queue_not_mirrored_on_node, MirrorNode}};
-                  [QPid] when SPids =:= [] ->
-                      {error, cannot_drop_only_mirror};
-                  [Pid] ->
-                      rabbit_log:info(
-                        "Dropping queue mirror on node ~p for ~s~n",
-                        [MirrorNode, rabbit_misc:rs(Name)]),
-                      exit(Pid, {shutdown, dropped}),
-                      {ok, dropped}
-              end
-      end).
+    case rabbit_amqqueue:lookup(QName) of
+        {ok, #amqqueue { name = Name, pid = QPid, slave_pids = SPids }} ->
+            case [Pid || Pid <- [QPid | SPids], node(Pid) =:= MirrorNode] of
+                [] ->
+                    {error, {queue_not_mirrored_on_node, MirrorNode}};
+                [QPid] when SPids =:= [] ->
+                    {error, cannot_drop_only_mirror};
+                [Pid] ->
+                    rabbit_log:info(
+                      "Dropping queue mirror on node ~p for ~s~n",
+                      [MirrorNode, rabbit_misc:rs(Name)]),
+                    exit(Pid, {shutdown, dropped}),
+                    {ok, dropped}
+            end;
+        {error, not_found} = E ->
+            E
+    end.
 
 add_mirrors(QName, Nodes, SyncMode) ->
     [add_mirror(QName, Node, SyncMode)  || Node <- Nodes],
     ok.
 
 add_mirror(QName, MirrorNode, SyncMode) ->
-    rabbit_amqqueue:with(
-      QName,
-      fun (#amqqueue { name = Name, pid = QPid, slave_pids = SPids } = Q) ->
-              case [Pid || Pid <- [QPid | SPids], node(Pid) =:= MirrorNode] of
-                  [] ->
-                      start_child(Name, MirrorNode, Q, SyncMode);
-                  [SPid] ->
-                      case rabbit_misc:is_process_alive(SPid) of
-                          true  -> {ok, already_mirrored};
-                          false -> start_child(Name, MirrorNode, Q, SyncMode)
-                      end
-              end
-      end).
+    case rabbit_amqqueue:lookup(QName) of
+        {ok, #amqqueue { name = Name, pid = QPid, slave_pids = SPids } = Q} ->
+            case [Pid || Pid <- [QPid | SPids], node(Pid) =:= MirrorNode] of
+                [] ->
+                    start_child(Name, MirrorNode, Q, SyncMode);
+                [SPid] ->
+                    case rabbit_misc:is_process_alive(SPid) of
+                        true  -> {ok, already_mirrored};
+                        false -> start_child(Name, MirrorNode, Q, SyncMode)
+                    end
+            end;
+        {error, not_found} = E ->
+            E
+    end.
 
 start_child(Name, MirrorNode, Q, SyncMode) ->
-    case rabbit_misc:with_exit_handler(
-           rabbit_misc:const(down),
-           fun () ->
-                   rabbit_mirror_queue_slave_sup:start_child(MirrorNode, [Q])
-           end) of
-        {ok, SPid} -> rabbit_log:info("Adding mirror of ~s on node ~p: ~p~n",
-                                      [rabbit_misc:rs(Name), MirrorNode, SPid]),
-                      rabbit_mirror_queue_slave:go(SPid, SyncMode);
-        _          -> ok
-    end.
+    rabbit_misc:with_exit_handler(
+      rabbit_misc:const(ok),
+      fun () ->
+              {ok, SPid} = rabbit_mirror_queue_slave_sup:start_child(
+                             MirrorNode, [Q]),
+              rabbit_log:info("Adding mirror of ~s on node ~p: ~p~n",
+                              [rabbit_misc:rs(Name), MirrorNode, SPid]),
+              rabbit_mirror_queue_slave:go(SPid, SyncMode)
+      end).
 
 report_deaths(_MirrorPid, _IsMaster, _QueueName, []) ->
     ok;
