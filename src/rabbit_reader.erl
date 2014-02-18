66db57e4014b70b2d36c7789f5322f41ed37a100
Matthias Radestock <matthias@rabbitmq.com>
2014-02-18 13:28:24 +0000

    clearer I think

--- src/rabbit_reader.erl
+++ src/rabbit_reader.erl
@@ -1076,11 +1076,13 @@ i(state, #v1{connection_state = ConnectionState,
              throttle         = #throttle{alarmed_by      = Alarms,
                                           last_blocked_by = WasBlockedBy,
                                           last_blocked_at = T}}) ->
-    Recently = T =/= never andalso timer:now_diff(erlang:now(), T) < 5000000,
-    case {credit_flow:blocked(), WasBlockedBy, Alarms, Recently} of
-        {true, _,    [], _}    -> flow;
-        {_,    flow, [], true} -> flow;
-        {_,    _,    _,  _}    -> ConnectionState
+    case Alarms =:= [] andalso %% not blocked by resource alarms
+        (credit_flow:blocked() %% blocked by flow now
+         orelse                %% blocked by flow recently
+           (WasBlockedBy =:= flow andalso T =/= never andalso
+            timer:now_diff(erlang:now(), T) < 5000000)) of
+        true  -> flow;
+        false -> ConnectionState
     end;
 i(Item,               #v1{connection = Conn}) -> ic(Item, Conn).
 
