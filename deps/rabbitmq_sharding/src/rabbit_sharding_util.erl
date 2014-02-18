44c7769c8dc9ad5963442242e4afbf11db58255f
Alvaro Videla <alvaro@rabbitmq.com>
2014-02-18 14:32:18 +0100

    cosmetics for shard/1 function

--- deps/rabbitmq_sharding/src/rabbit_sharding_util.erl
+++ deps/rabbitmq_sharding/src/rabbit_sharding_util.erl
@@ -10,14 +10,9 @@
 -import(rabbit_misc, [pget/3]).
 
 %% only shard CH or random exchanges.
-shard(X = #exchange{type = 'x-random'}) ->
-    shard0(X);
-
-shard(X = #exchange{type = 'x-consistent-hash'}) ->
-    shard0(X);
-
-shard(_X) ->
-    false.
+shard(X = #exchange{type = 'x-consistent-hash'}) -> shard0(X);
+shard(X = #exchange{type = 'x-random'})          -> shard0(X);
+shard(_X)                                        -> false.
 
 shard0(X) ->
     case get_policy(X) of
@@ -64,7 +59,7 @@ get_parameter_value(Comp, Param, X, Default) ->
         undefined -> Default;
         Name      ->
             case rabbit_runtime_parameters:value(
-                   vhost(X), Comp, Name) of
+                   vhos2t(X), Comp, Name) of
                 not_found -> Default;
                 Value     -> pget(Param, Value, Default)
             end
