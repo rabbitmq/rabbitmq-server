65250a169a2cd79e0499f1ddca378f4d44b940cb
Alvaro Videla <alvaro@rabbitmq.com>
2014-02-18 14:34:28 +0100

    moves get_parameter to the private function section

--- deps/rabbitmq_sharding/src/rabbit_sharding_util.erl
+++ deps/rabbitmq_sharding/src/rabbit_sharding_util.erl
@@ -42,17 +42,17 @@ routing_key(X) ->
 get_policy(X) ->
     rabbit_policy:get(<<"sharding-definition">>, X).
 
-get_parameter(Parameter, X, Default) ->
-    Default2 = rabbit_runtime_parameters:value(
-                 vhost(X), <<"sharding">>, Parameter, Default),
-    get_parameter_value(<<"sharding-definition">>, Parameter,
-                        X, Default2).
-
 exchange_bin(#resource{name = XBin}) -> XBin.
 
 a2b(A) -> list_to_binary(atom_to_list(A)).
 
 %%----------------------------------------------------------------------------
+
+get_parameter(Parameter, X, Default) ->
+    Default2 = rabbit_runtime_parameters:value(
+                 vhost(X), <<"sharding">>, Parameter, Default),
+    get_parameter_value(<<"sharding-definition">>, Parameter,
+                        X, Default2).
 
 get_parameter_value(Comp, Param, X, Default) ->
     case get_policy(X) of
