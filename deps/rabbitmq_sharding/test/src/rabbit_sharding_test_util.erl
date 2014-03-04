8d205b0759452dad31097998f646d3dbad3d3823
Alvaro Videla <alvaro@rabbitmq.com>
2014-03-04 19:14:32 +0100

    removes unused functions

--- deps/rabbitmq_sharding/test/src/rabbit_sharding_test_util.erl
+++ deps/rabbitmq_sharding/test/src/rabbit_sharding_test_util.erl
@@ -22,35 +22,6 @@
 
 -compile(export_all).
 
--import(rabbit_misc, [pget/2]).
-
-expect(Ch, Q, Fun) when is_function(Fun) ->
-    amqp_channel:subscribe(Ch, #'basic.consume'{queue  = Q,
-                                                no_ack = true}, self()),
-    receive
-        #'basic.consume_ok'{consumer_tag = CTag} -> ok
-    end,
-    Fun(),
-    amqp_channel:call(Ch, #'basic.cancel'{consumer_tag = CTag});
-
-expect(Ch, Q, Payloads) ->
-    expect(Ch, Q, fun() -> expect(Payloads) end).
-
-expect([]) ->
-    ok;
-expect(Payloads) ->
-    receive
-        {#'basic.deliver'{}, #amqp_msg{payload = Payload}} ->
-            case lists:member(Payload, Payloads) of
-                true  -> expect(Payloads -- [Payload]);
-                false -> throw({expected, Payloads, actual, Payload})
-            end
-    end.
-
-expect_empty(Ch, Q) ->
-    ?assertMatch(#'basic.get_empty'{},
-                 amqp_channel:call(Ch, #'basic.get'{ queue = Q })).
-
 set_param(Component, Name, Value) ->
     rabbitmqctl(fmt("set_parameter ~s ~s '~s'", [Component, Name, Value])).
 
@@ -116,26 +87,6 @@ plugin_dir() ->
     {ok, [[File]]} = init:get_argument(config),
     filename:dirname(filename:dirname(File)).
 
-%%----------------------------------------------------------------------------
-
-% assert_status(XorQs, Names) ->
-%     Links = lists:append([links(XorQ) || XorQ <- XorQs]),
-%     Remaining = lists:foldl(fun (Link, Status) ->
-%                                     assert_link_status(Link, Status, Names)
-%                             end, rabbit_federation_status:status(), Links),
-%     ?assertEqual([], Remaining),
-%     ok.
-
-assert_link_status({DXorQNameBin, UpstreamName, UXorQNameBin}, Status,
-                   {TypeName, UpstreamTypeName}) ->
-    {This, Rest} = lists:partition(
-                     fun(St) ->
-                             pget(upstream, St) =:= UpstreamName andalso
-                                 pget(TypeName, St) =:= DXorQNameBin andalso
-                                 pget(UpstreamTypeName, St) =:= UXorQNameBin
-                     end, Status),
-    ?assertMatch([_], This),
-    Rest.
 
 xr(Name) -> rabbit_misc:r(<<"/">>, exchange, Name).
 qr(Name) -> rabbit_misc:r(<<"/">>, queue, Name).
