55c2a86fd2eda42e307e1ac741ee8bb94f92707e
Simon MacMullen <simon@rabbitmq.com>
2014-03-10 12:17:14 +0000

    Be a tiny bit more correct

--- src/rabbit_vm.erl
+++ src/rabbit_vm.erl
@@ -36,7 +36,7 @@ memory() ->
     ConnProcs     = [rabbit_tcp_client_sup, ssl_connection_sup, amqp_sup],
     QProcs        = [rabbit_amqqueue_sup, rabbit_mirror_queue_slave_sup],
     MsgIndexProcs = [msg_store_transient, msg_store_persistent],
-    MgmtDbProcs   = [rabbit_mgmt_sup],
+    MgmtDbProcs   = [rabbit_mgmt_sup_sup],
     PluginProcs   = plugin_sups(),
 
     All = [ConnProcs, QProcs, MsgIndexProcs, MgmtDbProcs, PluginProcs],
