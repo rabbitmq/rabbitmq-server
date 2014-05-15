fd9d3ce5ad5311fa446a267065de939939f0457e
Simon MacMullen <simon@rabbitmq.com>
2014-05-15 11:47:08 +0100

    Add needed dependency so we can test from clean.

--- deps/rabbitmq_federation/package.mk
+++ deps/rabbitmq_federation/package.mk
@@ -6,7 +6,7 @@ WITH_BROKER_TEST_COMMANDS:=rabbit_test_runner:run_in_broker(\"$(PACKAGE_DIR)/tes
 WITH_BROKER_SETUP_SCRIPTS:=$(PACKAGE_DIR)/etc/setup-rabbit-test.sh
 STANDALONE_TEST_COMMANDS:=rabbit_test_runner:run_multi(\"$(PACKAGE_DIR)/test/ebin\",\"$(FILTER)\",$(COVER),\"/tmp/rabbitmq-multi-node/plugins\")
 
-$(PACKAGE_DIR)+pre-test::
+$(PACKAGE_DIR)+pre-test:: $(PACKAGE_DIR)/dist/.done
 	rm -rf /tmp/rabbitmq-multi-node/plugins
 	mkdir -p /tmp/rabbitmq-multi-node/plugins/plugins
 	cp -p dist/*.ez /tmp/rabbitmq-multi-node/plugins/plugins
