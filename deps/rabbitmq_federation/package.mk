a231fa83dee8963142c4100cdfd041a075046336
Simon MacMullen <simon@rabbitmq.com>
2014-05-15 12:19:10 +0100

    API change, slight cleanup.

--- deps/rabbitmq_federation/package.mk
+++ deps/rabbitmq_federation/package.mk
@@ -4,9 +4,9 @@ FILTER:=all
 COVER:=false
 WITH_BROKER_TEST_COMMANDS:=rabbit_test_runner:run_in_broker(\"$(PACKAGE_DIR)/test/ebin\",\"$(FILTER)\")
 WITH_BROKER_SETUP_SCRIPTS:=$(PACKAGE_DIR)/etc/setup-rabbit-test.sh
-STANDALONE_TEST_COMMANDS:=rabbit_test_runner:run_multi(\"$(PACKAGE_DIR)/test/ebin\",\"$(FILTER)\",$(COVER),\"/tmp/rabbitmq-multi-node/plugins\")
+STANDALONE_TEST_COMMANDS:=rabbit_test_runner:run_multi(\"$(UMBRELLA_BASE_DIR)/rabbitmq-server\",\"$(PACKAGE_DIR)/test/ebin\",\"$(FILTER)\",$(COVER),\"/tmp/rabbitmq-multi-node/plugins\")
 
-$(PACKAGE_DIR)+pre-test:: $(PACKAGE_DIR)/dist/.done
+$(PACKAGE_DIR)+pre-test:: $(PACKAGE_DIR)+dist
 	rm -rf /tmp/rabbitmq-multi-node/plugins
 	mkdir -p /tmp/rabbitmq-multi-node/plugins/plugins
 	cp -p dist/*.ez /tmp/rabbitmq-multi-node/plugins/plugins
