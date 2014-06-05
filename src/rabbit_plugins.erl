7ee15f72a6a617f78c524169b99e73aed3fbc5ec
Simon MacMullen <simon@rabbitmq.com>
2014-06-05 16:31:57 +0100

    Remove sync_notify, it doesn't seem to be needed.

--- src/rabbit_plugins.erl
+++ src/rabbit_plugins.erl
@@ -47,7 +47,7 @@ disable(Plugins) ->
     RunningApps = rabbit_misc:which_applications(),
     ToDisable = [P || P <- Plugins,
                       proplists:is_defined(P, RunningApps)],
-    ok = rabbit_event:sync_notify(plugins_changed, [{disabled, ToDisable}]),
+    ok = rabbit_event:notify(plugins_changed, [{disabled, ToDisable}]),
     rabbit:stop_apps(ToDisable).
 
 %% @doc Prepares the file system and installs all enabled plugins.
