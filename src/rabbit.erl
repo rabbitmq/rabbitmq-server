cf60977badf3d8d2feae9d8c127782389d82d0ce
Michael Klishin <michael@rabbitmq.com>
2014-06-19 10:16:52 +0400

    Add SMP support to the info banner

--- src/rabbit.erl
+++ src/rabbit.erl
@@ -803,6 +803,7 @@ log_banner() ->
                 {"sasl log",       log_location(sasl)},
                 {"database dir",   rabbit_mnesia:dir()},
                 {"kernel polling", erlang:system_info(kernel_poll)},
+                {"SMP support",    erlang:system_info(smp_support)},
                 {"schedulers",     integer_to_list(erlang:system_info(schedulers))},
                 {"I/O thread pool size", integer_to_list(erlang:system_info(thread_pool_size))}],
     DescrLen = 1 + lists:max([length(K) || {K, _V} <- Settings]),
