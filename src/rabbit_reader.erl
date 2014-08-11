26fab415b046e26abed1316e3b09e3ea04a087ad
Simon MacMullen <simon@rabbitmq.com>
2014-08-11 12:15:28 +0100

    ...and inline that again.

--- src/rabbit_reader.erl
+++ src/rabbit_reader.erl
@@ -597,9 +597,6 @@ handle_exception(State = #v1{connection = #connection{protocol = Protocol},
     ok = send_on_channel0(State1#v1.sock, CloseMethod, Protocol),
     State1;
 handle_exception(State, Channel, Reason) ->
-    fail_handshake_after_delay(State, Channel, Reason).
-
-fail_handshake_after_delay(State, Channel, Reason) ->
     %% We don't trust the client at this point - force them to wait
     %% for a bit so they can't DOS us with repeated failed logins etc.
     timer:sleep(?SILENT_CLOSE_DELAY * 1000),
