f5da98ae1e3604e5fefd03359bca8d24e9fabaed
Simon MacMullen <simon@rabbitmq.com>
2014-10-06 16:53:04 +0100

    Prevent the channel from holding a lot of binary garbage when accepting huge messages.

--- src/rabbit_channel.erl
+++ src/rabbit_channel.erl
@@ -607,7 +607,7 @@ check_internal_exchange(_) ->
     ok.
 
 check_msg_size(Content) ->
-    Size = rabbit_basic:msg_size(Content),
+    Size = rabbit_basic:msg_size_and_gc(Content),
     case Size > ?MAX_MSG_SIZE of
         true  -> precondition_failed("message size ~B larger than max size ~B",
                                      [Size, ?MAX_MSG_SIZE]);
