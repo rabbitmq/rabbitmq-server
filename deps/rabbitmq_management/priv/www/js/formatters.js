1cd0fa7eb03c88799317ea043fcd37e3fbfc8b81
Simon MacMullen <simon@rabbitmq.com>
2014-10-06 12:12:00 +0100

    Rejig the memory use templates to show connection process subtypes. It therefore seems sensible not to try to invent more colours but to put things together into groups.

--- deps/rabbitmq_management/priv/www/js/formatters.js
+++ deps/rabbitmq_management/priv/www/js/formatters.js
@@ -34,11 +34,6 @@ function fmt_si_prefix(num0, max0, thousand, allow_fractions) {
             num.toFixed(0)) + powers[power];
 }
 
-function fmt_memory(memory, key) {
-    return '<div class="colour-key memory_' + key + '"></div>' +
-        fmt_bytes(memory[key]);
-}
-
 function fmt_boolean(b) {
     if (b == undefined) return UNKNOWN_REPR;
 
