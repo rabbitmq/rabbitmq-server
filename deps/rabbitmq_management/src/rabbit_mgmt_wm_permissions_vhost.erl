f5d0d494b9a1c190182ca68b55408901e4d5b825
Simon MacMullen <simon@rabbitmq.com>
2014-10-29 11:45:24 +0000

    Fix 500s listing permissions for nonexistent users or vhosts.

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_permissions_vhost.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_permissions_vhost.erl
@@ -16,7 +16,8 @@
 
 -module(rabbit_mgmt_wm_permissions_vhost).
 
--export([init/1, to_json/2, content_types_provided/2, is_authorized/2]).
+-export([init/1, to_json/2, content_types_provided/2, resource_exists/2,
+         is_authorized/2]).
 
 -include("rabbit_mgmt.hrl").
 -include_lib("webmachine/include/webmachine.hrl").
@@ -28,6 +29,9 @@ init(_Config) -> {ok, #context{}}.
 
 content_types_provided(ReqData, Context) ->
    {[{"application/json", to_json}], ReqData, Context}.
+
+resource_exists(ReqData, Context) ->
+    {rabbit_vhost:exists(rabbit_mgmt_wm_vhost:id(ReqData)), ReqData, Context}.
 
 to_json(ReqData, Context) ->
     VHost = rabbit_mgmt_util:id(vhost, ReqData),
