9f2fc45ee211c213f8d530b7b951e49eb9342083
Simon MacMullen <simon@rabbitmq.com>
2014-11-04 15:04:14 +0000

    Cope if the shovel is not there.

--- deps/rabbitmq_shovel_management/src/rabbit_shovel_mgmt.erl
+++ deps/rabbitmq_shovel_management/src/rabbit_shovel_mgmt.erl
@@ -117,8 +117,14 @@ lookup_src_dest(static, _Name) ->
     [];
 
 lookup_src_dest(dynamic, {VHost, Name}) ->
-    Def = pget(value,
-               rabbit_runtime_parameters:lookup(VHost, <<"shovel">>, Name)),
-    Ks = [<<"src-queue">>,  <<"src-exchange">>,  <<"src-exchange-key">>,
-          <<"dest-queue">>, <<"dest-exchange">>, <<"dest-exchange-key">>],
-    [{definition, [{K, V} || {K, V} <- Def, lists:member(K, Ks)]}].
+    case rabbit_runtime_parameters:lookup(VHost, <<"shovel">>, Name) of
+        %% We might not find anything if the shovel has been deleted
+        %% before we got here
+        not_found ->
+            [];
+        Props ->
+            Def = pget(value, Props),
+            Ks = [<<"src-queue">>, <<"src-exchange">>, <<"src-exchange-key">>,
+                  <<"dest-queue">>,<<"dest-exchange">>,<<"dest-exchange-key">>],
+            [{definition, [{K, V} || {K, V} <- Def, lists:member(K, Ks)]}]
+    end.
