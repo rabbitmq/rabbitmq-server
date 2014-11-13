59fa43fb49c9a91fd2385a3879c2171a0a0dae38
Simon MacMullen <simon@rabbitmq.com>
2014-11-13 16:06:48 +0000

    Warn on no LDAP server.

--- deps/rabbitmq_auth_backend_ldap/package.mk
+++ deps/rabbitmq_auth_backend_ldap/package.mk
@@ -4,4 +4,6 @@ DEPS:=rabbitmq-server rabbitmq-erlang-client eldap-wrapper
 ifeq ($(shell nc -z localhost 389 && echo true),true)
 WITH_BROKER_TEST_COMMANDS:=eunit:test(rabbit_auth_backend_ldap_test,[verbose])
 WITH_BROKER_TEST_CONFIG:=$(PACKAGE_DIR)/etc/rabbit-test
+else
+$(warning Not running LDAP tests; no LDAP server found on localhost)
 endif
