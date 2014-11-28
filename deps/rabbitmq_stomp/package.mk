337b1512757c139b24e4a40575fa69dcc9411491
Simon MacMullen <simon@rabbitmq.com>
2014-11-28 09:46:40 +0000

    Rename this to avoid conflict with MQTT. Something of a hack, but it seems to work.

--- deps/rabbitmq_stomp/package.mk
+++ deps/rabbitmq_stomp/package.mk
@@ -10,7 +10,7 @@ ABS_PACKAGE_DIR:=$(abspath $(PACKAGE_DIR))
 CERTS_DIR:=$(ABS_PACKAGE_DIR)/test/certs
 CAN_RUN_SSL:=$(shell if [ -d $(RABBITMQ_TEST_PATH) ]; then echo "true"; else echo "false"; fi)
 
-TEST_CONFIG_PATH=$(TEST_EBIN_DIR)/test.config
+TEST_CONFIG_PATH_STOMP=$(TEST_EBIN_DIR)/test.config
 WITH_BROKER_TEST_CONFIG:=$(TEST_EBIN_DIR)/test
 
 .PHONY: $(TEST_CONFIG_PATH)
@@ -19,7 +19,7 @@ ifeq ($(CAN_RUN_SSL),true)
 
 WITH_BROKER_TEST_SCRIPTS += $(PACKAGE_DIR)/test/src/test_ssl.py
 
-$(TEST_CONFIG_PATH): $(CERTS_DIR) $(ABS_PACKAGE_DIR)/test/src/ssl.config
+$(TEST_CONFIG_PATH_STOMP): $(CERTS_DIR) $(ABS_PACKAGE_DIR)/test/src/ssl.config
 	sed -e "s|%%CERTS_DIR%%|$(CERTS_DIR)|g" < $(ABS_PACKAGE_DIR)/test/src/ssl.config > $@
 	@echo "\nRunning SSL tests\n"
 
@@ -28,7 +28,7 @@ $(CERTS_DIR):
 	make -C $(RABBITMQ_TEST_PATH)/certs all PASSWORD=test DIR=$(CERTS_DIR)
 
 else
-$(TEST_CONFIG_PATH): $(ABS_PACKAGE_DIR)/test/src/non_ssl.config
+$(TEST_CONFIG_PATH_STOMP): $(ABS_PACKAGE_DIR)/test/src/non_ssl.config
 	cp $(ABS_PACKAGE_DIR)/test/src/non_ssl.config $@
 	@echo "\nNOT running SSL tests - looked in $(RABBITMQ_TEST_PATH) \n"
 
