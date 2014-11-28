746495fc43b87e2243debb2f7be888fd1ca1489d
Simon MacMullen <simon@rabbitmq.com>
2014-11-28 09:46:53 +0000

    Rename this to avoid conflict with STOMP. Something of a hack, but it seems to work.

--- deps/rabbitmq_mqtt/package.mk
+++ deps/rabbitmq_mqtt/package.mk
@@ -7,7 +7,7 @@ ABS_PACKAGE_DIR:=$(abspath $(PACKAGE_DIR))
 CERTS_DIR:=$(ABS_PACKAGE_DIR)/test/certs
 CAN_RUN_SSL:=$(shell if [ -d $(RABBITMQ_TEST_PATH) ]; then echo "true"; else echo "false"; fi)
 
-TEST_CONFIG_PATH=$(TEST_EBIN_DIR)/test.config
+TEST_CONFIG_PATH_MQTT=$(TEST_EBIN_DIR)/test.config
 WITH_BROKER_TEST_CONFIG:=$(TEST_EBIN_DIR)/test
 WITH_BROKER_TEST_SCRIPTS:=$(PACKAGE_DIR)/test/test.sh
 
@@ -18,9 +18,8 @@ ifeq ($(CAN_RUN_SSL),true)
 
 WITH_BROKER_SETUP_SCRIPTS:=$(ABS_PACKAGE_DIR)/test/setup-rabbit-test.sh
 
-$(TEST_CONFIG_PATH): $(CERTS_DIR) $(ABS_PACKAGE_DIR)/test/src/ssl.config
-	sed -e "s|%%CERTS_DIR%%|$(CERTS_DIR)|g" < $(ABS_PACKAGE_DIR)/test/src/ssl.config > $(ABS_PACKAGE_DIR)/$@
-
+$(TEST_CONFIG_PATH_MQTT): $(CERTS_DIR) $(ABS_PACKAGE_DIR)/test/src/ssl.config
+	sed -e "s|%%CERTS_DIR%%|$(CERTS_DIR)|g" < $(ABS_PACKAGE_DIR)/test/src/ssl.config > $@
 	@echo "\nRunning SSL tests\n"
 
 $(CERTS_DIR):
@@ -29,7 +28,7 @@ $(CERTS_DIR):
 	make -C $(RABBITMQ_TEST_PATH)/certs all PASSWORD=bunnychow DIR=$(CERTS_DIR)
 
 else
-$(TEST_CONFIG_PATH): $(ABS_PACKAGE_DIR)/test/src/non_ssl.config
+$(TEST_CONFIG_PATH_MQTT): $(ABS_PACKAGE_DIR)/test/src/non_ssl.config
 	cp $(ABS_PACKAGE_DIR)/test/src/non_ssl.config $@
 	@echo "\nNOT running SSL tests - looked in $(RABBITMQ_TEST_PATH) \n"
 
