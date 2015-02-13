547b69d14bcdfdd2060ba1797ef626dfa60d0261
Simon MacMullen <simon@rabbitmq.com>
2015-02-13 12:26:24 +0000

    Compile, now that we are in the broker repo.

--- src/rabbit_priority_queue.erl
+++ src/rabbit_priority_queue.erl
@@ -16,8 +16,8 @@
 
 -module(rabbit_priority_queue).
 
--include_lib("rabbit_common/include/rabbit.hrl").
--include_lib("rabbit_common/include/rabbit_framing.hrl").
+-include_lib("rabbit.hrl").
+-include_lib("rabbit_framing.hrl").
 -behaviour(rabbit_backing_queue).
 
 -rabbit_boot_step({?MODULE,
