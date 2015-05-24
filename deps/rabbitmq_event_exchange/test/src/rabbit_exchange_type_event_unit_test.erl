56c3cda88e8e1835a52bb8fc9b7fbbcf8b26294f
Michael Klishin <michael@clojurewerkz.org>
2015-05-24 05:06:31 +0300

    (c) year

--- deps/rabbitmq_event_exchange/test/src/rabbit_exchange_type_event_unit_test.erl
+++ deps/rabbitmq_event_exchange/test/src/rabbit_exchange_type_event_unit_test.erl
@@ -11,7 +11,7 @@
 %% The Original Code is RabbitMQ Consistent Hash Exchange.
 %%
 %% The Initial Developer of the Original Code is GoPivotal, Inc.
-%% Copyright (c) 2011-2013 GoPivotal, Inc.  All rights reserved.
+%% Copyright (c) 2011-2015 Pivotal Software, Inc.  All rights reserved.
 %%
 
 -module(rabbit_exchange_type_event_unit_test).
