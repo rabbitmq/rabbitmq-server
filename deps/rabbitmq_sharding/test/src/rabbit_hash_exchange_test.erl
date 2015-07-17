fa1acf873c4850cffbdaf6d5b7c8a2b0ffc8003f
ash-lshift <ash@lshift.net>
2015-07-17 13:32:46 +0100

    fix race condition

--- deps/rabbitmq_sharding/test/src/rabbit_hash_exchange_test.erl
+++ deps/rabbitmq_sharding/test/src/rabbit_hash_exchange_test.erl
@@ -62,9 +62,13 @@ test0(MakeMethod, MakeMsg, Queues, MsgCount, Count) ->
                                                  routing_key = <<"">> })
      || Q <- Queues],
 
+    amqp_channel:call(Chan, #'confirm.select'{}),
+
     [amqp_channel:call(Chan,
                        MakeMethod(),
                        MakeMsg()) || _ <- lists:duplicate(MsgCount, const)],
+
+    amqp_channel:wait_for_confirms_or_die(Chan),
 
     Counts =
         [begin
