b8067986ebe165ea231e38c78653320ab2cf3758
Michael Klishin <michael@clojurewerkz.org>
2015-08-13 17:38:40 +0300

    Ignore x-headers used by federation when they are garbled by clients
    Usually unintentionally. This is the safest option.
    
    Per discussion with a user suffering from this. Fixes #16.

--- deps/rabbitmq_federation/src/rabbit_federation_queue_link.erl
+++ deps/rabbitmq_federation/src/rabbit_federation_queue_link.erl
@@ -253,23 +253,25 @@ update_headers(#upstream_params{table = Table}, Redelivered, X, K, Headers) ->
                 %% routing key the first time a message gets
                 %% forwarded; after that it's known that they were
                 %% <<>> and QueueName respectively.
-                {rabbit_misc:set_table_value(
-                   rabbit_misc:set_table_value(
-                     Headers, <<"x-original-exchange">>, longstr, X),
-                   <<"x-original-routing-key">>, longstr, K), 0};
+                {init_x_original_source_headers(Headers, X, K), 0};
             {array, Been} ->
                 update_visit_count(Table, Been, Headers);
             %% this means the header comes from the client
-            %% which re-published the message. We can't assume
-            %% much about the headers so we simply wrap it in
-            %% an array
-            Other ->
-                update_visit_count(Table, [Other], Headers)
+            %% which re-published the message, most likely unintentionally.
+            %% We can't assume much about the value, so we simply ignore it.
+            _Other ->
+                {init_x_original_source_headers(Headers, X, K), 0}
         end,
     rabbit_basic:prepend_table_header(
       ?ROUTING_HEADER, Table ++ [{<<"redelivered">>, bool, Redelivered},
                                  {<<"visit-count">>, long, Count + 1}],
       swap_cc_header(Headers1)).
+
+init_x_original_source_headers(Headers, X, K) ->
+    rabbit_misc:set_table_value(
+        rabbit_misc:set_table_value(
+            Headers, <<"x-original-exchange">>, longstr, X),
+        <<"x-original-routing-key">>, longstr, K).
 
 update_visit_count(Table, Been, Headers) ->
     {Found, Been1} = lists:partition(
