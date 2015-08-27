c7bc259770291bea783df82c8f830b97779770bc
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2015-08-27 14:43:56 +0200

    Remove debug $(info)

--- deps/rabbit_common/mk/rabbitmq-run.mk
+++ deps/rabbit_common/mk/rabbitmq-run.mk
@@ -1,7 +1,6 @@
 .PHONY: run-broker
 
 ifeq ($(filter rabbitmq-dist.mk,$(notdir $(MAKEFILE_LIST))),)
-$(info Loading rabbitmq-dist.mk)
 include $(dir $(lastword $(MAKEFILE_LIST)))rabbitmq-dist.mk
 endif
 
