26524ce2751615355bbdefe7bc9e559d5c8f913e
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2015-09-01 11:41:24 +0200

    rabbitmq-tests.mk: Ignore output from erl_call when stopping the node

--- deps/rabbit_common/mk/rabbitmq-tests.mk
+++ deps/rabbit_common/mk/rabbitmq-tests.mk
@@ -17,7 +17,7 @@ tests-with-broker:: test-dist
                | tee -a $(TEST_TMPDIR)/test-output \
                | $(ERL_CALL) $(ERL_CALL_OPTS) \
                | tee -a $(TEST_TMPDIR)/test-output \
-               | egrep "{ok, (ok|passed)}" >/dev/null &&) \
+               | egrep '{ok, (ok|passed)}' >/dev/null &&) \
 	    MAKE="$(MAKE)" \
 	    RABBITMQ_NODENAME="$(RABBITMQ_NODENAME)" \
 	      $(foreach SCRIPT,$(WITH_BROKER_TEST_SCRIPTS),$(SCRIPT) &&) : ; \
@@ -29,6 +29,6 @@ tests-with-broker:: test-dist
 	  printf "\n\nFAILED\n" ; \
 	fi
 	$(verbose) sleep 1
-	$(verbose) echo "rabbit_misc:report_cover(), init:stop()." | $(ERL_CALL) $(ERL_CALL_OPTS)
+	$(verbose) echo 'rabbit_misc:report_cover(), init:stop().' | $(ERL_CALL) $(ERL_CALL_OPTS) >/dev/null
 	$(verbose) sleep 1
 	$(verbose) test -f $(TEST_TMPDIR)/.passed
