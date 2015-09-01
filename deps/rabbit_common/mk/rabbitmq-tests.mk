69508285f25f0e6142547f1dcdb2de8abedb17bb
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2015-09-01 15:16:36 +0200

    Support $(STANDALONE_TEST_*) tests

--- deps/rabbit_common/mk/rabbitmq-tests.mk
+++ deps/rabbit_common/mk/rabbitmq-tests.mk
@@ -1,10 +1,10 @@
-.PHONY: tests-with-broker
+.PHONY: tests-with-broker standalone-tests
 
 ifeq ($(filter rabbitmq-run.mk,$(notdir $(MAKEFILE_LIST))),)
 include $(dir $(lastword $(MAKEFILE_LIST)))rabbitmq-run.mk
 endif
 
-tests:: tests-with-broker
+tests:: tests-with-broker standalone-tests
 
 tests-with-broker:: test-dist
 	$(verbose) $(MAKE) start-background-node RABBITMQ_SERVER_START_ARGS='-pa $(CURDIR)/test'
@@ -32,3 +32,13 @@ tests-with-broker:: test-dist
 	$(verbose) echo 'rabbit_misc:report_cover(), init:stop().' | $(ERL_CALL) $(ERL_CALL_OPTS) >/dev/null
 	$(verbose) sleep 1
 	$(verbose) test -f $(TEST_TMPDIR)/.passed
+
+standalone-tests:: test-dist
+	$(verbose) $(MAKE) run-broker-deps
+	$(exec_verbose) $(if $(STANDALONE_TEST_COMMANDS), \
+	  $(foreach CMD,$(STANDALONE_TEST_COMMANDS), \
+	    ERL_LIBS="$(CURDIR)/dist:$(DIST_ERL_LIBS)" \
+	    $(ERL) $(ERL_OPTS) -pa $(CURDIR)/test -sname standalone_test -eval "init:stop(case $(CMD) of ok -> 0; passed -> 0; _Else -> 1 end)" && \
+	  ) \
+	:)
+	$(verbose) $(if $(STANDALONE_TEST_SCRIPTS),$(foreach SCRIPT,$(STANDALONE_TEST_SCRIPTS),$(SCRIPT) &&) :)
