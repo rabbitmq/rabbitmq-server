6854d078f537b4c066e66683848a71e2fb608034
Michael Klishin <mklishin@pivotal.io>
2015-09-16 15:58:37 +0300

    Split this test into two cases

--- deps/rabbitmq_stomp/test/src/errors.py
+++ deps/rabbitmq_stomp/test/src/errors.py
@@ -4,14 +4,12 @@ import base
 import time
 
 class TestErrorsAndCloseConnection(base.BaseTest):
-    def test_duplicate_consumer_tag(self):
-        destination = "/exchange/amq.direct/duplicate-consumer-tag-test"
+    def __test_duplicate_consumer_tag_with_headers(self, destination, headers):
+        self.subscribe_dest(self.conn, destination, None,
+                            headers = headers)
 
         self.subscribe_dest(self.conn, destination, None,
-                            headers = {'id': 1})
-
-        self.subscribe_dest(self.conn, destination, None,
-                            headers = {'id': 1})
+                            headers = headers)
 
         self.assertTrue(self.listener.await())
 
@@ -19,7 +17,18 @@ class TestErrorsAndCloseConnection(base.BaseTest):
         errorReceived = self.listener.errors[0]
         self.assertEquals("Duplicated subscription identifier", errorReceived['headers']['message'])
         self.assertEquals("A subscription identified by 'T_1' alredy exists.", errorReceived['message'])
-        self.assertFalse(self.conn.is_connected())
+        self.assertFalse(self.conn.is_connected())        
+        
+        
+    def test_duplicate_consumer_tag_with_transient_destination(self):
+        destination = "/exchange/amq.direct/duplicate-consumer-tag-test1"
+        self.__test_duplicate_consumer_tag_with_headers(destination, {'id': 1})
+
+    def test_duplicate_consumer_tag_with_durable_destination(self):
+        destination = "/queue/duplicate-consumer-tag-test2"
+        self.__test_duplicate_consumer_tag_with_headers(destination, {'id': 1,
+                                                                      'persistent': True})
+        
 
 class TestErrors(base.BaseTest):
 
@@ -82,4 +91,3 @@ class TestErrors(base.BaseTest):
         self.assertEquals("'" + content + "' is not a valid " +
                               dtype + " destination\n",
                           err['message'])
-
