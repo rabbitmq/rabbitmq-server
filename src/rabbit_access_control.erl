32281bd205c20e3eca82077416dbd553a7e1733a
Robby Raschke <robby.raschke@erlang-solutions.com>
2015-10-05 17:53:55 +0200

    Be backwards compatible for homegrown auth modules.

--- src/rabbit_access_control.erl
+++ src/rabbit_access_control.erl
@@ -101,6 +101,7 @@ try_authorize(Modules, Username) ->
       fun (Module, {ok, ModsImpls, ModsTags}) ->
               case Module:user_login_authorization(Username) of
                   {ok, Impl, Tags}-> {ok, [{Module, Impl} | ModsImpls], ModsTags++Tags};
+                  {ok, Impl}      -> {ok, [{Module, Impl} | ModsImpls], ModsTags};
                   {error, E}      -> {refused, Username,
                                         "~s failed authorizing ~s: ~p~n",
                                         [Module, Username, E]};
