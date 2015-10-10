ed516d4622d04ff79d56a9c0a303dd546f26c20f
Michael Klishin <mklishin@pivotal.io>
2015-10-10 19:07:58 +0300

    Clarify this comment

--- src/rabbit_mirror_queue_misc.erl
+++ src/rabbit_mirror_queue_misc.erl
@@ -45,7 +45,7 @@
      {requires, rabbit_registry},
      {enables, recovery}]}).
 
-%% For compatibility with versions that don't support sync batching.
+%% The value of 1 disables batching.
 -define(DEFAULT_BATCH_SIZE, 1).
 
 %%----------------------------------------------------------------------------
