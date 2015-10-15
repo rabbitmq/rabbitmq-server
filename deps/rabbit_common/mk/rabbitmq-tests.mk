7bbd0e6c791ff2451dafd4912d50b895fb16c24f
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2015-10-15 16:16:59 +0200

    rabbitmq-tests.mk: Fix several bugs with testsuite handling
    More variables are passed to the test commands/scripts. The Erlang
    search path includes the current testsuite and helpers in rabbitmq-test.
    
    In tests-with-broker, make sure the broker is stopped even if the
    testsuite fails.

--- deps/rabbit_common/mk/rabbitmq-tests.mk
+++ deps/rabbit_common/mk/rabbitmq-tests.mk
@@ -7,14 +7,19 @@ endif
 test_verbose_0 = @echo " TEST  " $@;
 test_verbose = $(test_verbose_$(V))
 
+TEST_BEAM_DIRS = $(CURDIR)/test \
+		 $(DEPS_DIR)/rabbitmq_test/ebin
+
 tests:: tests-with-broker standalone-tests
 
 tests-with-broker:: test-dist
+	$(verbose) rm -f $(TEST_TMPDIR)/.passed
 	$(verbose) $(MAKE) start-background-node \
-		RABBITMQ_SERVER_START_ARGS='-pa $(CURDIR)/test' \
+		RABBITMQ_SERVER_START_ARGS='$(patsubst %, -pa %,$(TEST_BEAM_DIRS))' \
+		$(WITH_BROKER_TEST_MAKEVARS) \
 		LOG_TO_STDIO=yes
 	$(verbose) $(MAKE) start-rabbit-on-node
-	$(exec_verbose) echo > $(TEST_TMPDIR)/test-output && \
+	-$(exec_verbose) echo > $(TEST_TMPDIR)/test-output && \
 	if $(foreach SCRIPT,$(WITH_BROKER_SETUP_SCRIPTS),$(SCRIPT) &&) \
 	    $(foreach CMD,$(WITH_BROKER_TEST_COMMANDS), \
 	     echo >> $(TEST_TMPDIR)/test-output && \
@@ -23,11 +28,15 @@ tests-with-broker:: test-dist
                | $(ERL_CALL) $(ERL_CALL_OPTS) \
                | tee -a $(TEST_TMPDIR)/test-output \
                | egrep '{ok, (ok|passed)}' >/dev/null &&) \
-	    MAKE="$(MAKE)" \
-	    RABBITMQ_NODENAME="$(RABBITMQ_NODENAME)" \
+	    MAKE='$(MAKE)' \
+	    DEPS_DIR='$(DEPS_DIR)' \
+	    NODE_TMPDIR='$(NODE_TMPDIR)' \
+	    RABBITMQ_NODENAME='$(RABBITMQ_NODENAME)' \
 	      $(foreach SCRIPT,$(WITH_BROKER_TEST_SCRIPTS),$(SCRIPT) &&) : ; \
         then \
 	  touch $(TEST_TMPDIR)/.passed ; \
+	fi
+	$(verbose) if test -f $(TEST_TMPDIR)/.passed; then \
 	  printf "\nPASSED\n" ; \
 	else \
 	  cat $(TEST_TMPDIR)/test-output ; \
@@ -38,12 +47,24 @@ tests-with-broker:: test-dist
 	$(verbose) sleep 1
 	$(verbose) test -f $(TEST_TMPDIR)/.passed
 
-standalone-tests:: test-dist
-	$(verbose) $(MAKE) run-broker-deps
+pre-standalone-tests::
+	@:
+
+standalone-tests:: pre-standalone-tests test-dist
 	$(exec_verbose) $(if $(STANDALONE_TEST_COMMANDS), \
 	  $(foreach CMD,$(STANDALONE_TEST_COMMANDS), \
-	    ERL_LIBS="$(CURDIR)/$(DIST_DIR):$(DIST_ERL_LIBS)" \
-	    $(ERL) $(ERL_OPTS) -pa $(CURDIR)/test -sname standalone_test -eval "init:stop(case $(CMD) of ok -> 0; passed -> 0; _Else -> 1 end)" && \
+	    MAKE='$(MAKE)' \
+	    DEPS_DIR='$(DEPS_DIR)' \
+	    TEST_TMPDIR='$(TEST_TMPDIR)' \
+	    ERL_LIBS='$(CURDIR)/$(DIST_DIR):$(DIST_ERL_LIBS)' \
+	    $(ERL) $(ERL_OPTS) $(patsubst %,-pa %,$(TEST_BEAM_DIRS)) \
+	    -sname standalone_test \
+	    -eval "init:stop(case $(CMD) of ok -> 0; passed -> 0; _Else -> 1 end)" && \
 	  ) \
 	:)
-	$(verbose) $(if $(STANDALONE_TEST_SCRIPTS),$(foreach SCRIPT,$(STANDALONE_TEST_SCRIPTS),$(SCRIPT) &&) :)
+	$(verbose) $(if $(STANDALONE_TEST_SCRIPTS), \
+	$(foreach SCRIPT,$(STANDALONE_TEST_SCRIPTS), \
+	    MAKE='$(MAKE)' \
+	    DEPS_DIR='$(DEPS_DIR)' \
+	    TEST_TMPDIR='$(TEST_TMPDIR)' \
+	    $(SCRIPT) &&) :)
