3ebf25adb2d5865ad0d940a82375579b98bcf059
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2015-10-16 15:56:46 +0200

    Initial move to erlang.mk

--- deps/rabbitmq_shovel_management/package.mk
+++
@@ -1,9 +0,0 @@
-RELEASABLE:=true
-DEPS:=rabbitmq-management rabbitmq-shovel
-WITH_BROKER_TEST_COMMANDS:=rabbit_shovel_mgmt_test_all:all_tests()
-WITH_BROKER_TEST_CONFIG:=$(PACKAGE_DIR)/etc/rabbit-test
-
-CONSTRUCT_APP_PREREQS:=$(shell find $(PACKAGE_DIR)/priv -type f)
-define construct_app_commands
-	cp -r $(PACKAGE_DIR)/priv $(APP_DIR)
-endef
