50c1eadfb296f15abdc7579fee02e68f68e4c556
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2015-11-03 15:12:30 +0100

    Update rabbitmq-components.mk

--- deps/rabbitmq_mqtt/rabbitmq-components.mk
+++ deps/rabbitmq_mqtt/rabbitmq-components.mk
@@ -169,7 +169,7 @@ endif
 endif
 endif
 
-ifeq ($(filter rabbit_public_umbrella rabbit rabbit_common rabbitmq_test,$(PROJECT)),)
+ifeq ($(filter rabbit_public_umbrella amqp_client rabbit rabbit_common rabbitmq_test,$(PROJECT)),)
 ifeq ($(filter rabbitmq_test,$(DEPS) $(BUILD_DEPS) $(TEST_DEPS)),)
 TEST_DEPS += rabbitmq_test
 endif
