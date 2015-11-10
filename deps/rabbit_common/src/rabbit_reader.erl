d254c903665b20ef8f6cbb1c089616b326c38c63
Michael Klishin <michael@clojurewerkz.org>
2015-11-11 02:52:22 +0300

    Refactor

--- deps/rabbit_common/src/rabbit_reader.erl
+++ deps/rabbit_common/src/rabbit_reader.erl
@@ -787,11 +787,7 @@ handle_exception(State = #v1{connection = #connection{protocol = Protocol,
         "Error on AMQP connection ~p (~s,"
         " user: '~s', state: ~p):~n~s~n",
         [self(), ConnName, User#user.username, opening, ErrMsg]),
-    {0, CloseMethod} =
-        rabbit_binary_generator:map_exception(Channel, Reason, Protocol),
-    State1 = close_connection(terminate_channels(State)),
-    ok = send_on_channel0(State#v1.sock, CloseMethod, Protocol),
-    State1;
+    send_connection_exception_and_close(Channel, Protocol, Reason, State);
 handle_exception(State = #v1{connection = #connection{protocol = Protocol},
                              connection_state = CS = opening},
                  Channel, Reason = #amqp_error{}) ->
@@ -1431,6 +1427,9 @@ pack_for_1_0(Buf, BufLen, #v1{parent       = Parent,
 
 respond_and_close(State, Channel, Protocol, Reason, LogErr) ->
     log_hard_error(State, Channel, LogErr),
+    send_connection_exception_and_close(Channel, Protocol, Reason, State).
+
+send_connection_exception_and_close(Channel, Protocol, Reason, State) ->
     {0, CloseMethod} =
         rabbit_binary_generator:map_exception(Channel, Reason, Protocol),
     State1 = close_connection(terminate_channels(State)),
