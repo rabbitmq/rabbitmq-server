ad7f642b0014929e9995457ff93a8a8168c3dd85
Michael Klishin <michael@clojurewerkz.org>
2015-11-11 12:09:25 +0300

    Make log entries for "abruptly closed connections" make more sense

--- deps/rabbit_common/src/rabbit_reader.erl
+++ deps/rabbit_common/src/rabbit_reader.erl
@@ -755,6 +755,9 @@ log_connection_exception(Severity, Name, {heartbeat_timeout, TimeoutSec}) ->
   %% Long line to avoid extra spaces and line breaks in log
   log(Severity, "closing AMQP connection ~p (~s):~nMissed heartbeats from client, timeout: ~ps~n",
     [self(), Name, TimeoutSec]);
+log_connection_exception(Severity, Name, connection_closed_abruptly) ->
+  log(Severity, "closing AMQP connection ~p (~s):~nClient unexpectedly closed TCP connection~n",
+    [self(), Name]);
 log_connection_exception(Severity, Name, Ex) ->
   log(Severity, "closing AMQP connection ~p (~s):~n~p~n",
     [self(), Name, Ex]).
