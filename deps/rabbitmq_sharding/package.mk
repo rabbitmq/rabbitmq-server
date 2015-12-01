15f41750de9e8ef1d65e7bc69fd6cbb04ede64b4
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2015-12-01 10:41:02 +0100

    package.mk is useless with erlang.mk

--- deps/rabbitmq_sharding/package.mk
+++
@@ -1,7 +0,0 @@
-DEPS:=rabbitmq-erlang-client
-
-WITH_BROKER_TEST_COMMANDS:=rabbit_sharding_test_all:all_tests()
-WITH_BROKER_TEST_CONFIG:=$(PACKAGE_DIR)/etc/rabbit-test
-
-$(PACKAGE_DIR)+pre-test::
-	rm -rf ${TMPDIR}/rabbitmq-sharding-tests
