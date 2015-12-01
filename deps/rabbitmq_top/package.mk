001a67371c45f016aa01011504817778f9816cc1
Michael Klishin <michael@clojurewerkz.org>
2015-12-01 08:31:52 +0300

    package.mk can go now

--- deps/rabbitmq_top/package.mk
+++
@@ -1,7 +0,0 @@
-RELEASABLE:=true
-DEPS:=rabbitmq-management
-
-CONSTRUCT_APP_PREREQS:=$(shell find $(PACKAGE_DIR)/priv -type f)
-define construct_app_commands
-	cp -r $(PACKAGE_DIR)/priv $(APP_DIR)
-endef
