609b890fcc6a1832d680740a96f81757266dd20a
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2015-12-01 15:46:32 +0100

    Move sync-* and udpate-* targets from the Umbrella to rabbitmq-tools.mk

--- deps/rabbit_common/mk/rabbitmq-plugin.mk
+++ deps/rabbit_common/mk/rabbitmq-plugin.mk
@@ -9,3 +9,7 @@ endif
 ifeq ($(filter rabbitmq-tests.mk,$(notdir $(MAKEFILE_LIST))),)
 include $(dir $(lastword $(MAKEFILE_LIST)))rabbitmq-tests.mk
 endif
+
+ifeq ($(filter rabbitmq-tools.mk,$(notdir $(MAKEFILE_LIST))),)
+include $(dir $(lastword $(MAKEFILE_LIST)))rabbitmq-tools.mk
+endif
---
+++ deps/rabbit_common/mk/rabbitmq-tools.mk
@@ -0,0 +1,63 @@
+READY_DEPS = $(foreach DEP,\
+	       $(filter $(RABBITMQ_COMPONENTS),$(DEPS) $(BUILD_DEPS) $(TEST_DEPS)), \
+	       $(if $(wildcard $(DEPS_DIR)/$(DEP)),$(DEP),))
+
+.PHONY: update-erlang-mk update-rabbitmq-components.mk
+
+update-erlang-mk: erlang-mk
+	$(verbose) if test "$(DO_COMMIT)" = 'yes'; then \
+		git diff --quiet -- erlang.mk \
+		|| git commit -m 'Update erlang.mk' -- erlang.mk; \
+	fi
+	$(verbose) for repo in $(READY_DEPS:%=$(DEPS_DIR)/%); do \
+		! test -f $$repo/erlang.mk \
+		|| $(MAKE) -C $$repo erlang-mk; \
+		if test "$(DO_COMMIT)" = 'yes'; then \
+			(cd $$repo; \
+			 git diff --quiet -- erlang.mk \
+			 || git commit -m 'Update erlang.mk' -- erlang.mk); \
+		fi; \
+	done
+
+update-rabbitmq-components-mk: rabbitmq-components-mk
+	$(verbose) for repo in $(READY_DEPS:%=$(DEPS_DIR)/%); do \
+		! test -f $$repo/rabbitmq-components.mk \
+		|| $(MAKE) -C $$repo rabbitmq-components-mk; \
+	done
+
+ifneq ($(wildcard .git),)
+
+.PHONY: sync-gitremote sync-gituser
+
+sync-gitremote: $(READY_DEPS:%=$(DEPS_DIR)/%+sync-gitremote)
+	@:
+
+%+sync-gitremote:
+	$(exec_verbose) cd $* && \
+		git remote set-url origin \
+		'$(call dep_rmq_repo,$(RABBITMQ_CURRENT_FETCH_URL),$(notdir $*))'
+	$(verbose) cd $* && \
+		git remote set-url --push origin \
+		'$(call dep_rmq_repo,$(RABBITMQ_CURRENT_PUSH_URL),$(notdir $*))'
+
+RMQ_GIT_GLOBAL_USER_NAME := $(shell git config --global user.name)
+RMQ_GIT_GLOBAL_USER_EMAIL := $(shell git config --global user.email)
+RMQ_GIT_USER_NAME := $(shell git config user.name)
+RMQ_GIT_USER_EMAIL := $(shell git config user.email)
+
+sync-gituser: $(READY_DEPS:%=$(DEPS_DIR)/%+sync-gituser)
+	@:
+
+%+sync-gituser:
+ifeq ($(RMQ_GIT_USER_NAME),$(RMQ_GIT_GLOBAL_USER_NAME))
+	$(exec_verbose) cd $* && git config --unset user.name || :
+else
+	$(exec_verbose) cd $* && git config user.name "$(RMQ_GIT_USER_NAME)"
+endif
+ifeq ($(RMQ_GIT_USER_EMAIL),$(RMQ_GIT_GLOBAL_USER_EMAIL))
+	$(verbose) cd $* && git config --unset user.email || :
+else
+	$(verbose) cd $* && git config user.email "$(RMQ_GIT_USER_EMAIL)"
+endif
+
+endif # ($(wildcard .git),)
