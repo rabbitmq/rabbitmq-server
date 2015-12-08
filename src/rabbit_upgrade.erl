d8ab7395d07f8b9d948db3928d9f79519e3ca831
Daniil Fedotov <dfedotov@pivotal.io>
2015-12-08 14:06:52 +0000

    Report backup directory if previous update is failed

--- src/rabbit_upgrade.erl
+++ src/rabbit_upgrade.erl
@@ -100,7 +100,12 @@ ensure_backup_taken() ->
                      false -> ok = take_backup();
                      _     -> ok
                  end;
-        true  -> throw({error, previous_upgrade_failed})
+        true  -> 
+          error("Lock file exist at location \"~s\". 
+            Looks like previous upgrade is in process or has failed. 
+            Backup could have been taken in directory \"~s\"",
+            [lock_filename(), backup_dir()]),
+          throw({error, previous_upgrade_failed})
     end.
 
 take_backup() ->
@@ -108,7 +113,7 @@ take_backup() ->
     case rabbit_mnesia:copy_db(BackupDir) of
         ok         -> info("upgrades: Mnesia dir backed up to ~p~n",
                            [BackupDir]);
-        {error, E} -> throw({could_not_back_up_mnesia_dir, E})
+        {error, E} -> throw({could_not_back_up_mnesia_dir, E, BackupDir})
     end.
 
 ensure_backup_removed() ->
