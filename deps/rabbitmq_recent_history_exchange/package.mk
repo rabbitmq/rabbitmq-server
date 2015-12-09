697e7cfdf3479e84321c4471db6b1fb3c28f08b7
Alvaro Videla <videlalvaro@gmail.com>
2015-12-09 14:35:00 +0100

    port to erlang.mk

--- deps/rabbitmq_recent_history_exchange/package.mk
+++
@@ -1,4 +0,0 @@
-DEPS:=rabbitmq-server rabbitmq-erlang-client
-RETAIN_ORIGINAL_VERSION:=true
-WITH_BROKER_TEST_COMMANDS:=rabbit_exchange_type_recent_history_test:test()
-WITH_BROKER_TEST_CONFIG:=$(PACKAGE_DIR)/etc/rabbit-test
