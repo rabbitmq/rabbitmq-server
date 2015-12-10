a1d5fd36a872b656e405110df5d2e91ba7b39507
Michael Klishin <michael@clojurewerkz.org>
2015-12-10 15:02:16 +0300

    Switch to rabbit_data_coercion:to_binary/1 [from rabbit_common]

--- deps/rabbitmq_tracing/src/rabbit_tracing_util.erl
+++ deps/rabbitmq_tracing/src/rabbit_tracing_util.erl
@@ -1,13 +1,7 @@
 -module(rabbit_tracing_util).
 
--export([to_binary/1, coerce_env_value/2]).
+-export([coerce_env_value/2]).
 
-%% TODO: move to rabbit_common
-%% TODO: move to rabbit_common
-coerce_env_value(username, Val) -> to_binary(Val);
-coerce_env_value(password, Val) -> to_binary(Val);
+coerce_env_value(username, Val) -> rabbit_data_coercion:to_binary(Val);
+coerce_env_value(password, Val) -> rabbit_data_coercion:to_binary(Val);
 coerce_env_value(_,        Val) -> Val.
-
-%% TODO: move to rabbit_common
-to_binary(Val) when is_list(Val) -> list_to_binary(Val);
-to_binary(Val)                   -> Val.
