1c3bbc1820ebfa0a57670c9e4be525908e614231
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2015-12-16 09:23:38 +0100

    erlang.mk: Disable eunit
    Even though the eunit test succeeds here, it's executed twice because we
    also run it from `STANDALONE_TEST_COMMANDS`.

--- deps/rabbitmq_stomp/erlang.mk
+++ deps/rabbitmq_stomp/erlang.mk
@@ -16,7 +16,7 @@
 
 ERLANG_MK_FILENAME := $(realpath $(lastword $(MAKEFILE_LIST)))
 
-ERLANG_MK_VERSION = 2.0.0-pre.2-16-ga6d6bfe
+ERLANG_MK_VERSION = 2.0.0-pre.2-16-ga6d6bfe-dirty
 
 # Core configuration.
 
@@ -6113,57 +6113,6 @@ escript:: distclean-escript deps app
 
 distclean-escript:
 	$(gen_verbose) rm -f $(ESCRIPT_NAME)
-
-# Copyright (c) 2014, Enrique Fernandez <enrique.fernandez@erlang-solutions.com>
-# Copyright (c) 2015, Loïc Hoguin <essen@ninenines.eu>
-# This file is contributed to erlang.mk and subject to the terms of the ISC License.
-
-.PHONY: eunit
-
-# Configuration
-
-EUNIT_OPTS ?=
-
-# Core targets.
-
-tests:: eunit
-
-help::
-	$(verbose) printf "%s\n" "" \
-		"EUnit targets:" \
-		"  eunit       Run all the EUnit tests for this project"
-
-# Plugin-specific targets.
-
-define eunit.erl
-	case "$(COVER)" of
-		"" -> ok;
-		_ ->
-			case cover:compile_beam_directory("ebin") of
-				{error, _} -> halt(1);
-				_ -> ok
-			end
-	end,
-	case eunit:test([$(call comma_list,$(1))], [$(EUNIT_OPTS)]) of
-		ok -> ok;
-		error -> halt(2)
-	end,
-	case "$(COVER)" of
-		"" -> ok;
-		_ ->
-			cover:export("eunit.coverdata")
-	end,
-	halt()
-endef
-
-EUNIT_EBIN_MODS = $(notdir $(basename $(call core_find,ebin/,*.beam)))
-EUNIT_TEST_MODS = $(notdir $(basename $(call core_find,$(TEST_DIR)/,*.beam)))
-EUNIT_MODS = $(foreach mod,$(EUNIT_EBIN_MODS) $(filter-out \
-	$(patsubst %,%_tests,$(EUNIT_EBIN_MODS)),$(EUNIT_TEST_MODS)),{module,'$(mod)'})
-
-eunit: test-build
-	$(gen_verbose) $(ERL) -pa $(TEST_DIR) $(DEPS_DIR)/*/ebin ebin \
-		-eval "$(subst $(newline),,$(subst ",\",$(call eunit.erl,$(EUNIT_MODS))))"
 
 # Copyright (c) 2013-2015, Loïc Hoguin <essen@ninenines.eu>
 # This file is part of erlang.mk and subject to the terms of the ISC License.
