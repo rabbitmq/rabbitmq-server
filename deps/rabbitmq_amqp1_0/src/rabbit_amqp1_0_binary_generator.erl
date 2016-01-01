c2a28304e4c4b8be1547dce6d7937e34ec5fc6fa
Michael Klishin <michael@clojurewerkz.org>
2016-01-01 12:59:16 +0300

    Update (c) info

--- deps/rabbitmq_amqp1_0/src/rabbit_amqp1_0_binary_generator.erl
+++ deps/rabbitmq_amqp1_0/src/rabbit_amqp1_0_binary_generator.erl
@@ -11,7 +11,7 @@
 %% The Original Code is RabbitMQ.
 %%
 %% The Initial Developer of the Original Code is GoPivotal, Inc.
-%% Copyright (c) 2007-2014 GoPivotal, Inc.  All rights reserved.
+%% Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
 %%
 
 -module(rabbit_amqp1_0_binary_generator).
