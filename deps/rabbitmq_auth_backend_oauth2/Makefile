47da90b6522250a21c0a24161165bc5f924d0874
Daniil Fedotov <dfedotov@pivotal.io>
2016-01-15 14:50:21 +0000

    Init. Make request to /check_token

---
+++ deps/rabbitmq_auth_backend_oauth2/Makefile
@@ -0,0 +1,15 @@
+PROJECT = rabbitmq_auth_backend_http
+
+DEPS = mochiweb
+TEST_DEPS = rabbit
+
+DEP_PLUGINS = rabbit_common/mk/rabbitmq-plugin.mk
+
+# FIXME: Use erlang.mk patched for RabbitMQ, while waiting for PRs to be
+# reviewed and merged.
+
+ERLANG_MK_REPO = https://github.com/rabbitmq/erlang.mk.git
+ERLANG_MK_COMMIT = rabbitmq-tmp
+
+include rabbitmq-components.mk
+include erlang.mk
---
+++ deps/rabbitmq_auth_backend_oauth2/src/rabbit_auth_backend_uaa.erl
@@ -0,0 +1,116 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License
+%% at http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and
+%% limitations under the License.
+%%
+%% The Original Code is RabbitMQ HTTP authentication.
+%%
+%% The Initial Developer of the Original Code is VMware, Inc.
+%% Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
+%%
+
+-module(rabbit_auth_backend_uaa).
+
+-include_lib("rabbit_common/include/rabbit.hrl").
+
+-behaviour(rabbit_authn_backend).
+-behaviour(rabbit_authz_backend).
+
+-export([description/0, q/2]).
+-export([user_login_authentication/2, user_login_authorization/1,
+         check_vhost_access/3, check_resource_access/3]).
+
+%% httpc seems to get racy when using HTTP 1.1
+-define(HTTPC_OPTS, [{version, "HTTP/1.0"}]).
+
+%%--------------------------------------------------------------------
+
+description() ->
+    [{name, <<"HTTP">>},
+     {description, <<"HTTP authentication / authorisation">>}].
+
+%%--------------------------------------------------------------------
+
+user_login_authentication(Username, AuthProps) ->
+    case check_token(Username) of
+        {error, _} = E -> E;
+        {refused, Err} -> {refused, "Denied by UAA plugin with error: ~p", 
+                                     [Err]};
+        {ok, UserData} -> {ok, #auth_user{ username = Username, 
+                                           tags = [], 
+                                           impl = none}}
+    end.
+
+user_login_authorization(Username) ->
+    case user_login_authentication(Username, []) of
+        {ok, #auth_user{impl = Impl}} -> {ok, Impl};
+        Else                          -> Else
+    end.
+
+check_vhost_access(#auth_user{username = Username}, VHost, _Sock) ->
+    with_token(Username, 
+        fun(UserData) ->
+            vhost_scope_access(VHost, 
+                               proplists:get_value(<<"scope">>, UserData, []))
+        end).
+
+check_resource_access(#auth_user{username = Username}, Resource, Permission) ->
+    with_token(Username, 
+        fun(UserData) ->
+            resource_scope_access(Resource, 
+                                  proplists:get_value(<<"scope">>, 
+                                                      UserData, []))
+        end)..
+
+%%--------------------------------------------------------------------
+
+with_token(Token, Fun) ->
+    case check_token(Token) of
+        {ok, UserData} -> Fun(UserData);
+        _              -> false
+    end.
+
+check_token(Token) ->
+    UaaUri = application:get_env(rabbitmq_auth_backend_uaa, uri),
+    Path   = binary_to_list(<<UaaUri/binary, "/check_token">>),
+    AuthUser = application:get_env(rabbitmq_auth_backend_uaa, username),
+    AuthPass = application:get_env(rabbitmq_auth_backend_uaa, password),
+    Auth = base64:encode_to_string(<<AuthUser/binary, ":", AuthPass/binary>>),
+    URI  = uri_parser:parse(Path, [{port, 80}]),
+    {host, Host} = lists:keyfind(host, 1, URI),
+    {port, Port} = lists:keyfind(port, 1, URI),
+    HostHdr = rabbit_misc:format("~s:~b", [Host, Port]),
+    ReqBody = "token=" ++ binary_to_list(Token),
+    Resp = httpc:request(post, 
+                        {Path, 
+                         [{"Host", HostHdr}, {"Authorization", Auth}], 
+                         "application/x-www-form-encoded", 
+                         ReqBody}, 
+                        ?HTTPC_OPTS, []),
+    case Resp of
+        {ok, {{_HTTP, Code, _}, _Headers, Body}} ->
+            case Code of
+                200 -> parse_resp(Body);
+                400 -> parse_err(Body);
+                _   -> {error, {Code, Body}}
+            end;
+        {error, _} = E -> E
+    end.
+
+parse_resp(Body) -> 
+    mochijson2:decode(Body).
+
+parse_err(Body) ->
+    mochijson2:decode(Body).    
+
+vhost_scope_access(VHost, Scope) -> true.
+
+resource_scope_access(Resource, Scope) -> true.
+
+
+%%--------------------------------------------------------------------
