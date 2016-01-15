8431261b439f048dbc5b402da23f08cd19ade7eb
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2016-01-15 12:21:22 +0100

    rabbitmq-server.bat: Honor RABBITMQ_LOGS=- to log to stdout
    Note that at the time of this commit, Lager does not support logging
    to stdout on Windows. This commit still improves consistency between
    Unix and Windows.
    
    References #493.

--- scripts/rabbitmq-server.bat
+++ scripts/rabbitmq-server.bat
@@ -71,6 +71,24 @@ if not "!RABBITMQ_NODE_IP_ADDRESS!"=="" (
    )
 )
 
+REM If $RABBITMQ_LOGS is '-', send all log messages to stdout. Likewise
+REM for RABBITMQ_SASL_LOGS. This is particularily useful for Docker
+REM images.
+
+if "!RABBITMQ_LOGS!" == "-" (
+    set RABBIT_ERROR_LOGGER=tty
+) else (
+    set RABBIT_ERROR_LOGGER={file,\""!RABBITMQ_LOGS:\=/!"\"}
+)
+
+if "!RABBITMQ_SASL_LOGS!" == "-" (
+    set SASL_ERROR_LOGGER=tty
+    set RABBIT_SASL_ERROR_LOGGER=tty
+) else (
+    set SASL_ERROR_LOGGER=false
+    set RABBIT_SASL_ERROR_LOGGER={file,\""!RABBITMQ_SASL_LOGS:\=/!"\"}
+)
+
 set RABBITMQ_START_RABBIT=
 if "!RABBITMQ_ALLOW_INPUT!"=="" (
     set RABBITMQ_START_RABBIT=!RABBITMQ_START_RABBIT! -noinput
@@ -97,9 +115,9 @@ if "!RABBITMQ_IO_THREAD_POOL_SIZE!"=="" (
 -kernel inet_default_connect_options "[{nodelay, true}]" ^
 !RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS! ^
 -sasl errlog_type error ^
--sasl sasl_error_logger false ^
--rabbit error_logger {file,\""!RABBITMQ_LOGS:\=/!"\"} ^
--rabbit sasl_error_logger {file,\""!RABBITMQ_SASL_LOGS:\=/!"\"} ^
+-sasl sasl_error_logger !SASL_ERROR_LOGGER! ^
+-rabbit error_logger !RABBIT_ERROR_LOGGER! ^
+-rabbit sasl_error_logger !RABBIT_SASL_ERROR_LOGGER! ^
 -rabbit enabled_plugins_file \""!RABBITMQ_ENABLED_PLUGINS_FILE:\=/!"\" ^
 -rabbit plugins_dir \""!RABBITMQ_PLUGINS_DIR:\=/!"\" ^
 -rabbit plugins_expand_dir \""!RABBITMQ_PLUGINS_EXPAND_DIR:\=/!"\" ^
