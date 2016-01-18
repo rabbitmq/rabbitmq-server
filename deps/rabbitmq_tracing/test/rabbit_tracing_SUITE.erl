e104249a312579776ebc150e892eee9db24ae777
Loïc Hoguin <essen@ninenines.eu>
2016-01-18 13:11:19 +0100

    Change some NO_CONTENT into CREATED in the tests
    (cherry-picked from commit 66bf05d51702167b5672e668d952f8d6e9a25329)

--- deps/rabbitmq_tracing/test/rabbit_tracing_SUITE.erl
+++ deps/rabbitmq_tracing/test/rabbit_tracing_SUITE.erl
@@ -89,7 +89,7 @@ tracing_test(Config) ->
 
     Args = [{format,  <<"json">>},
             {pattern, <<"#">>}],
-    http_put(Config, "/traces/%2f/test", Args, ?NO_CONTENT),
+    http_put(Config, "/traces/%2f/test", Args, ?CREATED),
     assert_list([[{name,    <<"test">>},
                   {format,  <<"json">>},
                   {pattern, <<"#">>}]], http_get(Config, "/traces/%2f/")),
@@ -132,7 +132,7 @@ tracing_validation_test(Config) ->
                     {max_payload_bytes, <<"abc">>}],  ?BAD_REQUEST),
     http_put(Config, Path, [{format,            <<"json">>},
                     {pattern,           <<"#">>},
-                    {max_payload_bytes, 1000}],       ?NO_CONTENT),
+                    {max_payload_bytes, 1000}],       ?CREATED),
     http_delete(Config, Path, ?NO_CONTENT),
 
     passed.
