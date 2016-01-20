2ea908e2ef7e73ccf49b5c9e3abb135dc938c3f3
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2016-01-20 19:33:35 +0100

    Give tests enough time to complete
    EUnit will time the tests out after five minutes which isn't enough.

--- deps/rabbitmq_trust_store/test/rabbit_trust_store_test.erl
+++ deps/rabbitmq_trust_store/test/rabbit_trust_store_test.erl
@@ -13,52 +13,66 @@ library_test() ->
 
     {_Root, _Certificate, _Key} = ct_helper:make_certs().
 
-validation_success_test() ->
+validation_success_test_() ->
 
-    %% Given: an authority and a certificate rooted with that authority.
-    AuthorityInfo = {X = Root, _AuthorityKey} = erl_make_certs:make_cert([{key, dsa}]),
-    {Certificate, Key} = chain(AuthorityInfo), {Y, Z} = chain(AuthorityInfo),
+    {timeout,
+     10,
+     fun () ->
 
-    %% When: Rabbit accepts just this one authority's certificate
-    %% (i.e. these are options that'd be in the configuration file).
-    rabbit_networking:start_ssl_listener(port(), [
-        {cacerts, [Root]},
-        {cert, Y}, {key, Z},
-        {verify, verify_peer}, {fail_if_no_peer_cert, true}], 1),
+             %% Given: an authority and a certificate rooted with that authority.
+             AuthorityInfo = {X = Root, _AuthorityKey} = erl_make_certs:make_cert([{key, dsa}]),
+             {Certificate, Key} = chain(AuthorityInfo), {Y, Z} = chain(AuthorityInfo),
 
-    %% Then: a client presenting a certifcate rooted at the same
-    %% authority connects successfully.
-    {ok, Con} = amqp_connection:start(#amqp_params_network{host = "127.0.0.1", port = port(), ssl_options = [
-        {cacerts, [X]},
-        {cert, Certificate}, {key, Key}]}),
+             %% When: Rabbit accepts just this one authority's certificate
+             %% (i.e. these are options that'd be in the configuration file).
+             rabbit_networking:start_ssl_listener(port(), [
+                 {cacerts, [Root]},
+                 {cert, Y}, {key, Z},
+                 {verify, verify_peer}, {fail_if_no_peer_cert, true}], 1),
 
-    %% Clean: client & server TLS/TCP.
-    ok = amqp_connection:close(Con),
-    rabbit_networking:stop_tcp_listener(port()).
+             %% Then: a client presenting a certifcate rooted at the same
+             %% authority connects successfully.
+             {ok, Con} = amqp_connection:start(#amqp_params_network{host = "127.0.0.1", port = port(), ssl_options = [
+                 {cacerts, [X]},
+                 {cert, Certificate}, {key, Key}]}),
 
-validation_failure_test() ->
+             %% Clean: client & server TLS/TCP.
+             ok = amqp_connection:close(Con),
+             rabbit_networking:stop_tcp_listener(port())
 
-    %% Given: a root certificate and a certificate rooted with another
-    %% authority.
-    {R, _X, _Y} = ct_helper:make_certs(),
-    {S,  C, _Z} = ct_helper:make_certs(),
+     end
+    }.
 
-    %% When: Rabbit accepts certificates rooted with just one
-    %% particular authority.
-    rabbit_networking:start_ssl_listener(port(), [
-        {cacerts, [R]},
-        {cert, _X}, {key, _Y},
-        {verify, verify_peer}, {fail_if_no_peer_cert, true}], 1),
+validation_failure_test_() ->
 
-    %% Then: a client presenting a certificate rooted with another
-    %% authority is REJECTED.
-    {error, _} = amqp_connection:start(#amqp_params_network{host = "127.0.0.1",
-        port = port(), ssl_options = [
-            {cacerts, [S]},
-            {cert, C}, {key, _Z}]}),
+    {timeout,
+     10,
+     fun () ->
 
-    %% Clean: server TLS/TCP.
-    rabbit_networking:stop_tcp_listener(port()).
+        %% Given: a root certificate and a certificate rooted with another
+        %% authority.
+        {R, _X, _Y} = ct_helper:make_certs(),
+        {S,  C, _Z} = ct_helper:make_certs(),
+
+        %% When: Rabbit accepts certificates rooted with just one
+        %% particular authority.
+        rabbit_networking:start_ssl_listener(port(), [
+            {cacerts, [R]},
+            {cert, _X}, {key, _Y},
+            {verify, verify_peer}, {fail_if_no_peer_cert, true}], 1),
+
+        %% Then: a client presenting a certificate rooted with another
+        %% authority is REJECTED.
+        {error, _} = amqp_connection:start(#amqp_params_network{host = "127.0.0.1",
+            port = port(), ssl_options = [
+                {cacerts, [S]},
+                {cert, C}, {key, _Z}]}),
+
+        %% Clean: server TLS/TCP.
+        rabbit_networking:stop_tcp_listener(port())
+
+     end
+    }.
 
 
 %% Test Constants
