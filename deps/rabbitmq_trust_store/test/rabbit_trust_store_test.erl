9f0baac590a5c3cdb7db1cb1e3aa161a790c838f
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2016-01-21 13:53:41 +0100

    Format test set a little better
    Several changes:
    
    * Test names tell us which protocol they use (AMQP). STOMP and MQTT to
      follow.
    
    * Give tests more time to complete as old value proved to be short
    
    * Indicate variables which aren't really involved (but necessary) in a
      given test with a leading underscore
    
    * Format with more stringent guidlines in mind

--- deps/rabbitmq_trust_store/test/rabbit_trust_store_test.erl
+++ deps/rabbitmq_trust_store/test/rabbit_trust_store_test.erl
@@ -13,28 +13,28 @@ library_test() ->
 
     {_Root, _Certificate, _Key} = ct_helper:make_certs().
 
-validation_success_test_() ->
+validation_success_for_AMQP_client_test_() ->
 
     {timeout,
-     10,
+     15,
      fun () ->
 
              %% Given: an authority and a certificate rooted with that authority.
-             AuthorityInfo = {X = Root, _AuthorityKey} = erl_make_certs:make_cert([{key, dsa}]),
-             {Certificate, Key} = chain(AuthorityInfo), {Y, Z} = chain(AuthorityInfo),
+             AuthorityInfo = {_X = Root, _AuthorityKey} = erl_make_certs:make_cert([{key, dsa}]),
+             {Certificate, Key} = chain(AuthorityInfo),
+             {_Y, _Z} = chain(AuthorityInfo),
 
              %% When: Rabbit accepts just this one authority's certificate
              %% (i.e. these are options that'd be in the configuration file).
              rabbit_networking:start_ssl_listener(port(), [
-                 {cacerts, [Root]},
-                 {cert, Y}, {key, Z},
-                 {verify, verify_peer}, {fail_if_no_peer_cert, true}], 1),
+                 {verify, verify_peer}, {fail_if_no_peer_cert, true},
+                 {cacerts, [Root]}, {cert, _Y}, {key, _Z}], 1),
 
              %% Then: a client presenting a certifcate rooted at the same
              %% authority connects successfully.
-             {ok, Con} = amqp_connection:start(#amqp_params_network{host = "127.0.0.1", port = port(), ssl_options = [
-                 {cacerts, [X]},
-                 {cert, Certificate}, {key, Key}]}),
+             {ok, Con} = amqp_connection:start(#amqp_params_network{host = "127.0.0.1",
+                 port = port(), ssl_options = [{cert, Certificate}, {key, Key},
+                     {cacerts, [_X]}]}),
 
              %% Clean: client & server TLS/TCP.
              ok = amqp_connection:close(Con),
@@ -43,16 +43,16 @@ validation_success_test_() ->
      end
     }.
 
-validation_failure_test_() ->
+validation_failure_for_AMQP_client_test_() ->
 
     {timeout,
-     10,
+     15,
      fun () ->
 
         %% Given: a root certificate and a certificate rooted with another
         %% authority.
         {R, _X, _Y} = ct_helper:make_certs(),
-        {S,  C, _Z} = ct_helper:make_certs(),
+        {_S, C, _Z} = ct_helper:make_certs(),
 
         %% When: Rabbit accepts certificates rooted with just one
         %% particular authority.
@@ -65,7 +65,7 @@ validation_failure_test_() ->
         %% authority is REJECTED.
         {error, _} = amqp_connection:start(#amqp_params_network{host = "127.0.0.1",
             port = port(), ssl_options = [
-                {cacerts, [S]},
+                {cacerts, [_S]},
                 {cert, C}, {key, _Z}]}),
 
         %% Clean: server TLS/TCP.
@@ -78,6 +78,9 @@ validation_failure_test_() ->
 %% Test Constants
 
 port() -> 4096.
+
+
+%% Ancillary
 
 chain(Issuer) ->
     %% Theses are DER encoded.
