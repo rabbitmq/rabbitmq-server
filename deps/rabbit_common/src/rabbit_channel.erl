b6ae545b81d135b3a71c4bf509c15a37b10aecb8
Ayanda Dube <ayanda.dube@erlang-solutions.com>
2016-03-02 15:55:19 +0000

    Completely ignore unused channel's termination reason.

--- deps/rabbit_common/src/rabbit_channel.erl
+++ deps/rabbit_common/src/rabbit_channel.erl
@@ -607,14 +607,8 @@ handle_pre_hibernate(State) ->
                 end),
     {hibernate, rabbit_event:stop_stats_timer(State, #ch.stats_timer)}.
 
-terminate(Reason, State) ->
+terminate(_Reason, State) ->
     {_Res, _State1} = notify_queues(State),
-    case Reason of
-        normal            -> ok;
-        shutdown          -> ok;
-        {shutdown, _Term} -> ok;
-        _                 -> ok
-    end,
     pg_local:leave(rabbit_channels, self()),
     rabbit_event:if_enabled(State, #ch.stats_timer,
                             fun() -> emit_stats(State) end),
