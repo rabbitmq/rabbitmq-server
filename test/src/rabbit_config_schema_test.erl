efb5c041e93d371e5f95b5a9cdee7f1e529c8c56
Daniil Fedotov <dfedotov@pivotal.io>
2016-03-04 16:22:02 +0000

    testing snippets from documentation

--- test/src/rabbit_config_schema_test.erl
+++ test/src/rabbit_config_schema_test.erl
@@ -3,6 +3,15 @@
 -export([test_snippet/3]).
 -compile(export_all).
 
+
+run_snippets(FileName) ->
+    {ok, [Snippets]} = file:consult(FileName),
+    lists:map(
+        fun({N,S,C,P})   -> test_snippet({integer_to_list(N),S,[]},C,P);
+           ({N,S,A,C,P}) -> test_snippet({integer_to_list(N),S,A},C,P)
+        end,
+        Snippets).
+
 test_snippet(Snippet, Expected, Plugins) ->
     {Conf, Advanced} = write_snippet(Snippet),
     rabbit_file:recursive_delete("generated"),
@@ -10,7 +19,13 @@ test_snippet(Snippet, Expected, Plugins) ->
     SchemaDir = rabbit_config:schema_dir(),
     {ok, GeneratedFile} = generate_config(Conf, Advanced, SchemaDir),
     {ok, [Generated]} = file:consult(GeneratedFile),
-    Expected = Generated.
+    Gen = deepsort(Generated),
+    Exp = deepsort(Expected),
+    case Exp of
+        Gen -> ok;
+        _         -> 
+            error({config_mismatch, Snippet, Exp, Gen})
+    end.
 
 write_snippet({Name, Config, Advanced}) ->
     rabbit_file:recursive_delete(filename:join(["examples", Name])),
@@ -38,6 +53,18 @@ prepare_schemas(Plugins) ->
 
 
 
+deepsort(List) ->
+    case is_proplist(List) of
+        true ->
+            lists:keysort(1, lists:map(fun({K,V}) -> {K, deepsort(V)};
+                                          (V) -> V end,
+                                       List));
+        false -> 
+            case is_list(List) of
+                true  -> lists:sort(List);
+                false -> List
+            end
+    end.
 
-    
-
+is_proplist([{_K,_V}|_] = List) -> lists:all(fun({_K,_V}) -> true; (_) -> false end, List);
+is_proplist(_) -> false.
