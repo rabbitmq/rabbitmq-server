0f865d893f90f32d350d67f6b4a599976a548100
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2016-03-14 05:54:15 +0000

    Using '/temp-queue' destination instead of '/queue' for temporary-queues

--- deps/rabbitmq_stomp/examples/perl/rabbitmq_stomp_rpc_client.pl
+++ deps/rabbitmq_stomp/examples/perl/rabbitmq_stomp_rpc_client.pl
@@ -4,12 +4,11 @@ use Net::Stomp;
 my $stomp = Net::Stomp->new({hostname=>'localhost', port=>'61613'});
 $stomp->connect({login=>'guest', passcode=>'guest'});
 
-my $private_q_name = "/queue/c-" . time() . "-" . rand();
+my $private_q_name = "/temp-queue/test";
 
-$stomp->subscribe({destination => $private_q_name});
 $stomp->send({destination => '/queue/rabbitmq_stomp_rpc_service',
               'reply-to' => $private_q_name,
               body => "request from $private_q_name"});
-print "Reply: " . $stomp->receive_frame->body;
+print "Reply: " . $stomp->receive_frame->body . "\n";
 
 $stomp->disconnect;
