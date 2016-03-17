925c782cc5570737a2de5b9739f59364a5567b4c
Brandon Shroyer <bdshroyer@gmail.com>
2016-03-17 15:17:27 +0000

    Implement list_users command

---
+++ deps/rabbitmq_cli/test/list_users_command_test.exs
@@ -0,0 +1,85 @@
+## The contents of this file are subject to the Mozilla Public License
+## Version 1.1 (the "License"); you may not use this file except in
+## compliance with the License. You may obtain a copy of the License
+## at http://www.mozilla.org/MPL/
+##
+## Software distributed under the License is distributed on an "AS IS"
+## basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+## the License for the specific language governing rights and
+## limitations under the License.
+##
+## The Original Code is RabbitMQ.
+##
+## The Initial Developer of the Original Code is GoPivotal, Inc.
+## Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
+
+
+defmodule ListUsersCommandTest do
+  use ExUnit.Case, async: false
+  import TestHelper
+  import ExUnit.CaptureIO
+
+  setup_all do
+    :net_kernel.start([:rabbitmqctl, :shortnames])
+    on_exit([], fn -> :net_kernel.stop() end)
+
+		std_result = [
+			[{:user,"guest"},{:tags,[:administrator]}],
+			[{:user,"user1"},{:tags,[]}]
+		]
+
+		{
+			:ok,
+			std_result: std_result
+		}
+  end
+
+	setup context do
+    :net_kernel.connect_node(context[:target])
+    add_user "user1", "password"
+
+		on_exit([],
+			fn -> delete_user "user1"
+      :erlang.disconnect_node(context[:target])
+		end)
+		{
+			:ok,
+			opts: %{node: context[:target], timeout: context[:test_timeout]},
+		}
+	end
+
+	test "On incorrect number of commands, print usage" do
+		assert capture_io(
+			fn -> ListUsersCommand.list_users(["extra"], %{}) end
+			) =~ ~r/Usage:\n/
+	end
+
+	@tag target: get_rabbit_hostname, test_timeout: :infinity
+	test "On a successful query, return an array of lists of tuples", context do
+		matches_found = ListUsersCommand.list_users([], context[:opts])
+
+		assert Enum.all?(matches_found, fn(user) ->
+			Enum.find(context[:std_result], fn(found) -> found == user end)
+		end)
+	end
+
+	test "On an invalid rabbitmq node, return a bad rpc" do
+		assert ListUsersCommand.list_users([], %{node: :jake@thedog, timeout: :infinity}) == {:badrpc, :nodedown}
+	end
+
+  @tag target: get_rabbit_hostname, test_timeout: 30
+  test "sufficiently long timeouts don't interfere with results", context do
+    # checks to ensure that all expected users are in the results
+    assert ListUsersCommand.list_users([], context[:opts])
+    |> Enum.all?(fn(user) ->
+      Enum.find(context[:std_result], fn(found) -> found == user end)
+    end)
+  end
+
+  @tag target: get_rabbit_hostname, test_timeout: 0
+  test "timeout causes command to return a bad RPC", context do
+    assert ListVhostsCommand.list_vhosts([], context[:opts]) == 
+      {:badrpc, :timeout}
+  end
+end
+
