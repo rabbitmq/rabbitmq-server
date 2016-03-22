8088945c1d7e43c22aeb1b340063e31f12265e62
Brandon Shroyer <bdshroyer@gmail.com>
2016-03-22 17:37:25 +0000

    Implement authenticate_user command

---
+++ deps/rabbitmq_cli/lib/authenticate_user_command.ex
@@ -0,0 +1,45 @@
+## The contents of this file are subject to the Mozilla Public License
+## Version 1.1 (the "License"); you may not use this file except in
+## compliance with the License. You may obtain a copy of the License
+## at http://www.mozilla.org/MPL/
+##
+## Software distributed under the License is distributed on an "AS IS"
+## basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+## the License for the specific language governing rights and
+## limitations under the License.
+##
+## The Original Code is RabbitMQ.
+##
+## The Initial Developer of the Original Code is GoPivotal, Inc.
+## Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
+
+
+defmodule AuthenticateUserCommand do
+
+  def authenticate_user([], _) do
+    HelpCommand.help
+    {:bad_argument, ["<missing>", "<missing>"]}
+  end
+
+  def authenticate_user([_user], _) do
+    HelpCommand.help
+    {:bad_argument, ["user", "<missing>"]}
+  end
+
+  def authenticate_user([_user, _password | rest], _) when length(rest) > 0 do
+    HelpCommand.help
+    {:bad_argument, rest}
+  end
+
+  def authenticate_user([user, password], %{node: node_name}) do
+    node_name
+    |> Helpers.parse_node
+    |> :rabbit_misc.rpc_call(
+      :rabbit_access_control,
+      :check_user_pass_login,
+      [user, password]
+    )
+  end
+
+  def usage, do: "authenticate_user <username> <password>"
+end
---
+++ deps/rabbitmq_cli/test/authenticate_user_command_test.exs
@@ -0,0 +1,91 @@
+## at http://www.mozilla.org/MPL/
+##
+## Software distributed under the License is distributed on an "AS IS"
+## basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+## the License for the specific language governing rights and
+## limitations under the License.
+##
+## The Original Code is RabbitMQ.
+##
+## The Initial Developer of the Original Code is GoPivotal, Inc.
+## Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
+
+
+defmodule AuthenticateUserCommandTest do
+  use ExUnit.Case, async: false
+  import ExUnit.CaptureIO
+  import TestHelper
+
+  @user     "user1"
+  @password "password"
+
+  setup_all do
+    :net_kernel.start([:rabbitmqctl, :shortnames])
+    :net_kernel.connect_node(get_rabbit_hostname)
+
+    on_exit([], fn ->
+      :erlang.disconnect_node(get_rabbit_hostname)
+      :net_kernel.stop()
+    end)
+
+    :ok
+  end
+
+  setup context do
+    add_user(@user, @password)
+    on_exit(context, fn -> delete_user(context[:user]) end)
+    {:ok, opts: %{node: get_rabbit_hostname}}
+  end
+
+  test "invalid arguments print return bad_argument" do
+    assert capture_io(fn ->
+      AuthenticateUserCommand.authenticate_user([], %{})
+    end) =~ ~r/Usage:\n/
+
+    capture_io(fn->
+      assert AuthenticateUserCommand.authenticate_user([], %{}) ==
+        {:bad_argument, ["<missing>", "<missing>"]}
+    end)
+
+    assert capture_io(fn ->
+      AuthenticateUserCommand.authenticate_user(["user"], %{})
+    end) =~ ~r/Usage:\n/
+
+    capture_io(fn->
+      assert AuthenticateUserCommand.authenticate_user(["user"], %{}) ==
+        {:bad_argument, ["user", "<missing>"]}
+    end)
+
+    assert capture_io(fn ->
+      AuthenticateUserCommand.authenticate_user(["user", "password", "extra"], %{})
+    end) =~ ~r/Usage:\n/
+
+    capture_io(fn->
+      assert AuthenticateUserCommand.authenticate_user(["user", "password", "extra"], %{}) ==
+        {:bad_argument, ["extra"]}
+    end)
+  end
+
+  @tag user: @user, password: @password
+  test "a valid username and password returns okay", context do
+    assert {:ok, _} = AuthenticateUserCommand.authenticate_user([context[:user], context[:password]], context[:opts])
+  end
+
+  test "An invalid rabbitmq node throws a badrpc" do
+    target = :jake@thedog
+    :net_kernel.connect_node(target)
+    opts = %{node: target}
+
+    assert AuthenticateUserCommand.authenticate_user(["user", "password"], opts) == {:badrpc, :nodedown}
+  end
+
+  @tag user: @user, password: "treachery"
+  test "a valid username and invalid password returns refused", context do
+    assert {:refused, _, _, _} = AuthenticateUserCommand.authenticate_user([context[:user], context[:password]], context[:opts])
+  end
+
+  @tag user: "interloper", password: @password
+  test "an invalid username returns refused", context do
+    assert {:refused, _, _, _} = AuthenticateUserCommand.authenticate_user([context[:user], context[:password]], context[:opts])
+  end
+end
