6d443bbae5ce570e3fdfd51088c0b7bbeef3c9ce
Loïc Hoguin <essen@ninenines.eu>
2016-04-05 16:28:49 +0200

    Add upgrade function adding listener socket options

--- src/rabbit_upgrade_functions.erl
+++ src/rabbit_upgrade_functions.erl
@@ -24,6 +24,7 @@
 -rabbit_upgrade({remove_user_scope,     mnesia, []}).
 -rabbit_upgrade({hash_passwords,        mnesia, []}).
 -rabbit_upgrade({add_ip_to_listener,    mnesia, []}).
+-rabbit_upgrade({add_opts_to_listener,  mnesia, [add_ip_to_listener]}).
 -rabbit_upgrade({internal_exchanges,    mnesia, []}).
 -rabbit_upgrade({user_to_internal_user, mnesia, [hash_passwords]}).
 -rabbit_upgrade({topic_trie,            mnesia, []}).
@@ -60,6 +61,7 @@
 -spec(remove_user_scope/0     :: () -> 'ok').
 -spec(hash_passwords/0        :: () -> 'ok').
 -spec(add_ip_to_listener/0    :: () -> 'ok').
+-spec(add_opts_to_listener/0  :: () -> 'ok').
 -spec(internal_exchanges/0    :: () -> 'ok').
 -spec(user_to_internal_user/0 :: () -> 'ok').
 -spec(topic_trie/0            :: () -> 'ok').
@@ -125,6 +127,14 @@ add_ip_to_listener() ->
               {listener, Node, Protocol, Host, {0,0,0,0}, Port}
       end,
       [node, protocol, host, ip_address, port]).
+
+add_opts_to_listener() ->
+    transform(
+      rabbit_listener,
+      fun ({listener, Node, Protocol, Host, IP, Port}) ->
+              {listener, Node, Protocol, Host, IP, Port, []}
+      end,
+      [node, protocol, host, ip_address, port, opts]).
 
 internal_exchanges() ->
     Tables = [rabbit_exchange, rabbit_durable_exchange],
