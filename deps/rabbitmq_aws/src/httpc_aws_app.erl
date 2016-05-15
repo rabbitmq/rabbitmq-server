055fb5ea5ccfddfbb8be7400ca3bc632ba3e00a3
Gavin M. Roy <gavinmroy@gmail.com>
2016-05-15 19:51:59 -0400

    Updated application cleanup and base app

---
+++ deps/rabbitmq_aws/src/httpc_aws_app.erl
@@ -0,0 +1,50 @@
+%% ====================================================================
+%% @author Gavin M. Roy <gavinmroy@gmail.com>
+%% @copyright 2016, Gavin M. Roy
+%% @doc httpc_aws OTP application
+%% @end
+%% ====================================================================
+-module(httpc_aws_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+%%%===================================================================
+%%% Application callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[1,2], and should start the processes of the
+%% application. If the application is structured according to the OTP
+%% design principles as a supervision tree, this means starting the
+%% top supervisor of the tree.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start(StartType :: normal | {takeover, node()} | {failover, node()},
+    StartArgs :: term()) ->
+  {ok, pid()} |
+  {ok, pid(), State :: term()} |
+  {error, Reason :: term()}).
+start(_StartType, _StartArgs) ->
+  case httpc_aws_sup:start_link() of
+    {ok, Pid} -> {ok, Pid};
+    Error -> Error
+  end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application has stopped. It
+%% is intended to be the opposite of Module:start/2 and should do
+%% any necessary cleaning up. The return value is ignored.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(stop(State :: term()) -> term()).
+stop(_State) ->
+  ok.
