16ea14fff1b5bcf5c1b749826581b6a038ba6b38
Gavin M. Roy <gavinmroy@gmail.com>
2016-05-16 01:00:39 -0400

    Add a simple XML parser
    Looks like some AWS APIs dont speak JSON. \o/

---
+++ deps/rabbitmq_aws/src/httpc_aws_xml.erl
@@ -0,0 +1,26 @@
+%% ====================================================================
+%% @author Gavin M. Roy <gavinmroy@gmail.com>
+%% @copyright 2016, Gavin M. Roy
+%% @doc httpc_aws supervisor for the gen_server process
+%% @end
+%% ====================================================================
+-module(httpc_aws_xml).
+
+-export([parse/1]).
+
+-include_lib("xmerl/include/xmerl.hrl").
+
+parse(Value) ->
+  {Element, _} = xmerl_scan:string(Value),
+  parse_node(Element).
+
+parse_node(#xmlElement{name=Name, content=Content}) ->
+  [{list_to_binary(atom_to_list(Name)), parse_content(Content, [])}].
+
+parse_content([], Value) -> Value;
+parse_content(#xmlElement{} = Element, Accum) ->
+  lists:append(parse_node(Element), Accum);
+parse_content(#xmlText{value=Value}, _) ->
+  list_to_binary(Value);
+parse_content([H|T], Accum) ->
+  parse_content(T, parse_content(H, Accum)).
