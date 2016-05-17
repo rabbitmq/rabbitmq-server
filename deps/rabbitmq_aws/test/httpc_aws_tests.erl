0c46d745508ad89e19cd185b1c06a221b66c46c3
Gavin M. Roy <gavinr@aweber.com>
2016-05-17 00:10:22 -0400

    Start httpc_aws tests

--- deps/rabbitmq_aws/test/httpc_aws_tests.erl
+++ deps/rabbitmq_aws/test/httpc_aws_tests.erl
@@ -4,3 +4,11 @@
 
 -include("httpc_aws.hrl").
 
+
+endpoint_test_() ->
+  [
+    {"dynamodb service", fun() ->
+      Expectation = "dynamodb.us-west-2.amazonaws.com",
+      ?assertEqual(Expectation, httpc_aws:endpoint("us-west-2", "dynamodb"))
+     end}
+  ].
