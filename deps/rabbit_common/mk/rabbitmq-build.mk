1c63c8ba5afb1f3b9b0e9ed154c2518ea59eedba
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2016-05-30 18:01:47 +0200

    rabbitmq-build.mk: Copy Erlang.mk default compiler flags
    rabbitmq-build.mk is loaded as a plugin by Erlang.mk, before those
    default flags are defined. And because Erlang.mk uses '?=', the flags we
    set here in our Makefile override the default set.

--- deps/rabbit_common/mk/rabbitmq-build.mk
+++ deps/rabbit_common/mk/rabbitmq-build.mk
@@ -1,3 +1,14 @@
+# FIXME: We copy Erlang.mk default flags here: rabbitmq-build.mk is
+# loaded as a plugin, so before those variables are defined. And because
+# Erlang.mk uses '?=', the flags we set here override the default set.
+
+WARNING_OPTS += +debug_info \
+		+warn_export_vars \
+		+warn_shadow_vars \
+		+warn_obsolete_guard
+ERLC_OPTS += -Werror $(WARNING_OPTS)
+TEST_ERLC_OPTS += $(WARNING_OPTS)
+
 # Add Lager parse_transform module and our default Lager extra sinks.
 LAGER_EXTRA_SINKS += rabbit_log \
 		     rabbit_log_channel \
