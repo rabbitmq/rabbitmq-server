1b1534f86bfe5cf113f2ef3cf249e6c4a3e09b6e
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2016-06-06 16:02:58 +0200

    Implement the `join_cluster` command
    Compared to the Erlang-based rabbitmqctl's command, this one accepts
    `--disc` to specify the node type. In the implementation, it does
    nothing because that's the default value already. It is only to improve
    consistency, because there is a `--ram` flag.
    
    The testsuite for this command is incomplete in this commit. When a
    testcase will be able to start and stop multiple nodes, the testsuite
    will need to be completed.
    
    [#116551629]

--- deps/rabbitmq_cli/lib/standard_codes.ex
+++ deps/rabbitmq_cli/lib/standard_codes.ex
@@ -23,6 +23,7 @@ defmodule StandardCodes do
   def map_to_standard_code({:refused, _, _, _} = input), do: input
   def map_to_standard_code({:bad_option, _} = input), do: input
   def map_to_standard_code({:error, _} = input), do: input
+  def map_to_standard_code({:join_cluster_failed, _} = input), do: input
   def map_to_standard_code({:reset_failed, _} = input), do: input
   def map_to_standard_code({:validation_failure, _} = input), do: input
   def map_to_standard_code({:healthcheck_failed, _} = input), do: input
