ddf191dea93062ffdc2eac03e438294815efb7b3
Michael Klishin <michael@clojurewerkz.org>
2016-06-09 02:26:52 +0300

    Adapt to master

--- deps/rabbitmq_cli/test/sync_queue_command_test.exs
+++ deps/rabbitmq_cli/test/sync_queue_command_test.exs
@@ -22,7 +22,7 @@ defmodule SyncQueueCommandTest do
   @vhost "/"
 
   setup_all do
-    :net_kernel.start([:rabbitmqctl, :shortnames])
+    RabbitMQ.CLI.Distribution.start()
     :net_kernel.connect_node(get_rabbit_hostname)
 
     start_rabbitmq_app
