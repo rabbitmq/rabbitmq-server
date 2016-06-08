34961e7fed5b2b86794a117e0da8573d38a448c3
Michael Klishin <michael@clojurewerkz.org>
2016-06-08 03:32:41 +0300

    Implement sync_queue and cancel_sync_queue commands
    Happy path tests need a multi-node testing framework.

---
+++ deps/rabbitmq_cli/test/cancel_sync_command_test.exs
@@ -0,0 +1,78 @@
+## The contents of this file are subject to the Mozilla Public License
+## Version 1.1 (the "License"); you may not use this file except in
+## compliance with the License. You may obtain a copy of the License
+## at http://www.mozilla.org/MPL/
+##
+## Software distributed under the License is distributed on an "AS IS"
+## basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+## the License for the specific language governing rights and
+## limitations under the License.
+##
+## The Original Code is RabbitMQ.
+##
+## The Initial Developer of the Original Code is Pivotal Software, Inc.
+## Copyright (c) 2016 Pivotal Software, Inc.  All rights reserved.
+
+defmodule CancelSyncQueueCommandTest do
+  use ExUnit.Case, async: false
+  import TestHelper
+
+  @command CancelSyncQueueCommand
+
+  @vhost "/"
+
+  setup_all do
+    :net_kernel.start([:rabbitmqctl, :shortnames])
+    :net_kernel.connect_node(get_rabbit_hostname)
+
+    start_rabbitmq_app
+
+    on_exit([], fn ->
+      start_rabbitmq_app
+      :erlang.disconnect_node(get_rabbit_hostname)
+      :net_kernel.stop()
+    end)
+
+    :ok
+  end
+
+  setup do
+    {:ok, opts: %{
+      node: get_rabbit_hostname,
+      vhost: @vhost
+    }}
+  end
+
+  test "validate: specifying no queue name is reported as an error", context do
+    assert @command.validate([], context[:opts]) ==
+      {:validation_failure, :not_enough_args}
+  end
+
+  test "validate: specifying two queue names is reported as an error", context do
+    assert @command.validate(["q1", "q2"], context[:opts]) ==
+      {:validation_failure, :too_many_args}
+  end
+
+  test "validate: specifying three queue names is reported as an error", context do
+    assert @command.validate(["q1", "q2", "q3"], context[:opts]) ==
+      {:validation_failure, :too_many_args}
+  end
+
+  test "validate: specifying one queue name succeeds", context do
+    assert @command.validate(["q1"], context[:opts]) == :ok
+  end
+
+  test "run: request to a non-existent RabbitMQ node returns a nodedown" do
+    target = :jake@thedog
+    :net_kernel.connect_node(target)
+    opts = %{node: target, vhost: @vhost}
+    assert match?({:badrpc, :nodedown}, @command.run(["q1"], opts))
+  end
+
+  test "banner", context do
+    s = @command.banner(["q1"], context[:opts])
+
+    assert s =~ ~r/Stopping synchronising queue/
+    assert s =~ ~r/q1/
+  end
+end
