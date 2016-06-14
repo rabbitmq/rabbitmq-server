aa9b9eacb24100abb6c5732a59567c49d563581a
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2016-06-14 10:44:39 +0200

    rabbit_ct_broker_helpers: Create plugins' .ez files before starting broker

--- deps/rabbit_common/src/rabbit_ct_broker_helpers.erl
+++ deps/rabbit_common/src/rabbit_ct_broker_helpers.erl
@@ -355,7 +355,7 @@ do_start_rabbitmq_node(Config, NodeConfig, _I) ->
             end,
             StartArgs0 ++ " -kernel net_ticktime " ++ integer_to_list(Ticktime)
     end,
-    Cmd = ["start-background-broker",
+    Cmd = ["test-dist", "start-background-broker",
       {"PLUGINS=~s", [Plugins]},
       {"RABBITMQ_NODENAME=~s", [Nodename]},
       {"RABBITMQ_NODENAME_FOR_PATHS=~s", [InitialNodename]},
