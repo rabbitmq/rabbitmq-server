e1ed2a4409fc8a1bb9f068d98eb5dd84446ce144
Michael Klishin <michael@clojurewerkz.org>
2016-06-27 11:34:39 +0300

    Revert "Add rabbitmq [server] dependency"
    This reverts commit 35012a15dcc0aa2d569df796b68e9e3bea99de76.

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -79,13 +79,13 @@ defmodule RabbitMQCtl.MixfileBase do
     make = find_gnu_make()
 
     [
-      {
-        :rabbit,
-        git: "https://github.com/rabbitmq/rabbitmq-server.git",
-        branch: "master",
-        compile: make,
-        only: :test
-      },
+      # {
+      #   :rabbit,
+      #   git: "https://github.com/rabbitmq/rabbitmq-server.git",
+      #   branch: "master",
+      #   compile: make,
+      #   only: :test
+      # },
       {
         :rabbit_common,
         git: "https://github.com/rabbitmq/rabbitmq-common.git",
