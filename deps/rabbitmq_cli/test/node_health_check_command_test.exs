909a3b41fe40e82d5533fa6ea6cef25e6c757d7b
Michael Klishin <michael@clojurewerkz.org>
2016-07-15 23:47:15 +0300

    Adapt to recent server rabbit_health_check changes

--- deps/rabbitmq_cli/test/node_health_check_command_test.exs
+++ deps/rabbitmq_cli/test/node_health_check_command_test.exs
@@ -36,7 +36,7 @@ defmodule NodeHealthCheckCommandTest do
   end
 
   setup do
-    {:ok, opts: %{node: get_rabbit_hostname}}
+    {:ok, opts: %{node: get_rabbit_hostname, timeout: 70000}}
   end
 
   test "validate: with extra arguments returns an arg count error", context do
@@ -70,7 +70,7 @@ defmodule NodeHealthCheckCommandTest do
     target = :jake@thedog
     :net_kernel.connect_node(target)
 
-    assert match?({:badrpc, :nodedown}, @command.run([], %{node: target}))
+    assert match?({:badrpc, :nodedown}, @command.run([], %{node: target, timeout: 70000}))
   end
 
   test "banner", context do
