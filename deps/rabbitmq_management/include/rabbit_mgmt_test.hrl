035dcc30b6f4a9317aab56e6ac3377850b139f73
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2016-07-20 14:51:05 +0200

    rabbit_mgmt_test.hrl: Remove definition of ?PREFIX
    The URL must be queried using rabbit_ct_broker_helpers:node_uri/3
    instead.

--- deps/rabbitmq_management/include/rabbit_mgmt_test.hrl
+++ deps/rabbitmq_management/include/rabbit_mgmt_test.hrl
@@ -7,6 +7,5 @@
 -define(BAD_REQUEST, 400).
 -define(NOT_AUTHORISED, 401).
 %%-define(NOT_FOUND, 404). Defined for AMQP by amqp_client.hrl (as 404)
--define(PREFIX, "http://localhost:15672/api").
 %% httpc seems to get racy when using HTTP 1.1
 -define(HTTPC_OPTS, [{version, "HTTP/1.0"}]).
