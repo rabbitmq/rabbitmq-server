a84cd93d2e6ca4dbe2ce6aa22db9fecf439701f2
Daniil Fedotov <dfedotov@pivotal.io>
2016-07-22 17:17:15 +0100

    Implement eval command

---
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/eval_command.ex
@@ -0,0 +1,76 @@
+## The contents of this file are subject to the Mozilla Public License
+## Version 1.1 (the "License"); you may not use this file except in
+## compliance with the License. You may obtain a copy of the License
+## at http://www.mozilla.org/MPL/
+##
+## Software distributed under the License is distributed on an "AS IS"
+## basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+## the License for the specific language governing rights and
+## limitations under the License.
+##
+## The Original Code is RabbitMQ.
+##
+## The Initial Developer of the Original Code is GoPivotal, Inc.
+## Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
+
+
+defmodule RabbitMQ.CLI.Ctl.Commands.EvalCommand do
+  @behaviour RabbitMQ.CLI.CommandBehaviour
+
+  def merge_defaults(args, opts), do: {args, opts}
+
+  def switches(), do: []
+  def aliases(), do: []
+
+  def validate([], _) do
+    {:validation_failure, :not_enough_args}
+  end
+
+  def validate(args, _) when length(args) > 1 do
+    {:validation_failure, :too_many_args}
+  end
+
+  def validate([""], _) do
+    {:validation_failure, "Expression could not be empty"}
+  end
+
+  def validate([expr], _) do
+    case parse_expr(expr) do
+      {:ok, _} -> :ok;
+      {:error, err} -> {:validation_failure, err}
+    end
+  end
+
+  def validate([_,_], _), do: :ok
+
+  def run([expr],  %{node: node_name}) do
+    {:ok, parsed} = parse_expr(expr)
+    {:value, value, _} = :rabbit_misc.rpc_call(node_name, 
+                                               :erl_eval, :exprs, [parsed, []])
+    {:ok, value}
+  end
+
+  def usage, do: "eval <expr>"
+
+  def banner(_, _), do: nil
+
+  def flags(), do: []
+
+  def parse_expr(expr) do
+    expr_str = to_char_list(expr)
+    case :erl_scan.string(expr_str) do
+      {:ok, scanned, _} ->
+        case :erl_parse.parse_exprs(scanned) do
+          {:ok, parsed} -> {:ok, parsed};
+          {:error, err} -> {:error, format_parse_error(err)}
+        end;
+      {:error, err, _}  ->
+        {:error, format_parse_error(err)}
+    end
+  end
+
+  def format_parse_error({_line, mod, err}) do
+    to_string(:lists.flatten(mod.format_error(err)))
+  end
+
+end
---
+++ deps/rabbitmq_cli/test/eval_command_test.exs
@@ -0,0 +1,73 @@
+## The contents of this file are subject to the Mozilla Public License
+## Version 1.1 (the "License"); you may not use this file except in
+## compliance with the License. You may obtain a copy of the License
+## at http://www.mozilla.org/MPL/
+##
+## Software distributed under the License is distributed on an "AS IS"
+## basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+## the License for the specific language governing rights and
+## limitations under the License.
+##
+## The Original Code is RabbitMQ.
+##
+## The Initial Developer of the Original Code is GoPivotal, Inc.
+## Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
+
+
+defmodule EvalCommandTest do
+  use ExUnit.Case, async: false
+  import TestHelper
+  import ExUnit.CaptureIO
+
+  @command RabbitMQ.CLI.Ctl.Commands.EvalCommand
+
+  setup_all do
+    RabbitMQ.CLI.Distribution.start()
+    :net_kernel.connect_node(get_rabbit_hostname)
+
+    on_exit([], fn ->
+      :erlang.disconnect_node(get_rabbit_hostname)
+      :net_kernel.stop()
+    end)
+
+    :ok
+  end
+
+  setup _ do
+    {:ok, opts: %{node: get_rabbit_hostname}}
+  end
+
+  test "validate: on an inappropriate number of arguments, validate should return an arg count error" do
+    assert @command.validate([], %{}) == {:validation_failure, :not_enough_args}
+    assert @command.validate(["too", "many"], %{}) == {:validation_failure, :too_many_args}
+    assert @command.validate(["three", "too", "many"], %{}) == {:validation_failure, :too_many_args}
+  end
+
+  test "validate: report empty expression" do
+    assert @command.validate([""], %{}) == {:validation_failure, "Expression could not be empty"}
+  end
+
+  test "validate: Syntax error in command" do
+    assert @command.validate(["foo bar"], %{}) == {:validation_failure, "syntax error before: bar"}
+  end
+
+  test "run: executes erlang code", context do
+    assert @command.run(["foo."], context[:opts]) == {:ok, :foo}
+    assert @command.run(["length([1,2,3])."], context[:opts]) == {:ok, 3}
+    assert @command.run(["lists:sum([1,2,3])."], context[:opts]) == {:ok, 6}
+    {:ok, apps} = @command.run(["application:loaded_applications()."], context[:opts])
+    assert is_list(apps)
+  end
+
+  test "run: executes on rabbit node", context do
+    {:ok, apps} = @command.run(["application:loaded_applications()."], context[:opts])
+    assert is_list(apps)
+    assert List.keymember?(apps, :rabbit, 0)
+  end
+
+  test "run: returns stdout", context do
+    assert capture_io(fn -> 
+      assert @command.run(["io:format(\"output\")."], context[:opts]) == {:ok, :ok}
+    end) == "output"
+  end
+end
