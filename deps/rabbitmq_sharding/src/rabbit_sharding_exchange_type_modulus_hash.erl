dc0bf77cb602a097f0c068afd06e38b776fdbefc
Daniil Fedotov <dfedotov@pivotal.io>
2016-08-30 14:47:48 +0100

    Stubs for exchange specific info

--- deps/rabbitmq_sharding/src/rabbit_sharding_exchange_type_modulus_hash.erl
+++ deps/rabbitmq_sharding/src/rabbit_sharding_exchange_type_modulus_hash.erl
@@ -20,7 +20,7 @@
 
 -behaviour(rabbit_exchange_type).
 
--export([description/0, serialise_events/0, route/2]).
+-export([description/0, serialise_events/0, route/2, info/1, info/2]).
 -export([validate/1, validate_binding/2,
          create/2, delete/3, policy_changed/2,
          add_binding/3, remove_bindings/3, assert_args_equivalence/2]).
@@ -49,6 +49,10 @@ route(#exchange{name = Name},
         0 -> [];
         N -> [lists:nth(hash_mod(Routes, N), Qs)]
     end.
+
+info(_) -> [].
+
+info(_, _) -> [].
 
 validate(_X) -> ok.
 validate_binding(_X, _B) -> ok.
