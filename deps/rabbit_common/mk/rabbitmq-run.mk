86b5cd1cff2921ddf5ec9436048f111aa635af10
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2016-09-19 12:38:30 +0200

    rabbitmq-run.mk: Query the location of `erl_call`
    This has two advantages:
        o  We don't have to rely on a package ability to put `erl_call` in
           the $PATH.
        o  We are sure that `erl_call` comes from the same Erlang as `erl`.
    
    [#130099459]

--- deps/rabbit_common/mk/rabbitmq-run.mk
+++ deps/rabbit_common/mk/rabbitmq-run.mk
@@ -104,7 +104,7 @@ BASIC_SCRIPT_ENV_SETTINGS = \
 	$(call basic_script_env_settings,$(RABBITMQ_NODENAME),$(RABBITMQ_NODENAME_FOR_PATHS),$(RABBITMQ_NODE_PORT)) \
 	RABBITMQ_ENABLED_PLUGINS_FILE="$(RABBITMQ_ENABLED_PLUGINS_FILE)"
 
-ERL_CALL ?= erl_call
+ERL_CALL := $(shell $(ERL) -eval 'io:format("~s~n", [filename:join(code:lib_dir(erl_interface, bin), case os:type() of {win32, _} -> "erl_call.exe"; _ -> "erl_call" end)]), halt().')
 ERL_CALL_OPTS ?= -sname $(RABBITMQ_NODENAME) -e
 
 test-tmpdir:
