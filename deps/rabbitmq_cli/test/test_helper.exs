97035a2c9c2af29d4e0356748820e35577d156b0
Daniil Fedotov <dfedotov@pivotal.io>
2016-11-07 15:09:01 +0000

    Vhost limits test helpers

--- deps/rabbitmq_cli/test/test_helper.exs
+++ deps/rabbitmq_cli/test/test_helper.exs
@@ -311,7 +311,7 @@ defmodule TestHelper do
     {:ok, plugins_dir} = :rabbit_misc.rpc_call(node,
                                                :application, :get_env,
                                                [:rabbit, :plugins_dir])
-    {:ok, rabbitmq_home} = :rabbit_misc.rpc_call(node, :file, :get_cwd, [])
+    rabbitmq_home = :rabbit_misc.rpc_call(node, :code, :lib_dir, [:rabbit])
     {:ok, [_enabled_plugins]} = :file.consult(plugins_file)
 
     opts = %{enabled_plugins_file: plugins_file,
@@ -325,5 +325,18 @@ defmodule TestHelper do
       false ->
         set_enabled_plugins(get_rabbit_hostname, plugins ++ [:rabbitmq_federation], opts)
     end
+  end
+
+  def set_vhost_limits(vhost, limits) do
+    :rpc.call(get_rabbit_hostname,
+              :rabbit_vhost_limit, :parse_set, [vhost, limits])
+  end
+  def get_vhost_limits(vhost) do
+    :rpc.call(get_rabbit_hostname, :rabbit_vhost_limit, :list, [vhost])
+    |> Map.new
+  end
+
+  def clear_vhost_limits(vhost) do
+    :rpc.call(get_rabbit_hostname, :rabbit_vhost_limit, :clear, [vhost])
   end
 end
