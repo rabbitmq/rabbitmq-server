fbe7b90ab35c0daf69c5a196af4faa94ce57acc9
Daniil Fedotov <dfedotov@pivotal.io>
2017-01-18 15:22:11 +0000

    Make sure completion call has locale var

--- scripts/rabbitmqctl-autocomplete.sh
+++ scripts/rabbitmqctl-autocomplete.sh
@@ -1,7 +1,8 @@
-if which rabbitmqctl; then
+if which rabbitmqctl > /dev/null; then
     if [ -n "$BASH_VERSION" ]; then
       _rabbitmqctl_complete() {
           COMPREPLY=()
+          local LANG=en_US.UTF-8
           local word="${COMP_WORDS[COMP_CWORD]}"
           local completions="$(/usr/lib/rabbitmq/bin/rabbitmqctl --auto-complete "$COMP_LINE")"
           COMPREPLY=( $(compgen -W "$completions" -- "$word") )
@@ -11,6 +12,7 @@ if which rabbitmqctl; then
     elif [ -n "$ZSH_VERSION" ]; then
       _rabbitmqctl_complete() {
           local word completions a
+          local LANG=en_US.UTF-8
           read -cl a
           word="$1"
           completions="$(/usr/lib/rabbitmq/bin/rabbitmqctl --auto-complete "${a}")"
