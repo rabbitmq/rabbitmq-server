9983b0b6961cced4093934334c3ac4a7d33a84e0
Daniil Fedotov <dfedotov@pivotal.io>
2017-01-26 14:09:08 +0000

    Ignore pre-release version part in plugin version requirement validation

--- src/rabbit_plugins.erl
+++ src/rabbit_plugins.erl
@@ -340,7 +340,10 @@ check_plugins_versions(PluginName, AllPlugins, RequiredVersions) ->
 is_version_supported("", _)        -> true;
 is_version_supported("0.0.0", _)   -> true;
 is_version_supported(_Version, []) -> true;
-is_version_supported(Version, ExpectedVersions) ->
+is_version_supported(VersionFull, ExpectedVersions) ->
+    %% Pre-release version should be supported in plugins,
+    %% therefore alpha part should be removed
+    Version = remove_version_alpha_part(VersionFull),
     case lists:any(fun(ExpectedVersion) ->
                        rabbit_misc:version_minor_equivalent(ExpectedVersion, Version)
                        andalso
@@ -350,6 +353,10 @@ is_version_supported(Version, ExpectedVersions) ->
         true  -> true;
         false -> false
     end.
+
+remove_version_alpha_part(Version) ->
+    {Ver, _Alpha} = rabbit_semver:parse(Version),
+    iolist_to_binary(rabbit_semver:format({Ver, {[], []}})).
 
 clean_plugins(Plugins) ->
     {ok, ExpandDir} = application:get_env(rabbit, plugins_expand_dir),
