cd804aeb4ad40035e94a52fce4e720ddedfe139f
Daniil Fedotov <dfedotov@pivotal.io>
2017-01-30 17:54:41 +0000

    Check for rabbitmqctl using `[ -x ]` and not `which` in autocompletion

--- scripts/zsh_autocomplete.sh
+++ scripts/zsh_autocomplete.sh
@@ -1,10 +1,12 @@
 _rabbitmqctl_complete() {
-    local word completions a
-    local LANG=en_US.UTF-8
-    read -cl a
-    word="$1"
-    completions="$(export LANG=en_US.UTF-8; export LC_CTYPE=en_US.UTF-8;  /usr/lib/rabbitmq/bin/rabbitmqctl --auto-complete ${=a})"
-    reply=( "${(ps:\n:)completions}" )
+    if [ -x /usr/lib/rabbitmq/bin/rabbitmqctl ]; then
+        local word completions a
+        local LANG=en_US.UTF-8
+        read -cl a
+        word="$1"
+        completions="$(export LANG=en_US.UTF-8; export LC_CTYPE=en_US.UTF-8; /usr/lib/rabbitmq/bin/rabbitmqctl --auto-complete ${=a})"
+        reply=( "${(ps:\n:)completions}" )
+    fi
 }
 
 compctl -f -K _rabbitmqctl_complete rabbitmqctl
