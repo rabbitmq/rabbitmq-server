78e8e302b7aeb055f3b58bbb7cfe04bc9badd4e1
Michael Klishin <mklishin@pivotal.io>
2017-02-01 17:46:01 +0300

    Switch to rabbit_json:decode/1

--- deps/rabbitmq_federation_management/test/federation_mgmt_SUITE.erl
+++ deps/rabbitmq_federation_management/test/federation_mgmt_SUITE.erl
@@ -252,7 +252,7 @@ assert_code(CodeExp, CodeAct, Type, Path, Body) ->
     end.
 
 decode(?OK, _Headers,  ResBody) ->
-    cleanup(rabbit_json:decode(list_to_binary(ResBody), [return_maps]));
+    cleanup(rabbit_json:decode(rabbit_data_coercion:to_binary(ResBody)));
 decode(_,    Headers, _ResBody) -> Headers.
 
 cleanup(L) when is_list(L) ->
