a41d82973371dc0cdcbdde62c17a205f3733e1ed
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-02-02 15:43:08 +0100

    Add license to Heroku's Ranch Proxy Protocol files
    References rabbitmq/rabbitmq-server#589

--- deps/rabbit_common/include/ranch_proxy.hrl
+++ deps/rabbit_common/include/ranch_proxy.hrl
@@ -1,3 +1,33 @@
+%% Copyright (c) 2012, Heroku Inc.
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are
+%% met:
+%%
+%% * Redistributions of source code must retain the above copyright
+%%   notice, this list of conditions and the following disclaimer.
+%%
+%% * Redistributions in binary form must reproduce the above copyright
+%%   notice, this list of conditions and the following disclaimer in the
+%%   documentation and/or other materials provided with the distribution.
+%%
+%% * The names of its contributors may not be used to endorse or promote
+%%   products derived from this software without specific prior written
+%%   permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%% A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%% OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+%% LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+%% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 %%% proxy2 defines
 -define(HEADER, "\r\n\r\n\0\r\nQUIT\n").
 -define(VSN, 16#02).
@@ -35,3 +65,8 @@
     source_port :: inet:port_number(),
     dest_port :: inet:port_number(),
     connection_info = []}).
+
+-record(ssl_socket, { upgraded = false :: boolean(),
+    proxy_socket :: ranch_proxy_protocol:proxy_socket(),
+    sslopts :: ranch_ssl:opts()
+}).
--- deps/rabbit_common/src/ranch_proxy_protocol.erl
+++ deps/rabbit_common/src/ranch_proxy_protocol.erl
@@ -1,3 +1,33 @@
+%% Copyright (c) 2012, Heroku Inc.
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are
+%% met:
+%%
+%% * Redistributions of source code must retain the above copyright
+%%   notice, this list of conditions and the following disclaimer.
+%%
+%% * Redistributions in binary form must reproduce the above copyright
+%%   notice, this list of conditions and the following disclaimer in the
+%%   documentation and/or other materials provided with the distribution.
+%%
+%% * The names of its contributors may not be used to endorse or promote
+%%   products derived from this software without specific prior written
+%%   permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%% A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%% OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+%% LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+%% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 %%% @copyright (C) 2015, Heroku
 %%% @doc Ranch protocol handling for the HA Proxy PROXY protocol [http://www.haproxy.org/download/1.5/doc/proxy-protocol.txt]
 %%% @end
@@ -130,7 +160,6 @@ accept(Transport, #proxy_socket{lsocket = LSocket,
             ok = setopts(Transport, ProxySocket, [{active, once}, {packet, line}]),
             receive
                 {_, CSocket, <<"PROXY ", ProxyInfo/binary>>} ->
-                    io:format("PROXY ~p~n", [ProxyInfo]),
                     case parse_proxy_protocol_v1(ProxyInfo) of
                         {InetVersion, SourceAddress, DestAddress, SourcePort, DestPort} ->
                             reset_socket_opts(Transport, ProxySocket, Opts, DefaultValuesOfModifiedOptions),
