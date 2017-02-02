732d3e1412a64eb0c11b916f5d814e9a7a3a08c9
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2017-02-02 17:17:20 +0100

    Update rabbitmq-components.mk

--- deps/rabbitmq_stomp/rabbitmq-components.mk
+++ deps/rabbitmq_stomp/rabbitmq-components.mk
@@ -100,7 +100,7 @@ dep_rabbitmq_public_umbrella          = git_rmq rabbitmq-public-umbrella $(curre
 # all projects use the same versions. It avoids conflicts and makes it
 # possible to work with rabbitmq-public-umbrella.
 
-dep_cowboy_commit = 1.0.4
+dep_cowboy_commit = 1.1.0
 dep_mochiweb = git git://github.com/basho/mochiweb.git v2.9.0p2
 dep_ranch_commit = 1.3.1
 dep_webmachine_commit = 1.10.8p2
