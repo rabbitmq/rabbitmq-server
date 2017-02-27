682a13633dcc155f7e807e5985b01e56c527f7f9
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2017-02-27 10:44:47 +0100

    Update rabbitmq-components.mk

--- rabbitmq-components.mk
+++ rabbitmq-components.mk
@@ -104,6 +104,8 @@ dep_rabbitmq_public_umbrella          = git_rmq rabbitmq-public-umbrella $(curre
 
 dep_cowboy_commit = 1.0.4
 dep_mochiweb = git git://github.com/basho/mochiweb.git v2.9.0p2
+# Last commit of PropEr supporting Erlang R16B03.
+dep_proper_commit = 735d972758d8bd85b12483626fe1b66450d6a6fe
 dep_ranch_commit = 1.3.1
 dep_webmachine_commit = 1.10.8p2
 
