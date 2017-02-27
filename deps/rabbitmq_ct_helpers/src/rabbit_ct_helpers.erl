rabbitmq/rabbitmq-server

commit 9807500a0205517b5160a1c8e8c88f6ca07907ff

rabbit_ct_helpers:exec(): Pass binary args directory open_port()
... instead of converting them to strings. That way, we are sure to not
mess with character encoding.

deps/rabbitmq_ct_helpers/src/rabbit_ct_helpers.erl
@@ -493,7 +493,10 @@ exec([Cmd | Args], Options) when is_list(Cmd) orelse is_binary(Cmd) ->
                 "~n")
             }
     end,
-    ct:pal(?LOW_IMPORTANCE, Log1, [string:join([Cmd1 | Args1], " "), self()]),
+    %% Because Args1 may contain binaries, we don't use string:join().
+    %% Instead we do a list comprehension.
+    ArgsIoList = [Cmd1, [[$\s, Arg] || Arg <- Args1]],
+    ct:pal(?LOW_IMPORTANCE, Log1, [ArgsIoList, self()]),
     try
         Port = erlang:open_port(
           {spawn_executable, Cmd1}, [
@@ -512,8 +515,6 @@ format_arg({Format, FormatArgs}) ->
     rabbit_misc:format(Format, FormatArgs);
 format_arg(Arg) when is_atom(Arg) ->
     atom_to_list(Arg);
-format_arg(Arg) when is_binary(Arg) ->
-    binary_to_list(Arg);
 format_arg(Arg) ->
     Arg.
 

commit 1faa985710d639cbb4af366386f65613e545ed3a

rabbit_ct_helpers: Make sure env. variables are strings
... not binaries. This was a regression in commit
9807500a0205517b5160a1c8e8c88f6ca07907ff. This led to erlang:open_port()
throwing badarg.

deps/rabbitmq_ct_helpers/src/rabbit_ct_helpers.erl
@@ -481,7 +481,11 @@ exec([Cmd | Args], Options) when is_list(Cmd) orelse is_binary(Cmd) ->
               begin
                   Key1 = format_arg(Key),
                   Value1 = format_arg(Value),
-                  {Key1, Value1}
+                  Value2 = case is_binary(Value1) of
+                               true  -> binary_to_list(Value1);
+                               false -> Value1
+                           end,
+                  {Key1, Value2}
               end
               || {Key, Value} <- Env
             ],