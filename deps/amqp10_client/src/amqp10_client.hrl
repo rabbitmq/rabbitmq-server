9e01f3f8e5dc8ab60dd969404388af0f7c378611
kjnilsson <knilsson@pivotal.io>
2017-04-03 15:46:53 +0100

    move hrl file as not meant to be shared

---
+++ deps/amqp10_client/src/amqp10_client.hrl
@@ -0,0 +1,18 @@
+-define(AMQP_PROTOCOL_HEADER, <<"AMQP", 0, 1, 0, 0>>).
+-define(SASL_PROTOCOL_HEADER, <<"AMQP", 3, 1, 0, 0>>).
+-define(MIN_MAX_FRAME_SIZE, 512).
+-define(MAX_MAX_FRAME_SIZE, 4294967295).
+-define(FRAME_HEADER_SIZE, 8).
+
+
+-define(debug, true).
+-ifdef(debug).
+-define(DBG(F, A), error_logger:info_msg(F, A)).
+-else.
+-define(DBG(F, A), ok).
+-endif.
+
+-record(link_ref, {role :: sender | receiver, session :: pid(),
+                   link_handle :: non_neg_integer()}).
+
+-type result(Succ, Err) :: {ok, Succ} | {error, Err}.
