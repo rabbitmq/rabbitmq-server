5c18bcf8b014420fe67264bf4cbdcabe92eab0f9
Daniil Fedotov <dfedotov@pivotal.io>
2017-04-05 12:00:32 +0100

    Test shutdown command validation.

---
+++ deps/rabbitmq_cli/test/shutdown_command_test.exs
@@ -0,0 +1,59 @@
+## The contents of this file are subject to the Mozilla Public License
+## Version 1.1 (the "License"); you may not use this file except in
+## compliance with the License. You may obtain a copy of the License
+## at http://www.mozilla.org/MPL/
+##
+## Software distributed under the License is distributed on an "AS IS"
+## basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+## the License for the specific language governing rights and
+## limitations under the License.
+##
+## The Original Code is RabbitMQ.
+##
+## The Initial Developer of the Original Code is GoPivotal, Inc.
+## Copyright (c) 2007-2017 Pivotal Software, Inc.  All rights reserved.
+
+
+defmodule ShutdownCommandTest do
+  use ExUnit.Case, async: false
+  import TestHelper
+
+  @command RabbitMQ.CLI.Ctl.Commands.ShutdownCommand
+
+  setup_all do
+    RabbitMQ.CLI.Core.Distribution.start()
+    :net_kernel.connect_node(get_rabbit_hostname())
+
+    on_exit([], fn ->
+      :erlang.disconnect_node(get_rabbit_hostname())
+
+    end)
+
+    :ok
+  end
+
+  setup do
+    {:ok, opts: %{node: get_rabbit_hostname()}}
+  end
+
+  test "validate accepts no arguments", context do
+    assert @command.validate([], context[:opts]) == :ok
+  end
+
+  test "validate: with extra arguments returns an arg count error", context do
+    assert @command.validate(["extra"], context[:opts]) == {:validation_failure, :too_many_args}
+  end
+
+  # TODO: we don't have integration tests yet.
+
+  test "run: request to a non-existent node returns nodedown" do
+    target = :jake@thedog
+    :net_kernel.connect_node(target)
+    opts = %{node: target}
+    assert match?({:badrpc, :nodedown}, @command.run([], opts))
+  end
+
+  test "empty banner", context do
+    nil = @command.banner([], context[:opts])
+  end
+end
