64fa65ee827b91b4d2bf6b1d5630e50944a6160b
Michael Klishin <michael@clojurewerkz.org>
2017-04-20 11:44:29 +0100

    Initial cut at revisited configuration module
    This module is aware of environment variables, just
    like in rabbitmq-autocluster, for ease of migration.

---
+++ deps/rabbitmq_peer_discovery_common/src/rabbit_peer_discovery_config.erl
@@ -0,0 +1,125 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License at
+%% http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%% License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Original Code is RabbitMQ.
+%%
+%% The Initial Developer of the Original Code is AWeber Communications.
+%% Copyright (c) 2015-2016 AWeber Communications
+%% Copyright (c) 2016-2017 Pivotal Software, Inc. All rights reserved.
+%%
+
+-module(rabbit_peer_discovery_config).
+
+-include("rabbit_peer_discovery.hrl").
+
+-export([get/3]).
+
+%%
+%% API
+%%
+
+-spec get(Key :: atom(),
+          Mapping :: #{atom() => #peer_discovery_config_entry_meta{}},
+          Config  :: #{atom() => peer_discovery_config_value()}) -> peer_discovery_config_value().
+
+get(Key, Mapping, Config) ->
+    case maps:is_key(Key, Mapping) of
+        false ->
+            rabbit_log:error("Key ~s is not found in peer discovery config mapping ~p!"),
+            throw({badkey, Key});
+        true  ->
+            get_with_entry_meta(Key, maps:get(Key, Mapping), Config)
+    end.
+
+%%
+%% Implementation
+%%
+
+-spec get_with_entry_meta(Key       :: atom(),
+                          EntryMeta :: #peer_discovery_config_entry_meta{},
+                          Map       :: #{atom() => peer_discovery_config_value()}) -> peer_discovery_config_value().
+
+get_with_entry_meta(Key, #peer_discovery_config_entry_meta{env_variable = EV,
+                                                           default_value = Default,
+                                                           type    = Type}, Map) ->
+    normalize(Type, get_from_env_variable_or_map(Map, EV, Key, Default)).
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Return the a value from the OS environment value, or the provided
+%% map if OS env var is not set, or the
+%% default value.
+%% @end
+%%--------------------------------------------------------------------
+-spec get_from_env_variable_or_map(Map :: map(), OSKey :: string(), AppKey :: atom(),
+                                   Default :: atom() | integer() | string())
+                                  -> atom() | integer() | string().
+get_from_env_variable_or_map(Map, OSKey, AppKey, Default) ->
+  case getenv(OSKey) of
+    false -> maps:get(AppKey, Map, Default);
+    Value -> Value
+  end.
+
+-spec getenv(Key :: string()) -> string() | false.
+getenv(Key) ->
+  process_getenv_value(Key, os:getenv(Key)).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Check the response of os:getenv/1 to see if it's false and if it is
+%% chain down to maybe_getenv_with_subkey/2 to see if the environment
+%% variable has a prefix of RABBITMQ_, potentially trying to get an
+%% environment variable without the prefix.
+%% @end
+%%--------------------------------------------------------------------
+-spec process_getenv_value(Key :: string(), Value :: string() | false)
+    -> string() | false.
+process_getenv_value(Key, false) ->
+  maybe_getenv_with_subkey(Key, string:left(Key, 9));
+process_getenv_value(_, Value) -> Value.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Check to see if the OS environment variable starts with RABBITMQ_
+%% and if so, try and fetch the value from an environment variable
+%% with the prefix removed.
+%% @end
+%%--------------------------------------------------------------------
+-spec maybe_getenv_with_subkey(Key :: string(), Prefix :: string())
+    -> string() | false.
+maybe_getenv_with_subkey(Key, "RABBITMQ_") ->
+  os:getenv(string:sub_string(Key, 10));
+maybe_getenv_with_subkey(_, _) ->
+  false.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Return the normalized value in as the proper data type
+%% @end
+%%--------------------------------------------------------------------
+-spec normalize(Type  :: atom(),
+                Value :: atom() | boolean() | integer() | string()) ->
+  atom() | integer() | string().
+normalize(Type, Value) when Type =:= port ->
+  rabbit_peer_discovery_util:parse_port(Value);
+normalize(Type, Value) when Type =:= atom ->
+  rabbit_peer_discovery_util:as_atom(Value);
+normalize(Type, Value) when Type =:= integer ->
+  rabbit_peer_discovery_util:as_integer(Value);
+normalize(Type, Value) when Type =:= string ->
+  rabbit_peer_discovery_util:as_string(Value);
+normalize(Type, Value) when Type =:= proplist ->
+  rabbit_peer_discovery_util:as_proplist(Value).
