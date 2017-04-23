23c34faaf4608ff589bf78dca6ea4c27cbfb2b5c
Michael Klishin <michael@clojurewerkz.org>
2017-04-23 02:12:54 +0200

    Port httpc wrapper and util modules from rabbitmq-autocluster

---
+++ deps/rabbitmq_peer_discovery_common/src/rabbit_peer_discovery_httpc.erl
@@ -0,0 +1,264 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License at
+%% http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%% License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Original Code is RabbitMQ.
+%%
+%% The Initial Developer of the Original Code is AWeber Communications.
+%% Copyright (c) 2015-2016 AWeber Communications
+%% Copyright (c) 2016-2017 Pivotal Software, Inc. All rights reserved.
+%%
+
+-module(rabbit_peer_discovery_httpc).
+
+%%
+%% API
+%%
+
+-export([build_query/1,
+         build_path/1,
+         build_uri/5,
+         delete/6,
+         get/5,
+         get/7,
+         post/6,
+         put/6]).
+
+%% Export all for unit tests
+-ifdef(TEST).
+-compile(export_all).
+-endif.
+
+
+-define(CONTENT_JSON, "application/json").
+-define(CONTENT_URLENCODED, "application/x-www-form-urlencoded").
+
+-type path_component() :: atom() | binary() | integer() | string().
+-export_type([path_component/0]).
+
+%% @public
+%% @doc Build the path from a list of segments
+%% @end
+%%
+-spec build_path([path_component()]) -> string().
+build_path(Args) ->
+  build_path(Args, []).
+
+
+%% @public
+%% @spec build_path(string(), string()) -> string()
+%% @doc Build the path from a list of segments
+%% @end
+%%
+build_path([Part | Parts], Path) ->
+  build_path(Parts, string:join([Path, percent_encode(Part)], "/"));
+build_path([], Path) -> Path.
+
+
+%% @public
+%% @spec build_uri(Scheme, Host, Port, Path, QArgs) -> string()
+%% where Scheme = string()
+%%       Host = string()
+%%       Port = integer()
+%%       Path = string()
+%%       QArgs = proplist()
+%% @doc Build the request URI
+%% @end
+%%
+build_uri(Scheme, Host, Port, Path, QArgs) ->
+  build_uri(string:join([Scheme, "://", Host, ":", rabbit_peer_discovery_util:as_string(Port)], ""), Path, QArgs).
+
+
+%% @public
+%% @spec build_uri(string(), string(), proplist()) -> string()
+%% @doc Build the requst URI for the given base URI, path and query args
+%% @end
+%%
+build_uri(Base, Path, []) ->
+  string:join([Base, build_path(Path)], "");
+build_uri(Base, Path, QArgs) ->
+  string:join([Base, string:join([build_path(Path),
+                                  build_query(QArgs)], "?")], "").
+
+
+%% @public
+%% @spec build_query(proplist()) -> string()
+%% @doc Build the query parameters string from a proplist
+%% @end
+%%
+build_query(Args) ->
+  build_query(Args, []).
+
+
+%% @public
+%% @spec build_query(proplist(), string()) -> string()
+%% @doc Build the query parameters string from a proplist
+%% @end
+%%
+build_query([{Key, Value} | Args], Parts) when is_atom(Key) =:= true ->
+  build_query(Args, lists:merge(Parts, [string:join([percent_encode(Key),
+                                                     percent_encode(Value)], "=")]));
+build_query([{Key, Value} | Args], Parts) ->
+  build_query(Args, lists:merge(Parts, [string:join([percent_encode(Key),
+                                                     percent_encode(Value)], "=")]));
+build_query([Key | Args], Parts) ->
+  build_query(Args, lists:merge(Parts, [percent_encode(Key)]));
+build_query([], Parts) ->
+  string:join(Parts, "&").
+
+
+%% @public
+%% @spec get(Scheme, Host, Port, Path, Args) -> Result
+%% @where Scheme = string(),
+%%        Host   = string(),
+%%        Port   = integer(),
+%%        Path   = string(),
+%%        Args   = proplist(),
+%%        Result = {ok, mixed}|{error, Reason::string()}
+%% @doc Perform a HTTP GET request
+%% @end
+%%
+get(Scheme, Host, Port, Path, Args) ->
+    get(Scheme, Host, Port, Path, Args, [], []).
+
+
+%% @public
+%% @spec get(Scheme, Host, Port, Path, Args, Headers) -> Result
+%% @where Scheme  = string(),
+%%        Host    = string(),
+%%        Port    = integer(),
+%%        Path    = string(),
+%%        Args    = proplist(),
+%%        Headers = proplist(),
+%%        HttpOpts = proplist(),
+%%        Result  = {ok, mixed}|{error, Reason::string()}
+%% @doc Perform a HTTP GET request
+%% @end
+%%
+get(Scheme, Host, Port, Path, Args, Headers, HttpOpts) ->
+  URL = build_uri(Scheme, Host, Port, Path, Args),
+  rabbit_log:debug("GET ~s", [URL]),
+  Response = httpc:request(get, {URL, Headers}, HttpOpts, []),
+  rabbit_log:debug("Response: [~p]", [Response]),
+  parse_response(Response).
+
+
+%% @public
+%% @spec post(Scheme, Host, Port, Path, Args, Body) -> Result
+%% @where Scheme = string(),
+%%        Host   = string(),
+%%        Port   = integer(),
+%%        Path   = string(),
+%%        Args   = proplist(),
+%%        Body   = string(),
+%%        Result = {ok, mixed}|{error, Reason::string()}
+%% @doc Perform a HTTP POST request
+%% @end
+%%
+post(Scheme, Host, Port, Path, Args, Body) ->
+  URL = build_uri(Scheme, Host, Port, Path, Args),
+  rabbit_log:debug("POST ~s [~p]", [URL, Body]),
+  Response = httpc:request(post, {URL, [], ?CONTENT_JSON, Body}, [], []),
+  rabbit_log:debug("Response: [~p]", [Response]),
+  parse_response(Response).
+
+
+%% @public
+%% @spec put(Scheme, Host, Port, Path, Args, Body) -> Result
+%% @where Scheme = string(),
+%%        Host   = string(),
+%%        Port   = integer(),
+%%        Path   = string(),
+%%        Args   = proplist(),
+%%        Body   = string(),
+%%        Result = {ok, mixed}|{error, Reason::string()}
+%% @doc Perform a HTTP PUT request
+%% @end
+%%
+put(Scheme, Host, Port, Path, Args, Body) ->
+  URL = build_uri(Scheme, Host, Port, Path, Args),
+  rabbit_log:debug("PUT ~s [~p]", [URL, Body]),
+  Response = httpc:request(put, {URL, [], ?CONTENT_URLENCODED, Body}, [], []),
+  rabbit_log:debug("Response: [~p]", [Response]),
+  parse_response(Response).
+
+
+%% @public
+%% @spec delete(Scheme, Host, Port, Path, Args, Body) -> Result
+%% @where Scheme = string(),
+%%        Host   = string(),
+%%        Port   = integer(),
+%%        Path   = string(),
+%%        Args   = proplist(),
+%%        Body   = string(),
+%%        Result = {ok, mixed}|{error, Reason::string()}
+%% @doc Perform a HTTP DELETE request
+%% @end
+%%
+delete(Scheme, Host, Port, Path, Args, Body) ->
+  URL = build_uri(Scheme, Host, Port, Path, Args),
+  rabbit_log:debug("DELETE ~s [~p]", [URL, Body]),
+  Response = httpc:request(delete, {URL, [], ?CONTENT_URLENCODED, Body}, [], []),
+  rabbit_log:debug("Response: [~p]", [Response]),
+  parse_response(Response).
+
+
+%% @private
+%% @spec decode_body(mixed) -> list()
+%% @doc Decode the response body and return a list
+%% @end
+%%
+decode_body(_, []) -> [];
+decode_body(?CONTENT_JSON, Body) ->
+    case rabbit_json:try_decode(rabbit_data_coercion:to_binary(Body), []) of
+        {ok, Value} -> Value;
+        {error, Err}  ->
+            rabbit_log:error("HTTP client could not decode a JSON payload "
+                                  "(JSON parser returned an error): ~p.~n",
+                                  [Err]),
+            {ok, []}
+    end.
+
+%% @private
+%% @spec parse_response(Response) -> {ok, string()} | {error, mixed}
+%% @where Response = {status_line(), headers(), Body} | {status_code(), Body}
+%% @doc Decode the response body and return a list
+%% @end
+%%
+parse_response({error, Reason}) ->
+  rabbit_log:debug("HTTP Error ~p", [Reason]),
+  {error, Reason};
+
+parse_response({ok, 200, Body})  -> {ok, decode_body(?CONTENT_JSON, Body)};
+parse_response({ok, 201, Body})  -> {ok, decode_body(?CONTENT_JSON, Body)};
+parse_response({ok, 204, _})     -> {ok, []};
+parse_response({ok, Code, Body}) ->
+  rabbit_log:debug("HTTP Response (~p) ~s", [Code, Body]),
+  {error, integer_to_list(Code)};
+
+parse_response({ok, {{_,200,_},Headers,Body}}) ->
+  {ok, decode_body(proplists:get_value("content-type", Headers, ?CONTENT_JSON), Body)};
+parse_response({ok,{{_,201,_},Headers,Body}}) ->
+  {ok, decode_body(proplists:get_value("content-type", Headers, ?CONTENT_JSON), Body)};
+parse_response({ok,{{_,204,_},_,_}}) -> {ok, []};
+parse_response({ok,{{_Vsn,Code,_Reason},_,Body}}) ->
+  rabbit_log:debug("HTTP Response (~p) ~s", [Code, Body]),
+  {error, integer_to_list(Code)}.
+
+
+%% @private
+%% @spec percent_encode(Value) -> string()
+%% @where
+%%       Value = atom() or binary() or integer() or list()
+%% @doc Percent encode the query value, automatically
+%%      converting atoms, binaries, or integers
+%% @end
+%%
+percent_encode(Value) ->
+  http_uri:encode(rabbit_peer_discovery_util:as_string(Value)).
---
+++ deps/rabbitmq_peer_discovery_common/src/rabbit_peer_discovery_util.erl
@@ -0,0 +1,302 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License at
+%% http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%% License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Original Code is RabbitMQ.
+%%
+%% The Initial Developer of the Original Code is AWeber Communications.
+%% Copyright (c) 2015-2016 AWeber Communications
+%% Copyright (c) 2016-2017 Pivotal Software, Inc. All rights reserved.
+%%
+-module(rabbit_peer_discovery_util).
+
+%% API
+-export([as_atom/1,
+         as_integer/1,
+         as_string/1,
+         backend_module/0,
+         nic_ipv4/1,
+         node_hostname/1,
+         node_name/1,
+         parse_port/1,
+         as_proplist/1
+        ]).
+
+
+%% Export all for unit tests
+-ifdef(TEST).
+-compile(export_all).
+-endif.
+
+-type ifopt() :: {flag,[atom()]} | {addr, inet:ip_address()} |
+                 {netmask,inet:ip_address()} | {broadaddr,inet:ip_address()} |
+                 {dstaddr,inet:ip_address()} | {hwaddr,[byte()]}.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Return the passed in value as an atom.
+%% @end
+%%--------------------------------------------------------------------
+-spec as_atom(atom() | binary() | string()) -> atom().
+as_atom(Value) when is_atom(Value) ->
+  Value;
+as_atom(Value) when is_binary(Value) ->
+  list_to_atom(binary_to_list(Value));
+as_atom(Value) when is_list(Value) ->
+  list_to_atom(Value);
+as_atom(Value) ->
+  autocluster_log:error("Unexpected data type for atom value: ~p~n",
+                        [Value]),
+  Value.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Return the passed in value as an integer.
+%% @end
+%%--------------------------------------------------------------------
+-spec as_integer(binary() | integer() | string()) -> integer().
+as_integer([]) -> undefined;
+as_integer(Value) when is_binary(Value) ->
+  list_to_integer(as_string(Value));
+as_integer(Value) when is_list(Value) ->
+  list_to_integer(Value);
+as_integer(Value) when is_integer(Value) ->
+  Value;
+as_integer(Value) ->
+  autocluster_log:error("Unexpected data type for integer value: ~p~n",
+                        [Value]),
+  Value.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Return the passed in value as a string.
+%% @end
+%%--------------------------------------------------------------------
+-spec as_string(Value :: atom() | binary() | integer() | string())
+    -> string().
+as_string([]) -> "";
+as_string(Value) when is_atom(Value) ->
+  as_string(atom_to_list(Value));
+as_string(Value) when is_binary(Value) ->
+  as_string(binary_to_list(Value));
+as_string(Value) when is_integer(Value) ->
+  as_string(integer_to_list(Value));
+as_string(Value) when is_list(Value) ->
+  lists:flatten(Value);
+as_string(Value) ->
+  autocluster_log:error("Unexpected data type for list value: ~p~n",
+                        [Value]),
+  Value.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Return the module to use for node discovery.
+%% @end
+%%--------------------------------------------------------------------
+-spec backend_module() -> module() | undefined.
+backend_module() ->
+  backend_module(autocluster_config:get(backend)).
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Return the module to use for node discovery.
+%% @end
+%%--------------------------------------------------------------------
+-spec backend_module(atom()) -> module() | undefined.
+backend_module(aws)          -> autocluster_aws;
+backend_module(consul)       -> autocluster_consul;
+backend_module(dns)          -> autocluster_dns;
+backend_module(etcd)         -> autocluster_etcd;
+backend_module(k8s)          -> autocluster_k8s;
+backend_module(_)            -> undefined.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Return the IP address for the current node for the specified
+%% network interface controller.
+%% @end
+%%--------------------------------------------------------------------
+-spec nic_ipv4(Device :: string())
+    -> {ok, string()} | {error, not_found}.
+nic_ipv4(Device) ->
+  {ok, Interfaces} = inet:getifaddrs(),
+  nic_ipv4(Device, Interfaces).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Parse the interface out of the list of interfaces, returning the
+%% IPv4 address if found.
+%% @end
+%%--------------------------------------------------------------------
+-spec nic_ipv4(Device :: string(), Interfaces :: list())
+    -> {ok, string()} | {error, not_found}.
+nic_ipv4(_, []) -> {error, not_found};
+nic_ipv4(Device, [{Interface, Opts}|_]) when Interface =:= Device ->
+  {ok, nic_ipv4_address(Opts)};
+nic_ipv4(Device, [_|T]) ->
+  nic_ipv4(Device,T).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Return the formatted IPv4 address out of the list of addresses
+%% for the interface.
+%% @end
+%%--------------------------------------------------------------------
+-spec nic_ipv4_address([ifopt()]) -> {ok, string()} | {error, not_found}.
+nic_ipv4_address([]) -> {error, not_found};
+nic_ipv4_address([{addr, {A,B,C,D}}|_]) ->
+  inet_parse:ntoa({A,B,C,D});
+nic_ipv4_address([_|T]) ->
+  nic_ipv4_address(T).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Return the hostname for the current node (without the tuple)
+%% Hostname can be taken from network info or nodename.
+%% @end
+%%--------------------------------------------------------------------
+-spec node_hostname(boolean()) -> string().
+node_hostname(false = _FromNodename) ->
+    {ok, Hostname} = inet:gethostname(),
+    Hostname;
+node_hostname(true = _FromNodename) ->
+    {match, [Hostname]} = re:run(atom_to_list(node()), "@(.*)",
+                                 [{capture, [1], list}]),
+    Hostname.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Return the proper node name for clustering purposes
+%% @end
+%%--------------------------------------------------------------------
+-spec node_name(Value :: atom() | binary() | string()) -> atom().
+node_name(Value) when is_atom(Value) ->
+    node_name(atom_to_list(Value));
+node_name(Value) when is_binary(Value) ->
+    node_name(binary_to_list(Value));
+node_name(Value) when is_list(Value) ->
+  case lists:member($@, Value) of
+      true ->
+          list_to_atom(Value);
+      false ->
+          list_to_atom(string:join([node_prefix(),
+                                    node_name_parse(as_string(Value))],
+                                   "@"))
+  end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Parse the value passed into nodename, checking if it's an ip
+%% address. If so, return it. If not, then check to see if longname
+%% support is turned on. if so, return it. If not, extract the left
+%% most part of the name, delimited by periods.
+%% @end
+%%--------------------------------------------------------------------
+-spec node_name_parse(Value :: string()) -> string().
+node_name_parse(Value) ->
+  case inet:parse_ipv4strict_address(Value) of
+    {ok, _} ->
+      Value;
+    {error, einval} ->
+      node_name_parse(autocluster_config:get(longname), Value)
+  end.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Continue the parsing logic from node_name_parse/1. This is where
+%% result of the IPv4 check is processed.
+%% @end
+%%--------------------------------------------------------------------
+-spec node_name_parse(IsIPv4 :: true | false, Value :: string())
+    -> string().
+node_name_parse(true, Value) -> Value;
+node_name_parse(false, Value) ->
+  Parts = string:tokens(Value, "."),
+  node_name_parse(length(Parts), Value, Parts).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Properly deal with returning the hostname if it's made up of
+%% multiple segments like www.rabbitmq.com, returning www, or if it's
+%% only a single segment, return that.
+%% @end
+%%--------------------------------------------------------------------
+-spec node_name_parse(Segments :: integer(),
+                      Value :: string(),
+                      Parts :: [string()])
+    -> string().
+node_name_parse(1, Value, _) -> Value;
+node_name_parse(_, _, Parts) ->
+  as_string(lists:nth(1, Parts)).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Extract the "local part" of the ``RABBITMQ_NODENAME`` environment
+%% variable, if set, otherwise use the default node name value
+%% (rabbit).
+%% @end
+%%--------------------------------------------------------------------
+-spec node_prefix() -> string().
+node_prefix() ->
+  Value = autocluster_config:get(node_name),
+  lists:nth(1, string:tokens(Value, "@")).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns the port, even if Docker linking overwrites a configuration
+%% value to be a URI instead of numeric value
+%% @end
+%%--------------------------------------------------------------------
+-spec parse_port(Value :: integer() | string()) -> integer().
+parse_port(Value) when is_list(Value) ->
+  as_integer(lists:last(string:tokens(Value, ":")));
+parse_port(Value) -> as_integer(Value).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns a proplist from a JSON structure (environment variable) or
+%% the settings key (already a proplist).
+%% @end
+%%--------------------------------------------------------------------
+-spec as_proplist(Value :: string() | [{string(), string()}]) ->
+                         [{string(), string()}].
+as_proplist([Tuple | _] = Json) when is_tuple(Tuple) ->
+    Json;
+as_proplist([]) ->
+    [];
+as_proplist(List) when is_list(List) ->
+    Value = rabbit_data_coercion:to_binary(List),
+    case rabbit_json:try_decode(Value) of
+        {ok, Json} ->
+            [{binary_to_list(K), binary_to_list(V)}
+             || {K, V} <- maps:to_list(Json)];
+        {error, Error} ->
+            autocluster_log:error("Unexpected data type for proplist value: ~p. JSON parser returned an error: ~p!~n",
+                                  [Value, Error]),
+            []
+    end;
+as_proplist(Value) ->
+    autocluster_log:error("Unexpected data type for proplist value: ~p.~n",
+                          [Value]),
+    [].
