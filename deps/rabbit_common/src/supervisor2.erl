934508d358d0e4e354bc44a15be946482cc23e9b
Diana Corbacho <diana@rabbitmq.com>
2017-05-03 16:43:54 +0100

    Make callback prep_stop/0 optional

--- deps/rabbit_common/src/supervisor2.erl
+++ deps/rabbit_common/src/supervisor2.erl
@@ -142,7 +142,13 @@
            [ChildSpec :: child_spec()]}}
     | ignore.
 
--callback prep_stop() -> ok.
+%% Optional callback prep_stop/0. It cannot be exported as Erlang/OTP doesn't
+%% support definition of optional callbacks.
+%%
+%% Currently used to stop application dependencies.
+%%
+%% -callback prep_stop() -> ok.
+%%
 
 -define(restarting(_Pid_), {restarting,_Pid_}).
 
