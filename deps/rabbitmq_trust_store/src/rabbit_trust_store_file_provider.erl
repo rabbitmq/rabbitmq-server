6f97aadc5452af778e917a72fb02df3c86610e46
Daniil Fedotov <dfedotov@pivotal.io>
2017-05-08 10:30:42 +0100

    Do not check directory change time.
    [#58]
    Directory change time is not reliable to detect updates in
    certificate files. Time resolution is too big (1 second)
    and directory modification time does not update when a file
    contents is changed.

--- deps/rabbitmq_trust_store/src/rabbit_trust_store_file_provider.erl
+++ deps/rabbitmq_trust_store/src/rabbit_trust_store_file_provider.erl
@@ -8,37 +8,21 @@
 
 -define(DIRECTORY_OR_FILE_NAME_EXISTS, eexist).
 
--record(directory_state, {
-    directory_path,
-    directory_change_time}).
-
 -type cert_id() :: {FileName :: string(), ChangeTime :: integer()}.
 
 -spec list_certs(Config :: list())
     -> no_change | {ok, [{cert_id(), list()}], State}
-    when State :: #directory_state{}.
+    when State :: nostate.
 list_certs(Config) ->
     Path = directory_path(Config),
-    NewChangeTime = modification_time(Path),
     Certs = list_certs_0(Path),
-    {ok, Certs, #directory_state{directory_path = Path,
-                                 directory_change_time = NewChangeTime}}.
+    {ok, Certs, nostate}.
 
 -spec list_certs(Config :: list(), State)
     -> no_change | {ok, [{cert_id(), list()}], State}
-    when State :: #directory_state{}.
-list_certs(Config, #directory_state{directory_path = DirPath,
-                                    directory_change_time = ChangeTime}) ->
-    Path = directory_path(Config, DirPath),
-    NewChangeTime = modification_time(Path),
-    case NewChangeTime > ChangeTime of
-        false ->
-            no_change;
-        true  ->
-            Certs = list_certs_0(Path),
-            {ok, Certs, #directory_state{directory_path = Path,
-                                         directory_change_time = NewChangeTime}}
-    end.
+    when State :: nostate.
+list_certs(Config, _) ->
+    list_certs(Config).
 
 -spec load_cert(cert_id(), list(), Config :: list())
     -> {ok, Cert :: public_key:der_encoded()}.
