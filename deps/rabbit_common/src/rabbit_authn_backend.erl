d83543c22bfb407ecd7d1f92cbf9730ca9c42b47
Michael Klishin <mklishin@pivotal.io>
2017-06-07 18:24:13 +0300

    rabbit_authn_backend:user_login_authentication/2: allow maps as context info
    "Maps all the things" and all.

--- deps/rabbit_common/src/rabbit_authn_backend.erl
+++ deps/rabbit_common/src/rabbit_authn_backend.erl
@@ -30,7 +30,7 @@
 %%     Something went wrong. Log and die.
 %% {refused, Msg, Args}
 %%     Client failed authentication. Log and die.
--callback user_login_authentication(rabbit_types:username(), [term()]) ->
+-callback user_login_authentication(rabbit_types:username(), [term()] | map()) ->
     {'ok', rabbit_types:auth_user()} |
     {'refused', string(), [any()]} |
     {'error', any()}.
