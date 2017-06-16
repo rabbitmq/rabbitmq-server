31822baf8c0b1332d9a7b28913aa38a8bd772158
Michael Klishin <mklishin@pivotal.io>
2017-06-16 17:55:31 +0300

    Revert "Revert "Compile""
    This reverts commit 7908b17f01c20caf916742f559c8851d24540251.
    
    Test failures were due to something tripping up test node start,
    not the change in ec695461abc9a695b547439004dc33512872b7b8.

--- deps/rabbitmq_cli/lib/rabbitmqctl.ex
+++ deps/rabbitmq_cli/lib/rabbitmqctl.ex
@@ -251,13 +251,13 @@ defmodule RabbitMQCtl do
     exit({:shutdown, code})
   end
 
-  defp format_error({:error, {:badrpc_multi, :nodedown, [node | _]} = result}, opts, _) do
+  defp format_error({:error, {:badrpc_multi, :nodedown, [node | _]} = result}, _opts, _) do
     diagnostics = get_node_diagnostics(node)
     {:error, ExitCodes.exit_code_for(result),
      "Error: unable to connect to node '#{node}': nodedown\n" <>
      diagnostics}
   end
-  defp format_error({:error, {:badrpc_multi, :timeout, [node | _]} = result}, opts, _) do
+  defp format_error({:error, {:badrpc_multi, :timeout, [node | _]} = result}, opts, module) do
     op = CommandModules.module_to_command(module)
     {:error, ExitCodes.exit_code_for(result),
      "Error: operation #{op} on node #{node} timed out. Timeout: #{opts[:timeout]}"}
