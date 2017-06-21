18b6adac1acb0824fb8deed3d2219bb3b73ba84d
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2017-06-21 15:37:30 +0200

    rabbit_net:accept_ack() was moved to rabbit_networking
    [#118490793]

--- deps/rabbitmq_stomp/src/rabbit_stomp_reader.erl
+++ deps/rabbitmq_stomp/src/rabbit_stomp_reader.erl
@@ -67,7 +67,7 @@ info(Pid, InfoItems) ->
 
 init([SupHelperPid, Ref, Sock, Configuration]) ->
     process_flag(trap_exit, true),
-    rabbit_net:accept_ack(Ref, Sock),
+    rabbit_networking:accept_ack(Ref, Sock),
 
     case rabbit_net:connection_string(Sock, inbound) of
         {ok, ConnStr} ->
