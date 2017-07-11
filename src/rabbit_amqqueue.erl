36ef3ab70f56938e14f7ed0d65f8fa5ff2171219
Michael Klishin <michael@clojurewerkz.org>
2017-07-11 03:40:22 +0300

    Fix a merge

--- src/rabbit_amqqueue.erl
+++ src/rabbit_amqqueue.erl
@@ -29,8 +29,7 @@
 -export([list/0, list/1, info_keys/0, info/1, info/2, info_all/1, info_all/2,
          emit_info_all/5, list_local/1, info_local/1,
 	 emit_info_local/4, emit_info_down/4]).
--export([list_down/1, count/1, list_names/0,
-         list_names/0, list_local_names/0]).
+-export([list_down/1, count/1, list_names/0, list_local_names/0]).
 -export([force_event_refresh/1, notify_policy_changed/1]).
 -export([consumers/1, consumers_all/1,  emit_consumers_all/4, consumer_info_keys/0]).
 -export([basic_get/4, basic_consume/11, basic_cancel/5, notify_decorators/1]).
