84baebf1011f955f0093aff5bfdbd9fdac32c144
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2017-07-21 12:15:07 +0200

    rabbit_mgmt_wm_definitions: Copy maps:update_with/4 locally
    This function doesn't exist in Erlang 18.x which we still support. So
    copy this function from Erlang 20.x to the module.

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_definitions.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_definitions.erl
@@ -23,7 +23,7 @@
 
 -export([apply_defs/4]).
 
--import(rabbit_misc, [pget/2, pget/3]).
+-import(rabbit_misc, [pget/2]).
 
 -include("rabbit_mgmt.hrl").
 -include_lib("rabbitmq_management_agent/include/rabbit_mgmt_records.hrl").
@@ -481,11 +481,24 @@ build_filtered_map([Queue|Rest], AccMap0) ->
     {Rec, VHost} = build_queue_data(Queue),
     case rabbit_amqqueue:lookup(Rec) of
         {error, not_found} ->
-            AccMap1 = maps:update_with(VHost, fun(V) -> V + 1 end, 1, AccMap0),
+            AccMap1 = maps_update_with(VHost, fun(V) -> V + 1 end, 1, AccMap0),
             build_filtered_map(Rest, AccMap1);
         {ok, _} ->
             build_filtered_map(Rest, AccMap0)
     end.
+
+%% Copy of maps:with_util/3 from Erlang 20.0.1.
+maps_update_with(Key,Fun,Init,Map) when is_function(Fun,1), is_map(Map) ->
+    case maps:find(Key,Map) of
+        {ok,Val} -> maps:update(Key,Fun(Val),Map);
+        error -> maps:put(Key,Init,Map)
+    end;
+maps_update_with(Key,Fun,Init,Map) ->
+    erlang:error(maps_error_type(Map),[Key,Fun,Init,Map]).
+
+%% Copy of maps:error_type/1 from Erlang 20.0.1.
+maps_error_type(M) when is_map(M) -> badarg;
+maps_error_type(V) -> {badmap, V}.
 
 validate_vhost_limit(VHost, AddCount, ok) ->
     WouldExceed = rabbit_vhost_limit:would_exceed_queue_limit(AddCount, VHost),
