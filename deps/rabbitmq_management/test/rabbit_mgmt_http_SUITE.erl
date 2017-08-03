1113095d49ffddf35bf1873fd51243085ef90774
Michael Klishin <michael@clojurewerkz.org>
2017-08-03 16:59:51 +0300

    Update test expectations

--- deps/rabbitmq_management/test/rabbit_mgmt_http_SUITE.erl
+++ deps/rabbitmq_management/test/rabbit_mgmt_http_SUITE.erl
@@ -344,15 +344,17 @@ vhosts_test(Config) ->
 
 vhosts_trace_test(Config) ->
     http_put(Config, "/vhosts/myvhost", none, {group, '2xx'}),
-    Disabled = #{name => <<"myvhost">>, tracing => false, state => <<"running">>},
-    Enabled  = #{name => <<"myvhost">>, tracing => true, state => <<"running">>},
-    Disabled = http_get(Config, "/vhosts/myvhost"),
+    ?assertMatch(#{name := <<"myvhost">>, tracing := false,  cluster_state := _},
+                 http_get(Config, "/vhosts/myvhost")),
     http_put(Config, "/vhosts/myvhost", [{tracing, true}], {group, '2xx'}),
-    Enabled = http_get(Config, "/vhosts/myvhost"),
+    ?assertMatch(#{name := <<"myvhost">>, tracing := true, cluster_state := _},
+                 http_get(Config, "/vhosts/myvhost")),
     http_put(Config, "/vhosts/myvhost", [{tracing, true}], {group, '2xx'}),
-    Enabled = http_get(Config, "/vhosts/myvhost"),
+    ?assertMatch(#{name := <<"myvhost">>, tracing := true},
+                 http_get(Config, "/vhosts/myvhost")),
     http_put(Config, "/vhosts/myvhost", [{tracing, false}], {group, '2xx'}),
-    Disabled = http_get(Config, "/vhosts/myvhost"),
+    ?assertMatch(#{name := <<"myvhost">>, tracing := false},
+                 http_get(Config, "/vhosts/myvhost")),
     http_delete(Config, "/vhosts/myvhost", {group, '2xx'}),
 
     passed.
