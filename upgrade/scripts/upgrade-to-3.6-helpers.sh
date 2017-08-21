5edcecc937a1b9e616bc05a1e210167e683fef40
Daniil Fedotov <daniil@rabbitmq.com>
2017-08-21 17:28:41 +0100

    Move verify_steps function into upgrade-helpers
    No point in having 2 scripts
    
    Signed-off-by: Gerhard Lazu <gerhard@rabbitmq.com>

--- upgrade/scripts/upgrade-to-3.6-helpers.sh
+++ upgrade/scripts/upgrade-to-3.6-helpers.sh
@@ -2,5 +2,3 @@
 
 do_not_requeue="requeue=false"
 export do_not_requeue
-# shellcheck source=/dev/null
-. "$(dirname "$0")/upgrade-to-helpers.sh"
--- upgrade/scripts/upgrade-to-3.7-helpers.sh
+++ upgrade/scripts/upgrade-to-3.7-helpers.sh
@@ -2,5 +2,3 @@
 
 do_not_requeue="ackmode=ack_requeue_false"
 export do_not_requeue
-# shellcheck source=/dev/null
-. "$(dirname "$0")/upgrade-to-helpers.sh"
