79c1c2f7993e6df0556ac71474ed72a7c0bb925b
Gerhard Lazu <gerhard@rabbitmq.com>
2017-10-04 16:15:20 +0100

    Use maps:from_list so that it compiles on R16B03
    Since looking_glass requires Erlang 19, maps:from_list will not be
    called if it's not defined in earlier Erlang versions.
    
    Signed-off-by: Loïc Hoguin <loic@rabbitmq.com>

--- src/rabbit_looking_glass.erl
+++ src/rabbit_looking_glass.erl
@@ -27,12 +27,21 @@ boot() ->
             ok;
         Value ->
             Input = parse_value(Value),
-            rabbit_log:info("Enabling Looking Glass profiler, input value: ~p", [Input]),
+            rabbit_log:info(
+                "Enabling Looking Glass profiler, input value: ~p",
+                [Input]
+            ),
             {ok, _} = application:ensure_all_started(looking_glass),
-            lg:trace(Input, lg_file_tracer, "traces.lz4", #{
-                mode => profile,
-                running => true,
-                send => true})
+            lg:trace(
+                Input,
+                lg_file_tracer,
+                "traces.lz4",
+                maps:from_list([
+                    {mode, profile},
+                    {running, true},
+                    {send, true}]
+                )
+             )
     end.
 
 parse_value(Value) ->
