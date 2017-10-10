b6af5aedf5bb86f6c765c59bff900bbc2ca06926
arif-pathan <32669134+arif-pathan@users.noreply.github.com>
2017-10-10 15:51:27 +0530

    rabbitmq_auth_backend_webapi_dotnet example
    rabbitmq_auth_backend_webapi_dotnet example is a very minimalistic DotNet WebApi application that rabbitmq-auth-backend-http can authenticate against. It's really not designed to be anything other than an example.

---
+++ deps/rabbitmq_auth_backend_http/examples/rabbitmq_auth_backend_webapi_dotnet/App_Start/WebApiConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace WebApiHttpAuthService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
