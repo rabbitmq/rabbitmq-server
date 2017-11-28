fc5b0c6edcd8983a796e5146eb1b2a79c9317090
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2017-11-28 14:01:08 +0100

    Update rabbitmq-components.mk

--- deps/rabbitmq_peer_discovery_k8s/rabbitmq-components.mk
+++ deps/rabbitmq_peer_discovery_k8s/rabbitmq-components.mk
@@ -110,10 +110,11 @@ dep_rabbitmq_public_umbrella          = git_rmq rabbitmq-public-umbrella $(curre
 # all projects use the same versions. It avoids conflicts and makes it
 # possible to work with rabbitmq-public-umbrella.
 
-dep_cowboy = hex 1.1.2
+dep_cowboy = hex 2.0.0
+dep_cowlib = hex 2.0.0
 dep_jsx = hex 2.8.2
 dep_lager = hex 3.5.1
-dep_ranch = hex 1.3.2
+dep_ranch = hex 1.4.0
 dep_ranch_proxy_protocol = hex 1.4.2
 dep_recon = hex 2.3.2
 
