29da36157a987149e7ad234e741720b29b9637fa
Loïc Hoguin <essen@ninenines.eu>
2017-11-29 11:02:18 +0100

    Add a macro to identify processes in Looking Glass

--- deps/rabbit_common/src/rabbit_writer.erl
+++ deps/rabbit_common/src/rabbit_writer.erl
@@ -175,6 +175,7 @@ system_code_change(Misc, _Module, _OldVsn, _Extra) ->
     {ok, Misc}.
 
 enter_mainloop(Identity, State) ->
+    ?LG_PROCESS_TYPE(writer),
     Deb = sys:debug_options([]),
     ?store_proc_name(Identity),
     mainloop(Deb, State).
