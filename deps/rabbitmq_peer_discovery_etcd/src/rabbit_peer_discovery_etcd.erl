69fb36cfac27d21442930fd673e8c0fe147ade4e
Michael Klishin <michael@clojurewerkz.org>
2017-12-14 18:35:39 +0300

    Configure HTTP client proxy at init step
    Part of rabbitmq/rabbitmq-peer-discovery-common#5.
    
    [#153615554]

--- deps/rabbitmq_peer_discovery_etcd/src/rabbit_peer_discovery_etcd.erl
+++ deps/rabbitmq_peer_discovery_etcd/src/rabbit_peer_discovery_etcd.erl
@@ -22,7 +22,7 @@
 -include_lib("rabbitmq_peer_discovery_common/include/rabbit_peer_discovery.hrl").
 -include("rabbit_peer_discovery_etcd.hrl").
 
--export([list_nodes/0, supports_registration/0, register/0, unregister/0,
+-export([init/0, list_nodes/0, supports_registration/0, register/0, unregister/0,
          post_registration/0, lock/1, unlock/1]).
 
 -export([update_node_key/0]).
@@ -36,6 +36,15 @@
 %%
 %% API
 %%
+
+init() ->
+    rabbit_log:debug("Peer discovery etcd: initialising..."),
+    ok = application:ensure_started(inets),
+    %% we cannot start this plugin yet since it depends on the rabbit app,
+    %% which is in the process of being started by the time this function is called
+    application:load(rabbitmq_peer_discovery_common),
+    ?HTTPC_MODULE:maybe_configure_proxy().
+
 
 -spec list_nodes() -> {ok, {Nodes :: list(), NodeType :: rabbit_types:node_type()}} | {error, Reason :: string()}.
 
