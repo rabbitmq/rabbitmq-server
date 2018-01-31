97fb02934629f8264ed4783d375298ced59c2bd6
Diana Corbacho <diana@rabbitmq.com>
2018-01-31 13:44:24 +0000

    Test restart node with quorum queues
    [#154472164]

--- test/quorum_queue_SUITE.erl
+++ test/quorum_queue_SUITE.erl
@@ -32,7 +32,10 @@ groups() ->
       {non_parallel_tests, [], [
           declare_args,
           start_queue,
-          stop_queue
+          stop_queue,
+          restart_queue,
+          restart_all_types,
+          stop_start_rabbit_app
         ]}
     ].
 
@@ -78,33 +81,33 @@ end_per_testcase(Testcase, Config) ->
 %% -------------------------------------------------------------------
 
 declare_args(Config) ->
-    A = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
+    Node = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
 
-    Ch = rabbit_ct_client_helpers:open_channel(Config, A),
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Node),
     LQ = <<"quorum-q">>,
     declare(Ch, LQ, [{<<"x-queue-type">>, longstr, <<"quorum">>}]),
-    assert_queue_type(A, LQ, quorum),
+    assert_queue_type(Node, LQ, quorum),
 
     DQ = <<"classic-q">>,
     declare(Ch, DQ, [{<<"x-queue-type">>, longstr, <<"classic">>}]),
-    assert_queue_type(A, DQ, classic),
+    assert_queue_type(Node, DQ, classic),
 
     DQ2 = <<"classic-q2">>,
     declare(Ch, DQ2),
-    assert_queue_type(A, DQ2, classic).
+    assert_queue_type(Node, DQ2, classic).
 
 start_queue(Config) ->
-    A = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
+    Node = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
 
-    Ch = rabbit_ct_client_helpers:open_channel(Config, A),
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Node),
     LQ = <<"quorum-q">>,
     ?assertEqual({'queue.declare_ok', LQ, 0, 0},
                  declare(Ch, LQ, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
 
     %% Check that the application and one ra node are up
     ?assertMatch({ra, _, _}, lists:keyfind(ra, 1,
-                                           rpc:call(A, application, which_applications, []))),
-    ?assertMatch([_], rpc:call(A, supervisor, which_children, [ra_nodes_sup])),
+                                           rpc:call(Node, application, which_applications, []))),
+    ?assertMatch([_], rpc:call(Node, supervisor, which_children, [ra_nodes_sup])),
 
     %% Test declare an existing queue
     ?assertEqual({'queue.declare_ok', LQ, 0, 0},
@@ -115,29 +118,117 @@ start_queue(Config) ->
 
     %% Check that the application and process are still up
     ?assertMatch({ra, _, _}, lists:keyfind(ra, 1,
-                                           rpc:call(A, application, which_applications, []))),
-    ?assertMatch([_], rpc:call(A, supervisor, which_children, [ra_nodes_sup])).
+                                           rpc:call(Node, application, which_applications, []))),
+    ?assertMatch([_], rpc:call(Node, supervisor, which_children, [ra_nodes_sup])).
 
 stop_queue(Config) ->
-    A = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
+    Node = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
 
-    Ch = rabbit_ct_client_helpers:open_channel(Config, A),
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Node),
     LQ = <<"quorum-q">>,
     ?assertEqual({'queue.declare_ok', LQ, 0, 0},
                  declare(Ch, LQ, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
 
     %% Check that the application and one ra node are up
     ?assertMatch({ra, _, _}, lists:keyfind(ra, 1,
-                                           rpc:call(A, application, which_applications, []))),
-    ?assertMatch([_], rpc:call(A, supervisor, which_children, [ra_nodes_sup])),
+                                           rpc:call(Node, application, which_applications, []))),
+    ?assertMatch([_], rpc:call(Node, supervisor, which_children, [ra_nodes_sup])),
 
     %% Delete the quorum queue
     ?assertMatch(#'queue.delete_ok'{}, amqp_channel:call(Ch, #'queue.delete'{queue = LQ})),
 
     %% Check that the application and process are still up
     ?assertMatch({ra, _, _}, lists:keyfind(ra, 1,
-                                           rpc:call(A, application, which_applications, []))),
-    ?assertMatch([], rpc:call(A, supervisor, which_children, [ra_nodes_sup])).
+                                           rpc:call(Node, application, which_applications, []))),
+    ?assertMatch([], rpc:call(Node, supervisor, which_children, [ra_nodes_sup])).
+
+restart_queue(Config) ->
+    Node = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Node),
+    LQ = <<"quorum-q">>,
+    ?assertEqual({'queue.declare_ok', LQ, 0, 0},
+                 declare(Ch, LQ, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
+
+    ok = rabbit_ct_broker_helpers:stop_node(Config, Node),
+    ok = rabbit_ct_broker_helpers:start_node(Config, Node),
+
+    %% Check that the application and one ra node are up
+    ?assertMatch({ra, _, _}, lists:keyfind(ra, 1,
+                                           rpc:call(Node, application, which_applications, []))),
+    ?assertMatch([_], rpc:call(Node, supervisor, which_children, [ra_nodes_sup])).
+
+restart_all_types(Config) ->
+    %% Test the node restart with both types of queues (quorum and classic) to
+    %% ensure there are no regressions
+    Node = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Node),
+    QQ1 = <<"quorum-q1">>,
+    ?assertEqual({'queue.declare_ok', QQ1, 0, 0},
+                 declare(Ch, QQ1, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
+    QQ2 = <<"quorum-q2">>,
+    ?assertEqual({'queue.declare_ok', QQ2, 0, 0},
+                 declare(Ch, QQ2, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
+
+    CQ1 = <<"classic-q1">>,
+    ?assertEqual({'queue.declare_ok', CQ1, 0, 0}, declare(Ch, CQ1, [])),
+    rabbit_ct_client_helpers:publish(Ch, CQ1, 1),
+    CQ2 = <<"classic-q2">>,
+    ?assertEqual({'queue.declare_ok', CQ2, 0, 0}, declare(Ch, CQ2, [])),
+    rabbit_ct_client_helpers:publish(Ch, CQ2, 1),
+
+    ok = rabbit_ct_broker_helpers:stop_node(Config, Node),
+    ok = rabbit_ct_broker_helpers:start_node(Config, Node),
+
+    %% Check that the application and two ra nodes are up
+    ?assertMatch({ra, _, _}, lists:keyfind(ra, 1,
+                                           rpc:call(Node, application, which_applications, []))),
+    ?assertMatch([_,_], rpc:call(Node, supervisor, which_children, [ra_nodes_sup])),
+    %% Check the classic queues restarted correctly
+    Ch2 = rabbit_ct_client_helpers:open_channel(Config, Node),
+    {#'basic.get_ok'{}, #amqp_msg{}} =
+        amqp_channel:call(Ch2, #'basic.get'{queue  = CQ1, no_ack = false}),
+    {#'basic.get_ok'{}, #amqp_msg{}} =
+        amqp_channel:call(Ch2, #'basic.get'{queue  = CQ2, no_ack = false}).
+
+stop_start_rabbit_app(Config) ->
+    %% Test start/stop of rabbit app with both types of queues (quorum and
+    %%  classic) to ensure there are no regressions
+    Node = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Node),
+    QQ1 = <<"quorum-q1">>,
+    ?assertEqual({'queue.declare_ok', QQ1, 0, 0},
+                 declare(Ch, QQ1, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
+    QQ2 = <<"quorum-q2">>,
+    ?assertEqual({'queue.declare_ok', QQ2, 0, 0},
+                 declare(Ch, QQ2, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
+
+    CQ1 = <<"classic-q1">>,
+    ?assertEqual({'queue.declare_ok', CQ1, 0, 0}, declare(Ch, CQ1, [])),
+    rabbit_ct_client_helpers:publish(Ch, CQ1, 1),
+    CQ2 = <<"classic-q2">>,
+    ?assertEqual({'queue.declare_ok', CQ2, 0, 0}, declare(Ch, CQ2, [])),
+    rabbit_ct_client_helpers:publish(Ch, CQ2, 1),
+
+    rabbit_control_helper:command(stop_app, Node),
+    %% Check the ra application has stopped (thus its supervisor and queues)
+    ?assertMatch(false, lists:keyfind(ra, 1,
+                                      rpc:call(Node, application, which_applications, []))),
+
+    rabbit_control_helper:command(start_app, Node),
+
+    %% Check that the application and two ra nodes are up
+    ?assertMatch({ra, _, _}, lists:keyfind(ra, 1,
+                                           rpc:call(Node, application, which_applications, []))),
+    ?assertMatch([_,_], rpc:call(Node, supervisor, which_children, [ra_nodes_sup])),
+    %% Check the classic queues restarted correctly
+    Ch2 = rabbit_ct_client_helpers:open_channel(Config, Node),
+    {#'basic.get_ok'{}, #amqp_msg{}} =
+        amqp_channel:call(Ch2, #'basic.get'{queue  = CQ1, no_ack = false}),
+    {#'basic.get_ok'{}, #amqp_msg{}} =
+        amqp_channel:call(Ch2, #'basic.get'{queue  = CQ2, no_ack = false}).
 
 %%----------------------------------------------------------------------------
 
