47fa419237a481b6f86c90ccf01401d13423010e
Gerhard Lazu <gerhard@lazu.co.uk>
2018-02-22 14:35:55 +0000

    Do not read exchange & queue before deleting, simply delete
    Doing a wread operation requires a table write lock. Since a delete
    requires a table write lock as well, we have 2 write locks instead of 1.
    When Mnesia is under load, this can result in many lock collisions and
    slow down the entire set of operations. We think that it's better to
    leverage fast SSDs / NVMEs and defer the Mnesia disk log write
    optimisation to the filesystem.
    
    For more context, see #1513
    
    Partner-in-crime @essen

--- src/rabbit_exchange.erl
+++ src/rabbit_exchange.erl
@@ -484,14 +484,9 @@ conditional_delete(X = #exchange{name = XName}, OnlyDurable) ->
     end.
 
 unconditional_delete(X = #exchange{name = XName}, OnlyDurable) ->
-    %% this 'guarded' delete prevents unnecessary writes to the mnesia
-    %% disk log
-    case mnesia:wread({rabbit_durable_exchange, XName}) of
-        []  -> ok;
-        [_] -> ok = mnesia:delete({rabbit_durable_exchange, XName})
-    end,
     ok = mnesia:delete({rabbit_exchange, XName}),
     ok = mnesia:delete({rabbit_exchange_serial, XName}),
+    mnesia:delete({rabbit_durable_exchange, XName}),
     Bindings = rabbit_binding:remove_for_source(XName),
     {deleted, X, Bindings, rabbit_binding:remove_for_destination(
                              XName, OnlyDurable)}.
