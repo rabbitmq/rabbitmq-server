5e939693af25d561fdd099566a4a612fd03f571d
Michael Klishin <mklishin@pivotal.io>
2018-03-02 14:24:13 +0300

    Add ha-promote-on-shutdown to policy definition fields
    Closes #556.
    References rabbitmq/rabbitmq-server#1501.
    
    [#155629575]

--- deps/rabbitmq_management/priv/www/js/global.js
+++ deps/rabbitmq_management/priv/www/js/global.js
@@ -403,7 +403,9 @@ var HELP = {
     if <code>ha-mode</code> is <code>exactly</code>, or a list\
     of strings if <code>ha-mode</code> is <code>nodes</code>.',
 
-    'policy-ha-sync-mode' : 'One of <code>manual</code> or <code>automatic</code>.',
+    'policy-ha-sync-mode' : 'One of <code>manual</code> or <code>automatic</code>. <a target="_blank" href="http://www.rabbitmq.com/ha.html#unsynchronised-mirrors">Learn more</a>',
+
+    'policy-ha-promote-on-shutdown' : 'One of <code>when-synced</code> or <code>always</code>. <a target="_blank" href="http://www.rabbitmq.com/ha.html#unsynchronised-mirrors">Learn more</a>',
 
     'policy-federation-upstream-set' :
     'A string; only if the federation plugin is enabled. Chooses the name of a set of upstreams to use with federation, or "all" to use all upstreams. Incompatible with <code>federation-upstream</code>.',
