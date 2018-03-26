rabbitmq/rabbitmq-server

commit cac9583e1bb2705be7f06c2ab7f416a75d11c875

Start/stop multi-node quorum queue
[#154472231]
[#154472236]

src/rabbit_amqqueue.erl
@@ -306,7 +306,7 @@ find_durable_queues(VHost) ->
                                 %% the policy). Thus, we must check if the local pid is alive
                                 %% - if the record is present - in order to restart.
                                 (mnesia:read(rabbit_queue, Name, read) =:= []
-                                orelse not erlang:is_process_alive(Pid))]))
+                                orelse not is_queue_process_alive(Pid))]))
       end).
 
 find_durable_queues() ->
@@ -322,7 +322,7 @@ find_durable_queues() ->
                                 %% the policy). Thus, we must check if the local pid is alive
                                 %% - if the record is present - in order to restart.
                                 (mnesia:read(rabbit_queue, Name, read) =:= []
-                                orelse not erlang:is_process_alive(Pid))]))
+                                orelse not is_queue_process_alive(Pid))]))
       end).
 
 recover_durable_queues(QueuesAndRecoveryTerms) ->
@@ -707,10 +707,10 @@ qnode(QPid) when is_pid(QPid) ->
 qnode({_, Node}) ->
     Node.
 
-qpid(Pid) when is_pid(Pid) ->
-    Pid;
-qpid({Name, _}) ->
-    whereis(Name).
+is_queue_process_alive(Pid) when is_pid(Pid) ->
+    erlang:is_process_alive(Pid);
+is_queue_process_alive({Name, _}) ->
+    whereis(Name) /= undefined.
 
 list(VHostPath) ->
     list(VHostPath, rabbit_queue).
@@ -1122,6 +1122,8 @@ cancel_sync_mirrors(#amqqueue{pid = QPid}) ->
 cancel_sync_mirrors(QPid) ->
     delegate:invoke(QPid, {gen_server2, call, [cancel_sync_mirrors, infinity]}).
 
+is_mirrored(#amqqueue{type = quorum}) ->
+    true;
 is_mirrored(Q) ->
     rabbit_mirror_queue_misc:is_mirrored(Q).
 
@@ -1177,7 +1179,7 @@ on_node_down(Node) ->
                                   #amqqueue{name = QName, pid = Pid} =
                                   Q <- mnesia:table(rabbit_queue),
                                     qnode(Pid) == Node andalso
-                                    not rabbit_mnesia:is_process_alive(qpid(Pid)) andalso
+                                    not is_queue_process_alive(Pid) andalso
                                     (not rabbit_amqqueue:is_mirrored(Q) orelse
                                      rabbit_amqqueue:is_dead_exclusive(Q))])),
                 {Qs, Dels} = lists:unzip(QsDels),

src/rabbit_channel.erl
@@ -628,36 +628,42 @@ handle_cast({confirm, MsgSeqNos, QPid}, State) ->
     noreply_coalesce(confirm(MsgSeqNos, QPid, State)).
 
 handle_info({ra_event, {Name, _} = From, _} = Evt, #ch{queue_states = QueueStates,
-                                                   consumer_mapping = ConsumerMapping,
-                                                   queue_names = QNames } = State0) ->
-    {ok, QName} = maps:find(From, QNames),
-    FState0 = get_quorum_state(From, QName, QueueStates),
-    case rabbit_quorum_queue:handle_event(Evt, FState0) of
-        {{delivery, CTag, Msgs}, FState1} ->
-            AckRequired = case maps:find(CTag, ConsumerMapping) of
-                              error ->
-                                  true;
-                              {ok, {_, {NoAck, _, _, _}}} ->
-                                  not NoAck
-                          end,
-            FState2 = case AckRequired of
-                          false ->
-                              {MsgIds, _} = lists:unzip(Msgs),
-                              {ok, FS} = rabbit_quorum_queue:ack(CTag, MsgIds, FState1),
-                              FS;
-                          true ->
-                              FState1
-                      end,
-            State = lists:foldl(
-                      fun({MsgId, {MsgHeader, Msg}}, Acc) ->
-                              IsDelivered = maps:is_key(delivery_count, MsgHeader),
-                              handle_deliver(CTag, AckRequired,
-                                             {QName, From, MsgId, IsDelivered, Msg}, Acc)
-                      end, State0#ch{queue_states = maps:put(Name, FState2, QueueStates)}, Msgs),
-            noreply(State);
-        {internal, MsgSeqNos, FState1} ->
-            noreply_coalesce(confirm(MsgSeqNos, From,
-                                     State0#ch{queue_states = maps:put(Name, FState1, QueueStates)}))
+                                                       consumer_mapping = ConsumerMapping
+                                                      } = State0) ->
+    case ets:lookup(quorum_mapping, Name) of
+        [{_, QName}] ->
+            FState0 = get_quorum_state(From, QName, QueueStates),
+            case rabbit_quorum_queue:handle_event(Evt, FState0) of
+                {{delivery, CTag, Msgs}, FState1} ->
+                    AckRequired = case maps:find(CTag, ConsumerMapping) of
+                                      error ->
+                                          true;
+                                      {ok, {_, {NoAck, _, _, _}}} ->
+                                          not NoAck
+                                  end,
+                    FState2 = case AckRequired of
+                                  false ->
+                                      {MsgIds, _} = lists:unzip(Msgs),
+                                      {ok, FS} = rabbit_quorum_queue:ack(CTag, MsgIds, FState1),
+                                      FS;
+                                  true ->
+                                      FState1
+                              end,
+                    State = lists:foldl(
+                              fun({MsgId, {MsgHeader, Msg}}, Acc) ->
+                                      IsDelivered = maps:is_key(delivery_count, MsgHeader),
+                                      handle_deliver(CTag, AckRequired,
+                                                     {QName, From, MsgId, IsDelivered, Msg}, Acc)
+                              end, State0#ch{queue_states = maps:put(Name, FState2, QueueStates)}, Msgs),
+                    noreply(State);
+                {internal, MsgSeqNos, FState1} ->
+                    noreply_coalesce(confirm(MsgSeqNos, From,
+                                             State0#ch{queue_states = maps:put(Name, FState1, QueueStates)}));
+                eol ->
+                    noreply_coalesce(State0#ch{queue_states = maps:remove(Name, QueueStates)})
+            end;
+        [] ->
+            noreply_coalesce(State0)
     end;
 
 handle_info({bump_credit, Msg}, State) ->
@@ -706,6 +712,7 @@ handle_info({{Ref, Node}, LateAnswer}, State = #ch{channel = Channel})
     rabbit_log_channel:warning("Channel ~p ignoring late answer ~p from ~p",
         [Channel, LateAnswer, Node]),
     noreply(State);
+
 handle_info(queue_cleanup, State = #ch{queue_states = QueueStates0}) ->
     QueueStates =
         maps:filter(fun(Name, _) ->

src/rabbit_quorum_queue.erl
@@ -98,10 +98,14 @@ declare(#amqqueue{name = QName,
     check_invalid_arguments(QName, Arguments),
     RaName = qname_to_rname(QName),
     Id = {RaName, node()},
-    NewQ = Q#amqqueue{pid = Id},
+    Nodes = rabbit_mnesia:cluster_nodes(all),
+    NewQ = Q#amqqueue{pid = Id,
+                      quorum_nodes = Nodes},
     ets:insert(quorum_mapping, {RaName, QName}),
-    ok = ra:start_node(ra_node_config(NewQ)),
-    _ = ra_node_proc:trigger_election(Id),
+    RaMachine = {module, ra_fifo,
+                 #{dead_letter_handler => dlx_mfa(NewQ)}},
+    {ok, _Started, _NotStarted} = ra:start_cluster(RaName, RaMachine,
+                                                   [{RaName, Node} || Node <- Nodes]),
     FState = init_state(Id),
     _ = rabbit_amqqueue:internal_declare(NewQ, false),
     OwnerPid = case ExclusiveOwner of
@@ -130,12 +134,12 @@ stop(VHost) ->
     _ = [ra:stop_node(Pid) || #amqqueue{pid = Pid} <- find_quorum_queues(VHost)],
     ok.
 
-delete(#amqqueue{ type = quorum, pid = {Name, _} = QPid, name = QName},
+delete(#amqqueue{ type = quorum, pid = {Name, _}, name = QName, quorum_nodes = QNodes},
        _IfUnused, _IfEmpty, ActingUser) ->
     %% TODO Quorum queue needs to support consumer tracking for IfUnused
     Msgs = quorum_messages(Name),
     _ = rabbit_amqqueue:internal_delete(QName, ActingUser),
-    ok = ra:delete_node(QPid),
+    ok = ra:delete_cluster([{Name, Node} || Node <- QNodes]),
     rabbit_core_metrics:queue_deleted(QName),
     ets:delete(quorum_mapping, Name),
     {ok, Msgs}.
@@ -143,7 +147,7 @@ delete(#amqqueue{ type = quorum, pid = {Name, _} = QPid, name = QName},
 delete_immediately({Name, _} = QPid) ->
     [{_, QName}] = ets:lookup(quorum_mapping, Name),
     _ = rabbit_amqqueue:internal_delete(QName, ?INTERNAL_USER),
-    ok = ra:delete_node(QPid),
+    ok = ra:delete_cluster([QPid]),
     rabbit_core_metrics:queue_deleted(QName),
     ets:delete(quorum_mapping, Name),
     ok.
@@ -214,20 +218,6 @@ stat(_Q) ->
     {ok, 0, 0}.  %% TODO length, consumers count
 
 %%----------------------------------------------------------------------------
-
-ra_node_config(#amqqueue{pid = {Name, _} = Id} = Q) ->
-    {ok, DataDir} = application:get_env(ra, data_dir),
-    UId = atom_to_binary(Name, utf8),
-    #{cluster_id => Name,
-      id => Id,
-      uid => UId,
-      log_module => ra_log_file,
-      log_init_args => #{data_dir => DataDir,
-                         uid => UId},
-      initial_nodes => [],
-      machine => {module, ra_fifo,
-                  #{dead_letter_handler => dlx_mfa(Q)}}}.
-
 dlx_mfa(#amqqueue{name = Resource} = Q) ->
     #resource{virtual_host = VHost} = Resource,
     DLX = init_dlx(args_policy_lookup(<<"dead-letter-exchange">>, fun res_arg/2, Q), Q),
@@ -337,7 +327,7 @@ i(garbage_collection, #amqqueue{pid = {Name, _}}) ->
 i(_K, _Q) -> ''.
 
 quorum_messages(Name) ->
-    [{_, Enqueue, _, Settle, _}] = ets:lookup(ra_fifo_metrics, Name),
+    S = [{_, Enqueue, _, Settle, _}] = ets:lookup(ra_fifo_metrics, Name),
     Enqueue - Settle.
 
 quorum_ctag(Int) when is_integer(Int) ->

src/rabbit_upgrade_functions.erl
@@ -102,6 +102,7 @@
 -spec queue_options() -> 'ok'.
 -spec queue_type() -> 'ok'.
 -spec queue_created_at() -> 'ok'.
+-spec queue_quorum_nodes() -> 'ok'.
 -spec exchange_options() -> 'ok'.
 
 
@@ -620,6 +621,27 @@ queue_created_at(Table) ->
        sync_slave_pids, recoverable_slaves, policy, operator_policy,
        gm_pids, decorators, state, policy_version, slave_pids_pending_shutdown, vhost, options,
        type, created_at]).
+
+queue_quorum_nodes() ->
+    ok = queue_quorum_nodes(rabbit_queue),
+    ok = queue_quorum_nodes(rabbit_durable_queue),
+    ok.
+
+queue_quorum_nodes(Table) ->
+    transform(
+      Table,
+      fun ({amqqueue, Name, Durable, AutoDelete, ExclusiveOwner, Arguments,
+            Pid, SlavePids, SyncSlavePids, DSN, Policy, OperatorPolicy, GmPids, Decorators,
+            State, PolicyVersion, SlavePidsPendingShutdown, VHost, Options, Type, CreatedAt}) ->
+              {amqqueue, Name, Durable, AutoDelete, ExclusiveOwner, Arguments,
+               Pid, SlavePids, SyncSlavePids, DSN, Policy, OperatorPolicy, GmPids, Decorators,
+               State, PolicyVersion, SlavePidsPendingShutdown, VHost, Options, Type, CreatedAt,
+               undefined}
+      end,
+      [name, durable, auto_delete, exclusive_owner, arguments, pid, slave_pids,
+       sync_slave_pids, recoverable_slaves, policy, operator_policy,
+       gm_pids, decorators, state, policy_version, slave_pids_pending_shutdown, vhost, options,
+       type, created_at, quorum_nodes]).
 
 %% Prior to 3.6.0, passwords were hashed using MD5, this populates
 %% existing records with said default.  Users created with 3.6.0+ will

commit 46da2655936128da51e5dd763446cc7ac482c9ec

Use rabbit_mnesia:is_process_alive
Fixes bug introduced by cac9583e1bb2705be7f06c2ab7f416a75d11c875
[#154472231]

src/rabbit_amqqueue.erl
@@ -718,7 +718,7 @@ qnode({_, Node}) ->
     Node.
 
 is_queue_process_alive(Pid) when is_pid(Pid) ->
-    erlang:is_process_alive(Pid);
+    rabbit_mnesia:is_process_alive(Pid);
 is_queue_process_alive({Name, _}) ->
     whereis(Name) /= undefined.
 