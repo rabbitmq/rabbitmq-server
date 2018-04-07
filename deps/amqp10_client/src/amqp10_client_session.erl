b8e5b4e0a0de1af299c07405cb773da37cbae687
Patrick Detlefsen <patrickdet@users.noreply.github.com>
2018-04-07 18:24:24 +0200

    translate_filters tests

--- deps/amqp10_client/src/amqp10_client_session.erl
+++ deps/amqp10_client/src/amqp10_client_session.erl
@@ -657,43 +657,41 @@ translate_terminus_durability(none) -> 0;
 translate_terminus_durability(configuration) -> 1;
 translate_terminus_durability(unsettled_state) -> 2.
 
-translate_filters(#{}) -> undefined;
-translate_filters(Filters) ->
-    {
-        map,
-        maps:fold(
-            fun(<<"apache.org:legacy-amqp-direct-binding:string">> = K, V, Acc) when is_binary(V) ->
-                [{{symbol, K}, {utf8, V}} | Acc];
-            (<<"apache.org:legacy-amqp-topic-binding:string">> = K, V, Acc) when is_binary(V) ->
-                [{{symbol, K}, {utf8, V}} | Acc];
-            (<<"apache.org:legacy-amqp-headers-binding:map">> = K, V, Acc) when is_map(V) ->
-                [{{symbol, K}, translate_legacy_amqp_headers_binding(V)} | Acc];
-            (<<"apache.org:no-local-filter:list">> = K, V, Acc) when is_list(V) ->
-                [{{symbol, K}, lists:map(fun(Id) -> {utf8, Id} end, V)} | Acc];
-            (<<"apache.org:selector-filter:string">> = K, V, Acc) when is_binary(V) ->
-                [{{symbol, K}, {utf8, V}} | Acc]
-            end,
-            [],
-            Filters)
-    }.
+translate_filters(Filters) when is_map(Filters) andalso map_size(Filters) =< 0 -> undefined;
+translate_filters(Filters) when is_map(Filters) -> {
+    map,
+    maps:fold(
+        fun(<<"apache.org:legacy-amqp-direct-binding:string">> = K, V, Acc) when is_binary(V) ->
+            [{{symbol, K}, {utf8, V}} | Acc];
+        (<<"apache.org:legacy-amqp-topic-binding:string">> = K, V, Acc) when is_binary(V) ->
+            [{{symbol, K}, {utf8, V}} | Acc];
+        (<<"apache.org:legacy-amqp-headers-binding:map">> = K, V, Acc) when is_map(V) ->
+            [{{symbol, K}, translate_legacy_amqp_headers_binding(V)} | Acc];
+        (<<"apache.org:no-local-filter:list">> = K, V, Acc) when is_list(V) ->
+            [{{symbol, K}, lists:map(fun(Id) -> {utf8, Id} end, V)} | Acc];
+        (<<"apache.org:selector-filter:string">> = K, V, Acc) when is_binary(V) ->
+            [{{symbol, K}, {utf8, V}} | Acc]
+        end,
+        [],
+        Filters)
+}.
 
 % https://people.apache.org/~rgodfrey/amqp-1.0/apache-filters.html
-translate_legacy_amqp_headers_binding(LegacyHeaders) ->
-    {
-        map,
-        maps:fold(
-            fun(<<"x-match">> = K, <<"any">> = V, Acc) ->
-                [{{utf8, K}, {utf8, V}} | Acc];
-            (<<"x-match">> = K, <<"all">> = V, Acc) ->
-                [{{utf8, K}, {utf8, V}} | Acc];
-            (<<"x-",_/binary>>, _, Acc) ->
-                Acc;
-            (K, V, Acc) ->
-                [{{utf8, K}, {utf8, V}} | Acc]
-            end,
-            [],
-            LegacyHeaders)
-    }.
+translate_legacy_amqp_headers_binding(LegacyHeaders) -> {
+    map,
+    maps:fold(
+        fun(<<"x-match">> = K, <<"any">> = V, Acc) ->
+            [{{utf8, K}, {utf8, V}} | Acc];
+        (<<"x-match">> = K, <<"all">> = V, Acc) ->
+            [{{utf8, K}, {utf8, V}} | Acc];
+        (<<"x-",_/binary>>, _, Acc) ->
+            Acc;
+        (K, V, Acc) ->
+            [{{utf8, K}, {utf8, V}} | Acc]
+        end,
+        [],
+        LegacyHeaders)
+}.
 
 send_detach(Send, {detach, OutHandle}, _From, State = #state{links = Links}) ->
     case Links of
@@ -1064,4 +1062,67 @@ handle_link_flow_receiver_test() ->
     true = Outcome#link.drain,
     SenderDC = Outcome#link.delivery_count. % maintain delivery count
 
+translate_filters_empty_map_test() ->
+    undefined = translate_filters(#{}).
+
+translate_filters_legacy_amqp_direct_binding_filter_test() ->
+    {map,
+        [{
+            {symbol,<<"apache.org:legacy-amqp-direct-binding:string">>},
+            {utf8,<<"my topic">>}
+        }]
+    } = translate_filters(#{<<"apache.org:legacy-amqp-direct-binding:string">> => <<"my topic">>}).
+
+translate_filters_legacy_amqp_topic_binding_filter_test() ->
+    {map,
+        [{
+            {symbol, <<"apache.org:legacy-amqp-topic-binding:string">>},
+            {utf8, <<"*.stock.#">>}
+        }]
+    } = translate_filters(#{<<"apache.org:legacy-amqp-topic-binding:string">> => <<"*.stock.#">>}).
+
+translate_filters_legacy_amqp_headers_binding_filter_test() ->
+    {map,
+        [{
+            {symbol,<<"apache.org:legacy-amqp-headers-binding:map">>},
+            {map, [
+                    {{utf8, <<"x-match">>}, {utf8, <<"all">>}},
+                    {{utf8, <<"foo">>}, {utf8, <<"bar">>}},
+                    {{utf8, <<"bar">>}, {utf8, <<"baz">>}}
+                ]
+            }
+        }]
+    } = translate_filters(#{<<"apache.org:legacy-amqp-headers-binding:map">> => #{
+            <<"x-match">> => <<"all">>,
+            <<"x-something">> => <<"ignored">>,
+            <<"bar">> => <<"baz">>,
+            <<"foo">> => <<"bar">>
+        }}).
+
+translate_filters_legacy_amqp_no_local_filter_test() ->
+    {map,
+        [{
+            {symbol, <<"apache.org:no-local-filter:list">>},
+            [{utf8, <<"foo">>}, {utf8, <<"bar">>}]
+        }]
+    } = translate_filters(#{<<"apache.org:no-local-filter:list">> => [<<"foo">>, <<"bar">>]}).
+
+translate_filters_selector_filter_test() ->
+    {map,
+        [{
+            {symbol, <<"apache.org:selector-filter:string">>},
+            {utf8, <<"amqp.annotation.x-opt-enqueuedtimeutc > 123456789">>}
+        }]
+    } = translate_filters(#{<<"apache.org:selector-filter:string">> => <<"amqp.annotation.x-opt-enqueuedtimeutc > 123456789">>}).
+
+translate_filters_multiple_filters_test() ->
+    {map,
+        [
+            {{symbol, <<"apache.org:selector-filter:string">>}, {utf8, <<"amqp.annotation.x-opt-enqueuedtimeutc > 123456789">>}},
+            {{symbol, <<"apache.org:legacy-amqp-direct-binding:string">>}, {utf8,<<"my topic">>}}
+        ]
+    } = translate_filters(#{
+            <<"apache.org:legacy-amqp-direct-binding:string">> => <<"my topic">>,
+            <<"apache.org:selector-filter:string">> => <<"amqp.annotation.x-opt-enqueuedtimeutc > 123456789">>
+        }).
 -endif.
