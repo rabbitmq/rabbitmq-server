99e9a354dffc7479e5cba29fb14c85dd5ae892c6
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2018-04-11 10:44:25 +0200

    rabbitmq-components.mk: Update other third-party dependencies
    * Cowboy: 2.2.2 -> 2.3.0
    * Cowlib: 2.1.0 -> 2.2.1
    * jsx:    2.8.2 -> 2.9.0
    * Lager:  3.5.1 -> 3.6.1
    * recon:  2.3.2 -> 2.3.4

--- deps/rabbit_common/mk/rabbitmq-components.mk
+++ deps/rabbit_common/mk/rabbitmq-components.mk
@@ -111,13 +111,13 @@ dep_rabbitmq_public_umbrella          = git_rmq rabbitmq-public-umbrella $(curre
 # all projects use the same versions. It avoids conflicts and makes it
 # possible to work with rabbitmq-public-umbrella.
 
-dep_cowboy = hex 2.2.2
-dep_cowlib = hex 2.1.0
-dep_jsx = hex 2.8.2
-dep_lager = hex 3.5.1
+dep_cowboy = hex 2.3.0
+dep_cowlib = hex 2.2.1
+dep_jsx = hex 2.9.0
+dep_lager = hex 3.6.1
 dep_ranch = hex 1.5.0
 dep_ranch_proxy_protocol = hex 1.5.0
-dep_recon = hex 2.3.2
+dep_recon = hex 2.3.4
 
 dep_sockjs = git https://github.com/rabbitmq/sockjs-erlang.git 405990ea62353d98d36dbf5e1e64942d9b0a1daf
 
