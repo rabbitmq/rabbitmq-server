8327cbfbd5eb1ded6e12e96b5360b8c1b8ae9527
kjnilsson <knilsson@pivotal.io>
2018-04-24 16:53:11 +0100

    Improve use of untracked_enqueue
    Choose the persisted leader id instead of just using the id of the
    leader at point of creation.

--- src/rabbit_amqqueue.erl
+++ src/rabbit_amqqueue.erl
@@ -1327,8 +1327,11 @@ deliver(Qs, Delivery = #delivery{flow = Flow,
     {QPids ++ QuorumPids, QueueState}.
 
 qpids([]) -> {[], [], []}; %% optimisation
-qpids([#amqqueue{pid = QPid, type = quorum, name = QName}]) -> {[{QPid, QName}], [], []}; %% opt
-qpids([#amqqueue{pid = QPid, slave_pids = SPids}]) -> {[], [QPid], SPids}; %% opt
+qpids([#amqqueue{quorum_leader = LeaderNode, pid = {LocalName, _},
+                 type = quorum, name = QName}]) ->
+    {[{{LocalName, LeaderNode}, QName}], [], []}; %% opt
+qpids([#amqqueue{pid = QPid, slave_pids = SPids}]) ->
+    {[], [QPid], SPids}; %% opt
 qpids(Qs) ->
     {QuoPids, MPids, SPids} =
         lists:foldl(fun (#amqqueue{pid = QPid, type = quorum, name = QName},
