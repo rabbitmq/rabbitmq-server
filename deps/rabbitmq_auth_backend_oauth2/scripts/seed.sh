3ed9d7eeb0576c42d5b2ea9b8fc518c8083c2c54
Michael Klishin <michael@clojurewerkz.org>
2018-07-09 09:07:33 +0300

    [Re]add tag authorities to the seed script
    This is the same change as in b183a88cb68afe175d221381ff477c1b78ea08bf except
    it doesn't unintentionally limit the audience ("aud" field) of generated access tokens.
    
    N.B. that commas are treated as separators on the client authority list,
    so our original convention for tags, e.g. "rabbitmq.tags:management,policymaker",
    won't work with UAA.
    
    [#158782152]
    [#158782156]

--- deps/rabbitmq_auth_backend_oauth2/scripts/seed.sh
+++ deps/rabbitmq_auth_backend_oauth2/scripts/seed.sh
@@ -2,10 +2,11 @@
 
 uaac token client get admin -s adminsecret
 
+uaac client delete rabbit_client
 uaac client add rabbit_client --name rabbit_client \
  --secret rabbit_secret \
  --authorized_grant_types client_credentials \
- --authorities 'rabbitmq.read:*/* rabbitmq.write:*/* rabbitmq.configure:*/*'
+ --authorities 'rabbitmq.read:*/* rabbitmq.write:*/* rabbitmq.configure:*/* rabbitmq.tag:management rabbitmq.tag:administrator'
  --access_token_validity 86400
 
 uaac token client get rabbit_client -s rabbit_secret
