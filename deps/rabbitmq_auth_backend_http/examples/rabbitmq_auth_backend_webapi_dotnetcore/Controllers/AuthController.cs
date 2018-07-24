c956605c583aa30ea7f1c831be86941d33e2955a
xneg <xnegxneg@gmail.com>
2018-07-24 11:37:42 +0300

    ASP .NET Core 2.1 example
    (cherry picked from commit 4041017394bfc5e44d9ddc18d915b65ec6cd7666)

---
+++ deps/rabbitmq_auth_backend_http/examples/rabbitmq_auth_backend_webapi_dotnetcore/Controllers/AuthController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMqAuthBackendHttp.Requests;
+
+namespace RabbitMqAuthBackendHttp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AuthController";
+        }
+
+        [Route("user")]
+        [HttpPost]
+        public IActionResult CheckUser([FromForm]UserAuthRequest request)
+        {
+            var tags = new [] {"administrator", "management"};
+
+            try
+            {
+                var userlog = string.Format("user : {0}, password : {1}", request.UserName, request.Password);
+                Console.WriteLine(userlog);
+
+                if (request.UserName == "authuser") //Sample check you can put your custom logic over here
+                    return AuthResult.Deny();
+
+            }
+            catch (Exception ex)
+            {
+                //check or log error
+            }
+
+            return AuthResult.Allow(tags);
+        }
+
+        [Route("vhost")]
+        [HttpPost]
+        public IActionResult CheckVhost([FromForm]VhostAuthRequest request)
+        {
+            try
+            {
+                var userlog = string.Format("user : {0}, ip : {1}", request.UserName, request.Ip);
+                Console.WriteLine(userlog);
+
+                if (request.UserName == "authuser") //Sample checks you can put your custom logic over here
+                    return AuthResult.Deny();
+            }
+            catch (Exception ex)
+            {
+                //check or log error
+            }
+
+            return AuthResult.Allow();
+        }
+
+        [Route("resource")]
+        [HttpPost]
+        public IActionResult CheckResource([FromForm]ResourceAuthRequest request)
+        {
+            try
+            {
+                var userlog = $"user : {request.UserName}, vhost : {request.Vhost}, resource : {request.Resource}, " +
+                              $"name : {request.Name}, permission : {request.Permission}";
+                Console.WriteLine(userlog);
+
+                if (request.UserName == "authuser") //Sample checks you can put your custom logic over here
+                    return AuthResult.Deny();
+            }
+            catch (Exception ex)
+            {
+                //check or log error
+            }
+
+            return AuthResult.Allow();
+        }
+
+        [Route("topic")]
+        [HttpPost]
+        public IActionResult CheckTopic([FromForm]TopicAuthRequest request)
+        {
+            try
+            {
+                var userlog = $"user : {request.UserName}, vhost : {request.Vhost}, resource : {request.Resource}, " +
+                              $"name : {request.Name}, permission : {request.Permission}";
+                Console.WriteLine(userlog);
+
+                if (request.UserName == "authuser") //Sample checks you can put your custom logic over here
+                    return AuthResult.Deny();
+            }
+            catch (Exception ex)
+            {
+                //check or log error
+            }
+
+            return AuthResult.Allow();
+        }
+    }
+}
---
+++ deps/rabbitmq_auth_backend_http/examples/rabbitmq_auth_backend_webapi_dotnetcore/Requests/VhostAuthRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace RabbitMqAuthBackendHttp.Requests
+{
+    public class VhostAuthRequest
+    {
+        public string UserName { get; set; }
+
+        public string Vhost { get; set; }
+
+        public string Ip { get; set; }
+    }
+}
+ 
