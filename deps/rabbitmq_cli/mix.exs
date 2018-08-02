676e27aaa490e3a2ff5ef24b3506962ba0be3f00
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2018-08-02 18:04:03 +0200

    mix.exs: Require Elixir >= 1.6.6 and < 1.8.0
    Elixir 1.6.6 is the first version to support Erlang 21.0. Bumping the
    minimum to this version eases packaging: packagers don't have to think
    about Erlang/Elixir combinations which work.
    
    The upper limit is set to 1.8.0 (not included) because 1.7.x should work
    fine. However, 1.8.0 will probably drop Erlang 19.x support, at which
    point we should review this version pinning.
    
    [#159500280]
    
    (cherry picked from commit 746eca51aa3def1aeed061c3dbd072ed3e5cd9df)

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -19,8 +19,8 @@ defmodule RabbitMQCtl.MixfileBase do
   def project do
     [
       app: :rabbitmqctl,
-      version: "3.7.5",
-      elixir: "~> 1.6.0",
+      version: "3.7.8",
+      elixir: ">= 1.6.6 and < 1.8.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       escript: [main_module: RabbitMQCtl,
