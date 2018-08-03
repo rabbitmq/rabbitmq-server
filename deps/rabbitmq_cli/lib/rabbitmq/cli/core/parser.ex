4f3b2d39bdd932dab98fc5bef489483da51a701b
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2018-08-03 13:17:09 +0200

    RabbitMQ.CLI.Core.Parser: Set `allow_nonexistent_atoms` in OptionParser
    There is a change of behavior in OptionParser between Elixir 1.6.x and
    1.7.x. It's probably the deprecation documented in Elixir 1.7.0 release
    notes:
    
        [OptionParser] Deprecate dynamic option parser mode that depended on
        atoms to be previously loaded and therefore behaved inconsistently
    
    Setting the `allow_nonexistent_atoms` to true allows to restore the
    behavior from Elixir 1.6.x, thus giving a consistent parsing with both
    versions of Elixir.
    
    [#159500280]

--- deps/rabbitmq_cli/lib/rabbitmq/cli/core/parser.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/core/parser.ex
@@ -141,7 +141,12 @@ defmodule RabbitMQ.CLI.Core.Parser do
     switches = default_switches()
     aliases = default_aliases()
     {options, tail, invalid} =
-      OptionParser.parse_head(input, strict: switches, aliases: aliases)
+      OptionParser.parse_head(
+        input,
+        strict: switches,
+        aliases: aliases,
+        allow_nonexistent_atoms: true,
+      )
     norm_options = normalize_options(options, switches) |> Map.new
     {norm_options, tail, invalid}
   end
@@ -156,7 +161,8 @@ defmodule RabbitMQ.CLI.Core.Parser do
     {options, args, invalid} = OptionParser.parse(
       input,
       strict: switches,
-      aliases: aliases
+      aliases: aliases,
+      allow_nonexistent_atoms: true,
     )
     norm_options = normalize_options(options, switches) |> Map.new
     {args, norm_options, invalid}
