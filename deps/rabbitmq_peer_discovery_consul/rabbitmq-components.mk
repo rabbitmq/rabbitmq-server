e3fe7e73d66252c3898af8dd46ea2f3bdd67bb4d
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2018-09-11 14:22:36 +0200

    Update rabbitmq-components.mk

--- deps/rabbitmq_peer_discovery_consul/rabbitmq-components.mk
+++ deps/rabbitmq_peer_discovery_consul/rabbitmq-components.mk
@@ -114,9 +114,9 @@ dep_rabbitmq_public_umbrella          = git_rmq rabbitmq-public-umbrella $(curre
 dep_cowboy = hex 2.4.0
 dep_cowlib = hex 2.3.0
 dep_jsx = hex 2.9.0
-dep_lager = hex 3.6.4
-dep_ranch = hex 1.5.0
-dep_ranch_proxy_protocol = hex 1.5.0
+dep_lager = hex 3.6.5
+dep_ranch = hex 1.6.1
+dep_ranch_proxy_protocol = hex 2.1.0-rc.1
 dep_recon = hex 2.3.6
 
 dep_sockjs = git https://github.com/rabbitmq/sockjs-erlang.git 405990ea62353d98d36dbf5e1e64942d9b0a1daf
