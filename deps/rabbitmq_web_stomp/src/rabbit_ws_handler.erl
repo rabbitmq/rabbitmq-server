4194e10d5c1aff0f2f900e05d6bf0739cb43d63e
Michael Klishin <mklishin@pivotal.io>
2018-10-08 21:44:17 +0300

    Enable compression by default
    References #89, #90.
    
    [#161053760]

--- deps/rabbitmq_web_stomp/src/rabbit_ws_handler.erl
+++ deps/rabbitmq_web_stomp/src/rabbit_ws_handler.erl
@@ -52,7 +52,8 @@ init(Req0, Opts) ->
         undefined -> [];
         AuthHd    -> [{authorization, binary_to_list(AuthHd)}]
     end,
-    WsOpts = proplists:get_value(ws_opts, Opts, #{}),
+    WsOpts0 = proplists:get_value(ws_opts, Opts, #{}),
+    WsOpts  = maps:merge(#{compress => true}, WsOpts0),
     {cowboy_websocket, Req, {Socket, Peername, Sockname, Headers, FrameType}, WsOpts}.
 
 websocket_init({Socket, Peername, Sockname, Headers, FrameType}) ->
