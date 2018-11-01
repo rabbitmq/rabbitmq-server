c08f2202c8ed971124cf4d17e224c7095f8ec218
Andrey Somov <public.somov@gmail.com>
2018-11-01 17:15:40 +0100

    Add Kotlin project created by SPRING INITIALIZR

---
+++ deps/rabbitmq_auth_backend_http/examples/rabbitmq-auth-backend-spring-boot-kotlin/src/main/kotlin/com/rabbitmq/examples/rabbitmqauthbackendspringbootkotlin/RabbitmqAuthBackendSpringBootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.rabbitmq.examples.rabbitmqauthbackendspringbootkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RabbitmqAuthBackendSpringBootKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<RabbitmqAuthBackendSpringBootKotlinApplication>(*args)
+}
