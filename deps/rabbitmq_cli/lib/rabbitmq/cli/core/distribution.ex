33ea0138e80cafed0e45b33a6f24893597f63952
Luke Bakken <luke@bakken.io>
2018-11-01 09:31:43 -0700

    Merge pull request #271 from rabbitmq/rabbitmq-cli-270
    Ensure cli name ends with correct hostname
    
    (cherry picked from commit 2f16936e364a129c0a8358889649f6927843bcd5)

--- deps/rabbitmq_cli/lib/rabbitmq/cli/core/distribution.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/core/distribution.ex
@@ -13,9 +13,8 @@
 ## The Initial Developer of the Original Code is Pivotal Software, Inc.
 ## Copyright (c) 2016-2017 Pivotal Software, Inc.  All rights reserved.
 
-alias RabbitMQ.CLI.Core.Config
-
 defmodule RabbitMQ.CLI.Core.Distribution do
+  alias RabbitMQ.CLI.Core.{Config, Helpers}
 
   #
   # API
@@ -41,9 +40,12 @@ defmodule RabbitMQ.CLI.Core.Distribution do
   ## Optimization. We try to start EPMD only if distribution fails
   def start_with_epmd(node_name, node_name_type) do
     case :net_kernel.start([node_name, node_name_type]) do
-      {:ok, _} = ok -> ok;
-      {:error, {:already_started, _}} = started -> started;
-      {:error, {{:already_started, _}, _}} = started -> started;
+      {:ok, _} = ok ->
+        ok
+      {:error, {:already_started, _}} = started ->
+        started
+      {:error, {{:already_started, _}, _}} = started ->
+        started
       ## EPMD can be stopped. Retry with EPMD
       {:error, _} ->
         :rabbit_nodes_common.ensure_epmd()
@@ -57,9 +59,11 @@ defmodule RabbitMQ.CLI.Core.Distribution do
 
   def ensure_cookie(options) do
     case Config.get_option(:erlang_cookie, options) do
-      nil    -> :ok;
-      cookie -> Node.set_cookie(cookie)
-                :ok
+      nil ->
+        :ok
+      cookie ->
+        Node.set_cookie(cookie)
+        :ok
     end
   end
 
@@ -69,36 +73,80 @@ defmodule RabbitMQ.CLI.Core.Distribution do
 
   defp start(node_name_type, attempts, _last_err) do
     candidate = generate_cli_node_name(node_name_type)
+
     case start_with_epmd(candidate, node_name_type) do
-      {:ok, _} -> :ok
-      {:error, {:already_started, pid}}      -> {:ok, pid};
-      {:error, {{:already_started, pid}, _}} -> {:ok, pid};
+      {:ok, _} ->
+        :ok
+      {:error, {:already_started, pid}} ->
+        {:ok, pid}
+      {:error, {{:already_started, pid}, _}} ->
+        {:ok, pid}
       {:error, reason} ->
         start(node_name_type, attempts - 1, reason)
     end
   end
 
   defp generate_cli_node_name(node_name_type) do
-    base                 = "rabbitmqcli" <> to_string(:rabbit_misc.random(100))
+    rmq_hostname = Helpers.get_rabbit_hostname()
+    base = "rabbitmqcli-#{:os.getpid()}-#{rmq_hostname}"
     inet_resolver_config = :inet.get_rc()
 
     case {node_name_type, Keyword.get(inet_resolver_config, :domain)} do
-      {:longnames, nil} ->
-        generate_dot_no_domain_name(base);
-      {:longnames, ""}  ->
-        generate_dot_no_domain_name(base);
+      {:longnames, domain} ->
+        # Distribution will fail to start if it's unable to
+        # determine FQDN of a node (with at least one dot in
+        # the name).
+        ensure_ends_with_domain(base, domain)
       _ ->
         base
-    end |> String.to_atom
-
+    end |> String.to_atom()
   end
 
-  defp generate_dot_no_domain_name(base) do
-    # Distribution will fail to start if it's unable to
-    # determine FQDN of a node (with at least one dot in
-    # the name).
-    # The CLI always acts as a connection initiator, so it
-    # doesn't matter if the name will not resolve.
-    base <> "@" <> to_string(:inet_db.gethostname()) <> ".no-domain"
+  defp ensure_ends_with_domain(base, nil) do
+    maybe_add_domain(base)
+  end
+
+  defp ensure_ends_with_domain(base, "") do
+    maybe_add_domain(base)
+  end
+
+  defp ensure_ends_with_domain(base, domain) do
+    case String.ends_with?(base, to_string(domain)) do
+      true ->
+        base
+
+      false ->
+        "#{base}.#{domain}"
+    end
+  end
+
+  defp maybe_add_domain(base) do
+    # :inet:get_rc() didn't return a useful domain
+    # so come up with one
+    parts = String.split(base, "@", parts: 2)
+    maybe_add_domain(base, parts)
+  end
+
+  defp maybe_add_domain(base, [_, ""]) do
+    # base ends with an @, weird but possible?
+    hostname = Helpers.hostname()
+    "#{base}#{hostname}.localdomain"
+  end
+
+  defp maybe_add_domain(base, [_, domain]) do
+    case String.contains?(domain, ".") do
+      true ->
+        base
+      false ->
+        # the domain part of base does not contain
+        # a dot, so assume no full domain. Append
+        # one here
+        "#{base}.localdomain"
+    end
+  end
+
+  defp maybe_add_domain(base, [_]) do
+    hostname = Helpers.hostname()
+    "#{base}@#{hostname}.localdomain"
   end
 end
