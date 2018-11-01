72b5c7c34ff41897aefa9529258755f9af21c35a
Andrey Somov <public.somov@gmail.com>
2018-11-01 17:21:16 +0100

    Rename classes to be shorter

--- deps/rabbitmq_auth_backend_http/examples/rabbitmq-auth-backend-spring-boot-kotlin/src/test/kotlin/com/rabbitmq/examples/RabbitmqAuthBackendApplicationTests.kt
+++ deps/rabbitmq_auth_backend_http/examples/rabbitmq-auth-backend-spring-boot-kotlin/src/test/kotlin/com/rabbitmq/examples/RabbitmqAuthBackendApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class RabbitmqAuthBackendSpringBootKotlinApplicationTests {
+class RabbitmqAuthBackendApplicationTests {
 
 	@Test
 	fun contextLoads() {
