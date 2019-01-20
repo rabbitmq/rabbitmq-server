431a184990be95714ff7d812de83aad74b5fc3fc
Michael Klishin <mklishin@pivotal.io>
2019-01-20 09:07:37 +0300

    Be more resilient to net_kernel internal process management

--- deps/rabbitmq_cli/test/distribution_test.exs
+++ deps/rabbitmq_cli/test/distribution_test.exs
@@ -28,7 +28,12 @@ defmodule DistributionTest do
       :net_kernel.stop()
       System.delete_env("RABBITMQ_ERLANG_COOKIE")
     end)
-    :nocookie = Node.get_cookie()
+    try do
+      :nocookie = Node.get_cookie()
+    catch
+      # one of net_kernel processes is not running ¯\_(ツ)_/¯
+      :exit, _ -> :ok
+    end
     System.put_env("RABBITMQ_ERLANG_COOKIE", "mycookie")
     opts = %{}
     Distribution.start(opts)
@@ -39,7 +44,12 @@ defmodule DistributionTest do
     on_exit(fn ->
       :net_kernel.stop()
     end)
-    :nocookie = Node.get_cookie()
+    try do
+      :nocookie = Node.get_cookie()
+    catch
+      # one of net_kernel processes is not running ¯\_(ツ)_/¯
+      :exit, _ -> :ok
+    end
     opts = %{erlang_cookie: :mycookie}
     Distribution.start(opts)
     :mycookie = Node.get_cookie()
