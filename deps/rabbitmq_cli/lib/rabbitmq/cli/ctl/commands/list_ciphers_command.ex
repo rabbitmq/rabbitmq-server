de973f47b66e28009fa984eab4709869648752ed
Michael Klishin <mklishin@pivotal.io>
2019-02-03 04:32:37 +0300

    Make all list_* commands available to rabbitmq-diagnostics
    (cherry picked from commit bf00712addf799585902ff04579a80059267f035)
    
    Conflicts:
    	lib/rabbitmq/cli/ctl/commands/list_feature_flags_command.ex

--- deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/list_ciphers_command.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/list_ciphers_command.ex
@@ -17,6 +17,8 @@ defmodule RabbitMQ.CLI.Ctl.Commands.ListCiphersCommand do
   @behaviour RabbitMQ.CLI.CommandBehaviour
   use RabbitMQ.CLI.DefaultOutput
 
+  def scopes(), do: [:ctl, :diagnostics]
+
   def merge_defaults(args, opts) do
     {args, opts}
   end
