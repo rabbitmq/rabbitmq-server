8330c8823868d8ab19f98868c53a952cb094c14f
Josh Soref <jsoref@users.noreply.github.com>
2019-02-12 00:12:43 -0500

    spelling: javascript
    (cherry picked from commit d741ee8630c0ae4dffe05f7e4dce1aaeea0f8c45)

--- deps/rabbit_common/erlang.mk
+++ deps/rabbit_common/erlang.mk
@@ -1340,7 +1340,7 @@ pkg_erlang_cep_commit = master
 
 PACKAGES += erlang_js
 pkg_erlang_js_name = erlang_js
-pkg_erlang_js_description = A linked-in driver for Erlang to Mozilla's Spidermonkey Javascript runtime.
+pkg_erlang_js_description = A linked-in driver for Erlang to Mozilla's Spidermonkey JavaScript runtime.
 pkg_erlang_js_homepage = https://github.com/basho/erlang_js
 pkg_erlang_js_fetch = git
 pkg_erlang_js_repo = https://github.com/basho/erlang_js
