42e13a82fe1dabb6e765ac3746f4bb14359274fd
Josh Soref <jsoref@users.noreply.github.com>
2019-02-12 23:51:09 -0500

    spelling: github

--- deps/rabbitmq_management/erlang.mk
+++ deps/rabbitmq_management/erlang.mk
@@ -1100,7 +1100,7 @@ pkg_edns_commit = master
 
 PACKAGES += edown
 pkg_edown_name = edown
-pkg_edown_description = EDoc extension for generating Github-flavored Markdown
+pkg_edown_description = EDoc extension for generating GitHub-flavored Markdown
 pkg_edown_homepage = https://github.com/uwiger/edown
 pkg_edown_fetch = git
 pkg_edown_repo = https://github.com/uwiger/edown
