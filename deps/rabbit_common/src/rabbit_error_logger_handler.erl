860a3bc38fd99f6fab9848ab7c846dcc1dca09b5
Michael Klishin <mklishin@pivotal.io>
2019-02-20 01:27:30 +0300

    Try to make node name mismatch message clearer, provide a hint
    Per recent rabbitmq-users feedback.
    
    (cherry picked from commit 6736eb3629f1ad1b40fbf08962ba09fe2073010d)

--- deps/rabbit_common/src/rabbit_error_logger_handler.erl
+++ deps/rabbit_common/src/rabbit_error_logger_handler.erl
@@ -21,7 +21,7 @@
 -export([start_link/0, add_handler/0]).
 
 %% gen_event callbacks
--export([init/1, handle_event/2, handle_call/2, 
+-export([init/1, handle_event/2, handle_call/2,
          handle_info/2, terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
@@ -158,7 +158,9 @@ code_change(_OldVsn, State, _Extra) ->
 format({check_dflag_xnc_failed, _What}) ->
     {"  * Remote node uses an incompatible Erlang version ~n", []};
 format({recv_challenge_failed, no_node, Node}) ->
-    {"  * Hostname mismatch: node ~p believes its host is different. Please ensure that hostnames resolve the same way locally and on ~p~n", [Node, Node]};
+    {"  * Node name (or hostname) mismatch: node ~p believes its node name is not ~p but something else.~n"
+     "    All nodes and CLI tools must refer to node ~p using the same name the node itself uses (see its logs to find out what it is)~n",
+     [Node, Node, Node]};
 format({recv_challenge_failed, Error}) ->
     {"  * Distribution failed unexpectedly while waiting for challenge: ~p~n", [Error]};
 format({recv_challenge_ack_failed, bad_cookie}) ->
