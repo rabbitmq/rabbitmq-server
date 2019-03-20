9f6d2b449fe88bed6b9a32ceef52b4c3e2bd0988
Michael Klishin <michael@clojurewerkz.org>
2019-03-20 10:24:51 +0300

    Require Erlang 21.3

--- deps/rabbit_common/include/rabbit.hrl
+++ deps/rabbit_common/include/rabbit.hrl
@@ -212,8 +212,8 @@
 
 -define(COPYRIGHT_MESSAGE, "Copyright (C) 2007-2019 Pivotal Software, Inc.").
 -define(INFORMATION_MESSAGE, "Licensed under the MPL.  See http://www.rabbitmq.com/").
--define(OTP_MINIMUM, "21.2").
--define(ERTS_MINIMUM, "10.2").
+-define(OTP_MINIMUM, "21.3").
+-define(ERTS_MINIMUM, "10.3").
 
 %% EMPTY_FRAME_SIZE, 8 = 1 + 2 + 4 + 1
 %%  - 1 byte of frame type
