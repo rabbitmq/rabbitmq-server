3ce9849f272fd71bddcf9a69ab805574032afed1
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2019-04-16 12:06:13 +0200

    rabbit_mgmt_gc_SUITE: Skip `quorum_queue_stats` if quorum queues are unsupported

--- deps/rabbitmq_management_agent/test/rabbit_mgmt_gc_SUITE.erl
+++ deps/rabbitmq_management_agent/test/rabbit_mgmt_gc_SUITE.erl
@@ -86,6 +86,15 @@ init_per_group(_, Config) ->
 end_per_group(_, Config) ->
     Config.
 
+init_per_testcase(quorum_queue_stats = Testcase, Config) ->
+    case rabbit_ct_broker_helpers:enable_feature_flag(Config, quorum_queue) of
+        ok ->
+            rabbit_ct_helpers:testcase_started(Config, Testcase),
+            rabbit_ct_helpers:run_steps(
+              Config, rabbit_ct_client_helpers:setup_steps());
+        Skip ->
+            Skip
+    end;
 init_per_testcase(Testcase, Config) ->
     rabbit_ct_helpers:testcase_started(Config, Testcase),
     rabbit_ct_helpers:run_steps(Config,
