49de3185fc4c9854b6f8de1fdbd5ea56760bab88
Michael Klishin <mklishin@pivotal.io>
2019-04-18 19:14:02 +0400

    Print stack trace if command execution fails and --print-stacktrace was passed
    Per discussion with @dumbbell.
    
    (cherry picked from commit 7615b2067c117a8c20c3afd1da3a9a95485646a2)

--- deps/rabbitmq_cli/lib/rabbitmq/cli/core/parser.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/core/parser.ex
@@ -41,7 +41,8 @@ defmodule RabbitMQ.CLI.Core.Parser do
       enabled_plugins_file: :string,
       aliases_file: :string,
       erlang_cookie: :atom,
-      help: :boolean
+      help: :boolean,
+      print_stacktrace: :boolean
     ]
   end
 
