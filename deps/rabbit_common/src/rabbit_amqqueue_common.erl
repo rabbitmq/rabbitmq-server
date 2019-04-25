979cd56023937d159887e1269967ec0e8240dc07
Michael Klishin <mklishin@pivotal.io>
2019-04-25 21:57:45 +0300

    Merge pull request #325 from rabbitmq/rabbitmq-erlang-client-91
    Move rabbit_queue_collector to rabbit_common repo
    
    (cherry picked from commit 4562b6443410ef5563024af965c4cf3c5bc0862d)

--- deps/rabbit_common/src/rabbit_amqqueue_common.erl
+++ deps/rabbit_common/src/rabbit_amqqueue_common.erl
@@ -16,7 +16,7 @@
 
 -module(rabbit_amqqueue_common).
 
--export([notify_sent/2, notify_sent_queue_down/1]).
+-export([notify_sent/2, notify_sent_queue_down/1, delete_exclusive/2]).
 
 -define(MORE_CONSUMER_CREDIT_AFTER, 50).
 
@@ -39,4 +39,10 @@ notify_sent(QPid, ChPid) ->
 
 notify_sent_queue_down(QPid) ->
     erase({consumer_credit_to, QPid}),
+    ok.
+
+-spec delete_exclusive([pid()], pid()) -> 'ok'.
+
+delete_exclusive(QPids, ConnId) ->
+    [gen_server2:cast(QPid, {delete_exclusive, ConnId}) || QPid <- QPids],
     ok.
