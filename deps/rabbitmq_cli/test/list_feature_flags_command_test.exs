f251dde593ef6a96294315b6a01a69abc719b352
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2019-05-06 15:37:40 +0200

    test/ctl/*feature_flags*: Use arbitrary feature flags, not `quorum_queue`
    This allows to backport the test to v3.7.x.
    
    (cherry picked from commit 2c03dc56c2a4375aa0ffc2ce3e92117c3a07a8b4)

--- deps/rabbitmq_cli/test/list_feature_flags_command_test.exs
+++ deps/rabbitmq_cli/test/list_feature_flags_command_test.exs
@@ -19,11 +19,27 @@ defmodule ListFeatureFlagsCommandTest do
 
   @command RabbitMQ.CLI.Ctl.Commands.ListFeatureFlagsCommand
 
-  @flag1 :implicit_default_bindings
-  @flag2 :quorum_queue
+  @flag1 :ff1_from_list_ff_testsuite
+  @flag2 :ff2_from_list_ff_testsuite
 
   setup_all do
     RabbitMQ.CLI.Core.Distribution.start()
+
+    # Define an arbitrary feature flag for the test.
+    node = get_rabbit_hostname()
+    new_feature_flags = %{
+      @flag1 =>
+      %{desc: "My feature flag #1",
+        provided_by: :ListFeatureFlagsCommandTest,
+        stability: :stable},
+      @flag2 =>
+      %{desc: "My feature flag #2",
+        provided_by: :ListFeatureFlagsCommandTest,
+        stability: :stable}}
+    :ok = :rabbit_misc.rpc_call(
+      node, :rabbit_feature_flags, :initialize_registry, [new_feature_flags])
+    :ok = :rabbit_misc.rpc_call(
+      node, :rabbit_feature_flags, :enable_all, [])
 
     name_result = [
       [{:name, @flag1}],
@@ -81,25 +97,25 @@ defmodule ListFeatureFlagsCommandTest do
   @tag test_timeout: :infinity
   test "run: with the name tag, print just the names", context do
     matches_found = @command.run(["name"], context[:opts])
-    assert Enum.all?(context[:name_result], fn(vhost) ->
-      Enum.find(matches_found, fn(found) -> found == vhost end)
+    assert Enum.all?(context[:name_result], fn(feature_name) ->
+      Enum.find(matches_found, fn(found) -> found == feature_name end)
     end)
   end
 
   @tag test_timeout: :infinity
   test "run: duplicate args do not produce duplicate entries", context do
-    # checks to ensure that all expected vhosts are in the results
+    # checks to ensure that all expected feature flags are in the results
     matches_found = @command.run(["name", "name"], context[:opts])
-    assert Enum.all?(context[:name_result], fn(vhost) ->
-      Enum.find(matches_found, fn(found) -> found == vhost end)
+    assert Enum.all?(context[:name_result], fn(feature_name) ->
+      Enum.find(matches_found, fn(found) -> found == feature_name end)
     end)
   end
 
   @tag test_timeout: 30000
   test "run: sufficiently long timeouts don't interfere with results", context do
     matches_found = @command.run(["name", "state"], context[:opts])
-    assert Enum.all?(context[:full_result], fn(vhost) ->
-      Enum.find(matches_found, fn(found) -> found == vhost end)
+    assert Enum.all?(context[:full_result], fn(feature_name) ->
+      Enum.find(matches_found, fn(found) -> found == feature_name end)
     end)
   end
 
