22739742324e8066c95f10721597cfa3d3181224
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2019-05-15 07:47:13 +0200

    mix.exs: Ensure we pull observer_cli 1.4.5
    This is the one which can be built with mix(1).

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -89,7 +89,7 @@ defmodule RabbitMQCtl.MixfileBase do
       {:json, "~> 1.2.0"},
       {:csv, "~> 2.0.0"},
       {:stdout_formatter, "~> 0.2.3"},
-      {:observer_cli, "~> 1.4.4"},
+      {:observer_cli, "~> 1.4.5"},
 
       {:amqp, "~> 1.2.0", only: :test},
       {:dialyxir, "~> 0.5", only: :test, runtime: false},
