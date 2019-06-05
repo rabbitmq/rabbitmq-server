4edbdfc4ea276cf276608f987dc30b3311560832
Daniil Fedotov <hairyhum@gmail.com>
2019-06-05 17:44:49 -0400

    Confirm on ack tests for a single node

--- test/confirms_rejects_SUITE.erl
+++ test/confirms_rejects_SUITE.erl
@@ -20,7 +20,18 @@ groups() ->
         {overflow_reject_publish_dlx, [parallel], OverflowTests},
         {overflow_reject_publish, [parallel], OverflowTests},
         dead_queue_rejects,
-        mixed_dead_alive_queues_reject
+        mixed_dead_alive_queues_reject,
+        {confirm_on_ack, [],
+         [coa_policy_resets_to_enqueue,
+          coa_confirms_on_ack,
+          coa_rejects_on_nack_without_requeue,
+          coa_confirms_on_no_ack,
+          coa_rejects_on_ttl,
+          coa_rejects_on_drop_head,
+          coa_rejects_on_purge,
+          coa_rejects_on_dead_letter
+         ]
+        }
       ]}
     ].
 
@@ -59,7 +70,16 @@ end_per_group(_Group, Config) ->
       rabbit_ct_client_helpers:teardown_steps() ++
       rabbit_ct_broker_helpers:teardown_steps()).
 
-init_per_testcase(policy_resets_to_default = Testcase, Config) ->
+init_per_testcase(Testcase, Config)
+        when Testcase == policy_resets_to_default;
+             Testcase == coa_policy_resets_to_enqueue;
+             Testcase == coa_confirms_on_no_ack;
+             Testcase == coa_confirms_on_ack;
+             Testcase == coa_rejects_on_nack_without_requeue;
+             Testcase == coa_rejects_on_dead_letter;
+             Testcase == coa_rejects_on_drop_head;
+             Testcase == coa_rejects_on_ttl;
+             Testcase == coa_rejects_on_purge  ->
     Conn = rabbit_ct_client_helpers:open_unmanaged_connection(Config),
     rabbit_ct_helpers:testcase_started(
         rabbit_ct_helpers:set_config(Config, [{conn, Conn}]), Testcase);
@@ -74,17 +94,48 @@ init_per_testcase(Testcase, Config)
         rabbit_ct_helpers:set_config(Config, [{conn, Conn}, {conn1, Conn1}]),
         Testcase).
 
+end_per_testcase(coa_rejects_on_dead_letter = Testcase, Config) ->
+    {_, Ch} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
+    QueueName = atom_to_binary(Testcase, utf8),
+    amqp_channel:call(Ch, #'queue.delete'{queue = QueueName}),
+    amqp_channel:call(Ch, #'exchange.delete'{exchange = QueueName}),
+    amqp_channel:call(Ch, #'queue.delete'{queue = <<QueueName/binary, "_dlq">>}),
+    rabbit_ct_broker_helpers:clear_policy(Config, 0, QueueName),
+    rabbit_ct_client_helpers:close_channels_and_connection(Config, 0),
+    Conn = ?config(conn, Config),
+    rabbit_ct_client_helpers:close_connection(Conn),
+    clean_consume_mailbox(),
+    clean_acks_mailbox(),
+    rabbit_ct_helpers:testcase_finished(Config, Testcase);
+end_per_testcase(Testcase, Config)
+        when Testcase == coa_confirms_on_ack;
+             Testcase == coa_confirms_on_no_ack;
+             Testcase == coa_policy_resets_to_enqueue;
+             Testcase == coa_rejects_on_nack_without_requeue;
+             Testcase == coa_rejects_on_dead_letter;
+             Testcase == coa_rejects_on_drop_head;
+             Testcase == coa_rejects_on_ttl;
+             Testcase == coa_rejects_on_purge ->
+    {_, Ch} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
+    QueueName = atom_to_binary(Testcase, utf8),
+    amqp_channel:call(Ch, #'queue.delete'{queue = QueueName}),
+    rabbit_ct_broker_helpers:clear_policy(Config, 0, QueueName),
+    rabbit_ct_client_helpers:close_channels_and_connection(Config, 0),
+    Conn = ?config(conn, Config),
+    rabbit_ct_client_helpers:close_connection(Conn),
+    clean_consume_mailbox(),
+    clean_acks_mailbox(),
+    rabbit_ct_helpers:testcase_finished(Config, Testcase);
 end_per_testcase(policy_resets_to_default = Testcase, Config) ->
     {_, Ch} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
     XOverflow = ?config(overflow, Config),
     QueueName = <<"policy_resets_to_default", "_", XOverflow/binary>>,
     amqp_channel:call(Ch, #'queue.delete'{queue = QueueName}),
     rabbit_ct_client_helpers:close_channels_and_connection(Config, 0),
-
     Conn = ?config(conn, Config),
-
     rabbit_ct_client_helpers:close_connection(Conn),
-
+    clean_consume_mailbox(),
+    clean_acks_mailbox(),
     rabbit_ct_helpers:testcase_finished(Config, Testcase);
 end_per_testcase(confirms_rejects_conflict = Testcase, Config) ->
     {_, Ch} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
@@ -113,6 +164,7 @@ end_per_testcase0(Testcase, Config) ->
     rabbit_ct_client_helpers:close_connection(Conn1),
 
     clean_acks_mailbox(),
+    clean_consume_mailbox(),
 
     rabbit_ct_helpers:testcase_finished(Config, Testcase).
 
@@ -315,6 +367,386 @@ policy_resets_to_default(Config) ->
         _ -> ok
     end.
 
+coa_policy_resets_to_enqueue(Config) ->
+    Conn = ?config(conn, Config),
+
+    {ok, Ch} = amqp_connection:open_channel(Conn),
+
+    amqp_channel:call(Ch, #'confirm.select'{}),
+    amqp_channel:register_confirm_handler(Ch, self()),
+
+    QueueName = <<"coa_policy_resets_to_enqueue">>,
+    %% The queue is declared as default
+    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
+                                           durable = true}),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<>>}),
+
+    assert_ack(1),
+
+    rabbit_ct_broker_helpers:set_policy(
+        Config, 0,
+        QueueName, QueueName, <<"queues">>,
+        [{<<"confirm-on">>, <<"ack">>}]),
+
+    %% Wait for the policy to apply
+    wait_for_confirm_on(Config, QueueName, ack, 10000),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    receive {'basic.ack', _, _} -> error(unexpected_ack)
+    after 1000 -> ok
+    end,
+
+    rabbit_ct_broker_helpers:clear_policy(Config, 0, QueueName),
+
+    wait_for_confirm_on(Config, QueueName, enqueue, 10000),
+
+    assert_ack(2).
+
+coa_confirms_on_ack(Config) ->
+    Conn = ?config(conn, Config),
+
+    {ok, Ch} = amqp_connection:open_channel(Conn),
+
+    amqp_channel:call(Ch, #'confirm.select'{}),
+    amqp_channel:register_confirm_handler(Ch, self()),
+
+    QueueName = <<"coa_confirms_on_ack">>,
+    %% The queue is declared as default
+    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
+                                           durable = true}),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<>>}),
+
+    receive {'basic.ack', 1, _} -> ok
+    after 10000 -> error({timeout_waiting_for_ack, process_info(self(), messages)})
+    end,
+    clean_acks_mailbox(),
+
+    rabbit_ct_broker_helpers:set_policy(
+        Config, 0,
+        QueueName, QueueName, <<"queues">>,
+        [{<<"confirm-on">>, <<"ack">>}]),
+
+    %% Wait for the policy to apply
+    wait_for_confirm_on(Config, QueueName, ack, 10000),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    receive {'basic.ack', _, _} -> error(unexpected_ack)
+    after 1000 -> ok
+    end,
+
+    consume_all(Ch, QueueName, false,
+        fun(Tag) ->
+            amqp_channel:call(Ch, #'basic.ack'{delivery_tag = Tag})
+        end),
+
+    receive {'basic.ack', 2, _} -> ok
+    after 10000 -> error({timeout_waiting_for_ack, process_info(self(), messages)})
+    end,
+    clean_acks_mailbox().
+
+coa_rejects_on_nack_without_requeue(Config) ->
+    Conn = ?config(conn, Config),
+
+    {ok, Ch} = amqp_connection:open_channel(Conn),
+
+    amqp_channel:call(Ch, #'confirm.select'{}),
+    amqp_channel:register_confirm_handler(Ch, self()),
+
+    QueueName = <<"coa_rejects_on_nack_without_requeue">>,
+    %% The queue is declared as default
+    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
+                                           durable = true}),
+
+    rabbit_ct_broker_helpers:set_policy(
+        Config, 0,
+        QueueName, QueueName, <<"queues">>,
+        [{<<"confirm-on">>, <<"ack">>}]),
+
+    %% Wait for the policy to apply
+    wait_for_confirm_on(Config, QueueName, ack, 10000),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    receive {'basic.ack', _, _} -> error(unexpected_ack)
+    after 1000 -> ok
+    end,
+
+    #'basic.consume_ok'{consumer_tag = CTag} =
+        amqp_channel:subscribe(Ch, #'basic.consume'{queue = QueueName}, self()),
+
+    %% Requeue should not ack or nack the message.
+    receive
+        {#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{payload = <<"message">>}} ->
+            amqp_channel:call(Ch, #'basic.nack'{delivery_tag = Tag, requeue = true})
+    after 10000 ->
+        error(expecting_delivery)
+    end,
+
+    receive
+        {'basic.ack', _, _} -> error(unexpected_ack);
+        {'basic.nack', _, _, _} -> error(unexpected_nack)
+    after 1000 -> ok
+    end,
+
+    %% If requeue is false - the message should be nacked
+    receive
+        {#'basic.deliver'{delivery_tag = Tag1}, #amqp_msg{payload = <<"message">>}} ->
+            amqp_channel:call(Ch, #'basic.nack'{delivery_tag = Tag1, requeue = false})
+    after 10000 ->
+        error(expecting_delivery)
+    end,
+
+    receive
+        {'basic.ack', _, _} -> error(expecting_nack_got_ack);
+        {'basic.nack', _, _, _} -> ok
+    after 10000 -> error(timeout_waiting_for_nack)
+    end,
+    amqp_channel:call(Ch, #'basic.cancel'{consumer_tag = CTag}),
+    clean_consume_mailbox(),
+    clean_acks_mailbox().
+
+coa_confirms_on_no_ack(Config) ->
+    Conn = ?config(conn, Config),
+
+    {ok, Ch} = amqp_connection:open_channel(Conn),
+
+    amqp_channel:call(Ch, #'confirm.select'{}),
+    amqp_channel:register_confirm_handler(Ch, self()),
+
+    QueueName = <<"coa_confirms_on_no_ack">>,
+    %% The queue is declared as default
+    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
+                                           durable = true}),
+
+    rabbit_ct_broker_helpers:set_policy(
+        Config, 0,
+        QueueName, QueueName, <<"queues">>,
+        [{<<"confirm-on">>, <<"ack">>}]),
+
+    %% Wait for the policy to apply
+    wait_for_confirm_on(Config, QueueName, ack, 10000),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    receive {'basic.ack', _, _} -> error(unexpected_ack)
+    after 1000 -> ok
+    end,
+
+    consume_all(Ch, QueueName, true,
+        fun(Tag) ->
+            ok
+        end),
+
+    receive {'basic.ack', _, _} -> ok
+    after 10000 -> error({timeout_waiting_for_ack, process_info(self(), messages)})
+    end,
+    clean_acks_mailbox().
+
+coa_rejects_on_ttl(Config) ->
+    Conn = ?config(conn, Config),
+
+    {ok, Ch} = amqp_connection:open_channel(Conn),
+
+    amqp_channel:call(Ch, #'confirm.select'{}),
+    amqp_channel:register_confirm_handler(Ch, self()),
+
+    QueueName = <<"coa_rejects_on_ttl">>,
+    %% The queue is declared as default
+    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
+                                           durable = true}),
+
+    rabbit_ct_broker_helpers:set_policy(
+        Config, 0,
+        QueueName, QueueName, <<"queues">>,
+        [{<<"confirm-on">>, <<"ack">>}, {<<"message-ttl">>, 200}]),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    ct:sleep(200),
+
+    receive
+        {'basic.ack', _, _} -> error(expecting_nack_got_ack);
+        {'basic.nack', _, _, _} -> ok
+    after 10000 -> error(timeout_waiting_for_nack)
+    end,
+    clean_acks_mailbox().
+
+coa_rejects_on_drop_head(Config) ->
+    Conn = ?config(conn, Config),
+
+    {ok, Ch} = amqp_connection:open_channel(Conn),
+
+    amqp_channel:call(Ch, #'confirm.select'{}),
+    amqp_channel:register_confirm_handler(Ch, self()),
+
+    QueueName = <<"coa_rejects_on_drop_head">>,
+    %% The queue is declared as default
+    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
+                                           durable = true}),
+
+    rabbit_ct_broker_helpers:set_policy(
+        Config, 0,
+        QueueName, QueueName, <<"queues">>,
+        [{<<"confirm-on">>, <<"ack">>},
+         {<<"max-length">>, 1}]),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    receive
+        {'basic.ack', _, _} -> error(expecting_nack_got_ack);
+        %% First message is dropped from the head
+        {'basic.nack', 1, _, _} -> ok
+    after 10000 -> error(timeout_waiting_for_nack)
+    end,
+    clean_acks_mailbox().
+
+coa_rejects_on_purge(Config) ->
+    Conn = ?config(conn, Config),
+
+    {ok, Ch} = amqp_connection:open_channel(Conn),
+
+    amqp_channel:call(Ch, #'confirm.select'{}),
+    amqp_channel:register_confirm_handler(Ch, self()),
+
+    QueueName = <<"coa_rejects_on_purge">>,
+    %% The queue is declared as default
+    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
+                                           durable = true}),
+
+    rabbit_ct_broker_helpers:set_policy(
+        Config, 0,
+        QueueName, QueueName, <<"queues">>,
+        [{<<"confirm-on">>, <<"ack">>}]),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message">>}),
+
+    receive
+        {'basic.ack', _, _} -> error(unexpected_ack);
+        {'basic.nack', _, _, _} -> error(unexpected_nack)
+    after 1000 -> ok
+    end,
+
+    amqp_channel:call(Ch, #'queue.purge'{queue = QueueName}),
+
+    receive
+        {'basic.ack', _, _} ->
+            error(expecting_nack_got_ack);
+        {'basic.nack', 1, _, _} ->
+            receive {'basic.nack', 2, _, _} -> ok
+            after 10000 -> error(timeout_waiting_for_nack)
+            end;
+        {'basic.nack', 2, true, _} ->
+            ok
+    after 10000 ->
+        error(timeout_waiting_for_nack)
+    end,
+
+    clean_acks_mailbox().
+
+coa_rejects_on_dead_letter(Config) ->
+    Conn = ?config(conn, Config),
+
+    {ok, Ch} = amqp_connection:open_channel(Conn),
+
+    amqp_channel:call(Ch, #'confirm.select'{}),
+    amqp_channel:register_confirm_handler(Ch, self()),
+
+    QueueName = <<"coa_rejects_on_dead_letter">>,
+    %% The queue is declared as default
+    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
+                                           durable = true}),
+
+    DLX = QueueName,
+    DLQ = <<QueueName/binary, "_dlq">>,
+
+    amqp_channel:call(Ch, #'exchange.declare'{exchange = DLX, type = <<"fanout">>}),
+    amqp_channel:call(Ch, #'queue.declare'{queue = DLQ}),
+    amqp_channel:call(Ch, #'queue.bind'{exchange = DLX, queue = DLQ}),
+
+    rabbit_ct_broker_helpers:set_policy(
+        Config, 0,
+        QueueName, QueueName, <<"queues">>,
+        [{<<"confirm-on">>, <<"ack">>},
+         {<<"max-length">>, 1},
+         {<<"dead-letter-exchange">>, DLX}]),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message1">>}),
+
+    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
+                          #amqp_msg{payload = <<"message2">>}),
+
+    timer:sleep(10000),
+    receive
+        {'basic.ack', _, _} -> error(expecting_nack_got_ack);
+        %% First message is dropped from the head
+        {'basic.nack', 1, _, _} -> ok
+    after 10000 -> error(timeout_waiting_for_nack)
+    end,
+    clean_acks_mailbox(),
+
+    %% The message is dead-lettered
+    {#'basic.get_ok'{}, #amqp_msg{payload = <<"message1">>}} =
+        amqp_channel:call(Ch, #'basic.get'{queue = DLQ}).
+
+
+consume_all(Ch, QueueName, NoAck, Fun) ->
+    #'basic.consume_ok'{consumer_tag = CTag} =
+        amqp_channel:subscribe(Ch,
+                               #'basic.consume'{queue = QueueName,
+                                                no_ack = NoAck},
+                               self()),
+    loop_consume_and_ack(Ch, Fun),
+    amqp_channel:call(Ch, #'basic.cancel'{consumer_tag = CTag}),
+    clean_consume_mailbox().
+
+loop_consume_and_ack(Ch, Fun) ->
+    receive
+        {#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{}} ->
+            Fun(Tag),
+            loop_consume_and_ack(Ch, Fun)
+    after 10000 ->
+        ok
+    end.
+
+wait_for_confirm_on(_Config, QueueName, ConfirmOn, Time) when Time =< 0 ->
+    error({timeout_waiting_for_confirm_on_setting, ConfirmOn, QueueName});
+wait_for_confirm_on(Config, QueueName, ConfirmOn, Time) ->
+    {ok, Q} = rabbit_ct_broker_helpers:rpc(Config, 0,
+                                           rabbit_amqqueue, lookup,
+                                           [{resource, <<"/">>, queue, QueueName}]),
+    [{confirm_on, ConfirmOnInfo}] =
+        rabbit_ct_broker_helpers:rpc(Config, 0, rabbit_amqqueue, info, [Q, [confirm_on]]),
+    case ConfirmOnInfo of
+        ConfirmOn -> ok;
+        _ ->
+            SleepTime = 100,
+            ct:sleep(SleepTime),
+            wait_for_confirm_on(Config, QueueName, ConfirmOn, Time - SleepTime)
+    end.
+
+
+%% Helpers
+
 consume_all_messages(Ch, QueueName) ->
     consume_all_messages(Ch, QueueName, []).
 
@@ -324,6 +756,12 @@ consume_all_messages(Ch, QueueName, Msgs) ->
             consume_all_messages(Ch, QueueName, [Msg | Msgs]);
         #'basic.get_empty'{} -> Msgs
     end.
+
+assert_ack(N) ->
+    receive {'basic.ack', N, _} -> ok
+    after 10000 -> error({timeout_waiting_for_ack, process_info(self(), messages)})
+    end,
+    clean_acks_mailbox().
 
 assert_ack() ->
     receive {'basic.ack', _, _} -> ok
@@ -374,6 +812,15 @@ clean_acks_mailbox() ->
     receive
         {'basic.ack', _, _} -> clean_acks_mailbox();
         {'basic.nack', _, _, _} -> clean_acks_mailbox()
+    after
+        1000 -> done
+    end.
+
+clean_consume_mailbox() ->
+    receive
+        #'basic.consume_ok'{} -> clean_consume_mailbox();
+        #'basic.cancel_ok'{} -> clean_consume_mailbox();
+        {#'basic.deliver'{}, _} -> clean_consume_mailbox()
     after
         1000 -> done
     end.
