8764bfcefaf994d351f3bc9fdfb2d5ae2eb4592a
Luke Bakken <lbakken@pivotal.io>
2019-06-07 11:54:29 -0700

    Merge pull request #56 from rabbitmq/rabbitmq-web-mqtt-55
    Make sure default max_connections passed to Cowboy is "infinity"
    
    (cherry picked from commit 66e927756d062602ddc13826005b09c815416336)

--- deps/rabbitmq_web_mqtt/src/rabbit_web_mqtt_app.erl
+++ deps/rabbitmq_web_mqtt/src/rabbit_web_mqtt_app.erl
@@ -45,62 +45,66 @@ init([]) -> {ok, {{one_for_one, 1, 5}, []}}.
 %%----------------------------------------------------------------------------
 
 mqtt_init() ->
-    CowboyOpts0  = maps:from_list(get_env(cowboy_opts, [])),
-    CowboyWsOpts = maps:from_list(get_env(cowboy_ws_opts, [])),
+  CowboyOpts0  = maps:from_list(get_env(cowboy_opts, [])),
+  CowboyWsOpts = maps:from_list(get_env(cowboy_ws_opts, [])),
 
-    Routes = cowboy_router:compile([{'_', [
-        {get_env(ws_path, "/ws"), rabbit_web_mqtt_handler, [{ws_opts, CowboyWsOpts}]}
-    ]}]),
-    CowboyOpts = CowboyOpts0#{env          => #{dispatch => Routes},
-                              middlewares  => [cowboy_router, rabbit_web_mqtt_middleware, cowboy_handler],
-                              proxy_header => get_env(proxy_protocol, false)},
-    case get_env(tcp_config, []) of
-        [] ->
-            ok;
-        TCPConf0 ->
-            {TCPConf, IpStr, Port} = get_tcp_conf(TCPConf0),
-            case ranch:start_listener(web_mqtt, get_env(num_tcp_acceptors, 10),
-                ranch_tcp, TCPConf,
-                rabbit_web_mqtt_connection_sup, CowboyOpts) of
-                {ok, _}                       -> ok;
-                {error, {already_started, _}} -> ok;
-                {error, ErrTCP}               ->
-                    rabbit_log_connection:error(
-                        "Failed to start a WebSocket (HTTP) listener. Error: ~p,"
-                        " listener settings: ~p~n",
-                        [ErrTCP, TCPConf]),
-                    throw(ErrTCP)
-            end,
-            listener_started('http/web-mqtt', TCPConf),
-            rabbit_log:info("rabbit_web_mqtt: listening for HTTP connections on ~s:~w~n",
-                            [IpStr, Port])
-    end,
-    case get_env(ssl_config, []) of
-        [] ->
-            ok;
-        TLSConf0 ->
-            rabbit_networking:ensure_ssl(),
-            {TLSConf, TLSIpStr, TLSPort} = get_tls_conf(TLSConf0),
-            {ok, _} = ranch:start_listener(web_mqtt_secure, get_env(num_ssl_acceptors, 10),
-                ranch_ssl, TLSConf,
-                rabbit_web_mqtt_connection_sup, CowboyOpts),
-            case ranch:start_listener(web_mqtt_secure, get_env(num_ssl_acceptors, 10),
-                ranch_ssl, TLSConf,
-                rabbit_web_mqtt_connection_sup, CowboyOpts) of
-                {ok, _}                       -> ok;
-                {error, {already_started, _}} -> ok;
-                {error, ErrTLS}               ->
-                    rabbit_log_connection:error(
-                        "Failed to start a TLS WebSocket (HTTPS) listener. Error: ~p,"
-                        " listener settings: ~p~n",
-                        [ErrTLS, TLSConf]),
-                    throw(ErrTLS)
-            end,
-            listener_started('https/web-mqtt', TLSConf),
-            rabbit_log:info("rabbit_web_mqtt: listening for HTTPS connections on ~s:~w~n",
-                            [TLSIpStr, TLSPort])
-    end,
-    ok.
+  Routes = cowboy_router:compile([{'_', [
+      {get_env(ws_path, "/ws"), rabbit_web_mqtt_handler, [{ws_opts, CowboyWsOpts}]}
+  ]}]),
+  CowboyOpts = CowboyOpts0#{env          => #{dispatch => Routes},
+                            middlewares  => [cowboy_router, rabbit_web_mqtt_middleware, cowboy_handler],
+                            proxy_header => get_env(proxy_protocol, false)},
+  case get_env(tcp_config, []) of
+      []       -> ok;
+      TCPConf0 -> start_tcp_listener(TCPConf0, CowboyOpts)
+  end,
+  case get_env(ssl_config, []) of
+      []       -> ok;
+      TLSConf0 -> start_tls_listener(TLSConf0, CowboyOpts)
+  end,
+  ok.
+
+start_tcp_listener(TCPConf0, CowboyOpts) ->
+  {TCPConf, IpStr, Port} = get_tcp_conf(TCPConf0),
+  MaxConnections = get_max_connections(),
+  case ranch:start_listener(web_mqtt, get_env(num_tcp_acceptors, 10),
+      ranch_tcp, [{max_connections, MaxConnections}|TCPConf],
+      rabbit_web_mqtt_connection_sup, CowboyOpts) of
+      {ok, _}                       -> ok;
+      {error, {already_started, _}} -> ok;
+      {error, ErrTCP}               ->
+          rabbit_log_connection:error(
+              "Failed to start a WebSocket (HTTP) listener. Error: ~p,"
+              " listener settings: ~p~n",
+              [ErrTCP, TCPConf]),
+          throw(ErrTCP)
+  end,
+  listener_started('http/web-mqtt', TCPConf),
+  rabbit_log:info("rabbit_web_mqtt: listening for HTTP connections on ~s:~w~n",
+                  [IpStr, Port]).
+
+start_tls_listener(TLSConf0, CowboyOpts) ->
+  rabbit_networking:ensure_ssl(),
+  {TLSConf, TLSIpStr, TLSPort} = get_tls_conf(TLSConf0),
+  MaxConnections = get_max_connections(),
+  {ok, _} = ranch:start_listener(web_mqtt_secure, get_env(num_ssl_acceptors, 10),
+      ranch_ssl, [{max_connections, MaxConnections}|TLSConf],
+      rabbit_web_mqtt_connection_sup, CowboyOpts),
+  case ranch:start_listener(web_mqtt_secure, get_env(num_ssl_acceptors, 10),
+      ranch_ssl, [{max_connections, MaxConnections}|TLSConf],
+      rabbit_web_mqtt_connection_sup, CowboyOpts) of
+      {ok, _}                       -> ok;
+      {error, {already_started, _}} -> ok;
+      {error, ErrTLS}               ->
+          rabbit_log_connection:error(
+              "Failed to start a TLS WebSocket (HTTPS) listener. Error: ~p,"
+              " listener settings: ~p~n",
+              [ErrTLS, TLSConf]),
+          throw(ErrTLS)
+  end,
+  listener_started('https/web-mqtt', TLSConf),
+  rabbit_log:info("rabbit_web_mqtt: listening for HTTPS connections on ~s:~w~n",
+                  [TLSIpStr, TLSPort]).
 
 listener_started(Protocol, Listener) ->
     Port = rabbit_misc:pget(port, Listener),
@@ -138,6 +142,9 @@ normalize_ip(IpStr) when is_list(IpStr) ->
     Ip;
 normalize_ip(Ip) ->
     Ip.
+
+get_max_connections() ->
+  get_env(max_connections, infinity).
 
 get_env(Key, Default) ->
     rabbit_misc:get_env(rabbitmq_web_mqtt, Key, Default).
