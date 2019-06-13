71e830fa90b3d67bc00deb3f2e602da8a94104ae
Michael Klishin <mklishin@pivotal.io>
2019-06-13 15:44:01 +0300

    Merge pull request #2033 from rabbitmq/rabbitmq-cli-330
    Schema information in mnesia for cli command
    
    (cherry picked from commit a65728025c4c4662101c251f6d65264522594266)

--- src/rabbit_mnesia.erl
+++ src/rabbit_mnesia.erl
@@ -40,7 +40,10 @@
          ensure_mnesia_dir/0,
 
          on_node_up/1,
-         on_node_down/1
+         on_node_down/1,
+
+         %% Helpers for diagnostics commands
+         schema_info/1
         ]).
 
 %% Used internally in rpc calls
@@ -710,6 +713,18 @@ running_disc_nodes() ->
     {_AllNodes, DiscNodes, RunningNodes} = cluster_status(status),
     ordsets:to_list(ordsets:intersection(ordsets:from_list(DiscNodes),
                                          ordsets:from_list(RunningNodes))).
+
+%%--------------------------------------------------------------------
+%% Helpers for diagnostics commands
+%%--------------------------------------------------------------------
+
+schema_info(Items) ->
+    Tables = mnesia:system_info(tables),
+    [info(Table, Items) || Table <- Tables].
+
+info(Table, Items) ->
+    All = [{name, Table} | mnesia:table_info(Table, all)],
+    [{Item, proplists:get_value(Item, All)} || Item <- Items].
 
 %%--------------------------------------------------------------------
 %% Internal helpers
