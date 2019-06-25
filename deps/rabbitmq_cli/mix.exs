10fcee70c204207ffcd2d40ca8f8d506846abd59
Michael Klishin <michael@clojurewerkz.org>
2019-06-25 08:41:01 +0200

    Allow for Elixir 1.9
    (cherry picked from commit 2e14e090ad69a3aed9a7950796e580ae41de71e3)
    
    Conflicts:
    	mix.exs

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -20,7 +20,7 @@ defmodule RabbitMQCtl.MixfileBase do
     [
       app: :rabbitmqctl,
       version: "3.7.0-dev",
-      elixir: ">= 1.6.6 and < 1.9.0",
+      elixir: ">= 1.6.6 and < 1.10.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       escript: [main_module: RabbitMQCtl,
