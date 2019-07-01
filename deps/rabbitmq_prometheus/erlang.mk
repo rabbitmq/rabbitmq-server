642a4b47debd5c324773ba9217f8771f94ccdb43
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2019-07-01 12:48:42 +0200

    erlang.mk: Remove the `protobuffs` plugin
    It breaks of the build of the `prometheus` dependency:
    
    gmake[1]: Entering directory '.../prometheus'
     DEPEND prometheus.d
     PROTO  prometheus_model.proto
    {"init terminating in do_boot",{undef,[{protobuffs_compile,generate_source,["src/model/prometheus_model.proto",[{output_include_dir,"./include"},{output_src_dir,"./src"}]],[]},...
    init terminating in do_boot ({undef,[{protobuffs_compile,generate_source,[[_],[_]],[]},...
    gmake[2]: *** [.../rabbitmq_prometheus/erlang.mk:6874: prometheus.d] Error 1
    gmake[1]: *** [.../rabbitmq_prometheus/erlang.mk:5105: app] Error 2
    gmake[1]: Leaving directory '.../prometheus'
    gmake: *** [erlang.mk:4431: deps] Error 2

--- deps/rabbitmq_prometheus/erlang.mk
+++ deps/rabbitmq_prometheus/erlang.mk
@@ -18,7 +18,7 @@ ERLANG_MK_FILENAME := $(realpath $(lastword $(MAKEFILE_LIST)))
 export ERLANG_MK_FILENAME
 
 ERLANG_MK_VERSION = 2.0.0-pre.2-486-g2b7e434
-ERLANG_MK_WITHOUT = plugins/proper
+ERLANG_MK_WITHOUT = plugins/proper plugins/protobuffs
 
 # Make 3.81 and 3.82 are deprecated.
 
@@ -6814,64 +6814,6 @@ apps-eunit: test-build
 	$(verbose) eunit_retcode=0 ; for app in $(ALL_APPS_DIRS); do $(MAKE) -C $$app eunit IS_APP=1; \
 		[ $$? -ne 0 ] && eunit_retcode=1 ; done ; \
 		exit $$eunit_retcode
-endif
-endif
-
-# Copyright (c) 2015-2016, Loïc Hoguin <essen@ninenines.eu>
-# This file is part of erlang.mk and subject to the terms of the ISC License.
-
-# Verbosity.
-
-proto_verbose_0 = @echo " PROTO " $(filter %.proto,$(?F));
-proto_verbose = $(proto_verbose_$(V))
-
-# Core targets.
-
-ifneq ($(wildcard src/),)
-PROTO_FILES := $(filter %.proto,$(ALL_SRC_FILES))
-ERL_FILES += $(addprefix src/,$(patsubst %.proto,%_pb.erl,$(notdir $(PROTO_FILES))))
-
-ifeq ($(PROTO_FILES),)
-$(ERLANG_MK_TMP)/last-makefile-change-protobuffs:
-	$(verbose) :
-else
-# Rebuild proto files when the Makefile changes.
-# We exclude $(PROJECT).d to avoid a circular dependency.
-$(ERLANG_MK_TMP)/last-makefile-change-protobuffs: $(filter-out $(PROJECT).d,$(MAKEFILE_LIST)) | $(ERLANG_MK_TMP)
-	$(verbose) if test -f $@; then \
-		touch $(PROTO_FILES); \
-	fi
-	$(verbose) touch $@
-
-$(PROJECT).d:: $(ERLANG_MK_TMP)/last-makefile-change-protobuffs
-endif
-
-ifeq ($(filter gpb,$(BUILD_DEPS) $(DEPS)),)
-define compile_proto.erl
-	[begin
-		protobuffs_compile:generate_source(F, [
-			{output_include_dir, "./include"},
-			{output_src_dir, "./src"}])
-	end || F <- string:tokens("$1", " ")],
-	halt().
-endef
-else
-define compile_proto.erl
-	[begin
-		gpb_compile:file(F, [
-			{include_as_lib, true},
-			{module_name_suffix, "_pb"},
-			{o_hrl, "./include"},
-			{o_erl, "./src"}])
-	end || F <- string:tokens("$1", " ")],
-	halt().
-endef
-endif
-
-ifneq ($(PROTO_FILES),)
-$(PROJECT).d:: $(PROTO_FILES)
-	$(verbose) mkdir -p ebin/ include/
-	$(if $(strip $?),$(proto_verbose) $(call erlang,$(call compile_proto.erl,$?)))
 endif
 endif
 
