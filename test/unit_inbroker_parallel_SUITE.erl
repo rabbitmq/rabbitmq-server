53eaa808aaead4f634adff70718810f44fa620d2
Michael Klishin <michael@clojurewerkz.org>
2019-07-02 11:47:36 +0200

    Remove a test we decided against
    Integration tests in rabbitmq_auth_backend_oauth2
    cover more ground. In addition we would have to
    introduce a dummy backend that supports state
    updates which @acogoluegnes and I decided against.
    
    So all the testing currently happens in the only
    backend plugin that actually supports credential
    expiration and state/secret updating.

--- test/unit_inbroker_parallel_SUITE.erl
+++ test/unit_inbroker_parallel_SUITE.erl
@@ -61,9 +61,6 @@ groups() ->
               login_of_passwordless_user,
               set_tags_for_passwordless_user
             ]},
-          {access_control, [parallel], [
-              state_update
-          ]},
           set_disk_free_limit_command,
           set_vm_memory_high_watermark_command,
           topic_matching,
