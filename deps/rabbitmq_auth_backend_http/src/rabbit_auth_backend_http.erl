a484ede8f868351ded97562eccd5d2294c121a07
Michael Klishin <michael@clojurewerkz.org>
2019-07-03 09:54:14 +0200

    Adapt to rabbit_authz_backend interface changes
    from rabbitmq/rabbitmq-auth-backend-oauth2#28.

--- deps/rabbitmq_auth_backend_http/src/rabbit_auth_backend_http.erl
+++ deps/rabbitmq_auth_backend_http/src/rabbit_auth_backend_http.erl
@@ -23,7 +23,8 @@
 
 -export([description/0, p/1, q/1, join_tags/1]).
 -export([user_login_authentication/2, user_login_authorization/2,
-         check_vhost_access/3, check_resource_access/4, check_topic_access/4]).
+         check_vhost_access/3, check_resource_access/4, check_topic_access/4,
+         state_can_expire/0]).
 
 %% If keepalive connection is closed, retry N times before failing.
 -define(RETRY_ON_KEEPALIVE_CLOSED, 3).
@@ -89,6 +90,8 @@ check_topic_access(#auth_user{username = Username, tags = Tags},
         {name,       Name},
         {permission, Permission},
         {tags, join_tags(Tags)}] ++ OptionsParameters).
+
+state_can_expire() -> false.
 
 %%--------------------------------------------------------------------
 
