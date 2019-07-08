90b7783d800a822af58a2bbacbe3b4f850a4bc36
Michael Klishin <mklishin@pivotal.io>
2019-07-08 15:39:45 +0300

    Require Elixir 1.7 We'd go to 1.8 if some CI environments did not use OTP 19.3 which requires OTP 20. (cherry picked from commit 82f413c9267b7c4672c9d37655b67fd3db80b26d)

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -19,8 +19,8 @@ defmodule RabbitMQCtl.MixfileBase do
   def project do
     [
       app: :rabbitmqctl,
-      version: "3.7.0-dev",
-      elixir: ">= 1.6.6 and < 1.10.0",
+      version: "3.7.17",
+      elixir: ">= 1.7.0 and < 1.10.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       escript: [main_module: RabbitMQCtl,
