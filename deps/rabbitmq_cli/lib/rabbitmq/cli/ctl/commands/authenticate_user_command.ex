358aec5840262b9e2ca40c7f1f1643bfb473b976
Michael Klishin <michael@clojurewerkz.org>
2019-07-10 02:17:12 +0300

    ctl authenticate_user: make password optional, accept it via stdin
    Part of #365.

--- deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/authenticate_user_command.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/authenticate_user_command.ex
@@ -14,18 +14,37 @@
 ## Copyright (c) 2007-2019 Pivotal Software, Inc.  All rights reserved.
 
 defmodule RabbitMQ.CLI.Ctl.Commands.AuthenticateUserCommand do
-  alias RabbitMQ.CLI.Core.DocGuide
+  alias RabbitMQ.CLI.Core.{DocGuide, Input}
 
   @behaviour RabbitMQ.CLI.CommandBehaviour
 
-  def merge_defaults(args, opts), do: {args, opts}
+  def switches, do: [interactive: :boolean]
+  def aliases, do: [i: :interactive]
 
-  def validate(args, _) when length(args) < 2, do: {:validation_failure, :not_enough_args}
+  def merge_defaults(args, opts) do
+    {args, Map.merge(%{interactive: true}, opts)}
+  end
+
+  def validate(args, _) when length(args) < 1, do: {:validation_failure, :not_enough_args}
   def validate(args, _) when length(args) > 2, do: {:validation_failure, :too_many_args}
+  def validate([_], %{interactive: false}) do
+    {:validation_failure, {:not_enough_args, "password must be provided as an argument in non-interactive mode"}}
+  end
+  def validate([_], _), do: :ok
   def validate([_, _], _), do: :ok
 
   use RabbitMQ.CLI.Core.RequiresRabbitAppRunning
 
+  def run([user], %{node: node_name} = opts) do
+    password = Input.infer_password("Password: ", opts)
+
+    :rabbit_misc.rpc_call(
+      node_name,
+      :rabbit_access_control,
+      :check_user_pass_login,
+      [user, password]
+    )
+  end
   def run([user, password], %{node: node_name}) do
     :rabbit_misc.rpc_call(
       node_name,
@@ -35,12 +54,14 @@ defmodule RabbitMQ.CLI.Ctl.Commands.AuthenticateUserCommand do
     )
   end
 
-  def usage, do: "authenticate_user <username> <password>"
+  def usage, do: "authenticate_user <username> <password> [--interactive]"
 
   def usage_additional() do
     [
       ["<username>", "Username to use"],
-      ["<password>", "Password to use"]
+      ["<password>", "Password to use. Can be entered via stdin in interactive mode."],
+      ["--interactive", "Indicates that password might be provided via stdin. This is the default."],
+      ["--no-interactive", "Indicates that password must not be read from stdin."]
     ]
   end
 
@@ -54,7 +75,7 @@ defmodule RabbitMQ.CLI.Ctl.Commands.AuthenticateUserCommand do
 
   def description(), do: "Attempts to authenticate a user. Exits with a non-zero code if authentication fails."
 
-  def banner([username, _password], _), do: "Authenticating user \"#{username}\" ..."
+  def banner([username | _], _), do: "Authenticating user \"#{username}\" ..."
 
   def output({:refused, user, msg, args}, _) do
     {:error, RabbitMQ.CLI.Core.ExitCodes.exit_dataerr(),
--- deps/rabbitmq_cli/test/ctl/authenticate_user_command_test.exs
+++ deps/rabbitmq_cli/test/ctl/authenticate_user_command_test.exs
@@ -25,7 +25,6 @@ defmodule AuthenticateUserCommandTest do
   setup_all do
     RabbitMQ.CLI.Core.Distribution.start()
 
-
     :ok
   end
 
@@ -35,14 +34,26 @@ defmodule AuthenticateUserCommandTest do
     {:ok, opts: %{node: get_rabbit_hostname()}}
   end
 
-  test "validate: invalid number of arguments returns a validation failure" do
+  test "validate: no positional arguments fails" do
     assert @command.validate([], %{}) == {:validation_failure, :not_enough_args}
-    assert @command.validate(["user"], %{}) == {:validation_failure, :not_enough_args}
+  end
+
+  test "validate: too many positional arguments fails" do
     assert @command.validate(["user", "password", "extra"], %{}) ==
       {:validation_failure, :too_many_args}
   end
-  test "validate: correct arguments return :ok" do
+
+  test "validate: one argument in non-interactive mode fails" do
+    assert match?({:validation_failure, {:not_enough_args, _}},
+                  @command.validate(["user"], %{interactive: false}))
+  end
+
+  test "validate: two arguments passes" do
     assert @command.validate(["user", "password"], %{}) == :ok
+  end
+
+  test "validate: one argument in interactive mode passes" do
+    assert @command.validate(["user"], %{interactive: true}) == :ok
   end
 
   @tag user: @user, password: @password
