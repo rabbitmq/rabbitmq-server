ba983926c240185bc91ac4300cf9bb3072f03287
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-07-24 15:27:36 +0200

    Clean seed script to OAuth demo
    [#158496811]

--- deps/rabbitmq_management/scripts/seed.sh
+++ deps/rabbitmq_management/scripts/seed.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env sh
 
-uaac token client get admin -s adminsecret
-
-uaac client delete rabbit_client
-uaac client add rabbit_client --name rabbit_client \
- --secret rabbit_secret \
- --authorized_grant_types client_credentials \
- --authorities 'rabbitmq.read:*/* rabbitmq.write:*/* rabbitmq.configure:*/* rabbitmq.tag:management rabbitmq.tag:administrator' \
- --access_token_validity 86400
-
-uaac token client get rabbit_client -s rabbit_secret
-
-uaac token client get admin -s adminsecret
-
-uaac context rabbit_client
-
-# switch back to the admin context so that we have
-# the permissions to add the user
+# use admin context to add user and client
 uaac token client get admin -s adminsecret
 
 uaac user add rabbit_user -p rabbit_password --email rabbit_user@example.com
 
+# these groups will end up in the scope of the users
 uaac group add "rabbitmq.read:*/*"
 uaac group add "rabbitmq.write:*/*"
 uaac group add "rabbitmq.configure:*/*"
@@ -33,15 +18,11 @@ uaac member add "rabbitmq.configure:*/*" rabbit_user
 uaac member add "rabbitmq.tag:management" rabbit_user
 uaac member add "rabbitmq.tag:administrator" rabbit_user
 
+# add the client for the management plugin. It has the implicit grant type.
 uaac client add rabbit_user_client \
  --name rabbit_user_client \
+ --secret '' \
  --scope 'rabbitmq.* openid' \
  --authorized_grant_types implicit \
  --autoapprove true \
  --redirect_uri 'http://localhost:15672'
-
-uaac token owner get rabbit_user_client rabbit_user -s rabbit_secret -p rabbit_password
-
-uaac token client get admin -s adminsecret
-
-uaac context rabbit_client
