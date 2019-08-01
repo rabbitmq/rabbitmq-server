ed08b568296639d4348e6467af59e37f03448f47
Michael Klishin <michael@clojurewerkz.org>
2019-08-01 17:27:42 +0300

    Update rabbitmq-components.mk

--- deps/rabbitmq_web_mqtt_examples/rabbitmq-components.mk
+++ deps/rabbitmq_web_mqtt_examples/rabbitmq-components.mk
@@ -118,7 +118,7 @@ dep_prometheus = hex 4.2.2
 dep_ra = git https://github.com/rabbitmq/ra.git master
 dep_ranch = hex 1.7.1
 dep_recon = hex 2.5.0
-dep_observer_cli = git https://github.com/zhongwencool/observer_cli 1.5.0
+dep_observer_cli = hex 1.5.2
 dep_stdout_formatter = hex 0.2.2
 dep_sysmon_handler = hex 1.1.0
 
