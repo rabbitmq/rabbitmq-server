3da98e74dc27d588df4d9d46b199c8a1cdb0162c
Gerhard Lazu <gerhard@lazu.co.uk>
2019-08-13 14:13:25 +0100

    Review metric naming with @brian-brazil
    Started as a Prometheus docs discussion in prometheus/docs#1414, mostly
    based on https://prometheus.io/docs/instrumenting/writing_exporters/
    
    Raft metrics are of type gauge, not counter. _If you care about the
    absolute value rather than only how fast it's increasing, that's a
    gauge_
    
    All node_persister_metrics are now counters - some were gauges before.
    They are now named using metric naming best practices:
    https://prometheus.io/docs/practices/naming/
    
    All metrics names that should have units, do. Some use microseconds,
    others milliseconds and others bytes or ops (operations). We don't do
    any unit conversion in the collector but simply expose the units that
    are used when the metric value is written to ETS.
    
    While some metrics such as io_sync_time_microseconds_total would be
    better expressed as Sumarries, the refactoring required to achieve that
    is not worth the effort. Will keep things simple & imperfect for now,
    especially since we don't have a dashboard that helps visualise these
    metrics.
    
    The next step is to address global labels - will submit as a separate
    PR.
    
    [#167846096]

--- deps/rabbitmq_prometheus/src/collectors/prometheus_rabbitmq_core_metrics_collector.erl
+++ deps/rabbitmq_prometheus/src/collectors/prometheus_rabbitmq_core_metrics_collector.erl
@@ -55,44 +55,44 @@
     ]},
 
     {channel_process_metrics, [
-        {2, erlang_process_reductions, counter, "Erlang process reductions"}
+        {2, erlang_process_reductions_total, counter, "Total number of Erlang process reductions"}
     ]},
 
     {channel_queue_metrics, [
-        {2, channel_get_ack, counter, "Messages fetched with basic.get in manual acknowledgement mode"},
-        {3, channel_get, counter, "Messages fetched with basic.get in automatic acknowledgement mode"},
-        {4, channel_messages_delivered_ack, counter, "Messages delivered to consumers in manual acknowledgement mode"},
-        {5, channel_messages_delivered, counter, "Messages delivered to consumers in automatic acknowledgement mode"},
-        {6, channel_messages_redelivered, counter, "Messages redelivered to consumers"},
-        {7, channel_messages_acked, counter, "Messages acknowledged by consumers"},
-        {8, channel_get_empty, counter, "Number of times basic.get operations fetched no message"}
+        {2, channel_get_ack_total, counter, "Total number of messages fetched with basic.get in manual acknowledgement mode"},
+        {3, channel_get_total, counter, "Total number of messages fetched with basic.get in automatic acknowledgement mode"},
+        {4, channel_messages_delivered_ack_total, counter, "Total number of messages delivered to consumers in manual acknowledgement mode"},
+        {5, channel_messages_delivered_total, counter, "Total number of messages delivered to consumers in automatic acknowledgement mode"},
+        {6, channel_messages_redelivered_total, counter, "Total number of messages redelivered to consumers"},
+        {7, channel_messages_acked_total, counter, "Total number of messages acknowledged by consumers"},
+        {8, channel_get_empty_total, counter, "Total number of times basic.get operations fetched no message"}
     ]},
 
     {connection_churn_metrics, [
-        {2, connection_open, counter, "Connections opened"},
-        {3, connection_close, counter, "Connections closed or terminated"},
-        {4, channel_open, counter, "Channels opened"},
-        {5, channel_close, counter, "Channels closed"},
-        {6, queue_declare, counter, "Queues declared"},
-        {7, queue_create, counter, "Queues created"},
-        {8, queue_delete, counter, "Queues deleted"}
+        {2, connection_open_total, counter, "Total number of connections opened"},
+        {3, connection_close_total, counter, "Total number of connections closed or terminated"},
+        {4, channel_open_total, counter, "Total number of channels opened"},
+        {5, channel_close_total, counter, "Total number of channels closed"},
+        {6, queue_declare_total, counter, "Total number of queues declared"},
+        {7, queue_create_total, counter, "Total number of queues created"},
+        {8, queue_delete_total, counter, "Total number of queues deleted"}
     ]},
 
     {connection_coarse_metrics, [
-        {2, connection_bytes_in, counter, "Bytes received on a connection"},
-        {3, connection_bytes_out, counter, "Bytes sent on a connection"},
-        {4, erlang_process_reductions, counter, "Erlang process reductions"}
+        {2, connection_bytes_in_total, counter, "Total number of bytes received on a connection"},
+        {3, connection_bytes_out_total, counter, "Total number of bytes sent on a connection"},
+        {4, erlang_process_reductions_total, counter, "Total number of Erlang process reductions"}
     ]},
 
     {connection_metrics, [
-        {2, connection_packets_in, counter, "Packets received on a connection", recv_cnt},
-        {2, connection_packets_out, counter, "Packets sent on a connection", send_cnt},
-        {2, connection_packets_pending, counter, "Packets waiting to be sent on a connection", send_pend},
+        {2, connection_packets_in_total, counter, "Total number of packets received on a connection", recv_cnt},
+        {2, connection_packets_out_total, counter, "Total number of packets sent on a connection", send_cnt},
+        {2, connection_packets_pending_total, counter, "Total number of packets waiting to be sent on a connection", send_pend},
         {2, connection_channels, gauge, "Channels on a connection", channels}
     ]},
 
     {channel_queue_exchange_metrics, [
-        {2, queue_messages_published, counter, "Messages published to queues"}
+        {2, queue_messages_published_total, counter, "Total number of messages published to queues"}
     ]},
 
     {node_coarse_metrics, [
@@ -101,9 +101,9 @@
         {2, memory_used_bytes, gauge, "Memory used in bytes", mem_used},
         {2, disk_space_available_bytes, gauge, "Disk space available in bytes", disk_free},
         {2, erlang_processes_used, gauge, "Erlang processes used", proc_used},
-        {2, erlang_gc_runs, counter, "Number of Erlang garbage collector runs", gc_num},
-        {2, erlang_gc_bytes_reclaimed, counter, "Bytes of memory reclaimed by Erlang garbage collector", gc_bytes_reclaimed},
-        {2, erlang_scheduler_context_switches, counter, "Erlang scheduler context switches", context_switches}
+        {2, erlang_gc_runs_total, counter, "Total number of Erlang garbage collector runs", gc_num},
+        {2, erlang_gc_bytes_reclaimed_total, counter, "Total number of bytes of memory reclaimed by Erlang garbage collector", gc_bytes_reclaimed},
+        {2, erlang_scheduler_context_switches_total, counter, "Total number of Erlang scheduler context switches", context_switches}
     ]},
 
     {node_metrics, [
@@ -117,42 +117,35 @@
         {2, erlang_net_ticktime_seconds, gauge, "Inter-node heartbeat interval in seconds", net_ticktime}
     ]},
 
-    % These metrics are captured in Erlang Distribution collector, built-into prometheus.erl
-    % https://github.com/deadtrickster/prometheus.erl/pull/92
-    % {node_node_metrics, [
-    %     {2, peer_node_bytes_in, counter, "Bytes received from peer node", recv_bytes},
-    %     {2, peer_node_bytes_out, counter, "Bytes sent to peer node", send_bytes}
-    % ]},
-
     {node_persister_metrics, [
-        {2, io_read, counter, "I/O read operations", io_read_count},
-        {2, io_read_bytes, gauge, "I/O bytes read", io_read_bytes},
-        {2, io_read_time_microseconds, gauge, "I/O read total time in microseconds", io_read_time},
-        {2, io_write, counter, "I/O write operations", io_write_count},
-        {2, io_write_bytes, gauge, "I/O bytes written", io_write_bytes},
-        {2, io_write_time_microseconds, gauge, "I/O write total time in microseconds", io_write_time},
-        {2, io_sync, counter, "I/O sync operations", io_sync_count},
-        {2, io_sync_time_microseconds, gauge, "I/O sync total time in microseconds", io_sync_time},
-        {2, io_seek, counter, "I/O seek operations", io_seek_count},
-        {2, io_seek_time_microseconds, gauge, "I/O seek total time in microseconds", io_seek_time},
-        {2, io_open_attempt, counter, "File open attempts", io_file_handle_open_attempt_count},
-        {2, io_open_attempt_time_microseconds, gauge, "File open attempts total time in microseconds", io_file_handle_open_attempt_time},
-        {2, io_reopen, counter, "Number of times files have been reopened", io_reopen_count},
-        {2, schema_db_ram_tx, counter, "Schema DB memory transactions", mnesia_ram_tx_count},
-        {2, schema_db_disk_tx, counter, "Schema DB disk transactions", mnesia_disk_tx_count},
-        {2, msg_store_read, counter, "Message store read operations", msg_store_read_count},
-        {2, msg_store_write, counter, "Message store write operations", msg_store_write_count},
-        {2, queue_index_read, counter, "Queue Index read operations", queue_index_read_count},
-        {2, queue_index_write, counter, "Queue Index write operations", queue_index_write_count},
-        {2, queue_index_journal_write, counter, "Queue Index Journal write operations", queue_index_journal_write_count}
+        {2, io_read_ops_total, counter, "Total number of I/O read operations", io_read_count},
+        {2, io_read_bytes_total, counter, "Total number of I/O bytes read", io_read_bytes},
+        {2, io_read_time_microseconds_total, counter, "Total I/O read time", io_read_time},
+        {2, io_write_ops_total, counter, "Total number of I/O write operations", io_write_count},
+        {2, io_write_bytes_total, counter, "Total number of I/O bytes written", io_write_bytes},
+        {2, io_write_time_microseconds_total, gauge, "Total I/O write time", io_write_time},
+        {2, io_sync_ops_total, counter, "Total number of I/O sync operations", io_sync_count},
+        {2, io_sync_time_microseconds_total, counter, "Total I/O sync time", io_sync_time},
+        {2, io_seek_ops_total, counter, "Total number of I/O seek operations", io_seek_count},
+        {2, io_seek_time_microseconds_total, counter, "Total I/O seek time", io_seek_time},
+        {2, io_open_attempt_ops_total, counter, "Total number of file open attempts", io_file_handle_open_attempt_count},
+        {2, io_open_attempt_time_microseconds_total, counter, "Total file open attempts time", io_file_handle_open_attempt_time},
+        {2, io_reopen_ops_total, counter, "Total number of times files have been reopened", io_reopen_count},
+        {2, schema_db_ram_tx_total, counter, "Total number of Schema DB memory transactions", mnesia_ram_tx_count},
+        {2, schema_db_disk_tx_total, counter, "Total number of Schema DB disk transactions", mnesia_disk_tx_count},
+        {2, msg_store_read_total, counter, "Total number of Message Store read operations", msg_store_read_count},
+        {2, msg_store_write_total, counter, "Total number of Message Store write operations", msg_store_write_count},
+        {2, queue_index_read_ops_total, counter, "Total number of Queue Index read operations", queue_index_read_count},
+        {2, queue_index_write_ops_total, counter, "Total number of Queue Index write operations", queue_index_write_count},
+        {2, queue_index_journal_write_ops_total, counter, "Total number of Queue Index Journal write operations", queue_index_journal_write_count}
     ]},
 
     {ra_metrics, [
-        {2, raft_term, counter, "Raft member term"},
-        {3, raft_log_snapshot_index, counter, "Raft log snapshot index"},
-        {4, raft_log_last_applied_index, counter, "Raft log last applied index"},
-        {5, raft_log_commit_index, counter, "Raft log commit index"},
-        {6, raft_log_last_written_index, counter, "Raft log last written index"},
+        {2, raft_term, gauge, "Raft member term"},
+        {3, raft_log_snapshot_index, gauge, "Raft log snapshot index"},
+        {4, raft_log_last_applied_index, gauge, "Raft log last applied index"},
+        {5, raft_log_commit_index, gauge, "Raft log commit index"},
+        {6, raft_log_last_written_index, gauge, "Raft log last written index"},
         {7, raft_entry_commit_latency, gauge, "Time taken for an entry to be committed"}
     ]},
 
@@ -160,7 +153,7 @@
         {2, queue_messages_ready, gauge, "Messages ready to be delivered to consumers"},
         {3, queue_messages_unacked, gauge, "Messages delivered to consumers but not yet acknowledged"},
         {4, queue_messages, gauge, "Sum of ready and unacknowledged messages - total queue depth"},
-        {5, erlang_process_reductions, counter, "Erlang process reductions"}
+        {5, erlang_process_reductions_total, counter, "Total number of Erlang process reductions"}
     ]},
 
     {queue_metrics, [
@@ -176,16 +169,16 @@
         {2, queue_messages_persistent_bytes, gauge, "Size in bytes of persistent messages", queue_messages_bytes_persistent},
         {2, queue_messages_paged_out, gauge, "Messages paged out to disk", queue_messages_paged_out},
         {2, queue_messages_paged_out_bytes, gauge, "Size in bytes of messages paged out to disk", queue_messages_bytes_paged_out},
-        {2, queue_disk_reads, gauge, "Number of times queue read messages from disk", disk_reads},
-        {2, queue_disk_writes, gauge, "Number of times queue wrote messages to disk", disk_writes}
+        {2, queue_disk_reads_total, counter, "Total number of times queue read messages from disk", disk_reads},
+        {2, queue_disk_writes_total, counter, "Total number of times queue wrote messages to disk", disk_writes}
     ]}
 ]).
 
 -define(TOTALS, [
-    {connection_created, connections, gauge, "Total number of connections"},
-    {channel_created, channels, gauge, "Total number of channels"},
-    {consumer_created, consumers, gauge, "Total number of consumers"},
-    {queue_metrics, queues, gauge, "Total number of queues"}
+    {connection_created, connections, gauge, "Connections currently running"},
+    {channel_created, channels, gauge, "Channels currently running"},
+    {consumer_created, consumers, gauge, "Consumers currently connected"},
+    {queue_metrics, queues, gauge, "Queues available"}
 ]).
 
 %%====================================================================
