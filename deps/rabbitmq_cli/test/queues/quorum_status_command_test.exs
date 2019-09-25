254fe2045399d00e93e1dbac986802ab2daf3dd5
Michael Klishin <michael@clojurewerkz.org>
2019-09-25 19:52:19 +0300

    Move quorum_status to rabbitmq-queues, add basic tests

---
+++ deps/rabbitmq_cli/test/queues/quorum_status_command_test.exs
@@ -0,0 +1,54 @@
+## The contents of this file are subject to the Mozilla Public License
+## Version 1.1 (the "License"); you may not use this file except in
+## compliance with the License. You may obtain a copy of the License
+## at https://www.mozilla.org/MPL/
+##
+## Software distributed under the License is distributed on an "AS IS"
+## basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+## the License for the specific language governing rights and
+## limitations under the License.
+##
+## The Original Code is RabbitMQ.
+##
+## The Initial Developer of the Original Code is GoPivotal, Inc.
+## Copyright (c) 2007-2019 Pivotal Software, Inc.  All rights reserved.
+
+defmodule RabbitMQ.CLI.Queues.Commands.QuorumQueuesCommandTest do
+  use ExUnit.Case
+  import TestHelper
+
+  @command RabbitMQ.CLI.Queues.Commands.QuorumStatusCommand
+
+  setup_all do
+    RabbitMQ.CLI.Core.Distribution.start()
+
+    :ok
+  end
+
+  setup context do
+    {:ok, opts: %{
+        node: get_rabbit_hostname(),
+        timeout: context[:test_timeout] || 30000
+      }}
+  end
+
+
+  test "validate: treats no arguments as a failure" do
+    assert @command.validate([], %{}) == {:validation_failure, :not_enough_args}
+  end
+
+  test "validate: accepts a single positional argument" do
+    assert @command.validate(["quorum-queue-a"], %{}) == :ok
+  end
+
+  test "validate: when two or more arguments are provided, returns a failure" do
+    assert @command.validate(["quorum-queue-a", "one-extra-arg"], %{}) == {:validation_failure, :too_many_args}
+    assert @command.validate(["quorum-queue-a", "extra-arg", "another-extra-arg"], %{}) == {:validation_failure, :too_many_args}
+  end
+
+  @tag test_timeout: 3000
+  test "run: targeting an unreachable node throws a badrpc" do
+    assert match?({:badrpc, _}, @command.run(["quorum-queue-a"],
+                                             %{node: :jake@thedog, vhost: "/", timeout: 200}))
+  end
+end
