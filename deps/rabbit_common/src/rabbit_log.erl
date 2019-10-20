80dc973aaeffdd6feff6806a10ba3db67ab2f458
Yury.Alioshinov <yury.alioshinov@billing.ru>
2019-10-20 23:38:52 +0300

    add forgotten ldap type

--- deps/rabbit_common/src/rabbit_log.erl
+++ deps/rabbit_common/src/rabbit_log.erl
@@ -31,7 +31,7 @@
 -include("rabbit_log.hrl").
 %%----------------------------------------------------------------------------
 
--type category() :: connection | channel | mirroring | queue | federation | upgrade | ra.
+-type category() :: connection | channel | ldap | mirroring | queue | federation | upgrade | ra.
 
 -spec debug(string()) -> 'ok'.
 -spec debug(string(), [any()]) -> 'ok'.
