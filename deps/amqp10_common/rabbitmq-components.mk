4c5c1984a9733866259f2d30fdbd043943755064
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2019-12-13 14:40:17 +0100

    Update rabbitmq-components.mk

--- deps/amqp10_common/rabbitmq-components.mk
+++ deps/amqp10_common/rabbitmq-components.mk
@@ -116,7 +116,7 @@ dep_cowlib = hex 2.7.0
 dep_jsx = hex 2.9.0
 dep_lager = hex 3.8.0
 dep_prometheus = hex 4.4.0
-dep_ra = hex 1.0.4
+dep_ra = hex 1.0.5
 dep_ranch = hex 1.7.1
 dep_recon = hex 2.5.0
 dep_observer_cli = hex 1.5.2
@@ -322,15 +322,15 @@ prepare-dist::
 
 ifneq ($(wildcard ../../UMBRELLA.md),)
 UNDER_UMBRELLA = 1
+DEPS_DIR ?= $(abspath ..)
+else ifneq ($(wildcard ../../../../UMBRELLA.md),)
+UNDER_UMBRELLA = 1
+DEPS_DIR ?= $(abspath ../../..)
 else ifneq ($(wildcard UMBRELLA.md),)
 UNDER_UMBRELLA = 1
 endif
 
 ifeq ($(UNDER_UMBRELLA),1)
-ifneq ($(PROJECT),rabbitmq_public_umbrella)
-DEPS_DIR ?= $(abspath ..)
-endif
-
 ifneq ($(filter distclean distclean-deps,$(MAKECMDGOALS)),)
 SKIP_DEPS = 1
 endif
