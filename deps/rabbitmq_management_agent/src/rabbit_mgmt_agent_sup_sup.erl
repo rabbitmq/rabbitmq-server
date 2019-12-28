1ae483f56f2abea01fd79b873ebc7f2f0b33561d
Michael Klishin <michael@clojurewerkz.org>
2019-12-28 19:35:38 +0300

    (c) bump
    (cherry picked from commit 7f7b39f07b80c46cdf38903bf6c084d62c9747f7)

--- deps/rabbitmq_management_agent/src/rabbit_mgmt_agent_sup_sup.erl
+++ deps/rabbitmq_management_agent/src/rabbit_mgmt_agent_sup_sup.erl
@@ -10,7 +10,7 @@
 %%
 %%   The Original Code is RabbitMQ.
 %%
-%%   Copyright (c) 2007-2017 Pivotal Software, Inc. All rights reserved.
+%%   Copyright (c) 2007-2020 Pivotal Software, Inc. All rights reserved.
 %%
 
 -module(rabbit_mgmt_agent_sup_sup).
