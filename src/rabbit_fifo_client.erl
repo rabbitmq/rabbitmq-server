a28e8bc8061bcede9da9312eef7766f2846912b7
Michael Klishin <michael@clojurewerkz.org>
2019-12-28 19:35:38 +0300

    (c) bump
    (cherry picked from commit 999483872eeda4cd84ebfd5dbb21b2ee4cff93c1)

--- src/rabbit_fifo_client.erl
+++ src/rabbit_fifo_client.erl
@@ -11,7 +11,7 @@
 %% The Original Code is RabbitMQ.
 %%
 %% The Initial Developer of the Original Code is GoPivotal, Inc.
-%% Copyright (c) 2007-2019 Pivotal Software, Inc.  All rights reserved.
+%% Copyright (c) 2007-2020 Pivotal Software, Inc.  All rights reserved.
 %%
 
 %% @doc Provides an easy to consume API for interacting with the {@link rabbit_fifo.}
