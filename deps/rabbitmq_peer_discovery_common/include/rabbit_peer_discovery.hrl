c875a77d9ed8fcd5d2cf3eb2ced15ecdc3ee68fd
Michael Klishin <michael@clojurewerkz.org>
2019-12-28 19:35:38 +0300

    (c) bump

--- deps/rabbitmq_peer_discovery_common/include/rabbit_peer_discovery.hrl
+++ deps/rabbitmq_peer_discovery_common/include/rabbit_peer_discovery.hrl
@@ -11,7 +11,7 @@
 %% The Original Code is RabbitMQ.
 %%
 %% The Initial Developer of the Original Code is Pivotal Software, Inc.
-%% Copyright (c) 2007-2017 Pivotal Software, Inc.  All rights reserved.
+%% Copyright (c) 2007-2020 Pivotal Software, Inc.  All rights reserved.
 %%
 
 % rabbitmq/rabbitmq-peer-discovery-aws#25
