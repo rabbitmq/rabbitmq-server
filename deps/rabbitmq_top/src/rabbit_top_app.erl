00d38c770e87e716b68116688794fae65dfcdb75
Michael Klishin <michael@clojurewerkz.org>
2019-12-28 19:35:38 +0300

    (c) bump
    (cherry picked from commit a8aaf6d0b077229c22dd7cd006f4fb182216cfac)

--- deps/rabbitmq_top/src/rabbit_top_app.erl
+++ deps/rabbitmq_top/src/rabbit_top_app.erl
@@ -11,7 +11,7 @@
 %%   The Original Code is RabbitMQ.
 %%
 %%   The Initial Developer of the Original Code is VMware, Inc.
-%%   Copyright (c) 2007-2017 Pivotal Software, Inc.  All rights reserved.
+%%   Copyright (c) 2007-2020 Pivotal Software, Inc.  All rights reserved.
 %%
 
 -module(rabbit_top_app).
