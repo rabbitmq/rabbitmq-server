34067cc0d26595a7c50e2b0bb6bdedbc0a8a2b20
Michael Klishin <mklishin@pivotal.io>
2020-02-12 03:18:15 +0300

    Bump (c) year

--- src/rabbit_peer_discovery.erl
+++ src/rabbit_peer_discovery.erl
@@ -11,7 +11,7 @@
 %% The Original Code is RabbitMQ.
 %%
 %% The Initial Developer of the Original Code is Pivotal Software, Inc.
-%% Copyright (c) 2007-2019 Pivotal Software, Inc.  All rights reserved.
+%% Copyright (c) 2007-2020 Pivotal Software, Inc.  All rights reserved.
 %%
 
 -module(rabbit_peer_discovery).
