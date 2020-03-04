24eb24213547e8beda98df81ef7322a2d1be9e45
Gerhard Lazu <gerhard@lazu.co.uk>
2020-03-04 18:53:17 +0000

    Add erlang.mk query-deps* family of targets
    After checking out this branch, run the following command in the
    umbrella:
    
        make update-erlang-mk
    
    This will enable the following command across all RabbitMQ deps:
    
        make query-deps
    
    And this is what the output for one such dependency looks like:
    
        pwd
        /Users/gerhard/github.com/rabbitmq/3.9.x/deps/rabbitmq_management
    
        make query-deps
        rabbitmq_management: ranch hex https://hex.pm/packages/ranch 1.7.1
        rabbitmq_management: rabbit_common git_rmq https://github.com/rabbitmq/rabbitmq-common master
        rabbitmq_management: rabbit git_rmq https://github.com/rabbitmq/rabbitmq-server master
        rabbitmq_management: amqp_client git_rmq https://github.com/rabbitmq/rabbitmq-erlang-client master
        rabbitmq_management: cowboy hex https://hex.pm/packages/cowboy 2.6.1
        rabbitmq_management: cowlib hex https://hex.pm/packages/cowlib 2.7.0
        rabbitmq_management: rabbitmq_web_dispatch git_rmq https://github.com/rabbitmq/rabbitmq-web-dispatch master
        rabbitmq_management: rabbitmq_management_agent git_rmq https://github.com/rabbitmq/rabbitmq-management-agent master
        rabbit_common: rabbitmq_codegen git_rmq https://github.com/rabbitmq/rabbitmq-codegen master
        rabbit_common: lager hex https://hex.pm/packages/lager 3.8.0
        rabbit_common: jsx hex https://hex.pm/packages/jsx 2.9.0
        rabbit_common: ranch hex https://hex.pm/packages/ranch 1.7.1
        rabbit_common: recon hex https://hex.pm/packages/recon 2.5.0
        rabbit_common: credentials_obfuscation hex https://hex.pm/packages/credentials_obfuscation 1.1.0
        lager: goldrush git https://github.com/DeadZen/goldrush.git 0.1.9
        rabbit: rabbitmq_cli git_rmq https://github.com/rabbitmq/rabbitmq-cli master
        rabbit: syslog git https://github.com/schlagert/syslog 3.4.5
        rabbit: cuttlefish hex https://hex.pm/packages/cuttlefish 2.2.0
        rabbit: ranch hex https://hex.pm/packages/ranch 1.7.1
        rabbit: lager hex https://hex.pm/packages/lager 3.8.0
        rabbit: rabbit_common git_rmq https://github.com/rabbitmq/rabbitmq-common master
        rabbit: ra git https://github.com/rabbitmq/ra.git master
        rabbit: sysmon_handler hex https://hex.pm/packages/sysmon_handler 1.2.0
        rabbit: stdout_formatter hex https://hex.pm/packages/stdout_formatter 0.2.2
        rabbit: recon hex https://hex.pm/packages/recon 2.5.0
        rabbit: observer_cli hex https://hex.pm/packages/observer_cli 1.5.3
        rabbitmq_cli: rabbit_common git_rmq https://github.com/rabbitmq/rabbitmq-common master
        rabbitmq_cli: observer_cli hex https://hex.pm/packages/observer_cli 1.5.3
        observer_cli: recon hex https://hex.pm/packages/recon 2.5.0
        cuttlefish: getopt hex https://hex.pm/packages/getopt 1.0.1
        cuttlefish: lager hex https://hex.pm/packages/lager 3.7.0
        ra: elvis_mk git https://github.com/inaka/elvis.mk.git master
        ra: aten hex https://hex.pm/packages/aten 0.5.3
        ra: gen_batch_server hex https://hex.pm/packages/gen_batch_server 0.8.2
        amqp_client: rabbit_common git_rmq https://github.com/rabbitmq/rabbitmq-common master
        cowboy: cowlib git https://github.com/ninenines/cowlib 2.7.0
        cowboy: ranch git https://github.com/ninenines/ranch 1.7.1
        rabbitmq_web_dispatch: rabbit_common git_rmq https://github.com/rabbitmq/rabbitmq-common master
        rabbitmq_web_dispatch: rabbit git_rmq https://github.com/rabbitmq/rabbitmq-server master
        rabbitmq_web_dispatch: cowboy hex https://hex.pm/packages/cowboy 2.6.1
        rabbitmq_management_agent: rabbit_common git_rmq https://github.com/rabbitmq/rabbitmq-common master
        rabbitmq_management_agent: rabbit git_rmq https://github.com/rabbitmq/rabbitmq-server master
    
    re https://github.com/ninenines/erlang.mk/pull/875
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- deps/rabbit_common/mk/rabbitmq-tools.mk
+++ deps/rabbit_common/mk/rabbitmq-tools.mk
@@ -331,3 +331,10 @@ commits-since-release-title:
 	$(verbose) $(call show_commits_since_tag,$*)
 
 endif # ($(wildcard .git),)
+
+# --------------------------------------------------------------------
+# erlang.mk query-deps* formatting
+# --------------------------------------------------------------------
+
+# We need to provide a repo mapping for deps resolved via git_rmq fetch method
+query_repo_git_rmq = https://github.com/rabbitmq/$(call rmq_cmp_repo_name,$(1))
