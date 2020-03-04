79d0b942e94b561dd8d19118c7ab1bdaa4d679d5
Michael Klishin <michael@clojurewerkz.org>
2020-03-04 13:09:56 +0300

    Conditionally start rabbit_os_signal_handler
    We are expected to return an {ok, Pid} or an error
    from gen_event:start_link/1. ok is treated as an unsupported
    value. The easiest to reason about option is to conditionally
    the child in a boot step instead of mimicking a "no-op" child.
    
    Reference #2245.
    
    (cherry picked from commit 1f0c952a835289a9cd8126f77331dfde6159cc89)

--- src/rabbit_os_signal_handler.erl
+++ src/rabbit_os_signal_handler.erl
@@ -2,31 +2,35 @@
 
 -behaviour(gen_event).
 
--export([start_link/0, init/1,
+-export([start_link/0, boot/0, init/1,
          handle_event/2, handle_call/2, handle_info/2,
          terminate/2]).
+
+boot() ->
+  case os:type() of
+      {unix, _} ->
+        rabbit_sup:start_restartable_child(rabbit_os_signal_handler);
+      _ ->
+        rabbit_log:info("Will not override OS signal event handler (erl_signal_server) on this OS"),
+        ok
+  end.
 
 %% CAUTION: Signal handling in this module must be kept consistent
 %% with the same handling in rabbitmq-server(8).
 
 start_link() ->
-    case os:type() of
-        {unix, _} ->
-            rabbit_log:info("Swapping OS signal event handler (erl_signal_server) for our own"),
-            %% delete any previous incarnations, otherwise we would be accumulating
-            %% handlers
-            _ = gen_event:delete_handler(erl_signal_server, ?MODULE, []),
-            %% swap the standard OTP signal handler if there is one
-            ok = gen_event:swap_sup_handler(
-                   erl_signal_server,
-                   %% what to swap
-                   {erl_signal_handler, []},
-                   %% new event handler
-                   {?MODULE, []}),
-            gen_event:start_link({local, ?MODULE});
-        _ ->
-            ok
-    end.
+    rabbit_log:info("Swapping OS signal event handler (erl_signal_server) for our own"),
+    %% delete any previous incarnations, otherwise we would be accumulating
+    %% handlers
+    _ = gen_event:delete_handler(erl_signal_server, ?MODULE, []),
+    %% swap the standard OTP signal handler if there is one
+    ok = gen_event:swap_sup_handler(
+           erl_signal_server,
+           %% what to swap
+           {erl_signal_handler, []},
+           %% new event handler
+           {?MODULE, []}),
+    gen_event:start_link({local, ?MODULE}).
 
 init(_) ->
     ok = os:set_signal(sigterm, handle),
