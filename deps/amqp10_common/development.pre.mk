e7559ff2dad3400ffd42ba38fbf517ec00cd4985
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2020-03-06 12:04:08 +0100

    Makefile: Add rabbit_common to $BUILD_DEPS
    (cherry picked from commit cb5422539741f9e676e5ecdad588085bcd4347ac)

--- deps/amqp10_common/development.pre.mk
+++ deps/amqp10_common/development.pre.mk
@@ -3,7 +3,7 @@
 # Generated files are published to Hex.pm however so people using this
 # source won't have to depend on Python and rabbitmq-codegen.
 
-BUILD_DEPS = rabbitmq_codegen
+BUILD_DEPS += rabbitmq_codegen
 
 EXTRA_SOURCES += include/amqp10_framing.hrl				\
 		 src/amqp10_framing0.erl
