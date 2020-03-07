5d5c278a71bd5f3a011a226b05ab5a278ef31b4c
Michael Klishin <michael@clojurewerkz.org>
2020-03-07 20:29:27 +0300

    Compile

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_rebalance_queues.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_rebalance_queues.erl
@@ -53,7 +53,7 @@ resource_exists(Req, State) ->
 accept_content(Req, {_Mode, #context{user = #user{username = Username}}}=State) ->
     try
         spawn(fun() ->
-            rabbit_log:info("User '~s' has initiated a queue rebalance", [Username])
+            rabbit_log:info("User '~s' has initiated a queue rebalance", [Username]),
             rabbit_amqqueue:rebalance(all, ".*", ".*")
         end),
         {true, Req, State}
