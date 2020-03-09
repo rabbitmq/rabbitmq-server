597c4740f9941d745bebbb258147c7092f4f5aa4
Michael Klishin <michael@clojurewerkz.org>
2020-03-09 10:47:00 +0300

    Correct misleading licensing info
    This plugin had a few copy-and-paste artifacts:
    
     * MPL 2 licenses were used instead of MPL 1.1 (as the rest of RabbitMQ 
    does)
     * LICENSE.md claimed that the license is actually MIT
    
    It was never the intent of the core team to use either
    license as that would contradict the license used by all
    other tier 1 plugins.

--- deps/rabbitmq_recent_history_exchange/include/rabbit_recent_history.hrl
+++ deps/rabbitmq_recent_history_exchange/include/rabbit_recent_history.hrl
@@ -1,18 +1,14 @@
-%%  The contents of this file are subject to the Mozilla Public License
-%%  Version 2.0 (the "License"); you may not use this file except in
-%%  compliance with the License. You may obtain a copy of the License
-%%  at http://www.mozilla.org/MPL/
+%% The contents of this file are subject to the mozilla public license
+%% version 1.1 (the "license"); you may not use this file except in
+%% compliance with the license. you may obtain a copy of the license at
+%% https://www.mozilla.org/mpl/
 %%
-%%  Software distributed under the License is distributed on an "AS IS"
-%%  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-%%  the License for the specific language governing rights and
-%%  limitations under the License.
+%% software distributed under the license is distributed on an "as is"
+%% basis, without warranty of any kind, either express or implied. see the
+%% license for the specific language governing rights and limitations
+%% under the license.
 %%
-%%  The Original Code is RabbitMQ Delayed Message
-%%
-%%  The Initial Developer of the Original Code is Pivotal Software, Inc.
-%%  Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
-%%
+%% Copyright (c) 2007-2020 VMware, Inc and its affiliates. All rights reserved.
 
 -define(KEEP_NB, 20).
 -define(RH_TABLE, rh_exchange_table).
