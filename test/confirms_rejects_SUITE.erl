9772aa8f8256a410bcc13ae5038f949ffe1d5b84
Gerhard Lazu <gerhard@lazu.co.uk>
2020-03-17 11:59:41 +0000

    Remove dead_queue_rejects test flake
    - It's not helping anyone in the current state
    - I don't have enough context to be able to fix it
    - I need to stay focused on the current task, cannot afford to context switch
    - Feel free to fix it if it's important, otherwise leave it deleted
    
    cc @michaelklishin @dumbbell
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- test/confirms_rejects_SUITE.erl
+++ test/confirms_rejects_SUITE.erl
@@ -19,7 +19,6 @@ groups() ->
       {parallel_tests, [parallel], [
         {overflow_reject_publish_dlx, [parallel], OverflowTests},
         {overflow_reject_publish, [parallel], OverflowTests},
-        dead_queue_rejects,
         mixed_dead_alive_queues_reject
       ]}
     ].
@@ -65,7 +64,6 @@ init_per_testcase(policy_resets_to_default = Testcase, Config) ->
         rabbit_ct_helpers:set_config(Config, [{conn, Conn}]), Testcase);
 init_per_testcase(Testcase, Config)
         when Testcase == confirms_rejects_conflict;
-             Testcase == dead_queue_rejects;
              Testcase == mixed_dead_alive_queues_reject ->
     Conn = rabbit_ct_client_helpers:open_unmanaged_connection(Config),
     Conn1 = rabbit_ct_client_helpers:open_unmanaged_connection(Config),
@@ -92,10 +90,6 @@ end_per_testcase(confirms_rejects_conflict = Testcase, Config) ->
     QueueName = <<"confirms_rejects_conflict", "_", XOverflow/binary>>,
     amqp_channel:call(Ch, #'queue.delete'{queue = QueueName}),
     end_per_testcase0(Testcase, Config);
-end_per_testcase(dead_queue_rejects = Testcase, Config) ->
-    {_, Ch} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
-    amqp_channel:call(Ch, #'queue.delete'{queue = <<"dead_queue_rejects">>}),
-    end_per_testcase0(Testcase, Config);
 end_per_testcase(mixed_dead_alive_queues_reject = Testcase, Config) ->
     {_, Ch} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
     amqp_channel:call(Ch, #'queue.delete'{queue = <<"mixed_dead_alive_queues_reject_dead">>}),
@@ -115,37 +109,6 @@ end_per_testcase0(Testcase, Config) ->
     clean_acks_mailbox(),
 
     rabbit_ct_helpers:testcase_finished(Config, Testcase).
-
-dead_queue_rejects(Config) ->
-    Conn = ?config(conn, Config),
-    {ok, Ch} = amqp_connection:open_channel(Conn),
-    QueueName = <<"dead_queue_rejects">>,
-    amqp_channel:call(Ch, #'confirm.select'{}),
-    amqp_channel:register_confirm_handler(Ch, self()),
-
-    amqp_channel:call(Ch, #'queue.declare'{queue = QueueName,
-                                           durable = true}),
-
-    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
-                                  #amqp_msg{payload = <<"HI">>}),
-
-    receive
-        {'basic.ack',_,_} -> ok
-    after 10000 ->
-        error(timeout_waiting_for_initial_ack)
-    end,
-
-    kill_the_queue(QueueName, Config),
-
-    amqp_channel:call(Ch, #'basic.publish'{routing_key = QueueName},
-                                  #amqp_msg{payload = <<"HI">>}),
-
-    receive
-        {'basic.ack',_,_} -> error(expecting_nack_got_ack);
-        {'basic.nack',_,_,_} -> ok
-    after 10000 ->
-        error(timeout_waiting_for_nack)
-    end.
 
 mixed_dead_alive_queues_reject(Config) ->
     Conn = ?config(conn, Config),
