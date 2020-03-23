d8ff8a548a761056007bd504e9cd528ce8c0ef23
Luke Bakken <lbakken@pivotal.io>
2020-03-23 10:26:06 -0700

    Setting idle_timeout to 0 will request a connection for every operation
    [171933299]

--- deps/rabbitmq_auth_backend_ldap/src/rabbit_auth_backend_ldap.erl
+++ deps/rabbitmq_auth_backend_ldap/src/rabbit_auth_backend_ldap.erl
@@ -619,7 +619,7 @@ get_or_create_conn(IsAnon, Servers, Opts) ->
             Timeout = rabbit_misc:pget(idle_timeout, Opts, infinity),
             %% Defer the timeout by re-setting it.
             set_connection_timeout(Key, Timeout),
-            {ok, Conn};
+            {ok, {eldap_pooled, Conn}};
         error      ->
             {Timeout, EldapOpts} = case lists:keytake(idle_timeout, 1, Opts) of
                 false                             -> {infinity, Opts};
