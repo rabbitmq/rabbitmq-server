8006d36c04c8c38ca6e31cff40552a12c2614ca0
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2020-04-28 13:16:37 +0200

    rabbitmq-github-actions.mk: Set `DO_COMMIT=yes` to automatically commit
    ... the generated workflows.

--- deps/rabbit_common/mk/rabbitmq-github-actions.mk
+++ deps/rabbit_common/mk/rabbitmq-github-actions.mk
@@ -46,10 +46,11 @@ CT_SUITE_JOB_BASENAME := $(filter %-CT_SUITE.yaml,$(TESTING_JOB_BASENAMES))
 
 WORKFLOWS := $(TESTING_WORKFLOWS)
 
-SILENT := true
-
 github-actions: $(WORKFLOWS)
-	@$(SILENT)
+	$(verbose) if test "$(DO_COMMIT)" = 'yes'; then \
+		git diff --quiet -- $(WORKFLOWS) \
+		|| git commit -m 'GitHub Actions: Regen workflows' -- $(WORKFLOWS); \
+	fi
 
 # The actual recipe which creates the workflow.
 #
