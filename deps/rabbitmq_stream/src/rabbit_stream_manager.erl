c2e35eb55f2f0e2c9a5f761ebbd8066fa6e42ea2
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-05-06 17:35:41 +0200

    Use stream coordinator to manage streams

--- deps/rabbitmq_stream/src/rabbit_stream_manager.erl
+++ deps/rabbitmq_stream/src/rabbit_stream_manager.erl
@@ -17,83 +17,27 @@
 -module(rabbit_stream_manager).
 -behaviour(gen_server).
 
+-include_lib("rabbit_common/include/rabbit.hrl").
+
 %% API
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
--export([start_link/1, create/1, register/0, delete/1, lookup/1, unregister/0, init_mnesia_tables/0]).
+-export([start_link/1, create/3, register/0, delete/3, lookup/2, unregister/0]).
 
 -record(state, {
     configuration, listeners, monitors
 }).
 
--record(?MODULE, {name, leader_pid, leader, replicas}).
-
--define(TABLE, ?MODULE).
-
--rabbit_boot_step(
-{rabbit_exchange_type_consistent_hash_mnesia,
-    [{description, "rabbitmq stream: shared state"},
-        {mfa, {?MODULE, init_mnesia_tables, []}},
-        {requires, database},
-        {enables, external_infrastructure}]}).
-
-init_mnesia_tables() ->
-    mnesia:create_table(?TABLE,
-        [{attributes, record_info(fields, ?MODULE)},
-            {type, set}]),
-    mnesia:add_table_copy(?TABLE, node(), ram_copies),
-    mnesia:wait_for_tables([?TABLE], 30000).
-
 start_link(Conf) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [Conf], []).
 
 init([Conf]) ->
-    GetAllRecords = fun() ->
-        mnesia:foldl(fun(Record, Acc) ->
-            [Record] ++ Acc
-                     end,
-            [], ?TABLE)
-                    end,
-    Records = mnesia:activity(sync_transaction, GetAllRecords),
-    [begin
-         #?MODULE{
-             name = Name, leader = Leader, replicas = Replicas, leader_pid = LeaderPidInRecord
-         } = Record,
-         error_logger:info_msg("Loading from database ~p~n", [Record]),
-         case node() of
-             Leader ->
-                 case is_process_alive(LeaderPidInRecord) of
-                     true ->
-                         error_logger:info_msg("Process still alive, doing nothing~n"),
-                         ok;
-                     false ->
-                         error_logger:info_msg("Starting the Osiris cluster~n"),
-                         Reference = binary_to_list(Name),
-
-                         OsirisConf = #{leader_node => Leader,
-                             reference => Reference, name => Reference, epoch => 1,
-                             replica_nodes => Replicas},
-
-                         {ok, #{leader_pid := LeaderPid}} = osiris:start_cluster(OsirisConf),
-
-                         error_logger:info_msg("New PID for ~p is ~p~n", [Name, LeaderPid]),
-
-                         UpdateFunction = fun() ->
-                             mnesia:write(Record#?MODULE{leader_pid = LeaderPid}) end,
-                         mnesia:activity(sync_transaction, UpdateFunction)
-                 end;
-             _ ->
-                 error_logger:info_msg("Node not leader, not starting it~n"),
-                 ok
-         end
-     end || Record <- Records],
-
     {ok, #state{configuration = Conf, listeners = [], monitors = #{}}}.
 
-create(Reference) ->
-    gen_server:call(?MODULE, {create, Reference}).
+create(VirtualHost, Reference, Username) ->
+    gen_server:call(?MODULE, {create, VirtualHost, Reference, Username}).
 
-delete(Reference) ->
-    gen_server:call(?MODULE, {delete, Reference}).
+delete(VirtualHost, Reference, Username) ->
+    gen_server:call(?MODULE, {delete, VirtualHost, Reference, Username}).
 
 register() ->
     gen_server:call(?MODULE, {register, self()}).
@@ -101,54 +45,41 @@ register() ->
 unregister() ->
     gen_server:call(?MODULE, {unregister, self()}).
 
-lookup(Stream) ->
-    gen_server:call(?MODULE, {lookup, Stream}).
+lookup(VirtualHost, Stream) ->
+    gen_server:call(?MODULE, {lookup, VirtualHost, Stream}).
 
-replicas_for_current_node() ->
-    rabbit_mnesia:cluster_nodes(all) -- [node()].
-
-read(Name) ->
-    mnesia:activity(sync_transaction, fun() -> mnesia:read({?TABLE, Name}) end).
-
-handle_call({create, Reference}, _From, State) ->
-    Key = list_to_binary(Reference),
-    case read(Key) of
-        [] ->
-            LeaderNode = node(),
-            Replicas = replicas_for_current_node(),
-            error_logger:info_msg("Creating ~p cluster on ~p with replica(s) ~p~n", [Key, LeaderNode, Replicas]),
-            OsirisConf = #{leader_node => node(),
-                reference => Reference, name => Reference, epoch => 1,
-                replica_nodes => Replicas},
-            {ok, #{leader_pid := LeaderPid}} = Res = osiris:start_cluster(OsirisConf),
-            ClusterRecord = #?MODULE{name = Key, leader_pid = LeaderPid, leader = LeaderNode, replicas = Replicas},
-            F = fun() ->
-                mnesia:write(ClusterRecord)
-                end,
-            mnesia:activity(sync_transaction, F),
-            {reply, Res, State};
-        [_ClusterRecord] ->
-            {reply, {error, reference_already_exists}, State}
+handle_call({create, VirtualHost, Reference, Username}, _From, State) ->
+    Name = #resource{virtual_host = VirtualHost, kind = queue, name = Reference},
+    Q0 = amqqueue:new(
+        Name,
+        none, true, false, none, [{<<"x-queue-type">>, longstr, <<"stream">>}],
+        VirtualHost, #{user => Username}, rabbit_stream_queue
+    ),
+    try
+        case rabbit_stream_queue:declare(Q0, node()) of
+            {new, Q} ->
+                {reply, {ok, amqqueue:get_type_state(Q)}, State};
+            _ ->
+                {reply, {error, reference_already_exists}, State}
+        end
+    catch
+        exit:Error ->
+            error_logger:info_msg("Error while creating ~p stream, ~p~n", [Reference, Error]),
+            {reply, {error, internal_error}, State}
     end;
-handle_call({delete, Reference}, _From, #state{listeners = Listeners} = State) ->
-    Key = list_to_binary(Reference),
-    case read(Key) of
-        [] ->
-            {reply, {error, reference_not_found}, State};
-        [ClusterRecord] ->
-            Conf = #{
-                name => Reference,
-                reference => Reference,
-                replica_nodes => ClusterRecord#?MODULE.replicas,
-                leader_pid => ClusterRecord#?MODULE.leader_pid,
-                leader_node => ClusterRecord#?MODULE.leader},
-            ok = osiris:delete_cluster(Conf),
+handle_call({delete, VirtualHost, Reference, Username}, _From, #state{listeners = Listeners} = State) ->
+    Name = #resource{virtual_host = VirtualHost, kind = queue, name = Reference},
+    case rabbit_amqqueue:lookup(Name) of
+        {ok, Q} ->
+            {ok, _} = rabbit_stream_queue:delete(Q, false, false, Username),
             [Pid ! {stream_manager, cluster_deleted, Reference} || Pid <- Listeners],
-            F = fun() ->
-                mnesia:delete({?TABLE, Key})
-                end,
-            mnesia:activity(sync_transaction, F),
-            {reply, {ok, deleted}, State}
+            {reply, {ok, deleted}, State};
+        {error, not_found} ->
+            {reply, {error, reference_not_found}, State};
+        Other ->
+            error_logger:info_msg("Unexpected result when trying to look up stream ~p for deletion, ~p~n",
+                [Reference, Other]),
+            {reply, {error, internal_error}, State}
     end;
 handle_call({register, Pid}, _From, #state{listeners = Listeners, monitors = Monitors} = State) ->
     case lists:member(Pid, Listeners) of
@@ -167,12 +98,14 @@ handle_call({unregister, Pid}, _From, #state{listeners = Listeners, monitors = M
                         maps:remove(Pid, Monitors)
                 end,
     {reply, ok, State#state{listeners = lists:delete(Pid, Listeners), monitors = Monitors1}};
-handle_call({lookup, Stream}, _From, State) ->
-    Res = case read(Stream) of
-              [] ->
-                  cluster_not_found;
-              [#?MODULE{leader_pid = LeaderPid}] ->
-                  LeaderPid
+handle_call({lookup, VirtualHost, Stream}, _From, State) ->
+    Name = #resource{virtual_host = VirtualHost, kind = queue, name = Stream},
+    Res = case rabbit_amqqueue:lookup(Name) of
+              {ok, Q} ->
+                  #{leader_pid := LeaderPid} = amqqueue:get_type_state(Q),
+                  LeaderPid;
+              _ ->
+                  cluster_not_found
           end,
     {reply, Res, State}.
 
