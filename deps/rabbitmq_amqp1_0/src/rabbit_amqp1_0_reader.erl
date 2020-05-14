e95350999be387c44f507c82473897a8441fa0a5
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2020-05-14 11:36:01 +0200

    rabbit_amqp1_0_reader: Use `selected_cipher_suite` instead of `cipher_suite`
    ... in the ssl:connection_information/1 returned proplist. `cipher_suite`
    no longer exists in Erlang Git as of this commit (it is deprecated in
    the documentation for quite some time). However, `selected_cipher_suite`
    is supported in at least Erlang 21+, so let's use this one.

--- deps/rabbitmq_amqp1_0/src/rabbit_amqp1_0_reader.erl
+++ deps/rabbitmq_amqp1_0/src/rabbit_amqp1_0_reader.erl
@@ -797,14 +797,10 @@ ssl_info(F, #v1{sock = Sock}) ->
         {error, _}  -> '';
         {ok, Items} ->
             P = proplists:get_value(protocol, Items),
-            CS = proplists:get_value(cipher_suite, Items),
-            %% The first form is R14.
-            %% The second is R13 - the extra term is exportability (by
-            %% inspection, the docs are wrong).
-            case CS of
-                {K, C, H}    -> F({P, {K, C, H}});
-                {K, C, H, _} -> F({P, {K, C, H}})
-            end
+            #{cipher := C,
+              key_exchange := K,
+              mac := H} = proplists:get_value(selected_cipher_suite, Items),
+            F({P, {K, C, H}})
     end.
 
 cert_info(F, #v1{sock = Sock}) ->
