1250cf2c45a2cc86fae8ae93558498b550971a10
Ayanda-D <ayanda.dube@erlang-solutions.com>
2020-06-04 12:12:41 +0100

    Update per-vhost connection count references to new API

--- test/per_vhost_connection_limit_SUITE.erl
+++ test/per_vhost_connection_limit_SUITE.erl
@@ -137,7 +137,7 @@ clear_all_connection_tracking_tables(Config) ->
     [rabbit_ct_broker_helpers:rpc(Config,
         N,
         rabbit_connection_tracking,
-        clear_tracked_connection_tables_for_this_node,
+        clear_tracking_tables,
         []) || N <- rabbit_ct_broker_helpers:get_node_configs(Config, nodename)].
 
 %% -------------------------------------------------------------------
