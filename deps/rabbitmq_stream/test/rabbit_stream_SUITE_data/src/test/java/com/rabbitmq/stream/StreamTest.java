5c64a95ce9aa5d9d1c63d4d1011e9851f318d820
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-06-11 15:38:07 +0200

    Add Java-based tests for clustering

---
+++ deps/rabbitmq_stream/test/rabbit_stream_SUITE_data/src/test/java/com/rabbitmq/stream/StreamTest.java
@@ -0,0 +1,139 @@
+// The contents of this file are subject to the Mozilla Public License
+// Version 2.0 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License
+// at https://www.mozilla.org/en-US/MPL/2.0/
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+// the License for the specific language governing rights and
+// limitations under the License.
+//
+// The Original Code is RabbitMQ.
+//
+// The Initial Developer of the Original Code is Pivotal Software, Inc.
+// Copyright (c) 2020 VMware, Inc. or its affiliates.  All rights reserved.
+//
+
+package com.rabbitmq.stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(TestUtils.StreamTestInfrastructureExtension.class)
+public class StreamTest {
+
+    String stream;
+    TestUtils.ClientFactory cf;
+
+    static Stream<Arguments> shouldBePossibleToPublishFromAnyNodeAndConsumeFromAnyMember() {
+        return Stream.of(
+                brokers("leader", metadata -> metadata.getLeader(), "leader", metadata -> metadata.getLeader()),
+                brokers("leader", metadata -> metadata.getLeader(), "replica", metadata -> metadata.getReplicas().iterator().next()),
+                brokers("replica", metadata -> metadata.getReplicas().iterator().next(), "leader", metadata -> metadata.getLeader()),
+                brokers("replica", metadata -> new ArrayList<>(metadata.getReplicas()).get(0), "replica", metadata -> new ArrayList<>(metadata.getReplicas()).get(1))
+        );
+    }
+
+    static Arguments brokers(String dp, Function<Client.StreamMetadata, Client.Broker> publisherBroker,
+                             String dc, Function<Client.StreamMetadata, Client.Broker> consumerBroker) {
+        return Arguments.of(new FunctionWithToString<>(dp, publisherBroker), new FunctionWithToString<>(dc, consumerBroker));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void shouldBePossibleToPublishFromAnyNodeAndConsumeFromAnyMember(Function<Client.StreamMetadata, Client.Broker> publisherBroker,
+                                                                     Function<Client.StreamMetadata, Client.Broker> consumerBroker) throws Exception {
+        int messageCount = 10_000;
+        Client client = cf.get();
+        Map<String, Client.StreamMetadata> metadata = client.metadata(stream);
+        assertThat(metadata).hasSize(1).containsKey(stream);
+        Client.StreamMetadata streamMetadata = metadata.get(stream);
+
+        CountDownLatch publishingLatch = new CountDownLatch(messageCount);
+        Client publisher = cf.get(new Client.ClientParameters()
+                .host(publisherBroker.apply(streamMetadata).getHost())
+                .port(publisherBroker.apply(streamMetadata).getPort())
+                .confirmListener(publishingId -> publishingLatch.countDown()));
+
+        IntStream.range(0, messageCount).forEach(i -> publisher.publish(stream, ("hello " + i).getBytes(StandardCharsets.UTF_8)));
+
+        assertThat(publishingLatch.await(10, TimeUnit.SECONDS)).isTrue();
+
+        CountDownLatch consumingLatch = new CountDownLatch(messageCount);
+        Set<String> bodies = ConcurrentHashMap.newKeySet(messageCount);
+        Client consumer = cf.get(new Client.ClientParameters()
+                .host(consumerBroker.apply(streamMetadata).getHost())
+                .port(consumerBroker.apply(streamMetadata).getPort())
+                .chunkListener((client1, subscriptionId, offset, messageCount1, dataSize) -> client1.credit(subscriptionId, 10))
+                .messageListener((subscriptionId, offset, message) -> {
+                    bodies.add(new String(message.getBodyAsBinary(), StandardCharsets.UTF_8));
+                    consumingLatch.countDown();
+                })
+        );
+
+        consumer.subscribe(1, stream, OffsetSpecification.first(), 10);
+
+        assertThat(consumingLatch.await(10, TimeUnit.SECONDS)).isTrue();
+        assertThat(bodies).hasSize(messageCount);
+        IntStream.range(0, messageCount).forEach(i -> assertThat(bodies.contains("hello " + i)));
+    }
+
+    @Test
+    void metadataOnClusterShouldReturnLeaderAndReplicas() {
+        Client client = cf.get();
+        Map<String, Client.StreamMetadata> metadata = client.metadata(stream);
+        assertThat(metadata).hasSize(1).containsKey(stream);
+        Client.StreamMetadata streamMetadata = metadata.get(stream);
+        assertThat(streamMetadata.getResponseCode()).isEqualTo(Constants.RESPONSE_CODE_OK);
+        assertThat(streamMetadata.getReplicas()).hasSize(2);
+
+        BiConsumer<Client.Broker, Client.Broker> assertNodesAreDifferent = (node, anotherNode) -> {
+            assertThat(node.getHost()).isEqualTo(anotherNode.getHost());
+            assertThat(node.getPort()).isNotEqualTo(anotherNode.getPort());
+        };
+
+        streamMetadata.getReplicas().forEach(replica -> assertNodesAreDifferent.accept(replica, streamMetadata.getLeader()));
+        List<Client.Broker> replicas = new ArrayList<>(streamMetadata.getReplicas());
+        assertNodesAreDifferent.accept(replicas.get(0), replicas.get(1));
+    }
+
+    static class FunctionWithToString<T, R> implements Function<T, R> {
+
+        final String toString;
+        final Function<T, R> delegate;
+
+        FunctionWithToString(String toString, Function<T, R> delegate) {
+            this.toString = toString;
+            this.delegate = delegate;
+        }
+
+        @Override
+        public R apply(T t) {
+            return delegate.apply(t);
+        }
+
+        @Override
+        public String toString() {
+            return toString;
+        }
+    }
+
+}
