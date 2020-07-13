119eb99e8ddfaab0b50689c4ecc903189bee5ad5
dcorbacho <dparracorbacho@piotal.io>
2020-07-13 17:39:36 +0100

    Switch to Mozilla Public License 2.0 (MPL 2.0)

--- deps/rabbitmq_mqtt/test/auth_SUITE.erl
+++ deps/rabbitmq_mqtt/test/auth_SUITE.erl
@@ -1,3 +1,9 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2020 VMware, Inc. or its affiliates.  All rights reserved.
+%%
 -module(auth_SUITE).
 -compile([export_all]).
 
