105d68667a98e77da68621e3471d34f790f1d32d
dcorbacho <dparracorbacho@piotal.io>
2020-07-14 23:23:27 +0100

    Switch to Mozilla Public License 2.0 (MPL 2.0)

--- include/vhost_v2.hrl
+++ include/vhost_v2.hrl
@@ -1,3 +1,9 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2020 VMware, Inc. or its affiliates.  All rights reserved.
+%%
 -define(is_vhost_v2(V), is_record(V, vhost, 4)).
 
 -define(vhost_v2_field_name(Q), element(2, Q)).
