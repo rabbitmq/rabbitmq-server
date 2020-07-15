87de10ce67426279cc3954fd2be6d5519520ab7d
dcorbacho <dparracorbacho@piotal.io>
2020-07-15 15:08:19 +0100

    Switch to Mozilla Public License 2.0 (MPL 2.0)

--- deps/rabbitmq_auth_backend_oauth2/src/uaa_jwt_jwt.erl
+++ deps/rabbitmq_auth_backend_oauth2/src/uaa_jwt_jwt.erl
@@ -1,3 +1,9 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2020 VMware, Inc. or its affiliates.  All rights reserved.
+%%
 -module(uaa_jwt_jwt).
 
 %% Transitional step until we can require Erlang/OTP 21 and
