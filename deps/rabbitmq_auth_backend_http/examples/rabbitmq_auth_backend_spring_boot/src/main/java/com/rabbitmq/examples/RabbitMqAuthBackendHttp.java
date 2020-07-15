fa170b8cb5b10803e72bb3c677897d61a1465824
dcorbacho <dparracorbacho@piotal.io>
2020-07-15 12:31:17 +0100

    Switch to Mozilla Public License 2.0 (MPL 2.0)

--- deps/rabbitmq_auth_backend_http/examples/rabbitmq_auth_backend_spring_boot/src/main/java/com/rabbitmq/examples/RabbitMqAuthBackendHttp.java
+++ deps/rabbitmq_auth_backend_http/examples/rabbitmq_auth_backend_spring_boot/src/main/java/com/rabbitmq/examples/RabbitMqAuthBackendHttp.java
@@ -1,17 +1,8 @@
 /**
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License
- * at http://www.mozilla.org/MPL/
- * <p>
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
- * the License for the specific language governing rights and
- * limitations under the License.
- * <p>
- * The Original Code is RabbitMQ HTTP authentication.
- * <p>
- * The Initial Developer of the Original Code is VMware, Inc.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
  * Copyright (c) 2017-2020 VMware, Inc. or its affiliates.  All rights reserved.
  */
 
