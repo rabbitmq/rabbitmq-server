59691c26293accca8c3d69294354de8224d70c71
Michael Klishin <michael@clojurewerkz.org>
2020-09-02 17:44:23 +0300

    Use await_condition in this timing-sensitive test
    (cherry picked from commit 3bceaad700bfc08f343af1d2dbee30cc1a78af3d)

--- deps/rabbitmq_management/test/rabbit_mgmt_http_SUITE.erl
+++ deps/rabbitmq_management/test/rabbit_mgmt_http_SUITE.erl
@@ -3148,7 +3148,10 @@ user_limits_list_test(Config) ->
         end,
         Expected),
 
-    Expected = http_get(Config, "/user-limits", ?OK),
+    rabbit_ct_helpers:await_condition(
+        fun() ->
+            Expected =:= http_get(Config, "/user-limits", ?OK)
+        end),
     Limits1 = http_get(Config, "/user-limits/limit_test_user_1", ?OK),
     Limits2 = http_get(Config, "/user-limits/limit_test_user_2", ?OK),
 
