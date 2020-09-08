e8948634c59df522a96545eb72b1d0cab9265619
Noah Kantrowitz <noah@coderanger.net>
2020-09-08 16:20:47 -0700

    Fix destructuring assignment.
    I think.

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_healthchecks.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_healthchecks.erl
@@ -50,9 +50,9 @@ to_json(ReqData, Context) ->
     end.
 
 failure(Message, ReqData, Context) ->
-    Response, ReqData1, Context1 = rabbit_mgmt_util:reply([{status, failed},
-                                                           {reason, Message}],
-                                                          ReqData, Context),
+    {Response, ReqData1, Context1} = rabbit_mgmt_util:reply([{status, failed},
+                                                            {reason, Message}],
+                                                           ReqData, Context),
     {stop, cowboy_req:reply(500, #{}, Response, ReqData1), Context1}.
 
 is_authorized(ReqData, Context) ->
