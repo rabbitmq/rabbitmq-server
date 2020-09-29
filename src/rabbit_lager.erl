688539ce4e32b6d91ace847a38e6243dba83591a
Michael Klishin <michael@clojurewerkz.org>
2020-09-29 17:56:30 +0300

    Use application:ensure_all_started/1 to start syslog

--- src/rabbit_lager.erl
+++ src/rabbit_lager.erl
@@ -407,7 +407,7 @@ generate_handler(Backend, HandlerConfig) ->
                            lists:ukeysort(1, default_handler_config(Backend)))}].
 
 configure_handler_backend(syslog_lager_backend) ->
-    app_utils:start_applications([syslog]);
+    application:ensure_all_started(syslog);
 configure_handler_backend(_Backend) ->
     ok.
 
