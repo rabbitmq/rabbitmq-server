0976e35f21244c7ec8c7b594dc77433003efb5ed
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-09-29 12:05:29 +0200

    Bump dependencies in Java test suite
    And apply Google Java Format with Maven Spotless plugin.

--- deps/rabbitmq_stream/test/rabbit_stream_SUITE_data/src/test/java/com/rabbitmq/stream/Host.java
+++ deps/rabbitmq_stream/test/rabbit_stream_SUITE_data/src/test/java/com/rabbitmq/stream/Host.java
@@ -25,88 +25,93 @@ import java.net.UnknownHostException;
 
 public class Host {
 
-    private static String capture(InputStream is)
-            throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String line;
-        StringBuilder buff = new StringBuilder();
-        while ((line = br.readLine()) != null) {
-            buff.append(line).append("\n");
-        }
-        return buff.toString();
+  private static String capture(InputStream is) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    String line;
+    StringBuilder buff = new StringBuilder();
+    while ((line = br.readLine()) != null) {
+      buff.append(line).append("\n");
     }
+    return buff.toString();
+  }
 
-    private static Process executeCommand(String command) throws IOException {
-        Process pr = executeCommandProcess(command);
+  private static Process executeCommand(String command) throws IOException {
+    Process pr = executeCommandProcess(command);
 
-        int ev = waitForExitValue(pr);
-        if (ev != 0) {
-            String stdout = capture(pr.getInputStream());
-            String stderr = capture(pr.getErrorStream());
-            throw new IOException("unexpected command exit value: " + ev +
-                    "\ncommand: " + command + "\n" +
-                    "\nstdout:\n" + stdout +
-                    "\nstderr:\n" + stderr + "\n");
-        }
-        return pr;
+    int ev = waitForExitValue(pr);
+    if (ev != 0) {
+      String stdout = capture(pr.getInputStream());
+      String stderr = capture(pr.getErrorStream());
+      throw new IOException(
+          "unexpected command exit value: "
+              + ev
+              + "\ncommand: "
+              + command
+              + "\n"
+              + "\nstdout:\n"
+              + stdout
+              + "\nstderr:\n"
+              + stderr
+              + "\n");
     }
+    return pr;
+  }
 
-    private static int waitForExitValue(Process pr) {
-        while (true) {
-            try {
-                pr.waitFor();
-                break;
-            } catch (InterruptedException ignored) {
-            }
-        }
-        return pr.exitValue();
+  private static int waitForExitValue(Process pr) {
+    while (true) {
+      try {
+        pr.waitFor();
+        break;
+      } catch (InterruptedException ignored) {
+      }
     }
+    return pr.exitValue();
+  }
 
-    private static Process executeCommandProcess(String command) throws IOException {
-        String[] finalCommand;
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            finalCommand = new String[4];
-            finalCommand[0] = "C:\\winnt\\system32\\cmd.exe";
-            finalCommand[1] = "/y";
-            finalCommand[2] = "/c";
-            finalCommand[3] = command;
-        } else {
-            finalCommand = new String[3];
-            finalCommand[0] = "/bin/sh";
-            finalCommand[1] = "-c";
-            finalCommand[2] = command;
-        }
-        return Runtime.getRuntime().exec(finalCommand);
+  private static Process executeCommandProcess(String command) throws IOException {
+    String[] finalCommand;
+    if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+      finalCommand = new String[4];
+      finalCommand[0] = "C:\\winnt\\system32\\cmd.exe";
+      finalCommand[1] = "/y";
+      finalCommand[2] = "/c";
+      finalCommand[3] = command;
+    } else {
+      finalCommand = new String[3];
+      finalCommand[0] = "/bin/sh";
+      finalCommand[1] = "-c";
+      finalCommand[2] = command;
     }
+    return Runtime.getRuntime().exec(finalCommand);
+  }
 
-    public static Process rabbitmqctl(String command) throws IOException {
-        return rabbitmqctl(command, node1name());
+  public static Process rabbitmqctl(String command) throws IOException {
+    return rabbitmqctl(command, node1name());
+  }
+
+  public static Process rabbitmqctl(String command, String nodename) throws IOException {
+    return executeCommand(rabbitmqctlCommand() + " -n '" + nodename + "'" + " " + command);
+  }
+
+  public static String node1name() {
+    try {
+      return System.getProperty(
+          "node1.name", "rabbit-1@" + InetAddress.getLocalHost().getHostName());
+    } catch (UnknownHostException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public static Process rabbitmqctl(String command, String nodename) throws IOException {
-        return executeCommand(rabbitmqctlCommand() +
-                " -n '" + nodename + "'" +
-                " " + command);
+  public static String node2name() {
+    try {
+      return System.getProperty(
+          "node2.name", "rabbit-2@" + InetAddress.getLocalHost().getHostName());
+    } catch (UnknownHostException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public static String node1name() {
-        try {
-            return System.getProperty("node1.name", "rabbit-1@" + InetAddress.getLocalHost().getHostName());
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String node2name() {
-        try {
-            return System.getProperty("node2.name", "rabbit-2@" + InetAddress.getLocalHost().getHostName());
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    static String rabbitmqctlCommand() {
-        return System.getProperty("rabbitmqctl.bin");
-    }
-
+  static String rabbitmqctlCommand() {
+    return System.getProperty("rabbitmqctl.bin");
+  }
 }
--- deps/rabbitmq_stream/test/rabbit_stream_SUITE_data/src/test/java/com/rabbitmq/stream/TestUtils.java
+++ deps/rabbitmq_stream/test/rabbit_stream_SUITE_data/src/test/java/com/rabbitmq/stream/TestUtils.java
@@ -16,162 +16,164 @@
 
 package com.rabbitmq.stream;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import com.rabbitmq.stream.impl.Client;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import org.junit.jupiter.api.extension.*;
-
 import java.lang.reflect.Field;
 import java.time.Duration;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BooleanSupplier;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.extension.*;
 
 public class TestUtils {
 
-    static int streamPortNode1() {
-        String port = System.getProperty("node1.stream.port", "5555");
-        return Integer.valueOf(port);
+  static int streamPortNode1() {
+    String port = System.getProperty("node1.stream.port", "5555");
+    return Integer.valueOf(port);
+  }
+
+  static int streamPortNode2() {
+    String port = System.getProperty("node2.stream.port", "5556");
+    return Integer.valueOf(port);
+  }
+
+  static void waitAtMost(Duration duration, BooleanSupplier condition) throws InterruptedException {
+    if (condition.getAsBoolean()) {
+      return;
+    }
+    int waitTime = 100;
+    int waitedTime = 0;
+    long timeoutInMs = duration.toMillis();
+    while (waitedTime <= timeoutInMs) {
+      Thread.sleep(waitTime);
+      if (condition.getAsBoolean()) {
+        return;
+      }
+      waitedTime += waitTime;
+    }
+    fail("Waited " + duration.getSeconds() + " second(s), condition never got true");
+  }
+
+  static class StreamTestInfrastructureExtension
+      implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+
+    private static final ExtensionContext.Namespace NAMESPACE =
+        ExtensionContext.Namespace.create(StreamTestInfrastructureExtension.class);
+
+    private static ExtensionContext.Store store(ExtensionContext extensionContext) {
+      return extensionContext.getRoot().getStore(NAMESPACE);
     }
 
-    static int streamPortNode2() {
-        String port = System.getProperty("node2.stream.port", "5556");
-        return Integer.valueOf(port);
+    private static EventLoopGroup eventLoopGroup(ExtensionContext context) {
+      return (EventLoopGroup) store(context).get("nettyEventLoopGroup");
     }
 
-    static void waitAtMost(Duration duration, BooleanSupplier condition) throws InterruptedException {
-        if (condition.getAsBoolean()) {
-            return;
-        }
-        int waitTime = 100;
-        int waitedTime = 0;
-        long timeoutInMs = duration.toMillis();
-        while (waitedTime <= timeoutInMs) {
-            Thread.sleep(waitTime);
-            if (condition.getAsBoolean()) {
-                return;
-            }
-            waitedTime += waitTime;
-        }
-        fail("Waited " + duration.getSeconds() + " second(s), condition never got true");
+    @Override
+    public void beforeAll(ExtensionContext context) {
+      store(context).put("nettyEventLoopGroup", new NioEventLoopGroup());
     }
 
-    static class StreamTestInfrastructureExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+      try {
+        Field streamField =
+            context.getTestInstance().get().getClass().getDeclaredField("eventLoopGroup");
+        streamField.setAccessible(true);
+        streamField.set(context.getTestInstance().get(), eventLoopGroup(context));
+      } catch (NoSuchFieldException e) {
 
-        private static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(StreamTestInfrastructureExtension.class);
+      }
+      try {
+        Field streamField = context.getTestInstance().get().getClass().getDeclaredField("stream");
+        streamField.setAccessible(true);
+        String stream = UUID.randomUUID().toString();
+        streamField.set(context.getTestInstance().get(), stream);
+        Client client =
+            new Client(
+                new Client.ClientParameters()
+                    .eventLoopGroup(eventLoopGroup(context))
+                    .port(streamPortNode1()));
+        Client.Response response = client.create(stream);
+        assertThat(response.isOk()).isTrue();
+        client.close();
+        store(context).put("testMethodStream", stream);
+      } catch (NoSuchFieldException e) {
 
-        private static ExtensionContext.Store store(ExtensionContext extensionContext) {
-            return extensionContext.getRoot().getStore(NAMESPACE);
+      }
+
+      for (Field declaredField : context.getTestInstance().get().getClass().getDeclaredFields()) {
+        if (declaredField.getType().equals(ClientFactory.class)) {
+          declaredField.setAccessible(true);
+          ClientFactory clientFactory = new ClientFactory(eventLoopGroup(context));
+          declaredField.set(context.getTestInstance().get(), clientFactory);
+          store(context).put("testClientFactory", clientFactory);
+          break;
         }
-
-        private static EventLoopGroup eventLoopGroup(ExtensionContext context) {
-            return (EventLoopGroup) store(context).get("nettyEventLoopGroup");
-        }
-
-        @Override
-        public void beforeAll(ExtensionContext context) {
-            store(context).put("nettyEventLoopGroup", new NioEventLoopGroup());
-        }
-
-        @Override
-        public void beforeEach(ExtensionContext context) throws Exception {
-            try {
-                Field streamField = context.getTestInstance().get().getClass().getDeclaredField("eventLoopGroup");
-                streamField.setAccessible(true);
-                streamField.set(context.getTestInstance().get(), eventLoopGroup(context));
-            } catch (NoSuchFieldException e) {
-
-            }
-            try {
-                Field streamField = context.getTestInstance().get().getClass().getDeclaredField("stream");
-                streamField.setAccessible(true);
-                String stream = UUID.randomUUID().toString();
-                streamField.set(context.getTestInstance().get(), stream);
-                Client client = new Client(new Client.ClientParameters().eventLoopGroup(eventLoopGroup(context))
-                        .port(streamPortNode1())
-                );
-                Client.Response response = client.create(stream);
-                assertThat(response.isOk()).isTrue();
-                client.close();
-                store(context).put("testMethodStream", stream);
-            } catch (NoSuchFieldException e) {
-
-            }
-
-            for (Field declaredField : context.getTestInstance().get().getClass().getDeclaredFields()) {
-                if (declaredField.getType().equals(ClientFactory.class)) {
-                    declaredField.setAccessible(true);
-                    ClientFactory clientFactory = new ClientFactory(eventLoopGroup(context));
-                    declaredField.set(context.getTestInstance().get(), clientFactory);
-                    store(context).put("testClientFactory", clientFactory);
-                    break;
-                }
-            }
-
-        }
-
-        @Override
-        public void afterEach(ExtensionContext context) throws Exception {
-            try {
-                Field streamField = context.getTestInstance().get().getClass().getDeclaredField("stream");
-                streamField.setAccessible(true);
-                String stream = (String) streamField.get(context.getTestInstance().get());
-                Client client = new Client(new Client.ClientParameters().eventLoopGroup(eventLoopGroup(context))
-                        .port(streamPortNode1())
-                );
-                Client.Response response = client.delete(stream);
-                assertThat(response.isOk()).isTrue();
-                client.close();
-                store(context).remove("testMethodStream");
-            } catch (NoSuchFieldException e) {
-
-            }
-
-            ClientFactory clientFactory = (ClientFactory) store(context).get("testClientFactory");
-            if (clientFactory != null) {
-                clientFactory.close();
-            }
-        }
-
-        @Override
-        public void afterAll(ExtensionContext context) throws Exception {
-            EventLoopGroup eventLoopGroup = eventLoopGroup(context);
-            eventLoopGroup.shutdownGracefully(1, 10, SECONDS).get(10, SECONDS);
-        }
-
+      }
     }
 
-    static class ClientFactory {
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+      try {
+        Field streamField = context.getTestInstance().get().getClass().getDeclaredField("stream");
+        streamField.setAccessible(true);
+        String stream = (String) streamField.get(context.getTestInstance().get());
+        Client client =
+            new Client(
+                new Client.ClientParameters()
+                    .eventLoopGroup(eventLoopGroup(context))
+                    .port(streamPortNode1()));
+        Client.Response response = client.delete(stream);
+        assertThat(response.isOk()).isTrue();
+        client.close();
+        store(context).remove("testMethodStream");
+      } catch (NoSuchFieldException e) {
 
-        private final EventLoopGroup eventLoopGroup;
-        private final Set<Client> clients = ConcurrentHashMap.newKeySet();
+      }
 
-
-        public ClientFactory(EventLoopGroup eventLoopGroup) {
-            this.eventLoopGroup = eventLoopGroup;
-        }
-
-        public Client get() {
-            return get(new Client.ClientParameters());
-        }
-
-        public Client get(Client.ClientParameters parameters) {
-            // don't set the port, it would override the caller's port setting
-            Client client = new Client(parameters.eventLoopGroup(eventLoopGroup));
-            clients.add(client);
-            return client;
-        }
-
-        private void close() {
-            for (Client c : clients) {
-                c.close();
-            }
-        }
+      ClientFactory clientFactory = (ClientFactory) store(context).get("testClientFactory");
+      if (clientFactory != null) {
+        clientFactory.close();
+      }
     }
 
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+      EventLoopGroup eventLoopGroup = eventLoopGroup(context);
+      eventLoopGroup.shutdownGracefully(1, 10, SECONDS).get(10, SECONDS);
+    }
+  }
+
+  static class ClientFactory {
+
+    private final EventLoopGroup eventLoopGroup;
+    private final Set<Client> clients = ConcurrentHashMap.newKeySet();
+
+    public ClientFactory(EventLoopGroup eventLoopGroup) {
+      this.eventLoopGroup = eventLoopGroup;
+    }
+
+    public Client get() {
+      return get(new Client.ClientParameters());
+    }
+
+    public Client get(Client.ClientParameters parameters) {
+      // don't set the port, it would override the caller's port setting
+      Client client = new Client(parameters.eventLoopGroup(eventLoopGroup));
+      clients.add(client);
+      return client;
+    }
+
+    private void close() {
+      for (Client c : clients) {
+        c.close();
+      }
+    }
+  }
 }
