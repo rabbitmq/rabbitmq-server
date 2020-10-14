b84029f99efe40878e0ac8e74e7b05f0905b6506
Michael Klishin <klishinm@vmware.com>
2020-10-14 23:56:47 +0300

    Merge pull request #450 from rabbitmq/auth-attempt-metrics
    Add enable/disable and list auth attempt metrics
    
    (cherry picked from commit 8069ce8444fe473b56cc0c610a078112b74eb416)

---
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/diagnostics/commands/disable_auth_attempt_source_tracking_command.ex
@@ -0,0 +1,35 @@
+## This Source Code Form is subject to the terms of the Mozilla Public
+## License, v. 2.0. If a copy of the MPL was not distributed with this
+## file, You can obtain one at https://mozilla.org/MPL/2.0/.
+##
+## Copyright (c) 2007-2020 VMware, Inc. or its affiliates.  All rights reserved.
+
+defmodule RabbitMQ.CLI.Diagnostics.Commands.DisableAuthAttemptSourceTrackingCommand do
+  alias RabbitMQ.CLI.Core.DocGuide
+  @behaviour RabbitMQ.CLI.CommandBehaviour
+
+  use RabbitMQ.CLI.Core.MergesNoDefaults
+  use RabbitMQ.CLI.Core.AcceptsNoPositionalArguments
+  use RabbitMQ.CLI.Core.RequiresRabbitAppRunning
+
+  def run([], %{node: node_name}) do
+    :rabbit_misc.rpc_call(node_name, :application, :set_env,
+      [:rabbit, :track_auth_attempt_source, :false])
+  end
+  use RabbitMQ.CLI.DefaultOutput
+
+  def usage, do: "disable_track_auth_attempt_source"
+
+  def usage_doc_guides() do
+    [
+      DocGuide.access_control(),
+      DocGuide.monitoring()
+    ]
+  end
+
+  def help_section(), do: :configuration
+
+  def description(), do: "Disables the tracking of peer IP address and username of authentication attempts"
+
+  def banner([], _), do: "Disabling authentication attempt source tracking ..."
+end
