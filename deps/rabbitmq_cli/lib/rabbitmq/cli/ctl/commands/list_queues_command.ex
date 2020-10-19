d7aaf7bccb74d08928c6e20c875a53165cda45f1
dcorbacho <dparracorbacho@piotal.io>
2020-10-19 15:57:08 +0100

    Add relevant info keys for stream and quorum queues

--- deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/list_queues_command.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/list_queues_command.ex
@@ -21,7 +21,8 @@ defmodule RabbitMQ.CLI.Ctl.Commands.ListQueuesCommand do
             messages_persistent message_bytes message_bytes_ready
             message_bytes_unacknowledged message_bytes_ram message_bytes_persistent
             head_message_timestamp disk_reads disk_writes consumers
-            consumer_utilisation memory slave_pids synchronised_slave_pids state type)a
+            consumer_utilisation memory slave_pids synchronised_slave_pids state type
+            leader members online)a
 
   def description(), do: "Lists queues and their properties"
   def usage(), do: "list_queues [--vhost <vhost>] [--online] [--offline] [--local] [--no-table-headers] [<column>, ...]"
