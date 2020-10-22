1d7c191051a135f0468e1e33ceb04e9a6622edd6
Philip Kuryloski <kuryloskip@vmware.com>
2020-10-22 11:30:18 +0200

    Fix honeycomb build traces
    Using a unique span_id when invoking the buildevents cli

--- ci/scripts/collect.sh
+++ ci/scripts/collect.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 echo "Recording buildevents step finish for ${project} started at ${STEP_START}..."
-buildevents step ${GITHUB_RUN_ID} ${project} ${STEP_START} ${project}
+buildevents step ${GITHUB_RUN_ID} ${GITHUB_RUN_ID}-${project} ${STEP_START} ${project}
 echo "done."
--- ci/scripts/tests.sh
+++ ci/scripts/tests.sh
@@ -6,18 +6,20 @@ cd /workspace/rabbitmq/deps/$project
 
 trap 'catch $?' EXIT
 
+SPAN_ID=${GITHUB_RUN_ID}-${project}
+
 catch() {
     if [ "$1" != "0" ]; then
         make ct-logs-archive && mv *-ct-logs-*.tar.xz /workspace/ct-logs/
     fi
 
-    buildevents step ${GITHUB_RUN_ID} ${project} ${STEP_START} ${project}
+    buildevents step ${GITHUB_RUN_ID} ${SPAN_ID} ${STEP_START} ${project}
 }
 
-buildevents cmd ${GITHUB_RUN_ID} ${project} test-build -- \
+buildevents cmd ${GITHUB_RUN_ID} ${SPAN_ID} test-build -- \
             make test-build
 
-buildevents cmd ${GITHUB_RUN_ID} ${project} tests -- \
+buildevents cmd ${GITHUB_RUN_ID} ${SPAN_ID} tests -- \
             make tests \
                  FULL= \
                  FAIL_FAST=1 \
