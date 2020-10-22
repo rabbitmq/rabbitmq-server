8555cfe67b3c3a671d49628fd3e78ed75225d3d4
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2020-10-22 15:25:24 +0200

    tools/terraform: Fix warnings reported by recent versions of Terraform

--- deps/rabbitmq_ct_helpers/tools/terraform/autoscaling-group/outputs.tf
+++ deps/rabbitmq_ct_helpers/tools/terraform/autoscaling-group/outputs.tf
@@ -1,5 +1,5 @@
 # vim:sw=2:et:
 
 output "uuid" {
-  value = "${module.direct_vms.uuid}"
+  value = module.direct_vms.uuid
 }
--- deps/rabbitmq_ct_helpers/tools/terraform/direct-vms/outputs.tf
+++ deps/rabbitmq_ct_helpers/tools/terraform/direct-vms/outputs.tf
@@ -1,25 +1,25 @@
 # vim:sw=2:et:
 
 output "uuid" {
-  value = "${local.uuid}"
+  value = local.uuid
 }
 
 output "ct_peer_ipaddrs" {
-  value = "${zipmap(
+  value = zipmap(
     data.template_file.erlang_node_hostname.*.rendered,
-    aws_instance.vm.*.public_ip)}"
+    aws_instance.vm.*.public_ip)
 }
 
 output "ct_peer_nodenames" {
-  value = "${zipmap(
+  value = zipmap(
     data.template_file.erlang_node_hostname.*.rendered,
-    data.template_file.erlang_node_nodename.*.rendered)}"
+    data.template_file.erlang_node_nodename.*.rendered)
 }
 
 output "ssh_user_and_host" {
-  value = "${zipmap(
+  value = zipmap(
     data.template_file.erlang_node_hostname.*.rendered,
-    formatlist("%s@%s", local.username, aws_instance.vm.*.public_dns))}"
+    formatlist("%s@%s", local.username, aws_instance.vm.*.public_dns))
 }
 
 // The following variables are used by other modules (e.g.
@@ -27,23 +27,23 @@ output "ssh_user_and_host" {
 // (beside the actual instances).
 
 output "resource_prefix" {
-  value = "${local.resource_prefix}"
+  value = local.resource_prefix
 }
 
 output "instance_name" {
-  value = "${local.vm_name}"
+  value = local.vm_name
 }
 
 output "instance_ami" {
-  value = "${local.ami}"
+  value = local.ami
 }
 
 output "instance_type" {
-  value = "${local.ec2_instance_type}"
+  value = local.ec2_instance_type
 }
 
 output "ssh_key_name" {
-  value = "${aws_key_pair.ci_user.key_name}"
+  value = aws_key_pair.ci_user.key_name
 }
 
 output "security_groups" {
@@ -51,9 +51,9 @@ output "security_groups" {
 }
 
 output "instance_user_data" {
-  value = "${data.template_file.user_data.rendered}"
+  value = data.template_file.user_data.rendered
 }
 
 output "subnet_id" {
-  value = "${aws_subnet.vpc.id}"
+  value = aws_subnet.vpc.id
 }
