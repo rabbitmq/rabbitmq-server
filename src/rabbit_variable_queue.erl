rabbitmq/rabbitmq-server

commit d12b4d8e08005f97c5557f0948b3d03c15ef8c1b

include pending_acks in 'fold' we implement this as a zipper over three iterators

src/rabbit_variable_queue.erl
@@ -676,7 +676,19 @@ ackfold(MsgFun, Acc, State, AckTags) ->
                     end, {Acc, State}, AckTags),
     {AccN, a(StateN)}.
 
-fold(Fun, Acc, State) -> ifold(Fun, Acc, iterator(State), State).
+fold(Fun, Acc, State = #vqstate{index_state = IndexState}) ->
+    {Its, IndexState1} =
+        lists:foldl(fun (It, {Its, IndexState2}) ->
+                            case next(It, IndexState2) of
+                                {empty, IndexState3} ->
+                                    {Its, IndexState3};
+                                {value, MsgStatus, It1, IndexState3} ->
+                                    {[{MsgStatus, It1} | Its], IndexState3}
+                            end
+                    end, {[], IndexState}, [msg_iterator(State),
+                                            disk_ack_iterator(State),
+                                            ram_ack_iterator(State)]),
+    ifold(Fun, Acc, Its, State#vqstate{index_state = IndexState1}).
 
 len(#vqstate { len = Len }) -> Len.
 
@@ -1447,7 +1459,13 @@ delta_limit(#delta { start_seq_id = StartSeqId }) -> StartSeqId.
 %% Iterator
 %%----------------------------------------------------------------------------
 
-iterator(State) -> istate(start, State).
+ram_ack_iterator(State) ->
+    {ack, gb_trees:iterator(State#vqstate.ram_pending_ack)}.
+
+disk_ack_iterator(State) ->
+    {ack, gb_trees:iterator(State#vqstate.disk_pending_ack)}.
+
+msg_iterator(State) -> istate(start, State).
 
 istate(start, State) -> {q4,    State#vqstate.q4,    State};
 istate(q4,    State) -> {q3,    State#vqstate.q3,    State};
@@ -1456,6 +1474,11 @@ istate(delta, State) -> {q2,    State#vqstate.q2,    State};
 istate(q2,    State) -> {q1,    State#vqstate.q1,    State};
 istate(q1,   _State) -> done.
 
+next({ack, It}, IndexState) ->
+    case gb_trees:next(It) of
+        none                     -> {empty, IndexState};
+        {_SeqId, MsgStatus, It1} -> {value, MsgStatus, {ack, It1}, IndexState}
+    end;
 next(done, IndexState) -> {empty, IndexState};
 next({delta, #delta{start_seq_id = SeqId,
                     end_seq_id   = SeqId}, State}, IndexState) ->
@@ -1477,17 +1500,27 @@ next({Key, Q, State}, IndexState) ->
                                     {value, MsgStatus, Next, IndexState}
     end.
 
-ifold(Fun, Acc, It, State = #vqstate{index_state = IndexState}) ->
-    case next(It, IndexState) of
-        {value, MsgStatus, Next, IndexState1} ->
-            State1 = State#vqstate{index_state = IndexState1},
-            {Msg, State2} = read_msg(MsgStatus, State1),
-            case Fun(Msg, MsgStatus#msg_status.msg_props, Acc) of
-                {stop, Acc1} -> {Acc1, State2};
-                {cont, Acc1} -> ifold(Fun, Acc1, Next, State2)
-            end;
-        {empty, IndexState1} ->
-            {Acc, State#vqstate{index_state = IndexState1}}
+ifold(_Fun, Acc, [], State) ->
+    {Acc, State};
+ifold(Fun, Acc, Its, State) ->
+    [{MsgStatus, It} | Rest] = lists:sort(
+                                 fun ({MsgStatus1, _}, {MsgStatus2, _}) ->
+                                         MsgStatus1#msg_status.seq_id <
+                                             MsgStatus2#msg_status.seq_id
+                                 end, Its),
+    {Msg, State1} = read_msg(MsgStatus, State),
+    case Fun(Msg, MsgStatus#msg_status.msg_props, Acc) of
+        {stop, Acc1} ->
+            {Acc1, State};
+        {cont, Acc1} ->
+            case next(It, State1#vqstate.index_state) of
+                {empty, IndexState1} ->
+                    ifold(Fun, Acc1, Rest,
+                          State1#vqstate{index_state = IndexState1});
+                {value, MsgStatus1, It1, IndexState1} ->
+                    ifold(Fun, Acc1, [{MsgStatus1, It1} | Rest],
+                          State1#vqstate{index_state = IndexState1})
+            end
     end.
 
 %%----------------------------------------------------------------------------

commit 3215357d971a04a425bcc287bf9e4f57a8198f7f

rabbit_variable_queue: Ensure modified state is not discarded in ifold
Commit d12b4d8e08005f97c5557f0948b3d03c15ef8c1b introduced a bug where
the old value of `State` is returned in the `stop` case.

The problem was sometimes visible in the backing_queue_SUITE's
`variable_queue_fold` testcase. The testcase crashed with the following
exception:

    === Location: [{rabbit_ct_broker_helpers,rpc,1509},
                  {backing_queue_SUITE,variable_queue_fold,1144},
                  {test_server,ts_tc,1754},
                  {test_server,run_test_case_eval1,1263},
                  {test_server,run_test_case_eval,1195}]
    === === Reason: {error,
                         {case_clause,undefined},
                         [{file_handle_cache,'-partition_handles/1-fun-0-',2,
                              [{file,"src/file_handle_cache.erl"},{line,804}]},
                          {file_handle_cache,get_or_reopen,1,
                              [{file,"src/file_handle_cache.erl"},{line,743}]},
                          {file_handle_cache_stats,timer_tc,1,
                              [{file,"src/file_handle_cache_stats.erl"},
                               {line,54}]},
                          {file_handle_cache_stats,update,2,
                              [{file,"src/file_handle_cache_stats.erl"},
                               {line,40}]},
                          {file_handle_cache,with_handles,3,
                              [{file,"src/file_handle_cache.erl"},{line,700}]},
                          {rabbit_msg_store,read_from_disk,2,
                              [{file,"src/rabbit_msg_store.erl"},{line,1259}]},
                          {rabbit_msg_store,client_read3,3,
                              [{file,"src/rabbit_msg_store.erl"},{line,671}]},
                          {rabbit_msg_store,safe_ets_update_counter,5,
                              [{file,"src/rabbit_msg_store.erl"},{line,1314}]}]}

src/rabbit_variable_queue.erl
@@ -2366,21 +2366,23 @@ inext(It, {Its, IndexState}) ->
             {[{MsgStatus1, Unacked, It1} | Its], IndexState1}
     end.
 
-ifold(_Fun, Acc, [], State) ->
-    {Acc, State};
-ifold(Fun, Acc, Its, State) ->
+ifold(_Fun, Acc, [], State0) ->
+    {Acc, State0};
+ifold(Fun, Acc, Its0, State0) ->
     [{MsgStatus, Unacked, It} | Rest] =
         lists:sort(fun ({#msg_status{seq_id = SeqId1}, _, _},
                         {#msg_status{seq_id = SeqId2}, _, _}) ->
                            SeqId1 =< SeqId2
-                   end, Its),
-    {Msg, State1} = read_msg(MsgStatus, State),
+                   end, Its0),
+    {Msg, State1} = read_msg(MsgStatus, State0),
     case Fun(Msg, MsgStatus#msg_status.msg_props, Unacked, Acc) of
         {stop, Acc1} ->
-            {Acc1, State};
+            {Acc1, State1};
         {cont, Acc1} ->
-            {Its1, IndexState1} = inext(It, {Rest, State1#vqstate.index_state}),
-            ifold(Fun, Acc1, Its1, State1#vqstate{index_state = IndexState1})
+            IndexState0 = State1#vqstate.index_state,
+            {Its1, IndexState1} = inext(It, {Rest, IndexState0}),
+            State2 = State1#vqstate{index_state = IndexState1},
+            ifold(Fun, Acc1, Its1, State2)
     end.
 
 %%----------------------------------------------------------------------------