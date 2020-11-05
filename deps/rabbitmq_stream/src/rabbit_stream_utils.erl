36fee5653022bcae347beca036b623dd109d76f2
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-11-05 15:33:13 +0100

    Move some code to utils module

--- deps/rabbitmq_stream/src/rabbit_stream_utils.erl
+++ deps/rabbitmq_stream/src/rabbit_stream_utils.erl
@@ -17,7 +17,12 @@
 -module(rabbit_stream_utils).
 
 %% API
--export([enforce_correct_stream_name/1]).
+-export([enforce_correct_stream_name/1, write_messages/3, parse_map/2,
+         auth_mechanisms/1, auth_mechanism_to_module/2,
+         check_configure_permitted/3, check_write_permitted/3, check_read_permitted/3,
+         extract_stream_list/2]).
+
+-define(MAX_PERMISSION_CACHE_SIZE, 12).
 
 enforce_correct_stream_name(Name) ->
   % from rabbit_channel
@@ -35,3 +40,86 @@ check_name(<<"">>) ->
   error;
 check_name(_Name) ->
   ok.
+
+write_messages(_ClusterLeader, _PublisherId, <<>>) ->
+  ok;
+write_messages(ClusterLeader, PublisherId, <<PublishingId:64, 0:1, MessageSize:31, Message:MessageSize/binary, Rest/binary>>) ->
+  % FIXME handle write error
+  ok = osiris:write(ClusterLeader, {PublisherId, PublishingId}, Message),
+  write_messages(ClusterLeader, PublisherId, Rest);
+write_messages(ClusterLeader, PublisherId, <<PublishingId:64, 1:1, CompressionType:3, _Unused:4, MessageCount:16, BatchSize:32, Batch:BatchSize/binary, Rest/binary>>) ->
+  % FIXME handle write error
+  ok = osiris:write(ClusterLeader, {PublisherId, PublishingId}, {batch, MessageCount, CompressionType, Batch}),
+  write_messages(ClusterLeader, PublisherId, Rest).
+
+
+parse_map(<<>>, _Count) ->
+  {#{}, <<>>};
+parse_map(Content, 0) ->
+  {#{}, Content};
+parse_map(Arguments, Count) ->
+  parse_map(#{}, Arguments, Count).
+
+parse_map(Acc, <<>>, _Count) ->
+  {Acc, <<>>};
+parse_map(Acc, Content, 0) ->
+  {Acc, Content};
+parse_map(Acc, <<KeySize:16, Key:KeySize/binary, ValueSize:16, Value:ValueSize/binary, Rest/binary>>, Count) ->
+  parse_map(maps:put(Key, Value, Acc), Rest, Count - 1).
+
+auth_mechanisms(Sock) ->
+  {ok, Configured} = application:get_env(rabbit, auth_mechanisms),
+  [rabbit_data_coercion:to_binary(Name) || {Name, Module} <- rabbit_registry:lookup_all(auth_mechanism),
+    Module:should_offer(Sock), lists:member(Name, Configured)].
+
+auth_mechanism_to_module(TypeBin, Sock) ->
+  case rabbit_registry:binary_to_type(TypeBin) of
+    {error, not_found} ->
+      rabbit_log:warning("Unknown authentication mechanism '~p'~n", [TypeBin]),
+      {error, not_found};
+    T ->
+      case {lists:member(TypeBin, rabbit_stream_utils:auth_mechanisms(Sock)),
+        rabbit_registry:lookup_module(auth_mechanism, T)} of
+        {true, {ok, Module}} ->
+          {ok, Module};
+        _ ->
+          rabbit_log:warning("Invalid authentication mechanism '~p'~n", [T]),
+          {error, invalid}
+      end
+  end.
+
+check_resource_access(User, Resource, Perm, Context) ->
+  V = {Resource, Context, Perm},
+
+  Cache = case get(permission_cache) of
+            undefined -> [];
+            Other -> Other
+          end,
+  case lists:member(V, Cache) of
+    true -> ok;
+    false ->
+      try
+        rabbit_access_control:check_resource_access(
+          User, Resource, Perm, Context),
+        CacheTail = lists:sublist(Cache, ?MAX_PERMISSION_CACHE_SIZE - 1),
+        put(permission_cache, [V | CacheTail]),
+        ok
+      catch
+        exit:_ ->
+          error
+      end
+  end.
+
+check_configure_permitted(Resource, User, Context) ->
+  check_resource_access(User, Resource, configure, Context).
+
+check_write_permitted(Resource, User, Context) ->
+  check_resource_access(User, Resource, write, Context).
+
+check_read_permitted(Resource, User, Context) ->
+  check_resource_access(User, Resource, read, Context).
+
+extract_stream_list(<<>>, Streams) ->
+  Streams;
+extract_stream_list(<<Length:16, Stream:Length/binary, Rest/binary>>, Streams) ->
+  extract_stream_list(Rest, [Stream | Streams]).
