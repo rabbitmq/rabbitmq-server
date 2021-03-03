5c829ff599f1e50292744d21935d66a395fa232e
Loïc Hoguin <lhoguin@vmware.com>
2021-03-03 11:28:54 +0100

    Add rabbitmq-diagnostics remote_shell

---
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/diagnostics/commands/remote_shell_command.ex
@@ -0,0 +1,38 @@
+## This Source Code Form is subject to the terms of the Mozilla Public
+## License, v. 2.0. If a copy of the MPL was not distributed with this
+## file, You can obtain one at https://mozilla.org/MPL/2.0/.
+##
+## Copyright (c) 2007-2021 VMware, Inc. or its affiliates.  All rights reserved.
+
+defmodule RabbitMQ.CLI.Diagnostics.Commands.RemoteShellCommand do
+  @behaviour RabbitMQ.CLI.CommandBehaviour
+  use RabbitMQ.CLI.DefaultOutput
+
+  def merge_defaults(args, opts) do
+    {args, opts}
+  end
+
+
+  use RabbitMQ.CLI.Core.AcceptsNoPositionalArguments
+
+  def run([], %{node: node_name}) do
+    _ = Supervisor.terminate_child(:kernel_sup, :user)
+    Process.flag(:trap_exit, true)
+    user_drv = :user_drv.start(['tty_sl -c -e', {node_name, :shell, :start, []}])
+    Process.link(user_drv)
+    receive do
+        {'EXIT', user_drv, _} ->
+            {:ok, "Disconnected from #{node_name}."}
+    end
+  end
+
+  def help_section(), do: :observability_and_health_checks
+
+  def description(), do: "Starts a shell on the target node"
+
+  def usage, do: "remote_shell"
+
+  def banner(_, %{node: node_name}) do
+    "Starting a shell on node #{node_name}... Press 'Ctrl+G' then 'q' to exit."
+  end
+end
