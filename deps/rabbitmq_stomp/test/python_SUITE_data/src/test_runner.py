0801b61e8f4d0e8ffa45db03d59e22b7e35bad89
Michael Klishin <michael@clojurewerkz.org>
2021-03-11 22:03:29 +0300

    STOMP: run Python tests on Python 3

--- deps/rabbitmq_stomp/test/python_SUITE_data/src/test_runner.py
+++ deps/rabbitmq_stomp/test/python_SUITE_data/src/test_runner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ## This Source Code Form is subject to the terms of the Mozilla Public
 ## License, v. 2.0. If a copy of the MPL was not distributed with this
