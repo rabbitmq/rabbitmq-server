aca638abbbefb9493a7e60db66ccd8fddff69ef3
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2021-03-23 16:56:20 +0100

    Logging: Add configuration variables to set various formats
    In addition to the existing configuration variables to configure
    logging, the following variables were added to extend the settings.
    
    log.*.formatter = plaintext | json
      Selects between the plain text (default) and JSON formatters.
    
    log.*.formatter.time_format = rfc3339_space | rfc3339_T | epoch_usecs | epoch_secs | lager_default
      Configures how the timestamp should be formatted. It has several
      values to get RFC3339 date & time, Epoch-based integers and Lager
      default format.
    
    log.*.formatter.level_format = lc | uc | lc3 | uc3 | lc4 | uc4
      Configures how to format the level. Things like uppercase vs.
      lowercase, full vs. truncated.
      Examples:
        lc: debug
        uc: DEBUG
        lc3: dbg
        uc3: DBG
        lw4: dbug
        uc4: DBUG
    
    log.*.formatter.single_line = on | off
      Indicates if multi-line messages should be reformatted as a
      single-line message. A multi-line message is converted to a
      single-line message by joining all lines and separating them
      with ", ".
    
    log.*.formatter.plaintext.format
      Set to a pattern to indicate the format of the entire message. The
      format pattern is a string with $-based variables. Each variable
      corresponds to a field in the log event. Here is a non-exhaustive list
      of common fields:
        time
        level
        msg
        pid
        file
        line
      Example:
        $time [$level] $pid $msg
    
    log.*.formatter.json.field_map
      Indicates if fields should be renamed or removed, and the ordering
      which they should appear in the final JSON object. The order is set by
      the order of fields in that coniguration variable.
      Example:
        time:ts level msg *:-
      In this example, `time` is renamed to `ts`. `*:-` tells to remove all
      fields not mentionned in the list. In the end the JSON object will
      contain the fields in the following order: ts, level, msg.
    
    log.*.formatter.json.verbosity_map
      Indicates if a verbosity field should be added and how it should be
      derived from the level. If the verbosity map is not set, no verbosity
      field is added to the JSON object.
      Example:
        debug:2 info:1 notice:1 *:0
      In this example, debug verbosity is 2, info and notice verbosity is 1,
      other levels have a verbosity of 0.
    
    All of them work with the console, exchange, file and syslog outputs.
    
    The console output has specific variables too:
    
    log.console.stdio = stdout | stderr
      Indicates if stdout or stderr should be used. The default is stdout.
    
    log.console.use_colors = on | off
      Indicates if colors should be used in log messages. The default
      depends on the environment.
    
    log.console.color_esc_seqs.*
      Indicates how each level is mapped to a color. The value can be any
      string but the idea is to use an ANSI escape sequence.
      Example:
        log.console.color_esc_seqs.error = \033[1;31m
    
    V2: A custom time format pattern was introduced, first using variables,
        then a reference date & time (e.g. "Mon 2 Jan 2006"), thanks to
        @ansd. However, we decided to remove it for now until we have a
        better implementation of the reference date & time parser.
    
    V3: The testsuite was extended to cover new settings as well as the
        syslog output. To test it, a fake syslogd server was added (Erlang
        process, part of the testsuite).
    
    V4: The dependency to cuttlefish is moved to rabbitmq_prelaunch which
        actually uses the library. The version is updated to 3.0.1 because
        we need Kyorai/cuttlefish#25.

--- deps/rabbit/apps/rabbitmq_prelaunch/src/rabbit_logger_text_fmt.erl
+++ deps/rabbit/apps/rabbitmq_prelaunch/src/rabbit_logger_text_fmt.erl
@@ -12,130 +12,50 @@
 format(#{msg := Msg, meta := Meta} = LogEvent, Config) ->
     Prefix = format_prefix(LogEvent, Config),
     Color = pick_color(LogEvent, Config),
-    FormattedMsg = format_msg(Msg, Meta, Config),
-    prepend_prefix_to_msg_and_add_color(Prefix, Color, FormattedMsg, Config).
+    FormattedMsg = rabbit_logger_fmt_helpers:format_msg(Msg, Meta, Config),
+    prepend_prefix_to_msg_and_add_color(
+      Prefix, Color, FormattedMsg, LogEvent, Config).
 
-format_prefix(_, #{prefix := false}) ->
-    none;
-format_prefix(#{level := Level,
-                meta := #{time := Timestamp,
-                          pid := Pid}},
-              Config) ->
-    Time = format_time(Timestamp, Config),
-    LevelName = level_name(Level, Config),
-    io_lib:format("~ts [~ts] ~p", [Time, LevelName, Pid]).
+format_prefix(LogEvent, #{prefix_format := PrefixFormat} = Config) ->
+    format_prefix(PrefixFormat, LogEvent, Config, []);
+format_prefix(LogEvent, Config) ->
+    %% Default prefix format.
+    format_prefix([time, " [", level, "] ", pid, " "], LogEvent, Config, []).
 
-level_name(Level, #{level_name := full}) ->
-    Level;
-level_name(Level, #{level_name := uc3}) ->
-    level_3letter_uc_name(Level);
-level_name(Level, #{level_name := lc3}) ->
-    level_3letter_lc_name(Level);
-level_name(Level, #{level_name := uc4}) ->
-    level_4letter_uc_name(Level);
-level_name(Level, #{level_name := lc4}) ->
-    level_4letter_lc_name(Level);
-level_name(Level, _) ->
-    Level.
+format_prefix([String | Rest], LogEvent, Config, Prefix)
+  when is_list(String) ->
+    format_prefix(Rest, LogEvent, Config, [String | Prefix]);
+format_prefix([Var | Rest], LogEvent, Config, Prefix)
+  when is_atom(Var) ->
+    String = format_var(Var, LogEvent, Config),
+    format_prefix(Rest, LogEvent, Config, [String | Prefix]);
+format_prefix([], _, _, Prefix) ->
+    lists:reverse(Prefix).
 
-level_3letter_lc_name(debug)     -> "dbg";
-level_3letter_lc_name(info)      -> "inf";
-level_3letter_lc_name(notice)    -> "ntc";
-level_3letter_lc_name(warning)   -> "wrn";
-level_3letter_lc_name(error)     -> "err";
-level_3letter_lc_name(critical)  -> "crt";
-level_3letter_lc_name(alert)     -> "alt";
-level_3letter_lc_name(emergency) -> "emg".
+format_var(level, #{level := Level}, Config) ->
+    rabbit_logger_fmt_helpers:format_level(Level, Config);
+format_var(time, #{meta := #{time := Timestamp}}, Config) ->
+    rabbit_logger_fmt_helpers:format_time(Timestamp, Config);
+format_var(Var, #{meta := Meta}, _) ->
+    case maps:get(Var, Meta, undefined) of
+        undefined ->
+            io_lib:format("<unknown ~s>", [Var]);
+        Value ->
+            case io_lib:char_list(Value) of
+                true  -> io_lib:format("~s", [Value]);
+                false -> io_lib:format("~p", [Value])
+            end
+    end.
 
-level_3letter_uc_name(debug)     -> "DBG";
-level_3letter_uc_name(info)      -> "INF";
-level_3letter_uc_name(notice)    -> "NTC";
-level_3letter_uc_name(warning)   -> "WRN";
-level_3letter_uc_name(error)     -> "ERR";
-level_3letter_uc_name(critical)  -> "CRT";
-level_3letter_uc_name(alert)     -> "ALT";
-level_3letter_uc_name(emergency) -> "EMG".
-
-level_4letter_lc_name(debug)     -> "dbug";
-level_4letter_lc_name(info)      -> "info";
-level_4letter_lc_name(notice)    -> "noti";
-level_4letter_lc_name(warning)   -> "warn";
-level_4letter_lc_name(error)     -> "erro";
-level_4letter_lc_name(critical)  -> "crit";
-level_4letter_lc_name(alert)     -> "alrt";
-level_4letter_lc_name(emergency) -> "emgc".
-
-level_4letter_uc_name(debug)     -> "DBUG";
-level_4letter_uc_name(info)      -> "INFO";
-level_4letter_uc_name(notice)    -> "NOTI";
-level_4letter_uc_name(warning)   -> "WARN";
-level_4letter_uc_name(error)     -> "ERRO";
-level_4letter_uc_name(critical)  -> "CRIT";
-level_4letter_uc_name(alert)     -> "ALRT";
-level_4letter_uc_name(emergency) -> "EMGC".
-
-format_time(Timestamp, _) ->
-    Options = [{unit, microsecond},
-               {time_designator, $\s}],
-    calendar:system_time_to_rfc3339(Timestamp, Options).
-
-format_msg({string, Chardata}, Meta, Config) ->
-    format_msg({"~ts", [Chardata]}, Meta, Config);
-format_msg({report, Report}, Meta, Config) ->
-    FormattedReport = format_report(Report, Meta, Config),
-    format_msg(FormattedReport, Meta, Config);
-format_msg({Format, Args}, _, _) ->
-    io_lib:format(Format, Args).
-
-format_report(
-  #{label := {application_controller, _}} = Report, Meta, Config) ->
-    format_application_progress(Report, Meta, Config);
-format_report(
-  #{label := {supervisor, progress}} = Report, Meta, Config) ->
-    format_supervisor_progress(Report, Meta, Config);
-format_report(
-  Report, #{report_cb := Cb} = Meta, Config) ->
-    try
-        case erlang:fun_info(Cb, arity) of
-            {arity, 1} -> Cb(Report);
-            {arity, 2} -> {"~ts", [Cb(Report, #{})]}
-        end
-    catch
-        _:_:_ ->
-            format_report(Report, maps:remove(report_cb, Meta), Config)
-    end;
-format_report(Report, _, _) ->
-    logger:format_report(Report).
-
-format_application_progress(#{label := {_, progress},
-                              report := InternalReport}, _, _) ->
-    Application = proplists:get_value(application, InternalReport),
-    StartedAt = proplists:get_value(started_at, InternalReport),
-    {"Application ~w started on ~0p",
-     [Application, StartedAt]};
-format_application_progress(#{label := {_, exit},
-                              report := InternalReport}, _, _) ->
-    Application = proplists:get_value(application, InternalReport),
-    Exited = proplists:get_value(exited, InternalReport),
-    {"Application ~w exited with reason: ~0p",
-     [Application, Exited]}.
-
-format_supervisor_progress(#{report := InternalReport}, _, _) ->
-    Supervisor = proplists:get_value(supervisor, InternalReport),
-    Started = proplists:get_value(started, InternalReport),
-    Id = proplists:get_value(id, Started),
-    Pid = proplists:get_value(pid, Started),
-    Mfa = proplists:get_value(mfargs, Started),
-    {"Supervisor ~w: child ~w started (~w): ~0p",
-     [Supervisor, Id, Pid, Mfa]}.
-
-pick_color(_, #{color := false}) ->
-    {"", ""};
-pick_color(#{level := Level}, #{color := true} = Config) ->
+pick_color(#{level := Level}, #{use_colors := true} = Config) ->
     ColorStart = level_to_color(Level, Config),
     ColorEnd = "\033[0m",
-    {ColorStart, ColorEnd}.
+    {ColorStart, ColorEnd};
+pick_color(_, _) ->
+    {"", ""}.
 
+level_to_color(Level, #{color_esc_seqs := ColorEscSeqs}) ->
+    maps:get(Level, ColorEscSeqs);
 level_to_color(debug, _)     -> "\033[38;5;246m";
 level_to_color(info, _)      -> "";
 level_to_color(notice, _)    -> "\033[38;5;87m";
@@ -146,22 +66,35 @@ level_to_color(alert, _)     -> "\033[1;37m\033[48;5;93m";
 level_to_color(emergency, _) -> "\033[1;37m\033[48;5;196m".
 
 prepend_prefix_to_msg_and_add_color(
-  none, {ColorStart, ColorEnd}, FormattedMsg, Config) ->
+  Prefix, {ColorStart, ColorEnd}, FormattedMsg, LogEvent, Config) ->
     Lines = split_lines(FormattedMsg, Config),
-    [case Line of
-         "" -> [$\n];
-         _  -> [ColorStart, Line, ColorEnd, $\n]
-     end
-     || Line <- Lines];
-prepend_prefix_to_msg_and_add_color(
-  Prefix, {ColorStart, ColorEnd}, FormattedMsg, Config) ->
-    Lines = split_lines(FormattedMsg, Config),
-    [case Line of
-         "" -> [ColorStart, Prefix, ColorEnd, $\n];
-         _  -> [ColorStart, Prefix, " ", Line, ColorEnd, $\n]
-     end
+    [[ColorStart,
+      format_line(Prefix, Line, LogEvent, Config),
+      ColorEnd,
+      $\n]
      || Line <- Lines].
 
 split_lines(FormattedMsg, _) ->
     FlattenMsg = lists:flatten(FormattedMsg),
     string:split(FlattenMsg, [$\n], all).
+
+format_line(Prefix, Msg, LogEvent, #{line_format := Format} = Config) ->
+    format_line(Format, Msg, LogEvent, Config, [Prefix]);
+format_line(Prefix, Msg, LogEvent, Config) ->
+    format_line([msg], Msg, LogEvent, Config, [Prefix]).
+
+format_line([msg | Rest], Msg, LogEvent, Config, Line) ->
+    format_line(Rest, Msg, LogEvent, Config, [Msg | Line]);
+format_line([String | Rest], Msg, LogEvent, Config, Line)
+  when is_list(String) ->
+    format_line(Rest, Msg, LogEvent, Config, [String | Line]);
+format_line([Var | Rest], Msg, LogEvent, Config, Line)
+  when is_atom(Var) ->
+    String = format_var(Var, LogEvent, Config),
+    format_line(Rest, Msg, LogEvent, Config, [String | Line]);
+format_line([], _, _, _, Line) ->
+    remove_trailing_whitespaces(Line).
+
+remove_trailing_whitespaces([Tail | Line]) ->
+    Tail1 = string:strip(Tail, right),
+    lists:reverse([Tail1 | Line]).
