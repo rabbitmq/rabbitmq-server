3eee69abf4582991c227a771ef59f98255de8dda
Michael Klishin <michael@clojurewerkz.org>
2021-04-13 14:48:38 +0300

    binary_to_atom/1 is not available in Erlang 22.3
    so use rabbit_data_coercion:to_atom/1.
    
    References 2968

--- deps/rabbit/src/rabbit_quorum_queue.erl
+++ deps/rabbit/src/rabbit_quorum_queue.erl
@@ -173,7 +173,7 @@ start_cluster(Q) ->
                  {ok, A} ->
                      A;
                  {error, {too_long, N}} ->
-                     binary_to_atom(ra:new_uid(N))
+                     rabbit_data_coercion:to_atom(ra:new_uid(N))
              end,
     Id = {RaName, node()},
     Nodes = select_quorum_nodes(QuorumSize, rabbit_mnesia:cluster_nodes(all)),
