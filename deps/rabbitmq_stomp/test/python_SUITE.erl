e4aceb53a0b8f5420edea76189f8bb5b810ecb35
Philip Kuryloski <kuryloskip@vmware.com>
2021-04-14 12:44:16 +0200

    Add rabbitmq_stomp to bazel

--- deps/rabbitmq_stomp/test/python_SUITE.erl
+++ deps/rabbitmq_stomp/test/python_SUITE.erl
@@ -75,12 +75,13 @@ run(Config, Test) ->
     StompPortTls = rabbit_ct_broker_helpers:get_node_config(Config, 0, tcp_port_stomp_tls),
     AmqpPort = rabbit_ct_broker_helpers:get_node_config(Config, 0, tcp_port_amqp),
     NodeName = rabbit_ct_broker_helpers:get_node_config(Config, 0, nodename),
-    PythonPath = os:getenv("PYTHONPATH"),
-    os:putenv("PYTHONPATH", filename:join([DataDir, "deps", "pika","pika"])
-                            ++":"++
-                            filename:join([DataDir, "deps", "stomppy", "stomppy"])
-                            ++ ":" ++
-                            PythonPath),
+    PikaPath = filename:join([DataDir, "deps", "pika","pika"]),
+    StomppyPath = filename:join([DataDir, "deps", "stomppy", "stomppy"]),
+    PythonPath = case os:getenv("PYTHONPATH") of
+        false -> PikaPath ++ ":" ++ StomppyPath;
+        P -> PikaPath ++ ":" ++ StomppyPath ++ ":" ++ P
+    end,
+    os:putenv("PYTHONPATH", PythonPath),
     os:putenv("AMQP_PORT", integer_to_list(AmqpPort)),
     os:putenv("STOMP_PORT", integer_to_list(StompPort)),
     os:putenv("STOMP_PORT_TLS", integer_to_list(StompPortTls)),
