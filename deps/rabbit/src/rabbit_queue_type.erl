acddc0ea9d4595cede41f2835b1ac0a41980a6e6
dcorbacho <dparracorbacho@piotal.io>
2021-04-29 16:49:10 +0200

    Catch deliver errors in rabbit_queue_type

--- deps/rabbit/src/rabbit_queue_type.erl
+++ deps/rabbit/src/rabbit_queue_type.erl
@@ -439,14 +439,22 @@ module(QRef, Ctxs) ->
 
 -spec deliver([amqqueue:amqqueue()], Delivery :: term(),
               stateless | state()) ->
-    {ok, state(), actions()}.
-deliver(Qs, Delivery, stateless) ->
+    {ok, state(), actions()} | {error, Reason :: term()}.
+deliver(Qs, Delivery, State) ->
+    try
+        deliver0(Qs, Delivery, State)
+    catch
+        exit:Reason ->
+            {error, Reason}
+    end.
+
+deliver0(Qs, Delivery, stateless) ->
     _ = lists:map(fun(Q) ->
                           Mod = amqqueue:get_type(Q),
                           _ = Mod:deliver([{Q, stateless}], Delivery)
                   end, Qs),
     {ok, stateless, []};
-deliver(Qs, Delivery, #?STATE{} = State0) ->
+deliver0(Qs, Delivery, #?STATE{} = State0) ->
     %% TODO: optimise single queue case?
     %% sort by queue type - then dispatch each group
     ByType = lists:foldl(
