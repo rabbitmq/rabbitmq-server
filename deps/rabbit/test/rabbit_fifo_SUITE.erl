1c4f563db6920f4b6b153184fa18e43b22c8e1ab
Karl Nilsson <kjnilsson@gmail.com>
2021-06-21 13:17:48 +0100

    QQ memory use improvement
    The rabbit_fifo_index is now only used for messages that have moved from
    the messages queue to consumers/return queue. This reduces memory used
    per queued message.

--- deps/rabbit/test/rabbit_fifo_SUITE.erl
+++ deps/rabbit/test/rabbit_fifo_SUITE.erl
@@ -2,6 +2,7 @@
 
 %% rabbit_fifo unit tests suite
 
+-compile(nowarn_export_all).
 -compile(export_all).
 
 -compile({no_auto_import, [apply/3]}).
@@ -88,6 +89,7 @@ enq_enq_checkout_test(_) ->
     Cid = {<<"enq_enq_checkout_test">>, self()},
     {State1, _} = enq(1, 1, first, test_init(test)),
     {State2, _} = enq(2, 2, second, State1),
+    ?assertEqual(2, rabbit_fifo:query_messages_total(State2)),
     {_State3, _, Effects} =
         apply(meta(3),
               rabbit_fifo:make_checkout(Cid, {once, 2, simple_prefetch}, #{}),
@@ -201,9 +203,10 @@ enq_enq_checkout_get_settled_test(_) ->
     Cid = {?FUNCTION_NAME, self()},
     {State1, _} = enq(1, 1, first, test_init(test)),
     % get returns a reply value
-    {_State2, {dequeue, {0, {_, first}}, _}, _Effs} =
+    {State2, {dequeue, {0, {_, first}}, _}, _Effs} =
         apply(meta(3), rabbit_fifo:make_checkout(Cid, {dequeue, settled}, #{}),
               State1),
+    ?assertEqual(0, rabbit_fifo:query_messages_total(State2)),
     ok.
 
 checkout_get_empty_test(_) ->
@@ -240,6 +243,7 @@ checkout_enq_settle_test(_) ->
     {State1, [{mod_call, rabbit_quorum_queue, spawn_notify_decorators, _},
               {monitor, _, _} | _]} = check(Cid, 1, test_init(test)),
     {State2, Effects0} = enq(2, 1,  first, State1),
+    ct:pal("Effects0 ~p", [Effects0]),
     ?ASSERT_EFF({send_msg, _,
                  {delivery, ?FUNCTION_NAME,
                   [{0, {_, first}}]}, _},
@@ -362,9 +366,9 @@ return_checked_out_limit_test(_) ->
     {State2, ok, [{send_msg, _, {delivery, _, [{MsgId2, _}]}, _},
                   {aux, active}]} =
         apply(meta(3), rabbit_fifo:make_return(Cid, [MsgId]), State1),
-    {#rabbit_fifo{ra_indexes = RaIdxs}, ok, [_ReleaseEff]} =
+    {#rabbit_fifo{} = State, ok, [_ReleaseEff]} =
         apply(meta(4), rabbit_fifo:make_return(Cid, [MsgId2]), State2),
-    ?assertEqual(0, rabbit_fifo_index:size(RaIdxs)),
+    ?assertEqual(0, rabbit_fifo:query_messages_total(State)),
     ok.
 
 return_auto_checked_out_test(_) ->
@@ -583,9 +587,8 @@ pending_enqueue_is_enqueued_on_down_test(_) ->
 
 duplicate_delivery_test(_) ->
     {State0, _} = enq(1, 1, first, test_init(test)),
-    {#rabbit_fifo{ra_indexes = RaIdxs,
-            messages = Messages}, _} = enq(2, 1, first, State0),
-    ?assertEqual(1, rabbit_fifo_index:size(RaIdxs)),
+    {#rabbit_fifo{messages = Messages} = State, _} = enq(2, 1, first, State0),
+    ?assertEqual(1, rabbit_fifo:query_messages_total(State)),
     ?assertEqual(1, lqueue:len(Messages)),
     ok.
 
@@ -658,7 +661,7 @@ purge_with_checkout_test(_) ->
     {State3, {purge, 1}, _} = apply(meta(2), rabbit_fifo:make_purge(), State2),
     ?assert(State2#rabbit_fifo.msg_bytes_checkout > 0),
     ?assertEqual(0, State3#rabbit_fifo.msg_bytes_enqueue),
-    ?assertEqual(1, rabbit_fifo_index:size(State3#rabbit_fifo.ra_indexes)),
+    ?assertEqual(1, rabbit_fifo:query_messages_total(State3)),
     #consumer{checked_out = Checked} = maps:get(Cid, State3#rabbit_fifo.consumers),
     ?assertEqual(1, maps:size(Checked)),
     ok.
@@ -1697,10 +1700,10 @@ query_peek_test(_) ->
     ?assertEqual({error, no_message_at_pos}, rabbit_fifo:query_peek(1, State0)),
     {State1, _} = enq(1, 1, first, State0),
     {State2, _} = enq(2, 2, second, State1),
-    ?assertMatch({ok, {_, {_, first}}}, rabbit_fifo:query_peek(1, State1)),
+    ?assertMatch({ok, [_, _ | first]}, rabbit_fifo:query_peek(1, State1)),
     ?assertEqual({error, no_message_at_pos}, rabbit_fifo:query_peek(2, State1)),
-    ?assertMatch({ok, {_, {_, first}}}, rabbit_fifo:query_peek(1, State2)),
-    ?assertMatch({ok, {_, {_, second}}}, rabbit_fifo:query_peek(2, State2)),
+    ?assertMatch({ok, [_, _ | first]}, rabbit_fifo:query_peek(1, State2)),
+    ?assertMatch({ok, [_, _ | second]}, rabbit_fifo:query_peek(2, State2)),
     ?assertEqual({error, no_message_at_pos}, rabbit_fifo:query_peek(3, State2)),
     ok.
 
