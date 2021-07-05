dbb09f1bf5fbdfb03348e2c3a71f58010ef261b5
Philip Kuryloski <kuryloskip@vmware.com>
2021-07-05 15:21:27 +0200

    Add ?awaitMatch/4 to rabbit_assert.hrl
    ?awaitMatch/4 is the same as ?awaitMatch/3, but where the polling
    interval is configurable

--- deps/rabbitmq_ct_helpers/include/rabbit_assert.hrl
+++ deps/rabbitmq_ct_helpers/include/rabbit_assert.hrl
@@ -1,3 +1,29 @@
+-define(AWAIT_MATCH_DEFAULT_POLLING_INTERVAL, 50).
+
+-define(awaitMatch(Guard, Expr, Timeout, PollingInterval),
+        begin
+            ((fun AwaitMatchFilter(AwaitMatchHorizon) ->
+                      AwaitMatchResult = Expr,
+                      case (AwaitMatchResult) of
+                          Guard -> AwaitMatchResult;
+                          __V -> case erlang:system_time(millisecond) of
+                                     AwaitMatchNow when AwaitMatchNow < AwaitMatchHorizon ->
+                                         timer:sleep(
+                                           min(PollingInterval,
+                                               AwaitMatchHorizon - AwaitMatchNow)),
+                                         AwaitMatchFilter(AwaitMatchHorizon);
+                                     _ ->
+                                         erlang:error({awaitMatch,
+                                                       [{module, ?MODULE},
+                                                        {line, ?LINE},
+                                                        {expression, (??Expr)},
+                                                        {pattern, (??Guard)},
+                                                        {value, __V}]})
+                                 end
+                      end
+              end)(erlang:system_time(millisecond) + Timeout))
+        end).
+
 -define(awaitMatch(Guard, Expr, Timeout),
         begin
             ((fun AwaitMatchFilter(AwaitMatchHorizon) ->
@@ -6,7 +32,9 @@
                           Guard -> AwaitMatchResult;
                           __V -> case erlang:system_time(millisecond) of
                                      AwaitMatchNow when AwaitMatchNow < AwaitMatchHorizon ->
-                                         timer:sleep(min(50, AwaitMatchHorizon - AwaitMatchNow)),
+                                         timer:sleep(
+                                           min(?AWAIT_MATCH_DEFAULT_POLLING_INTERVAL,
+                                               AwaitMatchHorizon - AwaitMatchNow)),
                                          AwaitMatchFilter(AwaitMatchHorizon);
                                      _ ->
                                          erlang:error({awaitMatch,
