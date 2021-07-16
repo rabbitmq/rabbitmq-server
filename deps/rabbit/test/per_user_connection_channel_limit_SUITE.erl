0a78484999456771a4591251a7b7aae19ec9e2d6
Philip Kuryloski <kuryloskip@vmware.com>
2021-07-16 14:40:51 +0200

    Make things a little more consistent between per_*_limit suites

--- deps/rabbit/test/per_user_connection_channel_limit_SUITE.erl
+++ deps/rabbit/test/per_user_connection_channel_limit_SUITE.erl
@@ -474,10 +474,10 @@ most_basic_cluster_connection_and_channel_count(Config) ->
     ?assertEqual(15, count_channels_of_user(Config, Username)),
 
     close_channels(Chans1 ++ Chans2 ++ Chans3),
-    ?awaitMatch(0, count_channels_of_user(Config, Username), 60000),
+    ?awaitMatch(0, count_channels_of_user(Config, Username), 60000, 3000),
 
     close_connections([Conn1, Conn2, Conn3]),
-    ?awaitMatch(0, count_connections_of_user(Config, Username), 60000).
+    ?awaitMatch(0, count_connections_of_user(Config, Username), 60000, 3000).
 
 cluster_single_user_connection_and_channel_count(Config) ->
     Username = proplists:get_value(rmq_username, Config),
@@ -777,7 +777,6 @@ cluster_node_list_on_node(Config) ->
         end),
 
     rabbit_ct_broker_helpers:stop_broker(Config, 1),
-    await_running_node_refresh(Config, 0),
 
     rabbit_ct_helpers:await_condition(
         fun () ->
@@ -837,7 +836,7 @@ single_node_single_user_limit_with(Config, ConnLimit, ChLimit) ->
         end),
 
     close_connections([Conn1, Conn2, Conn3, Conn4, Conn5]),
-    ?awaitMatch(0, count_connections_of_user(Config, Username), 60000),
+    ?awaitMatch(0, count_connections_of_user(Config, Username), 60000, 3000),
 
     set_user_connection_and_channel_limit(Config, Username,  -1, -1).
 
@@ -884,7 +883,7 @@ single_node_single_user_zero_limit(Config) ->
         end),
 
     close_connections([Conn1, Conn2]),
-    ?awaitMatch(0, count_connections_of_user(Config, Username), 60000).
+    ?awaitMatch(0, count_connections_of_user(Config, Username), 60000, 3000).
 
 single_node_single_user_clear_limits(Config) ->
     Username = proplists:get_value(rmq_username, Config),
@@ -933,7 +932,7 @@ single_node_single_user_clear_limits(Config) ->
         end),
 
     close_connections([Conn2, Conn3, Conn4, Conn5, Conn6, Conn7]),
-    ?awaitMatch(0, count_connections_of_user(Config, Username), 5000),
+    ?awaitMatch(0, count_connections_of_user(Config, Username), 5000, 1000),
 
     set_user_connection_and_channel_limit(Config, Username,  -1, -1).
 
@@ -1194,10 +1193,10 @@ cluster_single_user_limit(Config) ->
     [Chans3, Chans4] = [open_channels(Conn, 5) || Conn <- Conns2],
 
     close_channels(Chans2 ++ Chans3 ++ Chans4),
-    ?awaitMatch(0, count_channels_of_user(Config, Username), 60000),
+    ?awaitMatch(0, count_channels_of_user(Config, Username), 60000, 3000),
 
     close_connections([Conn2, Conn3, Conn4]),
-    ?awaitMatch(0, count_connections_of_user(Config, Username), 60000),
+    ?awaitMatch(0, count_connections_of_user(Config, Username), 60000, 3000),
 
     set_user_connection_and_channel_limit(Config, Username,  -1, -1).
 
@@ -1241,7 +1240,7 @@ cluster_single_user_limit2(Config) ->
         end),
 
     close_connections([Conn2, Conn3, Conn4, Conn5, Conn6]),
-    ?awaitMatch(0, count_connections_of_user(Config, Username), 5000),
+    ?awaitMatch(0, count_connections_of_user(Config, Username), 5000, 1000),
 
     set_user_connection_and_channel_limit(Config, Username,  -1, -1).
 
@@ -1493,12 +1492,12 @@ cluster_multiple_users_zero_limit(Config) ->
     [Chans1, Chans2, Chans3, Chans4] = [open_channels(Conn, 5) || Conn <- Conns1],
 
     close_channels(Chans1 ++ Chans2 ++ Chans3 ++ Chans4),
-    ?awaitMatch(0, count_channels_of_user(Config, Username1), 60000),
-    ?awaitMatch(0, count_channels_of_user(Config, Username2), 60000),
+    ?awaitMatch(0, count_channels_of_user(Config, Username1), 60000, 3000),
+    ?awaitMatch(0, count_channels_of_user(Config, Username2), 60000, 3000),
 
     close_connections([Conn1, Conn2, Conn3, Conn4]),
-    ?awaitMatch(0, count_connections_of_user(Config, Username1), 60000),
-    ?awaitMatch(0, count_connections_of_user(Config, Username2), 60000),
+    ?awaitMatch(0, count_connections_of_user(Config, Username1), 60000, 3000),
+    ?awaitMatch(0, count_connections_of_user(Config, Username2), 60000, 3000),
 
     set_user_connection_and_channel_limit(Config, Username1, -1, -1),
     set_user_connection_and_channel_limit(Config, Username2, -1, -1).
@@ -1650,9 +1649,6 @@ clear_all_user_limits(Config, NodeIndex, Username) ->
               Config, NodeIndex, nodename),
     ok = rabbit_ct_broker_helpers:control_action(
         clear_user_limits, Node, [rabbit_data_coercion:to_list(Username), "all"]).
-
-await_running_node_refresh(_Config, _NodeIndex) ->
-    timer:sleep(250).
 
 expect_that_client_connection_is_rejected(Config) ->
     expect_that_client_connection_is_rejected(Config, 0).
