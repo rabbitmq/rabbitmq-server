22add3c8caced8ba3c8871ac2779d8ec3a3f047e
Michael Klishin <michael@clojurewerkz.org>
2021-08-05 00:37:17 +0300

    Acquire locks when starting mirrored supervisor children
    Unlike pg2, pg in Erlang 24 is eventually consistent. So this
    reintroduces some of the same kind of locking mirrored_supervisor
    used to rely on implicitly via pg2.
    
    Per discussion with @lhoguin.
    
    Closes #3260.
    
    References #3132, #3154.

---
+++ deps/rabbit_common/src/mirrored_supervisor_locks.erl
@@ -0,0 +1,33 @@
+ %% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2021 VMware, Inc. or its affiliates.  All rights reserved.
+%%
+
+-module(mirrored_supervisor_locks).
+
+-export([lock/1, unlock/1]).
+
+ -define(KEY_PREFIX, mirrored_supervisor).
+
+%%
+%% API
+%%
+
+lock(Group) ->
+    Nodes   = nodes(),
+    %% about 300s, same as rabbit_nodes:lock_retries/0 default
+    LockId  = case global:set_lock({?KEY_PREFIX, Group}, Nodes, 80) of
+        true  -> Group;
+        false -> undefined
+    end,
+    LockId.
+
+unlock(LockId) ->
+    Nodes = nodes(),
+    case LockId of
+        undefined -> ok;
+        Value     -> global:del_lock({?KEY_PREFIX, Value}, Nodes)
+    end,
+    ok.
