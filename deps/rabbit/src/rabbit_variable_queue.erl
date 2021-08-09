36ad3a5b3db6aa2cb4ecd0500f653001c9adda9e
Michael Klishin <michael@clojurewerkz.org>
2021-08-10 01:33:32 +0300

    Revert "CQ: don't deliver right before acking in the index"
    This reverts commit 3ef858746ceba1e27167264351d8450f5fc780b4.
    
    This change does not handle expired messages safely.
    
    See https://github.com/rabbitmq/rabbitmq-server/discussions/3272.

--- deps/rabbit/src/rabbit_variable_queue.erl
+++ deps/rabbit/src/rabbit_variable_queue.erl
@@ -1774,21 +1774,22 @@ purge_betas_and_deltas(DelsAndAcksFun, State = #vqstate { mode = Mode }) ->
 
 remove_queue_entries(Q, DelsAndAcksFun,
                      State = #vqstate{msg_store_clients = MSCState}) ->
-    {MsgIdsByStore, Acks, State1} =
+    {MsgIdsByStore, Delivers, Acks, State1} =
         ?QUEUE:foldl(fun remove_queue_entries1/2,
-                     {maps:new(), [], State}, Q),
+                     {maps:new(), [], [], State}, Q),
     remove_msgs_by_id(MsgIdsByStore, MSCState),
-    DelsAndAcksFun([], Acks, State1).
+    DelsAndAcksFun(Delivers, Acks, State1).
 
 remove_queue_entries1(
-  #msg_status { msg_id = MsgId, seq_id = SeqId,
+  #msg_status { msg_id = MsgId, seq_id = SeqId, is_delivered = IsDelivered,
                 msg_in_store = MsgInStore, index_on_disk = IndexOnDisk,
                 is_persistent = IsPersistent} = MsgStatus,
-  {MsgIdsByStore, Acks, State}) ->
+  {MsgIdsByStore, Delivers, Acks, State}) ->
     {case MsgInStore of
          true  -> rabbit_misc:maps_cons(IsPersistent, MsgId, MsgIdsByStore);
          false -> MsgIdsByStore
      end,
+     cons_if(IndexOnDisk andalso not IsDelivered, SeqId, Delivers),
      cons_if(IndexOnDisk, SeqId, Acks),
      stats({-1, 0}, {MsgStatus, none}, 0, State)}.
 
