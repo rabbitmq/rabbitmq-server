52a04198e8bae01b770899946a978cc2048c939d
Michal Kuratczyk <mkuratczyk@vmware.com>
2021-09-10 00:29:28 +0200

    Faster implementation of min-masters
    min-masters location strategy iterates over all the existing queues to
    find the perfect place for a new queue. Unfortunately, that means it
    gets slower and slower as the number of queues grows. While an
    individual queue creation is still relatively quick, importing
    definitions or restarting a cluster can be very slow.
    
    This is an alternative implementation that relies on the size of the queue_metrics
    ETS table. It's a bit less accurate as the metrics may show up a moment
    after the queue was declared and therfore, when creating a small number
    of queues, they may be distributed unevenly but that's probably a minor
    problem, compared to a node startup of 30-60 minutes when there are many
    queues.
    
    Accuracy may be addressed when we have a seshat metric, which should,
    I assume, always provide the exact number of queues on a given node (this
    will likely also improve performance further, since reading a single
    value should be faster than counting ETS table rows).

--- deps/rabbit/src/rabbit_queue_location_min_masters.erl
+++ deps/rabbit/src/rabbit_queue_location_min_masters.erl
@@ -6,65 +6,60 @@
 %%
 
 -module(rabbit_queue_location_min_masters).
+
 -behaviour(rabbit_queue_master_locator).
 
--include_lib("rabbit_common/include/rabbit.hrl").
 -include("amqqueue.hrl").
 
 -export([description/0, queue_master_location/1]).
 
--rabbit_boot_step({?MODULE,
-                   [{description, "locate queue master min bound queues"},
-                    {mfa,         {rabbit_registry, register,
-                                   [queue_master_locator,
-                                    <<"min-masters">>, ?MODULE]}},
-                    {requires,    rabbit_registry},
-                    {enables,     kernel_ready}]}).
+-rabbit_boot_step(
+    {?MODULE, [
+        {description, "locate queue master min bound queues"},
+        {mfa,
+            {rabbit_registry, register, [
+                queue_master_locator,
+                <<109, 105, 110, 45, 109, 97, 115, 116, 101, 114, 115>>,
+                ?MODULE
+            ]}},
+        {requires, rabbit_registry},
+        {enables, kernel_ready}
+    ]}
+).
 
 %%---------------------------------------------------------------------------
 %% Queue Master Location Callbacks
 %%---------------------------------------------------------------------------
 
 description() ->
-    [{description,
-      <<"Locate queue master node from cluster node with least bound queues">>}].
+    [
+        {description, <<
+            "Locate queue master node from cluster node with least bound "
+            "queues"
+        >>}
+    ].
 
 queue_master_location(Q) when ?is_amqqueue(Q) ->
-    Cluster = rabbit_queue_master_location_misc:all_nodes(Q),
-    QueueNames = rabbit_amqqueue:list_names(),
-    MastersPerNode0 = lists:foldl(
-        fun(#resource{virtual_host = VHost, name = QueueName}, NodeMasters) ->
-            case rabbit_queue_master_location_misc:lookup_master(QueueName, VHost) of
-                {ok, Master} when is_atom(Master) ->
-                    case maps:is_key(Master, NodeMasters) of
-                        true -> maps:update_with(Master,
-                                                 fun(N) -> N + 1 end,
-                                                 NodeMasters);
-                        false -> NodeMasters
-                    end;
-                _ -> NodeMasters
-            end
-        end,
-        maps:from_list([{N, 0} || N <- Cluster]),
-        QueueNames),
-    
-    MastersPerNode = maps:filter(fun (Node, _N) ->
-                                    not rabbit_maintenance:is_being_drained_local_read(Node)
-                                 end, MastersPerNode0),
-
-    case map_size(MastersPerNode) > 0 of
-        true ->
-            {MinNode, _NMasters} = maps:fold(
-                fun(Node, NMasters, init) ->
-                    {Node, NMasters};
-                (Node, NMasters, {MinNode, MinMasters}) ->
-                    case NMasters < MinMasters of
-                        true  -> {Node, NMasters};
-                        false -> {MinNode, MinMasters}
-                    end
-                end,
-                init, MastersPerNode),
-            {ok, MinNode};
-        false ->
-            undefined
+    Nodes0 = rabbit_nodes:all_running(),
+    Nodes = rabbit_maintenance:filter_out_drained_nodes_local_read(Nodes0),
+    case Nodes of
+        [] ->
+            {ok, node()};
+        _ ->
+            MastersPerNode0 = erpc:multicall(Nodes, ets, info, [queue_metrics, size], 10),
+            %% use {node(), infinity} whenever we don't get an answer;
+            %% this way if everything goes wrong, we select node()
+            MastersPerNode =
+                lists:zipwith(
+                    fun(N, Qs) ->
+                        case Qs of
+                            {ok, V} -> {N, V};
+                            _ -> {node(), infinity}
+                        end
+                    end,
+                    Nodes,
+                    MastersPerNode0
+                ),
+            {MinNode, _} = hd(lists:keysort(2, MastersPerNode)),
+            {ok, MinNode}
     end.
