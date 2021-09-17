4f59392dafe8bb74b97264389021ab1755c475a1
Philip Kuryloski <kuryloskip@vmware.com>
2021-09-17 11:08:48 +0200

    Split the feature_flags_SUITE into two parts for CI/Bazel
    Two testcases in the original suite fail if the test is run as the
    root user. Currently under remote execution with bazel this is the
    only working option. There is a workaround in place, but the entire
    suite when run that way takes around 12 minutes. This splits the suite
    so that the minimal set of cases is executed using the slower workaround.
    
    (cherry picked from commit eea99e1cd51debc8c6a3a59686087d1f1b2454ae)
    (cherry picked from commit acc57d12dbce799d587b7d09902b12a4050ed394)

---
+++ deps/rabbit/test/feature_flags_with_unpriveleged_user_SUITE.erl
@@ -0,0 +1,74 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2018-2021 VMware, Inc. or its affiliates.  All rights reserved.
+%%
+
+-module(feature_flags_with_unpriveleged_user_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-export([suite/0,
+         all/0,
+         groups/0,
+         init_per_suite/1,
+         end_per_suite/1,
+         init_per_group/2,
+         end_per_group/2,
+         init_per_testcase/2,
+         end_per_testcase/2,
+
+         enable_feature_flag_when_ff_file_is_unwritable/1
+        ]).
+
+suite() ->
+    [{timetrap, {minutes, 15}}].
+
+all() ->
+    [
+     {group, enabling_on_single_node},
+     {group, enabling_in_cluster}
+    ].
+
+groups() ->
+    [
+     {enabling_on_single_node, [],
+      [
+       enable_feature_flag_when_ff_file_is_unwritable
+      ]},
+     {enabling_in_cluster, [],
+      [
+       enable_feature_flag_when_ff_file_is_unwritable
+      ]}
+    ].
+
+%% This suite exists to allow running a portion of the feature_flags_SUITE
+%% under separate conditions in ci
+
+init_per_suite(Config) ->
+    feature_flags_SUITE:init_per_suite(Config).
+
+end_per_suite(Config) ->
+    feature_flags_SUITE:end_per_suite(Config).
+
+
+init_per_group(Group, Config) ->
+    feature_flags_SUITE:init_per_group(Group, Config).
+
+end_per_group(Group, Config) ->
+    feature_flags_SUITE:end_per_group(Group, Config).
+
+init_per_testcase(Testcase, Config) ->
+    feature_flags_SUITE:init_per_testcase(Testcase, Config).
+
+end_per_testcase(Testcase, Config) ->
+    feature_flags_SUITE:end_per_testcase(Testcase, Config).
+
+%% -------------------------------------------------------------------
+%% Testcases.
+%% -------------------------------------------------------------------
+
+enable_feature_flag_when_ff_file_is_unwritable(Config) ->
+    feature_flags_SUITE:enable_feature_flag_when_ff_file_is_unwritable(Config).
