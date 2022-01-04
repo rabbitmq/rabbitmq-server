d1fea82c80b007969544f0890fa96aada8465e96
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2022-01-04 16:16:56 +0100

    Use helpers in stream SAC coordinator test
    References #3753

--- deps/rabbit/test/rabbit_stream_sac_coordinator_SUITE.erl
+++ deps/rabbit/test/rabbit_stream_sac_coordinator_SUITE.erl
@@ -25,6 +25,8 @@
 %%% Common Test callbacks
 %%%===================================================================
 
+-define(STATE, rabbit_stream_sac_coordinator).
+
 all() ->
     [{group, tests}].
 
@@ -58,89 +60,64 @@ end_per_testcase(_TestCase, _Config) ->
 ensure_monitors_test(_) ->
     GroupId = {<<"/">>, <<"stream">>, <<"app">>},
     Group =
-        #group{partition_index = -1,
-               consumers =
-                   [#consumer{pid = self(),
-                              subscription_id = 0,
-                              active = true},
-                    #consumer{pid = self(),
-                              subscription_id = 1,
-                              active = false}]},
-    State0 =
-        #rabbit_stream_sac_coordinator{groups = #{GroupId => Group},
-                                       pids_groups = #{}},
+        cgroup([consumer(self(), 0, true), consumer(self(), 1, false)]),
+    State0 = state(#{GroupId => Group}),
     Monitors0 = #{},
     Command0 =
-        {register_consumer, <<"/">>, <<"stream">>, -1, <<"app">>, self(), 0},
-    {#rabbit_stream_sac_coordinator{pids_groups = PidsGroups1} = State1,
-     Monitors1, Effects1} =
+        register_consumer_command(<<"stream">>, -1, <<"app">>, self(), 0),
+    {#?STATE{pids_groups = PidsGroups1} = State1, Monitors1, Effects1} =
         rabbit_stream_sac_coordinator:ensure_monitors(Command0,
                                                       State0,
                                                       Monitors0,
                                                       []),
-    ?assertEqual(1, maps:size(PidsGroups1)),
-    ?assertEqual(1,
-                 sets:size(
-                     maps:get(self(), PidsGroups1))),
+    assertSize(1, PidsGroups1),
+    assertSize(1, maps:get(self(), PidsGroups1)),
     ?assertEqual(#{self() => {self(), sac}}, Monitors1),
     ?assertEqual([{monitor, process, self()}, {monitor, node, node()}],
                  Effects1),
-    Command1 =
-        {register_consumer, <<"/">>, <<"stream">>, -1, <<"app">>, self(), 1},
 
-    {#rabbit_stream_sac_coordinator{pids_groups = PidsGroups2} = State2,
-     Monitors2, Effects2} =
+    Command1 =
+        register_consumer_command(<<"stream">>, -1, <<"app">>, self(), 1),
+
+    {#?STATE{pids_groups = PidsGroups2} = State2, Monitors2, Effects2} =
         rabbit_stream_sac_coordinator:ensure_monitors(Command1,
                                                       State1,
                                                       Monitors1,
                                                       []),
-    ?assertEqual(1, maps:size(PidsGroups2)),
-    ?assertEqual(1,
-                 sets:size(
-                     maps:get(self(), PidsGroups2))),
+    assertSize(1, PidsGroups2),
+    assertSize(1, maps:get(self(), PidsGroups2)),
     ?assertEqual(#{self() => {self(), sac}}, Monitors2),
     ?assertEqual([{monitor, process, self()}, {monitor, node, node()}],
                  Effects2),
-    Group2 =
-        #group{partition_index = -1,
-               consumers =
-                   [#consumer{pid = self(),
-                              subscription_id = 1,
-                              active = true}]},
+    Group2 = cgroup([consumer(self(), 1, true)]),
 
     Command2 =
-        {unregister_consumer, <<"/">>, <<"stream">>, <<"app">>, self(), 0},
+        unregister_consumer_command(<<"stream">>, <<"app">>, self(), 0),
 
-    {#rabbit_stream_sac_coordinator{pids_groups = PidsGroups3} = State3,
-     Monitors3, Effects3} =
+    {#?STATE{pids_groups = PidsGroups3} = State3, Monitors3, Effects3} =
         rabbit_stream_sac_coordinator:ensure_monitors(Command2,
-                                                      State2#rabbit_stream_sac_coordinator{groups
-                                                                                               =
-                                                                                               #{GroupId
-                                                                                                     =>
-                                                                                                     Group2}},
+                                                      State2#?STATE{groups =
+                                                                        #{GroupId
+                                                                              =>
+                                                                              Group2}},
                                                       Monitors2,
                                                       []),
-    ?assertEqual(1, maps:size(PidsGroups3)),
-    ?assertEqual(1,
-                 sets:size(
-                     maps:get(self(), PidsGroups3))),
+    assertSize(1, PidsGroups3),
+    assertSize(1, maps:get(self(), PidsGroups3)),
     ?assertEqual(#{self() => {self(), sac}}, Monitors3),
     ?assertEqual([], Effects3),
 
     Command3 =
-        {unregister_consumer, <<"/">>, <<"stream">>, <<"app">>, self(), 1},
+        unregister_consumer_command(<<"stream">>, <<"app">>, self(), 1),
 
-    {#rabbit_stream_sac_coordinator{pids_groups = PidsGroups4} = _State4,
-     Monitors4, Effects4} =
+    {#?STATE{pids_groups = PidsGroups4} = _State4, Monitors4, Effects4} =
         rabbit_stream_sac_coordinator:ensure_monitors(Command3,
-                                                      State3#rabbit_stream_sac_coordinator{groups
-                                                                                               =
-                                                                                               #{}},
+                                                      State3#?STATE{groups =
+                                                                        #{}},
                                                       Monitors3,
                                                       []),
-    ?assertEqual(0, maps:size(PidsGroups4)),
-    ?assertEqual(0, maps:size(Monitors4)),
+    assertEmpty(PidsGroups4),
+    assertEmpty(Monitors4),
     ?assertEqual([{demonitor, process, self()}], Effects4),
 
     ok.
@@ -150,33 +127,18 @@ handle_connection_down_test(_) ->
     Pid0 = self(),
     Pid1 = spawn(fun() -> ok end),
     Group =
-        #group{partition_index = -1,
-               consumers =
-                   [#consumer{pid = Pid0,
-                              subscription_id = 0,
-                              active = true},
-                    #consumer{pid = Pid1,
-                              subscription_id = 1,
-                              active = false},
-                    #consumer{pid = Pid0,
-                              subscription_id = 2,
-                              active = false}]},
+        cgroup([consumer(Pid0, 0, true), consumer(Pid1, 1, false),
+                consumer(Pid0, 2, false)]),
     State0 =
-        #rabbit_stream_sac_coordinator{groups = #{GroupId => Group},
-                                       pids_groups =
-                                           #{Pid0 => sets:from_list([GroupId]),
-                                             Pid1 =>
-                                                 sets:from_list([GroupId])}},
+        state(#{GroupId => Group},
+              #{Pid0 => sets:from_list([GroupId]),
+                Pid1 => sets:from_list([GroupId])}),
 
-    {#rabbit_stream_sac_coordinator{pids_groups = PidsGroups1,
-                                    groups = Groups1} =
-         State1,
+    {#?STATE{pids_groups = PidsGroups1, groups = Groups1} = State1,
      Effects1} =
         rabbit_stream_sac_coordinator:handle_connection_down(Pid0, State0),
-    ?assertEqual(1, maps:size(PidsGroups1)),
-    ?assertEqual(1,
-                 sets:size(
-                     maps:get(Pid1, PidsGroups1))),
+    assertSize(1, PidsGroups1),
+    assertSize(1, maps:get(Pid1, PidsGroups1)),
     ?assertEqual([{mod_call,
                    rabbit_stream_sac_coordinator,
                    send_message,
@@ -184,20 +146,69 @@ handle_connection_down_test(_) ->
                     {sac,
                      {{subscription_id, 1}, {active, true}, {extra, []}}}]}],
                  Effects1),
-    ?assertEqual(#{GroupId =>
-                       #group{partition_index = -1,
-                              consumers =
-                                  [#consumer{pid = Pid1,
-                                             subscription_id = 1,
-                                             active = true}]}},
+    ?assertEqual(#{GroupId => cgroup([consumer(Pid1, 1, true)])},
                  Groups1),
-    {#rabbit_stream_sac_coordinator{pids_groups = PidsGroups2,
-                                    groups = Groups2} =
-         _State2,
+    {#?STATE{pids_groups = PidsGroups2, groups = Groups2} = _State2,
      Effects2} =
         rabbit_stream_sac_coordinator:handle_connection_down(Pid1, State1),
-    ?assertEqual(0, maps:size(PidsGroups2)),
-    ?assertEqual([], Effects2),
-    ?assertEqual(0, maps:size(Groups2)),
+    assertEmpty(PidsGroups2),
+    assertEmpty(Effects2),
+    assertEmpty(Groups2),
 
     ok.
+
+assertSize(Expected, []) ->
+    ?assertEqual(Expected, 0);
+assertSize(Expected, Map) when is_map(Map) ->
+    ?assertEqual(Expected, maps:size(Map));
+assertSize(Expected, List) when is_list(List) ->
+    ?assertEqual(Expected, length(List));
+assertSize(Expected, Other) ->
+    case sets:is_set(Other) of
+        true ->
+            ?assertEqual(Expected, sets:size(Other))
+    end.
+
+assertEmpty(Data) ->
+    assertSize(0, Data).
+
+consumer(Pid, SubId, Active) ->
+    #consumer{pid = Pid,
+              subscription_id = SubId,
+              active = Active}.
+
+cgroup(Consumers) ->
+    cgroup(-1, Consumers).
+
+cgroup(PartitionIndex, Consumers) ->
+    #group{partition_index = PartitionIndex, consumers = Consumers}.
+
+state(Groups) ->
+    state(Groups, #{}).
+
+state(Groups, PidsGroups) ->
+    #?STATE{groups = Groups, pids_groups = PidsGroups}.
+
+register_consumer_command(Stream,
+                          PartitionIndex,
+                          ConsumerName,
+                          ConnectionPid,
+                          SubId) ->
+    {register_consumer,
+     <<"/">>,
+     Stream,
+     PartitionIndex,
+     ConsumerName,
+     ConnectionPid,
+     SubId}.
+
+unregister_consumer_command(Stream,
+                            ConsumerName,
+                            ConnectionPid,
+                            SubId) ->
+    {unregister_consumer,
+     <<"/">>,
+     Stream,
+     ConsumerName,
+     ConnectionPid,
+     SubId}.
