dd89c18f6697f690d3403f9a5410fd47f2cf5732
Kosta Kalenteridis <kostakal@amazon.com>
2022-02-03 12:50:44 -0800

    Refactoring get_shovel_data function, as well as fixing indentation

--- deps/rabbitmq_shovel_management/priv/www/js/shovel.js
+++ deps/rabbitmq_shovel_management/priv/www/js/shovel.js
@@ -87,7 +87,7 @@ dispatcher_add(function(sammy) {
             if (sync_delete(this, '/shovels/vhost/:vhost/:name')) {
                 go_to('#/dynamic-shovels');
             } else {
-                show_popup('warn', 'Shovel was not able to be deleted');
+                show_popup('warn', 'Shovel could not be deleted');
                 return false;
             }
         });
@@ -95,7 +95,7 @@ dispatcher_add(function(sammy) {
             if (sync_delete(this, '/shovels/vhost/:vhost/:name/restart')) {
                 update();
             } else {
-                show_popup('warn', 'Shovel was not able to be restarted');
+                show_popup('warn', 'Shovel could not be restarted');
                 return false;
             }
         });
