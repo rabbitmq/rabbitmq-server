f56fd41e4f0c856d5261f0c3ec8dd219d3ca63ea
Kosta Kalenteridis <kostakal@amazon.com>
2022-02-08 11:58:49 -0800

    Splitting into util file, adding tests

--- deps/rabbitmq_shovel_management/src/rabbit_shovel_mgmt.erl
+++ deps/rabbitmq_shovel_management/src/rabbit_shovel_mgmt.erl
@@ -11,7 +11,7 @@
 
 -export([dispatcher/0, web_ui/0]).
 -export([init/2, to_json/2, resource_exists/2, content_types_provided/2,
-         is_authorized/2, allowed_methods/2, delete_resource/2]).
+         is_authorized/2, allowed_methods/2, delete_resource/2, get_shovel_node/4]).
 
 -import(rabbit_misc, [pget/2]).
 
@@ -59,7 +59,7 @@ resource_exists(ReqData, Context) ->
 
 to_json(ReqData, Context) ->
     rabbit_mgmt_util:reply_list(
-      filter_vhost_req(status(ReqData, Context), ReqData), ReqData, Context).
+      filter_vhost_req(rabbit_shovel_mgmt_util:status(ReqData, Context), ReqData), ReqData, Context).
 
 is_authorized(ReqData, Context) ->
     rabbit_mgmt_util:is_authorized_monitor(ReqData, Context).
@@ -99,16 +99,6 @@ filter_vhost_req(List, ReqData) ->
                            pget(vhost, I) =:= VHost]
     end.
 
-%% Allow users to see things in the vhosts they are authorised. But
-%% static shovels do not have a vhost, so only allow admins (not
-%% monitors) to see them.
-filter_vhost_user(List, _ReqData, #context{user = User = #user{tags = Tags}}) ->
-    VHosts = rabbit_mgmt_util:list_login_vhosts_names(User, undefined),
-    [I || I <- List, case pget(vhost, I) of
-                         undefined -> lists:member(administrator, Tags);
-                         VHost     -> lists:member(VHost, VHosts)
-                     end].
-
 restart_shovel(VHost, Name, Node) ->
     rabbit_log:info("Calling rabbit_shovel_util:restart_shovel on Node '~s' for shovel '~s' in vhost '~s'", [Node, Name, VHost]),
     case rpc:call(Node, rabbit_shovel_util, restart_shovel, [VHost, Name], infinity) of
@@ -126,7 +116,7 @@ delete_shovel(VHost, Name, Node, Username) ->
     end.
 
 get_shovel_node(VHost, Name, ReqData, Context) ->
-    AllShovels = status(ReqData, Context),
+    AllShovels = rabbit_shovel_mgmt_util:status(ReqData, Context),
     MatchedShovel = lists:filter(fun (Shovel) ->
         lists:member({name, Name}, Shovel) and lists:member({vhost, VHost}, Shovel) end, AllShovels),
     case MatchedShovel of
@@ -134,43 +124,3 @@ get_shovel_node(VHost, Name, ReqData, Context) ->
         ShovelData -> {_,Node}=lists:keyfind(node, 1, lists:nth(1, ShovelData)),
             Node
     end.
-
-status(ReqData, Context) ->
-    filter_vhost_user(
-      lists:append([status(Node) || Node <- [node() | nodes()]]),
-      ReqData, Context).
-
-status(Node) ->
-    case rpc:call(Node, rabbit_shovel_status, status, [], infinity) of
-        {badrpc, {'EXIT', _}} ->
-            [];
-        Status ->
-            [format(Node, I) || I <- Status]
-    end.
-
-format(Node, {Name, Type, Info, TS}) ->
-    [{node, Node}, {timestamp, format_ts(TS)}] ++
-        format_name(Type, Name) ++
-        format_info(Info).
-
-format_name(static,  Name)          -> [{name,  Name},
-                                        {type,  static}];
-format_name(dynamic, {VHost, Name}) -> [{name,  Name},
-                                        {vhost, VHost},
-                                        {type,  dynamic}].
-
-format_info(starting) ->
-    [{state, starting}];
-
-format_info({running, Props}) ->
-    [{state, running}] ++ Props;
-
-format_info({terminated, Reason}) ->
-    [{state,  terminated},
-     {reason, print("~p", [Reason])}].
-
-format_ts({{Y, M, D}, {H, Min, S}}) ->
-    print("~w-~2.2.0w-~2.2.0w ~w:~2.2.0w:~2.2.0w", [Y, M, D, H, Min, S]).
-
-print(Fmt, Val) ->
-    list_to_binary(io_lib:format(Fmt, Val)).
