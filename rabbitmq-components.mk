699bb53f62aa40cd36c0f6fd563033e229fb931e
David Ansari <david.ansari@gmx.de>
2022-02-11 16:49:38 +0100

    Bump observer_cli to v1.7.2
    (cherry picked from commit 1aac573cb3cfd435b81c16d62a1d0fdeadee697d)
    (cherry picked from commit 2ad58f829bdfc9dfe53a9c2e10473efddb894e24)

--- rabbitmq-components.mk
+++ rabbitmq-components.mk
@@ -120,7 +120,7 @@ dep_ra = hex 2.0.3
 dep_ranch = hex 2.1.0
 dep_recon = hex 2.5.2
 dep_redbug = hex 2.0.7
-dep_observer_cli = hex 1.7.1
+dep_observer_cli = hex 1.7.2
 dep_stdout_formatter = hex 0.2.4
 dep_sysmon_handler = hex 1.3.0
 
