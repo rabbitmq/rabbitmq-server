c42d37f22969a04b73ed429dc1f24241fa9ddda6
Kosta Kalenteridis <kostakal@amazon.com>
2022-02-14 20:43:28 -0800

    Adding timeout for shovel calls

---
+++ deps/rabbitmq_shovel_management/src/rabbit_shovel_mgmt.hrl
@@ -0,0 +1,8 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2021 VMware, Inc. or its affiliates.  All rights reserved.
+%%
+
+-define(SHOVEL_CALLS_TIMEOUT_MS, 5000).
