472bd0dbbc19deede0ff50ea5ed76749ae37f54a
David Ansari <david.ansari@gmx.de>
2022-02-18 16:50:52 +0100

    Resend pending commands if same leader re-elected
    Before this commit, if the same leader was re-elected,
    rabbit_fifo_client did not resend.
    That's undesired because a queue restart can happen on a single node cluster.
    
    (cherry picked from commit 9433100ef65444d8c20092cb59501021eceb3ef8)

--- deps/rabbit/src/rabbit_fifo_client.erl
+++ deps/rabbit/src/rabbit_fifo_client.erl
@@ -593,10 +593,6 @@ handle_ra_event(_, {machine, {queue_status, Status}},
     %% just set the queue status
     {ok, State#state{queue_status = Status}, []};
 handle_ra_event(Leader, {machine, leader_change},
-                #state{leader = Leader} = State) ->
-    %% leader already known
-    {ok, State, []};
-handle_ra_event(Leader, {machine, leader_change},
                 #state{leader = OldLeader} = State0) ->
     %% we need to update leader
     %% and resend any pending commands
