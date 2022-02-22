980ef3f93c5a855aff5a3ab2bcdea4d32c91492a
David Ansari <david.ansari@gmx.de>
2022-02-22 19:15:49 +0100

    Use one ?MSG macro
    instead of ?INDEX_MSG and ?DISK_MSG macros
    because from now on there will be only disk messages.
    
    This makes the code more readable.

--- deps/rabbit/src/rabbit_fifo_dlx.hrl
+++ deps/rabbit/src/rabbit_fifo_dlx.hrl
@@ -4,20 +4,14 @@
           %% creates only a single consumer to this quorum queue's discards queue.
           pid :: pid(),
           prefetch :: non_neg_integer(),
-          checked_out = #{} :: #{msg_id() => tuple(
-                                               rabbit_dead_letter:reason(),
-                                               indexed_msg()
-                                              )},
+          checked_out = #{} :: #{msg_id() => tuple(rabbit_dead_letter:reason(), msg())},
           next_msg_id = 0 :: msg_id()
          }).
 
 -record(rabbit_fifo_dlx,{
           consumer = undefined :: #dlx_consumer{} | undefined,
           %% Queue of dead-lettered messages.
-          discards = lqueue:new() :: lqueue:lqueue(tuple(
-                                                     rabbit_dead_letter:reason(),
-                                                     indexed_msg()
-                                                    )),
+          discards = lqueue:new() :: lqueue:lqueue(tuple(rabbit_dead_letter:reason(), msg())),
           %% Raft indexes of messages in both discards queue and dlx_consumer's checked_out map
           %% so that we get the smallest ra index in O(1).
           ra_indexes = rabbit_fifo_index:empty() :: rabbit_fifo_index:state(),
