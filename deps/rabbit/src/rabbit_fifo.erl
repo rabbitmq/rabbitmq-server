3c10a31852bb8cc5c68fd12e8cb2aef944d32b91
David Ansari <david.ansari@gmx.de>
2022-03-02 16:45:54 +0100

    Fix badmatch when basic.get on quorum queue with expired messages
    Before this commit, when a quorum queue has only expired messages, but the
    timer hasn't expired them yet, and a basic.get tries to dequeue a message
    the following error occurred:
    
    {{badmatch,
         {nochange,true,
             {rabbit_fifo, ...},
             [{mod_call,rabbit_global_counters,
                  messages_dead_lettered,
                  [expired,rabbit_quorum_queue,disabled,1]}]}},
     [{rabbit_fifo,apply,3,[{file,"rabbit_fifo.erl"},{line,387}]},
    
    (cherry picked from commit 08f20612b69d5d87314d6ab0dbe17ab2a5b7f8e8)

--- deps/rabbit/src/rabbit_fifo.erl
+++ deps/rabbit/src/rabbit_fifo.erl
@@ -380,32 +380,38 @@ apply(#{index := Index,
         _ when Exists ->
             %% a dequeue using the same consumer_id isn't possible at this point
             {State0, {dequeue, empty}};
-        Ready ->
+        _ ->
             State1 = update_consumer(Meta, ConsumerId, ConsumerMeta,
                                      {once, 1, simple_prefetch}, 0,
                                      State0),
-            {success, _, MsgId, ?MSG(RaftIdx, Header), ExpiredMsg, State2, Effects0} =
-                checkout_one(Meta, false, State1, []),
-            {State4, Effects1} = case Settlement of
-                                     unsettled ->
-                                         {_, Pid} = ConsumerId,
-                                         {State2, [{monitor, process, Pid} | Effects0]};
-                                     settled ->
-                                         %% immediately settle the checkout
-                                         {State3, _, SettleEffects} =
-                                             apply(Meta, make_settle(ConsumerId, [MsgId]),
-                                                   State2),
-                                         {State3, SettleEffects ++ Effects0}
-                                 end,
-            Effects2 = [reply_log_effect(RaftIdx, MsgId, Header, Ready - 1, From) | Effects1],
-            {State, DroppedMsg, Effects} = evaluate_limit(Index, false, State0, State4,
-                                                          Effects2),
-            Reply = '$ra_no_reply',
-            case {DroppedMsg, ExpiredMsg} of
-                {false, false} ->
-                    {State, Reply, Effects};
-                _ ->
-                    update_smallest_raft_index(Index, Reply, State, Effects)
+            case checkout_one(Meta, false, State1, []) of
+                {success, _, MsgId, ?MSG(RaftIdx, Header), ExpiredMsg, State2, Effects0} ->
+                    {State4, Effects1} = case Settlement of
+                                             unsettled ->
+                                                 {_, Pid} = ConsumerId,
+                                                 {State2, [{monitor, process, Pid} | Effects0]};
+                                             settled ->
+                                                 %% immediately settle the checkout
+                                                 {State3, _, SettleEffects} =
+                                                 apply(Meta, make_settle(ConsumerId, [MsgId]),
+                                                       State2),
+                                                 {State3, SettleEffects ++ Effects0}
+                                         end,
+                    Effects2 = [reply_log_effect(RaftIdx, MsgId, Header, messages_ready(State4), From) | Effects1],
+                    {State, DroppedMsg, Effects} = evaluate_limit(Index, false, State0, State4,
+                                                                  Effects2),
+                    Reply = '$ra_no_reply',
+                    case {DroppedMsg, ExpiredMsg} of
+                        {false, false} ->
+                            {State, Reply, Effects};
+                        _ ->
+                            update_smallest_raft_index(Index, Reply, State, Effects)
+                    end;
+                {nochange, _ExpiredMsg = true, State2, Effects0} ->
+                    %% All ready messages expired.
+                    State3 = State2#?MODULE{consumers = maps:remove(ConsumerId, State2#?MODULE.consumers)},
+                    {State, _, Effects} = evaluate_limit(Index, false, State0, State3, Effects0),
+                    update_smallest_raft_index(Index, {dequeue, empty}, State, Effects)
             end
     end;
 apply(#{index := Idx} = Meta,
