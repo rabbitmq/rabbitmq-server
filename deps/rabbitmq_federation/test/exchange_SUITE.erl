50f3dc8a3185af6b870c9a4ea984a9f72dd28bf4
Michael Klishin <michael@clojurewerkz.org>
2022-04-05 07:38:34 +0400

    Erlang 25 compat: drop a duplicate flaky test
    The test largely does what it's pattern-based version does.
    During our Erlang 24 and 25 upgrade cycles, this version required
    extra attention even though the feature works as advertised in
    other tests and during manual testing.
    
    (cherry picked from commit b18cd3e7497f615b444bf73d4b89705fe7649346)
    (cherry picked from commit c9e299372654bd985ee7e95cf2497d665176ee38)

--- deps/rabbitmq_federation/test/exchange_SUITE.erl
+++ deps/rabbitmq_federation/test/exchange_SUITE.erl
@@ -196,7 +196,7 @@ end_per_testcase(Testcase, Config) ->
     rabbit_ct_helpers:testcase_finished(Config, Testcase).
 
 %% -------------------------------------------------------------------
-%% Testcases.
+%% Test cases
 %% -------------------------------------------------------------------
 
 simple(Config) ->
