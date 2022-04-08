7cfac62a0f52169fa139033840e19b09d74db457
David Ansari <david.ansari@gmx.de>
2022-04-08 10:29:18 +0200

    Avoid expensive least replicas selection
    If there are a lot of queues, fetching them from the database
    and counting the number of replicas per node will be costly.
    
    Random replica selection is good enough, will balance the RabbitMQ
    cluster in the long term and this was the only way to select replicas
    before this PR.
    
    The only downside of random replica selection is when there are many
    RabbitMQ nodes in the cluster, say 7, but only few quorum queues are
    created, the randomness might not place replicas on all 7 nodes.
    
    Therefore, in this commit we ensure even distribution of replicas across
    RabbitMQ nodes only if the total number of queues is not high
    (hard-coded to 1000 right now).
    
    Only the total number of queues can be cheaply counted by calling
    ```
    mnesia:table_info(rabbit_queue, size)
    ```
    Number of queues by queue type or replicas per node cannot be cheaply
    counted as this would require a full table scan.
    Note that ets:select_count/2 would also need to scan the whole table.
    
    There can be edge cases such as the following:
    5 node RabbitMQ cluster with 10k non-HA queues on node 1, 2, and 3
    but no queues on node 4, and 5.
    New 3-replica quroum queue gets declared and the random replica selection
    could place the replicas on node 1, 2, and 3.
    
    The benefit of avoiding expensive least replica selection greatly outweighs
    the downside of such edge cases.
    
    (cherry picked from commit ae9c389eb92b7b2e362b782cb9a8db5dee108ab9)

--- deps/rabbit/src/rabbit_queue_location.erl
+++ deps/rabbit/src/rabbit_queue_location.erl
@@ -11,6 +11,10 @@
 
 -export([select_leader_and_followers/2]).
 
+-define(QUEUES_LIMIT_FOR_LEAST_REPLICAS_SELECTION, 1_000).
+
+-spec select_leader_and_followers(amqqueue:amqqueue(), pos_integer()) ->
+    {Leader :: node(), Followers :: [node()]}.
 select_leader_and_followers(Q, Size)
   when (?amqqueue_is_quorum(Q) orelse ?amqqueue_is_stream(Q)) andalso is_integer(Size) ->
     QueueType = amqqueue:get_type(Q),
@@ -27,50 +31,63 @@ select_leader_and_followers(Q, Size)
     Followers = lists:delete(Leader, Replicas),
     {Leader, Followers}.
 
+-spec select_replicas(pos_integer(), [node(),...], [node(),...], function()) ->
+    {[node(),...], function()}.
 select_replicas(Size, AllNodes, _, Fun)
   when length(AllNodes) =< Size ->
     {AllNodes, Fun};
-select_replicas(Size, _, RunningNodes, Fun)
-  when length(RunningNodes) =:= Size ->
-    {RunningNodes, Fun};
 select_replicas(Size, AllNodes, RunningNodes, GetQueues) ->
     %% Select nodes in the following order:
-    %% 1. local node (to have data locality for declaring client)
-    %% 2. running nodes
-    %% 3. nodes with least replicas (to have a "balanced" RabbitMQ cluster).
+    %% 1.   Local node to have data locality for declaring client.
+    %% 2.   Running nodes.
+    %% 3.1. If there are few queues: Nodes with least replicas to have a "balanced" RabbitMQ cluster.
+    %% 3.2. If there are many queues: Randomly to avoid expensive calculation of counting replicas
+    %%      per node. Random replica selection is good enough for most use cases.
     Local = node(),
     true = lists:member(Local, AllNodes),
-    true = lists:member(Local, RunningNodes),
-    Counters0 = maps:from_list([{Node, 0} || Node <- lists:delete(Local, AllNodes)]),
-    Queues = GetQueues(),
-    Counters = lists:foldl(fun(Q, Acc) ->
-                                   #{nodes := Nodes} = amqqueue:get_type_state(Q),
-                                   lists:foldl(fun(N, A)
-                                                     when is_map_key(N, A) ->
-                                                       maps:update_with(N, fun(C) -> C+1 end, A);
-                                                  (_, A) ->
-                                                       A
-                                               end, Acc, Nodes)
-                           end, Counters0, Queues),
-    L0 = maps:to_list(Counters),
-    L1 = lists:sort(fun({N0, C0}, {N1, C1}) ->
-                            case {lists:member(N0, RunningNodes),
-                                  lists:member(N1, RunningNodes)} of
-                                {true, false} ->
-                                    true;
-                                {false, true} ->
-                                    false;
-                                _ ->
-                                    C0 =< C1
-                            end
-                    end, L0),
-    {L2, _} = lists:split(Size - 1, L1),
-    L = lists:map(fun({N, _}) -> N end, L2),
-    {[Local | L], fun() -> Queues end}.
+    RemoteNodes = lists:delete(Local, AllNodes),
+    case rabbit_amqqueue:count() of
+        Count when Count =< ?QUEUES_LIMIT_FOR_LEAST_REPLICAS_SELECTION ->
+            Counters0 = maps:from_list([{N, 0} || N <- RemoteNodes]),
+            Queues = GetQueues(),
+            Counters = lists:foldl(fun(Q, Acc) ->
+                                           #{nodes := Nodes} = amqqueue:get_type_state(Q),
+                                           lists:foldl(fun(N, A)
+                                                             when is_map_key(N, A) ->
+                                                               maps:update_with(N, fun(C) -> C+1 end, A);
+                                                          (_, A) ->
+                                                               A
+                                                       end, Acc, Nodes)
+                                   end, Counters0, Queues),
+            L0 = maps:to_list(Counters),
+            L1 = lists:sort(fun({N0, C0}, {N1, C1}) ->
+                                    case {lists:member(N0, RunningNodes),
+                                          lists:member(N1, RunningNodes)} of
+                                        {true, false} ->
+                                            true;
+                                        {false, true} ->
+                                            false;
+                                        _ ->
+                                            C0 =< C1
+                                    end
+                            end, L0),
+            {L2, _} = lists:split(Size - 1, L1),
+            L = lists:map(fun({N, _}) -> N end, L2),
+            {[Local | L], fun() -> Queues end};
+        _ ->
+            L0 = shuffle(RemoteNodes),
+            L1 = lists:sort(fun(X, _Y) ->
+                                    lists:member(X, RunningNodes)
+                            end, L0),
+            {L, _} = lists:split(Size - 1, L1),
+            {[Local | L], GetQueues}
+    end.
 
 leader_locator(undefined) -> <<"client-local">>;
 leader_locator(Val) -> Val.
 
+-spec leader_node(nonempty_binary(), [node(),...], [node(),...], function()) ->
+    node().
 leader_node(<<"client-local">>, _, _, _) ->
     node();
 leader_node(<<"random">>, Nodes0, RunningNodes, _) ->
@@ -95,17 +112,21 @@ leader_node(<<"least-leaders">>, Nodes0, RunningNodes, GetQueues)
     {Node, _} = hd(lists:keysort(2, maps:to_list(Counters))),
     Node.
 
-potential_leaders(Nodes, AllRunningNodes) ->
-    RunningNodes = lists:filter(fun(N) ->
-                                        lists:member(N, AllRunningNodes)
-                                end, Nodes),
-    case rabbit_maintenance:filter_out_drained_nodes_local_read(RunningNodes) of
+potential_leaders(Replicas, RunningNodes) ->
+    case lists:filter(fun(R) ->
+                              lists:member(R, RunningNodes)
+                      end, Replicas) of
         [] ->
-            %% All running nodes are drained. Let's place the leader on a drained node
-            %% respecting the requested queue-leader-locator streategy.
-            RunningNodes;
-        Filtered ->
-            Filtered
+            Replicas;
+        RunningReplicas ->
+            case rabbit_maintenance:filter_out_drained_nodes_local_read(RunningReplicas) of
+                [] ->
+                    %% All selected replica nodes are drained. Let's place the leader on a
+                    %% drained node respecting the requested queue-leader-locator streategy.
+                    RunningReplicas;
+                Filtered ->
+                    Filtered
+            end
     end.
 
 %% Return a function so that queues are fetched lazily (i.e. only when needed,
@@ -117,3 +138,8 @@ get_queues_for_type(QueueType) ->
                                                  amqqueue:pattern_match_on_type(QueueType))
                end)
     end.
+
+shuffle(L0) when is_list(L0) ->
+    L1 = lists:map(fun(E) -> {rand:uniform(), E} end, L0),
+    L = lists:keysort(1, L1),
+    lists:map(fun({_, E}) -> E end, L).
