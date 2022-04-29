e4380948257afb85db5ada5510c8d5ba760bf538
Michael Klishin <klishinm@vmware.com>
2022-04-30 01:19:54 +0400

    Drop -Wrace_conditions from Dialyzer opts
    For Erlang 25 compatibility.
    
    Since ninenines/erlang.mk#947 has been merged, no reason
    not to do it in RabbitMQ's temporary fork.
    
    (cherry picked from commit 310e1dd157f95921a51619f36a06b4fd3b2fd2ed)

--- erlang.mk
+++ erlang.mk
@@ -6722,7 +6722,7 @@ export DIALYZER_PLT
 
 PLT_APPS ?=
 DIALYZER_DIRS ?= --src -r $(wildcard src) $(ALL_APPS_DIRS)
-DIALYZER_OPTS ?= -Werror_handling -Wrace_conditions -Wunmatched_returns # -Wunderspecs
+DIALYZER_OPTS ?= -Werror_handling -Wunmatched_returns # -Wunderspecs
 DIALYZER_PLT_OPTS ?=
 
 # Core targets.
