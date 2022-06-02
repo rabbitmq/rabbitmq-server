4b01b219e9e1df608b05d0ebe11d3c80ea1b87ff
Marcial Rosales <mrosales@pivotal.io>
2022-06-02 18:13:56 +0200

    Testing happy login for oauth
    (cherry picked from commit bcb26a20fc6924f0af6f761833630bbe11d60357)

--- deps/rabbitmq_management/selenium/tests/oauth/with-uaa-down/landing.js
+++ deps/rabbitmq_management/selenium/tests/oauth/with-uaa-down/landing.js
@@ -6,7 +6,7 @@ var buildDriver = function(caps) {
   return new Builder().forBrowser('chrome').build();
 };
 
-describe("Management UI with UAA running", function() {
+describe("Management UI with UAA down", function() {
   var driver;
   var page;
   this.timeout(3000);
@@ -17,19 +17,24 @@ describe("Management UI with UAA running", function() {
     done();
   });
 
-  it("should have a title", function(done) {
-      page.then(function() {
-        driver.getTitle().then(function(title) {
-          assert(title.match("RabbitMQ Management") != null);
-          done();
-      });
-    });
+  it("should have a warning message", function(done) {
+
+      driver.wait(until.elementLocated(By.id("outer")))
+        .then(function(outer) { return outer.findElement(By.className("warning")); })
+        .then(function(warning) {
+          warning.getText().then(function(text) {
+            assert.equal(text, "http://localhost:8080/uaa does not appear to be a running OAuth2.0 instance or may not have a trusted SSL certificate");
+            done();
+          });
+        });
+
   });
+
 
   it("should have a login button", function(done) {
       driver.wait(until.elementLocated(By.id("loginWindow"))).then(function(loginButton) {
         loginButton.getText().then(function(text) {
-          assert.equal(text, "Click here to log in");
+          assert.equal(text, "Single Sign On");
           done();
         })
     });
---
+++ deps/rabbitmq_management/selenium/tests/oauth/with-uaa-down/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export RABBITMQ_CONFIG=${SCRIPT}/rabbitmq.config
+
+. $SCRIPT/../../../bin/rabbitmq.sh
+
+rabbitmq
