bb800e338a0655457b4946c10c3eeb02bf6b59f1
Luke Bakken <luke@bakken.io>
2022-06-06 10:47:12 -0700

    Fix cluster links statistic
    Use the `sys_dist` ets table to get distribution port information.
    
    Fixes #4981
    
    Get cluster links stats for TLS dist
    
    Use code from prometheus.erl to get dist links info
    
    (cherry picked from commit 86a509df80ee81a799c08c783914cc0a95d5890c)

--- deps/rabbitmq_management_agent/src/rabbit_mgmt_external_stats.erl
+++ deps/rabbitmq_management_agent/src/rabbit_mgmt_external_stats.erl
@@ -15,8 +15,6 @@
 
 -export([list_registry_plugins/1]).
 
--import(rabbit_misc, [pget/2]).
-
 -include_lib("rabbit_common/include/rabbit.hrl").
 
 -define(METRICS_KEYS, [fd_used, sockets_used, mem_used, disk_free, proc_used, gc_num,
@@ -350,40 +348,7 @@ flatten_key({A, B}) ->
     list_to_atom(atom_to_list(A) ++ "_" ++ atom_to_list(B)).
 
 cluster_links() ->
-    {ok, Items} = net_kernel:nodes_info(),
-    [Link || Item <- Items,
-             Link <- [format_nodes_info(Item)], Link =/= undefined].
-
-format_nodes_info({Node, Info}) ->
-    Owner = proplists:get_value(owner, Info),
-    case catch process_info(Owner, links) of
-        {links, Links} ->
-            case [Link || Link <- Links, is_port(Link)] of
-                [Port] ->
-                    {Node, Owner, format_nodes_info1(Port)};
-                _ ->
-                    undefined
-            end;
-        _ ->
-            undefined
-    end.
-
-format_nodes_info1(Port) ->
-    case {rabbit_net:socket_ends(Port, inbound),
-          rabbit_net:getstat(Port, [recv_oct, send_oct])} of
-        {{ok, {PeerAddr, PeerPort, SockAddr, SockPort}}, {ok, Stats}} ->
-            [{peer_addr, maybe_ntoab(PeerAddr)},
-             {peer_port, PeerPort},
-             {sock_addr, maybe_ntoab(SockAddr)},
-             {sock_port, SockPort},
-             {recv_bytes, pget(recv_oct, Stats)},
-             {send_bytes, pget(send_oct, Stats)}];
-        _ ->
-            []
-    end.
-
-maybe_ntoab(A) when is_tuple(A) -> list_to_binary(rabbit_misc:ntoab(A));
-maybe_ntoab(H)                  -> H.
+    rabbit_net:dist_info().
 
 %%--------------------------------------------------------------------
 
