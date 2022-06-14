90df46c96822757b838fa2583a0625aa850ae1ac
Philip Kuryloski <kuryloskip@vmware.com>
2022-06-14 10:15:45 +0200

    Fixup merge

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -159,11 +159,13 @@ defmodule RabbitMQCtl.MixfileBase do
           {
             :goldrush,
             path: Path.join(deps_dir, "goldrush"),
+            compile: (if is_bazel, do: false, else: "make"),
             override: true
           },
           {
             :lager,
             path: Path.join(deps_dir, "lager"),
+            compile: (if is_bazel, do: false, else: "make"),
             override: true
           },
           {
