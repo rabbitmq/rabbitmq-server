44c99dd3422d1d0c67cb623816e7219c87b85e44
Michael Klishin <michael@clojurewerkz.org>
2022-06-30 20:51:38 +0400

    Resolve a conflict: rabbit_ff_registry_factory should not be in v3.10.x
    (cherry picked from commit 16a2b3ae8c39a243488ed96e8935671b3049d6dc)

--- deps/rabbit/src/rabbit_feature_flags.erl
+++ deps/rabbit/src/rabbit_feature_flags.erl
@@ -1049,7 +1049,7 @@ query_supported_feature_flags() ->
                          rabbit_feature_flag),
     T1 = erlang:timestamp(),
     rabbit_log_feature_flags:debug(
-      "Feature flags: time to find supported feature flags: ~p µs",
+      "Feature flags: time to find supported feature flags: ~p us",
       [timer:now_diff(T1, T0)]),
     prepare_queried_feature_flags(AttributesPerApp, #{}).
 -endif.
