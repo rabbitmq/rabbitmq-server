0c75ddcfba45407b766e191b7b7147b7da9820d7
Michael Klishin <michael@clojurewerkz.org>
2022-07-18 14:28:51 +0400

    Merge branch 'Haster2004-shovel/issue-4219'
    (cherry picked from commit ce514f084efb2c9f08854c44a6587b6b2ad51f52)

--- deps/rabbitmq_shovel/src/rabbit_amqp10_shovel.erl
+++ deps/rabbitmq_shovel/src/rabbit_amqp10_shovel.erl
@@ -200,6 +200,10 @@ handle_source({amqp10_event, {link, Link, _Evt}},
 handle_source({'EXIT', Conn, Reason},
               #{source := #{current := #{conn := Conn}}}) ->
     {stop, {outbound_conn_died, Reason}};
+
+handle_source({'EXIT', _Pid, {shutdown, {server_initiated_close, ?PRECONDITION_FAILED, Reason}}}, _State) ->
+    {stop, {inbound_link_or_channel_closure, Reason}};
+
 handle_source(_Msg, _State) ->
     not_handled.
 
@@ -254,6 +258,10 @@ handle_dest({amqp10_event, {link, Link, _Evt}},
 handle_dest({'EXIT', Conn, Reason},
             #{dest := #{current := #{conn := Conn}}}) ->
     {stop, {outbound_conn_died, Reason}};
+
+handle_dest({'EXIT', _Pid, {shutdown, {server_initiated_close, ?PRECONDITION_FAILED, Reason}}}, _State) ->
+    {stop, {outbound_link_or_channel_closure, Reason}};
+
 handle_dest(_Msg, _State) ->
     not_handled.
 
