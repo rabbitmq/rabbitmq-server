9063212f1c72ac3bd0d5cc9097f94315082bcd91
Michael Klishin <klishinm@vmware.com>
2022-07-25 13:07:56 +0400

    HTTP API: allow for setting a few extra security-related response headers
    All configurable via `rabbitmq.conf`:
    
    ``` ini
    management.headers.content_type_options = nosniff
    management.headers.xss_protection = 1; mode=block
    management.headers.frame_options = DENY
    ```
    
    Closes #5320

--- deps/rabbitmq_management/src/rabbit_mgmt_headers.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_headers.erl
@@ -12,6 +12,10 @@
 -export([set_common_permission_headers/2]).
 -export([set_cors_headers/2, set_hsts_headers/2, set_csp_headers/2]).
 
+-define(X_CONTENT_TYPE_OPTIONS_HEADER, <<"X-Content-Type-Options">>).
+-define(X_FRAME_OPTIONS_HEADER, <<"X-Frame-Options">>).
+-define(X_XSS_PROTECTION_HEADER, <<"X-XSS-Protection">>).
+
 %%
 %% API
 %%
@@ -25,10 +29,35 @@ set_hsts_headers(ReqData, _Module) ->
 set_csp_headers(ReqData, _Module) ->
     rabbit_mgmt_csp:set_headers(ReqData).
 
+set_content_type_options_header(ReqData, _Module) ->
+    maybe_set_known_configured_header(ReqData, content_type_options, ?X_CONTENT_TYPE_OPTIONS_HEADER).
+
+set_xss_protection_header(ReqData, _Module) ->
+    maybe_set_known_configured_header(ReqData, xss_protection, ?X_XSS_PROTECTION_HEADER).
+
+set_frame_options_header(ReqData, _Module) ->
+    maybe_set_known_configured_header(ReqData, frame_options, ?X_FRAME_OPTIONS_HEADER).
+
+maybe_set_known_configured_header(ReqData, AppEnvKey, HeaderName) ->
+    case application:get_env(rabbitmq_management, headers) of
+        undefined      -> ReqData;
+        {ok, Proplist} ->
+            case proplists:get_value(AppEnvKey, Proplist) of
+                undefined -> ReqData;
+                Value     ->
+                    cowboy_req:set_resp_header(HeaderName,
+                                               rabbit_data_coercion:to_binary(Value),
+                                               ReqData)
+            end
+    end.
+
 set_common_permission_headers(ReqData0, EndpointModule) ->
     lists:foldl(fun(Fun, ReqData) ->
                         Fun(ReqData, EndpointModule)
                 end, ReqData0,
                [fun set_csp_headers/2,
                 fun set_hsts_headers/2,
-                fun set_cors_headers/2]).
+                fun set_cors_headers/2,
+                fun set_content_type_options_header/2,
+                fun set_xss_protection_header/2,
+                fun set_frame_options_header/2]).
