5795ba94b1d3c6a33dd23f099f6fe4dfebb0ad4a
dcorbacho <dparracorbac@vmware.com>
2022-07-27 11:09:24 +0200

    Move connection and channel tracking tables to ETS

--- deps/rabbit/src/rabbit_core_ff.erl
+++ deps/rabbit/src/rabbit_core_ff.erl
@@ -9,7 +9,11 @@
 
 -export([direct_exchange_routing_v2_enable/1,
          listener_records_in_ets_enable/1,
-         listener_records_in_ets_post_enable/1]).
+         listener_records_in_ets_post_enable/1,
+         tracking_records_in_ets_enable/1,
+         tracking_records_in_ets_post_enable/1]).
+
+-include("feature_flags.hrl").
 
 -rabbit_feature_flag(
    {classic_mirrored_queue_version,
@@ -90,6 +94,14 @@
                       {?MODULE, listener_records_in_ets_post_enable}}
      }}).
 
+-rabbit_feature_flag(
+   {tracking_records_in_ets,
+    #{desc          => "Store tracking records in ETS instead of Mnesia",
+      stability     => stable,
+      depends_on    => [feature_flags_v2],
+      migration_fun => {?MODULE, tracking_records_in_ets_migration}
+     }}).
+
 %% -------------------------------------------------------------------
 %% Direct exchange routing v2.
 %% -------------------------------------------------------------------
@@ -164,5 +176,45 @@ listener_records_in_ets_post_enable(#{feature_name := FeatureName}) ->
             rabbit_log_feature_flags:error(
               "Feature flags: `~s`: failed to delete Mnesia table: ~p",
               [FeatureName, Reason]),
+            ok
+    end.
+
+tracking_records_in_ets_enable(#{feature_name := FeatureName}) ->
+    try
+        rabbit_connection_tracking:migrate_tracking_records(),
+        rabbit_channel_tracking:migrate_tracking_records()
+    catch
+        throw:{error, {no_exists, _}} ->
+            ok;
+        throw:{error, Reason} ->
+            rabbit_log_feature_flags:error("Enabling feature flag ~s failed: ~p",
+                                           [FeatureName, Reason]),
+            {error, Reason}
+    end.
+
+tracking_records_in_ets_post_enable(#{feature_name = FeatureName}) ->
+    try
+        [delete_table(FeatureName, Tab) ||
+            Tab <- rabbit_connection_tracking:get_all_tracked_connection_table_names_for_node(node())],
+        [delete_table(FeatureName, Tab) ||
+            Tab <- rabbit_channel_tracking:get_all_tracked_channel_table_names_for_node(node())]
+    catch
+        throw:{error, Reason} ->
+            rabbit_log_feature_flags:error("Enabling feature flag ~s failed: ~p",
+                                           [FeatureName, Reason]),
+            %% adheres to the callback interface
+            ok
+    end.
+
+delete_table(FeatureName, Tab) ->
+    case mnesia:delete_table(Tab) of
+        {atomic, ok} ->
+            ok;
+        {aborted, {no_exists, _}} ->
+            ok;
+        {aborted, Err} ->
+            rabbit_log_feature_flags:error("Enabling feature flag ~s failed to delete mnesia table ~p: ~p",
+                                           [FeatureName, Tab, Err]),
+            %% adheres to the callback interface
             ok
     end.
