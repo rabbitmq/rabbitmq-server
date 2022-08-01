2c4c51d62dbdcca3c6a0ea5034458c190e31fbfb
Michael Klishin <klishinm@vmware.com>
2022-08-01 15:56:10 +0400

    ctl add_vhost: check if relevant feature flags are enabled

--- deps/rabbitmq_cli/lib/rabbitmq/cli/core/feature_flags.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/core/feature_flags.ex
@@ -5,10 +5,30 @@
 ## Copyright (c) 2007-2022 VMware, Inc. or its affiliates.  All rights reserved.
 
 defmodule RabbitMQ.CLI.Core.FeatureFlags do
+  alias RabbitMQ.CLI.Core.ExitCodes
 
   #
   # API
   #
+
+  def is_enabled_remotely(node_name, feature_flag) do
+    case :rabbit_misc.rpc_call(node_name, :rabbit_feature_flags, :is_enabled, [feature_flag]) do
+      true  -> true
+      false -> false
+      {:error, _} = error -> error
+    end
+  end
+
+  def assert_feature_flag_enabled(node_name, feature_flag, success_fun) do
+    case is_enabled_remotely(node_name, feature_flag) do
+      true  ->
+        success_fun.()
+      false ->
+        {:error, ExitCodes.exit_dataerr(), "The #{feature_flag} feature flag is not enabled on the target node"}
+      {:error, _} = error ->
+        error
+    end
+  end
 
   def feature_flag_lines(feature_flags) do
     feature_flags
