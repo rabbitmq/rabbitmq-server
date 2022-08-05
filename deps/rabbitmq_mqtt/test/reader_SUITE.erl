d4f05dce6afcc5c0a0672337c1b741807453aed0
Péter Gömöri <peter@84codes.com>
2022-08-05 15:42:52 +0200

    Add login_timeout to mqtt and stomp reader
    Similarly to handshake_timeout in amqp reader.
    
    (cherry picked from commit c4b7cd98bf8aea91fb2ce030d0f9d045c4b17be6)
    (cherry picked from commit f3c17ceee739f1140db8b645ae1560fcf02844ef)
    
    # Conflicts:
    #	deps/rabbitmq_mqtt/test/reader_SUITE.erl
    (cherry picked from commit 316225ad06523322928769f2d92ac01648c7d40d)
    
    # Conflicts:
    #	deps/rabbitmq_mqtt/test/reader_SUITE.erl
    (cherry picked from commit 3484e993a188078065733b08c99aa030eee4c5b4)

--- deps/rabbitmq_mqtt/test/reader_SUITE.erl
+++ deps/rabbitmq_mqtt/test/reader_SUITE.erl
@@ -14,8 +14,23 @@ groups() ->
       {non_parallel_tests, [], [
                                 block,
                                 handle_invalid_frames,
+<<<<<<< HEAD
                                 stats
+<<<<<<< HEAD
                                ]}
+=======
+                               ]},
+      {non_parallel_tests_quorum, [], [
+=======
+                                login_timeout,
+                                stats,
+>>>>>>> f3c17ceee7 (Add login_timeout to mqtt and stomp reader)
+                                quorum_session_false,
+                                quorum_session_true,
+                                classic_session_true,
+                                classic_session_false
+      ]}
+>>>>>>> 316225ad06 (Add login_timeout to mqtt and stomp reader)
     ].
 
 suite() ->
@@ -120,6 +135,17 @@ handle_invalid_frames(Config) ->
     gen_tcp:close(C),
     %% No new stats entries should be inserted as connection never got to initialize
     N = rpc(Config, ets, info, [connection_metrics, size]).
+
+login_timeout(Config) ->
+    rpc(Config, application, set_env, [rabbitmq_mqtt, login_timeout, 400]),
+    P = rabbit_ct_broker_helpers:get_node_config(Config, 0, tcp_port_mqtt),
+    {ok, C} = gen_tcp:connect("localhost", P, [{active, false}]),
+
+    try
+        {error, closed} = gen_tcp:recv(C, 0, 500)
+    after
+        rpc(Config, application, unset_env, [rabbitmq_mqtt, login_timeout])
+    end.
 
 stats(Config) ->
     P = rabbit_ct_broker_helpers:get_node_config(Config, 0, tcp_port_mqtt),
