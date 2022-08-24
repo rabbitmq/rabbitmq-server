0b400d1183beb7449c7093e024419820a913dfad
Iliia Khaprov <i.khaprov@gmail.com>
2022-08-24 10:35:22 +0200

    Update prometheus to 4.9.1
    (cherry picked from commit 4a1721509cc7c41654ed2a77c4564f3083b00b2e)
    (cherry picked from commit 1429d3afcdeab0cb37386abba5218c648941a2f7)
    
    # Conflicts:
    #	rabbitmq-components.mk

--- rabbitmq-components.mk
+++ rabbitmq-components.mk
@@ -115,8 +115,13 @@ dep_cowboy = hex 2.8.0
 dep_cowlib = hex 2.9.1
 dep_jsx = hex 3.1.0
 dep_looking_glass = git https://github.com/rabbitmq/looking_glass master
+<<<<<<< HEAD
 dep_prometheus = hex 4.9.0
 dep_ra = hex 2.2.0
+=======
+dep_prometheus = hex 4.9.1
+dep_ra = git https://github.com/rabbitmq/ra.git main
+>>>>>>> 1429d3afcd (Update prometheus to 4.9.1)
 dep_ranch = hex 2.1.0
 dep_recon = hex 2.5.2
 dep_redbug = hex 2.0.7
