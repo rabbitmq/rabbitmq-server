4d4f950275f40a1218c038030ba2de39fcbd9913
Ayanda Dube <ayanda.dube@multiverse.io>
2022-08-28 19:52:44 +0100

    Use local include to reference rabbit.hrl in test_event_handler
    (cherry picked from commit 80f21169350424116fcab9f8f8352d0322156f24)

--- deps/rabbit_common/test/test_event_handler.erl
+++ deps/rabbit_common/test/test_event_handler.erl
@@ -14,7 +14,7 @@
 -export([init/1, handle_call/2, handle_event/2, handle_info/2,
          terminate/2, code_change/3]).
 
--include_lib("rabbit_common/include/rabbit.hrl").
+-include("rabbit.hrl").
 
 
 %%
