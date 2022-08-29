da26dc8bd35e239dd17e1d679e0288c33b1be4a8
Marcial Rosales <mrosales@pivotal.io>
2022-08-29 18:21:58 +0100

    Fix logout workflow
    The issue was primarily that UAA was
    not properly configured. We had to whitelist
    the uri used for logout otherwise UAA redirects
    to its login page
    
    WIP verify that logout.js works when running in
    headless mode. For that we need a docker image
    and at the moment, make docker-image is not
    working because it is still using old otp 24.0.2
    
    (cherry picked from commit e57b88f2b7f8b0dfc783858a183b35f1604179b6)

--- deps/rabbitmq_management/selenium/test/oauth/with-uaa/setup.sh
+++ deps/rabbitmq_management/selenium/test/oauth/with-uaa/setup.sh
@@ -6,27 +6,6 @@ export RABBITMQ_CONFIG=${SCRIPT}/${RABBITMQ_CONFIG_FILE:-rabbitmq.config}
 export UAA_CONFIG=${SCRIPT}/${UAA_CONFIG_FOLDER:-uaa}
 
 . $SCRIPT/../../../bin/rabbitmq.sh
-
-start_uaa() {
-
-  UAA_IMAGE_TAG=${UAA_IMAGE_TAG:-75.21.0}
-  UAA_IMAGE_NAME=${UAA_IMAGE_NAME:-cloudfoundry/uaa}
-
-  docker network inspect rabbitmq_net >/dev/null 2>&1 || docker network create rabbitmq_net
-  docker rm -f uaa 2>/dev/null || echo "uaa was not running"
-
-  echo "Running ${UAA_IMAGE_NAME}:${UAA_IMAGE_TAG} docker image with .."
-
-  docker run \
-  		--detach \
-      --name uaa --net rabbitmq_net \
-  		--publish 8080:8080 \
-  		--mount "type=bind,source=${UAA_CONFIG},target=/uaa" \
-  		--env UAA_CONFIG_PATH="/uaa" \
-  		--env JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom" \
-      "${UAA_IMAGE_NAME}:${UAA_IMAGE_TAG}"
-
-}
-
 start_rabbitmq
-start_uaa
+
+. $SCRIPT/uaa.sh
