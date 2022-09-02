233a40897c91698fdad6f287196b97ddbea290a7
Michael Klishin <michael@clojurewerkz.org>
2022-09-03 01:37:22 +0400

    Resolve conflicts
    (cherry picked from commit f0ce37a4d0338a0efa72e7567f8ac557a981fc23)
    
    # Conflicts:
    #	deps/rabbitmq_cli/mix.exs

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -11,6 +11,7 @@ defmodule RabbitMQCtl.MixfileBase do
     [
       app: :rabbitmqctl,
 <<<<<<< HEAD
+<<<<<<< HEAD
       version: "3.10.0-dev",
 =======
 <<<<<<< HEAD
@@ -19,8 +20,10 @@ defmodule RabbitMQCtl.MixfileBase do
       elixir: ">= 1.10.4 and < 1.14.0",
 =======
       version: "3.8.0-dev",
+=======
+      version: "3.11.0-dev",
+>>>>>>> f0ce37a4d0 (Resolve conflicts)
       elixir: ">= 1.10.4 and < 1.15.0",
->>>>>>> 7914e60d6a (CLI tools: whitelist Elixir 1.14)
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       escript: [main_module: RabbitMQCtl,
