0e0635ffee15fbcf73c610d23471c17b75981d3b
Loïc Hoguin <lhoguin@vmware.com>
2022-09-08 15:41:46 +0200

    CQ: Make the resume/1 function sync to disk
    This just restores behavior that was there before via
    reduce_memory_use. I am not sure if it is of any use
    but it doesn't hurt to have it.

--- deps/rabbit/src/rabbit_variable_queue.erl
+++ deps/rabbit/src/rabbit_variable_queue.erl
@@ -931,7 +931,7 @@ handle_pre_hibernate(State = #vqstate { index_mod   = IndexMod,
                      unconfirmed_simple = sets:new([{version,2}]),
                      confirmed   = sets:union(C, UCS) }.
 
-resume(State) -> a(State).
+resume(State) -> a(timeout(State)).
 
 msg_rates(#vqstate { rates = #rates { in  = AvgIngressRate,
                                       out = AvgEgressRate } }) ->
