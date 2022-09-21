60c7f2a3225bdc215b8ba5e61735cda02211fa42
Marcial Rosales <mrosales@pivotal.io>
2022-09-21 13:13:16 +0200

    WIP verify 1 connection listed
    (cherry picked from commit f72226525f04d070cf8b30a3bba75eb6182d15d2)
    (cherry picked from commit 0848b5a5cf577756d09a41db85cb23fbc0675201)
    (cherry picked from commit b6dad4c7ee8bd77f80fdc325b3293749ba1cf262)

--- deps/rabbitmq_amqp1_0/test/command_SUITE.erl
+++ deps/rabbitmq_amqp1_0/test/command_SUITE.erl
@@ -10,40 +10,49 @@
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
--include_lib("amqp_client/include/amqp_client.hrl").
 -include("rabbit_amqp1_0.hrl").
 
 -define(COMMAND, 'Elixir.RabbitMQ.CLI.Ctl.Commands.ListAmqp10ConnectionsCommand').
 
 all() ->
     [
-     {group, non_parallel_tests}
+       {group, non_parallel_tests}
     ].
 
 groups() ->
     [
      {non_parallel_tests, [], [
                                merge_defaults,
-                               validate
+                               validate,
+                               when_no_connections,
+                               when_one_connection
                               ]}
     ].
 
 init_per_suite(Config) ->
-    Config1 = rabbit_ct_helpers:set_config(Config,
-                                           [{rmq_nodename_suffix, ?MODULE}]),
+    application:ensure_all_started(amqp10_client),
     rabbit_ct_helpers:log_environment(),
-    rabbit_ct_helpers:run_setup_steps(Config1,
-      rabbit_ct_broker_helpers:setup_steps()).
+    Config.
 
 end_per_suite(Config) ->
-    rabbit_ct_helpers:run_teardown_steps(Config,
-      rabbit_ct_broker_helpers:teardown_steps()).
-
-init_per_group(_, Config) ->
     Config.
+
+init_per_group(Group, Config) ->
+    Suffix = rabbit_ct_helpers:testcase_absname(Config, "", "-"),
+    Config1 = rabbit_ct_helpers:set_config(
+                Config, [
+                         {rmq_nodename_suffix, Suffix},
+                         {amqp10_client_library, Group}
+                        ]),
+    rabbit_ct_helpers:run_setup_steps(
+      Config1,
+      rabbit_ct_broker_helpers:setup_steps() ++
+      rabbit_ct_client_helpers:setup_steps()).
 
 end_per_group(_, Config) ->
-    Config.
+    rabbit_ct_helpers:run_teardown_steps(Config,
+      rabbit_ct_client_helpers:teardown_steps() ++
+      rabbit_ct_broker_helpers:teardown_steps()).
 
 init_per_testcase(Testcase, Config) ->
     rabbit_ct_helpers:testcase_started(Config, Testcase).
@@ -67,3 +76,34 @@ validate(_Config) ->
     ok = ?COMMAND:validate([atom_to_binary(K, utf8) || K <- ?INFO_ITEMS], #{}),
     {validation_failure,{bad_info_key,[other]}} =
         ?COMMAND:validate([<<"other">>], #{}).
+
+println(What, Value) -> io:format("~p : ~p~n", [What, Value]).
+
+when_no_connections(_Config) ->
+    [A] = rabbit_ct_broker_helpers:get_node_configs(_Config, nodename),
+    Opts = #{node => A, timeout => 2000, verbose => true},
+    [] = 'Elixir.Enum':to_list(?COMMAND:run([], Opts)).
+
+when_one_connection(_Config) ->
+    [A] = rabbit_ct_broker_helpers:get_node_configs(_Config, nodename),
+    Opts = #{node => A, timeout => 2000, verbose => true},
+
+    Connection = open_client_connection(_Config),
+    List = 'Elixir.Enum':to_list(?COMMAND:run([], Opts)),
+    println("connections", List),
+    close_client_connection(Connection).
+
+open_client_connection(_Config) ->
+  Host = ?config(rmq_hostname, _Config),
+  Port = rabbit_ct_broker_helpers:get_node_config(_Config, 0, tcp_port_amqp),
+  % create a configuration map
+  OpnConf = #{address => Host,
+              port => Port,
+              container_id => atom_to_binary(?FUNCTION_NAME, utf8),
+              sasl => {plain, <<"guest">>, <<"guest">>}},
+  {ok, Connection} = amqp10_client:open_connection(OpnConf),
+  {ok, _} = amqp10_client:begin_session(Connection),
+  Connection.
+
+close_client_connection(Connection) ->
+  ok = amqp10_client:close_connection(Connection).
