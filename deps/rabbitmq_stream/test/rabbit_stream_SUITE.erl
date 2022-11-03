7739718764e084c34e69062d268a85865a1d1b60
Karl Nilsson <kjnilsson@gmail.com>
2022-11-03 14:16:24 +0000

    Streams: close osiris log on unsubscribe
    To ensure we clean up any log resources such as counter records.
    
    (cherry picked from commit e4921503db200bc8fe5a80e272154d027ec89d47)

--- deps/rabbitmq_stream/test/rabbit_stream_SUITE.erl
+++ deps/rabbitmq_stream/test/rabbit_stream_SUITE.erl
@@ -144,7 +144,8 @@ end_per_testcase(_Test, _Config) ->
     ok.
 
 test_global_counters(Config) ->
-    test_server(gen_tcp, Config),
+    Stream = atom_to_binary(?FUNCTION_NAME, utf8),
+    test_server(gen_tcp, Stream, Config),
     ?assertEqual(#{publishers => 0,
                    consumers => 0,
                    messages_confirmed_total => 2,
@@ -174,11 +175,13 @@ test_global_counters(Config) ->
     ok.
 
 test_stream(Config) ->
-    test_server(gen_tcp, Config),
+    Stream = atom_to_binary(?FUNCTION_NAME, utf8),
+    test_server(gen_tcp, Stream, Config),
     ok.
 
 test_stream_tls(Config) ->
-    test_server(ssl, Config),
+    Stream = atom_to_binary(?FUNCTION_NAME, utf8),
+    test_server(ssl, Stream, Config),
     ok.
 
 test_gc_consumers(Config) ->
@@ -294,7 +297,7 @@ sac_ff(Config) ->
     C = rabbit_stream_core:init(0),
     test_peer_properties(gen_tcp, S, C),
     test_authenticate(gen_tcp, S, C),
-    Stream = <<"stream1">>,
+    Stream = atom_to_binary(?FUNCTION_NAME, utf8),
     test_create_stream(gen_tcp, S, Stream, C),
     test_declare_publisher(gen_tcp, S, 1, Stream, C),
     ?awaitMatch(#{publishers := 1}, get_global_counters(Config), ?WAIT),
@@ -401,7 +404,8 @@ get_node_name(Config) ->
 get_node_name(Config, Node) ->
     rabbit_ct_broker_helpers:get_node_config(Config, Node, nodename).
 
-test_server(Transport, Config) ->
+test_server(Transport, Stream, Config) ->
+    QName = rabbit_misc:r(<<"/">>, queue, Stream),
     Port =
         case Transport of
             gen_tcp ->
@@ -416,7 +420,6 @@ test_server(Transport, Config) ->
     C0 = rabbit_stream_core:init(0),
     C1 = test_peer_properties(Transport, S, C0),
     C2 = test_authenticate(Transport, S, C1),
-    Stream = <<"stream1">>,
     C3 = test_create_stream(Transport, S, Stream, C2),
     PublisherId = 42,
     ?assertMatch(#{publishers := 0}, get_global_counters(Config)),
@@ -429,9 +432,21 @@ test_server(Transport, Config) ->
     ?assertMatch(#{consumers := 0}, get_global_counters(Config)),
     C7 = test_subscribe(Transport, S, SubscriptionId, Stream, C6),
     ?awaitMatch(#{consumers := 1}, get_global_counters(Config), ?WAIT),
+    CounterKeys = maps:keys(get_osiris_counters(Config)),
+    %% find the counter key for the subscriber
+    {value, SubKey} = lists:search(fun ({rabbit_stream_reader, Q, Id, _}) ->
+                                           Q == QName andalso
+                                           Id == SubscriptionId;
+                                       (_) ->
+                                           false
+                                   end, CounterKeys),
     C8 = test_deliver(Transport, S, SubscriptionId, 0, Body, C7),
-    C9 = test_deliver(Transport, S, SubscriptionId, 1, Body, C8),
+    C8b = test_deliver(Transport, S, SubscriptionId, 1, Body, C8),
 
+    C9 = test_unsubscribe(Transport, S, SubscriptionId, C8b),
+
+    %% assert the counter key got removed after unsubscribe
+    ?assertNot(maps:is_key(SubKey, get_osiris_counters(Config))),
     %% exchange capabilities, which says we support deliver v2
     %% the connection should adapt its deliver frame accordingly
     C10 = test_exchange_command_versions(Transport, S, C9),
@@ -565,6 +580,18 @@ test_subscribe(Transport,
     ?assertMatch({response, 1, {subscribe, ?RESPONSE_CODE_OK}}, Cmd),
     C.
 
+test_unsubscribe(Transport,
+                 Socket,
+                 SubscriptionId, C0) ->
+    UnsubCmd = {request, 1,
+                {unsubscribe, SubscriptionId}},
+    UnsubscribeFrame = rabbit_stream_core:frame(UnsubCmd),
+    ok = Transport:send(Socket, UnsubscribeFrame ),
+    {Cmd, C} = receive_commands(Transport, Socket, C0),
+    ?assertMatch({response, 1, {unsubscribe, ?RESPONSE_CODE_OK}}, Cmd),
+    C.
+
+
 test_deliver(Transport, S, SubscriptionId, COffset, Body, C0) ->
     ct:pal("test_deliver ", []),
     {{deliver, SubscriptionId, Chunk}, C} =
@@ -675,6 +702,12 @@ receive_commands(Transport, S, C0) ->
             Res
     end.
 
+get_osiris_counters(Config) ->
+    rabbit_ct_broker_helpers:rpc(Config,
+                                 0,
+                                 osiris_counters,
+                                 overview,
+                                 []).
 get_global_counters(Config) ->
     maps:get([{protocol, stream}],
              rabbit_ct_broker_helpers:rpc(Config,
