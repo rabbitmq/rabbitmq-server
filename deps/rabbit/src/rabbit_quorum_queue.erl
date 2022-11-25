7ce20bcdfc4fdca0d1290a5df3be0379517e8b32
Péter Gömöri <peter@84codes.com>
2022-11-26 00:29:20 +0100

    Fix case_clause crash when stream queue provided for quorum queue cmds
    (cherry picked from commit 25ad13ce24a8e533a0d23a1dbd2a5230a8378607)

--- deps/rabbit/src/rabbit_quorum_queue.erl
+++ deps/rabbit/src/rabbit_quorum_queue.erl
@@ -1040,6 +1040,8 @@ status(Vhost, QueueName) ->
                          ]
                  end
              end || N <- Nodes];
+        {ok, _Q} ->
+            {error, not_quorum_queue};
         {error, not_found} = E ->
             E
     end.
@@ -1075,6 +1077,8 @@ add_member(VHost, Name, Node, Timeout) ->
                             add_member(Q, Node, Timeout)
                     end
             end;
+        {ok, _Q} ->
+            {error, not_quorum_queue};
         {error, not_found} = E ->
                     E
     end.
@@ -1130,6 +1134,8 @@ delete_member(VHost, Name, Node) ->
                 true ->
                     delete_member(Q, Node)
             end;
+        {ok, _Q} ->
+            {error, not_quorum_queue};
         {error, not_found} = E ->
                     E
     end.
@@ -1287,6 +1293,8 @@ reclaim_memory(Vhost, QueueName) ->
         {ok, Q} when ?amqqueue_is_quorum(Q) ->
             ok = ra:pipeline_command(amqqueue:get_pid(Q),
                                      rabbit_fifo:make_garbage_collection());
+        {ok, _Q} ->
+            {error, not_quorum_queue};
         {error, not_found} = E ->
             E
     end.
@@ -1550,7 +1558,9 @@ peek(Pos, Q) when ?is_amqqueue(Q) andalso ?amqqueue_is_quorum(Q) ->
             Err
     end;
 peek(_Pos, Q) when ?is_amqqueue(Q) andalso ?amqqueue_is_classic(Q) ->
-    {error, classic_queue_not_supported}.
+    {error, classic_queue_not_supported};
+peek(_Pos, Q) when ?is_amqqueue(Q) ->
+    {error, not_quorum_queue}.
 
 online(Q) when ?is_amqqueue(Q) ->
     Nodes = get_connected_nodes(Q),
