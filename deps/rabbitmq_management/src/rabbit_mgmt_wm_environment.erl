32097035dcc55527d706fdc0e109b93db90c15be
Simon Unge <simunge@amazon.com>
2022-12-11 20:38:53 -0800

    See #6016. Add HTTP to fetch app environment config

---
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_environment.erl
@@ -0,0 +1,33 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2022 VMware, Inc. or its affiliates.  All rights reserved.
+%%
+
+-module(rabbit_mgmt_wm_environment).
+
+-export([init/2, to_plain/2, content_types_provided/2, is_authorized/2]).
+-export([variances/2]).
+
+-include_lib("rabbitmq_management_agent/include/rabbit_mgmt_records.hrl").
+-include_lib("rabbit_common/include/rabbit.hrl").
+
+%%--------------------------------------------------------------------
+
+init(Req, _State) ->
+    {cowboy_rest, rabbit_mgmt_headers:set_common_permission_headers(Req, ?MODULE), #context{}}.
+
+variances(Req, Context) ->
+    {[<<"accept-encoding">>, <<"origin">>], Req, Context}.
+
+content_types_provided(ReqData, Context) ->
+    {[{{ <<"text">>, <<"plain">>, '*'}, to_plain}], ReqData, Context}.
+
+to_plain(ReqData, Context) ->
+    Res = rabbit:environment(),
+    Req = cowboy_req:reply(200, #{}, io_lib:format("~p~n",[Res]), ReqData),
+    {stop, Req, Context}.
+
+is_authorized(ReqData, Context) ->
+    rabbit_mgmt_util:is_authorized_admin(ReqData, Context).
