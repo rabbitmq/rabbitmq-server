36728042ecc6c87577d9564ed0dc58cd232a13c6
Marcial Rosales <mrosales@pivotal.io>
2022-12-15 20:09:17 +0100

    Test with selenium export/import definitions

---
+++ deps/rabbitmq_management/selenium/test/definitions/import.js
@@ -0,0 +1,36 @@
+const { By, Key, until, Builder } = require('selenium-webdriver')
+require('chromedriver')
+const assert = require('assert')
+const { buildDriver, goToHome, captureScreensFor, teardown, delay } = require('../utils')
+
+const LoginPage = require('../pageobjects/LoginPage')
+const OverviewPage = require('../pageobjects/OverviewPage')
+
+describe('Import definititions', function () {
+  let login
+  let overview
+  let captureScreen
+
+  before(async function () {
+    driver = buildDriver()
+    await goToHome(driver)
+    login = new LoginPage(driver)
+    overview = new OverviewPage(driver)
+    captureScreen = captureScreensFor(driver, __filename)
+
+    await login.login('guest', 'guest')
+    if (!await overview.isLoaded()) {
+      throw new Error('Failed to login')
+    }
+  })
+
+  it('is allowed to administrator users', async function () {
+    let message = await overview.uploadBrokerDefinitions(process.cwd() + "/test/definitions/import-newguest-user.json")
+    assert.equal(true, message.indexOf('Your definitions were imported successfully.') !== -1)
+  })
+
+
+  after(async function () {
+    await teardown(driver, this, captureScreen)
+  })
+})
