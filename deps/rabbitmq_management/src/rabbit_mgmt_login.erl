c25c6f6950493bdcc5f35b2186bd37b060776646
Marcial Rosales <mrosales@pivotal.io>
2022-12-16 15:56:54 +0100

    Refactor code following recommendations
    from @essen
    
    (cherry picked from commit e8fc9af13aab392b04008e836d32f5f54870f9b6)

--- deps/rabbitmq_management/src/rabbit_mgmt_login.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_login.erl
@@ -15,19 +15,17 @@
 %%--------------------------------------------------------------------
 
 init(Req0, State) ->
+  login(cowboy_req:method(Req0), Req0, State).
+
+login(<<"POST">>, Req0, State) ->
   {ok, Body, _} = cowboy_req:read_urlencoded_body(Req0),
   AccessToken = proplists:get_value(<<"access_token">>, Body),
   case rabbit_mgmt_util:is_authorized_user(Req0, #context{}, <<"">>, AccessToken, false) of
     {true, Req1, _} ->
-      Req2 = cowboy_req:stream_reply(200, Req1),
-      cowboy_req:stream_body("<html><head></head>", nofin, Req2),
-      cowboy_req:stream_body("<body>", nofin, Req2),
-      cowboy_req:stream_body("<script src='js/prefs.js'></script>", nofin, Req2),
-      cowboy_req:stream_body("<script type='text/javascript'>", nofin, Req2),
-      cowboy_req:stream_body("set_token_auth('", nofin, Req2),
-      cowboy_req:stream_body(AccessToken, nofin, Req2),
-      cowboy_req:stream_body("'); window.location = '" ++ rabbit_mgmt_util:get_path_prefix() ++ "/'", nofin, Req2),
-      cowboy_req:stream_body("</script></body></html>", fin, Req2),
+      NewBody = ["<html><head></head><body><script src='js/prefs.js'></script><script type='text/javascript'>",
+          "set_token_auth('", AccessToken, "'); window.location = '", rabbit_mgmt_util:get_path_prefix(),
+          "/'</script></body></html>"],
+      Req2 = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html; charset=utf-8">>}, NewBody, Req1),
       {ok, Req2, State};
     {false, ReqData1, Reason} ->
       Home = cowboy_req:uri(ReqData1, #{path => rabbit_mgmt_util:get_path_prefix() ++ "/", qs => "error=" ++ Reason}),
@@ -35,4 +33,8 @@ init(Req0, State) ->
               #{<<"Location">> => iolist_to_binary(Home) },
               <<>>, ReqData1),
       {ok, ReqData2, State}
-  end.
+  end;
+
+login(_, Req0, _) ->
+  %% Method not allowed.
+	cowboy_req:reply(405, Req0).
