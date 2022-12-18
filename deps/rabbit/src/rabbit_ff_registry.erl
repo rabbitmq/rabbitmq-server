bddbc0ba7b482eab0a644a361d14d9ed1cc7e701
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2022-12-18 19:15:57 +0100

    rabbit_feature_flags: Initialize registry in `rabbit_ff_registry:inventory/0`
    With the way the registry is used in the feature flags controller, the
    registry was never initialized if `feature_flags_v2` was enabled
    out-of-the-box (i.e. once marked as required).
    
    The bug was discovered after marking `feature_flags_v2` as required and
    removing the v1 code: the `feature_flags_v2_SUITE` testsuite failed
    because the controller got an empty inventory when it called
    `rabbit_ff_registry:inventory()`.

--- deps/rabbit/src/rabbit_ff_registry.erl
+++ deps/rabbit/src/rabbit_ff_registry.erl
@@ -165,9 +165,11 @@ is_registry_written_to_disk() ->
 -spec inventory() -> rabbit_feature_flags:inventory().
 
 inventory() ->
-    #{applications => [],
-      feature_flags => #{},
-      states => #{}}.
+    rabbit_ff_registry_factory:initialize_registry(),
+    Inventory = #{applications => [],
+                  feature_flags => #{},
+                  states => #{}},
+    ?convince_dialyzer(?MODULE:inventory(), Inventory, Inventory).
 
 always_return_true() ->
     %% This function is here to trick Dialyzer. We want some functions
