83cc0664d7e200ab315d1b6eabb07a769f891c9f
Michael Klishin <klishinm@vmware.com>
2023-01-11 22:09:30 -0600

    diagnostics check_port_connectivity: support --address
    For scenarios where node hostname resolution on the invoking
    host is not a suitable option and a particular address
    should be tried instead.
    
    Closes #6853.
    
    (cherry picked from commit 226a7603e81734882308364ca4ce668b167caa60)

--- deps/rabbitmq_cli/lib/rabbitmq/cli/diagnostics/diagnostics_helpers.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/diagnostics/diagnostics_helpers.ex
@@ -5,20 +5,31 @@
 ## Copyright (c) 2007-2023 VMware, Inc. or its affiliates.  All rights reserved.
 
 defmodule RabbitMQ.CLI.Diagnostics.Helpers do
-  def test_connection(hostname, port, timeout) do
-    case :gen_tcp.connect(hostname, port, [], timeout) do
-      {:error, _} -> :gen_tcp.connect(hostname, port, [:inet6], timeout)
+  def test_connection(hostname_or_ip, port, timeout) do
+    hostname_as_list = :rabbit_data_coercion.to_list(hostname_or_ip)
+    case :gen_tcp.connect(hostname_as_list, port, [], timeout) do
+      {:error, _} -> :gen_tcp.connect(hostname_as_list, port, [:inet6], timeout)
       r -> r
     end
   end
 
   def check_port_connectivity(port, node_name, timeout) do
+    check_port_connectivity(port, node_name, nil, timeout)
+  end
+
+
+  def check_port_connectivity(port, node_name, nil, timeout) do
     regex = Regex.recompile!(~r/^(.+)@/)
     hostname = Regex.replace(regex, to_string(node_name), "") |> to_charlist
 
+    check_port_connectivity(port, node_name, hostname, timeout)
+  end
+  def check_port_connectivity(port, node_name, hostname_or_ip, timeout) do
     try do
-      case test_connection(hostname, port, timeout) do
-        {:error, _} ->
+      IO.puts("Will connect to #{hostname_or_ip}:#{port}")
+      case test_connection(hostname_or_ip, port, timeout) do
+        {:error, err} ->
+          IO.puts("Error connecting to #{hostname_or_ip}:#{port}: #{err}")
           false
 
         {:ok, port} ->
@@ -33,7 +44,7 @@ defmodule RabbitMQ.CLI.Diagnostics.Helpers do
     end
   end
 
-  def check_listener_connectivity(%{port: port}, node_name, timeout) do
-    check_port_connectivity(port, node_name, timeout)
+  def check_listener_connectivity(%{port: port}, node_name, target_ip, timeout) do
+    check_port_connectivity(port, node_name, target_ip, timeout)
   end
 end
