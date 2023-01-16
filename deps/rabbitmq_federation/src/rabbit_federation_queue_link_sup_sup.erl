232a6e1a5a67e643f6c270b32521c50b79000416
Alexey Lebedeff <binarin@binarin.ru>
2023-01-16 17:27:06 +0100

    Fix all dialyzer warnings in rabbitmq_federation
    (cherry picked from commit a1ee10bbf689c4bf44db09d4645b31a64aee0fa5)
    (cherry picked from commit e63803cf5f8453223cb21f0b7a336443c57fbc9a)

--- deps/rabbitmq_federation/src/rabbit_federation_queue_link_sup_sup.erl
+++ deps/rabbitmq_federation/src/rabbit_federation_queue_link_sup_sup.erl
@@ -26,7 +26,7 @@ start_link() ->
     %% This scope is used by concurrently starting exchange and queue links,
     %% and other places, so we have to start it very early outside of the supervision tree.
     %% The scope is stopped in stop/1.
-    rabbit_federation_pg:start_scope(),
+    _ = rabbit_federation_pg:start_scope(),
     mirrored_supervisor:start_link({local, ?SUPERVISOR}, ?SUPERVISOR,
                                    fun rabbit_misc:execute_mnesia_transaction/1,
                                    ?MODULE, []).
@@ -51,13 +51,13 @@ start_child(Q) ->
 
 
 adjust({clear_upstream, VHost, UpstreamName}) ->
-    [rabbit_federation_link_sup:adjust(Pid, Q, {clear_upstream, UpstreamName}) ||
-        {Q, Pid, _, _} <- mirrored_supervisor:which_children(?SUPERVISOR),
-        ?amqqueue_vhost_equals(Q, VHost)],
+    _ = [rabbit_federation_link_sup:adjust(Pid, Q, {clear_upstream, UpstreamName}) ||
+            {Q, Pid, _, _} <- mirrored_supervisor:which_children(?SUPERVISOR),
+            ?amqqueue_vhost_equals(Q, VHost)],
     ok;
 adjust(Reason) ->
-    [rabbit_federation_link_sup:adjust(Pid, Q, Reason) ||
-        {Q, Pid, _, _} <- mirrored_supervisor:which_children(?SUPERVISOR)],
+    _ = [rabbit_federation_link_sup:adjust(Pid, Q, Reason) ||
+            {Q, Pid, _, _} <- mirrored_supervisor:which_children(?SUPERVISOR)],
     ok.
 
 stop_child(Q) ->
