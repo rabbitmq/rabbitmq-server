521a1742d90f902541049c5ba2e7d9812bfd81fc
Alexey Lebedeff <binarin@binarin.ru>
2023-01-18 18:01:16 +0100

    Fix all dialyzer warnings in rabbitmq_management
    (cherry picked from commit 1865284ab3cc05768566b64ecab4122201747353)
    (cherry picked from commit 0a892e713933287e4e30f887959c5b0ed00a52d7)

--- deps/rabbitmq_management/src/rabbit_mgmt_util.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_util.erl
@@ -846,8 +846,6 @@ decode(Body) ->
         case rabbit_json:decode(Body) of
             Val when is_map(Val) ->
                 {ok, Val};
-            Val when is_atom(Val) ->
-                {ok, #{}};
             %% handle double encoded JSON, see rabbitmq/rabbitmq-management#839
             Bin when is_binary(Bin) ->
                 {error, "invalid payload: the request body JSON-decoded to a string. "
@@ -1218,7 +1216,7 @@ int(Name, ReqData) ->
 def(undefined, Def) -> Def;
 def(V, _) -> V.
 
--spec qs_val(binary(), cowboy:req()) -> any() | undefined.
+-spec qs_val(binary(), cowboy_req:req()) -> any() | undefined.
 qs_val(Name, ReqData) ->
     Qs = cowboy_req:parse_qs(ReqData),
     proplists:get_value(Name, Qs, undefined).
