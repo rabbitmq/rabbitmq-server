b4178088c368903e5a118d9668f3012d85f97a82
Marcial Rosales <mrosales@pivotal.io>
2023-01-27 19:00:08 +0100

    Fixes a memory leak in AMQP 1.0 plugin (#7015)
    * Fixes #6969 by
    
     * tracking channels using a map rather than
    process dictionary
     * modifying Program.cs so that it opens 5
    simulatneous sessions to send messages before
    closing them and repeat again
    
    * Address PR feedback: formatting, code style
    
    (cherry picked from commit 6806a9a45e1e2aa7ce8806c8ca5c4ee14dfef02e)
    (cherry picked from commit 59b595d519386dd4e19edb9364c3b415586a56fd)
    (cherry picked from commit 7decdc535ae3c5928e313b8467f30cf63617a5e9)
    
    # Conflicts:
    #	deps/rabbitmq_amqp1_0/test/system_SUITE_data/console/Program.cs

---
+++ deps/rabbitmq_amqp1_0/test/system_SUITE_data/console/Program.cs
@@ -0,0 +1,57 @@
+﻿using Amqp;
+using Amqp.Framing;
+
+// See https://aka.ms/new-console-template for more information
+
+// Create the AMQP connection string
+var connectionString = $"amqp://guest:guest@localhost:5672/%2F";
+
+// Create the AMQP connection
+var connection = new Connection(new Address(connectionString));
+
+
+for (var i = 0; i < 10; i++)
+{
+    Session[] sessions = new Session[5];
+    SenderLink[] senders = new SenderLink[5];
+
+    for (var j = 0; j < 5; j++)
+    {
+      // Create the AMQP session
+      sessions[j] = new Session(connection);
+
+        // Give a name to the sender
+      var senderSubscriptionId = "rabbitmq.amqp.sender";
+
+      // Name of the topic you will be sending messages (Name of the Queue)
+      var topic = "amqp10-queue";
+
+      // Create the AMQP sender
+      senders[j] = new SenderLink(sessions[j], senderSubscriptionId, topic);
+
+      // Create message
+      var message = new Message($"Received message {i}");
+
+      // Add a meesage id
+      message.Properties = new Properties() { MessageId = Guid.NewGuid().ToString() };
+
+      // Add some message properties
+      message.ApplicationProperties = new ApplicationProperties();
+      message.ApplicationProperties["Message.Type.FullName"] = typeof(string).FullName;
+
+      // Send message
+      senders[j].Send(message);
+      Console.WriteLine("Sent message " + (i+1) + "/10 to amqp10-queue from session " + j + ". Press a button to send next message");
+    }
+     // Wait for a key to close last sessions
+    Console.Read();
+
+    for (var j = 0; j < 5; j++)
+    {
+      senders[j].Close();
+      sessions[j].Close();
+    }
+}
+
+Console.WriteLine("Sent all messages. Press a button to close the connection");
+connection.Close();
