0bd5b7ce89859a08c8b0f89d71daa0fb088ed144
Marcial Rosales <mrosales@pivotal.io>
2023-03-21 12:39:28 +0100

    Refactor selenium tests
    (cherry picked from commit 67b952c28c22894d7f1aa7c4811b12eeebe7e224)
    (cherry picked from commit eef543d6a4c09ff0f88dafefd425c0a228c10300)

--- deps/rabbitmq_management/selenium/suites/oauth-with-uaa-and-mgt-prefix.sh
+++ deps/rabbitmq_management/selenium/suites/oauth-with-uaa-and-mgt-prefix.sh
@@ -2,9 +2,9 @@
 
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-TEST_CASES_PATH=/oauth/with-idp-initiated
+TEST_CASES_PATH=/oauth/with-sp-initiated
 TEST_CONFIG_PATH=/oauth
-PROFILES="uaa fakeportal-oauth-provider idp-initiated"
+PROFILES="uaa uaa-oauth-provider mgt-prefix"
 
 source $SCRIPT/../bin/suite_template
-runWith uaa fakeportal
+runWith uaa
