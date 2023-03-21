52283818458be7ed7711742db26d1e80d55604f3
Marcial Rosales <mrosales@pivotal.io>
2023-03-21 12:39:28 +0100

    Refactor selenium tests
    (cherry picked from commit 67b952c28c22894d7f1aa7c4811b12eeebe7e224)
    (cherry picked from commit eef543d6a4c09ff0f88dafefd425c0a228c10300)
    (cherry picked from commit 0bd5b7ce89859a08c8b0f89d71daa0fb088ed144)

--- deps/rabbitmq_management/selenium/test/oauth/test.sh
+++
@@ -1,5 +0,0 @@
- curl 'http://localhost:8080/oauth/token' -i -X POST \
-    -H 'Content-Type: application/x-www-form-urlencoded' \
-    -H 'Accept: application/json' \
-    -d 'scope=rabbitmq&client_id=rabbit_client_code&client_secret=rabbit_client_code&grant_type=client_credentials&token_format=jwt&response_type=token'
-
