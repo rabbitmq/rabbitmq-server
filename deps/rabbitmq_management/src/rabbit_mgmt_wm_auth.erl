1209b866718a379eada6f204208c7ccb1426442a
Michael Klishin <klishinm@vmware.com>
2023-04-14 19:02:08 +0400

    Remove an old version of rabbit_mgmt_wm_auth
    rabbit_mgmt_oauth_bootstrap is not hooked up to the dispatcher,
    and appears to be an older version of what is now rabbit_mgmt_wm_auth

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_auth.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_auth.erl
@@ -65,52 +65,8 @@ authSettings() ->
         [{oauth_enabled, false}]
   end.
 
-to_json(ReqData, Context) ->
-    EnableUAA = application:get_env(rabbitmq_management, enable_uaa, false),
-    EnableOAUTH = application:get_env(rabbitmq_management, oauth_enabled, false),
-    Data = case EnableOAUTH of
-      true ->
-          OAuthInitiatedLogonType = application:get_env(rabbitmq_management, oauth_initiated_logon_type, sp_initiated),
-          OAuthProviderUrl = application:get_env(rabbitmq_management, oauth_provider_url, ""),
-
-          case OAuthInitiatedLogonType of
-            sp_initiated ->
-              OAuthClientId = application:get_env(rabbitmq_management, oauth_client_id, ""),
-              OAuthClientSecret = application:get_env(rabbitmq_management, oauth_client_secret, ""),
-              OAuthMetadataUrl = application:get_env(rabbitmq_management, oauth_metadata_url, ""),
-              OAuthScopes = application:get_env(rabbitmq_management, oauth_scopes, ""),
-              OAuthResourceId = application:get_env(rabbitmq_auth_backend_oauth2, resource_server_id, ""),
-              case is_invalid([OAuthResourceId]) of
-                  true ->
-                      rabbit_log:warning("Disabling OAuth 2 authorization, missing resource_server_id in oauth2 plugin", []),
-                       [{oauth_enabled, false}];
-                  false ->
-                      case is_invalid([OAuthClientId, OAuthProviderUrl]) of
-                          true ->
-                              rabbit_log:warning("Disabling OAuth 2 authorization, missing relevant configuration in management plugin", []),
-                              [{oauth_enabled, false}, {oauth_client_id, <<>>}, {oauth_provider_url, <<>>}];
-                          false ->
-                              append_oauth_optional_secret([
-                               {oauth_enabled, true},
-                               {enable_uaa, rabbit_data_coercion:to_binary(EnableUAA)},
-                               {oauth_client_id, rabbit_data_coercion:to_binary(OAuthClientId)},
-                               {oauth_provider_url, rabbit_data_coercion:to_binary(OAuthProviderUrl)},
-                               {oauth_scopes, rabbit_data_coercion:to_binary(OAuthScopes)},
-                               {oauth_metadata_url, rabbit_data_coercion:to_binary(OAuthMetadataUrl)},
-                               {oauth_resource_id, rabbit_data_coercion:to_binary(OAuthResourceId)}
-                               ], OAuthClientSecret)
-                      end
-              end;
-            idp_initiated ->
-               [{oauth_enabled, true},
-                {oauth_initiated_logon_type, rabbit_data_coercion:to_binary(OAuthInitiatedLogonType)},
-                {oauth_provider_url, rabbit_data_coercion:to_binary(OAuthProviderUrl)}
-                ]
-            end;
-       false ->
-           [{oauth_enabled, false}]
-     end,
-     rabbit_mgmt_util:reply(Data, ReqData, Context).
+  to_json(ReqData, Context) ->
+    rabbit_mgmt_util:reply(authSettings(), ReqData, Context).
 
 is_authorized(ReqData, Context) ->
     {true, ReqData, Context}.
