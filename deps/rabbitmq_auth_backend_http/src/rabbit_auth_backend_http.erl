77c8ca951d875af8ecb2a50fdc597fa634b1bc24
Luke Bakken <luke@bakken.io>
2023-04-24 11:32:16 -0700

    Make dialyzer happy by ensuring `rabbit_data_coercion:to_list/1` can handle `inet:ip_address()`
    (cherry picked from commit 6c39f6812a90070089b37137cbb61734afde5ec1)

--- deps/rabbitmq_auth_backend_http/src/rabbit_auth_backend_http.erl
+++ deps/rabbitmq_auth_backend_http/src/rabbit_auth_backend_http.erl
@@ -200,7 +200,7 @@ parse_peeraddr(unknown) ->
 parse_peeraddr(PeerAddr) ->
     handle_inet_ntoa_peeraddr(inet:ntoa(PeerAddr), PeerAddr).
 
--spec handle_inet_ntoa_peeraddr({'error', term()} | string(), inet:ip_address()) -> string().
+-spec handle_inet_ntoa_peeraddr({'error', term()} | string(), inet:ip_address() | unknown) -> string().
 handle_inet_ntoa_peeraddr({error, einval}, PeerAddr) ->
     rabbit_data_coercion:to_list(PeerAddr);
 handle_inet_ntoa_peeraddr(PeerAddrStr, _PeerAddr0) ->
