57917e1ddfdd734d6461fa7d4bef623891f791a0
Rin Kuryloski <kuryloskip@vmware.com>
2023-05-16 16:48:18 +0200

    Check additional applications when comparing bazel and make results

--- tools/compare_dist.sh
+++ tools/compare_dist.sh
@@ -18,15 +18,15 @@ plugins_rel=rabbitmq_server-${VERSION}/plugins
 diff <(grep $plugins_rel make.manifest | grep -v ".ez") <(grep $plugins_rel bazel.manifest | grep -v ".ez")
 
 echo "Plugins exist with same version and deps"
-for p in ${PLUGINS}; do
+for p in ${PLUGINS} ${EXTRA_PLUGINS}; do
     echo "$p"
     f="$(cd $GOLDEN && ls -d $plugins_rel/$p-*)"
     test -f $GOLDEN/$f/ebin/$p.app || (echo "$GOLDEN/$f/ebin/$p.app does not exist"; exit 1)
     test -d $SECOND/$f || (echo "$SECOND/$f does not exist"; exit 1)
     test -f $SECOND/$f/ebin/$p.app || (echo "$SECOND/$f/ebin/$p.app does not exist"; exit 1)
     ./rabbitmq-server/tools/erlang_app_equal \
-    $GOLDEN/$f/ebin/$p.app \
-    $SECOND/$f/ebin/$p.app
+        $GOLDEN/$f/ebin/$p.app \
+        $SECOND/$f/ebin/$p.app
 done
 
 echo "Both have escript"
