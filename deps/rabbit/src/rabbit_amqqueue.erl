deffbdae89474af6a7c8a89e1d0829ff8b86bd6e
Michael Klishin <klishinm@vmware.com>
2023-06-18 16:07:43 +0400

    Closes #8564
    rabbit_amqqueue:list* functions: take queue type into account when
    determining if a queue is local to a set of nodes.
    
    (cherry picked from commit 499a7c7755ca58fd3ff5b75b46e6c91e9041f8ae)
    (cherry picked from commit fab3130e475f889b510df16d5f3f79cdb76b588a)
    
    # Conflicts:
    #	deps/rabbit/src/rabbit_amqqueue.erl

--- deps/rabbit/src/rabbit_amqqueue.erl
+++ deps/rabbit/src/rabbit_amqqueue.erl
@@ -47,6 +47,7 @@
          list_local_mirrored_classic_without_synchronised_mirrors_for_cli/0,
          list_local_quorum_queues_with_name_matching/1,
          list_local_quorum_queues_with_name_matching/2]).
+-export([is_local_to_node/2, is_local_to_node_set/2]).
 -export([ensure_rabbit_queue_record_is_initialized/1]).
 -export([format/1]).
 -export([delete_immediately_by_resource/1]).
@@ -1151,7 +1152,7 @@ do_list() ->
     lists:filter(fun (Q) ->
                          Pid = amqqueue:get_pid(Q),
                          St = amqqueue:get_state(Q),
-                         St =/= stopped orelse lists:member(node(Pid), NodesRunning)
+                         St =/= stopped orelse is_local_to_node_set(Pid, NodesRunning)
                  end, All).
 
 -spec count() -> non_neg_integer().
@@ -1327,6 +1328,11 @@ is_local_to_node({_, Leader} = QPid, Node) when ?IS_QUORUM(QPid) ->
 is_local_to_node(_QPid, _Node) ->
     false.
 
+is_local_to_node_set(QPid, Nodes) when is_list(Nodes) ->
+  lists:any(fun(Node) -> is_local_to_node(QPid, Node) end, Nodes);
+is_local_to_node_set(QPid, OneNode) when is_atom(OneNode) ->
+  is_local_to_node(QPid, OneNode).
+
 is_in_virtual_host(Q, VHostName) ->
     VHostName =:= get_resource_vhost_name(amqqueue:get_name(Q)).
 
@@ -1337,7 +1343,7 @@ list(VHostPath) ->
     lists:filter(fun (Q) ->
                          Pid = amqqueue:get_pid(Q),
                          St = amqqueue:get_state(Q),
-                         St =/= stopped orelse lists:member(node(Pid), NodesRunning)
+                         St =/= stopped orelse is_local_to_node_set(Pid, NodesRunning)
                  end, All).
 
 list(VHostPath, TableName) ->
@@ -1393,6 +1399,7 @@ list_down(VHostPath) ->
         false -> [];
         true  ->
             Alive = sets:from_list([amqqueue:get_name(Q) || Q <- list(VHostPath)]),
+<<<<<<< HEAD
             Durable = list(VHostPath, rabbit_durable_queue),
             NodesRunning = rabbit_nodes:all_running(),
             lists:filter(fun (Q) ->
@@ -1403,6 +1410,20 @@ list_down(VHostPath) ->
                                  orelse
                                  (not sets:is_element(N, Alive))
                          end, Durable)
+=======
+            NodesRunning = rabbit_nodes:list_running(),
+            rabbit_db_queue:filter_all_durable(
+              fun (Q) ->
+                      N = amqqueue:get_name(Q),
+                      Pid = amqqueue:get_pid(Q),
+                      St = amqqueue:get_state(Q),
+                      amqqueue:get_vhost(Q) =:= VHostPath
+                          andalso
+                            ((St =:= stopped andalso not is_local_to_node_set(Pid, NodesRunning))
+                             orelse
+                               (not sets:is_element(N, Alive)))
+              end)
+>>>>>>> fab3130e47 (Closes #8564)
     end.
 
 count(VHost) ->
