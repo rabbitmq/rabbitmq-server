7ca66b6d955a7251e83602aaf855ec38d0879363
Iliia Khaprov <i.khaprov@gmail.com>
2023-06-20 01:03:53 +0200

    fixup! reenable main tests

--- deps/rabbitmq_stomp/test/python_SUITE_data/src/main_runner.py
+++ deps/rabbitmq_stomp/test/python_SUITE_data/src/main_runner.py
@@ -4,20 +4,20 @@ import test_runner
 
 if __name__ == '__main__':
     modules = [
-        'parsing'# ,
-        # 'errors',
-        # 'connect_disconnect',
-        # 'ack',
-        # 'amqp_headers',
-        # 'queue_properties',
-        # 'reliability',
-        # 'transactions',
-        # 'x_queue_name',
-        # 'destinations',
-        # 'redelivered',
-        # 'topic_permissions',
-        # 'unsubscribe',
-        # 'x_queue_type_quorum',
-        # 'x_queue_type_stream'
+        'parsing',
+        'errors',
+        'connect_disconnect',
+        'ack',
+        'amqp_headers',
+        'queue_properties',
+        'reliability',
+        'transactions',
+        'x_queue_name',
+        'destinations',
+        'redelivered',
+        'topic_permissions',
+        'unsubscribe',
+        'x_queue_type_quorum',
+        'x_queue_type_stream'
     ]
     test_runner.run_unittests(modules)
