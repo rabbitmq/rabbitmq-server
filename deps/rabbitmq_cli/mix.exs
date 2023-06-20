7ecb79c462e2733d98cd546237aab49eeb61125a
Michael Klishin <klishinm@vmware.com>
2023-06-21 03:03:33 +0400

    CLI: allow for Elixir 1.15
    (cherry picked from commit d3e01675b1968dc99d2aed482bc74c084bf811b2)

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -11,7 +11,7 @@ defmodule RabbitMQCtl.MixfileBase do
     [
       app: :rabbitmqctl,
       version: "3.11.0",
-      elixir: ">= 1.13.4 and < 1.15.0",
+      elixir: ">= 1.13.4 and < 1.16.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       escript: [main_module: RabbitMQCtl, emu_args: "-hidden", path: "escript/rabbitmqctl"],
