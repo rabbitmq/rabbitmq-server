7be8a53be18f3af8aaebe2d1bf4815ead805581e
David Ansari <david.ansari@gmx.de>
2023-06-27 06:53:26 +0000

    Include error reason in shovel log
    Ignoring exception class and pattern makes debugging hard.
    
    (cherry picked from commit 58360b532f300a18558884ad370182ecaceecb29)

--- deps/rabbitmq_shovel/src/rabbit_shovel_worker.erl
+++ deps/rabbitmq_shovel/src/rabbit_shovel_worker.erl
@@ -70,8 +70,8 @@ handle_cast(init, State = #state{config = Config0}) ->
         %% rabbitmq/rabbitmq-shovel#54 for context.
         gen_server2:cast(self(), connect_dest),
         {noreply, State#state{config = Config}}
-    catch _:_ ->
-      rabbit_log_shovel:error("Shovel ~ts could not connect to source", [human_readable_name(maps:get(name, Config0))]),
+    catch E:R ->
+      rabbit_log_shovel:error("Shovel ~ts could not connect to source: ~p ~p", [human_readable_name(maps:get(name, Config0)), E, R]),
       {stop, shutdown, State}
     end;
 handle_cast(connect_dest, State = #state{config = Config0}) ->
@@ -80,8 +80,8 @@ handle_cast(connect_dest, State = #state{config = Config0}) ->
         rabbit_log_shovel:debug("Shovel ~ts connected to destination", [human_readable_name(maps:get(name, Config))]),
         gen_server2:cast(self(), init_shovel),
         {noreply, State#state{config = Config}}
-    catch _:_ ->
-      rabbit_log_shovel:error("Shovel ~ts could not connect to destination", [human_readable_name(maps:get(name, Config0))]),
+    catch E:R ->
+      rabbit_log_shovel:error("Shovel ~ts could not connect to destination: ~p ~p", [human_readable_name(maps:get(name, Config0)), E, R]),
       {stop, shutdown, State}
     end;
 handle_cast(init_shovel, State = #state{config = Config}) ->
