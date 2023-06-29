daa2b724c4bce28c26d16d9ec76ebd178fdf2b6d
markus812498 <markus@84codes.com>
2023-06-29 12:03:50 +1200

    Fixes a bug where if destination exchange isn't present, and prefetch_count is met with unacked messages, shovel won't automatically reconnect when destination exchange is created.
    (cherry picked from commit 2daed29eb14827d03ef02e70ada62f6bcd0f6355)

--- deps/rabbitmq_shovel/src/rabbit_amqp091_shovel.erl
+++ deps/rabbitmq_shovel/src/rabbit_amqp091_shovel.erl
@@ -230,7 +230,7 @@ handle_source({'EXIT', Conn, Reason},
               #{source := #{current := {Conn, _, _}}}) ->
     {stop, {inbound_conn_died, Reason}};
 
-handle_source({'EXIT', _Pid, {shutdown, {server_initiated_close, ?PRECONDITION_FAILED, Reason}}}, _State) ->
+handle_source({'EXIT', _Pid, {shutdown, {server_initiated_close, _, Reason}}}, _State) ->
     {stop, {inbound_link_or_channel_closure, Reason}};
 
 handle_source(_Msg, _State) ->
@@ -255,7 +255,7 @@ handle_dest(#'basic.cancel'{}, #{name := Name}) ->
 handle_dest({'EXIT', Conn, Reason}, #{dest := #{current := {Conn, _, _}}}) ->
     {stop, {outbound_conn_died, Reason}};
 
-handle_dest({'EXIT', _Pid, {shutdown, {server_initiated_close, ?PRECONDITION_FAILED, Reason}}}, _State) ->
+handle_dest({'EXIT', _Pid, {shutdown, {server_initiated_close, _, Reason}}}, _State) ->
     {stop, {outbound_link_or_channel_closure, Reason}};
 
 handle_dest(_Msg, _State) ->
