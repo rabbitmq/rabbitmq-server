41946fc4d6b42c6742652b24648cf5951d3e5865
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-06-30 10:02:01 +0200

    Revamp AMQP 1.0 plugin Java test project
    Bump dependencies (using JMS 3.1 now, which requires Java 11),
    format code.
    
    (cherry picked from commit b94a7d876d2fee9d4ec3e298ccb570303d27bb95)

--- deps/rabbitmq_amqp1_0/test/system_SUITE_data/java-tests/src/test/java/com/rabbitmq/amqp1_0/tests/jms/RoundTripTest.java
+++ deps/rabbitmq_amqp1_0/test/system_SUITE_data/java-tests/src/test/java/com/rabbitmq/amqp1_0/tests/jms/RoundTripTest.java
@@ -2,83 +2,58 @@
 
 package com.rabbitmq.amqp1_0.tests.jms;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.util.*;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.DeliveryMode;
-import javax.jms.Destination;
-import javax.jms.ExceptionListener;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
+//import javax.jms.Connection;
+//import javax.jms.ConnectionFactory;
+//import javax.jms.DeliveryMode;
+//import javax.jms.Destination;
+//import javax.jms.Message;
+//import javax.jms.MessageConsumer;
+//import javax.jms.MessageProducer;
+//import javax.jms.Session;
+//import javax.jms.TextMessage;
 import javax.naming.Context;
-import javax.naming.InitialContext;
+import jakarta.jms.*;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class RoundTripTest
-    extends TestCase
-{
-    public static final String ADDRESS = "/jms-roundtrip-q";
-    public static final String PAYLOAD = "Payload";
+/** Unit test for simple App. */
+public class RoundTripTest {
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public RoundTripTest(String testName)
-    {
-        super(testName);
-    }
+  @Test
+  public void test_roundtrip() throws Exception {
+    String uri = System.getProperty("rmq_broker_uri", "amqp://localhost:5672");
+    Hashtable<Object, Object> env = new Hashtable<>();
+    env.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.qpid.jms.jndi.JmsInitialContextFactory");
+    env.put("connectionfactory.myFactoryLookup", uri);
+    env.put("queue.myQueueLookup", "my-queue");
+    env.put("jms.sendTimeout", 5);
+    env.put("jms.requestTimeout", 5);
+    javax.naming.Context context = new javax.naming.InitialContext(env);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite(RoundTripTest.class);
-    }
+    assertNotNull(uri);
 
-    public void test_roundtrip () throws Exception
-    {
-        String uri = System.getProperty("rmq_broker_uri");
-        String address = uri + ADDRESS;
-        Hashtable<Object, Object> env = new Hashtable<Object, Object>();
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.qpid.jms.jndi.JmsInitialContextFactory");
-        env.put("connectionfactory.myFactoryLookup", uri);
-        env.put("queue.myQueueLookup", "my-queue");
-        env.put("jms.sendTimeout", 5);
-        env.put("jms.requestTimeout", 5);
-        javax.naming.Context context = new javax.naming.InitialContext(env);
+    ConnectionFactory factory = (ConnectionFactory) context.lookup("myFactoryLookup");
+    Destination queue = (Destination) context.lookup("myQueueLookup");
 
-        assertNotNull(uri);
+    Connection connection = factory.createConnection("guest", "guest");
+    connection.start();
 
-        ConnectionFactory factory = (ConnectionFactory) context.lookup("myFactoryLookup");
-        Destination queue = (Destination) context.lookup("myQueueLookup");
+    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        Connection connection = factory.createConnection("guest", "guest");
-        connection.start();
+    MessageProducer messageProducer = session.createProducer(queue);
+    MessageConsumer messageConsumer = session.createConsumer(queue);
 
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    TextMessage message = session.createTextMessage("Hello world!");
+    messageProducer.send(
+        message,
+        DeliveryMode.NON_PERSISTENT,
+        Message.DEFAULT_PRIORITY,
+        Message.DEFAULT_TIME_TO_LIVE);
+    TextMessage receivedMessage = (TextMessage) messageConsumer.receive(2000L);
 
-        MessageProducer messageProducer = session.createProducer(queue);
-        MessageConsumer messageConsumer = session.createConsumer(queue);
-
-        TextMessage message = session.createTextMessage("Hello world!");
-        messageProducer.send(message, DeliveryMode.NON_PERSISTENT,
-                Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
-        TextMessage receivedMessage = (TextMessage) messageConsumer.receive(2000L);
-
-        assertEquals(message.getText(), receivedMessage.getText());
-    }
+    assertEquals(message.getText(), receivedMessage.getText());
+  }
 }
