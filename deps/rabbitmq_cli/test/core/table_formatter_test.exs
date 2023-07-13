4677469d70206a1dc294a2e1eb9769281d22ff78
Michael Klishin <michael@clojurewerkz.org>
2023-07-14 00:33:23 +0400

    mix format on Elixir 1.15

--- deps/rabbitmq_cli/test/core/table_formatter_test.exs
+++ deps/rabbitmq_cli/test/core/table_formatter_test.exs
@@ -17,7 +17,7 @@ defmodule TableFormatterTest do
              "apple\tbeer\t1"
            ]
 
-    assert @formatter.format_output(%{a: "apple", b: 'beer', c: 1}, %{}) == [
+    assert @formatter.format_output(%{a: "apple", b: ~c"beer", c: 1}, %{}) == [
              "a\tb\tc",
              "apple\t\"beer\"\t1"
            ]
@@ -39,7 +39,7 @@ defmodule TableFormatterTest do
 
   test "format_stream tab-separates map values" do
     assert @formatter.format_stream(
-             [%{a: :apple, b: :beer, c: 1}, %{a: "aadvark", b: 'bee', c: 2}],
+             [%{a: :apple, b: :beer, c: 1}, %{a: "aadvark", b: ~c"bee", c: 2}],
              %{}
            )
            |> Enum.to_list() ==
