99c101c1d3f902064186a18200c6991376765c2e
Marcial Rosales <mrosales@pivotal.io>
2023-08-10 12:46:20 +0100

    Add reproducer test

---
+++ deps/rabbitmq_management/selenium/suites/auth-http-backend.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+TEST_CASES_PATH=/auth-http-backend
+PROFILES=""
+
+source $SCRIPT/../bin/suite_template
+runWith auth-backend-http
---
+++ deps/rabbitmq_management/selenium/test/auth-http-backend/amqp10.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+const { getURLForProtocol } = require('../utils')
+const {execSync} = require('child_process')
+
+
+describe('AMQP1.0 client authenticate with basic auth against http backend', function () {
+
+  before(function () {
+
+  })
+
+  it('can open a connection', function () {
+    execSync("npm run amqp10_roundtriptest")
+  })
+
+  after(function () {
+
+  })
+})
