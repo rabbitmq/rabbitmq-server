940d2a70d95807dc7d6eea10122a69743a63ad39
Michael Klishin <michael@clojurewerkz.org>
2023-08-15 15:53:16 +0400

    Resolve a conflict (#9081)

--- deps/rabbitmq_management/selenium/suites/auth-http-backend.sh
+++ deps/rabbitmq_management/selenium/suites/auth-http-backend.sh
@@ -2,13 +2,8 @@
 
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-<<<<<<< HEAD
-TEST_CASES_PATH=/auth-http-backend
-PROFILES="auth-http auth_backends-http"
-=======
 TEST_CASES_PATH=/authnz-msg-protocols
 PROFILES="http-user auth-http auth_backends-http"
->>>>>>> c3ee4e5a6d (Fix #9043)
 
 source $SCRIPT/../bin/suite_template
 runWith mock-auth-backend-http
