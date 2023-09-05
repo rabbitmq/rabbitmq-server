b46c25c30836b9ad833d19091410c7eb0ee98179
Marcial Rosales <mrosales@pivotal.io>
2023-09-05 11:22:06 +0200

    Refactor suites to shorten pipeline execution
    - Separate pure management ui suites from authnz
    - Run full management ui suite on every commit to main or
     release brances
     - Fun full management ui suite on every change done to
     rabbitmq_management plugin on any PR
    
    (cherry picked from commit 7fb55881a43879060edded0afd191cf86e855b08)

--- deps/rabbitmq_management/selenium/suites/authnz-mgt/basic-auth-behind-proxy.sh
+++ deps/rabbitmq_management/selenium/suites/authnz-mgt/basic-auth-behind-proxy.sh
@@ -5,5 +5,5 @@ SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEST_CASES_PATH=/basic-auth
 PROFILES="proxy"
 
-source $SCRIPT/../bin/suite_template
+source $SCRIPT/../../bin/suite_template
 runWith proxy
--- deps/rabbitmq_management/selenium/suites/authnz/auth-http-backend.sh
+++ deps/rabbitmq_management/selenium/suites/authnz/auth-http-backend.sh
@@ -5,5 +5,5 @@ SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEST_CASES_PATH=/authnz-msg-protocols
 PROFILES="http-user auth-http auth_backends-http"
 
-source $SCRIPT/../bin/suite_template
+source $SCRIPT/../../bin/suite_template
 runWith mock-auth-backend-http
