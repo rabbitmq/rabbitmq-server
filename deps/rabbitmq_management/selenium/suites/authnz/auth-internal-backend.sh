7c0b7da403de109560e39cb113ea3dc0a5794f30
Marcial Rosales <mrosales@pivotal.io>
2023-09-05 11:22:06 +0200

    Refactor suites to shorten pipeline execution
    - Separate pure management ui suites from authnz
    - Run full management ui suite on every commit to main or
     release brances
     - Fun full management ui suite on every change done to
     rabbitmq_management plugin on any PR
    
    (cherry picked from commit 7fb55881a43879060edded0afd191cf86e855b08)
    (cherry picked from commit b46c25c30836b9ad833d19091410c7eb0ee98179)
    
    # Conflicts:
    #	deps/rabbitmq_management/selenium/suites/authnz-mgt/oauth-idp-initiated-with-uaa-and-prefix-via-proxy.sh
    #	deps/rabbitmq_management/selenium/suites/authnz-mgt/oauth-idp-initiated-with-uaa-via-proxy.sh

--- deps/rabbitmq_management/selenium/suites/authnz/auth-internal-backend.sh
+++ deps/rabbitmq_management/selenium/suites/authnz/auth-internal-backend.sh
@@ -5,5 +5,5 @@ SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEST_CASES_PATH=/authnz-msg-protocols
 PROFILES="internal-user auth_backends-internal "
 
-source $SCRIPT/../bin/suite_template
+source $SCRIPT/../../bin/suite_template
 run
