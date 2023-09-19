3e59531a04ce5ad168257d5a1f3c295776697905
Diana Parra Corbacho <dparracorbac@vmware.com>
2023-09-19 16:07:39 +0200

    cli_forget_cluster_node_SUITE: wait for leader election in both queues
    (cherry picked from commit 17def72a3b410404b9489fa69518b7eb62ff68dc)
    
    # Conflicts:
    #	deps/rabbit/test/cli_forget_cluster_node_SUITE.erl

--- deps/rabbit/test/cli_forget_cluster_node_SUITE.erl
+++ deps/rabbit/test/cli_forget_cluster_node_SUITE.erl
@@ -99,6 +99,18 @@ forget_cluster_node_with_quorum_queues(Config) ->
     ?awaitMatch(Members when length(Members) == 3, get_quorum_members(Rabbit, QQ2), 30000),
 
     ?assertEqual(ok, rabbit_control_helper:command(stop_app, Bunny)),
+<<<<<<< HEAD
+=======
+    %% Leaders are most likely on Rabbit where the queue was declared.
+    %% But let's wait anyway until a new leader is elected to not have unexpected
+    %% delete_replica failures. This can easily happen if queues are declared in Bunny
+    %% as it takes some time to elect a new leader
+    ?awaitMatch(Members when length(Members) == 2, get_online_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 2, get_online_members(Rabbit, QQ2), 30000),
+    ?awaitMatch(Member when Member =/= '', get_leader(Rabbit, QQ1), 30000),
+    ?awaitMatch(Member when Member =/= '', get_leader(Rabbit, QQ2), 30000),
+
+>>>>>>> 17def72a3b (cli_forget_cluster_node_SUITE: wait for leader election in both queues)
     ?assertEqual(ok, forget_cluster_node(Rabbit, Bunny)),
     assert_cluster_status({[Rabbit, Hare], [Rabbit, Hare], [Rabbit, Hare]},
                           [Rabbit, Hare]),
@@ -128,6 +140,189 @@ forget_cluster_node_with_last_quorum_member(Config) ->
     ?awaitMatch(Members when length(Members) == 1, get_quorum_members(Rabbit, QQ1), 30000),
     ?awaitMatch(Members when length(Members) == 1, get_quorum_members(Rabbit, QQ2), 30000).
 
+<<<<<<< HEAD
+=======
+forget_cluster_node_with_streams(Config) ->
+    [Rabbit, Hare, Bunny] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
+
+    assert_clustered([Rabbit, Hare, Bunny]),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Rabbit),
+    S1 = <<"stream-1">>,
+    S2 = <<"stream-2">>,
+    declare(Ch, S1, [{<<"x-queue-type">>, longstr, <<"stream">>}]),
+    declare(Ch, S2, [{<<"x-queue-type">>, longstr, <<"stream">>}]),
+
+    ?awaitMatch(Members when length(Members) == 3, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members])), 30000),
+    ?awaitMatch(Members when length(Members) == 3, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000),
+
+    ?assertEqual(ok, rabbit_control_helper:command(stop_app, Bunny)),
+
+    ?assertEqual(ok, forget_cluster_node(Rabbit, Bunny)),
+    assert_cluster_status({[Rabbit, Hare], [Rabbit, Hare], [Rabbit, Hare]},
+                          [Rabbit, Hare]),
+    ?awaitMatch(Members when length(Members) == 2, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 2, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000).
+
+forget_cluster_node_with_one_last_stream(Config) ->
+    [Rabbit, Hare, Bunny] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
+
+    assert_clustered([Rabbit, Hare, Bunny]),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Bunny),
+    S1 = <<"stream-1">>,
+    S2 = <<"stream-2">>,
+    declare(Ch, S1, [{<<"x-queue-type">>, longstr, <<"stream">>},
+                     {<<"x-initial-cluster-size">>, long, 1}]),
+    declare(Ch, S2, [{<<"x-queue-type">>, longstr, <<"stream">>}]),
+
+    ?awaitMatch(Members when length(Members) == 1, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 3, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000),
+
+    ?assertEqual(ok, rabbit_control_helper:command(stop_app, Bunny)),
+    ?assertMatch({error, 69, _}, forget_cluster_node(Rabbit, Bunny)),
+    assert_cluster_status({[Rabbit, Hare], [Rabbit, Hare], [Rabbit, Hare]},
+                          [Rabbit, Hare]),
+    ?awaitMatch(Members when length(Members) == 1, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 2, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000).
+
+forget_cluster_node_with_all_last_streams(Config) ->
+    [Rabbit, Hare, Bunny] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
+
+    assert_clustered([Rabbit, Hare, Bunny]),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Bunny),
+    S1 = <<"stream-1">>,
+    S2 = <<"stream-2">>,
+    declare(Ch, S1, [{<<"x-queue-type">>, longstr, <<"stream">>},
+                     {<<"x-initial-cluster-size">>, long, 1}]),
+    declare(Ch, S2, [{<<"x-queue-type">>, longstr, <<"stream">>},
+                     {<<"x-initial-cluster-size">>, long, 1}]),
+
+    ?awaitMatch(Members when length(Members) == 1, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 1, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000),
+
+    ?assertEqual(ok, rabbit_control_helper:command(stop_app, Bunny)),
+    ?assertMatch({error, 69, _}, forget_cluster_node(Rabbit, Bunny)),
+    assert_cluster_status({[Rabbit, Hare], [Rabbit, Hare], [Rabbit, Hare]},
+                          [Rabbit, Hare]),
+    ?awaitMatch(Members when length(Members) == 1, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 1, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000).
+
+forget_cluster_node_with_quorum_queues_and_streams(Config) ->
+    [Rabbit, Hare, Bunny] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
+
+    assert_clustered([Rabbit, Hare, Bunny]),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Rabbit),
+    QQ1 = <<"quorum-queue-1">>,
+    QQ2 = <<"quorum-queue-2">>,
+    S1 = <<"stream-1">>,
+    S2 = <<"stream-2">>,
+    declare(Ch, QQ1, [{<<"x-queue-type">>, longstr, <<"quorum">>}]),
+    declare(Ch, QQ2, [{<<"x-queue-type">>, longstr, <<"quorum">>}]),
+    declare(Ch, S1, [{<<"x-queue-type">>, longstr, <<"stream">>}]),
+    declare(Ch, S2, [{<<"x-queue-type">>, longstr, <<"stream">>}]),
+
+    ?awaitMatch(Members when length(Members) == 3, get_quorum_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 3, get_quorum_members(Rabbit, QQ2), 30000),
+    ?awaitMatch(Members when length(Members) == 3, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members])), 30000),
+    ?awaitMatch(Members when length(Members) == 3, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000),
+
+    ?assertEqual(ok, rabbit_control_helper:command(stop_app, Bunny)),
+    %% Leaders are most likely on Rabbit where the queue was declared.
+    %% But let's wait anyway until a new leader is elected to not have unexpected
+    %% delete_replica failures. This can easily happen if queues are declared in Bunny
+    %% as it takes some time to elect a new leader
+    ?awaitMatch(Members when length(Members) == 2, get_online_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 2, get_online_members(Rabbit, QQ2), 30000),
+    ?awaitMatch(Member when Member =/= '', get_leader(Rabbit, QQ1), 30000),
+    ?awaitMatch(Member when Member =/= '', get_leader(Rabbit, QQ2), 30000),
+
+    ?assertEqual(ok, forget_cluster_node(Rabbit, Bunny)),
+    assert_cluster_status({[Rabbit, Hare], [Rabbit, Hare], [Rabbit, Hare]},
+                          [Rabbit, Hare]),
+    ?awaitMatch(Members when length(Members) == 2, get_quorum_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 2, get_quorum_members(Rabbit, QQ2), 30000),
+    ?awaitMatch(Members when length(Members) == 2, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members])), 30000),
+?awaitMatch(Members when length(Members) == 2, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000).
+
+forget_cluster_node_with_one_last_quorum_member_and_streams(Config) ->
+    [Rabbit, Hare, Bunny] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
+
+    assert_clustered([Rabbit, Hare, Bunny]),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Bunny),
+    QQ1 = <<"quorum-queue-1">>,
+    QQ2 = <<"quorum-queue-2">>,
+    S1 = <<"stream-1">>,
+    S2 = <<"stream-2">>,
+    declare(Ch, QQ1, [{<<"x-queue-type">>, longstr, <<"quorum">>},
+                      {<<"x-quorum-initial-group-size">>, long, 1}]),
+    declare(Ch, QQ2, [{<<"x-queue-type">>, longstr, <<"quorum">>}]),
+    declare(Ch, S1, [{<<"x-queue-type">>, longstr, <<"stream">>}]),
+    declare(Ch, S2, [{<<"x-queue-type">>, longstr, <<"stream">>}]),
+
+    ?awaitMatch(Members when length(Members) == 1, get_quorum_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 3, get_quorum_members(Rabbit, QQ2), 30000),
+    ?awaitMatch(Members when length(Members) == 3, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members])), 30000),
+    ?awaitMatch(Members when length(Members) == 3, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000),
+
+    ?assertEqual(ok, rabbit_control_helper:command(stop_app, Bunny)),
+    %% Leaders are most likely on Bunny where the queue was declared. Let's wait until
+    %% a new leader is elected to not have unexpected delete_replica failures
+    ?awaitMatch(Members when length(Members) == 0, get_online_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 2, get_online_members(Rabbit, QQ2), 30000),
+    ?awaitMatch(Member when Member =/= '', get_leader(Rabbit, QQ2), 30000),
+
+    ?assertMatch({error, 69, _}, forget_cluster_node(Rabbit, Bunny)),
+    assert_cluster_status({[Rabbit, Hare], [Rabbit, Hare], [Rabbit, Hare]},
+                          [Rabbit, Hare]),
+    ?awaitMatch(Members when length(Members) == 1, get_quorum_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 2, get_quorum_members(Rabbit, QQ2), 30000),
+    ?awaitMatch(Members when length(Members) == 2, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members])), 30000),
+?awaitMatch(Members when length(Members) == 2, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000).
+
+forget_cluster_node_with_one_last_stream_and_quorum_queues(Config) ->
+    [Rabbit, Hare, Bunny] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
+
+    assert_clustered([Rabbit, Hare, Bunny]),
+
+    Ch = rabbit_ct_client_helpers:open_channel(Config, Bunny),
+    S1 = <<"stream-1">>,
+    S2 = <<"stream-2">>,
+    QQ1 = <<"quorum-queue-1">>,
+    QQ2 = <<"quorum-queue-2">>,
+    declare(Ch, S1, [{<<"x-queue-type">>, longstr, <<"stream">>},
+                     {<<"x-initial-cluster-size">>, long, 1}]),
+    declare(Ch, S2, [{<<"x-queue-type">>, longstr, <<"stream">>}]),
+    declare(Ch, QQ1, [{<<"x-queue-type">>, longstr, <<"quorum">>}]),
+    declare(Ch, QQ2, [{<<"x-queue-type">>, longstr, <<"quorum">>}]),
+
+    ?awaitMatch(Members when length(Members) == 1, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 3, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 3, get_quorum_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 3, get_quorum_members(Rabbit, QQ2), 30000),
+
+    ?assertEqual(ok, rabbit_control_helper:command(stop_app, Bunny)),
+    %% Leaders are most likely on Rabbit where the queue was declared.
+    %% But let's wait anyway until a new leader is elected to not have unexpected
+    %% delete_replica failures. This can easily happen if queues are declared in Bunny
+    %% as it takes some time to elect a new leader
+    ?awaitMatch(Members when length(Members) == 2, get_online_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 2, get_online_members(Rabbit, QQ2), 30000),
+    ?awaitMatch(Member when Member =/= '', get_leader(Rabbit, QQ1), 30000),
+    ?awaitMatch(Member when Member =/= '', get_leader(Rabbit, QQ2), 30000),
+
+    ?assertMatch({error, 69, _}, forget_cluster_node(Rabbit, Bunny)),
+    assert_cluster_status({[Rabbit, Hare], [Rabbit, Hare], [Rabbit, Hare]},
+                          [Rabbit, Hare]),
+    ?awaitMatch(Members when length(Members) == 1, proplists:get_value(members, find_queue_info(Config, Rabbit, S1, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 2, proplists:get_value(members, find_queue_info(Config, Rabbit, S2, [members]), 30000), 30000),
+    ?awaitMatch(Members when length(Members) == 2, get_quorum_members(Rabbit, QQ1), 30000),
+    ?awaitMatch(Members when length(Members) == 2, get_quorum_members(Rabbit, QQ2), 30000).
+
+>>>>>>> 17def72a3b (cli_forget_cluster_node_SUITE: wait for leader election in both queues)
 forget_cluster_node(Node, Removee) ->
     rabbit_control_helper:command(forget_cluster_node, Node, [atom_to_list(Removee)],
                                   []).
