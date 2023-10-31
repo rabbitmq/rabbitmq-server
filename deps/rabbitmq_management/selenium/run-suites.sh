c169f6ef50ae61063c18d72c4eee9ebc0def68e6
Marcial Rosales <mrosales@pivotal.io>
2023-10-31 12:55:31 +0100

    Fix issue in run-scripts

--- deps/rabbitmq_management/selenium/run-suites.sh
+++ deps/rabbitmq_management/selenium/run-suites.sh
@@ -11,9 +11,11 @@ NC='\033[0m'
 SUCCESSFUL_SUITES=()
 FAILED_SUITES=()
 
-cat $SCRIPT/$SUITE_FILE | sort | while read SUITE
+TOTAL_SUITES=`wc -l $SCRIPT/$SUITE_FILE | awk '{print $1}'`
+
+while read SUITE
 do
-  echo "=== Running suite $SUITE ============================================"
+  echo -e "=== Running suite (${TOTAL_SUITES}/${GREEN}${#SUCCESSFUL_SUITES[@]}/${RED}${#FAILED_SUITES[@]}${NC}) $SUITE ============================================"
   echo " "
   ENV_MODES="docker" $SCRIPT/suites/$SUITE
   TEST_RESULT="$?"
@@ -28,9 +30,9 @@ do
   fi
   echo -e "=== $TEST_STATUS $SUITE ==========================================="
   echo " "
-done
+done <<< "$(cat $SCRIPT/$SUITE_FILE | sort)"
 
-echo "=== Summary ============================================"
+echo -e "=== Summary (${TOTAL_SUITES}/${GREEN}${#SUCCESSFUL_SUITES[@]}/${RED}${#FAILED_SUITES[@]}${NC}) ============================================"
 if [ ${#SUCCESSFUL_SUITES[@]} -gt 0 ]; then echo -e " > ${GREEN}Successful suites ${NC}"; fi
 for f in ${SUCCESSFUL_SUITES[@]}
 do
@@ -43,4 +45,5 @@ do
   echo "   - $f"
 done
 
+echo "Terminating with $OVERALL_TEST_RESULT"
 exit $OVERALL_TEST_RESULT
