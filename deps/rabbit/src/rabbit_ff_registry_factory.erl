4168f012a91c44f8c3e7320181dc77856f04a0b4
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2023-11-22 10:24:20 +0100

    rabbit_feature_flags: Fix registry reset vs. Erlang embedded mode
    [Why]
    If the Erlang VM is running in embedded mode, it will refuse to
    auto-load any module from disk. That's because all modules are loaded
    from the Erlang release right from the start.
    
    [How]
    After purging the module, we explicitly reload the stub version from the
    disk.
    
    (cherry picked from commit 54bc464f508c85ccec94f17c61d6ddba65031497)
    
    # Conflicts:
    #	deps/rabbit/src/rabbit_ff_registry_factory.erl

--- deps/rabbit/src/rabbit_ff_registry_factory.erl
+++ deps/rabbit/src/rabbit_ff_registry_factory.erl
@@ -747,3 +747,22 @@ do_purge_old_registry(Mod) ->
         true  -> ok;
         false -> do_purge_old_registry(Mod)
     end.
+<<<<<<< HEAD
+=======
+
+-spec reset_registry() -> ok.
+
+reset_registry() ->
+    ?LOG_DEBUG(
+       "Feature flags: resetting loaded registry",
+       [],
+       #{domain => ?RMQLOG_DOMAIN_FEAT_FLAGS}),
+    _ = code:purge(rabbit_ff_registry),
+    _ = code:delete(rabbit_ff_registry),
+    %% After purging the module, we explicitly reload the stub version from the
+    %% disk. We need to do this because if the Erlang VM is running in embedded
+    %% mode, it will refuse to auto-load the module.
+    _ = code:load_file(rabbit_ff_registry),
+    ?assertNot(rabbit_ff_registry:is_registry_initialized()),
+    ok.
+>>>>>>> 54bc464f50 (rabbit_feature_flags: Fix registry reset vs. Erlang embedded mode)
