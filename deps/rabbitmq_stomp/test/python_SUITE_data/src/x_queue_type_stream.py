6b61c2cc7d7189ec793923eb214cca59eed24a89
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-12-05 10:39:08 +0100

    Support stream filtering in STOMP
    Forward the x-stream-filter-size-bytes header when
    a subscription creates a stream queue.
    
    Extract the x-stream-filter header for subscriptions,
    tokenize it (using the comma as the separator) in case
    several filter values are provided.

--- deps/rabbitmq_stomp/test/python_SUITE_data/src/x_queue_type_stream.py
+++ deps/rabbitmq_stomp/test/python_SUITE_data/src/x_queue_type_stream.py
@@ -27,6 +27,7 @@ class TestUserGeneratedQueueName(base.BaseTest):
                     'x-queue-type': 'stream',
                     'x-max-age' : '10h',
                     'x-stream-max-segment-size-bytes' : 1048576,
+                    'x-stream-filter-size-bytes' : 32,
                     'durable': True,
                     'auto-delete': False,
                     'id': 1234,
