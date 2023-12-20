8670aca85ac236841d4bcbb931765eff901d4a5a
Diana Parra Corbacho <diana.parra-corbacho@broadcom.com>
2023-12-20 09:33:55 +0100

    Retrieve oldest message received timestamp

--- deps/rabbit/src/rabbit_variable_queue.erl
+++ deps/rabbit/src/rabbit_variable_queue.erl
@@ -833,6 +833,10 @@ info(head_message_timestamp, #vqstate{
           q3               = Q3,
           ram_pending_ack  = RPA}) ->
     head_message_timestamp(Q3, RPA);
+info(oldest_message_received_timestamp, #vqstate{
+                                           q3               = Q3,
+                                           ram_pending_ack  = RPA}) ->
+    oldest_message_received_timestamp(Q3, RPA);
 info(disk_reads, #vqstate{disk_read_count = Count}) ->
     Count;
 info(disk_writes, #vqstate{disk_write_count = Count}) ->
@@ -1194,7 +1198,6 @@ convert_from_v2_to_v1_loop(QueueName, V1Index0, V2Index0, V2Store0,
 %% regarded as unprocessed until acked, this also prevents the result
 %% apparently oscillating during repeated rejects.
 %%
-%% @todo OK I think we can do this differently
 head_message_timestamp(Q3, RPA) ->
     HeadMsgs = [ HeadMsgStatus#msg_status.msg ||
                    HeadMsgStatus <-
@@ -1207,6 +1210,26 @@ head_message_timestamp(Q3, RPA) ->
         [Timestamp div 1000
          || HeadMsg <- HeadMsgs,
             Timestamp <- [mc:timestamp(HeadMsg)],
+            Timestamp /= undefined
+        ],
+
+    case Timestamps == [] of
+        true -> '';
+        false -> lists:min(Timestamps)
+    end.
+
+oldest_message_received_timestamp(Q3, RPA) ->
+    HeadMsgs = [ HeadMsgStatus#msg_status.msg ||
+                   HeadMsgStatus <-
+                       [ get_q_head(Q3),
+                         get_pa_head(RPA) ],
+                   HeadMsgStatus /= undefined,
+                   HeadMsgStatus#msg_status.msg /= undefined ],
+
+    Timestamps =
+        [Timestamp
+         || HeadMsg <- HeadMsgs,
+            Timestamp <- [mc:get_annotation(rts, HeadMsg)],
             Timestamp /= undefined
         ],
 
