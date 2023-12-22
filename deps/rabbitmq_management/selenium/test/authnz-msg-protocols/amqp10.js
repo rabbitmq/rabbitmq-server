2fc8d2b3ae4cedeff66138d1faefc695a25ad778
Marcial Rosales <mrosales@pivotal.io>
2023-12-22 13:54:34 +0000

    Propagate all credentials to http backend

--- deps/rabbitmq_management/selenium/test/authnz-msg-protocols/amqp10.js
+++ deps/rabbitmq_management/selenium/test/authnz-msg-protocols/amqp10.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const { getURLForProtocol } = require('../utils')
+const { reset, expectUser, expectVhost, expectResource, allow, verifyAll } = require('../mock_http_backend')
 const {execSync} = require('child_process')
 
 const profiles = process.env.PROFILES || ""
@@ -10,17 +11,28 @@ for (const element of profiles.split(" ")) {
   }
 }
 
-describe('Having the following auth_backends enabled: ' + backends, function () {
+describe('Having AMQP 1.0 protocol enabled and the following auth_backends: ' + backends, function () {
+  let expectations = []
 
   before(function () {
-
+    if ( backends.includes("http") ) {
+      reset()
+      expectations.push(expectUser({ "username": "httpuser", "password": "httppassword" }, "allow"))
+      expectations.push(expectVhost({ "username": "httpuser", "vhost": "/"}, "allow"))
+      expectations.push(expectResource({ "username": "httpuser", "vhost": "/", "resource": "queue", "name": "my-queue", "permission":"configure", "tags":""}, "allow"))
+      expectations.push(expectResource({ "username": "httpuser", "vhost": "/", "resource": "queue", "name": "my-queue", "permission":"read", "tags":""}, "allow"))
+      expectations.push(expectResource({ "username": "httpuser", "vhost": "/", "resource": "exchange", "name": "amq.default", "permission":"write", "tags":""}, "allow"))
+    }
   })
 
   it('can open an AMQP 1.0 connection', function () {
     execSync("npm run amqp10_roundtriptest")
+
   })
 
   after(function () {
-
+      if ( backends.includes("http") ) {
+        verifyAll(expectations)
+      }
   })
 })
