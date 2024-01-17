059084d59e6d986bf1e1a27b7223e17157d8586d
Ayanda Dube <adube14@bloomberg.net>
2024-01-17 18:10:19 +0200

    strict match undefined state of gm members to _let it crash_ and allow exposing any unexpected member states

--- deps/rabbit/src/gm.erl
+++ deps/rabbit/src/gm.erl
@@ -1373,12 +1373,12 @@ with_member_acc(Fun, Id, {MembersState, Acc}) ->
     {MemberState, Acc1} = Fun(find_member_or_blank(Id, MembersState), Acc),
     {store_member(Id, MemberState, MembersState), Acc1}.
 
+find_member_or_blank(_Id, undefined) -> blank_member();
 find_member_or_blank(Id, MembersState) when is_map(MembersState) ->
     case maps:find(Id, MembersState) of
         {ok, Result} -> Result;
         error        -> blank_member()
-    end;
-find_member_or_blank(_Id, _MembersState) -> blank_member().
+    end.
 
 erase_member(Id, MembersState) -> maps:remove(Id, MembersState).
 
