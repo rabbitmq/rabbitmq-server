0d046e58c09641fe27d2c42cc7af298de40b4753
Marcial Rosales <mrosales@pivotal.io>
2024-01-29 20:48:55 +0100

    Fix issue with multi-oauth
    It turns out the rabbitmq url
    configured in keycloak was not
    rendered but fixed to localhost

--- deps/rabbitmq_management/selenium/suites/authnz-mgt/oauth-with-multi-oauth.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-TEST_CASES_PATH=/multi-oauth
-PROFILES="devkeycloak prodkeycloak "
-
-source $SCRIPT/../../bin/suite_template $@
-runWith devkeycloak prodkeycloak
