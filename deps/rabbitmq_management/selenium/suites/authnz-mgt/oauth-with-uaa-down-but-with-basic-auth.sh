9d9b2f21349a5d70c31262eec8779c9edc45cac9
Marcial Rosales <mrosales@pivotal.io>
2024-02-10 21:24:45 +0100

    Do not use tls with uaa
    Because uaa is not exposing https

--- deps/rabbitmq_management/selenium/suites/authnz-mgt/oauth-with-uaa-down-but-with-basic-auth.sh
+++ deps/rabbitmq_management/selenium/suites/authnz-mgt/oauth-with-uaa-down-but-with-basic-auth.sh
@@ -4,7 +4,7 @@ SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TEST_CASES_PATH=/oauth/with-basic-auth-idp-down
 TEST_CONFIG_PATH=/oauth
-PROFILES="uaa uaa-oauth-provider uaa-mgt-oauth-provider tls enable-basic-auth"
+PROFILES="uaa uaa-oauth-provider uaa-mgt-oauth-provider enable-basic-auth"
 
 source $SCRIPT/../../bin/suite_template $@
 run
