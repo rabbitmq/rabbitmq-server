e28f403eff1a7924b6c91d0f491a88be333ae107
Michael Davis <mcarsondavis@gmail.com>
2024-04-02 13:21:40 -0400

    Improve cluster_minority_SUITE
    This is a mix of a few changes:
    
    * Suppress the compiler warning from the export_all attribute.
    * Lower Khepri's command handling timeout value. By default this is
      set to 30s in rabbit which makes each of the cases in
      `client_operations` take an excessively long time. Before this change
      the suite took around 10 minutes to complete. Now it takes between two
      and three minutes.
    * Swap the order of client and broker teardown steps in end_per_group
      hook. The client teardown steps will always fail if run after the
      broker teardown steps because they rely on a value in `Config` that
      is deleted by broker teardown.
    
    (cherry picked from commit 7a8669ea73c8dc7e32b2be416ae9a64129619549)

--- deps/rabbit/test/cluster_minority_SUITE.erl
+++ deps/rabbit/test/cluster_minority_SUITE.erl
@@ -11,7 +11,7 @@
 -include_lib("amqp_client/include/amqp_client.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--compile(export_all).
+-compile([export_all, nowarn_export_all]).
 
 all() ->
     [
@@ -89,6 +89,14 @@ init_per_group(Group, Config0) when Group == client_operations;
             #'queue.declare_ok'{} = amqp_channel:call(Ch, #'queue.declare'{queue = <<"test-queue">>,
                                                                            arguments = [{<<"x-queue-type">>, longstr, <<"classic">>}]}),
 
+            %% Lower the default Khepri command timeout. By default this is set
+            %% to 30s in `rabbit_khepri:setup/1' which makes the cases in this
+            %% group run unnecessarily slow.
+            [ok = rabbit_ct_broker_helpers:rpc(
+                    Config1, N,
+                    application, set_env,
+                    [khepri, default_timeout, 100]) || N <- lists:seq(0, 4)],
+
             %% Create partition
             partition_5_node_cluster(Config1),
             Config1
@@ -105,8 +113,8 @@ init_per_group(Group, Config0) ->
 
 end_per_group(_, Config) ->
     rabbit_ct_helpers:run_steps(Config,
-                                rabbit_ct_broker_helpers:teardown_steps() ++
-                                    rabbit_ct_client_helpers:teardown_steps()).
+                                rabbit_ct_client_helpers:teardown_steps() ++
+                                    rabbit_ct_broker_helpers:teardown_steps()).
 
 init_per_testcase(Testcase, Config) ->
     rabbit_ct_helpers:testcase_started(Config, Testcase).
