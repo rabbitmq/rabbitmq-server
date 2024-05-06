2b59f3f266cb58c8fa94291fb408c0dfe64bc3ad
David Ansari <david.ansari@gmx.de>
2024-05-06 08:50:00 +0200

    wip

--- deps/rabbit/src/mc_amqp.erl
+++ deps/rabbit/src/mc_amqp.erl
@@ -569,13 +569,6 @@ msg_body_encoded([{{pos, Pos}, {body, Code}}], BarePos, Msg)
 binary_part_bare_and_footer(Payload, Start) ->
     binary_part(Payload, Start, byte_size(Payload) - Start).
 
-key_find(K, [{{_, K}, {_, V}} | _]) ->
-    V;
-key_find(K, [_ | Rem]) ->
-    key_find(K, Rem);
-key_find(_K, []) ->
-    undefined.
-
 -spec first_acquirer(mc:annotations()) -> boolean().
 first_acquirer(Anns) ->
     Redelivered = case Anns of
@@ -584,48 +577,12 @@ first_acquirer(Anns) ->
                   end,
     not Redelivered.
 
-recover_deaths([], Acc) ->
-    Acc;
-recover_deaths([{map, Kvs} | Rem], Acc) ->
-    Queue = key_find(<<"queue">>, Kvs),
-    Reason = binary_to_existing_atom(key_find(<<"reason">>, Kvs)),
-    DA0 = case key_find(<<"original-expiration">>, Kvs) of
-              undefined ->
-                  #{};
-              Exp ->
-                  #{ttl => binary_to_integer(Exp)}
-          end,
-    RKeys = [RK || {_, RK} <- key_find(<<"routing-keys">>, Kvs)],
-    Ts = key_find(<<"time">>, Kvs),
-    DA = DA0#{first_time => Ts,
-              last_time => Ts},
-    recover_deaths(Rem,
-                   Acc#{{Queue, Reason} =>
-                        #death{anns = DA,
-                               exchange = key_find(<<"exchange">>, Kvs),
-                               count = key_find(<<"count">>, Kvs),
-                               routing_keys = RKeys}}).
-
 essential_properties(#msg_body_encoded{message_annotations = MA} = Msg) ->
     Durable = get_property(durable, Msg),
     Priority = get_property(priority, Msg),
     Timestamp = get_property(timestamp, Msg),
     Ttl = get_property(ttl, Msg),
 
-    Deaths = case message_annotation(<<"x-death">>, Msg, undefined) of
-                 {list, DeathMaps}  ->
-                     %% TODO: make more correct?
-                     Def = {utf8, <<>>},
-                     {utf8, FstQ} = message_annotation(<<"x-first-death-queue">>, Msg, Def),
-                     {utf8, FstR} = message_annotation(<<"x-first-death-reason">>, Msg, Def),
-                     {utf8, LastQ} = message_annotation(<<"x-last-death-queue">>, Msg, Def),
-                     {utf8, LastR} = message_annotation(<<"x-last-death-reason">>, Msg, Def),
-                     #deaths{first = {FstQ, binary_to_existing_atom(FstR)},
-                             last = {LastQ, binary_to_existing_atom(LastR)},
-                             records = recover_deaths(DeathMaps, #{})};
-                 _ ->
-                     undefined
-             end,
     Anns0 = #{?ANN_DURABLE => Durable},
     Anns = maps_put_truthy(
              ?ANN_PRIORITY, Priority,
@@ -633,9 +590,7 @@ essential_properties(#msg_body_encoded{message_annotations = MA} = Msg) ->
                ?ANN_TIMESTAMP, Timestamp,
                maps_put_truthy(
                  ttl, Ttl,
-                 maps_put_truthy(
-                   deaths, Deaths,
-                   Anns0)))),
+                   Anns0))),
     case MA of
         [] ->
             Anns;
