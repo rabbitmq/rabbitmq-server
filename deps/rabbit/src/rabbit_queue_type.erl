36f7e8d6f4c689f60e2514d911d31d5ee38677aa
Simon Unge <simunge@amazon.com>
2024-05-14 16:09:28 +0000

    Instead of throwing an error, return protocol_error

--- deps/rabbit/src/rabbit_queue_type.erl
+++ deps/rabbit/src/rabbit_queue_type.erl
@@ -307,8 +307,12 @@ is_compatible(Type, Durable, Exclusive, AutoDelete) ->
 declare(Q0, Node) ->
     Q = rabbit_queue_decorator:set(rabbit_policy:set(Q0)),
     Mod = amqqueue:get_type(Q),
-    ok = check_vhost_queue_limit(Q),
-    Mod:declare(Q, Node).
+    case check_vhost_queue_limit(Q) of
+        ok ->
+            Mod:declare(Q, Node);
+        Error ->
+            Error
+    end.
 
 -spec delete(amqqueue:amqqueue(), boolean(),
              boolean(), rabbit_types:username()) ->
@@ -771,8 +775,11 @@ check_vhost_queue_limit(Q) ->
     #resource{name = QueueName} = amqqueue:get_name(Q),
     VHost = amqqueue:get_vhost(Q),
     case rabbit_vhost_limit:is_over_queue_limit(VHost) of
-        false         -> ok;
-        {true, Limit} -> rabbit_misc:precondition_failed("cannot declare queue '~ts': "
-                                                         "queue limit in vhost '~ts' (~tp) is reached",
-                                                         [QueueName, VHost, Limit])
+        false->
+            ok;
+        {true, Limit} ->
+            {protocol_error, precondition_failed,
+             "cannot declare queue '~ts': "
+             "queue limit in vhost '~ts' (~tp) is reached",
+             [QueueName, VHost, Limit]}
     end.
