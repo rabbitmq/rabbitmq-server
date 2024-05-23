a5d37f62bdc49cb30155453c29b8160a8f5bfdc9
Michael Klishin <mikhail.klishinm@broadcom.com>
2024-05-23 13:42:51 -0400

    Periodically reconcile virtual host processes
    for up to 10 times.
    
    When a cluster is formed from scratch and a virtual
    host is declared in the process (can be via
    definitions, or plugins, or any other way),
    that virtual host's process tree will be started
    on all reachable cluster nodes at that moment
    in time.
    
    However, this can be just a subset of all nodes
    expected to join the cluster.
    
    The most effective solution is to run this
    reconciliation process on a timer for up to
    5 minutes by default. This matches how long
    some other parts of RabbitMQ (3.x) expect
    cluster formation to take, at most.
    
    Per discussion with @dcorbacho @mkuratczyk.
    
    (cherry picked from commit 37778fd934c9578490954a899f57b01d3d114403)
    
    # Conflicts:
    #	deps/rabbit/src/rabbit.erl

--- deps/rabbit/src/rabbit.erl
+++ deps/rabbit/src/rabbit.erl
@@ -267,6 +267,24 @@
                     {mfa,         {logger, debug, ["'networking' boot step skipped and moved to end of startup", [], #{domain => ?RMQLOG_DOMAIN_GLOBAL}]}},
                     {requires,    notify_cluster}]}).
 
+<<<<<<< HEAD
+=======
+%% This mechanism is necessary in environments where a cluster is formed in parallel,
+%% which is the case with many container orchestration tools.
+%% In such scenarios, a virtual host can be declared before the cluster is formed and all
+%% cluster members are known, e.g. via definition import.
+-rabbit_boot_step({virtual_host_reconciliation,
+    [{description, "makes sure all virtual host have running processes on all nodes"},
+        {mfa,         {rabbit_vhosts, boot, []}},
+        {requires,    notify_cluster}]}).
+
+-rabbit_boot_step({pg_local,
+                   [{description, "local-only pg scope"},
+                    {mfa,         {rabbit, pg_local, []}},
+                    {requires,    kernel_ready},
+                    {enables,     core_initialized}]}).
+
+>>>>>>> 37778fd934 (Periodically reconcile virtual host processes)
 %%---------------------------------------------------------------------------
 
 -include_lib("rabbit_common/include/rabbit_framing.hrl").
