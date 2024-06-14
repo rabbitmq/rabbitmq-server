ce16e61d08e93cbf17b2b5dccba8a5a8e12d3e85
Luke Bakken <luke@bakken.io>
2024-06-14 11:03:48 -0700

    Do not overwrite `default_queue_type` with `undefined`
    Importing a definitions file with no `default_queue_type` metadata for a vhost will result in that vhosts value being set to `undefined`.
    
    Once set to a non-`undefined` value, this PR prevents `default_queue_type` from being set back to `undefined`

--- deps/rabbit/test/vhost_SUITE.erl
+++ deps/rabbit/test/vhost_SUITE.erl
@@ -33,6 +33,7 @@ groups() ->
         vhost_failure_forces_connection_closure,
         vhost_creation_idempotency,
         vhost_update_idempotency,
+        vhost_update_default_queue_type_undefined,
         vhost_deletion,
         parse_tags
     ],
@@ -42,6 +43,8 @@ groups() ->
         vhost_failure_forces_connection_closure_on_failure_node,
         node_starts_with_dead_vhosts,
         vhost_creation_idempotency,
+        vhost_update_idempotency,
+        vhost_update_default_queue_type_undefined,
         vhost_deletion
     ],
     [
@@ -297,6 +300,30 @@ vhost_update_idempotency(Config) ->
     after
         rabbit_ct_broker_helpers:rpc(Config, 0,
                                      gen_event, delete_handler, [rabbit_event, test_rabbit_event_handler, []]),
+        rabbit_ct_broker_helpers:delete_vhost(Config, VHost)
+    end.
+
+vhost_update_default_queue_type_undefined(Config) ->
+    VHost = <<"update-default_queue_type-with-undefined-test">>,
+    Description = <<"rmqfpas-105 test vhost">>,
+    Tags = [replicate, private],
+    DefaultQueueType = quorum,
+    Trace = false,
+    ActingUser = <<"acting-user">>,
+    try
+        ?assertMatch(ok, rabbit_ct_broker_helpers:add_vhost(Config, VHost)),
+
+        PutVhostArgs0 = [VHost, Description, Tags, DefaultQueueType, Trace, ActingUser],
+        ?assertMatch(ok,
+                     rabbit_ct_broker_helpers:rpc(Config, 0, rabbit_vhost, put_vhost, PutVhostArgs0)),
+
+        PutVhostArgs1 = [VHost, Description, Tags, undefined, Trace, ActingUser],
+        ?assertMatch(ok,
+                     rabbit_ct_broker_helpers:rpc(Config, 0, rabbit_vhost, put_vhost, PutVhostArgs1)),
+
+        V = rabbit_ct_broker_helpers:rpc(Config, 0, rabbit_vhost, lookup, [VHost]),
+        ?assertMatch(#{default_queue_type := DefaultQueueType}, vhost:get_metadata(V))
+    after
         rabbit_ct_broker_helpers:delete_vhost(Config, VHost)
     end.
 
