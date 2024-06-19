b9966295bf0af1f504035999be997dc495b81887
Marcial Rosales <marcial.rosales@broadcom.com>
2024-06-19 18:31:49 +0200

    Move most javascript logic to helper.js

--- deps/rabbitmq_management/src/rabbit_mgmt_oauth_bootstrap.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_oauth_bootstrap.erl
@@ -18,24 +18,18 @@ init(Req0, State) ->
 bootstrap_oauth(Req0, State) ->
     AuthSettings = rabbit_mgmt_wm_auth:authSettings(),
     Dependencies = oauth_dependencies(),
-    JSContent = case proplists:get_value(oauth_enabled, AuthSettings, false) of 
-        false -> declare_oauth_initialize_if_required(AuthSettings); 
-        true -> import_dependencies(Dependencies) ++ 
-                declare_oauth_initialize_if_required(AuthSettings) ++ 
-                set_token_auth(Req0) ++ 
-                export_dependencies(Dependencies)
-                
-    end ++ export_dependencies(["oauth_initialize_if_required"]),
+    JSContent = import_dependencies(Dependencies) ++ 
+                set_oauth_settings(AuthSettings) ++ 
+                case proplists:get_value(oauth_enabled, AuthSettings, false) of             
+                    true -> set_token_auth(Req0) ++ export_dependencies(oauth_dependencies());
+                    false -> export_dependencies(["oauth_initialize_if_required", "set_oauth_settings"])
+                end,
     {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"text/javascript; charset=utf-8">>}, JSContent, Req0), State}.
 
-declare_oauth_initialize_if_required(AuthSettings) ->
+set_oauth_settings(AuthSettings) ->    
     case proplists:get_value(oauth_enabled, AuthSettings, false) of 
-        true ->  ["export default function oauth_initialize_if_required(state) { ",
-                "let oauth = oauth_initialize(", rabbit_json:encode(rabbit_mgmt_format:format_nulls(AuthSettings)), "); ", 
-                "if (!oauth.enabled) return oauth;"
-                "switch (state) { case 'login-callback': oauth_completeLogin(); break; case 'logout-callback': oauth_completeLogout(); break; default: oauth = oauth_initiate(oauth);}",
-                "return oauth; }"];
-        false -> ["export default function oauth_initialize_if_required(state) { return {oauth_enabled: false}; }"] 
+        true ->  ["set_oauth_settings(", rabbit_json:encode(rabbit_mgmt_format:format_nulls(AuthSettings)), "); "];
+        false -> ["set_oauth_settings({oauth_enabled: false});"] 
     end.
 
 set_token_auth(Req0) ->
@@ -52,7 +46,7 @@ import_dependencies(Dependencies) ->
     ["import {", string:join(Dependencies, ","), "} from './helper.js';"].
 
 oauth_dependencies() ->
-    ["hasAnyResourceServerReady", "oauth_initialize", "oauth_initiate", "oauth_initiateLogin", "oauth_initiateLogout", "oauth_completeLogin", "oauth_completeLogout"].
+    ["oauth_initialize_if_required", "hasAnyResourceServerReady", "oauth_initialize", "oauth_initiate", "oauth_initiateLogin", "oauth_initiateLogout", "oauth_completeLogin", "oauth_completeLogout", "set_oauth_settings"].
 
 export_dependencies(Dependencies) ->
     [ io_lib:format("window.~s = ~s;", [Dep, Dep]) || Dep <- Dependencies ].
