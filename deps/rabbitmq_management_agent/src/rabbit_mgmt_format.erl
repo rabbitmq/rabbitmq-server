5836eebe0756dde95c34f8471203fe0a0ed0418b
Marcial Rosales <marcial.rosales@broadcom.com>
2024-06-20 10:58:46 +0200

    Do not expose which backend was used
    instead just indicate if the user is internal or not
    
    (cherry picked from commit f0adf3a2e8af4cef327c88ed30b18a1adace5ea1)

--- deps/rabbitmq_management_agent/src/rabbit_mgmt_format.erl
+++ deps/rabbitmq_management_agent/src/rabbit_mgmt_format.erl
@@ -269,10 +269,11 @@ internal_user(User) ->
      {tags,              tags_as_binaries(internal_user:get_tags(User))},
      {limits,            internal_user:get_limits(User)}].
 
-user(User) ->
+user(User) ->    
     [{name, User#user.username},
      {tags, tags_as_binaries(User#user.tags)},
-     {backends, [ Module || {Module, _} <- User#user.authz_backends]}].
+     {is_internal_user, lists:any(fun({Module,_}) -> Module == rabbit_auth_backend_internal  end, 
+                                    User#user.authz_backends)}].
 
 tags_as_binaries(Tags) ->
     [to_binary(T) || T <- Tags].
