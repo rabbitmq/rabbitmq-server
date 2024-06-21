ecae055a2388fb208ab835cf8a7908b887b2f881
Karl Nilsson <kjnilsson@gmail.com>
2024-06-21 12:38:18 +0100

    reliability
    (cherry picked from commit db91415fab58532115df9dd597788bea101c0015)

--- deps/rabbitmq_management/test/clustering_prop_SUITE.erl
+++ deps/rabbitmq_management/test/clustering_prop_SUITE.erl
@@ -116,6 +116,9 @@ prop_connection_channel_counts(Config) ->
                 force_stats(Config),
                 Res = validate_counts(Config, Cons),
                 cleanup(Cons),
+                rabbit_ct_helpers:await_condition(
+                  fun () -> validate_counts(Config, []) end,
+                  60000),
                 force_stats(Config),
                 Res
             end).
