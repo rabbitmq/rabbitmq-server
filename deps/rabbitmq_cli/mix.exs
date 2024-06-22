732a75d71d82f028f7bd8d88ea13c61cabde5158
Volker Schlecht <47375452+VlkrS@users.noreply.github.com>
2024-06-22 09:40:27 +0200

    Allow elixir 1.17.x
    RabbitMQ builds fine with elixir 1.17.1, tested with Release 3.13.3 on OTP26

--- deps/rabbitmq_cli/mix.exs
+++ deps/rabbitmq_cli/mix.exs
@@ -11,7 +11,7 @@ defmodule RabbitMQCtl.MixfileBase do
     [
       app: :rabbitmqctl,
       version: "3.13.0-dev",
-      elixir: ">= 1.13.4 and < 1.17.0",
+      elixir: ">= 1.13.4 and < 1.18.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       escript: [
