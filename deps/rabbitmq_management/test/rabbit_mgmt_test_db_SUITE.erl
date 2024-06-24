c8c2beea25f3749574c2bebc054ccb8c211139dd
Michael Klishin <michael@clojurewerkz.org>
2024-06-24 17:32:41 -0400

    Resolve a conflict #11496 #11522

--- deps/rabbitmq_management/test/rabbit_mgmt_test_db_SUITE.erl
+++ deps/rabbitmq_management/test/rabbit_mgmt_test_db_SUITE.erl
@@ -10,11 +10,6 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("rabbit_common/include/rabbit_core_metrics.hrl").
 -include_lib("rabbitmq_management_agent/include/rabbit_mgmt_records.hrl").
-<<<<<<< HEAD
--include_lib("rabbitmq_management_agent/include/rabbit_mgmt_metrics.hrl").
--include("rabbit_mgmt.hrl").
-=======
->>>>>>> de821c33f8 (rabbitmq_management: speed up rabbit_mgmt_test_db_SUITE.erl)
 -include_lib("rabbitmq_ct_helpers/include/rabbit_mgmt_test.hrl").
 -import(rabbit_mgmt_test_util, [assert_list/2,
                                 reset_management_settings/1]).
