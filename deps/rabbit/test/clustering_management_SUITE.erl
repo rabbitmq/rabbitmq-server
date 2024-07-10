0a4e5a98458b5ae773062ddb4dea7e09f80599bf
Michael Davis <mcarsondavis@gmail.com>
2024-07-10 13:44:05 -0400

    Respect RABBITMQ_METADATA_STORE in clustering_management_SUITE

--- deps/rabbit/test/clustering_management_SUITE.erl
+++ deps/rabbit/test/clustering_management_SUITE.erl
@@ -129,10 +129,20 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
     rabbit_ct_helpers:run_teardown_steps(Config).
 
-init_per_group(mnesia_store, Config) ->
-    rabbit_ct_helpers:set_config(Config, [{metadata_store, mnesia}]);
 init_per_group(khepri_store, Config) ->
-    rabbit_ct_helpers:set_config(Config, [{metadata_store, khepri}]);
+    case rabbit_ct_broker_helpers:configured_metadata_store(Config) of
+        mnesia ->
+            {skip, "These tests target Khepri"};
+        _ ->
+            Config
+    end;
+init_per_group(mnesia_store, Config) ->
+    case rabbit_ct_broker_helpers:configured_metadata_store(Config) of
+        {khepri, _} ->
+            {skip, "These tests target mnesia"};
+        _ ->
+            Config
+    end;
 init_per_group(unclustered_2_nodes, Config) ->
     rabbit_ct_helpers:set_config(Config, [{rmq_nodes_clustered, false}]);
 init_per_group(unclustered_3_nodes, Config) ->
