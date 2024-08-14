1cd66e8788c5dbe6b95904d77c6f9c11362c875a
Michael Davis <mcarsondavis@gmail.com>
2024-08-14 15:11:28 -0400

    Handle database timeouts in `rabbit_amqqueue:store_queue/1`
    (cherry picked from commit 8eef20979109c3c7fd66da1ecda398c1055c6715)

--- deps/rabbit/src/rabbit_db_queue.erl
+++ deps/rabbit/src/rabbit_db_queue.erl
@@ -927,8 +927,9 @@ create_or_get_in_khepri(Q) ->
 %% set().
 %% -------------------------------------------------------------------
 
--spec set(Queue) -> ok when
-      Queue :: amqqueue:amqqueue().
+-spec set(Queue) -> Ret when
+      Queue :: amqqueue:amqqueue(),
+      Ret :: ok | rabbit_khepri:timeout_error().
 %% @doc Writes a queue record. If the queue is durable, it writes both instances:
 %% durable and transient. For the durable one, it resets decorators.
 %% The transient one is left as it is.
