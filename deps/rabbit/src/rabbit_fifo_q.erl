887c2ad020ff17a563a69f1997e8055f047235f3
David Ansari <david.ansari@gmx.de>
2024-08-20 11:14:21 +0200

    Rename quorum queue priority from "low" to "normal"
    Rename the two quorum queue priority levels from "low" and "high" to "normal" and
    "high". This improves user experience because the default priority level is low /
    normal. Prior to this commit users were confused why their messages show
    up as low priority. Furthermore there is no need to consult the docs to
    know whether the default priority level is low or high.
    
    (cherry picked from commit 1c6f4be308a50ae0f66fa345f43949a925382eea)

--- deps/rabbit/src/rabbit_fifo_q.erl
+++ deps/rabbit/src/rabbit_fifo_q.erl
@@ -18,8 +18,8 @@
 
 %% a weighted priority queue with only two priorities
 
--record(?MODULE, {hi = ?EMPTY :: {list(msg()), list(msg())},
-                  lo = ?EMPTY :: {list(msg()), list(msg())},
+-record(?MODULE, {hi = ?EMPTY :: {list(msg()), list(msg())}, %% high
+                  no = ?EMPTY :: {list(msg()), list(msg())}, %% normal
                   len = 0 :: non_neg_integer(),
                   dequeue_counter = 0 :: non_neg_integer()}).
 
@@ -31,12 +31,12 @@
 new() ->
     #?MODULE{}.
 
--spec in(hi | lo, msg(), state()) -> state().
+-spec in(hi | no, msg(), state()) -> state().
 in(hi, Item, #?MODULE{hi = Hi, len = Len} = State) ->
     State#?MODULE{hi = in(Item, Hi),
                   len = Len + 1};
-in(lo, Item, #?MODULE{lo = Lo, len = Len} = State) ->
-    State#?MODULE{lo = in(Item, Lo),
+in(no, Item, #?MODULE{no = No, len = Len} = State) ->
+    State#?MODULE{no = in(Item, No),
                   len = Len + 1}.
 
 -spec out(state()) ->
@@ -44,7 +44,7 @@ in(lo, Item, #?MODULE{lo = Lo, len = Len} = State) ->
 out(#?MODULE{len = 0}) ->
     empty;
 out(#?MODULE{hi = Hi0,
-             lo = Lo0,
+             no = No0,
              len = Len,
              dequeue_counter = C0} = State) ->
     C = case C0 of
@@ -58,8 +58,8 @@ out(#?MODULE{hi = Hi0,
             {Msg, State#?MODULE{hi = drop(Hi0),
                                 dequeue_counter = C,
                                 len = Len - 1}};
-        {lo, Msg} ->
-            {Msg, State#?MODULE{lo = drop(Lo0),
+        {no, Msg} ->
+            {Msg, State#?MODULE{no = drop(No0),
                                 dequeue_counter = C,
                                 len = Len - 1}}
     end.
@@ -78,21 +78,21 @@ len(#?MODULE{len = Len}) ->
 -spec from_lqueue(lqueue:lqueue(msg())) -> state().
 from_lqueue(LQ) ->
     lqueue:fold(fun (Item, Acc) ->
-                        in(lo, Item, Acc)
+                        in(no, Item, Acc)
                 end, new(), LQ).
 
 -spec get_lowest_index(state()) -> undefined | ra:index().
 get_lowest_index(#?MODULE{len = 0}) ->
     undefined;
-get_lowest_index(#?MODULE{hi = Hi, lo = Lo}) ->
+get_lowest_index(#?MODULE{hi = Hi, no = No}) ->
     case peek(Hi) of
         empty ->
-            ?MSG(LoIdx, _) = peek(Lo),
-            LoIdx;
+            ?MSG(NoIdx, _) = peek(No),
+            NoIdx;
         ?MSG(HiIdx, _) ->
-            case peek(Lo) of
-                ?MSG(LoIdx, _) ->
-                    min(HiIdx, LoIdx);
+            case peek(No) of
+                ?MSG(NoIdx, _) ->
+                    min(HiIdx, NoIdx);
                 empty ->
                     HiIdx
             end
@@ -101,38 +101,38 @@ get_lowest_index(#?MODULE{hi = Hi, lo = Lo}) ->
 -spec overview(state()) ->
     #{len := non_neg_integer(),
       num_hi := non_neg_integer(),
-      num_lo := non_neg_integer(),
+      num_no := non_neg_integer(),
       lowest_index := ra:index()}.
 overview(#?MODULE{len = Len,
                   hi = {Hi1, Hi2},
-                  lo = _} = State) ->
+                  no = _} = State) ->
     %% TODO: this could be very slow with large backlogs,
-    %% consider keeping a separate counter for hi, lo messages
+    %% consider keeping a separate counter for 'hi', 'no' messages
     NumHi = length(Hi1) + length(Hi2),
     #{len => Len,
       num_hi => NumHi,
-      num_lo => Len - NumHi,
+      num_no => Len - NumHi,
       lowest_index => get_lowest_index(State)}.
 
 %% internals
 
 next(#?MODULE{hi = ?NON_EMPTY = Hi,
-              lo = ?NON_EMPTY = Lo,
+              no = ?NON_EMPTY = No,
               dequeue_counter = ?WEIGHT}) ->
     ?MSG(HiIdx, _) = HiMsg = peek(Hi),
-    ?MSG(LoIdx, _) = LoMsg = peek(Lo),
+    ?MSG(NoIdx, _) = NoMsg = peek(No),
     %% always favour hi priority messages when it is safe to do so,
-    %% i.e. the index is lower than the next index for the lo queue
-    case HiIdx < LoIdx of
+    %% i.e. the index is lower than the next index for the 'no' queue
+    case HiIdx < NoIdx of
         true ->
             {hi, HiMsg};
         false ->
-            {lo, LoMsg}
+            {no, NoMsg}
     end;
 next(#?MODULE{hi = ?NON_EMPTY = Hi}) ->
     {hi, peek(Hi)};
-next(#?MODULE{lo = Lo}) ->
-    {lo, peek(Lo)}.
+next(#?MODULE{no = No}) ->
+    {no, peek(No)}.
 
 %% invariant, if the queue is non empty so is the Out (right) list.
 in(X, ?EMPTY) ->
