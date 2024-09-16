a65ceb6372bbc45a33339c985991e75dd7dda25f
Michael Davis <mcarsondavis@gmail.com>
2024-09-16 11:24:12 -0400

    rabbit_amqqueue: Catch exits when reading classic Q `consumers/1`
    `delegate:invoke/2` catches errors but not exits of the delegate
    process. Another process might query for a classic queue's consumers
    while the classic queue is being deleted or otherwise terminating and
    that would result in an exit of the calling process previously.

--- deps/rabbit/src/rabbit_amqqueue.erl
+++ deps/rabbit/src/rabbit_amqqueue.erl
@@ -1523,7 +1523,14 @@ notify_policy_changed(Q) when ?is_amqqueue(Q) ->
 
 consumers(Q) when ?amqqueue_is_classic(Q) ->
     QPid = amqqueue:get_pid(Q),
-    delegate:invoke(QPid, {gen_server2, call, [consumers, infinity]});
+    try
+        delegate:invoke(QPid, {gen_server2, call, [consumers, infinity]})
+    catch
+        exit:_ ->
+            %% The queue process exited during the call.
+            %% Note that `delegate:invoke/2' catches errors but not exits.
+            []
+    end;
 consumers(Q) when ?amqqueue_is_quorum(Q) ->
     QPid = amqqueue:get_pid(Q),
     case ra:local_query(QPid, fun rabbit_fifo:query_consumers/1) of
