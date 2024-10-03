e4abbfd6c243ffa517d5008413e264a1f11d46b7
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2024-10-03 12:54:24 +0200

    rabbit_feature_flags: Fix copyright year
    The subsystem didn't exist before 2019. The deprecated features support
    was added in 2023.

--- deps/rabbit/src/rabbit_depr_ff_extra.erl
+++ deps/rabbit/src/rabbit_depr_ff_extra.erl
@@ -2,7 +2,7 @@
 %% License, v. 2.0. If a copy of the MPL was not distributed with this
 %% file, You can obtain one at https://mozilla.org/MPL/2.0/.
 %%
-%% Copyright (c) 2023 Broadcom. All Rights Reserved. The term “Broadcom”
+%% Copyright (c) 2023-2024 Broadcom. All Rights Reserved. The term “Broadcom”
 %% refers to Broadcom Inc. and/or its subsidiaries.  All rights reserved.
 %%
 %% @doc
