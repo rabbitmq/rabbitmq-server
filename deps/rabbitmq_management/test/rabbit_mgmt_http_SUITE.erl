a621ef4737c384c315f92e483757f7cde6e8f941
Simon Unge <simunge@amazon.com>
2024-10-23 20:57:08 +0000

    Add cluster tag config that is exposed via HTTP /api/overview and CTL cluster_status
    (cherry picked from commit f5ef64ad06a084998394ccd86f1c295472bcd548)
    
    # Conflicts:
    #	deps/rabbitmq_management/test/rabbit_mgmt_http_SUITE.erl

--- deps/rabbitmq_management/test/rabbit_mgmt_http_SUITE.erl
+++ deps/rabbitmq_management/test/rabbit_mgmt_http_SUITE.erl
@@ -201,7 +201,17 @@ all_tests() -> [
     disabled_qq_replica_opers_test,
     qq_status_test,
     list_deprecated_features_test,
+<<<<<<< HEAD
     list_used_deprecated_features_test
+=======
+    list_used_deprecated_features_test,
+    connections_amqpl,
+    connections_amqp,
+    amqp_sessions,
+    amqpl_sessions,
+    enable_plugin_amqp,
+    cluster_tags_test
+>>>>>>> f5ef64ad06 (Add cluster tag config that is exposed via HTTP /api/overview and CTL cluster_status)
 ].
 
 %% -------------------------------------------------------------------
@@ -282,6 +292,11 @@ init_per_testcase(Testcase = disabled_qq_replica_opers_test, Config) ->
     rabbit_ct_broker_helpers:rpc_all(Config,
       application, set_env, [rabbitmq_management, restrictions, Restrictions]),
     rabbit_ct_helpers:testcase_started(Config, Testcase);
+init_per_testcase(Testcase = cluster_tags_test, Config) ->
+    Tags = [{<<"az">>, <<"us-east-3">>}, {<<"region">>,<<"us-east">>}, {<<"environment">>,<<"production">>}],
+    rabbit_ct_broker_helpers:rpc_all(Config,
+      application, set_env, [rabbit, cluster_tags, Tags]),
+    rabbit_ct_helpers:testcase_started(Config, Testcase);
 init_per_testcase(queues_detailed_test, Config) ->
     IsEnabled = rabbit_ct_broker_helpers:is_feature_flag_enabled(
                   Config, detailed_queues_endpoint),
@@ -347,6 +362,9 @@ end_per_testcase0(disabled_operator_policy_test, Config) ->
     Config;
 end_per_testcase0(disabled_qq_replica_opers_test, Config) ->
     rpc(Config, application, unset_env, [rabbitmq_management, restrictions]),
+    Config;
+end_per_testcase0(cluster_tags_test, Config) ->
+    rpc(Config, application, unset_env, [rabbit, cluster_tags]),
     Config;
 end_per_testcase0(Testcase, Config)
   when Testcase == list_deprecated_features_test;
@@ -3935,6 +3953,14 @@ list_used_deprecated_features_test(Config) ->
     ?assertEqual(atom_to_binary(?MODULE), maps:get(provided_by, Feature)),
     ?assertEqual(list_to_binary(Desc), maps:get(desc, Feature)),
     ?assertEqual(list_to_binary(DocUrl), maps:get(doc_url, Feature)).
+
+cluster_tags_test(Config) ->
+    Overview = http_get(Config, "/overview"),
+    Tags = maps:get(cluster_tags, Overview),
+    ExpectedTags = #{az => <<"us-east-3">>,environment => <<"production">>,
+                     region => <<"us-east">>},
+    ?assertEqual(ExpectedTags, Tags),
+    passed.
 
 %% -------------------------------------------------------------------
 %% Helpers.
