2e035dea3905a123143f6f0f79d173d55dbae6bb
Michael Klishin <mikhail.klishin@broadcom.com>
2024-11-09 16:16:35 -0500

    HTTP API: limit default body size for binding creation
    It does not need to use the "worst case scenario"
    default HTTP request body size limit that
    is primarily necessary because definition imports
    can be large (MiBs in size, for example).
    
    Since exchange, queue names and routing key
    have limits of 255 bytes and optional arguments
    can practically be expected to be short, we
    can lower the limit to < 10 KiB.
    
    (cherry picked from commit fb300d2a4b67b1cfeb433e7986e2f87d240f3198)

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_definitions.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_definitions.erl
@@ -84,9 +84,9 @@ all_definitions(ReqData, Context) ->
       Context).
 
 accept_json(ReqData0, Context) ->
-    case rabbit_mgmt_util:read_complete_body(ReqData0) of
+    BodySizeLimit = application:get_env(rabbitmq_management, max_http_body_size, ?MANAGEMENT_DEFAULT_HTTP_MAX_BODY_SIZE),
+    case rabbit_mgmt_util:read_complete_body_with_limit(ReqData0, BodySizeLimit) of
         {error, Reason} ->
-            BodySizeLimit = application:get_env(rabbitmq_management, max_http_body_size, ?MANAGEMENT_DEFAULT_HTTP_MAX_BODY_SIZE),
             _ = rabbit_log:warning("HTTP API: uploaded definition file exceeded the maximum request body limit of ~p bytes. "
                                    "Use the 'management.http.max_body_size' key in rabbitmq.conf to increase the limit if necessary", [BodySizeLimit]),
             rabbit_mgmt_util:bad_request(Reason, ReqData0, Context);
