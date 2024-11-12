05717ccccf34a03dec57534f922bcde4d6199ba7
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2024-11-12 18:43:16 +0100

    rabbit_khepri: Remove serial file during reset

--- deps/rabbit/src/rabbit_khepri.erl
+++ deps/rabbit/src/rabbit_khepri.erl
@@ -591,7 +591,10 @@ reset() ->
             %% Restart it.
             ok = setup(),
             ok = khepri_cluster:reset(?RA_CLUSTER_NAME),
-            ok = khepri:stop(?RA_CLUSTER_NAME);
+            ok = khepri:stop(?RA_CLUSTER_NAME),
+
+            _ = file:delete(rabbit_guid:filename()),
+            ok;
         true ->
             throw({error, rabbitmq_unexpectedly_running})
     end.
@@ -605,7 +608,10 @@ force_reset() ->
             DataDir = maps:get(data_dir, ra_system:fetch(coordination)),
             ok = ra_system:ensure_ra_system_stopped(coordination),
             ok = rabbit_file:recursive_delete(
-                   filelib:wildcard(DataDir ++ "/*"));
+                   filelib:wildcard(DataDir ++ "/*")),
+
+            _ = file:delete(rabbit_guid:filename()),
+            ok;
         true ->
             throw({error, rabbitmq_unexpectedly_running})
     end.
