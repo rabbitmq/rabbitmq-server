6bf27a212f5e058a935ed01b05ab4033d0068d18
Marcial Rosales <marcial.rosales@broadcom.com>
2024-11-14 19:38:27 +0100

    Use tls in oauth providers and rabbitmq

--- selenium/test/authnz-msg-protocols/mqtt.js
+++ selenium/test/authnz-msg-protocols/mqtt.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const assert = require('assert')
 const { tokenFor, openIdConfiguration } = require('../utils')
 const { reset, expectUser, expectVhost, expectResource, allow, verifyAll } = require('../mock_http_backend')
@@ -14,11 +15,14 @@ for (const element of profiles.split(" ")) {
 describe('Having MQTT protocol enbled and the following auth_backends: ' + backends, function () {
   let mqttOptions
   let expectations = []
-  let client_id = 'selenium-client'
+  let mqttProtocol = process.env.MQTT_PROTOCOL || 'mqtt'
+  let usemtls = process.env.MQTT_USE_MTLS || false
   let rabbit = process.env.RABBITMQ_HOSTNAME || 'localhost'
+  let mqttUrl = process.env.RABBITMQ_MQTT_URL || "mqtt://" + rabbit + ":1883"
   let username = process.env.RABBITMQ_AMQP_USERNAME
   let password = process.env.RABBITMQ_AMQP_PASSWORD
-
+  let client_id = process.env.RABBITMQ_AMQP_USERNAME || 'selenium-client'
+  
   before(function () {
     if (backends.includes("http") && username.includes("http")) {
       reset()
@@ -36,17 +40,26 @@ describe('Having MQTT protocol enbled and the following auth_backends: ' + backe
     mqttOptions = {
       clientId: client_id,
       protocolId: 'MQTT',
+      protocol: mqttProtocol,
       protocolVersion: 4,
       keepalive: 10000,
       clean: false,
-      reconnectPeriod: '1000',
-      username: username,
-      password: password,
+      reconnectPeriod: '1000'
+    }
+    if (mqttProtocol == 'mqtts') {
+      mqttOptions["ca"] = [fs.readFileSync(process.env.RABBITMQ_CERTS + "/ca_rabbitmq_certificate.pem")]      
+    } 
+    if (usemtls) {
+      mqttOptions["cert"] = fs.readFileSync(process.env.RABBITMQ_CERTS + "/client_rabbitmq_certificate.pem")
+      mqttOptions["key"] = fs.readFileSync(process.env.RABBITMQ_CERTS + "/client_rabbitmq_key.pem")
+    } else {
+      mqttOptions["username"] = username
+      mqttOptions["password"] = password
     }
   })
 
   it('can open an MQTT connection', function () {
-    var client = mqtt.connect("mqtt://" + rabbit + ":1883", mqttOptions)
+    var client = mqtt.connect(mqttUrl, mqttOptions)
     client.on('error', function(err) {
       assert.fail("Mqtt connection failed due to " + err)
       client.end()
