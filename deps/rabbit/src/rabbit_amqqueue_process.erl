6a979b6c7dcd74ed37e7bc55378ad52f4db19fc4
Matteo Cafasso <noxdafox@gmail.com>
2024-12-10 23:57:44 +0200

    amqqueue_process: adopt new `is_duplicate` backing queue callback
    As the de-duplication plugin is the only adopter of the `is_duplicate`
    callback, we now use a simpler signature.
    
    When a message is deemed duplicated, we discard it and re-route it to
    dead letter exchange.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- deps/rabbit/src/rabbit_amqqueue_process.erl
+++ deps/rabbit/src/rabbit_amqqueue_process.erl
@@ -722,16 +722,27 @@ maybe_deliver_or_enqueue(Delivery = #delivery{message = Message},
             %% Drop publish and nack to publisher
             send_reject_publish(Delivery, State);
         _ ->
-            {IsDuplicate, BQS1} = BQ:is_duplicate(Message, BQS),
-            State1 = State#q{backing_queue_state = BQS1},
-            case IsDuplicate of
-                true -> State1;
-                {true, drop} -> State1;
-                %% Drop publish and nack to publisher
-                {true, reject} ->
+            case BQ:is_duplicate(Message, BQS) of
+                {true, State1} ->
+                    %% Publish to DLX
+                    _ = with_dlx(
+                          DLX,
+                          fun (X) ->
+                                  rabbit_global_counters:messages_dead_lettered(maxlen,
+                                                                                rabbit_classic_queue,
+                                                                                at_most_once, 1),
+                                  QName = qname(State1),
+                                  rabbit_dead_letter:publish(Message, maxlen, X, RK, QName)
+                          end,
+                          fun () ->
+                                  rabbit_global_counters:messages_dead_lettered(maxlen,
+                                                                                rabbit_classic_queue,
+                                                                                disabled, 1)
+                          end),
+                    %% Drop publish and nack to publisher
                     send_reject_publish(Delivery, State1);
                 %% Enqueue and maybe drop head later
-                false ->
+                {false, State1} ->
                     deliver_or_enqueue(Delivery, Delivered, State1)
             end
     end.
