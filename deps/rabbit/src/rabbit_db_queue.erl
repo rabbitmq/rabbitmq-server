c607ed8a98c1bc03e81f2bdfd7a9018ee013caf7
Michael Davis <mcarsondavis@gmail.com>
2024-12-18 17:14:39 -0500

    rabbit_db_*: Handle breaking change in khepri adv API return type
    All callers of `khepri_adv` and `khepri_tx_adv` need updates to handle
    the now consistent return type of `khepri:node_props_map()` in Khepri
    0.17.
    
    We don't need any compatibility code to handle "either the old return
    type or the new return type" because the translation is done entirely
    in the "client side" code in Khepri - meaning that the return value from
    the Ra server is the same but it is translated differently by the
    functions in `khepri_adv` and `khepri_tx_adv`.

--- deps/rabbit/src/rabbit_db_queue.erl
+++ deps/rabbit/src/rabbit_db_queue.erl
@@ -412,7 +412,7 @@ delete_in_khepri(QueueName, OnlyDurable) ->
       fun () ->
               Path = khepri_queue_path(QueueName),
               case khepri_tx_adv:delete(Path) of
-                  {ok, #{data := _}} ->
+                  {ok, #{Path := #{data := _}}} ->
                       %% we want to execute some things, as decided by rabbit_exchange,
                       %% after the transaction.
                       rabbit_db_binding:delete_for_destination_in_khepri(QueueName, OnlyDurable);
@@ -607,7 +607,7 @@ update_in_khepri(QName, Fun) ->
     Path = khepri_queue_path(QName),
     Ret1 = rabbit_khepri:adv_get(Path),
     case Ret1 of
-        {ok, #{data := Q, payload_version := Vsn}} ->
+        {ok, #{Path := #{data := Q, payload_version := Vsn}}} ->
             UpdatePath = khepri_path:combine_with_conditions(
                            Path, [#if_payload_version{version = Vsn}]),
             Q1 = Fun(Q),
@@ -658,7 +658,7 @@ update_decorators_in_khepri(QName, Decorators) ->
     Path = khepri_queue_path(QName),
     Ret1 = rabbit_khepri:adv_get(Path),
     case Ret1 of
-        {ok, #{data := Q1, payload_version := Vsn}} ->
+        {ok, #{Path := #{data := Q1, payload_version := Vsn}}} ->
             Q2 = amqqueue:set_decorators(Q1, Decorators),
             UpdatePath = khepri_path:combine_with_conditions(
                            Path, [#if_payload_version{version = Vsn}]),
@@ -1098,15 +1098,12 @@ delete_transient_in_khepri(FilterFun) ->
     case rabbit_khepri:adv_get_many(PathPattern) of
         {ok, Props} ->
             Qs = maps:fold(
-                   fun(Path0, #{data := Q, payload_version := Vsn}, Acc)
+                   fun(Path, #{data := Q, payload_version := Vsn}, Acc)
                        when ?is_amqqueue(Q) ->
                            case FilterFun(Q) of
                                true ->
-                                   Path = khepri_path:combine_with_conditions(
-                                            Path0,
-                                            [#if_payload_version{version = Vsn}]),
                                    QName = amqqueue:get_name(Q),
-                                   [{Path, QName} | Acc];
+                                   [{Path, Vsn, QName} | Acc];
                                false ->
                                    Acc
                            end
@@ -1125,20 +1122,7 @@ do_delete_transient_queues_in_khepri([], _FilterFun) ->
 do_delete_transient_queues_in_khepri(Qs, FilterFun) ->
     Res = rabbit_khepri:transaction(
             fun() ->
-                    rabbit_misc:fold_while_ok(
-                      fun({Path, QName}, Acc) ->
-                              %% Also see `delete_in_khepri/2'.
-                              case khepri_tx_adv:delete(Path) of
-                                  {ok, #{data := _}} ->
-                                      Deletions = rabbit_db_binding:delete_for_destination_in_khepri(
-                                                    QName, false),
-                                      {ok, [{QName, Deletions} | Acc]};
-                                  {ok, _} ->
-                                      {ok, Acc};
-                                  {error, _} = Error ->
-                                      Error
-                              end
-                      end, [], Qs)
+                    do_delete_transient_queues_in_khepri_tx(Qs, [])
             end),
     case Res of
         {ok, Items} ->
@@ -1151,6 +1135,24 @@ do_delete_transient_queues_in_khepri(Qs, FilterFun) ->
         {error, _} = Error ->
             Error
     end.
+
+do_delete_transient_queues_in_khepri_tx([], Acc) ->
+    {ok, Acc};
+do_delete_transient_queues_in_khepri_tx([{Path, Vsn, QName} | Rest], Acc) ->
+     %% Also see `delete_in_khepri/2'.
+     VersionedPath = khepri_path:combine_with_conditions(
+                       Path, [#if_payload_version{version = Vsn}]),
+     case khepri_tx_adv:delete(VersionedPath) of
+         {ok, #{Path := #{data := _}}} ->
+             Deletions = rabbit_db_binding:delete_for_destination_in_khepri(
+                           QName, false),
+             Acc1 = [{QName, Deletions} | Acc],
+             do_delete_transient_queues_in_khepri_tx(Rest, Acc1);
+         {ok, _} ->
+             do_delete_transient_queues_in_khepri_tx(Rest, Acc);
+         {error, _} = Error ->
+             Error
+     end.
 
 %% -------------------------------------------------------------------
 %% foreach_transient().
