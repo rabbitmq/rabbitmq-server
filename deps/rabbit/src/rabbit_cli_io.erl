3bf2f7e374a3c330b0e6b2f470a92aaa27df9b45
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2025-01-02 16:30:58 +0100

    Add JSON formatting

--- deps/rabbit/src/rabbit_cli_io.erl
+++ deps/rabbit/src/rabbit_cli_io.erl
@@ -45,7 +45,7 @@ argparse_def(record_stream) ->
          long => "-format",
          short => $f,
          type => {atom, [plain, json]},
-         nargs => 1,
+         default => plain,
          help => "Format output acccording to <FORMAT>"}
       ]
      }.
@@ -80,20 +80,17 @@ init(#{progname := Progname}) ->
     {ok, State}.
 
 handle_call(
-  {start_record_stream, Name, Fields, _ArgMap},
+  {start_record_stream, Name, Fields, ArgMap},
   From,
   #?MODULE{record_streams = Streams} = State) ->
-    Stream = #{name => Name, fields => Fields},
-    gen_server:reply(From, {ok, Stream}),
-
-    FieldNames = [atom_to_list(FieldName)
-                  || #{name := FieldName} <- Fields],
-    Header = string:join(FieldNames, "\t"),
-    io:format("~ts~n", [Header]),
-
+    Stream = #{name => Name, fields => Fields, arg_map => ArgMap},
     Streams1 = Streams#{Name => Stream},
     State1 = State#?MODULE{record_streams = Streams1},
-    {noreply, State1};
+    gen_server:reply(From, {ok, Stream}),
+
+    {ok, State2} = format_record_stream_start(Name, State1),
+
+    {noreply, State2};
 handle_call(stop, _From, State) ->
     {stop, normal, ok, State};
 handle_call(_Request, _From, State) ->
@@ -109,16 +106,12 @@ handle_cast(
     Help = argparse:help(ArgparseDef, Options),
     io:format("~s~n", [Help]),
     {noreply, State};
-handle_cast(
-  {push_new_record, Name, Record},
-  #?MODULE{record_streams = Streams} = State) ->
-    #{fields := Fields} = maps:get(Name, Streams),
-    Values = format_fields(Fields, Record),
-    Line = string:join(Values, "\t"),
-    io:format("~ts~n", [Line]),
-    {noreply, State};
-handle_cast({end_record_stream, _Name}, State) ->
-    {noreply, State};
+handle_cast({push_new_record, Name, Record}, State) ->
+    {ok, State1} = format_record(Name, Record, State),
+    {noreply, State1};
+handle_cast({end_record_stream, Name}, State) ->
+    {ok, State1} = format_record_stream_end(Name, State),
+    {noreply, State1};
 handle_cast(_Request, State) ->
     {noreply, State}.
 
@@ -131,11 +124,106 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+format_record_stream_start(
+  Name,
+  #?MODULE{record_streams = Streams} = State) ->
+    Stream = maps:get(Name, Streams),
+    format_record_stream_start1(Stream, State).
+
+format_record_stream_start1(
+  #{name := Name, fields := Fields, arg_map := #{format := plain}} = Stream,
+  #?MODULE{record_streams = Streams} = State) ->
+    FieldNames = [atom_to_list(FieldName) || #{name := FieldName} <- Fields],
+    FieldWidths = [case Field of
+                       #{type := string, name := FieldName} ->
+                           lists:max([length(atom_to_list(FieldName)), 20]);
+                       #{name := FieldName} ->
+                           length(atom_to_list(FieldName))
+                   end || Field <- Fields],
+    Format0 = [rabbit_misc:format("~~-~b.. ts", [Width])
+               || Width <- FieldWidths],
+    Format1 = string:join(Format0, " "),
+    case isatty(standard_io) of
+        true ->
+            io:format("\033[1m" ++ Format1 ++ "\033[0m~n", FieldNames);
+        false ->
+            io:format(Format1 ++ "~n", FieldNames)
+    end,
+    Stream1 = Stream#{format => Format1},
+    Streams1 = Streams#{Name => Stream1},
+    State1 = State#?MODULE{record_streams = Streams1},
+    {ok, State1};
+format_record_stream_start1(
+  #{name := Name, arg_map := #{format := json}} = Stream,
+  #?MODULE{record_streams = Streams} = State) ->
+    Stream1 = Stream#{emitted_fields => 0},
+    Streams1 = Streams#{Name => Stream1},
+    State1 = State#?MODULE{record_streams = Streams1},
+    {ok, State1}.
+
+format_record(Name, Record, #?MODULE{record_streams = Streams} = State) ->
+    Stream = maps:get(Name, Streams),
+    format_record1(Stream, Record, State).
+
+format_record1(
+  #{fields := Fields, arg_map := #{format := plain},
+    format := Format},
+  Record,
+  State) ->
+    Values = format_fields(Fields, Record),
+    io:format(Format ++ "~n", Values),
+    {ok, State};
+format_record1(
+  #{fields := Fields, arg_map := #{format := json},
+    name := Name, emitted_fields := Emitted} = Stream,
+  Record,
+  #?MODULE{record_streams = Streams} = State) ->
+    Fields1 = [FieldName || #{name := FieldName} <- Fields],
+    Struct = lists:zip(Fields1, Record),
+    Json = json:encode(
+             Struct,
+             fun
+                 ([{_, _} | _] = Value, Encode) ->
+                     json:encode_key_value_list(Value, Encode);
+                 (Value, Encode) ->
+                     json:encode_value(Value, Encode)
+             end),
+    case Emitted of
+        0 ->
+            io:format("[~n ~ts", [Json]);
+        _ ->
+            io:format(",~n ~ts", [Json])
+    end,
+    Stream1 = Stream#{emitted_fields => Emitted + 1},
+    Streams1 = Streams#{Name => Stream1},
+    State1 = State#?MODULE{record_streams = Streams1},
+    {ok, State1}.
+
+format_record_stream_end(
+  Name,
+  #?MODULE{record_streams = Streams} = State) ->
+    Stream = maps:get(Name, Streams),
+    {ok, State1} = format_record_stream_end1(Stream, State),
+    #?MODULE{record_streams = Streams1} = State1,
+    Streams2 = maps:remove(Name, Streams1),
+    State2 = State1#?MODULE{record_streams = Streams2},
+    {ok, State2}.
+
+format_record_stream_end1(#{arg_map := #{format := plain}}, State) ->
+    {ok, State};
+format_record_stream_end1(#{arg_map := #{format := json}}, State) ->
+    io:format("~n]~n", []),
+    {ok, State}.
+
 format_fields(Fields, Values) ->
     format_fields(Fields, Values, []).
 
 format_fields([#{type := string} | Rest1], [Value | Rest2], Acc) ->
     String = io_lib:format("~ts", [Value]),
+    Acc1 = [String | Acc],
+    format_fields(Rest1, Rest2, Acc1);
+format_fields([#{type := binary} | Rest1], [Value | Rest2], Acc) ->
+    String = io_lib:format("~-20.. ts", [Value]),
     Acc1 = [String | Acc],
     format_fields(Rest1, Rest2, Acc1);
 format_fields([#{type := integer} | Rest1], [Value | Rest2], Acc) ->
@@ -146,14 +234,18 @@ format_fields([#{type := boolean} | Rest1], [Value | Rest2], Acc) ->
     String = io_lib:format("~ts", [if Value -> "☑"; true -> "☐" end]),
     Acc1 = [String | Acc],
     format_fields(Rest1, Rest2, Acc1);
-format_fields([#{type := resource} | Rest1], [Value | Rest2], Acc) ->
-    #resource{name = Name} = Value,
-    String = io_lib:format("~ts", [Name]),
-    Acc1 = [String | Acc],
-    format_fields(Rest1, Rest2, Acc1);
 format_fields([#{type := term} | Rest1], [Value | Rest2], Acc) ->
     String = io_lib:format("~0p", [Value]),
     Acc1 = [String | Acc],
     format_fields(Rest1, Rest2, Acc1);
 format_fields([], [], Acc) ->
     lists:reverse(Acc).
+
+isatty(IoDevice) ->
+    Opts = io:getopts(IoDevice),
+    case proplists:get_value(stdout, Opts) of
+        true ->
+            true;
+        _ ->
+            false
+    end.
