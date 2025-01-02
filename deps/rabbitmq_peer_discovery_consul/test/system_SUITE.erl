d542e3f3dd29807302e5ef341a3cb2d8d96102a4
Michael Klishin <michaelklishin@icloud.com>
2025-01-02 14:39:22 -0500

    v3.13.x: update copyright year
    References #13008.

--- deps/rabbitmq_peer_discovery_consul/test/system_SUITE.erl
+++ deps/rabbitmq_peer_discovery_consul/test/system_SUITE.erl
@@ -2,7 +2,7 @@
 %% License, v. 2.0. If a copy of the MPL was not distributed with this
 %% file, You can obtain one at https://mozilla.org/MPL/2.0/.
 %%
-%% Copyright (c) 2024 Broadcom. The term “Broadcom” refers to Broadcom Inc.
+%% Copyright (c) 2007-2025 Broadcom. The term “Broadcom” refers to Broadcom Inc.
 %% and/or its subsidiaries. All rights reserved. All rights reserved.
 %%
 
