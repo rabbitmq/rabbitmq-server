cdc8f22d38a0fb8bd75dcb7d09462981c20ea6a1
Marcial Rosales <marcial.rosales@broadcom.com>
2025-03-10 12:11:50 +0100

    Use POST+Redirect_with_cookie
    For idp-initiated logon

--- deps/rabbitmq_management/src/rabbit_mgmt_login.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_login.erl
@@ -16,23 +16,34 @@ init(Req0, State) ->
   login(cowboy_req:method(Req0), Req0, State).
 
 login(<<"POST">>, Req0, State) ->
-  {ok, Body, _} = cowboy_req:read_urlencoded_body(Req0),
-  AccessToken = proplists:get_value(<<"access_token">>, Body),
-  case rabbit_mgmt_util:is_authorized_user(Req0, #context{}, <<"">>, AccessToken, false) of
-    {true, Req1, _} ->
-      NewBody = ["<html><head></head><body><script src='js/prefs.js'></script><script type='text/javascript'>",
-          "set_token_auth('", AccessToken, "'); window.location = '", rabbit_mgmt_util:get_path_prefix(),
-          "/'</script></body></html>"],
-      Req2 = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html; charset=utf-8">>}, NewBody, Req1),
-      {ok, Req2, State};
-    {false, ReqData1, Reason} ->
-      Home = cowboy_req:uri(ReqData1, #{path => rabbit_mgmt_util:get_path_prefix() ++ "/", qs => "error=" ++ Reason}),
-      ReqData2 = cowboy_req:reply(302,
-              #{<<"Location">> => iolist_to_binary(Home) },
-              <<>>, ReqData1),
-      {ok, ReqData2, State}
-  end;
+    {ok, Body, _} = cowboy_req:read_urlencoded_body(Req0),
+    AccessToken = proplists:get_value(<<"access_token">>, Body),
+    case rabbit_mgmt_util:is_authorized_user(Req0, #context{}, <<"">>, AccessToken, false) of
+        {true, Req1, _} ->     
+            SetCookie = cowboy_req:set_resp_cookie(<<"token">>, AccessToken, Req1),    
+            Home = cowboy_req:uri(SetCookie, #{
+                path => rabbit_mgmt_util:get_path_prefix() ++ "/"
+            }),
+            Redirect = cowboy_req:reply(302, #{
+                 <<"Location">> => iolist_to_binary(Home) 
+            }, <<>>, SetCookie),      
+            {ok, Redirect, State};
+        {false, ReqData1, Reason} ->
+            replyWithError(Reason, ReqData1, State)
+    end;
 
 login(_, Req0, State) ->
     %% Method not allowed.
     {ok, cowboy_req:reply(405, Req0), State}.
+
+replyWithError(Reason, Req, State) ->
+    Home = cowboy_req:uri(Req, #{
+        path => rabbit_mgmt_util:get_path_prefix() ++ "/", 
+        qs => "error=" ++ Reason
+    }),
+    Req2 = cowboy_req:reply(302, #{
+        <<"Location">> => iolist_to_binary(Home) 
+    }, <<>>, Req),
+    {ok, Req2, State}.
+
+
