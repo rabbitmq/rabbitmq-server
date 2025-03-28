3756775ebe505ac1a1441c83ae93059ef8c9da9b
Michael Klishin <mikhail.klishin@broadcom.com>
2025-03-28 02:31:09 -0400

    Revert "Redirect to end_session_endpoint for idp_initiated logon when it is configured"

--- selenium/suites/authnz-mgt/oauth-idp-initiated-with-uaa-via-proxy.sh
+++ selenium/suites/authnz-mgt/oauth-idp-initiated-with-uaa-via-proxy.sh
@@ -4,7 +4,7 @@ SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TEST_CASES_PATH=/oauth/with-idp-initiated-via-proxy
 TEST_CONFIG_PATH=/oauth
-PROFILES="uaa fakeportal fakeproxy fakeportal-mgt-oauth-provider idp-initiated uaa-oauth-provider tls"
+PROFILES="uaa fakeportal fakeproxy fakeportal-mgt-oauth-provider idp-initiated uaa-oauth-provider"
 
 source $SCRIPT/../../bin/suite_template $@
 runWith rabbitmq uaa fakeportal fakeproxy
