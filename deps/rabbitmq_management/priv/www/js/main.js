61b0f152fdf23c004d20566d49f0d89ac510f037
Luke Bakken <lrbakken@amazon.com>
2025-04-28 12:18:06 -0700

    Remove unused Javascript functions
    A security scanner flagged the use of `RegExp` with unsanitized input.
    Turns out, these functions are no longer used and can be deleted.

--- deps/rabbitmq_management/priv/www/js/main.js
+++ deps/rabbitmq_management/priv/www/js/main.js
@@ -81,15 +81,6 @@ function dispatcher() {
     }
 }
 
-function getParameterByName(name) {
-    var match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
-
-function getAccessToken() {
-    return getParameterByName('access_token');
-}
-
 function start_app_login () {
   app = new Sammy.Application(function () {
     this.get('/', function () {})
