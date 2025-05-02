bbeef880bf45f8287bd5602d50e8e2b9a38a40bf
Marcial Rosales <marcial.rosales@broadcom.com>
2025-05-02 16:12:32 +0200

    Test columns available for queues and stream
    (cherry picked from commit ba0510f85de17fd5d78c19c2772531d5954d56a0)

--- selenium/test/queuesAndStreams/list.js
+++ selenium/test/queuesAndStreams/list.js
@@ -1,7 +1,7 @@
 const { By, Key, until, Builder } = require('selenium-webdriver')
 require('chromedriver')
 const assert = require('assert')
-const { buildDriver, goToHome, captureScreensFor, teardown, delay } = require('../utils')
+const { buildDriver, goToHome, captureScreensFor, teardown, doWhile } = require('../utils')
 
 const LoginPage = require('../pageobjects/LoginPage')
 const OverviewPage = require('../pageobjects/OverviewPage')
@@ -39,6 +39,67 @@ describe('Queues and Streams management', function () {
   it('display summary of queues and streams', async function () {
     let text = await queuesAndStreams.getPagingSectionHeaderText()
     assert.equal(true, text.startsWith('All queues') )
+  })
+
+  it('queue selectable columns', async function () {  
+    await overview.clickOnOverviewTab()
+    await overview.clickOnQueuesTab()
+    await doWhile(async function() { return queuesAndStreams.getQueuesTable() },
+      function(table) { 
+        return table.length > 0
+      })
+
+    await queuesAndStreams.clickOnSelectTableColumns()
+    let table = await queuesAndStreams.getSelectableTableColumns()
+    console.log("table: " + JSON.stringify(table))
+    
+    assert.equal(4, table.length)
+    let overviewGroup = { 
+        "name" : "Overview:",
+        "columns": [
+          {"name:":"Type","id":"checkbox-queues-type"},
+          {"name:":"Features (with policy)","id":"checkbox-queues-features"},
+          {"name:":"Features (no policy)","id":"checkbox-queues-features_no_policy"},
+          {"name:":"Policy","id":"checkbox-queues-policy"},
+          {"name:":"Consumer count","id":"checkbox-queues-consumers"},
+          {"name:":"Consumer capacity","id":"checkbox-queues-consumer_capacity"},
+          {"name:":"State","id":"checkbox-queues-state"}
+        ]
+    }    
+    assert.equal(JSON.stringify(table[0]), JSON.stringify(overviewGroup))
+    let messagesGroup = { 
+      "name" : "Messages:",
+      "columns": [
+        {"name:":"Ready","id":"checkbox-queues-msgs-ready"},
+        {"name:":"Unacknowledged","id":"checkbox-queues-msgs-unacked"},
+        {"name:":"In memory","id":"checkbox-queues-msgs-ram"},
+        {"name:":"Persistent","id":"checkbox-queues-msgs-persistent"},
+        {"name:":"Total","id":"checkbox-queues-msgs-total"}
+      ]
+    }    
+    assert.equal(JSON.stringify(table[1]), JSON.stringify(messagesGroup))    
+    let messageBytesGroup = { 
+      "name" : "Message bytes:",
+      "columns": [
+        {"name:":"Ready","id":"checkbox-queues-msg-bytes-ready"},
+        {"name:":"Unacknowledged","id":"checkbox-queues-msg-bytes-unacked"},
+        {"name:":"In memory","id":"checkbox-queues-msg-bytes-ram"},
+        {"name:":"Persistent","id":"checkbox-queues-msg-bytes-persistent"},
+        {"name:":"Total","id":"checkbox-queues-msg-bytes-total"}
+      ]
+    }
+    assert.equal(JSON.stringify(table[2]), JSON.stringify(messageBytesGroup))
+    let messageRatesGroup = { 
+      "name" : "Message rates:",
+      "columns": [
+        {"name:":"incoming","id":"checkbox-queues-rate-incoming"},
+        {"name:":"deliver / get","id":"checkbox-queues-rate-deliver"},
+        {"name:":"redelivered","id":"checkbox-queues-rate-redeliver"},
+        {"name:":"ack","id":"checkbox-queues-rate-ack"}
+      ]
+    }
+    assert.equal(JSON.stringify(table[3]), JSON.stringify(messageRatesGroup))
+
   })
 
   after(async function () {
