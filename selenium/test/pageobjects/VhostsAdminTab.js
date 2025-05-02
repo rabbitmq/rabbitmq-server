4fd25e05181f0d97c89cedd7f9e902c5503d5429
Marcial Rosales <marcial.rosales@broadcom.com>
2025-05-02 14:56:49 +0200

    Testing new vhost has the tag
    (cherry picked from commit ea66a25dfa5d3a192354efa8fd335d2dc22b76c6)

--- selenium/test/pageobjects/VhostsAdminTab.js
+++ selenium/test/pageobjects/VhostsAdminTab.js
@@ -9,7 +9,7 @@ const FILTER_VHOST = By.css('div#main div.filter input#filter')
 const CHECKBOX_REGEX = By.css('div#main div.filter input#filter-regex-mode')
 
 const VHOSTS_TABLE_ROWS = By.css('div#main table.list tbody tr')
-const TABLE_SECTION = By.css('div#main table.list')
+const TABLE_SECTION = By.css('div#main div#vhosts.section table.list')
 
 module.exports = class VhostsAdminTab extends AdminTab {
   async isLoaded () {
