62cbb987370d10c790f7da081f07c2b41af0a200
David Ansari <david.ansari@gmx.de>
2025-05-28 17:53:38 +0200

    Make map operations deterministic in quorum queues
    Prior to this commit map iteration order was undefined in quorum queues
    and could therefore be different on different versions of Erlang/OTP.
    
    Example:
    
    OTP 26.2.5.3
    ```
    Erlang/OTP 26 [erts-14.2.5.3] [source] [64-bit] [smp:12:12] [ds:12:12:10] [async-threads:1] [jit]
    
    Eshell V14.2.5.3 (press Ctrl+G to abort, type help(). for help)
    1> maps:foreach(fun(K, _) -> io:format("~b,", [K]) end, maps:from_keys(lists:seq(1, 33), ok)).
    4,25,8,1,23,10,7,9,11,12,28,24,13,3,18,29,26,22,19,2,33,21,32,20,17,30,14,5,6,27,16,31,15,ok
    ```
    
    OTP 27.3.3
    ```
    Erlang/OTP 27 [erts-15.2.6] [source] [64-bit] [smp:12:12] [ds:12:12:10] [async-threads:1] [jit]
    
    Eshell V15.2.6 (press Ctrl+G to abort, type help(). for help)
    1> maps:foreach(fun(K, _) -> io:format("~b,", [K]) end, maps:from_keys(lists:seq(1, 33), ok)).
    18,4,12,19,29,13,2,7,31,8,10,23,9,15,32,1,25,28,20,6,11,17,24,14,33,3,16,30,21,5,27,26,22,ok
    ```
    
    This can lead to non-determinism on different members. For example, different
    members could potentially return messages in a different order.
    
    This commit introduces a new machine version fixing this bug.

--- deps/rabbit/src/rabbit_fifo_index.erl
+++ deps/rabbit/src/rabbit_fifo_index.erl
@@ -1,3 +1,9 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2025 Broadcom. All Rights Reserved. The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries. All rights reserved.
+
 -module(rabbit_fifo_index).
 
 -export([
