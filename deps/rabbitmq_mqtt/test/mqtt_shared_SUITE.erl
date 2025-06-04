bc7a8be85a9651c4adc4089556e87698363b7957
Marcial Rosales <marcial.rosales@broadcom.com>
2025-06-04 16:14:46 +0200

    Move test to v5
    because it is a feature exclusive of v5

--- deps/rabbitmq_mqtt/test/mqtt_shared_SUITE.erl
+++ deps/rabbitmq_mqtt/test/mqtt_shared_SUITE.erl
@@ -112,8 +112,7 @@ cluster_size_1_tests() ->
      ,keepalive_turned_off
      ,block
      ,amqp_to_mqtt_qos0
-     ,clean_session_disconnect_client
-     ,zero_session_expiry_interval_disconnect_client
+     ,clean_session_disconnect_client     
      ,clean_session_node_restart
      ,clean_session_node_kill
      ,rabbit_status_connection_count
@@ -1579,18 +1578,6 @@ clean_session_disconnect_client(Config) ->
     QsClassic = rpc(Config, rabbit_amqqueue, list_by_type, [rabbit_classic_queue]),
     ?assertEqual(1, length(QsQos0)),
     ?assertEqual(1, length(QsClassic)),
-
-    ok = emqtt:disconnect(C),
-    %% After terminating a clean session, we expect any session state to be cleaned up on the server.
-    timer:sleep(200), %% Give some time to clean up exclusive classic queue.
-    L = rpc(Config, rabbit_amqqueue, list, []),
-    ?assertEqual(0, length(L)).
-
-zero_session_expiry_interval_disconnect_client(Config) ->
-    C = connect(?FUNCTION_NAME, Config, [{properties, #{'Session-Expiry-Interval' => 0}}]),
-    {ok, _, _} = emqtt:subscribe(C, <<"topic0">>, qos0),
-    QsQos0 = rpc(Config, rabbit_amqqueue, list_by_type, [rabbit_mqtt_qos0_queue]),
-    ?assertEqual(1, length(QsQos0)),
 
     ok = emqtt:disconnect(C),
     %% After terminating a clean session, we expect any session state to be cleaned up on the server.
