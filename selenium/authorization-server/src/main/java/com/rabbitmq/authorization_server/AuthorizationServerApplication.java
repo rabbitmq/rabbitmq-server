81ea448a4ecacfb0775aabed1515057587579b55
Marcial Rosales <marcial.rosales@broadcom.com>
2025-06-06 15:35:02 +0200

    Add spring-authorization-server for selenium
    And ci job to build it

---
+++ selenium/authorization-server/src/main/java/com/rabbitmq/authorization_server/AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
