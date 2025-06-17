29e714ccece967e09cc9b18299a17aa25f94ef8f
Marcial Rosales <marcial.rosales@broadcom.com>
2025-06-17 11:37:38 +0200

    Fix some issues to obtain a token
    using client_credentials with spring

--- selenium/suites/authnz-messaging/auth-oauth-backend-with-opaque-tokens.sh
+++ selenium/suites/authnz-messaging/auth-oauth-backend-with-opaque-tokens.sh
@@ -3,7 +3,7 @@
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TEST_CASES_PATH=/authnz-msg-protocols
-PROFILES="spring oauth-producer auth-oauth-spring auth_backends-oauth-opaque "
+PROFILES="spring oauth-producer auth-oauth-spring auth_backends-opaque-oauth auth-mtls"
 
 source $SCRIPT/../../bin/suite_template
 runWith spring
