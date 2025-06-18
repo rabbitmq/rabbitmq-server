8c40f0b76e7fc5a5720308e436543a3063656154
Marcial Rosales <marcial.rosales@broadcom.com>
2025-06-18 09:24:20 +0200

    Move changes from PR that created the spring auth server

--- selenium/suites/authnz-messaging/auth-internal-mtls-backend.sh
+++ selenium/suites/authnz-messaging/auth-internal-mtls-backend.sh
@@ -3,7 +3,7 @@
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TEST_CASES_PATH=/authnz-msg-protocols
-PROFILES="internal-user auth_backends-internal tls auth-mtls"
+PROFILES="amqp-internal-user auth_backends-internal tls auth-mtls"
 
 source $SCRIPT/../../bin/suite_template
 run
