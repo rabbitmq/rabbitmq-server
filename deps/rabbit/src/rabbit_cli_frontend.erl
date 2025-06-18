31b5d06cd20bb59c8817dbba6c857c939614ba04
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2025-06-18 11:41:57 +0200

    Setup logging in CLI frontend

--- deps/rabbit/src/rabbit_cli_frontend.erl
+++ deps/rabbit/src/rabbit_cli_frontend.erl
@@ -4,7 +4,6 @@
 -include_lib("stdlib/include/assert.hrl").
 
 -export([main/1,
-         merge_argparse_def/2,
          noop/1]).
 
 -record(?MODULE, {scriptname,
@@ -19,20 +18,18 @@
 
 main(Args) ->
     ScriptName = escript:script_name(),
+    add_rabbitmq_code_path(ScriptName),
+    configure_logging(),
+
     Ret = run_cli(ScriptName, Args),
-    io:format(standard_error, "CLI run_cli() -> ~p~n", [Ret]),
+    ?LOG_NOTICE("CLI: run_cli() return value: ~p", [Ret]),
     erlang:halt().
 
-run_cli(ScriptName, Args) ->
-    ProgName = filename:basename(ScriptName, ".escript"),
-    GroupLeader = erlang:group_leader(),
-    Context = #?MODULE{scriptname = ScriptName,
-                       progname = ProgName,
-                       args = Args,
-                       group_leader = GroupLeader},
-    add_rabbitmq_code_path(Context).
+%% -------------------------------------------------------------------
+%% CLI frontend setup.
+%% -------------------------------------------------------------------
 
-add_rabbitmq_code_path(#?MODULE{scriptname = ScriptName} = Context) ->
+add_rabbitmq_code_path(ScriptName) ->
     ScriptDir = filename:dirname(ScriptName),
     PluginsDir0 = filename:join([ScriptDir, "..", "plugins"]),
     PluginsDir1 = case filelib:is_dir(PluginsDir0) of
@@ -41,12 +38,36 @@ add_rabbitmq_code_path(#?MODULE{scriptname = ScriptName} = Context) ->
                   end,
     Glob = filename:join([PluginsDir1, "*", "ebin"]),
     AppDirs = filelib:wildcard(Glob),
-    lists:foreach(fun code:add_path/1, AppDirs),
+    lists:foreach(fun code:add_path/1, AppDirs).
+
+configure_logging() ->
+    Config = #{level => debug,
+               config => #{type => standard_error},
+               filters => [{progress_reports,
+                            {fun logger_filters:progress/2, stop}}],
+               formatter => {rabbit_logger_text_fmt,
+                             #{single_line => false,
+                               use_colors => true}}},
+    ok = logger:add_handler(rmq_cli, rabbit_logger_std_h, Config),
+    ok = logger:remove_handler(default),
+    ok.
+
+%% -------------------------------------------------------------------
+%% Preparation for remote command execution.
+%% -------------------------------------------------------------------
+
+run_cli(ScriptName, Args) ->
+    ProgName = filename:basename(ScriptName, ".escript"),
+    GroupLeader = erlang:group_leader(),
+    Context = #?MODULE{scriptname = ScriptName,
+                       progname = ProgName,
+                       args = Args,
+                       group_leader = GroupLeader},
     init_local_args(Context).
 
 init_local_args(Context) ->
     maybe
-        LocalArgparseDef = local_argparse_def(),
+        LocalArgparseDef = initial_argparse_def(),
         Context1 = Context#?MODULE{argparse_def = LocalArgparseDef},
 
         {ok,
@@ -56,53 +77,36 @@ init_local_args(Context) ->
         Context2 = Context1#?MODULE{arg_map = PartialArgMap,
                                     cmd_path = PartialCmdPath,
                                     command = PartialCommand},
-        connect_to_node(Context2)
+        set_log_level(Context2)
     end.
 
-connect_to_node(#?MODULE{arg_map = #{node := NodenameOrUri}} = Context) ->
-    maybe
-        %% TODO: Send a list of supported features:
-        %% * support for some messages, like Erlang I/O protocol, file
-        %%   read/write support
-        %% * type of terminal (or no terminal)
-        %% * capabilities of the terminal
-        %% * is plain test or HTTP
-        %% * evolutions in the communication between the frontend and the
-        %%   backend
-        {ok, Connection} ?= rabbit_cli_transport2:connect(NodenameOrUri),
-        Context1 = Context#?MODULE{connection = Connection},
-        init_final_args(Context1)
-    end;
-connect_to_node(#?MODULE{} = Context) ->
-    maybe
-        {ok, Connection} ?= rabbit_cli_transport2:connect(),
-        Context1 = Context#?MODULE{connection = Connection},
-        init_final_args(Context1)
-    end.
+set_log_level(#?MODULE{arg_map = #{verbose := Verbosity}} = Context)
+  when Verbosity >= 3 ->
+    logger:set_primary_config(level, debug),
+    connect_to_node(Context);
+set_log_level(#?MODULE{} = Context) ->
+    connect_to_node(Context).
 
-init_final_args(Context) ->
-    maybe
-        %% We can query the argparse definition from the remote node to know
-        %% the commands it supports and proceed with the execution.
-        {ok, ArgparseDef} ?= final_argparse_def(Context),
-        Context1 = Context#?MODULE{argparse_def = ArgparseDef},
-
-        {ok,
-         ArgMap,
-         CmdPath,
-         Command} ?= final_parse(Context1),
-        Context2 = Context1#?MODULE{arg_map = ArgMap,
-                                    cmd_path = CmdPath,
-                                    command = Command},
-
-        run_command(Context2)
-    end.
+connect_to_node(#?MODULE{arg_map = ArgMap} = Context) ->
+    Ret = case ArgMap of
+              #{node := NodenameOrUri} ->
+                  rabbit_cli_transport2:connect(NodenameOrUri);
+              _ ->
+                  rabbit_cli_transport2:connect()
+          end,
+    Context1 = case Ret of
+                   {ok, Connection} ->
+                       Context#?MODULE{connection = Connection};
+                   {error, _Reason} ->
+                       Context#?MODULE{connection = none}
+               end,
+    run_command(Context1).
 
 %% -------------------------------------------------------------------
 %% Arguments definition and parsing.
 %% -------------------------------------------------------------------
 
-local_argparse_def() ->
+initial_argparse_def() ->
     #{arguments =>
       [
        #{name => help,
@@ -158,57 +162,28 @@ partial_parse(Args, ArgparseDef, Options, RemainingArgs) ->
             Error
     end.
 
-final_argparse_def(#?MODULE{argparse_def = PartialArgparseDef} = Context) ->
-    maybe
-        {ok, FullArgparseDef} ?= get_full_argparse_def(Context),
-        ArgparseDef1 = merge_argparse_def(PartialArgparseDef, FullArgparseDef),
-        {ok, ArgparseDef1}
-    end.
-
-get_full_argparse_def(#?MODULE{connection = Connection})
-  when Connection =/= undefined ->
-    %% TODO: Handle an undef failure when the remote node is too old.
-    RemoteArgparseDef = rabbit_cli_transport2:rpc(
-                          Connection,
-                          rabbit_cli_backend, final_argparse_def, []),
-    {ok, RemoteArgparseDef};
-get_full_argparse_def(#?MODULE{}) ->
-    %% FIXME: Load applications first, otherwise module attributes are
-    %% unavailable.
-    LocalArgparseDef = rabbit_cli_backend:final_argparse_def(),
-    {ok, LocalArgparseDef}.
-
-merge_argparse_def(ArgparseDef1, ArgparseDef2) ->
-    Args1 = maps:get(arguments, ArgparseDef1, []),
-    Args2 = maps:get(arguments, ArgparseDef2, []),
-    Args = merge_arguments(Args1, Args2),
-    Cmds1 = maps:get(commands, ArgparseDef1, #{}),
-    Cmds2 = maps:get(commands, ArgparseDef2, #{}),
-    Cmds = merge_commands(Cmds1, Cmds2),
-    maps:merge(
-      ArgparseDef1,
-      ArgparseDef2#{arguments => Args, commands => Cmds}).
-
-merge_arguments(Args1, Args2) ->
-    Args1 ++ Args2.
-
-merge_commands(Cmds1, Cmds2) ->
-    maps:merge(Cmds1, Cmds2).
-
-final_parse(
-  #?MODULE{progname = ProgName, args = Args, argparse_def = ArgparseDef}) ->
-    Options = #{progname => ProgName},
-    argparse:parse(Args, ArgparseDef, Options).
-
 %% -------------------------------------------------------------------
 %% Command execution.
 %% -------------------------------------------------------------------
 
-run_command(#?MODULE{connection = Connection} = Context) ->
+%% TODO: Send a list of supported features:
+%% * support for some messages, like Erlang I/O protocol, file read/write
+%%   support
+%% * type of terminal (or no terminal)
+%% * capabilities of the terminal
+%% * is plain test or HTTP
+%% * evolutions in the communication between the frontend and the backend
+
+run_command(#?MODULE{connection = Connection} = Context)
+  when Connection =/= none ->
     ContextMap = context_to_map(Context),
     rabbit_cli_transport2:rpc(
       Connection, rabbit_cli_backend, run_command, [ContextMap]);
 run_command(Context) ->
+    %% TODO: If we can't connect to a node, try to parse args locally and run
+    %% the command on this CLI node.
+    %% FIXME: Load applications first, otherwise module attributes are
+    %% unavailable.
     %% FIXME: Do we need to spawn a process?
     ContextMap = context_to_map(Context),
     rabbit_cli_backend:run_command(ContextMap).
