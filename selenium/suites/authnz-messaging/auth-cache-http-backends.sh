07b68d6e31c4c6bfc6c3ae0560b48feb972cf4ca
Marcial Rosales <marcial.rosales@broadcom.com>
2025-06-18 09:33:44 +0200

    Moved changes to feature-8662

--- selenium/suites/authnz-messaging/auth-cache-http-backends.sh
+++ selenium/suites/authnz-messaging/auth-cache-http-backends.sh
@@ -3,7 +3,7 @@
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TEST_CASES_PATH=/authnz-msg-protocols
-PROFILES="amqp-http-user auth-http auth_backends-cache-http "
+PROFILES="http-user auth-http auth_backends-cache-http "
 
 source $SCRIPT/../../bin/suite_template
 runWith mock-auth-backend-http
--- selenium/suites/authnz-messaging/auth-oauth-backend-with-opaque-tokens.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-TEST_CASES_PATH=/authnz-msg-protocols
-PROFILES="spring oauth-producer auth-oauth-spring auth_backends-opaque-oauth "
-
-source $SCRIPT/../../bin/suite_template
-runWith spring
