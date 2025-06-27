a381064918268393a9d75acd23261765797be1e0
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2025-06-27 15:03:10 +0200

    Remove older experimental code

--- deps/rabbit/src/rabbit_cli_transport.erl
+++ deps/rabbit/src/rabbit_cli_transport.erl
@@ -1,62 +1,53 @@
 -module(rabbit_cli_transport).
--behaviour(gen_server).
 
--export([connect/1,
-         rpc/4]).
--export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         config_change/3]).
+-include_lib("kernel/include/logger.hrl").
 
--record(http, {uri :: uri_string:uri_map(),
-               conn :: pid(),
-               stream :: gun:stream_ref(),
-               stream_ready = false :: boolean(),
-               pending = [] :: [any()],
-               pending_io_requests = #{} :: map(),
-               group_leader :: pid()
-              }).
+-export([connect/0, connect/1,
+         get_client_info/1,
+         run_command/2,
+         gen_reply/3,
+         send/3]).
 
-connect(#{arg_map := #{node := NodenameOrUri}} = Context) ->
-    case re:run(NodenameOrUri, "://", [{capture, none}]) of
-        nomatch ->
-            connect_using_erldist(Context);
-        match ->
-            connect_using_transport(Context)
-    end;
-connect(Context) ->
-    connect_using_erldist(Context).
+-record(?MODULE, {type :: erldist | http,
+                  peer :: atom() | pid()}).
 
-rpc(Nodename, Mod, Func, Args) when is_atom(Nodename) ->
-    rpc_using_erldist(Nodename, Mod, Func, Args);
-rpc(TransportPid, Mod, Func, Args) when is_pid(TransportPid) ->
-    rpc_using_transport(TransportPid, Mod, Func, Args).
+connect() ->
+    Nodename = guess_rabbitmq_nodename(),
+    connect(erldist, Nodename).
 
-%% -------------------------------------------------------------------
-%% Erlang distribution.
-%% -------------------------------------------------------------------
+connect(NodenameOrUri) ->
+    Proto = determine_proto(NodenameOrUri),
+    connect(Proto, NodenameOrUri).
 
-connect_using_erldist(#{arg_map := #{node := Nodename}}) ->
-    do_connect_using_erldist(Nodename);
-connect_using_erldist(_Context) ->
-    GuessedNodename = guess_rabbitmq_nodename(),
-    do_connect_using_erldist(GuessedNodename).
-
-do_connect_using_erldist(Nodename) ->
+connect(erldist = Proto, Nodename) ->
     maybe
         Nodename1 = complete_nodename(Nodename),
-        {ok, _} ?= net_kernel:start(
-                     undefined, #{name_domain => shortnames}),
+        ?LOG_DEBUG(
+           "CLI: connect to node ~s using Erlang distribution",
+           [Nodename1]),
+
+        %% FIXME: Handle short vs. long names.
+        {ok, _} ?= net_kernel:start(undefined, #{name_domain => shortnames}),
 
         %% Can we reach the remote node?
         case net_kernel:connect_node(Nodename1) of
             true ->
-                {ok, Nodename1};
+                Connection = #?MODULE{type = Proto,
+                                      peer = Nodename1},
+                {ok, Connection};
             false ->
                 {error, noconnection}
         end
+    end;
+connect(http = Proto, Uri) ->
+    maybe
+        ?LOG_DEBUG(
+           "CLI: connect to URI ~s using HTTP",
+           [Uri]),
+        {ok, Client} ?= rabbit_cli_http_client:start_link(Uri),
+        Connection = #?MODULE{type = Proto,
+                              peer = Client},
+        {ok, Connection}
     end.
 
 guess_rabbitmq_nodename() ->
@@ -79,6 +70,14 @@ guess_rabbitmq_nodename() ->
             "rabbit"
     end.
 
+determine_proto(NodenameOrUri) ->
+    case re:run(NodenameOrUri, "://", [{capture, none}]) of
+        nomatch ->
+            erldist;
+        match ->
+            http
+    end.
+
 complete_nodename(Nodename) ->
     case re:run(Nodename, "@", [{capture, none}]) of
         nomatch ->
@@ -88,129 +87,23 @@ complete_nodename(Nodename) ->
             list_to_atom(Nodename)
     end.
 
-rpc_using_erldist(Nodename, Mod, Func, Args) ->
-    erpc:call(Nodename, Mod, Func, Args).
+get_client_info(#?MODULE{type = Proto}) ->
+    {ok, Hostname} = inet:gethostname(),
+    #{hostname => Hostname,
+      proto => Proto}.
 
-%% -------------------------------------------------------------------
-%% HTTP(S) transport.
-%% -------------------------------------------------------------------
+run_command(#?MODULE{type = erldist, peer = Node}, ContextMap) ->
+    Caller = self(),
+    erpc:call(Node, rabbit_cli_backend, run_command, [ContextMap, Caller]);
+run_command(#?MODULE{type = http, peer = Client}, ContextMap) ->
+    rabbit_cli_http_client:run_command(Client, ContextMap).
 
-connect_using_transport(Context) ->
-    gen_server:start_link(?MODULE, Context, []).
+gen_reply(#?MODULE{type = erldist}, From, Reply) ->
+    gen:reply(From, Reply);
+gen_reply(#?MODULE{type = http, peer = Client}, From, Reply) ->
+    rabbit_cli_http_client:gen_reply(Client, From, Reply).
 
-rpc_using_transport(TransportPid, Mod, Func, Args) when is_pid(TransportPid) ->
-    gen_server:call(TransportPid, {rpc, {Mod, Func, Args}}).
-
-init(#{arg_map := #{node := Uri}, group_leader := GL}) ->
-    maybe
-        {ok, _} ?= application:ensure_all_started(gun),
-        #{host := Host, port := Port} = UriMap = uri_string:parse(Uri),
-        {ok, ConnPid} ?= gun:open(Host, Port),
-        State = #http{uri = UriMap,
-                      group_leader = GL,
-                      conn = ConnPid},
-        %logger:alert("Transport: State=~p", [State]),
-        {ok, State}
-    end.
-
-handle_call(
-  Request, From,
-  #http{stream_ready = true} = State) ->
-    %% HTTP message to the server side.
-    send_call(Request, From, State),
-    {noreply, State};
-handle_call(
-  Request, From,
-  #http{stream_ready = false, pending = Pending} = State) ->
-    %logger:alert("Transport(call): ~p", [Request]),
-    State1 = State#http{pending = [{From, Request} | Pending]},
-    {noreply, State1};
-handle_call(_Request, _From, State) ->
-    %logger:alert("Transport(call): ~p", [_Request]),
-    {reply, ok, State}.
-
-handle_cast(_Request, State) ->
-    %logger:alert("Transport(cast): ~p", [_Request]),
-    {noreply, State}.
-
-handle_info(
-  {gun_up, ConnPid, _},
-  #http{conn = ConnPid} = State) ->
-    %logger:alert("Transport(info): Conn up"),
-    StreamRef = gun:ws_upgrade(ConnPid, "/", []),
-    State1 = State#http{stream = StreamRef},
-    {noreply, State1};
-handle_info(
-  {gun_upgrade, ConnPid, StreamRef, _Frames, _},
-  #http{conn = ConnPid, stream = StreamRef, pending = Pending} = State) ->
-    %logger:alert("Transport(info): WS upgraded, ~p", [_Frames]),
-    State1 = State#http{stream_ready = true, pending = []},
-    Pending1 = lists:reverse(Pending),
-    lists:foreach(
-      fun({From, Request}) ->
-              send_call(Request, From, State1)
-      end, Pending1),
-    {noreply, State1};
-handle_info(
-  {gun_ws, ConnPid, StreamRef, {binary, ReplyBin}},
-  #http{conn = ConnPid,
-        stream = StreamRef,
-        group_leader = GL,
-        pending_io_requests = Pending} = State) ->
-    %% HTTP message from the server side.
-    Reply = binary_to_term(ReplyBin),
-    State1 = case Reply of
-                 % {io_call, From, Msg} ->
-                 %     %logger:alert("IO call from WS: ~p -> ~p", [Msg, From]),
-                 %     Ret = gen_server:call(IO, Msg),
-                 %     RequestBin = term_to_binary({io_reply, From, Ret}),
-                 %     Frame = {binary, RequestBin},
-                 %     gun:ws_send(ConnPid, StreamRef, Frame);
-                 % {io_cast, Msg} ->
-                 %     %logger:alert("IO cast from WS: ~p", [Msg]),
-                 %     gen_server:cast(IO, Msg);
-                 {msg, group_leader, {io_request, RemoteFrom, ReplyAs, Request} = _Msg} ->
-                     % logger:alert("Message from WS: ~p", [Msg]),
-                     Ref = erlang:make_ref(),
-                     IoRequest1 = {io_request, self(), Ref, Request},
-                     GL ! IoRequest1,
-                     Pending1 = Pending#{Ref => {RemoteFrom, ReplyAs}},
-                     State#http{pending_io_requests = Pending1};
-                 {ret, From, Ret} ->
-                     %logger:alert("Reply from WS: ~p -> ~p", [Ret, From]),
-                     gen_server:reply(From, Ret),
-                     State;
-                 _Other ->
-                     %logger:alert("Reply from WS: ~p", [_Other]),
-                     State
-             end,
-    {noreply, State1};
-handle_info(
-  {io_reply, ReplyAs, Reply} = _IoReply,
-  #http{conn = ConnPid,
-        stream = StreamRef,
-        pending_io_requests = Pending} = State) ->
-    % logger:alert("io_reply to WS: ~p", [IoReply]),
-    {RemoteFrom, RemoteReplyAs} = maps:get(ReplyAs, Pending),
-    Msg = {io_reply, RemoteReplyAs, Reply},
-    RequestBin = term_to_binary({msg, RemoteFrom, Msg}),
-    Frame = {binary, RequestBin},
-    gun:ws_send(ConnPid, StreamRef, Frame),
-
-    Pending1 = maps:remove(ReplyAs, Pending),
-    State1 = State#http{pending_io_requests = Pending1},
-    {noreply, State1};
-handle_info(_Info, State) ->
-    %logger:alert("Transport(info): ~p", [_Info]),
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-config_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-send_call(Request, From, #http{conn = ConnPid, stream = StreamRef}) ->
-    RequestBin = term_to_binary({call, From, Request}),
-    Frame = {binary, RequestBin},
-    gun:ws_send(ConnPid, StreamRef, Frame).
+send(#?MODULE{type = erldist}, Dest, Msg) ->
+    erlang:send(Dest, Msg);
+send(#?MODULE{type = http, peer = Client}, Dest, Msg) ->
+    rabbit_cli_http_client:send(Client, Dest, Msg).
