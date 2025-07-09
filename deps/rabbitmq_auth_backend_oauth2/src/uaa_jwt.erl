0704fb2a14e88b4db5dd10fd504be97193b0a807
Marcial Rosales <marcial.rosales@broadcom.com>
2025-07-09 09:12:27 +0200

    Remove funtion as not needed

--- deps/rabbitmq_auth_backend_oauth2/src/uaa_jwt.erl
+++ deps/rabbitmq_auth_backend_oauth2/src/uaa_jwt.erl
@@ -190,7 +190,3 @@ sub(DecodedToken) ->
 sub(DecodedToken, Default) ->
     maps:get(<<"sub">>, DecodedToken, Default).
 
--spec validate_introspected_token(map()) ->
-    {ok, map()} | {error, term()}.
-validate_introspected_token(_Token) ->
-    {error, not_implemented}.
