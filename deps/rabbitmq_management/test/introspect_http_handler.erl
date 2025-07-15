ed76af13dd28c3058f6d51b8316d9d090b7c6c80
Marcial Rosales <marcial.rosales@broadcom.com>
2025-07-15 13:18:13 +0200

    Successful unit test introspect token

--- deps/rabbitmq_management/test/introspect_http_handler.erl
+++ deps/rabbitmq_management/test/introspect_http_handler.erl
@@ -4,16 +4,23 @@
 -export([init/2, terminate/3]).
 
 init(Req, State) ->
-    case cowboy_req:parse_header(<<"authorization">>, Req) of
-        {bearer, <<"active">>} -> 
-            Body = rabbit_json:encode([{"active", true}, {"scope", "rabbitmq.tag:administrator"}]),
-            {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, 
-                Body, Req), State};
-        {bearer, <<"inactive">>} -> 
-            Body = rabbit_json:encode([{"active", false}, {"scope", "rabbitmq.tag:administrator"}]),
-            {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, 
-                Body, Req), State};
-        _ -> 
+    ct:log("introspect_http_handler init : ~p", [Req]),
+    case cowboy_req:read_urlencoded_body(Req) of
+        {ok, KeyValues, _Req} -> 
+            ct:log("introspect_http_handler responding with active token: ~p", [KeyValues]),
+            case proplists:get_value(<<"token">>, KeyValues) of 
+                undefined -> 
+                    {ok, cowboy_req:reply(401, #{}, [], Req), State};
+                <<"active">> -> 
+                    Body = rabbit_json:encode([{"active", true}, {"scope", "rabbitmq.tag:administrator"}]),
+                    {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, 
+                        Body, Req), State};
+                <<"inactive">> -> Body = rabbit_json:encode([{"active", false}, {"scope", "rabbitmq.tag:administrator"}]),
+                    {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, 
+                        Body, Req), State}
+            end;
+        Other -> 
+            ct:log("introspect_http_handler responding with 401 : ~p", [Other]),
             {ok, cowboy_req:reply(401, #{}, [], Req), State}
     end.
 
