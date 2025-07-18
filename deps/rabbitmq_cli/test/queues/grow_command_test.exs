b31b5215f25ab4194368aef93a78918e6b0b41ff
Ayanda Dube <adube14@bloomberg.net>
2025-07-18 17:42:39 +0100

    Revert QQ grow-to-N in original grow command

--- deps/rabbitmq_cli/test/queues/grow_command_test.exs
+++ deps/rabbitmq_cli/test/queues/grow_command_test.exs
@@ -82,46 +82,12 @@ defmodule RabbitMQ.CLI.Queues.Commands.GrowCommandTest do
              {:validation_failure, "voter status 'banana' is not recognised."}
   end
 
-  test "validate: when target quorum cluster size greater than zero and membership is voter, returns a success" do
-    assert @command.validate(["7", "all"], %{membership: "voter", queue_pattern: "qq.*"}) == :ok
-  end
-
-  test "validate: when target quorum cluster size greater than zero and membership is promotable, returns a success" do
-    assert @command.validate(["5", "all"], %{membership: "promotable", queue_pattern: "qq.*"}) == :ok
-  end
-
-  test "validate: when target quorum cluster size is zero, returns failure" do
-    assert @command.validate(["0", "all"], %{membership: "voter", queue_pattern: "qq.*"}) ==
-             {:validation_failure, "target quorum cluster size '0' must be greater than 0."}
-  end
-
-  test "validate: when target quorum cluster size is less than zero, returns failure" do
-    assert @command.validate(["-1", "all"], %{membership: "voter", queue_pattern: "qq.*"}) ==
-             {:validation_failure, "target quorum cluster size '-1' must be greater than 0."}
-  end
-
-  test "validate: when target quorum cluster size is provided and membership is not voter, returns failure" do
-    assert @command.validate(["5", "all"], %{membership: "non_voter", queue_pattern: "qq.*"}) ==
-             {:validation_failure, "voter status 'non_voter' must be 'voter' or 'promotable' to grow to target quorum cluster size '5'."}
-  end
-
   @tag test_timeout: 3000
   test "run: targeting an unreachable node throws a badrpc when growing to a target node", context do
     assert match?(
              {:badrpc, _},
              @command.run(
                ["target@node", "all"],
-               Map.merge(context[:opts], %{node: :jake@thedog, queue_pattern: "qq.*"})
-             )
-           )
-  end
-
-  @tag test_timeout: 3000
-  test "run: targeting an unreachable node throws a badrpc when growing to a target quorum cluster size", context do
-    assert match?(
-             {:badrpc, _},
-             @command.run(
-               ["5", "all"],
                Map.merge(context[:opts], %{node: :jake@thedog, queue_pattern: "qq.*"})
              )
            )
