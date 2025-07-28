2a0401633e7e6139f87175bb837f1f4d104f55ca
Diana Parra Corbacho <diana.parra-corbacho@broadcom.com>
2025-07-28 13:22:37 +0200

    Shovel: status tests

--- deps/rabbitmq_shovel/test/shovel_status_command_SUITE.erl
+++ deps/rabbitmq_shovel/test/shovel_status_command_SUITE.erl
@@ -8,6 +8,7 @@
 -module(shovel_status_command_SUITE).
 
 -include_lib("amqp_client/include/amqp_client.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 -compile(export_all).
 
@@ -26,7 +27,8 @@ groups() ->
                                run_starting,
                                output_starting,
                                run_running,
-                               output_running
+                               output_running,
+                               e2e
         ]}
     ].
 
@@ -95,8 +97,14 @@ output_starting(Config) ->
     [A] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
     Opts = #{node => A},
     {stream, [#{vhost := <<"/">>, name := <<"test">>, type := dynamic,
-                state := starting, last_changed := <<"2016-11-17 10:00:00">>}]}
+                state := starting, last_changed := <<"2016-11-17 10:00:00">>,
+                remaining := 10, remaining_unacked := 8,
+                pending := 5, forwarded := 3}]}
         = ?CMD:output({stream, [{{<<"/">>, <<"test">>}, dynamic, starting,
+                                 #{remaining => 10,
+                                   remaining_unacked => 8,
+                                   pending => 5,
+                                   forwarded => 3},
                                  {{2016, 11, 17}, {10, 00, 00}}}]}, Opts),
     shovel_test_utils:clear_param(Config, <<"test">>).
 
@@ -118,9 +126,43 @@ output_running(Config) ->
                 state := running, source := <<"amqp://server-1">>,
                 destination := <<"amqp://server-2">>,
                 termination_reason := <<>>,
-                last_changed := <<"2016-11-17 10:00:00">>}]} =
+                last_changed := <<"2016-11-17 10:00:00">>,
+                remaining := 10,
+                remaining_unacked := 8,
+                pending := 5,
+                forwarded := 3}]} =
         ?CMD:output({stream, [{{<<"/">>, <<"test">>}, dynamic,
                                {running, [{src_uri, <<"amqp://server-1">>},
                                           {dest_uri, <<"amqp://server-2">>}]},
+                               #{remaining => 10,
+                                 remaining_unacked => 8,
+                                 pending => 5,
+                                 forwarded => 3},
                                {{2016, 11, 17}, {10, 00, 00}}}]}, Opts),
+    shovel_test_utils:clear_param(Config, <<"test">>).
+
+e2e(Config) ->
+    shovel_test_utils:set_param_nowait(
+      Config,
+      <<"test">>, [{<<"src-queue">>,  <<"src">>},
+                   {<<"dest-queue">>, <<"dest">>}]),
+    {ok, StdOut} = rabbit_ct_broker_helpers:rabbitmqctl(Config, 0, [<<"shovel_status">>]),
+    [Msg, Headers0, Shovel0] = re:split(StdOut, <<"\n">>, [trim]),
+    ?assertMatch(match, re:run(Msg, "Shovel status on node", [{capture, none}])),
+    Headers = re:split(Headers0, <<"\t">>, [trim]),
+    ExpectedHeaders = [<<"name">>, <<"vhost">>, <<"type">>, <<"state">>,
+                       <<"source">>, <<"destination">>, <<"termination_reason">>,
+                       <<"destination_protocol">>, <<"source_protocol">>,
+                       <<"last_changed">>, <<"source_queue">>, <<"destination_queue">>,
+                       <<"remaining">>, <<"remaining_unacked">>,
+                       <<"pending">>, <<"forwarded">>],
+    ?assert(lists:all(fun(H) ->
+                              lists:member(H, Headers)
+                      end, ExpectedHeaders)),
+    %% Check some values are there
+    ExpectedValues = [<<"test">>, <<"dynamic">>, <<"running">>],
+    Shovel = re:split(Shovel0, <<"\t">>, [trim]),
+    ?assert(lists:all(fun(V) ->
+                              lists:member(V, Shovel)
+                      end, ExpectedValues)),
     shovel_test_utils:clear_param(Config, <<"test">>).
