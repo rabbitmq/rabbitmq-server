9e63db80acc46888480903396c1a3be8f2e559a0
Marcial Rosales <marcial.rosales@broadcom.com>
2025-08-04 17:26:15 +0200

    Fix configuration for idp-initiated with opaque tokens

--- selenium/suites/authnz-mgt/oauth-idp-initiated-with-spring-opaque-tokens.sh
+++ selenium/suites/authnz-mgt/oauth-idp-initiated-with-spring-opaque-tokens.sh
@@ -4,7 +4,7 @@ SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TEST_CASES_PATH=/oauth/with-idp-initiated
 TEST_CONFIG_PATH=/oauth
-PROFILES="spring idp-initiated spring-oauth-provider fakeportal-mgt-oauth-provider tls opaque-token"
+PROFILES="spring idp-initiated spring-oauth-provider fakeportal-mgt-oauth-provider opaque-token"
 
 source $SCRIPT/../../bin/suite_template $@
 runWith spring fakeportal
