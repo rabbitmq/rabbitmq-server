02b1561556f059861f4baa23b95949536fa8ef88
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2025-08-06 15:09:33 +0200

    auth_SUITE: Wait for connection tracking to be up-to-date
    ... when testing user limits
    
    [How]
    This is the same fix as the one for the vhost limits test case made in
    commit 5aab965db44b32f53b77f8e31b452122270e67bc.
    
    While here, fix a compiler warning about an unused variable.

--- deps/rabbitmq_mqtt/test/auth_SUITE.erl
+++ deps/rabbitmq_mqtt/test/auth_SUITE.erl
@@ -1262,7 +1262,6 @@ vhost_connection_limit(Config) ->
     ok = rabbit_ct_broker_helpers:clear_vhost_limit(Config, 0, <<"/">>).
 
 count_connections_per_vhost(Config)  ->
-    NodeConfig = rabbit_ct_broker_helpers:get_node_config(Config, 0),
     rabbit_ct_broker_helpers:rpc(
       Config, 0,
       rabbit_connection_tracking, count_local_tracked_items_in_vhost,
@@ -1287,6 +1286,7 @@ user_connection_limit(Config) ->
     ok = rabbit_ct_broker_helpers:set_user_limits(Config, DefaultUser, #{max_connections => 1}),
     {ok, C1} = connect_anonymous(Config, <<"client1">>),
     {ok, _} = emqtt:connect(C1),
+    ?awaitMatch(1, count_connections_per_vhost(Config), 30000),
     {ok, C2} = connect_anonymous(Config, <<"client2">>),
     ExpectedError = expected_connection_limit_error(Config),
     unlink(C2),
