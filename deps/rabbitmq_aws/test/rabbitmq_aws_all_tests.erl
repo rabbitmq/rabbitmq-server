6544fded269942480af5bbf7efc7696c17ec399a
Simon Unge <simunge@amazon.com>
2025-08-07 09:29:08 +0000

    Format entire plugin with erlfmt

--- deps/rabbitmq_aws/test/rabbitmq_aws_all_tests.erl
+++ deps/rabbitmq_aws/test/rabbitmq_aws_all_tests.erl
@@ -5,14 +5,14 @@
 -include_lib("eunit/include/eunit.hrl").
 
 run() ->
-  Result = {
-    eunit:test(rabbitmq_aws_app_tests, [verbose]),
-    eunit:test(rabbitmq_aws_config_tests, [verbose]),
-    eunit:test(rabbitmq_aws_json_tests, [verbose]),
-    eunit:test(rabbitmq_aws_sign_tests, [verbose]),
-    eunit:test(rabbitmq_aws_sup_tests, [verbose]),
-    eunit:test(rabbitmq_aws_tests, [verbose]),
-    eunit:test(rabbitmq_aws_urilib_tests, [verbose]),
-    eunit:test(rabbitmq_aws_xml_tests, [verbose])
-  },
-  ?assertEqual({ok, ok, ok, ok, ok, ok, ok, ok}, Result).
+    Result = {
+        eunit:test(rabbitmq_aws_app_tests, [verbose]),
+        eunit:test(rabbitmq_aws_config_tests, [verbose]),
+        eunit:test(rabbitmq_aws_json_tests, [verbose]),
+        eunit:test(rabbitmq_aws_sign_tests, [verbose]),
+        eunit:test(rabbitmq_aws_sup_tests, [verbose]),
+        eunit:test(rabbitmq_aws_tests, [verbose]),
+        eunit:test(rabbitmq_aws_urilib_tests, [verbose]),
+        eunit:test(rabbitmq_aws_xml_tests, [verbose])
+    },
+    ?assertEqual({ok, ok, ok, ok, ok, ok, ok, ok}, Result).
--- deps/rabbitmq_aws/test/rabbitmq_aws_json_tests.erl
+++ deps/rabbitmq_aws/test/rabbitmq_aws_json_tests.erl
@@ -3,69 +3,93 @@
 -include_lib("eunit/include/eunit.hrl").
 
 parse_test_() ->
-  [
-    {"string decoding", fun() ->
-      Value = "{\"requestId\":\"bda7fbdb-eddb-41fa-8626-7ba87923d690\",\"number\":128,\"enabled\":true,\"tagSet\":[{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"Environment\",\"value\":\"prod-us-east-1\"},{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"aws:cloudformation:logical-id\",\"value\":\"AutoScalingGroup\"},{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"aws:cloudformation:stack-name\",\"value\":\"prod-us-east-1-ecs-1\"}]}",
-      Expectation = [
-        {"requestId","bda7fbdb-eddb-41fa-8626-7ba87923d690"},
-        {"number", 128},
-        {"enabled", true},
-        {"tagSet",
-          [{"resourceId","i-13a4abea"},
-            {"resourceType","instance"},
-            {"key","Environment"},
-            {"value","prod-us-east-1"},
-            {"resourceId","i-13a4abea"},
-            {"resourceType","instance"},
-            {"key","aws:cloudformation:logical-id"},
-            {"value","AutoScalingGroup"},
-            {"resourceId","i-13a4abea"},
-            {"resourceType","instance"},
-            {"key","aws:cloudformation:stack-name"},
-            {"value","prod-us-east-1-ecs-1"}]}
-      ],
-      Proplist = rabbitmq_aws_json:decode(Value),
-      ?assertEqual(proplists:get_value("requestId", Expectation), proplists:get_value("requestId", Proplist)),
-      ?assertEqual(proplists:get_value("number", Expectation), proplists:get_value("number", Proplist)),
-      ?assertEqual(proplists:get_value("enabled", Expectation), proplists:get_value("enabled", Proplist)),
-      ?assertEqual(lists:usort(proplists:get_value("tagSet", Expectation)),
-                   lists:usort(proplists:get_value("tagSet", Proplist)))
-     end},
-    {"binary decoding", fun() ->
-      Value = <<"{\"requestId\":\"bda7fbdb-eddb-41fa-8626-7ba87923d690\",\"number\":128,\"enabled\":true,\"tagSet\":[{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"Environment\",\"value\":\"prod-us-east-1\"},{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"aws:cloudformation:logical-id\",\"value\":\"AutoScalingGroup\"},{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"aws:cloudformation:stack-name\",\"value\":\"prod-us-east-1-ecs-1\"}]}">>,
-      Expectation = [
-        {"requestId","bda7fbdb-eddb-41fa-8626-7ba87923d690"},
-        {"number", 128},
-        {"enabled", true},
-        {"tagSet",
-          [{"resourceId","i-13a4abea"},
-            {"resourceType","instance"},
-            {"key","Environment"},
-            {"value","prod-us-east-1"},
-            {"resourceId","i-13a4abea"},
-            {"resourceType","instance"},
-            {"key","aws:cloudformation:logical-id"},
-            {"value","AutoScalingGroup"},
-            {"resourceId","i-13a4abea"},
-            {"resourceType","instance"},
-            {"key","aws:cloudformation:stack-name"},
-            {"value","prod-us-east-1-ecs-1"}]}
-      ],
-      Proplist = rabbitmq_aws_json:decode(Value),
-      ?assertEqual(proplists:get_value("requestId", Expectation), proplists:get_value("requestId", Proplist)),
-      ?assertEqual(proplists:get_value("number", Expectation), proplists:get_value("number", Proplist)),
-      ?assertEqual(proplists:get_value("enabled", Expectation), proplists:get_value("enabled", Proplist)),
-      ?assertEqual(lists:usort(proplists:get_value("tagSet", Expectation)),
-                   lists:usort(proplists:get_value("tagSet", Proplist)))
-     end},
-    {"list values", fun() ->
-      Value = "{\"misc\": [\"foo\", true, 123]\}",
-      Expectation = [{"misc", ["foo", true, 123]}],
-      ?assertEqual(Expectation, rabbitmq_aws_json:decode(Value))
-     end},
-    {"empty objects", fun() ->
-      Value = "{\"tags\": [{}]}",
-      Expectation = [{"tags", [{}]}],
-      ?assertEqual(Expectation, rabbitmq_aws_json:decode(Value))
-      end}
-  ].
+    [
+        {"string decoding", fun() ->
+            Value =
+                "{\"requestId\":\"bda7fbdb-eddb-41fa-8626-7ba87923d690\",\"number\":128,\"enabled\":true,\"tagSet\":[{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"Environment\",\"value\":\"prod-us-east-1\"},{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"aws:cloudformation:logical-id\",\"value\":\"AutoScalingGroup\"},{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"aws:cloudformation:stack-name\",\"value\":\"prod-us-east-1-ecs-1\"}]}",
+            Expectation = [
+                {"requestId", "bda7fbdb-eddb-41fa-8626-7ba87923d690"},
+                {"number", 128},
+                {"enabled", true},
+                {"tagSet", [
+                    {"resourceId", "i-13a4abea"},
+                    {"resourceType", "instance"},
+                    {"key", "Environment"},
+                    {"value", "prod-us-east-1"},
+                    {"resourceId", "i-13a4abea"},
+                    {"resourceType", "instance"},
+                    {"key", "aws:cloudformation:logical-id"},
+                    {"value", "AutoScalingGroup"},
+                    {"resourceId", "i-13a4abea"},
+                    {"resourceType", "instance"},
+                    {"key", "aws:cloudformation:stack-name"},
+                    {"value", "prod-us-east-1-ecs-1"}
+                ]}
+            ],
+            Proplist = rabbitmq_aws_json:decode(Value),
+            ?assertEqual(
+                proplists:get_value("requestId", Expectation),
+                proplists:get_value("requestId", Proplist)
+            ),
+            ?assertEqual(
+                proplists:get_value("number", Expectation), proplists:get_value("number", Proplist)
+            ),
+            ?assertEqual(
+                proplists:get_value("enabled", Expectation),
+                proplists:get_value("enabled", Proplist)
+            ),
+            ?assertEqual(
+                lists:usort(proplists:get_value("tagSet", Expectation)),
+                lists:usort(proplists:get_value("tagSet", Proplist))
+            )
+        end},
+        {"binary decoding", fun() ->
+            Value =
+                <<"{\"requestId\":\"bda7fbdb-eddb-41fa-8626-7ba87923d690\",\"number\":128,\"enabled\":true,\"tagSet\":[{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"Environment\",\"value\":\"prod-us-east-1\"},{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"aws:cloudformation:logical-id\",\"value\":\"AutoScalingGroup\"},{\"resourceId\":\"i-13a4abea\",\"resourceType\":\"instance\",\"key\":\"aws:cloudformation:stack-name\",\"value\":\"prod-us-east-1-ecs-1\"}]}">>,
+            Expectation = [
+                {"requestId", "bda7fbdb-eddb-41fa-8626-7ba87923d690"},
+                {"number", 128},
+                {"enabled", true},
+                {"tagSet", [
+                    {"resourceId", "i-13a4abea"},
+                    {"resourceType", "instance"},
+                    {"key", "Environment"},
+                    {"value", "prod-us-east-1"},
+                    {"resourceId", "i-13a4abea"},
+                    {"resourceType", "instance"},
+                    {"key", "aws:cloudformation:logical-id"},
+                    {"value", "AutoScalingGroup"},
+                    {"resourceId", "i-13a4abea"},
+                    {"resourceType", "instance"},
+                    {"key", "aws:cloudformation:stack-name"},
+                    {"value", "prod-us-east-1-ecs-1"}
+                ]}
+            ],
+            Proplist = rabbitmq_aws_json:decode(Value),
+            ?assertEqual(
+                proplists:get_value("requestId", Expectation),
+                proplists:get_value("requestId", Proplist)
+            ),
+            ?assertEqual(
+                proplists:get_value("number", Expectation), proplists:get_value("number", Proplist)
+            ),
+            ?assertEqual(
+                proplists:get_value("enabled", Expectation),
+                proplists:get_value("enabled", Proplist)
+            ),
+            ?assertEqual(
+                lists:usort(proplists:get_value("tagSet", Expectation)),
+                lists:usort(proplists:get_value("tagSet", Proplist))
+            )
+        end},
+        {"list values", fun() ->
+            Value = "{\"misc\": [\"foo\", true, 123]\}",
+            Expectation = [{"misc", ["foo", true, 123]}],
+            ?assertEqual(Expectation, rabbitmq_aws_json:decode(Value))
+        end},
+        {"empty objects", fun() ->
+            Value = "{\"tags\": [{}]}",
+            Expectation = [{"tags", [{}]}],
+            ?assertEqual(Expectation, rabbitmq_aws_json:decode(Value))
+        end}
+    ].
