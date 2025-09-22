90de6d188c7030f19833a68554b5058055821cd3
Karl Nilsson <kjnilsson@gmail.com>
2025-09-22 11:38:16 +0100

    QQ: add v8 modules
    And tidy up some backwards compatible code that is no longer
    needed.

--- deps/rabbit/test/rabbit_fifo_int_SUITE.erl
+++ deps/rabbit/test/rabbit_fifo_int_SUITE.erl
@@ -43,8 +43,7 @@ all_tests() ->
      untracked_enqueue,
      flow,
      test_queries,
-     duplicate_delivery,
-     usage
+     duplicate_delivery
     ].
 
 groups() ->
@@ -280,23 +279,6 @@ duplicate_delivery(Config) ->
     rabbit_quorum_queue:stop_server(ServerId),
     ok.
 
-usage(Config) ->
-    ClusterName = ?config(cluster_name, Config),
-    ServerId = ?config(node_id, Config),
-    ok = start_cluster(ClusterName, [ServerId]),
-    F0 = rabbit_fifo_client:init([ServerId]),
-    {ok, _, F1} = rabbit_fifo_client:checkout(<<"tag">>, {simple_prefetch, 10}, #{}, F0),
-    {ok, F2, []} = rabbit_fifo_client:enqueue(ClusterName, corr1, msg1, F1),
-    {ok, F3, []} = rabbit_fifo_client:enqueue(ClusterName, corr2, msg2, F2),
-    {_, _, _} = process_ra_events(receive_ra_events(2, 2), ClusterName, F3),
-    % force tick and usage stats emission
-    ServerId ! tick_timeout,
-    timer:sleep(50),
-    Use = rabbit_fifo:usage(element(1, ServerId)),
-    rabbit_quorum_queue:stop_server(ServerId),
-    ?assert(Use > 0.0),
-    ok.
-
 resends_lost_command(Config) ->
     ClusterName = ?config(cluster_name, Config),
     ServerId = ?config(node_id, Config),
@@ -523,6 +505,7 @@ discard(Config) ->
              uid => UId,
              log_init_args => #{data_dir => PrivDir, uid => UId},
              initial_member => [],
+             initial_machine_version => rabbit_fifo:version(),
              machine => {module, rabbit_fifo,
                          #{queue_resource => discard,
                            dead_letter_handler =>
