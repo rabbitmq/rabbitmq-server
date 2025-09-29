4e96997dc2fb336edbad02d48ecd53a31505e3b4
Beichen Zhang <zbeichen@amazon.com>
2025-09-29 02:01:41 -0700

    1.Configure and Use Confused Deputy Header for STS assume role call 2. Solve bug where STS is called more than once. 3. Solve bugs where type mismatches happened during refactoring. 4. Solve bugs where AWS_LOG_ERROR receives more than 2 args

--- deps/rabbitmq_aws/src/rabbitmq_aws_resource_fetcher.erl
+++ deps/rabbitmq_aws/src/rabbitmq_aws_resource_fetcher.erl
@@ -19,6 +19,10 @@
 
 -include_lib("kernel/include/logger.hrl").
 
+
+-define(AWS_LOG_DEBUG(Arg),
+    ?LOG_DEBUG("~tp: ~tp", [?MODULE, Arg])).
+
 -define(AWS_LOG_DEBUG(Fmt, Args),
     ?LOG_DEBUG("~tp: " ++ Fmt, [?MODULE | Args])).
 
@@ -26,7 +30,7 @@
     ?LOG_WARNING("~tp: ~ts", [?MODULE, Arg])).
 
 -define(AWS_LOG_ERROR(Arg),
-    ?LOG_ERROR("~tp: ~ts", [?MODULE, Arg])).
+    ?LOG_ERROR("~tp: ~tp", [?MODULE, Arg])).
 
 -define(AWS_LOG_ERROR(Fmt, Args),
     ?LOG_ERROR("~tp: " ++ Fmt, [?MODULE | Args])).
@@ -43,7 +47,7 @@ process_arns() ->
         ok = process_arns(application:get_env(rabbitmq_aws, aws_arns)),
         ?AWS_LOG_INFO("success")
     catch Class:Reason:Stacktrace ->
-        ?AWS_LOG_ERROR("~tp:~tp", [Class, Reason]),
+        ?AWS_LOG_ERROR("~tp", [{Class, Reason}]),
         ?AWS_LOG_ERROR("~tp", [Stacktrace])
     end.
 
@@ -73,7 +77,11 @@ process_arns([{_Key, Arn, Handler} | Rest]) ->
     end,
     process_arns(Rest);
 process_arns({ok, ArnList}) ->
-    process_arns(ArnList);
+    % Assume customer role once, then process all ARNs with those credentials
+    F = fun() ->
+        process_arns(ArnList)
+    end,
+    with_credentials_and_role(F);
 process_arns(undefined) ->
     ok.
 
@@ -99,7 +107,7 @@ parse_arn(Arn) ->
     try
         % resource name in arn could contain ":" itself, therefore using parts.
         % eg: arn:aws:secretsmanager:us-east-1:12345678910:secret:mysecret
-        case re:split(Arn, ":", [{parts,6}, {return, list}]) of
+        case re:split(Arn, ":", [{parts, 6}, {return, list}]) of
             ["arn", Partition, Service, Region, Account, Resource] ->
                 ?AWS_LOG_DEBUG("parsed arn: ~tp into ~tp:~tp:~tp:~tp:~tp",
                           [Arn, Partition, Service, Region, Account, Resource]),
@@ -122,10 +130,7 @@ fetch_s3_object(Resource) ->
     %% Note: splits on the first / only
     %% https://www.erlang.org/doc/apps/stdlib/string.html#split/2
     [Bucket | Key] = string:split(Resource, "/"),
-    F = fun() ->
-            fetch_s3_object_final(Bucket, Key)
-        end,
-    with_credentials_and_role(F).
+    fetch_s3_object_final(Bucket, Key).
 
 -spec fetch_s3_object_final(string(), string()) -> {ok, binary()} | {error, term()}.
 fetch_s3_object_final(Bucket, Key) ->
@@ -142,10 +147,7 @@ fetch_s3_object_final(Bucket, Key) ->
 -spec fetch_secretsmanager_secret(string(), string()) -> {ok, binary()} | {error, term()}.
 fetch_secretsmanager_secret(Arn, Region) ->
     rabbitmq_aws:set_region(Region),
-    F = fun() ->
-            fetch_secretsmanager_secret_after_env_credential_set(Arn, Region)
-        end,
-    with_credentials_and_role(Region, F).
+    fetch_secretsmanager_secret_after_env_credential_set(Arn, Region).
 
 fetch_secretsmanager_secret_after_env_credential_set(Arn, _Region) ->
     RequestBody = binary_to_list(rabbit_json:encode(#{
@@ -193,16 +195,46 @@ assume_role(RoleArn) ->
     Body = "Action=AssumeRole&RoleArn=" ++ uri_string:quote(RoleArn) ++
            "&RoleSessionName=" ++ uri_string:quote(SessionName) ++
            "&Version=2011-06-15",
-    Headers = [
+    
+    BaseHeaders = [
         {"content-type", "application/x-www-form-urlencoded"},
         {"accept", "application/json"}
     ],
+
+    % append custom headers for confused deputy prevention to base headers.
+    Headers = add_custom_headers(BaseHeaders),
+    
     case rabbitmq_aws:post("sts", "/", Body, Headers) of
         {ok, {_Headers, ResponseBody}} ->
             parse_assume_role_response(ResponseBody);
         Error ->
             Error
     end.
+
+add_custom_headers(BaseHeaders) ->
+    SourceArn = application:get_env(rabbitmq_aws, 'resource_fetcher.custom_headers.X-Amzn-Source-Arn', undefined),
+    SourceAccount = application:get_env(rabbitmq_aws, 'resource_fetcher.custom_headers.X-Amzn-Source-Account', undefined),
+    
+    case {SourceArn, SourceAccount} of
+        {undefined, _} ->
+            ?AWS_LOG_WARNING("X-Amzn-Source-Arn header is not configured");
+        {_, undefined} ->
+            ?AWS_LOG_WARNING("X-Amzn-Source-Account header is not configured");
+        _ ->
+            ok
+    end,
+    
+    CustomHeaders = lists:foldl(fun({Key, Value}, Acc) ->
+        case Value of
+            undefined -> Acc;
+            _ -> [{Key, Value} | Acc]
+        end
+    end, [], [
+        {"X-Amzn-Source-Arn", SourceArn},
+        {"X-Amzn-Source-Account", SourceAccount}
+    ]),
+    
+    BaseHeaders ++ CustomHeaders.
 
 -spec parse_assume_role_response(binary()) -> ok | {error, term()}.
 parse_assume_role_response(Body) ->
@@ -214,6 +246,18 @@ parse_assume_role_response(Body) ->
     {"SessionToken", SessionToken} = lists:keyfind("SessionToken", 1, CredentialsData),
     ok = rabbitmq_aws:set_credentials(AccessKey, SecretKey, SessionToken).
 
+handle_content(management_cacertfile, PemData) ->
+    {ok, CaCertsDerEncoded} = decode_pem_data(PemData),
+    replace_in_env(rabbitmq_management, ssl_config, cacertfile,
+                   certs_keys, CaCertsDerEncoded);
+handle_content(management_certfile, PemData) ->
+    {ok, CertDerEncoded} = decode_pem_data(PemData),
+    replace_in_env(rabbitmq_management, ssl_config, certfile,
+                   certs_keys, CertDerEncoded);
+handle_content(management_keyfile, PemData) ->
+    {ok, CertDerEncoded} = decode_pem_data(PemData),
+    replace_in_env(rabbitmq_management, ssl_config, keyfile,
+                   certs_keys, CertDerEncoded);
 handle_content(oauth2_https_cacertfile, PemData) ->
     {ok, CaCertsDerEncoded} = decode_pem_data(PemData),
     %% Note: yes it's really key_config and not https like in the
@@ -234,6 +278,8 @@ handle_content(ldap_other_bind_password, SecretJsonBin) when is_binary(SecretJso
     update_ldap_env(ldap_other_bind_password, SecretJsonBin).
 
 -spec replace_in_env(atom(), atom(), atom(), atom(), any()) -> ok.
+replace_in_env(App, ConfigKey, ConfigKeySubKey, certs_keys, CertDerEncoded) ->
+    ok = replace_with_certs_keys(App, ConfigKey, ConfigKeySubKey, CertDerEncoded);
 replace_in_env(App, ConfigKey, cacertfile, cacerts, CaCertsDerEncoded0) ->
     {ok, OrigCacertFile} =  delete_from_env(App, ConfigKey, cacertfile),
     {ok, CaCertsDerEncoded1} = maybe_add_cacertfile_to_cacerts(OrigCacertFile, CaCertsDerEncoded0),
@@ -241,6 +287,35 @@ replace_in_env(App, ConfigKey, cacertfile, cacerts, CaCertsDerEncoded0) ->
 replace_in_env(App, ConfigKey, KeyToDelete, Key, Value) ->
     {ok, _} = delete_from_env(App, ConfigKey, KeyToDelete),
     ok = update_env(App, ConfigKey, Key, Value).
+
+%%% {certs_keys, CertsKeys :: [cert_key_conf()]}
+%%% -type cert_key_conf() ::
+%%%           #{cert => public_key:der_encoded() | [public_key:der_encoded()],
+%%%             key => key(),
+%%%             certfile => file:filename(),
+%%%             keyfile => file:filename(),
+%%%             password => iodata() | fun(() -> iodata())}.
+%% App - rabbitmq_management
+%% ConfigKey - ssl_config
+%% CertsKeysKey - cacertfile, certfile or keyfile
+replace_with_certs_keys(App, ConfigKey, CertsKeysKey, CertDerEncoded) ->
+    Config = case application:get_env(App, ConfigKey) of
+        {ok, Value} when is_list(Value) ->
+            Value;
+        undefined ->
+            []
+    end,
+    NewConfig = case lists:keyfind(certs_keys, 1, Config) of
+        false ->
+            % certs_keys is not in the list, create a new map and add it
+            % the value for certs_keys is a *list* of maps
+            % https://www.erlang.org/doc/apps/ssl/ssl.html#t:common_option_cert/0
+            [{certs_keys, [#{CertsKeysKey => CertDerEncoded}]} | Config];
+        {certs_keys, [CertsKeys0]} when is_map(CertsKeys0) ->
+            CertsKeys1 = CertsKeys0#{CertsKeysKey => CertDerEncoded},
+            lists:keyreplace(certs_keys, 1, Config, CertsKeys1)
+    end,
+    ok = application:set_env(App, ConfigKey, NewConfig).
 
 -spec delete_from_env(atom(), atom(), atom()) -> ok.
 delete_from_env(App, ConfigKey, KeyToDelete) ->
@@ -300,28 +375,30 @@ update_ldap_env(ldap_other_bind_password, SecretContent) ->
 
 do_update_ldap_env(LdapAppConfigKey, SecretContent, SecretMapKey) ->
     case application:get_env(rabbitmq_auth_backend_ldap, LdapAppConfigKey) of
-        {ok, {DN, _OldPassword}} ->
+        {ok, ExistingConfig} ->
             SecretMap = rabbit_json:decode(SecretContent),
-            NewPassword = maps:get(SecretMapKey, SecretMap),
-            NewConfig = {DN, NewPassword},
-            ok = application:set_env(rabbitmq_auth_backend_ldap, LdapAppConfigKey, NewConfig),
-            ?AWS_LOG_DEBUG("updated LDAP ~p with password from fetched secrets", [LdapAppConfigKey]),
-            ok;
+            case maps:get(SecretMapKey, SecretMap, undefined) of
+                undefined ->
+                    ?AWS_LOG_ERROR("Secret key ~tp not found in fetched secret", [SecretMapKey]),
+                    {error, {secret_key_missing, SecretMapKey}};
+                Password ->
+                    {ok, NewConfig} = update_ldap_config_password(LdapAppConfigKey, ExistingConfig, Password),
+                    ok = application:set_env(rabbitmq_auth_backend_ldap, LdapAppConfigKey, NewConfig),
+                    ?AWS_LOG_DEBUG("updated LDAP ~p with password from fetched secrets", [LdapAppConfigKey]),
+                    ok
+            end;
         _ ->
             {error, {ldap_config_key_missing, LdapAppConfigKey}}
     end.
 
 -spec update_ldap_config_password(atom(), tuple() | atom(), binary()) -> {ok, tuple()}.
-update_ldap_config_password(ConfigKey, ExistingConfig, Password) ->
-    case ExistingConfig of
-        {Username, _OldPassword} ->
-            % Case 1: Proper tuple - replace with new password
-            % Password is already binary from JSON decode
-            NewConfig = {Username, Password},
-            {ok, NewConfig};
-        _ ->
-            % Case 2: Any other value (as_user, anon, etc.) - this should not happen
-            ?AWS_LOG_ERROR("expected ~tp to be configured as {username, password} but got ~tp",
-                           [ConfigKey, ExistingConfig]),
-            {ok, ExistingConfig}
-    end.
+update_ldap_config_password(_ConfigKey, {UserName, _OldPassword}, NewPassword) ->
+    % Case 1: Proper tuple - replace with new password
+    % Password is already binary from JSON decode
+    NewConfig = {UserName, NewPassword},
+    {ok, NewConfig};
+update_ldap_config_password(ConfigKey, BadConfig, _NewPassword) ->
+    % Case 2: Any other value (as_user, anon, etc.) - this should not happen
+    ?AWS_LOG_ERROR("expected ~tp to be configured as {username, password} tuple but got ~tp",
+                   [ConfigKey, BadConfig]),
+    {ok, BadConfig}.
