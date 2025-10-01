60b5a1333bc6e16aab2622288b57d93f38aeef16
Lois Soto Lopez <lois@84codes.com>
2025-10-01 13:34:32 +0200

    Apply PR suggestions

--- deps/rabbit/src/rabbit_stream_coordinator.erl
+++ deps/rabbit/src/rabbit_stream_coordinator.erl
@@ -154,7 +154,7 @@ stop() ->
 new_stream(Q, LeaderNode)
   when ?is_amqqueue(Q) andalso is_atom(LeaderNode) ->
     #{name := StreamId} = amqqueue:get_type_state(Q),
-    Nodes = rabbit_amqqueue:get_nodes(Q),
+    Nodes = rabbit_queue_type:get_nodes(Q),
     %% assertion leader is in nodes configuration
     true = lists:member(LeaderNode, Nodes),
     process_command({new_stream, StreamId,
