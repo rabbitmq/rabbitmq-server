b447b2632ae1690fd868c0637ff2297652973bd3
Michael Davis <mcrdavis@amazon.com>
2025-10-23 16:50:58 -0400

    Set per-queue-type disk alarms for configured mounts
    This introduces a new variant of `rabbit_alarm:resource_alarm_source()`:
    `{disk, QueueType}` which triggers when the configured mount for queue
    type(s) fall under their limit of available space.

--- deps/rabbitmq_prometheus/src/collectors/prometheus_rabbitmq_alarm_metrics_collector.erl
+++ deps/rabbitmq_prometheus/src/collectors/prometheus_rabbitmq_alarm_metrics_collector.erl
@@ -35,11 +35,13 @@ collect_mf(_Registry, Callback) ->
         Alarms = rabbit_alarm:get_local_alarms(500), %% TODO: figure out timeout
         ActiveAlarms =
             lists:foldl(fun ({{resource_limit, disk, _}, _}, Acc) ->
-                                maps:put(disk_limit, 1, Acc);
+                                Acc#{disk_limit => 1};
+                            ({{resource_limit, {disk, QT}, _}, _}, Acc) ->
+                                Acc#{{disk, QT} => 1};
                             ({{resource_limit, memory, _}, _}, Acc) ->
-                                maps:put(memory_limit, 1, Acc);
+                                Acc#{memory_limit => 1};
                             ({file_descriptor_limit, _}, Acc) ->
-                                maps:put(file_descriptor_limit, 1, Acc)
+                                Acc#{file_descriptor_limit => 1}
                         end,
                         #{},
                         Alarms),
@@ -58,6 +60,15 @@ collect_mf(_Registry, Callback) ->
                            <<"is 1 if VM memory watermark alarm is in effect">>,
                            untyped,
                            [untyped_metric(maps:get(memory_limit, ActiveAlarms, 0))])),
+
+        Callback(create_mf(?METRIC_NAME(<<"queue_type_free_disk_space_watermark">>),
+                           <<"is 1 if the queue type disk-space alarm is in effect">>,
+                           untyped,
+                           [prometheus_model_helpers:untyped_metric(
+                              #{queue_type => QT},
+                              maps:get({disk, QT}, ActiveAlarms, 0)) ||
+                            {_, QT} <- rabbit_registry:lookup_all(queue)])),
+
         ok
     catch
         exit:{timeout, _} ->
