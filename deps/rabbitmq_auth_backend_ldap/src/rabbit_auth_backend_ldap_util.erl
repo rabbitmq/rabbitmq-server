813617a85c5b106f6e65a5b264a5e9277da8b91e
Luke Bakken <lrbakken@amazon.com>
2025-10-28 10:06:09 -0700

    Add support for multiline cuttlefish values in LDAP plugin
    The cuttlefish project recently added support for multiline values. The
    primary motivation for this is so that LDAP settings like
    `vhost_access_query` could now be expressed in `rabbitmq.conf`, rather
    than requiring `advanced.config`.
    
    These changes require cuttlefish version `3.6.0` or later.
    
    References:
    * https://github.com/Kyorai/cuttlefish/issues/60
    * https://github.com/Kyorai/cuttlefish/pull/63

--- deps/rabbitmq_auth_backend_ldap/src/rabbit_auth_backend_ldap_util.erl
+++ deps/rabbitmq_auth_backend_ldap/src/rabbit_auth_backend_ldap_util.erl
@@ -7,7 +7,7 @@
 
 -module(rabbit_auth_backend_ldap_util).
 
--export([fill/2, get_active_directory_args/1]).
+-export([fill/2, get_active_directory_args/1, parse_query/1]).
 
 fill(Fmt, []) ->
     binary_to_list(iolist_to_binary(Fmt));
@@ -32,3 +32,57 @@ get_active_directory_args(Username) when is_binary(Username) ->
     % If Username is in Domain\User format, provide additional fill
     % template arguments
     get_active_directory_args(binary:split(Username, <<"\\">>, [trim_all])).
+
+parse_query(Query) when is_binary(Query) ->
+    parse_query(rabbit_data_coercion:to_unicode_charlist(Query));
+parse_query(Query0) when is_list(Query0) ->
+    Query1 = fixup_query(Query0),
+    parse_query_handle_erl_scan(erl_scan:string(Query1)).
+
+fixup_query(Query0) ->
+    Query1 = string:trim(Query0, both),
+    fixup_query(lists:last(Query1) =:= $., Query1).
+
+fixup_query(true, Query) ->
+    Query;
+fixup_query(false, Query) ->
+    Query ++ ".".
+
+parse_query_handle_erl_scan({ok, Tokens, _EndLine}) ->
+    parse_query_handle_exprs(erl_parse:parse_exprs(Tokens));
+parse_query_handle_erl_scan(Error) ->
+    cuttlefish:invalid(fmt("invalid query: ~tp", [Error])).
+
+parse_query_handle_exprs({ok, AbsForm}) ->
+    parse_query_handle_eval(erl_eval:exprs(AbsForm, erl_eval:new_bindings()));
+parse_query_handle_exprs(Error) ->
+    cuttlefish:invalid(fmt("invalid query: ~tp", [Error])).
+
+parse_query_handle_eval({value, {constant, true}=T, _}) ->
+    T;
+parse_query_handle_eval({value, {constant, false}=T, _}) ->
+    T;
+parse_query_handle_eval({value, {in_group, _}=T, _}) ->
+    T;
+parse_query_handle_eval({value, {in_group_nested, _, _}=T, _}) ->
+    T;
+parse_query_handle_eval({value, {for, Q}=T, _}) when is_list(Q) ->
+    T;
+parse_query_handle_eval({value, {'not', _}=T, _}) ->
+    T;
+parse_query_handle_eval({value, {'and', Q}=T, _}) when is_list(Q) ->
+    T;
+parse_query_handle_eval({value, {'or', Q}=T, _}) when is_list(Q) ->
+    T;
+parse_query_handle_eval({value, {equals, _, _}=T, _}) ->
+    T;
+parse_query_handle_eval({value, {match, _, _}=T, _}) ->
+    T;
+parse_query_handle_eval({value, T, _}) when is_list(T) ->
+    %% NB: tag_queries uses this form
+    T;
+parse_query_handle_eval({value, Unexpected, _}) ->
+    cuttlefish:invalid(fmt("invalid query: ~tp", [Unexpected])).
+
+fmt(Fmt, Args) ->
+    rabbit_data_coercion:to_unicode_charlist(io_lib:format(Fmt, Args)).
