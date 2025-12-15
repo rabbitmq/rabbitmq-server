e7e8040d803fb5ee4c083402cc6891d38e3cf080
Michael Klishin <mikhail.klishin@broadcom.com>
2025-12-15 08:53:57 -0800

    Reduce log spam, trailing ws #15101 #15137

--- deps/rabbitmq_management/src/rabbit_mgmt_oauth_bootstrap.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_oauth_bootstrap.erl
@@ -12,24 +12,24 @@
 -include_lib("kernel/include/logger.hrl").
 
 %%--------------------------------------------------------------------
-%% js/oidc-oauth/bootstrap.js 
-%% It produces a javascript file with all the oauth2 configuration needed 
+%% js/oidc-oauth/bootstrap.js
+%% It produces a javascript file with all the oauth2 configuration needed
 %% in the client-side of the management ui.
 %% This endpoint only accepts GET method.
 %%
-%% It can work in conjunction with the /api/login endpoint. If the users are 
+%% It can work in conjunction with the /api/login endpoint. If the users are
 %% redirected to the home page of the management ui, and eventually to this endpoint,
-%% via the /api/login endpoint is very likely that the request carries a cookie. 
+%% via the /api/login endpoint is very likely that the request carries a cookie.
 %% It can be the <<"access_token">> cookie or the cookies <<"strict_auth_mechanism">>
 %% or <<"preferred_auth_mechanism">>.
 %% These cookies are consumed by this endpoint and removed afterwards.
 %%
 %% Additionally, this endpoint may accept users' authentication mechanism preferences
-%% via its corresponding header, in addition to the two cookies mentioned above. 
-%% But not via request parameters. If this endpoint would have accepted request parameters, 
-%% it would have to use the "Referer" header to extract the original request parameters. 
+%% via its corresponding header, in addition to the two cookies mentioned above.
+%% But not via request parameters. If this endpoint would have accepted request parameters,
+%% it would have to use the "Referer" header to extract the original request parameters.
 %% It is possible that in some environments, these headers may be dropped before they reach this endpoint.
-%% Therefore, users who can only use request parameters, they have to use the /api/login 
+%% Therefore, users who can only use request parameters, they have to use the /api/login
 %% endpoint instead.
 
 init(Req0, State) ->
@@ -45,7 +45,7 @@ bootstrap_oauth(Req0, State) ->
                 set_oauth_settings(AuthSettings) ++
                 SetTokenAuth ++
                 export_dependencies(Dependencies),
-    
+
     {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"text/javascript; charset=utf-8">>},
         JSContent, Req2), State}.
 
@@ -56,11 +56,11 @@ enrich_oauth_settings(Req0, AuthSettings) ->
         {preferred_auth_mechanism, Args} -> {Req1, [{preferred_auth_mechanism, Args} | AuthSettings]};
         {strict_auth_mechanism, Args} -> {Req1, [{strict_auth_mechanism, Args} | AuthSettings]};
         {error, Reason} -> ?LOG_DEBUG("~p", [Reason]),
-                           {Req1, AuthSettings}        
+                           {Req1, AuthSettings}
     end.
 get_auth_mechanism(Req) ->
-    case get_auth_mechanism_from_cookies(Req) of 
-        undefined -> 
+    case get_auth_mechanism_from_cookies(Req) of
+        undefined ->
             case cowboy_req:header(<<"x-", ?MANAGEMENT_LOGIN_STRICT_AUTH_MECHANISM/binary>>, Req) of
                 undefined ->
                     case cowboy_req:header(<<"x-", ?MANAGEMENT_LOGIN_PREFERRED_AUTH_MECHANISM/binary>>, Req) of
@@ -69,38 +69,37 @@ get_auth_mechanism(Req) ->
                     end;
                 Val -> {Req, {strict_auth_mechanism, Val}}
             end;
-        {Type, _} = Auth -> { cowboy_req:set_resp_cookie(term_to_binary(Type), 
+        {Type, _} = Auth -> { cowboy_req:set_resp_cookie(term_to_binary(Type),
                                     <<"">>, Req, #{
                                         max_age => 0,
                                         http_only => true,
                                         path => ?OAUTH2_BOOTSTRAP_PATH,
                                         same_site => strict
-                                }), 
+                                }),
                                 Auth
                             }
     end.
 
 get_auth_mechanism_from_cookies(Req) ->
     Cookies = cowboy_req:parse_cookies(Req),
-    ?LOG_DEBUG("get_auth_mechanism_from_cookies: ~p", [Cookies]),
-    case proplists:get_value(?MANAGEMENT_LOGIN_STRICT_AUTH_MECHANISM, Cookies) of 
-        undefined -> 
-            case proplists:get_value(?MANAGEMENT_LOGIN_PREFERRED_AUTH_MECHANISM, Cookies) of 
+    case proplists:get_value(?MANAGEMENT_LOGIN_STRICT_AUTH_MECHANISM, Cookies) of
+        undefined ->
+            case proplists:get_value(?MANAGEMENT_LOGIN_PREFERRED_AUTH_MECHANISM, Cookies) of
                 undefined -> undefined;
                 Val -> {preferred_auth_mechanism, Val}
             end;
         Val -> {strict_auth_mechanism, Val}
     end.
-validate_auth_mechanism({Type, <<"oauth2:", Id/binary>>}, AuthSettings) ->    
-    case maps:is_key(Id, proplists:get_value(oauth_resource_servers, AuthSettings)) of 
+validate_auth_mechanism({Type, <<"oauth2:", Id/binary>>}, AuthSettings) ->
+    case maps:is_key(Id, proplists:get_value(oauth_resource_servers, AuthSettings)) of
         true -> {Type, [{type, <<"oauth2">>}, {resource_id, Id}]};
         _ -> {error, {unknown_resource_id, Id}}
     end;
-validate_auth_mechanism({Type, <<"basic">>}, _AuthSettings) -> 
+validate_auth_mechanism({Type, <<"basic">>}, _AuthSettings) ->
     {Type, [{type, <<"basic">>}]};
 validate_auth_mechanism({_, _}, _AuthSettings) -> {error, unknown_auth_mechanism};
 validate_auth_mechanism(_, _) -> {error, unknown_auth_mechanism}.
-    
+
 set_oauth_settings(AuthSettings) ->
     JsonAuthSettings = rabbit_json:encode(rabbit_mgmt_format:format_nulls(AuthSettings)),
     ["set_oauth_settings(", JsonAuthSettings, ");"].
@@ -109,20 +108,19 @@ set_token_auth(AuthSettings, Req0) ->
     case proplists:get_value(oauth_enabled, AuthSettings, false) of
         true ->
             case cowboy_req:parse_header(<<"authorization">>, Req0) of
-                {bearer, Token} -> 
+                {bearer, Token} ->
                     {
-                        Req0, 
+                        Req0,
                         ["set_token_auth('", Token, "');"]
                     };
-                _ -> 
+                _ ->
                     Cookies = cowboy_req:parse_cookies(Req0),
-                    ?LOG_DEBUG("set_token_auth: ~p", [Cookies]),
-                    case proplists:get_value(?OAUTH2_ACCESS_TOKEN, Cookies) of 
+                    case proplists:get_value(?OAUTH2_ACCESS_TOKEN, Cookies) of
                          undefined -> {
-                                Req0, 
+                                Req0,
                                 []
                             };
-                        Token -> 
+                        Token ->
                             {
                                 cowboy_req:set_resp_cookie(
                                     ?OAUTH2_ACCESS_TOKEN, <<"">>, Req0, #{
@@ -130,13 +128,13 @@ set_token_auth(AuthSettings, Req0) ->
                                         http_only => true,
                                         path => ?OAUTH2_BOOTSTRAP_PATH,
                                         same_site => strict
-                                    }), 
+                                    }),
                                 ["set_token_auth('", Token, "');"]
-                            }                       
+                            }
                     end
             end;
         false -> {
-                Req0, 
+                Req0,
                 []
             }
     end.
