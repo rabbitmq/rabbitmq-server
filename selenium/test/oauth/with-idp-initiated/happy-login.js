9aff1fb0428181967dbed2dc6ec5ae905dfcb97f
Marcial Rosales <marcial.rosales@broadcom.com>
2025-12-18 12:21:48 +0100

    Merge upstream changes in tanzu
    To avoid future merge from oss to tanzu

--- selenium/test/oauth/with-idp-initiated/happy-login.js
+++ selenium/test/oauth/with-idp-initiated/happy-login.js
@@ -11,8 +11,12 @@ describe('A user with a JWT token', function () {
   let overview
   let captureScreen
   let fakePortal
-  
+  let username
+  let password
+
   before(async function () {
+    username = process.env.MGT_CLIENT_ID_FOR_IDP_INITIATED || 'rabbit_idp_user'
+    password = process.env.MGT_CLIENT_SECRET_FOR_IDP_INITIATED || 'rabbit_idp_user'
     driver = buildDriver()
     overview = new OverviewPage(driver)
     captureScreen = captureScreensFor(driver, __filename)
@@ -20,13 +24,13 @@ describe('A user with a JWT token', function () {
   })
 
   it('can log in presenting the token to the /login URL via fakeportal', async function () {
-    await fakePortal.goToHome("rabbit_idp_user", "rabbit_idp_user")
+    await fakePortal.goToHome(username, password)
     if (!await fakePortal.isLoaded()) {
       throw new Error('Failed to load fakePortal')
     }
     await fakePortal.login()
     await overview.isLoaded()
-    assert.equal(await overview.getUser(), 'User rabbit_idp_user')
+    assert.equal(await overview.getUser(), 'User ' + username)
   })
 
   after(async function () {
