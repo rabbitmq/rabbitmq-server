27cb757bfb1a6092e91860ced5e86c631971cfb9
Michael Klishin <michaelklishin@icloud.com>
2026-01-14 20:19:55 -0800

    Federation: make connection.close timeout configurable
    Currently the worst case scenario is 10s per connection.
    
    Since any lower default will be wrong for someone,
    let's just make the whole thing configurable in each
    plugin.
    
    (cherry picked from commit 3a73033cb2e980f4111c8b36b9bc69a0db1ed359)

--- deps/rabbitmq_federation_common/src/rabbit_federation_link_util.erl
+++ deps/rabbitmq_federation_common/src/rabbit_federation_link_util.erl
@@ -14,17 +14,20 @@
 
 %% real
 -export([start_conn_ch/5, disposable_channel_call/2, disposable_channel_call/3,
-         disposable_connection_call/3, ensure_connection_closed/1,
+         disposable_connection_call/3,
+         ensure_connection_closed/1, ensure_connection_closed/2,
          log_terminate/4, unacked_new/0, ack/3, nack/3, forward/9,
          handle_downstream_down/3, handle_upstream_down/3,
-         get_connection_name/2]).
+         get_connection_name/2,
+         connection_close_timeout/0]).
 
 %% temp
 -export([connection_error/6]).
 
 -import(rabbit_misc, [pget/2]).
 
--define(MAX_CONNECTION_CLOSE_TIMEOUT, 10000).
+-define(CONNECTION_OPEN_TIMEOUT, 10000).
+-define(MAX_CONNECTION_CLOSE_TIMEOUT, 5000).
 
 %%----------------------------------------------------------------------------
 
@@ -123,7 +126,13 @@ open(Params, Name) ->
 ensure_channel_closed(Ch) -> catch amqp_channel:close(Ch).
 
 ensure_connection_closed(Conn) ->
-    catch amqp_connection:close(Conn, ?MAX_CONNECTION_CLOSE_TIMEOUT).
+    ensure_connection_closed(Conn, ?CONNECTION_OPEN_TIMEOUT).
+
+ensure_connection_closed(Conn, Timeout) ->
+    catch amqp_connection:close(Conn, Timeout).
+
+connection_close_timeout() ->
+    ?MAX_CONNECTION_CLOSE_TIMEOUT.
 
 connection_error(remote_start, {{shutdown, {server_initiated_close, Code, Message}}, _} = E,
                                Upstream, UParams, XorQName, State) ->
@@ -315,6 +324,9 @@ disposable_channel_call(Conn, Method, ErrFun) ->
     end.
 
 disposable_connection_call(Params, Method, ErrFun) ->
+    disposable_connection_call(Params, Method, ErrFun, connection_close_timeout()).
+
+disposable_connection_call(Params, Method, ErrFun, Timeout) ->
     try
         ?LOG_DEBUG("Disposable connection parameters: ~tp", [Params]),
         case open(Params, <<"Disposable exchange federation link connection">>) of
@@ -326,7 +338,7 @@ disposable_connection_call(Params, Method, ErrFun) ->
                       exit:{{shutdown, {server_initiated_close, Code, Message}}, _} ->
                           ErrFun(Code, Message)
                 after
-                    ensure_connection_closed(Conn)
+                    ensure_connection_closed(Conn, Timeout)
                 end;
             {error, {auth_failure, Message}} ->
                 ?LOG_ERROR("Federation link could not open a disposable (one-off) connection "
