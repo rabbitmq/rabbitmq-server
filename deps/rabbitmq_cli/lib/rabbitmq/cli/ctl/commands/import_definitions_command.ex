b4ed713f801c4c7c2061b77c2f889a57fdc64fe4
Michael Klishin <michaelklishin@icloud.com>
2026-04-08 14:15:46 -0700

    Switch CLI tools to an Erlang JSON module; re-enable Dialyzer for 4 plugins
    Elixir 1.18+ introduces a standard library JSON module
    which conflicts with the `json` library we've been using
    in CLI tools for `--formatter=json` and such.
    
    Since the two Elixir versions are not compatible
    and we have reasons to not bump Elixir too aggressively
    just yet, the most pragmatic option was to
    switch CLI tools to use `thoas`, the Erlang JSON
    parser RabbitMQ core and HTTP API use.
    
    This unblocks Dialyzer runs in four plugins disabled
    about 10 months ago and reveals a `rabbitmq_mqtt`
    Dialyzer failure due to a field that was removed
    from a record but not its type spec.
    
    Due to the MQTT plugin issue above, this can only
    be backported to `v4.3.x`, a `v4.2.x` version
    will need some tweaks.

--- deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/import_definitions_command.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/import_definitions_command.ex
@@ -166,7 +166,7 @@ defmodule RabbitMQ.CLI.Ctl.Commands.ImportDefinitionsCommand do
   #
 
   defp deserialise(bin, "json") do
-    JSON.decode(bin)
+    RabbitMQ.CLI.Core.JSON.decode(bin)
   end
 
   defp deserialise(bin, "erlang") do
