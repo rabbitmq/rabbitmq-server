a8275bdec8d3e10db864d27769bf737fb54ce974
Michael Klishin <michaelklishin@icloud.com>
2026-04-09 23:47:57 -0700

    Revert "Fall back to tracked-connection record"
    This reverts commit 916a53c459c244762707a7bcb4a2f43742412994.

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_connection.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_connection.erl
@@ -40,16 +40,7 @@ to_json(ReqData, Context) ->
             false ->
                 conn_stats(ReqData);
             true ->
-                case rabbit_mgmt_db:get_connection(
-                       rabbit_mgmt_util:id(connection, ReqData)) of
-                    not_found ->
-                        %% IMPORTANT: connection_created_stats is empty when
-                        %% the metrics collector is disabled. Fall back to
-                        %% the tracked-connection record.
-                        conn(ReqData);
-                    Stats ->
-                        Stats
-                end
+                rabbit_mgmt_db:get_connection(rabbit_mgmt_util:id(connection, ReqData))
         end,
     ConnStatsWithoutPids = rabbit_mgmt_format:strip_pids(ConnStats),
     ReplyData = maps:from_list(ConnStatsWithoutPids),
