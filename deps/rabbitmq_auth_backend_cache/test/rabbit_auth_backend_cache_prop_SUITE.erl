690f15eb6283d65af1fc7a76500c4778bf569cbc
Michael Klishin <michaelklishin@icloud.com>
2026-04-28 18:00:39 -0700

    `rabbitmq_auth_backend_cache`: avoid sensitive values in cache keys
    The 2nd item brought up in #16255.

--- deps/rabbitmq_auth_backend_cache/test/rabbit_auth_backend_cache_prop_SUITE.erl
+++ deps/rabbitmq_auth_backend_cache/test/rabbit_auth_backend_cache_prop_SUITE.erl
@@ -25,7 +25,9 @@ all() ->
      prop_identical_authprops_hit_cache,
      prop_separates_loopback_from_non_loopback,
      prop_different_passwords_yield_different_keys,
-     prop_different_usernames_yield_different_keys
+     prop_different_usernames_yield_different_keys,
+     prop_password_is_absent_from_cache_key,
+     prop_redaction_is_deterministic
     ].
 
 init_per_suite(Config) ->
@@ -96,6 +98,25 @@ prop_different_usernames_yield_different_keys(Config) ->
                                distinct_username_count_two(Config, U1, U2, P)))
       end, [], ?NUMTESTS).
 
+%% Security invariant: the plaintext password must not be a substring of
+%% the cache key, regardless of username and loopback flag.
+prop_password_is_absent_from_cache_key(Config) ->
+    run_proper(
+      fun() ->
+              ?FORALL({U, P, L},
+                      {username(), non_trivial_password(), boolean()},
+                      password_absent_from_key(Config, U, P, L))
+      end, [], ?NUMTESTS).
+
+%% Equality invariant: redacting the same AuthProps twice must produce
+%% byte-identical keys, otherwise repeat logins miss.
+prop_redaction_is_deterministic(Config) ->
+    run_proper(
+      fun() ->
+              ?FORALL({U, P, L}, {username(), password(), boolean()},
+                      redaction_deterministic(Config, U, P, L))
+      end, [], ?NUMTESTS).
+
 %%%=========================================================================
 %%% Property bodies
 %%%=========================================================================
@@ -125,6 +146,16 @@ distinct_username_count_two(Config, U1, U2, P) ->
     {ok, _} = login(Config, U2, [{password, P}]),
     2 =:= call_count(Config).
 
+password_absent_from_key(Config, U, P, L) ->
+    Key = cache_key(Config, user_login_authentication,
+                    [U, [{password, P}, {is_loopback, L}]]),
+    binary:match(term_to_binary(Key), P) =:= nomatch.
+
+redaction_deterministic(Config, U, P, L) ->
+    Args = [U, [{password, P}, {is_loopback, L}]],
+    cache_key(Config, user_login_authentication, Args)
+        =:= cache_key(Config, user_login_authentication, Args).
+
 %%%=========================================================================
 %%% Helpers
 %%%=========================================================================
@@ -136,6 +167,9 @@ fresh_state(Config) ->
 login(Config, U, Props) ->
     rpc(Config, rabbit_auth_backend_cache, user_login_authentication,
         [U, Props]).
+
+cache_key(Config, F, A) ->
+    rpc(Config, rabbit_auth_backend_cache, cache_key, [F, A]).
 
 call_count(Config) ->
     rpc(Config, rabbit_auth_backend_cache_counting_mock,
@@ -153,3 +187,8 @@ username() ->
 
 password() ->
     non_empty(binary()).
+
+%% A password long enough that random collisions with hash output bytes
+%% are negligible, used by the "password absent from key" property.
+non_trivial_password() ->
+    ?LET(B, non_empty(binary()), <<"pw-", B/binary, "-marker">>).
