5cc50dac4f728022ef6b63dfe440d58135ce9d0e
Michal Kuratczyk <michal.kuratczyk@broadcom.com>
2026-05-13 14:29:33 +0200

    Prevent false-positive vhost deletion (#16422)
    * Prevent false-positive vhost deletion
    
    (cherry picked from commit 972921a99d5ead78097b718e02dcb6ac07bf2707)

--- deps/rabbit/src/rabbit_db_vhost.erl
+++ deps/rabbit/src/rabbit_db_vhost.erl
@@ -19,6 +19,7 @@
          merge_metadata/2,
          set_tags/2,
          exists/1,
+         exists_uncached/1,
          get/1,
          get_all/0,
          count_all/0,
@@ -64,9 +65,9 @@ create_or_get(VHostName, Limits, Metadata)
     case rabbit_khepri:create(Path, VHost) of
         ok ->
             {new, VHost};
-        {error, {khepri, mismatching_node,
-                 #{node_path := Path,
-                   node_props := #{data := ExistingVHost}}}} ->
+        {error, ?khepri_error(mismatching_node,
+                              #{node_path := Path,
+                                node_props := #{data := ExistingVHost}})} ->
             {existing, ExistingVHost};
         Error ->
             throw(Error)
@@ -114,12 +115,12 @@ do_merge_metadata(VHostName, Metadata) ->
             case Ret2 of
                 ok ->
                     {ok, VHost};
-                {error, {khepri, mismatching_node, _}} ->
+                {error, ?khepri_error(mismatching_node, _)} ->
                     merge_metadata(VHostName, Metadata);
                 {error, _} = Error ->
                     Error
             end;
-        {error, {khepri, node_not_found, _}} ->
+        {error, ?khepri_error(node_not_found, _)} ->
             {error, {no_such_vhost, VHostName}};
         {error, _} = Error ->
             Error
@@ -200,6 +201,32 @@ exists(VHostName) when is_binary(VHostName) ->
     catch
         error:badarg ->
             false
+    end.
+
+%% -------------------------------------------------------------------
+%% exists_uncached().
+%% -------------------------------------------------------------------
+
+-spec exists_uncached(VHostName) -> Ret when
+      VHostName :: vhost:name(),
+      Ret :: boolean() | {error, any()}.
+%% @doc Checks if the virtual host named `VHostName' exists by querying
+%% Khepri directly, bypassing the ETS-backed projection.
+%%
+%% @returns `true' if the virtual host exists in Khepri, `false' if it does
+%% not exist, or `{error, Reason}' if Khepri is unavailable.
+%%
+%% @private
+
+exists_uncached(VHostName) when is_binary(VHostName) ->
+    Path = khepri_vhost_path(VHostName),
+    case rabbit_khepri:get(Path) of
+        {ok, _} ->
+            true;
+        {error, ?khepri_error(node_not_found, _)} ->
+            false;
+        {error, _} = Error ->
+            Error
     end.
 
 %% -------------------------------------------------------------------
@@ -309,12 +336,12 @@ update(VHostName, UpdateFun)
             case rabbit_khepri:put(Path1, V1) of
                 ok ->
                     V1;
-                {error, {khepri, mismatching_node, _}} ->
+                {error, ?khepri_error(mismatching_node, _)} ->
                     update(VHostName, UpdateFun);
                 Error ->
                     throw(Error)
             end;
-        {error, {khepri, node_not_found, _}} ->
+        {error, ?khepri_error(node_not_found, _)} ->
             throw({error, {no_such_vhost, VHostName}});
         Error ->
             throw(Error)
