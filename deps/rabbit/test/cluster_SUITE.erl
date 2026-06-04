fc4b939c0ace04b00a978bd34b1fa5b4cf48e230
Michael Klishin <michaelklishin@icloud.com>
2026-06-03 21:14:10 -0700

    Don't look for queue process crashes in `crashing_queues_SUITE` and `cluster_SUITE`

--- deps/rabbit/test/cluster_SUITE.erl
+++ deps/rabbit/test/cluster_SUITE.erl
@@ -63,7 +63,9 @@ init_per_group(Group, Config) ->
         true ->
             Config1 = rabbit_ct_helpers:set_config(Config, [
                 {rmq_nodename_suffix, Group},
-                {rmq_nodes_count, 2}
+                {rmq_nodes_count, 2},
+                %% This suite intentionally forces queue processes to fail.
+                {find_crashes, false}
               ]),
             rabbit_ct_helpers:run_steps(Config1,
               rabbit_ct_broker_helpers:setup_steps() ++
