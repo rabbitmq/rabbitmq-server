004f673b127c2c2c16213dfd129fbdc7f30b490b
Ayanda Dube <adube14@bloomberg.net>
2026-06-10 14:27:48 +0100

    remove unnecessary rabbit_channel:ready_for_close/1 - prefer that in rabbit_channel_common
    (cherry picked from commit 694499ba8636f65cc0801efdeeb178ccb63e9561)
    (cherry picked from commit 06413f0e56c73da933fb5aa82425d51700a8ab7e)

--- deps/rabbit/src/rabbit_reader.erl
+++ deps/rabbit/src/rabbit_reader.erl
@@ -611,7 +611,7 @@ handle_other({conserve_resources, Source, Conserve},
           end,
     control_throttle(State#v1{throttle = Throttle#throttle{blocked_by = Blockers1}});
 handle_other({channel_closing, ChPid}, State) ->
-    ok = rabbit_channel:ready_for_close(ChPid),
+    ok = rabbit_channel_common:ready_for_close(ChPid),
     {_, State1} = channel_cleanup(ChPid, State),
     maybe_close(control_throttle(State1));
 handle_other({'EXIT', Parent, normal}, State = #v1{parent = Parent}) ->
