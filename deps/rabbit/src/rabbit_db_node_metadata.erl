177459f1780cf3a959a07f08301201cebbd41705
Michal Kuratczyk <michal.kuratczyk@broadcom.com>
2026-06-26 16:59:13 +0200

    Add Khepri-based node metadata storage and projection
    Introduce `rabbit_db_node_metadata` to store generic node-specific metadata
    in Khepri and query it locally via an ETS-backed projection.

---
+++ deps/rabbit/src/rabbit_db_node_metadata.erl
@@ -0,0 +1,102 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2026 Broadcom. All Rights Reserved. The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries. All rights reserved.
+%%
+
+-module(rabbit_db_node_metadata).
+
+-include_lib("kernel/include/logger.hrl").
+-include_lib("khepri/include/khepri.hrl").
+-include_lib("rabbit_common/include/rabbit.hrl").
+-include_lib("rabbit_common/include/logging.hrl").
+
+-include("include/rabbit_khepri.hrl").
+
+-export([
+         set/2,
+         get/1,
+         get_consistent/1,
+         delete/1
+        ]).
+
+-export([
+         khepri_node_metadata_path/1
+        ]).
+
+-define(KHEPRI_PROJECTION, rabbit_khepri_node_metadata).
+
+%% -------------------------------------------------------------------
+%% set().
+%% -------------------------------------------------------------------
+
+-spec set(node(), map()) -> boolean().
+%% @doc Sets the node metadata map for the given node.
+set(Node, Metadata) when is_map(Metadata) ->
+    Path = khepri_node_metadata_path(Node),
+    Record = #node_metadata{node = Node, metadata = Metadata},
+    case rabbit_khepri:put(Path, Record) of
+        ok ->
+            true;
+        Error ->
+            ?LOG_WARNING("Failed to store node metadata for ~ts in Khepri: ~tp",
+                         [Node, Error],
+                         #{domain => ?RMQLOG_DOMAIN_DB}),
+            false
+    end.
+
+%% -------------------------------------------------------------------
+%% get().
+%% -------------------------------------------------------------------
+
+-spec get(node()) -> map().
+%% @doc Returns the node metadata map for the given node using the local ETS projection.
+get(Node) ->
+    try
+        case ets:lookup(?KHEPRI_PROJECTION, Node) of
+            [#node_metadata{metadata = Metadata}] ->
+                Metadata;
+            _ ->
+                #{}
+        end
+    catch
+        error:badarg ->
+            #{}
+    end.
+
+%% -------------------------------------------------------------------
+%% get_consistent().
+%% -------------------------------------------------------------------
+
+-spec get_consistent(node()) -> map().
+%% @doc Returns the node metadata map for the given node using a consistent query.
+get_consistent(Node) ->
+    Path = khepri_node_metadata_path(Node),
+    Options = #{favor => consistency},
+    case rabbit_khepri:get(Path, Options) of
+        {ok, #node_metadata{metadata = Metadata}} ->
+            Metadata;
+        _ ->
+            #{}
+    end.
+
+%% -------------------------------------------------------------------
+%% delete().
+%% -------------------------------------------------------------------
+
+-spec delete(node()) -> boolean().
+%% @doc Deletes the node metadata for the given node.
+delete(Node) ->
+    Path = khepri_node_metadata_path(Node),
+    case rabbit_khepri:delete(Path) of
+        ok -> true;
+        _ -> false
+    end.
+
+%% -------------------------------------------------------------------
+%% Khepri paths
+%% -------------------------------------------------------------------
+
+khepri_node_metadata_path(Node) when ?IS_KHEPRI_PATH_CONDITION(Node) ->
+    ?RABBITMQ_KHEPRI_NODE_METADATA_PATH(Node).
