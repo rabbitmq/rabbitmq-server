27f12d9c3b792388c8ecc4c353a9efc53a4a67cd
Michal Kuratczyk <michal.kuratczyk@broadcom.com>
2026-07-22 21:20:40 +0200

    amqp10_client: handle DOWN in more states
    When running AMQP 1.0 shovels, sometimes they would crash with errors
    like this:
    
    ```
      reason: {function_clause,
                  [{amqp10_client_connection,hdr_sent,
                       [info,
                        {'DOWN',#Ref<0.2037148225.2260205569.233493>,process,
                            <0.29675.0>,normal},
                        {state,0,<0.29672.0>,
                            #Ref<0.2037148225.2260205569.233493>,<0.29676.0>,
                            [{<0.29530.0>,
                              [alias|
                               #Ref<0.0.3779843.2037148225.2260271105.233488>]}],
                            <0.29675.0>,
                            {tcp,#Port<0.396>},
                            undefined,undefined,
                            #{notify => <0.29530.0>,port => 5672,
                              address => "green",
                              sasl =>
                                  {encrypted,
                                      <<"6APRufcR3aNjXGKFDynwAyf37GF3YqrJq06ecxGXPXNcdqSEXEKXoitUXhMTLBRhzdnWAQmdu9lw2aZ5TlxChT04sNM7XiCPZVhVFwKI124OLrxsEKjS0zOvYZc8wAlT02XqNHL4HGv9YHRGNX2aFpYCsLVTdEL4yRA8COvRomXsCDuCJ1XpWNvo/Y6fTjkW9PzfZiOwPWyfyvBx2nVCMw==">>},
                              hostname => <<"green">>,
                              properties =>
                                  #{<<"ignore-maintenance">> => {boolean,true}},
                              notify_when_opened => <0.29530.0>,
                              notify_when_closed => <0.29530.0>,
                              transfer_limit_margin => 0,
                              max_frame_size => 1048576}}],
                       [{file,"amqp10_client_connection.erl"},{line,244}]},
                   {gen_statem,loop_state_callback,11,
                       [{file,"gen_statem.erl"},{line,3735}]},
                   {proc_lib,init_p_do_apply,3,
                       [{file,"proc_lib.erl"},{line,329}]}]}
    ```

--- deps/amqp10_client/src/amqp10_client_connection.erl
+++ deps/amqp10_client/src/amqp10_client_connection.erl
@@ -227,7 +227,10 @@ sasl_hdr_rcvds(_EvtType, #'v1_0.sasl_mechanisms'{
 sasl_hdr_rcvds({call, From}, begin_session,
                #state{pending_session_reqs = PendingSessionReqs} = State) ->
     State1 = State#state{pending_session_reqs = [From | PendingSessionReqs]},
-    {keep_state, State1}.
+    {keep_state, State1};
+sasl_hdr_rcvds(info, {'DOWN', MRef, process, _Pid, _},
+               #state{reader_m_ref = MRef}) ->
+    {stop, {shutdown, reader_down}}.
 
 sasl_init_sent(_EvtType, #'v1_0.sasl_outcome'{code = {ubyte, 0}},
                #state{socket = Socket} = State) ->
@@ -239,7 +242,10 @@ sasl_init_sent(_EvtType, #'v1_0.sasl_outcome'{code = {ubyte, C}},
 sasl_init_sent({call, From}, begin_session,
                #state{pending_session_reqs = PendingSessionReqs} = State) ->
     State1 = State#state{pending_session_reqs = [From | PendingSessionReqs]},
-    {keep_state, State1}.
+    {keep_state, State1};
+sasl_init_sent(info, {'DOWN', MRef, process, _Pid, _},
+               #state{reader_m_ref = MRef}) ->
+    {stop, {shutdown, reader_down}}.
 
 hdr_sent(_EvtType, {protocol_header_received, 0, 1, 0, 0}, State) ->
     case send_open(State) of
@@ -254,7 +260,10 @@ hdr_sent(_EvtType, {protocol_header_received, Protocol, Maj, Min,
 hdr_sent({call, From}, begin_session,
          #state{pending_session_reqs = PendingSessionReqs} = State) ->
     State1 = State#state{pending_session_reqs = [From | PendingSessionReqs]},
-    {keep_state, State1}.
+    {keep_state, State1};
+hdr_sent(info, {'DOWN', MRef, process, _Pid, _},
+         #state{reader_m_ref = MRef}) ->
+    {stop, {shutdown, reader_down}}.
 
 open_sent(_EvtType, #'v1_0.open'{max_frame_size = MaybeMaxFrameSize,
                                  idle_time_out = Timeout} = Open,
