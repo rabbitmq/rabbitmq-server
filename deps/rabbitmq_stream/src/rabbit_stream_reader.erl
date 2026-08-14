04f520566e0864c6d3ad8c4362515e51a1274309
Arnaud Cogoluègnes <514737+acogoluegnes@users.noreply.github.com>
2026-08-14 16:22:01 +0200

    Update metrics, CLI, and management UI
    For the credit unit.

--- deps/rabbitmq_stream/src/rabbit_stream_reader.erl
+++ deps/rabbitmq_stream/src/rabbit_stream_reader.erl
@@ -3258,6 +3258,7 @@ maybe_dispatch_on_subscription(Transport,
                                                    stream_r(Stream, Connection),
                                                    SubscriptionId,
                                                    Credit1,
+                                                   consumer_i(credit_unit, ConsumerState1),
                                                    messages_consumed(ConsumerCounters1),
                                                    ConsumerOffset,
                                                    ConsumerOffsetLag,
@@ -3282,13 +3283,15 @@ maybe_dispatch_on_subscription(_Transport,
                      [SubscriptionId]),
     #consumer{credit = Credit,
               configuration =
-                  #consumer_configuration{offset = Offset, active = Active}} =
+                  #consumer_configuration{offset = Offset, active = Active,
+                                          credit_unit = CreditUnit}} =
         ConsumerState,
 
     rabbit_stream_metrics:consumer_created(self(),
                                            stream_r(Stream, Connection),
                                            SubscriptionId,
                                            Credit,
+                                           CreditUnit,
                                            0, %% messages consumed
                                            Offset,
                                            0, %% offset lag
@@ -4153,6 +4156,7 @@ emit_stats(#stream_connection{publishers = Publishers} = Connection,
                                             stream_r(S, Connection),
                                             Id,
                                             Credit,
+                                            CreditUnit,
                                             messages_consumed(Counters),
                                             consumer_offset(Counters),
                                             consumer_i(offset_lag, Consumer),
@@ -4163,7 +4167,8 @@ emit_stats(#stream_connection{publishers = Publishers} = Connection,
                                               subscription_id = Id,
                                               counters = Counters,
                                               active = Active,
-                                              properties = Properties},
+                                              properties = Properties,
+                                              credit_unit = CreditUnit},
                   credit = Credit} =
             Consumer
             <- maps:values(Consumers)],
@@ -4218,6 +4223,10 @@ consumer_i(subscription_id,
     SubId;
 consumer_i(credits, #consumer{credit = Credits}) ->
     Credits;
+consumer_i(credit_unit,
+           #consumer{configuration =
+                         #consumer_configuration{credit_unit = Unit}}) ->
+    Unit;
 consumer_i(messages_consumed,
            #consumer{configuration =
                          #consumer_configuration{counters = Counters}}) ->
