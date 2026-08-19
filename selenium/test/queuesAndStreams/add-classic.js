7e34a76df499aa6659fe65b039bbf4e3b36e70e5
Marcial Rosales <marcial.rosales@broadcom.com>
2026-08-19 10:08:45 +0200

    Replace delay for doUntil
    (cherry picked from commit 7c207dcfd6f989673034c8d78229c58163d4e6d4)

--- selenium/test/queuesAndStreams/add-classic.js
+++ selenium/test/queuesAndStreams/add-classic.js
@@ -1,6 +1,6 @@
 const { By, Key, until, Builder } = require('selenium-webdriver')
 const assert = require('assert')
-const { buildDriver, goToHome, captureScreensFor, teardown, delay } = require('../utils')
+const { buildDriver, goToHome, captureScreensFor, teardown, doUntil } = require('../utils')
 
 const LoginPage = require('../pageobjects/LoginPage')
 const OverviewPage = require('../pageobjects/OverviewPage')
@@ -39,9 +39,12 @@ describe('Classic queues', function () {
     await queuesAndStreams.ensureAddQueueSectionIsVisible()
     
     await queuesAndStreams.fillInAddNewQueue({"name" : queueName, "type" : "classic"})
-    await delay(5000)
     await queuesAndStreams.filterQueues(queueName)
-    await delay(2000)
+    await doUntil(async function () {
+      return queuesAndStreams.getQueuesTable(5)
+    }, function (table) {
+      return table.length > 0
+    }, 2000)
     let table = await queuesAndStreams.getQueuesTable(5)
     assert.equal(1, table.length)
     assert.equal(table[0][0], '/')
