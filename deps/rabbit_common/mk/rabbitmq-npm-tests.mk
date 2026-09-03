2e81384bfe9a57ec76264740adc51399578f5ede
Marcial Rosales <marcial.rosales@broadcom.com>
2026-09-03 09:50:57 +0200

    Run tests under /test folder on each plugin
    (cherry picked from commit b7607323ed4ea9d4a141d836a8460cd26aa72e09)

--- deps/rabbit_common/mk/rabbitmq-npm-tests.mk
+++ deps/rabbit_common/mk/rabbitmq-npm-tests.mk
@@ -6,7 +6,7 @@ ifneq ($(wildcard $(CURDIR)/package.json),)
 .PHONY: npm-test
 npm-test:
 	@if command -v npm > /dev/null 2>&1; then \
-		cd $(CURDIR) && npm install && npm test; \
+		cd $(CURDIR) && npm install && npm test -- 'test/**/*.test.js'; \
 	else \
 		echo "Skipped JavaScript tests for $(PROJECT): npm is not installed locally"; \
 	fi
