7662ebb88d2a16ea5ca792f3c2eec98b18994715
Michael Klishin <michaelklishin@icloud.com>
2026-09-03 18:12:35 -0700

    Add missing license headers to JavaScript tests #17287

--- deps/rabbitmq_management/test/formatters.test.js
+++ deps/rabbitmq_management/test/formatters.test.js
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2007-2026 Broadcom. All Rights Reserved. The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries. All rights reserved.
+
 import { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
 import fs from 'node:fs';
