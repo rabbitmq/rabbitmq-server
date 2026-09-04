9aee36598275841f3bc972fa693a9acd358deb04
Michael Klishin <michaelklishin@icloud.com>
2026-09-03 18:04:15 -0700

    Add missing license headers #17287
    (cherry picked from commit 2d5b3e6ca30fa3428bce697a978348cdddce3d13)

--- deps/rabbitmq_management/src/rabbit_mgmt_wm_init.erl
+++ deps/rabbitmq_management/src/rabbit_mgmt_wm_init.erl
@@ -1,3 +1,10 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2026 Broadcom. All Rights Reserved. The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries. All rights reserved.
+%%
+
 -module(rabbit_mgmt_wm_init).
 -export([init/2]).
 -export([content_types_provided/2, is_authorized/2, to_json/2]).
