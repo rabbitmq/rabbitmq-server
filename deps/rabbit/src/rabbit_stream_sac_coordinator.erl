81d9858946a49c836175d5ef8f1553095424ab10
Michael Klishin <mikhail.klishin@broadcom.com>
2026-09-10 17:49:11 -0700

    Revert "Fix incomplete case clause in SAC coordinator state_enter/2"

--- deps/rabbit/src/rabbit_stream_sac_coordinator.erl
+++ deps/rabbit/src/rabbit_stream_sac_coordinator.erl
@@ -1206,9 +1206,7 @@ state_enter(leader, #?MODULE{groups = Groups,
                     %% 10 seconds is arbitrary, nothing specific about the value
                     10_000;
              T when T > DisTimeout ->
-                 DisTimeout;
-             T ->
-                 T
+                 DisTimeout
          end,
          node_disconnected_timer_effect(P, Time)
      end || P := Ts <- maps:iterator(DisConns, ordered)];
