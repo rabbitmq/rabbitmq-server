d1b7dce43a91f60ce1d7f344590682041d143a8b
Michal Kuratczyk <michal.kuratczyk@broadcom.com>
2026-09-11 13:45:05 +0200

    Don't auto-remove nodes with Kubernetes, etcd and consul PD
    When only_log_warning=false is used (which is not the default),
    nodes not returned by the discovery backend are automatically removed
    from the cluster. However, k8s, consul and etcd peer discovery backends
    always return only 1 node, so any unreachable node (eg. during a network
    partition) could be removed from the cluster, leading to more problems.

--- deps/rabbitmq_peer_discovery_consul/src/rabbit_peer_discovery_consul.erl
+++ deps/rabbitmq_peer_discovery_consul/src/rabbit_peer_discovery_consul.erl
@@ -15,7 +15,7 @@
 -include("rabbit_peer_discovery_consul.hrl").
 
 -export([init/0, list_nodes/0, supports_registration/0, register/0, unregister/0,
-         post_registration/0, lock/1, unlock/1]).
+         post_registration/0, lock/1, unlock/1, reports_all_nodes/0]).
 -export([send_health_check_pass/0]).
 -export([session_ttl_update_callback/1]).
 %% for debugging from the REPL
@@ -98,6 +98,11 @@ list_nodes() ->
            end,
     rabbit_peer_discovery_util:maybe_backend_configured(?BACKEND_CONFIG_KEY, Fun0, Fun1, Fun2).
 
+
+-spec reports_all_nodes() -> boolean().
+
+reports_all_nodes() ->
+    false.
 
 -spec supports_registration() -> boolean().
 
