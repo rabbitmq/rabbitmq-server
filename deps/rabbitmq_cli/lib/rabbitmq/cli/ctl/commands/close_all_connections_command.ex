c2bb1bd0ffe6e289556eecf7d458add0b8083e4b
Loïc Hoguin <loic.hoguin@broadcom.com>
2026-09-17 15:09:02 +0200

    Call rabbit_connection_tracking directly from close_all_connections CLI cmd
    rabbit_connection_tracking_handler:close_connections/3 is a pure
    delegating shim kept for compatibility with servers older than
    3.7.15. No supported server is that old, so call the real
    implementation directly.
    
    We can't drop the shim for now as previous nodes may call it.

--- deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/close_all_connections_command.ex
+++ deps/rabbitmq_cli/lib/rabbitmq/cli/ctl/commands/close_all_connections_command.ex
@@ -44,10 +44,7 @@ defmodule RabbitMQ.CLI.Ctl.Commands.CloseAllConnectionsCommand do
       _ ->
         :rabbit_misc.rpc_call(
           node_name,
-          # As of 3.7.15, this function was moved to the rabbit_connection_tracking module.
-          # rabbit_connection_tracking_handler still contains a delegating function with the same name.
-          # Continue using this one for now for maximum CLI/server version compatibility. MK.
-          :rabbit_connection_tracking_handler,
+          :rabbit_connection_tracking,
           :close_connections,
           [conns, explanation, delay]
         )
