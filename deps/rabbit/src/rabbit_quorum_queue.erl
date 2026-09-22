d64a0e0d42c96b0be320630c3e6c4b6e3c480af4
Michael Klishin <michaelklishin@icloud.com>
2026-09-21 18:00:03 -0700

    Relax a pattern that does not have to be so strict

--- deps/rabbit/src/rabbit_quorum_queue.erl
+++ deps/rabbit/src/rabbit_quorum_queue.erl
@@ -387,8 +387,8 @@ start_cluster(Q) ->
 
             try
                 %% Khepri projections on remote nodes are eventually consistent.
-                %% A timeout must go through the same cleanup as a failed
-                %% Ra cluster start, otherwise the queue metadata remains.
+                %% This call is inside the try so that a timeout is cleaned up
+                %% like a failed Ra cluster start.
                 rabbit_queue_type_util:wait_for_projection(LeaderNode, QName),
                 erpc_call(LeaderNode, ra, start_cluster,
                           [?RA_SYSTEM, RaConfs, ?START_CLUSTER_TIMEOUT],
@@ -418,7 +418,7 @@ start_cluster(Q) ->
                 {error, Error} ->
                     declare_queue_error(Error, NewQ, LeaderNode, ActingUser)
             catch
-                exit:{timeout, wait_for_remote_projection, LeaderNode, QName} = Error ->
+                exit:{timeout, wait_for_remote_projection, _, _} = Error ->
                     declare_queue_error(Error, NewQ, LeaderNode, ActingUser);
                 error:Error ->
                     declare_queue_error(Error, NewQ, LeaderNode, ActingUser)
@@ -2949,3 +2949,4 @@ annotate_delivery_time(Msg) ->
         false ->
             Msg
     end.
+
