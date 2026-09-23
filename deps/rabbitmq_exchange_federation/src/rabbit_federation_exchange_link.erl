240df78a3824c338db8108a4d7a7151d3ce8443e
Shashank <shashank-ta.singh@broadcom.com>
2026-09-23 12:25:05 +0530

    Register a default consumer on the exchange federation link's upstream channel

--- deps/rabbitmq_exchange_federation/src/rabbit_federation_exchange_link.erl
+++ deps/rabbitmq_exchange_federation/src/rabbit_federation_exchange_link.erl
@@ -664,6 +664,10 @@ consume_from_upstream_queue(
         false -> amqp_channel:call(Ch, #'basic.qos'{prefetch_count = Prefetch});
         true  -> ok
     end,
+    %% Without this, an unmatched basic.cancel/basic.deliver (e.g. from a
+    %% forced disconnect) crashes amqp_selective_consumer instead of
+    %% reaching handle_info/2.
+    ok = amqp_selective_consumer:register_default_consumer(Ch, self()),
     #'basic.consume_ok'{consumer_tag = CTag} =
         amqp_channel:subscribe(Ch, #'basic.consume'{queue  = Q,
                                                     no_ack = NoAck}, self()),
